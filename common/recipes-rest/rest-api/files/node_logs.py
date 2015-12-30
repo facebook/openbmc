34413bc35bd82351a7d58a8943db9953b1bebcb7
Sai Dasari <sdasari@fb.com>
2015-12-30 11:35:32 -0800

    Add logs node support
    Reviewed By: jmehta
    
    Differential Revision: D2794682
    
    fbshipit-source-id: af634c2

---
+++ common/recipes-rest/rest-api/files/node_logs.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+#
+# Copyright 2015-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+import re
+from subprocess import *
+from node import node
+
+class logsNode(node):
+    def __init__(self, name, info = None, actions = None):
+        self.name = name
+
+        if info == None:
+            self.info = {}
+        else:
+            self.info = info
+        if actions == None:
+            self.actions = []
+        else:
+            self.actions = actions
+
+    def getInformation(self):
+        linfo = []
+        cmd = '/usr/local/bin/log-util ' + self.name +' --print'
+        data = Popen(cmd, shell=True, stdout=PIPE).stdout.read()
+        sdata = data.split('\n')
+        for line in sdata:
+            # skip lines with --- or startin with FRU
+            if line.startswith( 'FRU' ):
+                continue
+
+            # skip empty lines
+            if line in ['\n', '\r\n']:
+                continue
+
+            line = line.rstrip()
+            cells = re.split("\s{2,}", line, maxsplit=4)
+            if (len(cells) != 5):
+                continue
+
+            temp = {
+                    "FRU#": cells[0],
+                    "FRU_NAME": cells[1],
+                    "TIME_STAMP": cells[2],
+                    "APP_NAME": cells[3],
+                    "MESSAGE": cells[4],
+                   }
+
+            linfo.append(temp)
+
+        result = { "Logs": linfo }
+        return result
+
+    def doAction(self, data):
+        if data["action"] != "clear":
+            res = 'failure'
+        else:
+            cmd = '/usr/local/bin/log-util ' + self.name +' --clear'
+            data = Popen(cmd, shell=True, stdout=PIPE).stdout.read()
+            if data.startswith( 'Usage' ):
+                res = 'failure'
+            else:
+                res = 'success'
+
+        result = { "result": res }
+
+        return result
+
+def get_node_logs(name):
+    actions = [ "clear" ]
+    return logsNode(name = name, actions = actions)

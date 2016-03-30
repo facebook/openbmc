5f77b5cae514cfacfd28254600d8dee7eb58173a
Jubin Mehta <jmehta@fb.com>
2016-03-29 22:19:05 -0700

    Add support for kv set and get
    Differential Revision: D3112462
    
    fb-gh-sync-id: 137bc26
    fbshipit-source-id: 137bc26

---
+++ common/recipes-lib/kv/files/kv.c
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2015-present Facebook. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <string.h>
+#include <sys/file.h>
+#include "kv.h"
+
+
+#define MAX_KEY_PATH_LEN  96
+#define MAX_KEY_LEN       64
+#define MAX_VALUE_LEN     64
+
+#define KV_STORE "/mnt/data/kv_store/%s"
+#define KV_STORE_PATH "/mnt/data/kv_store"
+
+int
+kv_set(char *key, char *value) {
+
+  FILE *fp;
+  int rc;
+  char kpath[MAX_KEY_PATH_LEN] = {0};
+
+  sprintf(kpath, KV_STORE, key);
+
+  if (access(KV_STORE_PATH, F_OK) == -1) {
+    mkdir(KV_STORE_PATH, 0777);
+  }
+
+  fp = fopen(kpath, "w");
+  if (!fp) {
+    int err = errno;
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_set: failed to open %s", kpath);
+#endif
+    return err;
+  }
+
+  rc = flock(fileno(fp), LOCK_EX);
+  if (rc < 0) {
+    int err = errno;
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_set: failed to flock on %s, err %d", kpath, err);
+#endif
+    fclose(fp);
+    return -1;
+  }
+
+  rc = fwrite(value, 1, strlen(value), fp);
+  if (rc < 0) {
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_set: failed to write to %s", kpath;
+#endif
+    fclose(fp);
+    return ENOENT;
+  }
+
+  rc = flock(fileno(fp), LOCK_UN);
+  if (rc < 0) {
+    int err = errno;
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_set: failed to unlock flock on %s, err %d", kpath, err);
+#endif
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  return 0;
+}
+
+
+int
+kv_get(char *key, char *value) {
+
+  FILE *fp;
+  int rc;
+  char kpath[MAX_KEY_PATH_LEN] = {0};
+
+  sprintf(kpath, KV_STORE, key);
+
+  if (access(KV_STORE_PATH, F_OK) == -1) {
+    mkdir(KV_STORE_PATH, 0777);
+  }
+
+  fp = fopen(kpath, "r");
+  if (!fp) {
+    int err = errno;
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_get: failed to open %s, err %d", kpath, err);
+#endif
+    return -1;
+  }
+
+  rc = flock(fileno(fp), LOCK_EX);
+  if (rc < 0) {
+    int err = errno;
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_get: failed to flock %s, err %d", kpath, err);
+#endif
+    fclose(fp);
+    return -1;
+  }
+
+  rc = (int) fread(value, 1, MAX_VALUE_LEN, fp);
+  if (rc <= 0) {
+#ifdef DEBUG
+    syslog(LOG_INFO, "kv_get: failed to read %s", kpath);
+#endif
+    fclose(fp);
+    return ENOENT;
+  }
+
+  rc = flock(fileno(fp), LOCK_UN);
+  if (rc < 0) {
+    int err = errno;
+#ifdef DEBUG
+    syslog(LOG_WARNING, "kv_get: failed to unlock flock on %s, err %d", kpath, err);
+#endif
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  return 0;
+}
---
+++ common/recipes-lib/kv/libkv_0.1.bb
@@ -0,0 +1,41 @@
+# Copyright 2016-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+SUMMARY = "KV Store Library"
+DESCRIPTION = "library for get set of kv pairs"
+SECTION = "base"
+PR = "r1"
+LICENSE = "GPLv2"
+LIC_FILES_CHKSUM = "file://kv.c;beginline=4;endline=16;md5=da35978751a9d71b73679307c4d296ec"
+
+SRC_URI = "file://Makefile \
+           file://kv.c \
+           file://kv.h \
+          "
+
+S = "${WORKDIR}"
+
+do_install() {
+	  install -d ${D}${libdir}
+    install -m 0644 libkv.so ${D}${libdir}/libkv.so
+
+    install -d ${D}${includedir}/openbmc
+    install -m 0644 kv.h ${D}${includedir}/openbmc/kv.h
+}
+
+FILES_${PN} = "${libdir}/libkv.so"
+FILES_${PN}-dev = "${includedir}/openbmc/kv.h"

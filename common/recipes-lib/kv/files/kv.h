2150d4c42e2e84e7ff2f887bc1d161cef20e9bf1
Jubin Mehta <jmehta@fb.com>
2016-03-29 22:19:05 -0700

    Add support for kv set and get
    Reviewed By: sdasari73
    
    Differential Revision: D3112462
    
    fbshipit-source-id: 0544c1c

---
+++ common/recipes-lib/kv/files/kv.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015-present Facebook. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef __KV_H__
+#define __KV_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+#include <stdint.h>
+
+int kv_get(char* key, char *value);
+int kv_set(char* key, char *value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __KV_H__ */

943d2c70f8f944f827556959b078a8c965689a4c
Sai Dasari <sdasari@fb.com>
2016-03-29 18:32:39 -0700

    Add dummy support for DCMI command in PAL
    Reviewed By: okeeblow, siutong
    
    Differential Revision: D3113189
    
    fbshipit-source-id: 43d0df7

--- meta-facebook/meta-lightning/recipes-lightning/fblibs/files/pal/pal.h
+++ meta-facebook/meta-lightning/recipes-lightning/fblibs/files/pal/pal.h
@@ -175,6 +175,7 @@ int pal_get_uart_chan_btn(uint8_t *status);
 int pal_get_uart_chan(uint8_t *status);
 int pal_set_uart_chan(uint8_t status);
 void pal_update_ts_sled();
+int pal_handle_dcmi(uint8_t fru, uint8_t *tbuf, uint8_t tlen, uint8_t *rbuf, uint8_t *rlen);
 
 
 #ifdef __cplusplus

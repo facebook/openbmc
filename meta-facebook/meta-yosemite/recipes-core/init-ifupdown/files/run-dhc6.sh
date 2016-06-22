93e4ca25f4de06d8c0567a16c85bfcd8c55997e5
Vineela Kukkadapu <vineelak@fb.com>
2016-06-21 21:42:45 -0700

    Delete files in Yosemite layer
    Reviewed By: sdasari73
    
    Differential Revision: D3468550
    
    fbshipit-source-id: 81b719b

--- meta-facebook/meta-yosemite/recipes-core/init-ifupdown/files/run-dhc6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-logger "run-dhc6.sh: dhclient -6 Started.."
-pid="/var/run/dhclient6.eth0.pid"
-exec dhclient -6 -d -pf ${pid} eth0 "$@"
--- meta-facebook/meta-yosemite/recipes-core/init-ifupdown/files/setup-dhc6.sh
+++
@@ -1,4 +0,0 @@
-# Short-Description: Setup dhclient for IPv6
-echo -n "Setup dhclient for IPv6... "
-runsv /etc/sv/dhc6 > /dev/null 2>&1 &
-echo "done."

062372c72466dd54993685eebd73d0bef915752c
Jubin Mehta <jmehta@fb.com>
2016-07-20 20:52:04 -0700

    Fix the PDPB_P3V3 sensor value for DVT systems
    Reviewed By: apage43, sdasari73
    
    Differential Revision: D3597805
    
    fbshipit-source-id: 9304269

--- meta-facebook/meta-lightning/recipes-lightning/fblibs/files/pal/pal.c
+++ meta-facebook/meta-lightning/recipes-lightning/fblibs/files/pal/pal.c
@@ -968,3 +968,11 @@ pal_update_ts_sled() {
 int
 pal_handle_dcmi(uint8_t fru, uint8_t *tbuf, uint8_t tlen, uint8_t *rbuf, uint8_t *rlen) {
 }
+
+int
+pal_is_fru_ready(uint8_t fru, uint8_t *status) {
+
+  *status = 1;
+
+  return 0;
+}

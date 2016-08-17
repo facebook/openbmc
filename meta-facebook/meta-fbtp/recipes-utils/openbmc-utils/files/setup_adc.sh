8193f4d62d28f7c8b93aefc505f21bb58de1effa
Sai Dasari <sdasari@fb.com>
2016-08-17 14:00:37 -0700

    Fix adc config for pin 3 and 4
    Reviewed By: sdasari73
    
    Differential Revision: D3731542
    
    fbshipit-source-id: 7a2d03d

--- meta-facebook/meta-fbtp/recipes-utils/openbmc-utils/files/setup_adc.sh
+++ meta-facebook/meta-fbtp/recipes-utils/openbmc-utils/files/setup_adc.sh
@@ -44,8 +44,8 @@ config_adc() {
 config_adc 0  287 200 0
 config_adc 1  536 200 0
 config_adc 2 1580 200 0
-config_adc 3  200 200 0
-config_adc 4  200 200 0
+config_adc 3   10 200 0
+config_adc 4   10 200 0
 config_adc 5  287 200 0
 config_adc 6  536 200 0
 config_adc 7  400 200 0

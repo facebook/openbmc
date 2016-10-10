e50a82030f856e02128e12f40cb05e826e462f2d
Vineela Kukkadapu <vineelak@fb.com>
2016-10-10 10:40:31 -0700

    Add util in platform-wedge layer to reset cp2112
    Reviewed By: tfangit
    
    Differential Revision: D3997043
    
    fbshipit-source-id: 8c497c0

---
+++ meta-facebook/meta-wedge/recipes-wedge/fbutils/files/reset_cp2112.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Copyright 2014-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+source /usr/local/bin/openbmc-utils.sh 
+
+echo -n "Reset CP2112 Switch ... "
+
+gpio_set USB_BRDG_RST 0
+usleep 50000
+gpio_set USB_BRDG_RST 1
+
+echo "Done"

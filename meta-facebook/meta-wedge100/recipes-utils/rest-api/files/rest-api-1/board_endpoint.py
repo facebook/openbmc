cb51c9c7900e6751f3067665485256c5dbdfe8e5
Vineela Kukkadapu <vineelak@fb.com>
2016-10-24 22:25:43 -0700

    Add rest endpoint for CP2112 i2c flush
    Reviewed By: tfangit
    
    Differential Revision: D4073519
    
    fbshipit-source-id: 490743d

--- meta-facebook/meta-wedge100/recipes-utils/rest-api/files/rest-api-1/board_endpoint.py
+++ meta-facebook/meta-wedge100/recipes-utils/rest-api/files/rest-api-1/board_endpoint.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 # Copyright 2014-present Facebook. All Rights Reserved.
 #
 # This program file is free software; you can redistribute it and/or modify it
@@ -14,10 +16,19 @@
 # Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
+#
+import bottle
+import rest_usb2i2c_reset
+import rest_i2cflush
 
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+boardApp = bottle.Bottle()
 
-SRC_URI += "file://rest-api-1/rest_i2cflush.py \
-          "
+# Disable the endpoint in BMC until we root cause cp2112 issues.
+# Handler to reset usb-to-i2c
+#@boardApp.route('/api/sys/usb2i2c_reset')
+#def rest_usb2i2c_reset_hdl():
+#    return rest_usb2i2c_reset.set_usb2i2c()
 
-binfiles += "rest_i2cflush.py"
+@boardApp.route('/api/sys/i2cflush')
+def rest_i2cflush_hdl():
+    return rest_i2cflush.i2cflush()

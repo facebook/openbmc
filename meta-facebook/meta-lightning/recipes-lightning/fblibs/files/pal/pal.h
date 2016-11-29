ace38789d9cc0370e5ea2e5cb47fa37f57da7792
Jubin Mehta <jmehta@fb.com>
2016-11-28 16:43:28 -0800

    Reset ssd mux to recover i2c bus
    Reviewed By: sdasari73
    
    Differential Revision: D4232102
    
    fbshipit-source-id: c268397

--- meta-facebook/meta-lightning/recipes-lightning/fblibs/files/pal/pal.h
+++ meta-facebook/meta-lightning/recipes-lightning/fblibs/files/pal/pal.h
@@ -214,6 +214,7 @@ int pal_fan_recovered_handle(int fan_num);
 int pal_peer_tray_detection(uint8_t *value);
 int pal_self_tray_location(uint8_t *value);
 int pal_is_crashdump_ongoing(uint8_t slot);
+int pal_reset_ssd_switch();
 
 #ifdef __cplusplus
 } // extern "C"

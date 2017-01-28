35d22e374af2fe9476a52bdf0a602ece48f99682
Tian Fang <tfang@fb.com>
2017-01-27 16:45:03 -0800

    Import ODM changes from 1.2.0 to 1.2.3
    Differential Revision: D4481056
    
    fbshipit-source-id: 100d5c3

--- meta-facebook/meta-galaxy100/recipes-utils/openbmc-utils/files/power-on.sh
+++ meta-facebook/meta-galaxy100/recipes-utils/openbmc-utils/files/power-on.sh
@@ -47,7 +47,6 @@ if galaxy100_scm_is_present && [ $soft_reboot -eq 0 ] ; then
 	# First power on TH, and if Panther+ is used,
 	# provide standby power to Panther+.
 	repeater_config
-	KR10G_repeater_config
 	wedge_power_on_board
 
 	echo -n "Checking microserver present status ... "
@@ -65,6 +64,48 @@ if galaxy100_scm_is_present && [ $soft_reboot -eq 0 ] ; then
 	    # Power on now
 	    wedge_power.sh on -f
 	fi
+	KR10G_repeater_config
 elif ! galaxy100_scm_is_present ; then
 	echo "SCM isn't present..."
+fi
+
+
+if galaxy100_scm_is_present ; then
+	echo -n "SCM CPLD access "
+	ver=$(i2cget -f -y 0 0x3e 0x1 2> /dev/null)
+	if [ $? -ne 0 ] || [ $ver = "0xff" ]; then
+		echo "[Fail]"
+	else
+		echo "[OK]"
+	fi
+	if [ $soft_reboot -eq 1 ]; then
+		echo -n "Checking microserver present status ... "
+		if wedge_is_us_on 10 "."; then
+			echo "on"
+		    on=1
+		else
+		    echo "off"
+			on=0
+		fi
+		if [ $on -eq 0 ]; then
+			repeater_config
+			wedge_power_on_board
+		    # Power on now
+		    wedge_power.sh on -f
+			KR10G_repeater_config
+			#reset LPC bridge
+			i2cset -f -y 0 0x3e 0x31 0x0 2> /dev/null
+			usleep 10000
+			i2cset -f -y 0 0x3e 0x31 0x1 2> /dev/null
+		fi
+
+		mac=$(/usr/local/bin/wedge_us_mac.sh)
+		echo -n "COM-e MAC: $mac "
+		if [ $mac = "00:00:00:00:00:00" ]; then
+			echo "[Warning]"
+		else
+			echo "[OK]"
+		fi
+		/usr/local/bin/repeater_verify.sh
+	fi
 fi

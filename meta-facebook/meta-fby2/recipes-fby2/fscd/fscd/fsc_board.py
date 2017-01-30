734eb06e123ccd2a856a1ac1167d01181740e912
Vineela Kukkadapu <vineelak@fb.com>
2017-01-30 14:56:13 -0800

    FSCD refactor - helium fby2 layer
    Summary: in title
    
    Test Plan: unit test
    
    Reviewed By: sdasari73
    
    fbshipit-source-id: 2cfbdbb

---
+++ meta-facebook/meta-fby2/recipes-fby2/fscd/fscd/fsc_board.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#
+# Copyright 2015-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+from fsc_util import Logger
+from ctypes import *
+from subprocess import Popen, PIPE
+
+
+def board_fan_actions(fan, action='None'):
+    '''
+    Override the method to define fan specific actions like:
+    - handling dead fan
+    - handling fan led
+    '''
+    Logger.warn("Fan %d needs action %s" % (fan, str(action),))
+    pass
+
+
+def board_host_actions(action='None', cause='None'):
+    '''
+    Override the method to define fan specific actions like:
+    - handling host power off
+    - alarming/syslogging criticals
+    '''
+    pass
+
+
+def board_callout(callout='None', **kwargs):
+    '''
+    Override this method for defining board specific callouts:
+    - Exmaple chassis intrusion
+    '''
+    if 'init_fans' in callout:
+        boost = 100 # define a boost for the platform or respect fscd override
+        if 'boost' in kwargs:
+            boost = kwargs['boost']
+        return set_all_pwm(boost)
+    else:
+        Logger.warning("Callout %s not handled" % callout)
+    pass
+
+
+def set_all_pwm(boost):
+    cmd = ('/usr/local/bin/fan-util --set %d' % (boost))
+    response = Popen(cmd, shell=True, stdout=PIPE).stdout.read()
+    return response
---
+++ meta-facebook/meta-fby2/recipes-fby2/fscd/fscd_0.2.bbappend
@@ -0,0 +1,33 @@
+# Copyright 2014-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+
+SRC_URI += "file://init_pwm.sh \
+            file://setup-fan.sh \
+            file://fsc-config.json \
+            file://zone1.fsc \
+            file://fsc_board.py \
+           "
+
+FSC_BIN_FILES += "init_pwm.sh"
+
+FSC_CONFIG += "fsc-config.json"
+
+FSC_ZONE_CONFIG +="zone1.fsc"
+
+FSC_INIT_FILE += "setup-fan.sh"

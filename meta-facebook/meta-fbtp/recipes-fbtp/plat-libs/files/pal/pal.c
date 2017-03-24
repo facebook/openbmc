612b76bdeb4ea6b74e6a81f025eb0a93b455a93d
Amithash Prasad <amithash@fb.com>
2017-03-24 15:23:13 -0700

    Correctly return the health status of FRU from kv store
    Summary: Fix errors 'nr_health_monitor - kv get health status failed' logged into /var/log/messages
    
    Test Plan: Boot up and ensure error messages are no longer seen
    
    Reviewed By: sdasari73
    
    fbshipit-source-id: 37e490a

--- meta-facebook/meta-fbtp/recipes-fbtp/plat-libs/files/pal/pal.c
+++ meta-facebook/meta-fbtp/recipes-fbtp/plat-libs/files/pal/pal.c
@@ -5432,11 +5432,30 @@ msleep(int msec) {
   }
 }
 
+static int
+pal_get_sensor_health_key(uint8_t fru, char *key)
+{
+  switch (fru) {
+    case FRU_MB:
+      sprintf(key, "server_sernsor_health");
+      break;
+    case FRU_NIC:
+      sprintf(key, "nic_sensor_health");
+      break;
+    default:
+      return -1;
+  }
+  return 0;
+}
+
 int
 pal_set_sensor_health(uint8_t fru, uint8_t value) {
 
   char key[MAX_KEY_LEN] = {0};
   char cvalue[MAX_VALUE_LEN] = {0};
+
+  if (pal_get_sensor_health_key(fru, key))
+    return -1;
 
   sprintf(cvalue, (value > 0) ? "1": "0");
 
@@ -5450,18 +5469,29 @@ pal_get_fru_health(uint8_t fru, uint8_t *value) {
   char key[MAX_KEY_LEN] = {0};
   int ret;
 
+  if (pal_get_sensor_health_key(fru, key)) {
+    syslog(LOG_INFO, "pal_get_fru_health(%d): unsupported fru\n", fru);
+    return -1;
+  }
+
   ret = pal_get_key_value(key, cvalue);
   if (ret) {
+    syslog(LOG_INFO, "pal_get_fru_health(%s): getting value for %s failed\n", fru, key);
     return ret;
   }
 
   *value = atoi(cvalue);
 
-  memset(key, 0, MAX_KEY_LEN);
+  if (fru != FRU_MB)
+    return 0;
+
+  // If MB, get SEL error status.
+  sprintf(key, "server_sel_error");
   memset(cvalue, 0, MAX_VALUE_LEN);
 
   ret = pal_get_key_value(key, cvalue);
   if (ret) {
+    syslog(LOG_INFO, "pal_get_fru_health(%s): getting value for %s failed\n", fru, key);
     return ret;
   }
 

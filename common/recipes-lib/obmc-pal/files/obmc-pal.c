3d12facdb2d92e735a598e820c487088d50f8353
Sai Dasari <sdasari@fb.com>
2017-05-03 18:16:54 -0700

    obmc-pal: Add dummy function for set ppin command
    Summary: Add dummy function for set ppin command in obmc-pal.c
    
    Test Plan: Build pass.
    
    Reviewed By: amithash
    
    fbshipit-source-id: b4e07a2

--- common/recipes-lib/obmc-pal/files/obmc-pal.c
+++ common/recipes-lib/obmc-pal/files/obmc-pal.c
@@ -103,3 +103,8 @@ pal_parse_oem_sel(uint8_t fru, uint8_t *sel, char *error_log)
   return 0;
 }
 
+int __attribute__((weak))
+pal_set_ppin_info(uint8_t slot, uint8_t *req_data, uint8_t req_len, uint8_t *res_data, uint8_t *res_len)
+{
+	return PAL_EOK;
+}
--- common/recipes-lib/obmc-pal/files/obmc-pal.h
+++ common/recipes-lib/obmc-pal/files/obmc-pal.h
@@ -47,7 +47,7 @@ void pal_set_post_start(uint8_t slot, uint8_t *req_data, uint8_t *res_data, uint
 void pal_set_post_end(uint8_t slot, uint8_t *req_data, uint8_t *res_data, uint8_t *res_len);
 int pal_get_board_id(uint8_t slot, uint8_t *req_data, uint8_t req_len, uint8_t *res_data, uint8_t *res_len);
 int pal_parse_oem_sel(uint8_t fru, uint8_t *sel, char *error_log);
-
+int pal_set_ppin_info(uint8_t slot, uint8_t *req_data, uint8_t req_len, uint8_t *res_data, uint8_t *res_len);
 #ifdef __cplusplus
 }
 #endif

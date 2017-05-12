8269c030ef9cfd28dcb6545cc5366d506b9e5d9c
Sai Dasari <sdasari@fb.com>
2017-05-12 09:28:51 -0700

    obmc-pal: Fix nightly build failures
    Summary: per title.
    
    Test Plan: Build
    
    Reviewed By: sdasari73
    
    fbshipit-source-id: 77b7d78

--- common/recipes-lib/obmc-pal/files/obmc-pal.c
+++ common/recipes-lib/obmc-pal/files/obmc-pal.c
@@ -166,6 +166,8 @@ pal_set_ppin_info(uint8_t slot, uint8_t *req_data, uint8_t req_len, uint8_t *res
 {
 	return PAL_EOK;
 }
+
+int __attribute__((weak))
 pal_get_poss_pcie_config(uint8_t slot, uint8_t *req_data, uint8_t req_len, uint8_t *res_data, uint8_t *res_len)
 {
   return PAL_EOK;

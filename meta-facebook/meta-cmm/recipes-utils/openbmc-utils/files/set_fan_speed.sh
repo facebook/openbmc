f7e60010ce0023b3a3eb8d696d6fac4167f4c742
Nicholas Gott <ngott@fb.com>
2017-05-30 14:35:14 -0700

    set fan now includes setting a specific fan number
    Summary: Added setting a single fan on cmm
    
    Test Plan:
    here is an example run
    
    sh set_fan_speed.sh 10 8
    Successfully set fan 2 on bus 187 speed to 10%
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 58a578a

--- meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/set_fan_speed.sh
+++ meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/set_fan_speed.sh
@@ -17,25 +17,53 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
+
+# There are 4 busses and 3 fans on each bus. The fans are labeled 1-12 in the
+# following order: Bus 171 fan 1,2,3; Bus 179 fan 4,5,6; Bus 187 fan 7,8,9; and
+# Bus 195 fan 10,11,12.
 usage() {
-    echo "Usage: $0 <PERCENT (0..100)>" >&2
+    echo "Usage: $0 <PERCENT (0..100)>  <Fan ID (1..12)>" >&2
 }
 
 set -e
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ] && [ "$#" -ne 1 ]; then
     usage
     exit 1
+fi
+
+if [ "$2" -le 0 ] || ["$2" -ge 13]; then
+  echo "Fan id $2: not a valid id"
+  exit 1
+fi
+
+busses="171 179 187 195"
+if [ "$#" -eq 1 ]; then
+    ids="1 2 3"
+else
+    FAN="$2"
+    FAN=$((${FAN} - 1))
+    busIndex=$((${FAN} / 3))
+    temp=$((${FAN} - ( ${FAN} / 3 * 3 )))
+    ids=$((${temp} + 1))
+    if [ ${busIndex} -eq 0 ]; then
+      busses="171"
+    elif [ ${busIndex} -eq 1 ]; then
+      busses="179"
+    elif [ ${busIndex} -eq 2 ]; then
+      busses="187"
+    else
+      busses="195"
+    fi
 fi
 
 # Convert the percentage to our 1/32th unit (0-31).
 unit=$(( ( $1 * 31 ) / 100 ))
 
-busses="171 179 187 195"
 for bus in ${busses}; do
-    for id in 1 2 3; do
+    for id in ${ids}; do
         pwm="/sys/class/i2c-dev/i2c-${bus}/device/${bus}-0033/fantray${id}_pwm"
         echo "$unit" > "${pwm}"
+        echo "Successfully set fan ${id} on bus ${bus} speed to $1%"
     done
 done
-echo "Successfully set fans' speed to $1%"

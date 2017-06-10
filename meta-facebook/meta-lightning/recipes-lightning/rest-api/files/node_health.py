b42295a1a3f2d16e62bfc7547873126848ee7a0a
Jubin Mehta <jmehta@fb.com>
2017-06-09 17:32:40 -0700

    lightning: update REST API about enclosure health LED
    Summary:
    1. Modify the file path set in REST API to get enclosure healthy information
       because of all XXX_health file has changed from /mnt/data/kv_store/ to /tmp/
       to make all XXX_health file be volatile.
    2. Add to read the bmc_health file in REST API health node
       to detect the health status about BMC CPU/Memory utilization and ECC.
    
    Test Plan:
    Check REST API behavior: pass
     ---
    
    [root@localhost ~]# curl -x "" http://fe80::ff:fe00:1%enp0s29u1u2u1:8080/api/peb/health | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100    91  100    91    0     0    313      0 --:--:-- --:--:-- --:--:--   314
    {
        "Actions": [],
        "Information": {
            "Status of enclosure health LED": "Good"
        },
        "Resources": []
    }
    
    Reviewed By: jmehta
    
    fbshipit-source-id: 1d14681

--- meta-facebook/meta-lightning/recipes-lightning/rest-api/files/node_health.py
+++ meta-facebook/meta-lightning/recipes-lightning/rest-api/files/node_health.py
@@ -20,17 +20,20 @@ class healthNode(node):
     def getInformation(self):
     
         # Enclosure health LED status (GOOD/BAD)
-        peb_hlth = Popen('cat /mnt/data/kv_store/peb_sensor_health', \
+        peb_hlth = Popen('cat /tmp/peb_sensor_health', \
                             shell=True, stdout=PIPE).stdout.read()
-        pdpb_hlth = Popen('cat /mnt/data/kv_store/pdpb_sensor_health', \
+        pdpb_hlth = Popen('cat /tmp/pdpb_sensor_health', \
                             shell=True, stdout=PIPE).stdout.read()
-        fcb_hlth = Popen('cat /mnt/data/kv_store/fcb_sensor_health', \
+        fcb_hlth = Popen('cat /tmp/fcb_sensor_health', \
+                            shell=True, stdout=PIPE).stdout.read()
+        bmc_hlth = Popen('cat /tmp/bmc_health', \
                             shell=True, stdout=PIPE).stdout.read()
 
-        if ((peb_hlth == "1")&(pdpb_hlth == "1")&(fcb_hlth == "1")):
-           result="Good"
+        if ((peb_hlth == "1") and (pdpb_hlth == "1")
+                and (fcb_hlth == "1") and (bmc_hlth == "1")):
+            result="Good"
         else:
-           result="Bad"
+            result="Bad"
         
         info = {
            "Status of enclosure health LED": result

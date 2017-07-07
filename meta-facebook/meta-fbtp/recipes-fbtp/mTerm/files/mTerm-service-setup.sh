7e572d82c1417e9734ed6931afe7268c24ad41dd
Amithash Prasad <amithash@fb.com>
2017-07-07 14:22:57 -0700

    fbtp: Use mTerm to log BMC console with UART4
    Reviewed By: benwei13
    
    fbshipit-source-id: 4057462

---
+++ meta-facebook/meta-fbtp/recipes-fbtp/mTerm/files/mTerm-service-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# PLATFORM MAY CHOOSE TO OVERRIDE THIS FILE
+echo -n "Starting mTerm console server..."
+runsv /etc/sv/mTerm > /dev/null 2>&1 &
+runsv /etc/sv/mTermBMC > /dev/null 2>&1 &
+echo "done."

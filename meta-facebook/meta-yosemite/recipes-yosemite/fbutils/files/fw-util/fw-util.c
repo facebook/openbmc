8b0950800c8bb09878db3442ed9e8604044d42c1
Ben Wei <benwei@fb.com>
2017-08-24 17:48:51 -0700

    Yosemite: add NIC firmware version to fw-util
    Summary: add display nic version in fw-util
    
    Test Plan: verified on Yosemite
    
    Reviewed By: amithash
    
    fbshipit-source-id: 2e44c08

--- meta-facebook/meta-yosemite/recipes-yosemite/fbutils/files/fw-util/fw-util.c
+++ meta-facebook/meta-yosemite/recipes-yosemite/fbutils/files/fw-util/fw-util.c
@@ -30,18 +30,68 @@
 #include <openbmc/pal.h>
 #include <openbmc/ipmi.h>
 
+
+#define NCSI_DATA_PAYLOAD 64
+#define NIC_FW_VER_PATH "/tmp/cache_store/nic_fw_ver"
+
+#define MAX_NUM_OPTIONS 6
+enum {
+  OPT_SLOT1 = 1,
+  OPT_SLOT2 = 2,
+  OPT_SLOT3 = 3,
+  OPT_SLOT4 = 4,
+  OPT_NIC   = 5,
+  OPT_ALL   = 6,
+};
+
+typedef struct
+{
+  char  mfg_name[10];  //manufacture name
+  uint32_t mfg_id;     //manufacture id
+  void (*get_nic_fw)(uint8_t *, char *);
+} nic_info_st;
+
 static void
 print_usage_help(void) {
-  printf("Usage: fw-util <all|slot1|slot2|slot3|slot4> <--version>\n");
-  printf("       fw-util <all|slot1|slot2|slot3|slot4> <--update> <--cpld|--bios|--bic|--bicbl> <path>\n");
+  printf("Usage: fw-util <all|slot1|slot2|slot3|slot4|nic> <--version>\n");
+  printf("       fw-util <all|slot1|slot2|slot3|slot4|nic> <--update> <--cpld|--bios|--bic|--bicbl> <path>\n");
 }
 
-// TODO: Need to confirm the interpretation of firmware version for print
-// Right now using decimal to print the versions
 static void
-print_fw_ver(uint8_t slot_id) {
+get_nic_fw_version(uint8_t *buf, char *version) {
+  int ver_index_based = 20;
+  int major = 0;
+  int minor = 0;
+  int revision = 0;
+
+  major = buf[ver_index_based++];
+  minor = buf[ver_index_based++];
+  revision = buf[ver_index_based++] << 8;
+  revision += buf[ver_index_based++];
+
+  sprintf(version, "%d.%d.%d", major, minor, revision);
+}
+
+static nic_info_st support_nic_list[] =
+{
+  {"Mellanox", 0x19810000, get_nic_fw_version},
+  {"Broadcom", 0x3D110000, get_nic_fw_version}
+};
+
+int nic_list_size = sizeof(support_nic_list) / sizeof(nic_info_st);
+
+
+static void
+print_slot_version(uint8_t slot_id) {
   int i;
   uint8_t ver[32] = {0};
+  uint8_t status;
+  int ret;
+
+  if (slot_id < OPT_SLOT1 || slot_id > OPT_SLOT4) {
+    printf("Not Supported Operation\n");
+    return;
+  }
 
   // Print CPLD Version
   if (bic_get_fw_ver(slot_id, FW_CPLD, ver)) {
@@ -116,8 +166,80 @@ print_fw_ver(uint8_t slot_id) {
   for (i = 3; i < 3+ver[2]; i++) {
     printf("%c", ver[i]);
   }
-    printf("\n");
+  printf("\n");
 }
+
+
+static void
+print_nic_version(uint8_t slot_id) {
+  // Need to read nic card firmware version for print
+  // There are 1 nic card vendor,MEZZ
+  char display_nic_str[128]={0};
+  char version[32]={0};
+  char vendor[32]={0};
+  uint8_t buf[NCSI_DATA_PAYLOAD]={0};
+  uint32_t nic_mfg_id=0;
+  FILE *file = NULL;
+  bool is_unknown_mfg_id = true;
+  int current_nic;
+
+  file = fopen(NIC_FW_VER_PATH, "rb");
+  if ( NULL != file ) {
+    fread(buf, sizeof(uint8_t), NCSI_DATA_PAYLOAD, file);
+    fclose(file);
+  }
+  else {
+    syslog(LOG_WARNING, "[%s]Cannot open the file at %s",__func__, NIC_FW_VER_PATH);
+  }
+
+  //get the manufcture id
+  nic_mfg_id = (buf[35]<<24) + (buf[34]<<16) + (buf[33]<<8) + buf[32];
+
+  for ( current_nic=0; current_nic <nic_list_size; current_nic++) {
+    //check the nic on the system is supported or not
+    if ( support_nic_list[current_nic].mfg_id == nic_mfg_id ) {
+      sprintf(vendor, support_nic_list[current_nic].mfg_name);
+      support_nic_list[current_nic].get_nic_fw(buf, version);
+      is_unknown_mfg_id = false;
+      break;
+    }
+    else {
+      is_unknown_mfg_id = true;
+    }
+  }
+
+  if ( is_unknown_mfg_id ) {
+    sprintf(display_nic_str ,"NIC firmware version: NA (Unknown Manufacture ID: 0x%04x)", nic_mfg_id);
+  }
+  else {
+    sprintf(display_nic_str ,"%s NIC firmware version: %s", vendor, version);
+  }
+  printf("%s\n", display_nic_str);
+}
+
+
+// TODO: Need to confirm the interpretation of firmware version for print
+// Right now using decimal to print the versions
+static void
+print_fw_ver(uint8_t slot_id) {
+  int i;
+  uint8_t ver[32] = {0};
+
+  switch(slot_id) {
+    case OPT_SLOT1:
+    case OPT_SLOT2:
+    case OPT_SLOT3:
+    case OPT_SLOT4:
+      printf("Get version info for slot%d\n", slot_id);
+      print_slot_version(slot_id);
+      break;
+    case OPT_NIC:
+      print_nic_version(slot_id);
+      break;
+  }
+
+}
+
 
 static int
 check_dup_process(uint8_t slot_id) {
@@ -189,25 +311,27 @@ main(int argc, char **argv) {
 
   // Derive slot_id from first parameter
   if (!strcmp(argv[1], "slot1")) {
-    slot_id = 1;
+    slot_id = OPT_SLOT1;
   } else if (!strcmp(argv[1] , "slot2")) {
-    slot_id =2;
+    slot_id =OPT_SLOT2;
   } else if (!strcmp(argv[1] , "slot3")) {
-    slot_id =3;
+    slot_id =OPT_SLOT3;
   } else if (!strcmp(argv[1] , "slot4")) {
-    slot_id =4;
+    slot_id =OPT_SLOT4;
+  } else if (!strcmp(argv[1] , "nic")) {
+    slot_id = OPT_NIC;
   } else if (!strcmp(argv[1] , "all")) {
-    slot_id =5;
+    slot_id =OPT_ALL;
   } else {
       goto err_exit;
   }
   // check operation to perform
   if (!strcmp(argv[2], "--version")) {
-     if (slot_id < 5) {
+     if (slot_id < OPT_ALL) {
        print_fw_ver(slot_id);
        return 0;
      }
-     for (slot_id = 1; slot_id < 5; slot_id++) {
+     for (slot_id = 1; slot_id < MAX_NUM_OPTIONS; slot_id++) {
         printf("Get version info for slot%d\n", slot_id);
         print_fw_ver(slot_id);;
         printf("\n");
@@ -218,7 +342,8 @@ main(int argc, char **argv) {
     if (argc != 5) {
       goto err_exit;
     }
-    if (slot_id < 5) {
+    // single slot update
+    if (slot_id <= OPT_SLOT4) {
       if (check_dup_process(slot_id) < 0) {
         printf("fw_util: another instance is running...\n");
         return -1;
@@ -231,7 +356,8 @@ main(int argc, char **argv) {
       return 0;
     }
 
-    for (slot_id = 1; slot_id < 5; slot_id++) {
+    // update ALL slots (minus NIC)
+    for (slot_id = OPT_SLOT1; slot_id <= OPT_SLOT4; slot_id++) {
       if (check_dup_process(slot_id) < 0) {
         printf("fw_util: another instance is running...\n");
         return -1;
@@ -239,7 +365,7 @@ main(int argc, char **argv) {
     }
 
     printf("Updating all slots....\n");
-    for (slot_id = 1; slot_id < 5; slot_id++) {
+    for (slot_id = OPT_SLOT1; slot_id <= OPT_SLOT4; slot_id++) {
        if (fw_update_slot(argv, slot_id)) {
          printf("fw_util: updating %s on slot %d failed!\n", argv[3], slot_id);
          ret++;

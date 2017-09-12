74fdae31c48d4d94f457211bb82be80f2ee3fdf3
Jubin Mehta <jmehta@fb.com>
2017-09-12 13:25:11 -0700

    lightning: Add Tray intrusion status info to RESTful node api/peb
    Summary: `Tray Status` indicates if the tray is pushed IN or pulled OUT.
    
    Test Plan:
    Tested on rtptestaoehead015.snc1
    
    [root@disagghead001.24.snc1 ~]# curl http://fe80::ff:fe00:1%usb0:8080/api/peb | python -m json.t
    ool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   205  100   205    0     0   1370      0 --:--:-- --:--:-- --:--:--  1375
    {
        "Actions": [],
        "Information": {
            "Description": "Lightning PCIe Expansion Board",
            "Tray Location": "Lower Tray",
            "Tray Status": "In"
        },
        "Resources": [
            "fruid",
            "sensors",
            "bmc",
            "health",
            "identify",
            "logs"
        ]
    }
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 3ee40af

--- meta-facebook/meta-lightning/recipes-lightning/rest-api/files/node_peb.py
+++ meta-facebook/meta-lightning/recipes-lightning/rest-api/files/node_peb.py
@@ -7,9 +7,19 @@ def get_node_peb():
     name = pal_get_platform_name()
     location = Popen('cat /tmp/tray_location', \
                      shell=True, stdout=PIPE).stdout.read().strip('\n')
+    data = Popen('cat /sys/class/gpio/gpio108/value', \
+                   shell=True, stdout=PIPE).stdout.read().strip('\n')
+    if data == "0":
+        status = "In"
+    elif data == "1":
+        status = "Out"
+    else:
+        status = "Unknown"
+
     info = {
             "Description": name + " PCIe Expansion Board",
             "Tray Location": location,
+            "Tray Status" : status,
     }
 
     return node(info)

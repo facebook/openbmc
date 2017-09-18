faecc0ca35ab41739c88e86655e55e89d871fa06
Mike Choi <mikechoi@fb.com>
2017-09-18 11:43:53 -0700

    Fix CMM's board-specific REST API handler (not common)
    Summary: Fix CMM's board-specific REST API handler, to run asynchronously
    
    Test Plan: Tested and verified on fsw001-p030/34/37/41/46/52.f01.ftw2
    
    Reviewed By: QueryErrorException
    
    fbshipit-source-id: 58db72f

--- meta-facebook/meta-cmm/recipes-utils/rest-api/files/rest-api-1/board_endpoint.py
+++ meta-facebook/meta-cmm/recipes-utils/rest-api/files/rest-api-1/board_endpoint.py
@@ -18,26 +18,63 @@
 # Boston, MA 02110-1301 USA
 #
 from aiohttp import web
+import asyncio
 import rest_component_presence
 import rest_firmware
 import rest_chassis_eeprom
 import rest_chassis_all_serial_and_location
+from concurrent.futures import ThreadPoolExecutor
 
+# A separate decorator for board specific API function
+# Note that we use different executor stored in different
+# class variable name (also a different class)
+def board_force_async(func):
+    async def func_wrapper(self, *args, **kwargs):
+        loop = asyncio.get_event_loop()
+        # As we separated *arts into (self, *args) pair, we need to
+        # write "self" again, as the first argument before *args
+        result = await loop.run_in_executor(self.board_executor, func, \
+                                            self, *args, \
+                                            **kwargs)
+        return result
+    return func_wrapper
 
+# Method implementation using the decorator
 class boardApp_Handler:
+    # Just as in common REST API handler, we use a separate
+    # executor, in order to prevent any issue in common REST handlers
+    # from using up all threads for board specific handlers.
+    def __init__(self):
+        self.board_executor = ThreadPoolExecutor(5)
 
     # Handler to fetch component presence
-    async def rest_comp_presence(self,request):
+    def helper_rest_comp_presence(self,request):
         return web.json_response(rest_component_presence.get_presence())
 
+    @board_force_async
+    def rest_comp_presence(self,request):
+        return self.helper_rest_comp_presence(request)
+
     # Handler to fetch firmware_info
-    async def rest_firmware_info(self,request):
+    def helper_rest_firmware_info(self,request):
         return web.json_response(rest_firmware.get_firmware_info())
 
+    @board_force_async
+    def rest_firmware_info(self,request):
+        return self.helper_rest_firmware_info(request)
+
     # Handler to fetch chassis eeprom
-    async def rest_chassis_eeprom_hdl(self,request):
+    def helper_rest_chassis_eeprom_hdl(self,request):
         return web.json_response(rest_chassis_eeprom.get_chassis_eeprom())
 
+    @board_force_async
+    def rest_chassis_eeprom_hdl(self,request):
+        return self.helper_rest_chassis_eeprom_hdl(request)
+
     # Handler to fetch SN and location for each card on chassis
-    async def rest_all_serial_and_location_hdl(self,request):
+    def helper_rest_all_serial_and_location_hdl(self,request):
         return web.json_response(rest_chassis_all_serial_and_location.get_all_serials_and_locations())
+
+    @board_force_async
+    def rest_all_serial_and_location_hdl(self,request):
+        return self.helper_rest_all_serial_and_location_hdl(request)

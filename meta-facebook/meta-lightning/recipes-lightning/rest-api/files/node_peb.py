41f4569e4ae61e90bc8d2f91c1ec76ae9bfd7ec5
Jubin Mehta <jmehta@fb.com>
2017-09-18 04:32:26 -0700

    lightning: modify RESTful node api/peb for refreshing
    Summary: Modify RESTful node api/peb to refresh its info at each request.
    
    Test Plan:
    Check node api/peb behaiver: pass
    
    --
    
    [root@localhost ~]# sshpass -p '0penBmc' ssh -o 'StrictHostKeyChecking no' fe80::ff:fe00:1%enp0s29u1u2u1 enclosure-util --error
    Error Counter: 0
    
    [root@localhost ~]# curl -x "" http://fe80::ff:fe00:1%enp0s29u1u2u1:8080/api/peb | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   205  100   205    0     0    284      0 --:--:-- --:--:-- --:--:--   283
    {
        "Actions": [],
        "Information": {
            "Description": "Lightning PCIe Expansion Board",
            "Tray Location": "Upper Tray",
            "Tray Status": "In"
        },
        "Resources": [
            "fruid",
            "sensors",
            "bmc",
            "health",
            "identify",
            "logs"
        ]
    }
    [root@localhost ~]# sshpass -p '0penBmc' ssh -o 'StrictHostKeyChecking no' fe80::ff:fe00:1%enp0s29u1u2u1 enclosure-util --error
    Error Counter: 1
    Error Code 93: Self Tray Pulled-out
    
    [root@localhost ~]# curl -x "" http://fe80::ff:fe00:1%enp0s29u1u2u1:8080/api/peb | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   206  100   206    0     0    546      0 --:--:-- --:--:-- --:--:--   547
    {
        "Actions": [],
        "Information": {
            "Description": "Lightning PCIe Expansion Board",
            "Tray Location": "Upper Tray",
            "Tray Status": "Out"
        },
        "Resources": [
            "fruid",
            "sensors",
            "bmc",
            "health",
            "identify",
            "logs"
        ]
    }
    
    Reviewed By: jmehta
    
    fbshipit-source-id: 76fb8b0

--- meta-facebook/meta-lightning/recipes-lightning/rest-api/files/node_peb.py
+++ meta-facebook/meta-lightning/recipes-lightning/rest-api/files/node_peb.py
@@ -3,23 +3,40 @@
 from node import node
 from pal import *
 
+class pebNode(node):
+    def __init__(self, name = None, info = None, actions = None):
+        name = pal_get_platform_name()
+        if info == None:
+            self.info = {}
+        else:
+            self.info = info
+        if actions == None:
+            self.actions = []
+        else:
+            self.actions = actions
+
+    def getInformation(self):
+        name = pal_get_platform_name()
+        location = Popen('cat /tmp/tray_location', \
+                         shell=True, stdout=PIPE).stdout.read().strip('\n')
+        data = Popen('cat /sys/class/gpio/gpio108/value', \
+                       shell=True, stdout=PIPE).stdout.read().strip('\n')
+        if data == "0":
+            status = "In"
+        elif data == "1":
+            status = "Out"
+        else:
+            status = "Unknown"
+
+        info = {
+                "Description": name + " PCIe Expansion Board",
+                "Tray Location": location,
+                "Tray Status" : status,
+        }
+
+        return info
+
+
 def get_node_peb():
-    name = pal_get_platform_name()
-    location = Popen('cat /tmp/tray_location', \
-                     shell=True, stdout=PIPE).stdout.read().strip('\n')
-    data = Popen('cat /sys/class/gpio/gpio108/value', \
-                   shell=True, stdout=PIPE).stdout.read().strip('\n')
-    if data == "0":
-        status = "In"
-    elif data == "1":
-        status = "Out"
-    else:
-        status = "Unknown"
+    return pebNode()
 
-    info = {
-            "Description": name + " PCIe Expansion Board",
-            "Tray Location": location,
-            "Tray Status" : status,
-    }
-
-    return node(info)

cad533ff6bd32682de82363529182670e79a6632
Ben Wei <benwei@fb.com>
2017-09-21 15:03:41 -0700

    Yosemite: move sensord to runsv
    Summary: as title
    
    Test Plan: verified on Yosemite
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 4fd7438

--- meta-facebook/meta-yosemite/recipes-yosemite/sensor-mon/files/setup-sensord.sh
+++ meta-facebook/meta-yosemite/recipes-yosemite/sensor-mon/files/setup-sensord.sh
@@ -30,27 +30,5 @@
 . /usr/local/fbpackages/utils/ast-functions
 
 echo -n "Setup sensor monitoring for yosemite... "
-
-# Check for the slots present and run sensord for those slots only.
-SLOTS=
-  if [ $(is_server_prsnt 1) == "1" ] && [ $(is_bic_ready 1) == "1" ]; then
-    SLOTS="$SLOTS slot1"
-  fi
-
-  if [ $(is_server_prsnt 2) == "1" ] && [ $(is_bic_ready 2) == "1" ]; then
-    SLOTS="$SLOTS slot2"
-  fi
-
-  if [ $(is_server_prsnt 3) == "1" ] && [ $(is_bic_ready 3) == "1" ]; then
-    SLOTS="$SLOTS slot3"
-  fi
-
-  if [ $(is_server_prsnt 4) == "1" ] && [ $(is_bic_ready 4) == "1" ]; then
-    SLOTS="$SLOTS slot4"
-  fi
-
-SLOTS="$SLOTS spb nic"
-echo $SLOTS
-/usr/local/bin/sensord $SLOTS
-
+runsv /etc/sv/sensord > /dev/null 2>&1 &
 echo "done."

010fae6dd08bc9aedfbb3d2a4198317cc537a598
Ben Wei <benwei@fb.com>
2017-09-27 09:51:05 -0700

    fby2: fix bypass cmd is invalid on fby2
    Summary:
    1. Add bypass cmd function definition in pal.h
    
    TestPlan
    Test and build pass on system
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 0b2b822

--- meta-facebook/meta-fby2/recipes-fby2/plat-libs/files/pal/pal.h
+++ meta-facebook/meta-fby2/recipes-fby2/plat-libs/files/pal/pal.h
@@ -175,6 +175,7 @@ void pal_post_end_chk(uint8_t *post_end_chk);
 int pal_get_fw_info(unsigned char target, unsigned char* res, unsigned char* res_len);
 void pal_add_cri_sel(char *str);
 uint8_t pal_get_status(void);
+int pal_bypass_cmd(uint8_t slot, uint8_t *req_data, uint8_t req_len, uint8_t *res_data, uint8_t *res_len);
 #ifdef __cplusplus
 } // extern "C"
 #endif

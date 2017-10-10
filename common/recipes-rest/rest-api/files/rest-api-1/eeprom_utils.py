894fc8fbb834c04d7a33324a43c16c71e2ae8132
Vineela Kukkadapu <vineelak@fb.com>
2017-10-10 10:08:42 -0700

    Fix the eeprom_utils for py3
    Summary: in title
    
    Test Plan:
    scp the eeprom_utils.py then hit the endpoint:
    curl -g http://fsw001-mm001-oob.p081.f01.prn3.tfbnw.net:8080/api/sys/mb/chassis_eeprom | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   791  100   791    0     0    773      0  0:00:01  0:00:01 --:--:--   773
    {
        "Assembled At": "CLS",
        "CRC8": "0x34",
        "Extended MAC Address Size": "0",
        "Extended MAC Base": "00:00:00:00:00:00",
        "Facebook PCB Part Number": "131-000032-02",
        "Facebook PCBA Part Number": "132-000032-02",
        "Local MAC": "00:00:00:00:00:00",
        "Location on Fabric": "",
        "ODM PCBA Part Number": "R1018G000802",
        "ODM PCBA Serial Number": "G082117096018",
        "PCB Manufacturer": "WUS",
        "Product Asset Tag": "2809271",
        "Product Name": "GALAXY-PDB",
        "Product Part Number": "20-001711",
        "Product Production State": "4",
        "Product Serial Number": "F004317122004",
        "Product Sub-Version": "0",
        "Product Version": "1",
        "System Assembly Part Number": "",
        "System Manufacturer": "CLS",
        "System Manufacturing Date": "03-19-17",
        "Version": "2",
        "Wedge EEPROM /sys/class/i2c-dev/i2c-28/device/28-0050/eeprom": ""
    }
    
    fbshipit-source-id: 9f0fdf3

--- common/recipes-rest/rest-api/files/rest-api-1/eeprom_utils.py
+++ common/recipes-rest/rest-api/files/rest-api-1/eeprom_utils.py
@@ -15,26 +15,26 @@
 import subprocess
 import bmc_command
 
+
 def _parse_eeprom_data(output):
-  eeprom_data = {}
-
-  for line in output.splitlines():
-    parts = line.split(':', 1)
-    if len(parts) != 2:
-      # This generally shouldn't happen, but it's possible there was some
-      # error message logged on the console in the middle of the w|seutil
-      # output.
-      continue
-    key = parts[0].strip()
-    value = parts[1].strip()
-    eeprom_data[key] = value
-
-  return eeprom_data
+    eeprom_data = {}
+    output = output.decode('utf8')
+    for line in output.splitlines():
+        parts = line.split(':', 1)
+        if len(parts) != 2:
+            # This generally shouldn't happen, but it's possible there was some
+            # error message logged on the console in the middle of the w|seutil
+            # output.
+            continue
+        key = parts[0].strip()
+        value = parts[1].strip()
+        eeprom_data[key] = value
+    return eeprom_data
 
 
 def get_eeprom_data(util):
-  proc = subprocess.Popen([util],
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE)
-  data, err = bmc_command.timed_communicate(proc)
-  return _parse_eeprom_data(data)
+    proc = subprocess.Popen([util],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    data, err = bmc_command.timed_communicate(proc)
+    return _parse_eeprom_data(data)

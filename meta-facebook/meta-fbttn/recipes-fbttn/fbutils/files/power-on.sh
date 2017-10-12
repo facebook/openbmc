9a7f2ab9035af2d1a08eea430fc156f5a332d7d6
Jubin Mehta <jmehta@fb.com>
2017-10-11 21:29:00 -0700

    fbttn: modify get SKU info and remove unnecessary continue funtion in power-on.sh
    Summary:
    Summary
    - Modify get SKU info in power-on.sh, and if conditions
    - Remove Unnecessary continue funtion and add if error handling for sync_date
    
    Test Plan
    - Build and Test with Type5 and Type7 System, and power-on.sh works correctly.
    - Remove SCC and AC cycle system, the BMC time sync failed message will be save in /var/log/messages.
    
    Reviewed By: jmehta
    
    fbshipit-source-id: c7db1dc

--- meta-facebook/meta-fbttn/recipes-fbttn/fbutils/files/power-on.sh
+++ meta-facebook/meta-fbttn/recipes-fbttn/fbutils/files/power-on.sh
@@ -76,24 +76,24 @@ check_por_config()
     PWR_POLICY=`cat ${KEYDIR}/slot${slot_num}_por_cfg`
 
     # Case ON
-    if [ $PWR_POLICY == "on" ]; then
+    if [ "$PWR_POLICY" == "on" ]; then
       TO_PWR_ON=1;
 
     # Case OFF
-    elif [ $PWR_POLICY == "off" ]; then
+    elif [ "$PWR_POLICY" == "off" ]; then
       TO_PWR_ON=0;
 
     # Case LPS
-    elif [ $PWR_POLICY == "lps" ]; then
+    elif [ "$PWR_POLICY" == "lps" ]; then
 
       # Check if the file/key doesn't exist
       if [ ! -f "${KEYDIR}/pwr_server${slot_num}_last_state" ]; then
         TO_PWR_ON=$DEF_PWR_ON
       else
         LS=`cat ${KEYDIR}/pwr_server${slot_num}_last_state`
-        if [ $LS == "on" ]; then
+        if [ "$LS" == "on" ]; then
           TO_PWR_ON=1;
-        elif [ $LS == "off" ]; then
+        elif [ "$LS" == "off" ]; then
           TO_PWR_ON=0;
         fi
       fi
@@ -104,25 +104,25 @@ check_por_config()
 # Sync BMC's date with one of the four servers
 sync_date()
 {
-  if [ $(is_server_prsnt) == "1" ] ; then
+  if [ "$(is_server_prsnt)" == "1" ] ; then
     # Use standard IPMI command 'get-sel-time' to read RTC time
     output=$(/usr/local/bin/me-util server 0x28 0x48)
-    # if the command fails, continue to next slot
-    [ $(echo $output | wc -c) != 12 ] && continue
-    col1=$(echo $output | cut -d' ' -f1 | sed 's/^0*//')
-    col2=$(echo $output | cut -d' ' -f2 | sed 's/^0*//')
-    col3=$(echo $output | cut -d' ' -f3 | sed 's/^0*//')
-    col4=$(echo $output | cut -d' ' -f4 | sed 's/^0*//')
-
-    # create the integer from the hex bytes returned
-    val=$((0x$col4 << 24 | 0x$col3 << 16 | 0x$col2 << 8 | 0x$col1))
-
-    # create the timestamp required for busybox's date command
-    ts=$(date -d @$val +"%Y.%m.%d-%H:%M:%S")
-
-    # set the command
-    logger -s -p user.info -t power-on "Syncing up BMC time with server..."
-    logger -s -p user.info -t power-on "`date $ts`"
+    # Sync BMC time only when ME returns correct response length
+    if [ "$(echo $output | wc -c)" == "12" ] ; then
+      col1=$(echo $output | cut -d' ' -f1 | sed 's/^0*//')
+      col2=$(echo $output | cut -d' ' -f2 | sed 's/^0*//')
+      col3=$(echo $output | cut -d' ' -f3 | sed 's/^0*//')
+      col4=$(echo $output | cut -d' ' -f4 | sed 's/^0*//')
+      # create the integer from the hex bytes returned
+      val=$((0x$col4 << 24 | 0x$col3 << 16 | 0x$col2 << 8 | 0x$col1))
+      # create the timestamp required for busybox's date command
+      ts=$(date -d @$val +"%Y.%m.%d-%H:%M:%S")
+      # set the command
+      logger -s -p user.info -t power-on "Syncing up BMC time with server..."
+      logger -s -p user.info -t power-on "`date $ts`"
+    else
+      logger -s -p user.info -t power-on "Get SEL time from ME failed, so syncing up BMC time with server failed..."
+    fi
   fi
 }
 
@@ -133,34 +133,34 @@ sync_date()
 #    (2) If chsssis type is 5, check SCC is present or not on current side
 #    (3) If chsssis type is 7, only check SCCA present
 is_server_12v_off="1"
-if [ $(is_server_prsnt) == "0" ]; then
+if [ "$(is_server_prsnt)" == "0" ]; then
   logger -s -p user.warn -t power-on "The Mono Lake is absent, turn off Mono Lake HSC 12V and IOM 3V3."
   gpio_set O7 0
   gpio_set AA7 0
   is_server_12v_off="0"
 else
-  # Get chassis type
+  # Get chassis type and iom position
   sh /usr/local/bin/check_pal_sku.sh > /dev/NULL
-  chassis_type=$(($(($? >> 6)) & 0x1))
+  PAL_SKU=$?
+  chassis_type=$(($(($PAL_SKU >> 6)) & 0x1))
+  iom_local=$(($(($PAL_SKU >> 4)) & 0x3))
 
-  if [ $chassis_type -eq 0 ]; then  # type 5
-    sh /usr/local/bin/check_pal_sku.sh > /dev/NULL
-    iom_local=$(($(($? >> 4)) & 0x3))
+  if [ "$chassis_type" == "0" ]; then  # type 5
     # IOMA
-    if [ $iom_local -eq 1 ] && [ $(is_scc_prsnt 478) == "0" ]; then
+    if [ "$iom_local" == "1" ] && [ "$(is_scc_prsnt 478)" == "0" ]; then
       logger -s -p user.warn -t power-on "The SCCA is absent, turn off Mono Lake HSC 12V and IOM 3V3."
       gpio_set O7 0
       gpio_set AA7 0
       is_server_12v_off="0"
     #IOMB
-    elif [ $iom_local -eq 2 ] && [ $(is_scc_prsnt 479) == "0" ]; then
+    elif [ "$iom_local" == "2" ] && [ "$(is_scc_prsnt 479)" == "0" ]; then
       logger -s -p user.warn -t power-on "The SCCB is absent, turn off Mono Lake HSC 12V and IOM 3V3."
       gpio_set O7 0
       gpio_set AA7 0
       is_server_12v_off="0"
     fi
   else  # type 7, only check SCCA
-    if [ $iom_local -eq 1 ] && [ $(is_scc_prsnt 478) == "0" ]; then
+    if [ "$iom_local" == "1" ] && [ "$(is_scc_prsnt 478)" == "0" ]; then
       logger -s -p user.warn -t power-on "The SCCA is absent, turn off Mono Lake HSC 12V and IOM 3V3."
       gpio_set O7 0
       gpio_set AA7 0
@@ -170,27 +170,26 @@ else
 fi
 
 # Check whether it is fresh power on reset
-if [ $(is_bmc_por) -eq 1 ] && [ $is_server_12v_off -eq 1 ]; then
-
+if [ "$(is_bmc_por)" == "1" ] && [ "$is_server_12v_off" == "1" ]; then
   # Disable clearing of PWM block on WDT SoC Reset
   devmem_clear_bit $(scu_addr 9c) 17
 
   # For fbttn remote SCC PWR sequence
   sh /usr/local/bin/check_pal_sku.sh > /dev/NULL
   chassis_type=$(($(($? >> 6)) & 0x1))
-  if [ $chassis_type -eq 1 ]; then  # type 7, always power-on SCC B
+  if [ "$chassis_type" == "1" ]; then  # type 7, always power-on SCC B
     gpio_set F1 1
     gpio_tolerance_fun F1
   fi
 
   # For fbttn MonoLake PWR sequence
-  if [ $(gpio_get $IOM_FULL_GOOD) == 1 ]; then
+  if [ "$(gpio_get $IOM_FULL_GOOD)" == "1" ]; then
     #set ML board power enable (12V on)
     gpio_set O7 1
     sleep 3  # waiting for ME ready
     sync_date 
     check_por_config 1
-    if [ $TO_PWR_ON -eq 1 ] && [ $(is_server_prsnt) == "1" ] ; then
+    if [ "$TO_PWR_ON" == "1" ] && [ "$(is_server_prsnt)" == "1" ] ; then
         power-util server on
         logger -s -p user.info -t power-on "Power on server"
     fi

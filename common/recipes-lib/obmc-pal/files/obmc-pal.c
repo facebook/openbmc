a007e6f88624cc104b8679c1afad96a74d416c58
Amithash Prasad <amithash@fb.com>
2017-10-16 10:07:17 -0700

    pal: Remove check for flashcp
    Summary: Remove check for flashcp in ps output when user calls is_fw_update_ongoing().
    
    Test Plan:
    doitlive
    and build testing.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: fa073bd

--- common/recipes-lib/obmc-pal/files/obmc-pal.c
+++ common/recipes-lib/obmc-pal/files/obmc-pal.c
@@ -1374,10 +1374,6 @@ pal_is_fw_update_ongoing(uint8_t fruid) {
   int ret;
   struct timespec ts;
 
-  ret = system("ps | grep -v 'grep' | grep 'flashcp' &> /dev/null");
-  if (WIFEXITED(ret) && WEXITSTATUS(ret) == 0)
-    return true;
-
   sprintf(key, "fru%d_fwupd", fruid);
   ret = edb_cache_get(key, value);
   if (ret < 0) {

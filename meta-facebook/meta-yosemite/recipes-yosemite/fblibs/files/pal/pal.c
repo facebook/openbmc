cbb7073eade116a5611bc1f4da0d4a7019fc8a77
Ben Wei <benwei@fb.com>
2017-10-26 11:05:03 -0700

    Yosemite: override pal_parse_sel() to parse MEMORY_ECC_ERR event
    Summary: override pal_parse_sel() to parse MEMORY_ECC_ERR event since the event data is different with common
    
    Test Plan:
    1    slot1    2017-10-26 08:16:33    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2017-10-26 08:16:33, Sensor: MEMORY_ECC_ERR (0x63), Event Data: (A10308) Uncorrec
    table DIMM B0 Logical Rank 3 (CPU# 0, CHN# 1, DIMM# 0)
    1    slot1    2017-10-26 08:16:40    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2017-10-26 08:16:40, Sensor: MEMORY_ECC_ERR (0x63), Event Data: (A10309) Uncorrec
    table DIMM B1 Logical Rank 3 (CPU# 0, CHN# 1, DIMM# 1)
    1    slot1    2017-10-26 08:16:50    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2017-10-26 08:16:50, Sensor: MEMORY_ECC_ERR (0x63), Event Data: (A10301) Uncorrec
    table DIMM A1 Logical Rank 3 (CPU# 0, CHN# 0, DIMM# 1)
    1    slot1    2017-10-26 08:17:24    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2017-10-26 08:17:24, Sensor: MEMORY_ECC_ERR (0x63), Event Data: (A10300) Uncorrec
    table DIMM A0 Logical Rank 3 (CPU# 0, CHN# 0, DIMM# 0)
    
    Reviewed By: amithash
    
    fbshipit-source-id: 9a89118

--- meta-facebook/meta-yosemite/recipes-yosemite/fblibs/files/pal/pal.c
+++ meta-facebook/meta-yosemite/recipes-yosemite/fblibs/files/pal/pal.c
@@ -2134,6 +2134,92 @@ pal_sel_handler(uint8_t fru, uint8_t snr_num, uint8_t *event_data) {
 }
 
 int
+pal_parse_sel(uint8_t fru, uint8_t *sel, char *error_log)
+{
+  uint8_t snr_num = sel[11];
+  uint8_t *event_data = &sel[10];
+  uint8_t *ed = &event_data[3];
+  char temp_log[512] = {0};
+  uint8_t sen_type = event_data[0];
+  uint8_t chn_num, dimm_num;
+
+  switch(snr_num) {
+    case MEMORY_ECC_ERR:
+    case MEMORY_ERR_LOG_DIS:
+      strcpy(error_log, "");
+      if (snr_num == MEMORY_ECC_ERR) {
+        // SEL from MEMORY_ECC_ERR Sensor
+        if ((ed[0] & 0x0F) == 0x0) {
+          if (sen_type == 0x0C) {
+            strcat(error_log, "Correctable");
+            sprintf(temp_log, "DIMM%02X ECC err", ed[2]);
+            pal_add_cri_sel(temp_log);
+          } else if (sen_type == 0x10)
+            strcat(error_log, "Correctable ECC error Logging Disabled");
+        } else if ((ed[0] & 0x0F) == 0x1) {
+          strcat(error_log, "Uncorrectable");
+          sprintf(temp_log, "DIMM%02X UECC err", ed[2]);
+          pal_add_cri_sel(temp_log);
+        } else if ((ed[0] & 0x0F) == 0x5)
+          strcat(error_log, "Correctable ECC error Logging Limit Reached");
+        else
+          strcat(error_log, "Unknown");
+      } else {
+        // SEL from MEMORY_ERR_LOG_DIS Sensor
+        if ((ed[0] & 0x0F) == 0x0)
+          strcat(error_log, "Correctable Memory Error Logging Disabled");
+        else
+          strcat(error_log, "Unknown");
+      }
+
+      // DIMM number (ed[2]):
+      // Bit[7:5]: Socket number  (Range: 0-7)
+      // Bit[4:3]: Channel number (Range: 0-3)
+      // Bit[2:0]: DIMM number    (Range: 0-7)
+      if (((ed[1] & 0xC) >> 2) == 0x0) {
+        /* All Info Valid */
+        chn_num = (ed[2] & 0x18) >> 3;
+        dimm_num = ed[2] & 0x7;
+
+        /* If critical SEL logging is available, do it */
+        if (sen_type == 0x0C) {
+          if ((ed[0] & 0x0F) == 0x0) {
+            sprintf(temp_log, "DIMM%c%d ECC err,FRU:%u", 'A'+chn_num,
+                    dimm_num, fru);
+            pal_add_cri_sel(temp_log);
+          } else if ((ed[0] & 0x0F) == 0x1) {
+            sprintf(temp_log, "DIMM%c%d UECC err,FRU:%u", 'A'+chn_num,
+                    dimm_num, fru);
+            pal_add_cri_sel(temp_log);
+          }
+        }
+        /* Then continue parse the error into a string. */
+        /* All Info Valid                               */
+        sprintf(temp_log, " DIMM %c%d Logical Rank %d (CPU# %d, CHN# %d, DIMM# %d)",
+            'A'+chn_num, dimm_num, ed[1] & 0x03, (ed[2] & 0xE0) >> 5, chn_num, dimm_num);
+      } else if (((ed[1] & 0xC) >> 2) == 0x1) {
+        /* DIMM info not valid */
+        sprintf(temp_log, " (CPU# %d, CHN# %d)",
+            (ed[2] & 0xE0) >> 5, (ed[2] & 0x18) >> 3);
+      } else if (((ed[1] & 0xC) >> 2) == 0x2) {
+        /* CHN info not valid */
+        sprintf(temp_log, " (CPU# %d, DIMM# %d)",
+            (ed[2] & 0xE0) >> 5, ed[2] & 0x7);
+      } else if (((ed[1] & 0xC) >> 2) == 0x3) {
+        /* CPU info not valid */
+        sprintf(temp_log, " (CHN# %d, DIMM# %d)",
+            (ed[2] & 0x18) >> 3, ed[2] & 0x7);
+      }
+      strcat(error_log, temp_log);
+      return 0;
+  }
+
+  pal_parse_sel_helper(fru, sel, error_log);
+
+  return 0;
+}
+
+int
 pal_set_sensor_health(uint8_t fru, uint8_t value) {
 
   char key[MAX_KEY_LEN] = {0};

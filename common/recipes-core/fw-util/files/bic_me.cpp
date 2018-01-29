f36b5d7ade31f44362e66751d95490c892786e34
Ben Wei <benwei@fb.com>
2018-01-29 08:58:22 -0800

    fw-util: Add IMC version print in fw-util
    Summary: Add IMC version print in fw-util for RC platform
    
    Test Plan:
    Build and test pass on RC EVT system.
    
    root@bmc-oob:~# fw-util slot3 --version
    Bridge-IC Version: v3.08
    Bridge-IC Bootloader Version: v4.01
    BIOS Version: RCE02
    CPLD Version: 0x00000e02
    IMC Version: 0.0.0.00
    VDD_APC_CBF VR Version: 0x01da3de582
    DDR510 VR Version: 0x0153a00707
    DDR423 VR Version: 0x0152dea954
    
    Note: The ifdef CONFIG_FBY2_RC is for current implementation, we will update it to get server type to get ME/IMC version automatically.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: a911c71

--- common/recipes-core/fw-util/files/bic_me.cpp
+++ common/recipes-core/fw-util/files/bic_me.cpp
@@ -10,16 +10,18 @@ using namespace std;
 
 int MeComponent::print_version() {
   uint8_t ver[32] = {0};
+  uint8_t target_name[10] = {0};
+  pal_get_me_name(target_name);
   try {
     server.ready();
     if (bic_get_fw_ver(slot_id, FW_ME, ver)){
-      printf("ME Version: NA\n");
+      printf("%s Version: NA\n",target_name);
     }
     else {
-      printf("ME Version: %x.%x.%x.%x%x\n", ver[0], ver[1], ver[2], ver[3], ver[4]);
+      printf("%s Version: %x.%x.%x.%x%x\n",target_name, ver[0], ver[1], ver[2], ver[3], ver[4]);
     }
   } catch(string err) {
-    printf("ME Version: NA (%s)\n", err.c_str());
+    printf("%s Version: NA (%s)\n",target_name, err.c_str());
   }
   return 0;
 }

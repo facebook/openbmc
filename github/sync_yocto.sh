c96e8267b9e836710ebd7ee272c8b6455e6130b9
Amithash Prasad <amithash@fb.com>
2018-03-05 10:32:23 -0800

    add sync script for yocto
    Summary: Add script to help in syncing external yocto repos
    
    Test Plan: Build github fbtp
    
    Reviewed By: tfangit
    
    fbshipit-source-id: 90ecdc0

---
+++ github/sync_yocto.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+repos="fido krogoth rocko"
+
+if [ ! -d ./yocto ]; then
+  mkdir ./yocto
+fi
+
+for branch in $repos
+do
+  # Make the branch if it does not exist.
+  if [ ! -d ./yocto/$branch ]; then
+    mkdir ./yocto/$branch
+  fi
+  # Remove the branches.
+  if [ -d ./yocto/$branch/poky ]; then
+    rm -rf ./yocto/$branch/poky
+  fi
+  if [ -d ./yocto/$branch/meta-openembedded ]; then
+    rm -rf ./yocto/$branch/meta-openembedded
+  fi
+  if [ -d ./yocto/$branch/meta-security ]; then
+    rm -rf ./yocto/$branch/meta-security
+  fi
+  rm -r ./yocto/$branch/source_mirrors
+
+  git clone -b $branch https://git.yoctoproject.org/git/poky yocto/$branch/poky
+  git clone -b $branch https://github.com/openembedded/meta-openembedded.git yocto/$branch/meta-openembedded
+  git clone -b $branch https://git.yoctoproject.org/git/meta-security yocto/$branch/meta-security
+done
+

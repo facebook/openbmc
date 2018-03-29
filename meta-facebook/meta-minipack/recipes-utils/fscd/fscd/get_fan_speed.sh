d0774bc4038aafe67332312e20432feabdf8db07
Mike Choi <mikechoi@fb.com>
2018-03-28 21:18:26 -0700

    minipack: Support get fan speed and set fan speed
    Summary:
    1. Add get_fan_speed.sh
    2. Add set_fan_speed.sh
    3. Disable fscd run because thermal plan is not ready
    
    Test Plan: Test on Minipack: pass
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: e281922

---
+++ meta-facebook/meta-minipack/recipes-utils/fscd/fscd/get_fan_speed.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+usage() {
+    echo "Usage: $0 [Fan Unit (1..8)]" >&2
+}
+
+# Top FCM    (BUS=64) PWM 1 ~ 4 control Fantray 1 3 5 7
+# Bottom FCM (BUS=72) PWM 1 ~ 4 control Fantray 2 4 6 8
+show_pwm()
+{
+    pwm="/sys/class/i2c-dev/i2c-$1/device/$1-0033/fantray$2_pwm"
+    val=$(cat $pwm | head -n 1)
+    echo "$((val * 100 / 31))%"
+}
+
+show_rpm()
+{
+    front_rpm="/sys/class/i2c-dev/i2c-$1/device/$1-0033/fan$((($2 * 2 - 1)))_input"
+    rear_rpm="/sys/class/i2c-dev/i2c-$1/device/$1-0033/fan$((($2 * 2)))_input"
+    echo "$(cat $front_rpm), $(cat $rear_rpm)"
+}
+
+set -e
+
+# refer to the comments in init_pwn.sh regarding
+# the fan unit and tacho mapping
+if [ "$#" -eq 0 ]; then
+    FANS="1 2 3 4 5 6 7 8"
+elif [ "$#" -eq 1 ]; then
+    if [ "$1" -le 0 ] || [ "$1" -ge 9 ]; then
+        echo "Fan $1: not a valid Fan Unit"
+        exit 1
+    fi
+    FANS="$1"
+else
+    usage
+    exit 1
+fi
+
+
+for fan in $FANS; do
+    FAN="${fan}"
+
+    if [ $(($fan % 2)) -eq 0 ]; then
+      BUS="72"
+      UNIT=$((${FAN} / 2 ))
+    else
+      BUS="64"
+      FAN=$((${FAN} / 2 ))
+      UNIT=$((${FAN} + 1))
+    fi
+
+    echo "Fan $fan RPMs: $(show_rpm $BUS $UNIT), ($(show_pwm $BUS $UNIT))"
+done
---
+++ meta-facebook/meta-minipack/recipes-utils/fscd/fscd/set_fan_speed.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+usage() {
+    echo "Usage: $0 <PERCENT (0..100)> <Fan Unit (1..8)> " >&2
+}
+
+set -e
+
+if [ "$#" -ne 2 ] && [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+# Top FCM    (BUS=64) PWM 1 ~ 4 control Fantray 1 3 5 7
+# Bottom FCM (BUS=72) PWM 1 ~ 4 control Fantray 2 4 6 8
+BUS="64 72"
+if [ "$#" -eq 1 ]; then
+    FAN_UNIT="1 2 3 4"
+else
+    if [ "$2" -le 0 ] || [ "$2" -ge 9 ]; then
+        echo "Fan $2: not a valid Fan Unit"
+        exit 1
+    fi
+
+    FAN="$2"
+
+    if [ $(($FAN % 2)) -eq 0 ]; then
+      BUS="72"
+      FAN_UNIT=$((${FAN} / 2 ))
+    else
+      BUS="64"
+      FAN=$((${FAN} / 2 ))
+      FAN_UNIT=$((${FAN} + 1))
+    fi
+fi
+
+# Convert the percentage to our 1/32th step (0-31).
+step=$((($1 * 31) / 100))
+cnt=1
+
+for bus in ${BUS}; do
+    for unit in ${FAN_UNIT}; do
+        pwm="/sys/class/i2c-dev/i2c-${bus}/device/${bus}-0033/fantray${unit}_pwm"
+        echo "$step" > "${pwm}"
+
+    if [ "$#" -eq 1 ]; then
+        echo "Successfully set fan $cnt speed to $1%"
+        cnt=$(($cnt+1))
+    else
+        echo "Successfully set fan $2 speed to $1%"
+    fi
+    done
+done

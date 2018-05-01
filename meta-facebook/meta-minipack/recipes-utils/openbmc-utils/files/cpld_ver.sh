ae5dfca8f0c8048c697e06cde128709fdda3e885
Tian Fang <tfang@fb.com>
2018-04-30 20:47:21 -0700

    minipack: Only get first line when cpld register is read by using i2c driver node
    Summary:
    1. Only get first line when cpld register is read by using i2c sysfs node.
        Because reading i2c sysfs node shows register offset/bits info.
    
    Test Plan: Test on Minipack:pass
    
    Reviewed By: tfangit
    
    fbshipit-source-id: 7de8e97

--- meta-facebook/meta-minipack/recipes-utils/openbmc-utils/files/cpld_ver.sh
+++ meta-facebook/meta-minipack/recipes-utils/openbmc-utils/files/cpld_ver.sh
@@ -24,14 +24,14 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
 board_rev=$(wedge_board_rev)
 
-smb_ver=$(cat /sys/class/i2c-adapter/i2c-12/12-003e/cpld_ver)
-smb_sub_ver=$(cat /sys/class/i2c-adapter/i2c-12/12-003e/cpld_sub_ver)
-scm_ver=$(cat /sys/class/i2c-adapter/i2c-2/2-0035/cpld_ver)
-scm_sub_ver=$(cat /sys/class/i2c-adapter/i2c-2/2-0035/cpld_sub_ver)
-fcm_top_ver=$(cat /sys/class/i2c-adapter/i2c-64/64-0033/cpld_ver)
-fcm_top_sub_ver=$(cat /sys/class/i2c-adapter/i2c-64/64-0033/cpld_sub_ver)
-fcm_bottom_ver=$(cat /sys/class/i2c-adapter/i2c-72/72-0033/cpld_ver)
-fcm_bottom_sub_ver=$(cat /sys/class/i2c-adapter/i2c-72/72-0033/cpld_sub_ver)
+smb_ver=$(cat /sys/class/i2c-adapter/i2c-12/12-003e/cpld_ver | head -n 1)
+smb_sub_ver=$(cat /sys/class/i2c-adapter/i2c-12/12-003e/cpld_sub_ver | head -n 1)
+scm_ver=$(cat /sys/class/i2c-adapter/i2c-2/2-0035/cpld_ver | head -n 1)
+scm_sub_ver=$(cat /sys/class/i2c-adapter/i2c-2/2-0035/cpld_sub_ver | head -n 1)
+fcm_top_ver=$(cat /sys/class/i2c-adapter/i2c-64/64-0033/cpld_ver | head -n 1)
+fcm_top_sub_ver=$(cat /sys/class/i2c-adapter/i2c-64/64-0033/cpld_sub_ver | head -n 1)
+fcm_bottom_ver=$(cat /sys/class/i2c-adapter/i2c-72/72-0033/cpld_ver | head -n 1)
+fcm_bottom_sub_ver=$(cat /sys/class/i2c-adapter/i2c-72/72-0033/cpld_sub_ver | head -n 1)
 
 echo "SMBCPLD: $(($smb_ver)).$(($smb_sub_ver))"
 echo "SCMCPLD: $(($scm_ver)).$(($scm_sub_ver))"
@@ -39,10 +39,10 @@ echo "Top FCMCPLD: $(($fcm_top_ver)).$(($fcm_top_sub_ver))"
 echo "Bottom FCMCPLD: $(($fcm_bottom_ver)).$(($fcm_bottom_sub_ver))"
 
 if [ $board_rev -ne 4 ]; then
-  pdbl_ver=$(cat /sys/class/i2c-adapter/i2c-55/55-0060/cpld_ver)
-  pdbl_sub_ver=$(cat /sys/class/i2c-adapter/i2c-55/55-0060/cpld_sub_ver)
-  pdbr_ver=$(cat /sys/class/i2c-adapter/i2c-63/63-0060/cpld_ver)
-  pdbr_sub_ver=$(cat /sys/class/i2c-adapter/i2c-63/63-0060/cpld_sub_ver)
+  pdbl_ver=$(cat /sys/class/i2c-adapter/i2c-55/55-0060/cpld_ver | head -n 1)
+  pdbl_sub_ver=$(cat /sys/class/i2c-adapter/i2c-55/55-0060/cpld_sub_ver | head -n 1)
+  pdbr_ver=$(cat /sys/class/i2c-adapter/i2c-63/63-0060/cpld_ver | head -n 1)
+  pdbr_sub_ver=$(cat /sys/class/i2c-adapter/i2c-63/63-0060/cpld_sub_ver | head -n 1)
 
   echo "Left PDBCPLD: $(($pdbl_ver)).$(($pdbl_sub_ver))"
   echo "Right PDBCPLD: $(($pdbr_ver)).$(($pdbr_sub_ver))"

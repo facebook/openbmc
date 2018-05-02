d65acae7fb0f6e11a9c75452de30e685e65c5608
Mike Choi <mikechoi@fb.com>
2018-05-01 18:36:49 -0700

    YAMP: Revise setup-i2c.sh to include all PMBUS compatible devices
    Summary: Before accessing any sensors in PIMs, we need to take LC SMB MUX in SCB out of reset. This code does it.
    
    Test Plan: Tested in YAMP
    
    Reviewed By: tfangit
    
    fbshipit-source-id: 3fe13a0

--- meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/setup_i2c.sh
+++ meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/setup_i2c.sh
@@ -30,32 +30,39 @@ i2c_device_add 1 0x50 supsfp
 # Bus  2 - SMBus 3 - LC, MUXED as Bus 16-23
 i2c_device_add 2 0x75 pca9548
 
+# Bus  3 - SMBus 4 - SCB ECB/VRD and sensors
+i2c_device_add 3 0x40 pmbus
+i2c_device_add 3 0x41 pmbus
+i2c_device_add 3 0x4e ucd90120
+i2c_device_add 3 0x42 pmbus
+i2c_device_add 3 0x44 pmbus
+
 # Bus  4 - SMBus 5 SCD
 i2c_device_add 4 0x23 scdcpld
 i2c_device_add 4 0x4d max6581
 i2c_device_add 4 0x50 24c512
+# Unreset LC SMBUS MUX on the Switch Card
+/bin/echo 0 > /sys/bus/i2c/drivers/scdcpld/4-0023/lc_smb_mux_rst
 
 # Bus  5 - SMBus 6 PSU1
-i2c_device_add 5 0x50 psu1
+i2c_device_add 5 0x58 pmbus
 
-# Bus  6 - SMBus 7 PSU2
-i2c_device_add 6 0x50 psu2
-
-# Bus  7 - SMBus 8 PSU3
-i2c_device_add 7 0x50 psu3
+# For now, we assume that PSU2 and PSU3 are not present
+#i2c_device_add 6 0x58 pmbus
+#i2c_device_add 7 0x58 pmbus
 
 # Bus  8 - SMBus 9 PSU4
-i2c_device_add 8 0x50 psu4
+i2c_device_add 8 0x58 pmbus
 
 # Bus  9 - SMBus 10 SUP DPM
-i2c_device_add 9 0x4e ucd90320
+i2c_device_add 9 0x11 pmbus
 
 # Bus  10 - SMBus 11 SUP POWER
 i2c_device_add 10 0x21 cpupwr
 i2c_device_add 10 0x27 mempwr
 
 # Bus  11 - SMBus 12 SUP TEMP
-i2c_device_add 11 0x40 supecb
+i2c_device_add 11 0x40 pmbus
 i2c_device_add 11 0x4c max6658
 
 # Bus  12 - SMBus 13 SUP CPLD
@@ -68,64 +75,57 @@ i2c_device_add 13 0x60 fanctrl
 
 # Bus  16 - Muxed LC1 
 i2c_device_add 14 0x4e ucd90120
-i2c_device_add 14 0x42 lcecb
+i2c_device_add 14 0x42 pmbus
 i2c_device_add 14 0x20 pca9555
 i2c_device_add 14 0x4c max6658
 i2c_device_add 14 0x50 24c512
 
 # Bus  17 - Muxed LC2 
 i2c_device_add 15 0x4e ucd90120
-i2c_device_add 15 0x42 lcecb
+i2c_device_add 15 0x42 pmbus
 i2c_device_add 15 0x20 pca9555
 i2c_device_add 15 0x4c max6658
 i2c_device_add 15 0x50 24c512
 
 # Bus  18 - Muxed LC3 
 i2c_device_add 16 0x4e ucd90120
-i2c_device_add 16 0x42 lcecb
+i2c_device_add 16 0x42 pmbus
 i2c_device_add 16 0x20 pca9555
 i2c_device_add 16 0x4c max6658
 i2c_device_add 16 0x50 24c512
 
 # Bus  19 - Muxed LC4 
 i2c_device_add 17 0x4e ucd90120
-i2c_device_add 17 0x42 lcecb
+i2c_device_add 17 0x42 pmbus
 i2c_device_add 17 0x20 pca9555
 i2c_device_add 17 0x4c max6658
 i2c_device_add 17 0x50 24c512
 
 # Bus  20 - Muxed LC5 
 i2c_device_add 18 0x4e ucd90120
-i2c_device_add 18 0x42 lcecb
+i2c_device_add 18 0x42 pmbus
 i2c_device_add 18 0x20 pca9555
 i2c_device_add 18 0x4c max6658
 i2c_device_add 18 0x50 24c512
 
 # Bus  21 - Muxed LC6 
 i2c_device_add 19 0x4e ucd90120
-i2c_device_add 19 0x42 lcecb
+i2c_device_add 19 0x42 pmbus
 i2c_device_add 19 0x20 pca9555
 i2c_device_add 19 0x4c max6658
 i2c_device_add 19 0x50 24c512
 
 # Bus  22 - Muxed LC7 
 i2c_device_add 20 0x4e ucd90120
-i2c_device_add 20 0x42 lcecb
+i2c_device_add 20 0x42 pmbus
 i2c_device_add 20 0x20 pca9555
 i2c_device_add 20 0x4c max6658
 i2c_device_add 20 0x50 24c512
 
 # Bus  23 - Muxed LC8 
 i2c_device_add 21 0x4e ucd90120
-i2c_device_add 21 0x42 lcecb
+i2c_device_add 21 0x42 pmbus
 i2c_device_add 21 0x20 pca9555
 i2c_device_add 21 0x4c max6658
 i2c_device_add 21 0x50 24c512
-
-# Bus  3 - SMBus 4 SCB
-i2c_device_add 3 0x40 scbecb1
-i2c_device_add 3 0x41 scbecb2
-i2c_device_add 3 0x4e ucd90120
-i2c_device_add 3 0x12 scbvrd1
-i2c_device_add 3 0x14 scbvrd2
 

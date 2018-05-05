4466358ef34463bf40bbecab4751d4094434171f
Ben Wei <benwei@fb.com>
2018-05-04 17:54:07 -0700

    REST: platform-specific changes to pass ro/wr command line options to nodes
    Summary:
    as title, by default common REST specify all node as read-only.
      This change passes user overwrite option to common REST code
    
    Test Plan: verified on fby2, yosemiteV1, fbtp
    
    Reviewed By: amithash
    
    fbshipit-source-id: 7b895b1

--- meta-facebook/meta-fby2/recipes-fby2/rest-api/files/plat_tree.py
+++ meta-facebook/meta-fby2/recipes-fby2/rest-api/files/plat_tree.py
@@ -44,7 +44,7 @@ def get_slot_type(num):
     f.close()
     return slot_type
 
-def populate_server_node(num):
+def populate_server_node(num, is_read_only=True):
     prsnt = pal_is_fru_prsnt(num)
     if prsnt == None or prsnt == 0:
         return None
@@ -52,14 +52,14 @@ def populate_server_node(num):
     slot_type = get_slot_type(num)
 
     if slot_type == 0:
-      r_server = tree("server" + repr(num), data = get_node_server(num))
+      r_server = tree("server" + repr(num), data = get_node_server(num, is_read_only))
     else :
       r_server = tree("device" + repr(num), data = get_node_device(num))
 
     r_fruid = tree("fruid", data = get_node_fruid("slot" + repr(num)))
     r_sensors = tree("sensors", data = get_node_sensors("slot" + repr(num)))
-    r_logs = tree("logs", data = get_node_logs("slot" + repr(num)))
-    r_config = tree("config", data = get_node_config("slot" + repr(num)))
+    r_logs = tree("logs", data = get_node_logs("slot" + repr(num), is_read_only))
+    r_config = tree("config", data = get_node_config("slot" + repr(num), is_read_only))
     r_bios = tree("bios", data = get_node_bios("server" + repr(num)))
     r_server.addChildren([r_fruid, r_sensors, r_logs, r_config, r_bios])
 
@@ -68,22 +68,22 @@ def populate_server_node(num):
     r_plat_info_trunk = tree("plat-info", data = get_node_bios_plat_info_trunk("slot" + repr(num)))
     r_bios.addChildren([r_boot_order_trunk, r_postcode_trunk, r_plat_info_trunk])
 
-    r_boot_mode = tree("boot_mode", data = get_node_bios_boot_mode("slot" + repr(num)))
-    r_clear_cmos = tree("clear_cmos", data = get_node_bios_clear_cmos("slot" + repr(num)))
-    r_force_boot_bios_setup = tree("force_boot_bios_setup", data = get_node_bios_force_boot_setup("slot" + repr(num)))
-    r_boot_order = tree("boot_order", data = get_node_bios_boot_order("slot" + repr(num)))
+    r_boot_mode = tree("boot_mode", data = get_node_bios_boot_mode("slot" + repr(num), is_read_only))
+    r_clear_cmos = tree("clear_cmos", data = get_node_bios_clear_cmos("slot" + repr(num), is_read_only))
+    r_force_boot_bios_setup = tree("force_boot_bios_setup", data = get_node_bios_force_boot_setup("slot" + repr(num), is_read_only))
+    r_boot_order = tree("boot_order", data = get_node_bios_boot_order("slot" + repr(num), is_read_only))
     r_boot_order_trunk.addChildren([r_boot_mode, r_clear_cmos, r_force_boot_bios_setup, r_boot_order])
 
     return r_server
 
 # Initialize Platform specific Resource Tree
-def init_plat_tree():
+def init_plat_tree(is_read_only=True):
 
     # Create /api end point as root node
     r_api = tree("api", data = get_node_api())
 
     # Add /api/spb to represent side plane board
-    r_spb = tree("spb", data = get_node_spb())
+    r_spb = tree("spb", data = get_node_spb(is_read_only))
     r_api.addChild(r_spb)
 
     # Add /api/mezz to represent Network Mezzaine card
@@ -93,7 +93,7 @@ def init_plat_tree():
     # Add servers /api/server[1-max]
     num = pal_get_num_slots()
     for i in range(1, num+1):
-        r_server = populate_server_node(i)
+        r_server = populate_server_node(i, is_read_only)
         if r_server:
             r_api.addChild(r_server)
 
@@ -101,7 +101,7 @@ def init_plat_tree():
     r_temp = tree("fruid", data = get_node_fruid("spb"))
     r_spb.addChild(r_temp)
     # /api/spb/bmc end point
-    r_temp = tree("bmc", data = get_node_bmc())
+    r_temp = tree("bmc", data = get_node_bmc(is_read_only))
     r_spb.addChild(r_temp)
     # /api/spb/sensors end point
     r_temp = tree("sensors", data = get_node_sensors("spb"))
@@ -110,7 +110,7 @@ def init_plat_tree():
     r_temp = tree("fans", data = get_node_fans())
     r_spb.addChild(r_temp)
     # /api/spb/logs end point
-    r_temp = tree("logs", data = get_node_logs("spb"))
+    r_temp = tree("logs", data = get_node_logs("spb", is_read_only))
     r_spb.addChild(r_temp)
 
     # Add /api/mezz/fruid end point
@@ -120,7 +120,7 @@ def init_plat_tree():
     r_temp = tree("sensors", data = get_node_sensors("nic"))
     r_mezz.addChild(r_temp)
     # /api/mezz/logs end point
-    r_temp = tree("logs", data = get_node_logs("nic"))
+    r_temp = tree("logs", data = get_node_logs("nic", is_read_only))
     r_mezz.addChild(r_temp)
 
     return r_api

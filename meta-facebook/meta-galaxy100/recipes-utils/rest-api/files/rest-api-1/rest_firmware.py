f65b7f9b5974405be0e017efcc9f8bc2e84c1ca5
Vineela Kukkadapu <vineelak@fb.com>
2018-05-17 11:54:51 -0700

    Fix cpld firmware_info endpoint in galaxy
    Summary:
    While testing rest endpoints on helium, observed firmware endpoint
    was completely broken. Fixed it and made it use the corresponding scripts
    instead of sysfs, so there is only 1 point to change later.
    
    Test Plan:
    curl -g http://[2620:10d:c082:9002:2e0:ecff:fe45:6b66%984]:8080/api/sys/firmware_info | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   141  100   141    0     0     95      0  0:00:01  0:00:01 --:--:--    95
    {
        "QSFP_CPLD": {
            "sub_version": 0,
            "version": 0
        },
        "SCM_CPLD": {
            "sub_version": 1,
            "version": 17
        },
        "SYS_CPLD": {
            "sub_version": 2,
            "version": 16
        }
    }
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: db62a6d

--- meta-facebook/meta-galaxy100/recipes-utils/rest-api/files/rest-api-1/rest_firmware.py
+++ meta-facebook/meta-galaxy100/recipes-utils/rest-api/files/rest-api-1/rest_firmware.py
@@ -11,59 +11,52 @@
 # FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 # for more details.
 #
-import os
 import subprocess
 import bmc_command
 import syslog
 
-
-def _get_version(version_file):
-    with open(version_file) as f:
-        return f.read()
+SCM_CPLD_PATH = '/usr/local/bin/scm_cpld_rev.sh'
+SYS_CPLD_VERSION = '/usr/local/bin/cpld_rev.sh'
+QSFP_CPLD_VERSION = '/usr/local/bin/qsfp_cpld_ver.sh'
 
 
 def _firmware_json(ver, sub_ver, int_base=16):
     return {
-        'version' : int(ver, int_base),
-        'sub_version' : int(sub_ver, int_base)
+        'version': int(ver, int_base),
+        'sub_version': int(sub_ver, int_base)
     }
 
 
-def _get_syscpld_info():
-    SYS_CPLD_PATH = '/sys/class/i2c-adapter/i2c-12/12-0031'
-    ver = _get_version(os.path.join(SYS_CPLD_PATH, 'cpld_rev'))
-    sub_ver = _get_version(os.path.join(SYS_CPLD_PATH, 'cpld_sub_rev'))
-    return _firmware_json(ver, sub_ver)
-
-
-def _get_scm_cpld_info():
-    SCM_CPLD_PATH = '/sys/class/i2c-adapter/i2c-0/0-003e'
-    ver = _get_version(os.path.join(SCM_CPLD_PATH, 'cpld_rev'))
-    sub_ver = _get_version(os.path.join(SCM_CPLD_PATH,'cpld_sub_rev'))
-    return _firmware_json(ver, sub_ver)
-
-
-def _get_qsfp_cpld_info():
+def _get_qsfp_cpld_info(cmd):
     ver, sub_ver = 0, 0
-    proc = subprocess.Popen(['/usr/local/bin/qsfp_cpld_ver.sh'],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
     try:
-        data, err = bmc_command.timed_communicate(proc)
+        data = subprocess.check_output([cmd])
         data = data.decode()
         ver, sub_ver = data.split(',')
     except Exception as e:
-        syslog.syslog(syslog.LOG_ERR, 'Error getting QSFP CPLD versions : {}'
+        syslog.syslog(syslog.LOG_ERR, 'Error getting CPLD versions : {}'
                       .format(e))
 
     return _firmware_json(ver, sub_ver)
 
 
+def _get_cpld_info(cmd):
+    ver, sub_ver = 0, 0
+    try:
+        data = subprocess.check_output([cmd])
+        data = data.decode()
+        ver, sub_ver = data.split('.')
+    except Exception as e:
+        syslog.syslog(syslog.LOG_ERR, 'Error getting CPLD versions : {}'
+                      .format(e))
+
+    return _firmware_json(ver, sub_ver, int_base=10)
+
 def get_firmware_info():
     return {
-        'SYS_CPLD': _get_syscpld_info(),
-        'SCM_CPLD': _get_scm_cpld_info(),
-        'QSFP_CPLD': _get_qsfp_cpld_info()
+        'SYS_CPLD': _get_cpld_info(cmd=SYS_CPLD_VERSION),
+        'SCM_CPLD': _get_cpld_info(cmd=SCM_CPLD_PATH),
+        'QSFP_CPLD': _get_qsfp_cpld_info(cmd=QSFP_CPLD_VERSION)
     }
 
 

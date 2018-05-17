49e1bf5d9d11a88f77c56cc4119ea90dc2619e3d
Vineela Kukkadapu <vineelak@fb.com>
2018-05-17 15:31:51 -0700

    Add support for testing simple scripts and check exit status
    Summary:
    Added integration test to check if scritps are exiting
    properly. This diff adds a json that a python test will take as
    input and simply executes the script and checks the returncode.
    
    Test Plan:
    root@bmc-oob:/tmp/tests# python common/bmcUtiltest.py galaxy100/unittests/bmcUtilTest.json
    executing command: cpld_rev.sh
    executing command: scm_cpld_rev.sh
    executing command: ec_version.sh
    executing command: qsfp_cpld_ver.sh
    executing command: wedge_us_mac.sh
    executing command: galaxy100_cp2112_i2c_flush.sh
    BMC util Test: All scripts ran successfully [PASSED]
    
    Reviewed By: covracer
    
    fbshipit-source-id: bf6fab6

---
+++ tests/common/bmcUtiltest.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import sys
+import subprocess
+import unitTestUtil
+import logging
+
+
+def bmc_util_check(data):
+    """
+    Given a json of shell scripts, check that all of them run and have
+    exited properly
+    """
+    for script in data:
+        print("executing command: {}".format(script))
+        f = subprocess.Popen(script,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        failed = []
+        if f.returncode is not None:
+            failed += [str(script)]
+    if len(failed) == 0:
+        print("BMC util Test: All scripts ran successfully [PASSED]")
+        sys.exit(0)
+    else:
+        print("BMC util Test Failed scripts: " + str(failed) + "[FAILED]")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    """
+    Input to this file should look like the following:
+    python bmcUtilTest.py bmcUtil.json
+    """
+    util = unitTestUtil.UnitTestUtil()
+    logger = util.logger(logging.WARN)
+    try:
+        data = {}
+        args = util.Argparser(['json', '--verbose'],
+                              [str, None],
+                              ['json file',
+                              'output all steps from test with mode options:\
+                              DEBUG, INFO, WARNING, ERROR'])
+        if args.verbose is not None:
+            logger = util.logger(args.verbose)
+        data = util.JSONparser(args.json)
+        bmc_util_check(data)
+    except Exception as e:
+        print("BMC util test [FAILED]")
+        print("Error code returned: " + str(e))
+    sys.exit(1)

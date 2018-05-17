d71f7aa41cbfb0e5e5ca2d7c79053f0833fd0662
Ben Wei <benwei@fb.com>
2018-05-17 15:53:31 -0700

    fby2: hsvc-util, restores host power at the end of hot-service
    Summary:
    in case BMC was rebooted (e.g. via sled removal), ensure
       hosts are powered back up after hot service
    
    Test Plan: verified on YosemiteV2 PVT systems
    
    Reviewed By: amithash
    
    fbshipit-source-id: a3c6c9b

--- meta-facebook/meta-fby2/recipes-fby2/plat-utils/files/hsvc-util/hsvc-util.c
+++ meta-facebook/meta-fby2/recipes-fby2/plat-utils/files/hsvc-util/hsvc-util.c
@@ -63,6 +63,8 @@ print_usage_help(void) {
 int
 activate_hsvc(uint8_t slot_id) {
   char cmd[128] = {0};
+  char cvalue[MAX_VALUE_LEN] = {0};
+  char key[MAX_KEY_LEN] = {0};
   int pair_set_type;
   int pair_slot_id;
   int runoff_id = slot_id;
@@ -134,6 +136,20 @@ activate_hsvc(uint8_t slot_id) {
     printf("SLED is ready to be pulled out for hot-service now\n");
   } else {
     printf("Fail to turn slot%d to 12V-off\n",slot_id);
+    return -1;
+  }
+
+  sprintf(key, "slot%d_por_cfg", slot_id);
+  if (pal_get_key_value(key, cvalue) == 0) {
+    if (!memcmp(cvalue, "lps",  strlen("lps"))) {
+      ret = pal_set_last_pwr_state(slot_id, "on");
+      if (ret < 0) {
+        printf("Failed to set slot power status to on during Hot-service\n");
+        return ret;
+      }
+    }
+  } else {
+    printf("Failed to get slot %d power policy\n", slot_id);
     return -1;
   }
 

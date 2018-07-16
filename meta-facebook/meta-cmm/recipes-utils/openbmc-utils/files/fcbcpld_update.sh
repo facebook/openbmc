222ece19a61a8a72926db64b528a6cf375fde344
Tao Ren <taoren@fb.com>
2018-07-16 16:54:53 -0700

    fix gpio number in fcbcpld_update.sh
    Summary:
    Instead of using hardcoded gpio pin numbers, let's extract gpio number
    from symbolic links (under /tmp/gpionames) so that the script won't be
    broken even though pin number changed in the future.
    
    Test Plan:
    1) Dumped gpio pin numbers in both 4.1 and 4.17 kernel: the numbers are
       correct.
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: f0fd48863

--- meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/fcbcpld_update.sh
+++ meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/fcbcpld_update.sh
@@ -19,15 +19,21 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
+source /usr/local/bin/gpio-utils.sh
+
 if [ $# -ne 2 ]; then
     exit -1
 fi
 
 declare -A fcb_gpio_map
-fcb_gpio_map[fcb1]=456
-fcb_gpio_map[fcb2]=464
-fcb_gpio_map[fcb3]=472
-fcb_gpio_map[fcb4]=480
+fcb1_gpiochip=$(gpiochip_lookup_by_i2c_path "172-0022")
+fcb2_gpiochip=$(gpiochip_lookup_by_i2c_path "180-0022")
+fcb3_gpiochip=$(gpiochip_lookup_by_i2c_path "188-0022")
+fcb4_gpiochip=$(gpiochip_lookup_by_i2c_path "196-0022")
+fcb_gpio_map[fcb1]=$(gpiochip_get_base ${fcb1_gpiochip})
+fcb_gpio_map[fcb2]=$(gpiochip_get_base ${fcb2_gpiochip})
+fcb_gpio_map[fcb3]=$(gpiochip_get_base ${fcb3_gpiochip})
+fcb_gpio_map[fcb4]=$(gpiochip_get_base ${fcb4_gpiochip})
 
 card="${1,,}"
 img="$2"

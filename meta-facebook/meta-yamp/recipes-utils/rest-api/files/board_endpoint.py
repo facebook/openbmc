61c6ea5aa2995477615f7d07f2a47b6ff822e0fc
Mike Choi <mikechoi@fb.com>
2018-08-08 23:00:26 -0700

    Expose board specific APIs of piminfo, pim_presence and fruid_scm, through YAMP BMC's REST server
    Summary: [YAMP] Expose board specific APIs of piminfo, pim_presence and fruid_scm
    
    Test Plan:
    P59919896
    
    ```
    [cov@devvm3446.prn2 ~/fbcode] buck run bmc/tools/proxy:central_proxy -- --devices '[2620:10d:c082:9002:7683:efff:fe02:95d0]'
    [...]
    Checking 1 OpenBMC REST APIs.
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.bmc_version: 90000
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.load-1: 18.0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.load-5: 13.0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.load-15: 10.0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.idle: 81
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.io: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.irq: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.nic: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.sirq: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.sys: 18
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.cpu.usr: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.mem.buff_KB: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.mem.cached_KB: 68128
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.mem.free_KB: 654772
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.mem.shrd_KB: 5888
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.mem.used_KB: 112020
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.uptime: 34682
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.reachable.restapi: 1
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.asd.running: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.vboot.status: -1
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.vboot.hw_enforcement: 0
    [2620:10d:c082:9002:7683:efff:fe02:95d0] bmc.vboot.supported: 0
    100.00% (1) OpenBMC REST APIs were reachable; 0.00% (0) were unreachable.
    Versions {90000: 1}
    Uploaded 22 ODS values including 1 REST API reachability measurements.
    ```
    
    Reviewed By: covracer
    
    fbshipit-source-id: e42064860

---
+++ meta-facebook/meta-yamp/recipes-utils/rest-api/files/board_endpoint.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+from aiohttp import web
+from rest_utils import get_endpoints
+from rest_utils import dumps_bytestr
+import rest_fruid_scm
+import rest_pim_present
+import rest_piminfo
+class boardApp_Handler:
+    # Handler for sys/mb/fruid/scm resource endpoint
+    async def rest_fruid_scm_hdl(self, request):
+        return web.json_response(rest_fruid_scm.get_fruid_scm(), dumps=dumps_bytestr)
+
+    # Handler for sys/pim_present resource endpoint
+    async def rest_pim_present_hdl(self, request):
+        return web.json_response(rest_pim_present.get_pim_present(), dumps=dumps_bytestr)
+
+    # Handler for sys/pim_info resource endpoint (version)
+    async def rest_piminfo_hdl(self,request):
+        return web.json_response(rest_piminfo.get_piminfo())
---
+++ meta-facebook/meta-yamp/recipes-utils/rest-api/rest-api_0.1.bbappend
@@ -0,0 +1,30 @@
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+SRC_URI += " \
+    file://rest-api-1/rest_fruid_scm.py \
+    file://rest-api-1/rest_piminfo.py \
+    file://rest-api-1/rest_pim_present.py \
+"
+
+binfiles1 += " \
+    rest_fruid_scm.py \
+    rest_piminfo.py \
+    rest_pim_present.py \
+"

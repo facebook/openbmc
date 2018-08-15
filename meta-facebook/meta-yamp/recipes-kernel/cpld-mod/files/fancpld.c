6ff4d7a9be12d526e2ccaa936863d8933ae5870f
Mike Choi <mikechoi@fb.com>
2018-08-15 13:55:02 -0700

    Enhance YAMP FANCPLD driver to work well with fscd
    Summary:
    [YAMP] Enhance YAMP FANCPLD driver to work well with fscd
    1) RPM read will fail if FAN is not present
    2) If pwm value is bigger than 255, we will write 255 to pwm, as pwm range is [0,255]
    3) If pwm value is small than 0, we will write 0 to pwm, as pwm range is [0,255]
    
    Test Plan:
    Testing that RPM read fails when fan is unplugged : P59946232
    
    Testing that PWM should stay within [0,255] range : P59946213
    
    Reviewed By: benwei13
    
    fbshipit-source-id: c6090d991

--- meta-facebook/meta-yamp/recipes-kernel/cpld-mod/files/fancpld.c
+++ meta-facebook/meta-yamp/recipes-kernel/cpld-mod/files/fancpld.c
@@ -76,13 +76,71 @@
   "0x1: masked"
 
 #define YAMP_FAN_RPM_CONSTANT 6000000
+#define YAMP_FAN1_RPM 0x10
+#define YAMP_FAN2_RPM 0x12
+#define YAMP_FAN3_RPM 0x14
+#define YAMP_FAN4_RPM 0x16
+#define YAMP_FAN5_RPM 0x18
+#define YAMP_FAN_INSTANCES 5
+#define YAMP_FAN_PRESENCE 0x49
 
+static bool detect_fan_presence(struct device *dev,
+                                struct device_attribute *attr,
+                                char *buf)
+{
+  struct i2c_client *client = to_i2c_client(dev);
+  i2c_dev_data_st *data = i2c_get_clientdata(client);
+  i2c_sysfs_attr_st *i2c_attr = TO_I2C_SYSFS_ATTR(attr);
+  const i2c_dev_attr_st *dev_attr = i2c_attr->isa_i2c_attr;
+  int instance = -1, i = 0;
+  int ret_val = 0;
+  int yamp_fan_map[YAMP_FAN_INSTANCES] = { YAMP_FAN1_RPM, YAMP_FAN2_RPM,
+                                           YAMP_FAN3_RPM, YAMP_FAN4_RPM,
+                                           YAMP_FAN5_RPM };
+  int reg_addr = dev_attr->ida_reg;
+  // Based on register addr, try detecting instance number
+  for (i = 0; i < YAMP_FAN_INSTANCES; i++) {
+    if (reg_addr == yamp_fan_map[i])
+      instance = i;
+  }
+
+  // If the address is none of the fan rpm register, it's an invalid input.
+  // Therefore, just return false.
+  if (instance == -1) {
+    return false;
+  }
+
+  // Now, read fan presence register
+  ret_val = i2c_smbus_read_byte_data(client, YAMP_FAN_PRESENCE);
+  if (ret_val < 0)
+    return false;
+
+  // Check if our instance of fan exists, if so return true
+  if (ret_val & (1 << instance))
+    return true;
+
+  // Fan not present. Return false
+  return false;
+}
+
+// This is RPM SHOW function tailored for YAMP. The function will
+// first detect fan presence, and if the fan is present, it will
+// read RPM register, calculate the RPM and return the value.
+// Otherwise, it returns error.
 static ssize_t fancpld_fan_rpm_show(struct device *dev,
                                     struct device_attribute *attr,
                                     char *buf)
 {
   int val;
 
+  // In YAMP, when a fan is missing or dead, FANCPLD will
+  // still show the RPM value from the last successful read,
+  // which is not correct. So we check if the fan is present
+  // , and will return error if the fan is not present.
+  if (!detect_fan_presence(dev, attr, buf))
+    return -EINVAL;
+
+  // Fan exists. Move ahead to read RPM register
   val = i2c_dev_read_word_littleendian(dev, attr);
   if (val < 0) {
     return val;
@@ -92,6 +150,42 @@ static ssize_t fancpld_fan_rpm_show(struct device *dev,
   val = YAMP_FAN_RPM_CONSTANT / val;
 
   return scnprintf(buf, PAGE_SIZE, "%u\n", val);
+}
+
+// FANCPLD PWM STORE function. If the RPM value is not within [0,255] range
+// , change the value to fit into this range. For example, if a user try to
+// write 280, it will still write 255, instead of writing 280 to cause
+// overflow, which ends up writing 24 (280 - 256) as PWM.
+// This behavior is required by FSCD boost algorithm.
+static ssize_t fancpld_fan_pwm_store(struct device *dev,
+                                     struct device_attribute *attr,
+                                     const char *buf, size_t count)
+{
+  struct i2c_client *client = to_i2c_client(dev);
+  i2c_dev_data_st *data = i2c_get_clientdata(client);
+  i2c_sysfs_attr_st *i2c_attr = TO_I2C_SYSFS_ATTR(attr);
+  const i2c_dev_attr_st *dev_attr = i2c_attr->isa_i2c_attr;
+  int val;
+  int req_val;
+
+  /* parse the buffer */
+  if (sscanf(buf, "%i", &req_val) <= 0) {
+    return -EINVAL;
+  }
+  if (req_val > 255)
+    req_val=255;
+  if (req_val < 0)
+    req_val = 0;
+
+  mutex_lock(&data->idd_lock);
+  val = i2c_smbus_write_byte_data(client, dev_attr->ida_reg, req_val);
+  mutex_unlock(&data->idd_lock);
+
+  if (val < 0) {
+    /* error case */
+    return val;
+  }
+  return count;
 }
 
 static const i2c_dev_attr_st fancpld_attr_table[] = {
@@ -114,69 +208,69 @@ static const i2c_dev_attr_st fancpld_attr_table[] = {
     NULL,
     fancpld_fan_rpm_show,
     NULL,
-    0x10, 0, 8,
+    YAMP_FAN1_RPM, 0, 8,
   },
   {
     "fan2_input",
     NULL,
     fancpld_fan_rpm_show,
     NULL,
-    0x12, 0, 8,
+    YAMP_FAN2_RPM, 0, 8,
   },
   {
     "fan3_input",
     NULL,
     fancpld_fan_rpm_show,
     NULL,
-    0x14, 0, 8,
+    YAMP_FAN3_RPM, 0, 8,
   },
   {
     "fan4_input",
     NULL,
     fancpld_fan_rpm_show,
     NULL,
-    0x16, 0, 8,
+    YAMP_FAN4_RPM, 0, 8,
   },
   {
     "fan5_input",
     NULL,
     fancpld_fan_rpm_show,
     NULL,
-    0x18, 0, 8,
+    YAMP_FAN5_RPM, 0, 8,
   },
   {
     "fan1_pwm",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
-    I2C_DEV_ATTR_STORE_DEFAULT,
+    fancpld_fan_pwm_store,
     0x30, 0, 8,
   },
   {
     "fan2_pwm",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
-    I2C_DEV_ATTR_STORE_DEFAULT,
+    fancpld_fan_pwm_store,
     0x31, 0, 8,
   },
   {
     "fan3_pwm",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
-    I2C_DEV_ATTR_STORE_DEFAULT,
+    fancpld_fan_pwm_store,
     0x32, 0, 8,
   },
   {
     "fan4_pwm",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
-    I2C_DEV_ATTR_STORE_DEFAULT,
+    fancpld_fan_pwm_store,
     0x33, 0, 8,
   },
   {
     "fan5_pwm",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
-    I2C_DEV_ATTR_STORE_DEFAULT,
+    fancpld_fan_pwm_store,
     0x34, 0, 8,
   },
   {
@@ -219,112 +313,112 @@ static const i2c_dev_attr_st fancpld_attr_table[] = {
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
     NULL,
-    0x49, 0, 1,
+    YAMP_FAN_PRESENCE, 0, 1,
   },
   {
     "fan2_present",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
     NULL,
-    0x49, 1, 1,
+    YAMP_FAN_PRESENCE, 1, 1,
   },
   {
     "fan3_present",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
     NULL,
-    0x49, 2, 1,
+    YAMP_FAN_PRESENCE, 2, 1,
   },
   {
     "fan4_present",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
     NULL,
-    0x49, 3, 1,
+    YAMP_FAN_PRESENCE, 3, 1,
   },
   {
     "fan5_present",
     NULL,
     I2C_DEV_ATTR_SHOW_DEFAULT,
     NULL,
-    0x49, 4, 1,
+    YAMP_FAN_PRESENCE, 4, 1,
   },
   {
     "fan1_led_green",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4b, 0, 1,
   },
   {
     "fan2_led_green",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4b, 1, 1,
   },
   {
     "fan3_led_green",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4b, 2, 1,
   },
   {
     "fan4_led_green",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4b, 3, 1,
   },
   {
     "fan5_led_green",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4b, 4, 1,
   },
   {
     "fan1_led_red",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4c, 0, 1,
   },
   {
     "fan2_led_red",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4c, 1, 1,
   },
   {
     "fan3_led_red",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4c, 2, 1,
   },
   {
     "fan4_led_red",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4c, 3, 1,
   },
   {
     "fan5_led_red",
-    "0x1: On\n"
-    "0x0: Off",
+    "0x1: Off\n"
+    "0x0: On",
     I2C_DEV_ATTR_SHOW_DEFAULT,
     I2C_DEV_ATTR_STORE_DEFAULT,
     0x4c, 4, 1,

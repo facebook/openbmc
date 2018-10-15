88df7ae8835526973d5a72e87ef83eb8e6246ba0
Tao Ren <taoren@fb.com>
2018-10-14 23:34:43 -0700

    Move common i2c utility functions to i2c-utils.sh
    Summary:
    Move common i2c utility functions from cmm setup_i2c.sh to i2c-utils.sh
    because these functions are also needed by minipack and other platforms.
    
    Test Plan:
    1) "bitbake cmm-image" succeeded.
    2) booted up cmm image and verified all i2c devices are created
       properly.
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: 3aeb5294a

--- common/recipes-utils/openbmc-utils/files/i2c-utils.sh
+++ common/recipes-utils/openbmc-utils/files/i2c-utils.sh
@@ -15,6 +15,15 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 
+SYSFS_I2C_DEVICES="/sys/bus/i2c/devices"
+SYSFS_I2C_DRIVERS="/sys/bus/i2c/drivers"
+
+#
+# instantiate an i2c device.
+# $1 - parent bus number
+# $2 - device address
+# $3 - device name/type
+#
 i2c_device_add() {
     local bus
     local addr
@@ -25,10 +34,173 @@ i2c_device_add() {
     echo ${device} ${addr} > /sys/class/i2c-dev/i2c-${bus}/device/new_device
 }
 
+#
+# delete an i2c device.
+# $1 - parent bus number
+# $2 - device address
+#
 i2c_device_delete() {
     local bus
     local addr
     bus=$"$1"
     addr="$2"
     echo ${addr} > /sys/class/i2c-dev/i2c-${bus}/device/delete_device
+}
+
+#
+# instantiate an i2c-mux device and wait untill all its child buses
+# are initialized before the function returns.
+# $1 - parent bus number
+# $2 - i2c-mux device address
+# $3 - i2c-mux device name/type
+# $4 - bus number of the last i2c-mux channel
+#
+i2c_mux_add_sync() {
+    retry=0
+    max_retry=100
+    bus_dir="/sys/class/i2c-dev/i2c-${4}"
+
+    i2c_device_add ${1} ${2} ${3}
+
+    until [ -d ${bus_dir} ]
+    do
+        usleep 2000 # sleep for 2 milliseconds
+
+        retry=$((retry + 1))
+        if [ $retry -ge ${max_retry} ]
+        then
+            echo "failed to create child buses for i2c-mux ${1}-${2}!"
+            return 1
+        fi
+    done
+
+    return 0
+}
+
+#
+# Lookup i2c driver based on device name.
+# $1 - device name
+#
+i2c_driver_map() {
+    local device_name=$1
+
+    case ${device_name} in
+        "24c64")
+            echo "at24";;
+        "tmp75")
+            echo "lm75";;
+        "pca9505" | "pca9534" | "pca9535")
+            echo "pca953x";;
+        "pca9548")
+            echo "pca954x";;
+        "pfe3000")
+            echo "pfe3000";;
+        "fancpld")
+            echo "fancpld";;
+        "cmmcpld")
+            echo "cmmcpld";;
+        *)
+            echo "";;
+    esac
+}
+
+#
+# Trigger driver binding manually
+# $1 - driver name
+# $2 - device path in <bus>-<addr> format.
+#
+i2c_driver_bind_device() {
+    local retry=0
+    local retry_max=2
+    local sleep_cnt=0
+    local sleep_max=20
+    local driver_name=${1}
+    local i2c_dev=${2}
+    local driver_dir="${SYSFS_I2C_DRIVERS}/${driver_name}"
+    local driver_link="${SYSFS_I2C_DEVICES}/${i2c_dev}/driver"
+
+    if [ ! -d ${driver_dir} ]; then
+        echo "unable to locate i2c driver ${driver_name} in sysfs"
+        return 1
+    fi
+
+    until [ -L ${driver_link} ]
+    do
+        if [ $retry -ge ${retry_max} ]; then
+            echo "failed to bind ${i2c_dev} after ${retry} retries"
+            return 1
+        fi
+        echo ${i2c_dev} > "${driver_dir}/bind" 2> /dev/null
+
+        sleep_cnt=0
+        while [ ! -L ${driver_link} ] && [ ${sleep_cnt} -lt ${sleep_max} ]
+        do
+            usleep 500 # sleep for 500 microseconds
+            sleep_cnt=$((sleep_cnt + 1))
+        done
+
+        retry=$((retry + 1))
+    done
+
+    echo "i2c driver ${driver_name} binded to ${i2c_dev} successfully"
+    return 0
+}
+
+#
+# The function goes through all i2c devices and check if some devices
+# are not claimed by drivers (due to scheduling delay, missing drivers,
+# intermittent i2c transaction errors in device_probe() function, and
+# etc.).
+# If callers pass "force_binding" parameter, then the function will
+# trigger manual driver binding for devices without drivers.
+# $1 - force binding driver
+#
+i2c_check_driver_binding() {
+    local total_devs=0
+    local errors=0
+    local errors_fixed=0
+    local force_binding=$1
+    local sum_info error_info
+    local driver_name dev_name dev_path
+
+    echo "checking i2c driver binding status"
+    for filename in `ls ${SYSFS_I2C_DEVICES}`; do
+        if [[ ${filename} == *-00* ]]; then
+            total_devs=$((total_devs + 1))
+
+            dev_path="${SYSFS_I2C_DEVICES}/${filename}"
+            if [ -L "${dev_path}/driver" ]; then
+                continue # driver binded successfully
+            fi
+
+            errors=$((errors + 1))
+            dev_name=`cat ${dev_path}/name`
+            echo "${filename} (${dev_name}) - no driver binded"
+
+            if [ -z ${force_binding} ] ||
+               [ ${force_binding} != "force_binding" ]; then
+                continue
+            fi
+
+            driver_name=$(i2c_driver_map ${dev_name})
+            if [ -z ${driver_name} ]; then
+                echo "unable to find driver for ${filename}"
+                continue
+            fi
+
+            echo "manually binding ${filename} to driver ${driver_name}"
+            i2c_driver_bind_device ${driver_name} ${filename}
+            if [ "$?" -eq "0" ]; then
+                errors_fixed=$((errors_fixed + 1))
+            fi
+        fi
+    done
+
+    sum_info="total ${total_devs} i2c devices checked"
+    if [ ${errors} -eq 0 ]; then
+        err_info="no errors found"
+    else
+        err_info="${errors} errors found, ${errors_fixed} errors fixed"
+    fi
+    echo "${sum_info}: ${err_info}"
 }
--- meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/setup_i2c.sh
+++ meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/setup_i2c.sh
@@ -31,46 +31,7 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
-SYSFS_I2C_DEVICES="/sys/bus/i2c/devices"
-SYSFS_I2C_DRIVERS="/sys/bus/i2c/drivers"
-
-#
-# instantiate an i2c device
-# $1 - device name/type
-# $2 - device address
-# $3 - parent bus number
-#
-add_device() {
-    echo ${1} ${2} > /sys/class/i2c-dev/i2c-${3}/device/new_device
-}
-
-#
-# instantiate an i2c-mux device and wait untill all its child buses
-# are initialized before the function returns.
-# $1 - device name/type
-# $2 - device address
-# $3 - parent bus number
-# $4 - bus number of the last i2c-mux channel
-#
-add_i2c_mux_sync() {
-    retry=0
-    max_retry=100
-    bus_dir="/sys/class/i2c-dev/i2c-${4}"
-
-    add_device ${1} ${2} ${3}
-
-    until [ -d ${bus_dir} ]
-    do
-        usleep 2000 # sleep for 2 milliseconds
-
-        retry=$((retry + 1))
-        if [ $retry -ge ${max_retry} ]
-        then
-            echo "i2c-mux ${3}-${2} initialization failed: timer expired"
-            exit 1
-        fi
-    done
-}
+. /usr/local/bin/i2c-utils.sh
 
 #
 # instantiate all the i2c-muxes.
@@ -102,7 +63,7 @@ bulk_create_i2c_mux() {
     for bus in ${parent_buses}; do
         for addr in ${mux_addresses}; do
             last_child_bus=$((last_child_bus + 8))
-            add_i2c_mux_sync ${i2c_mux_name} ${addr} ${bus} ${last_child_bus}
+            i2c_mux_add_sync ${bus} ${addr} ${i2c_mux_name} ${last_child_bus}
         done
     done
 
@@ -113,136 +74,8 @@ bulk_create_i2c_mux() {
     parent_buses="32 33 34 35"
     for bus in ${parent_buses}; do
         last_child_bus=$((last_child_bus + 8))
-        add_i2c_mux_sync ${i2c_mux_name} 0x70 ${bus} ${last_child_bus}
+        i2c_mux_add_sync ${bus} 0x70 ${i2c_mux_name} ${last_child_bus}
     done
-}
-
-#
-# Lookup i2c driver based on device name.
-# $1 - device name
-#
-i2c_lookup_driver() {
-    local dev_name=$1
-
-    case ${dev_name} in
-        "24c64")
-            echo "at24";;
-        "tmp75")
-            echo "lm75";;
-        "pca9505" | "pca9534" | "pca9535")
-            echo "pca953x";;
-        "pca9548")
-            echo "pca954x";;
-        "pfe3000")
-            echo "pfe3000";;
-        "fancpld")
-            echo "fancpld";;
-        "cmmcpld")
-            echo "cmmcpld";;
-        *)
-            echo "";;
-    esac
-}
-
-#
-# Trigger driver binding manually
-# $1 - driver name
-# $2 - device path in <bus>-<addr> format.
-#
-manual_driver_binding() {
-    local retry=0
-    local retry_max=2
-    local sleep_cnt=0
-    local sleep_max=20
-    local driver_name=${1}
-    local i2c_dev=${2}
-    local driver_dir="${SYSFS_I2C_DRIVERS}/${driver_name}"
-    local driver_link="${SYSFS_I2C_DEVICES}/${i2c_dev}/driver"
-
-    if [ ! -d ${driver_dir} ]; then
-        echo "unable to locate i2c driver ${driver_name} in sysfs"
-        return 1
-    fi
-
-    until [ -L ${driver_link} ]
-    do
-        if [ $retry -ge ${retry_max} ]; then
-            echo "failed to bind ${i2c_dev} after ${retry} retries"
-            return 1
-        fi
-        echo ${i2c_dev} > "${driver_dir}/bind" 2> /dev/null
-
-        sleep_cnt=0
-        while [ ! -L ${driver_link} ] && [ ${sleep_cnt} -lt ${sleep_max} ]
-        do
-            usleep 500 # sleep for 500 microseconds
-            sleep_cnt=$((sleep_cnt + 1))
-        done
-
-        retry=$((retry + 1))
-    done
-
-    echo "i2c driver ${driver_name} binded to ${i2c_dev} successfully"
-    return 0
-}
-
-#
-# The function goes through all i2c devices and manually trigger driver
-# binding if device binding failed/delayed. It's needed because:
-#   1) i2c device creation and driver binding happens asynchronous,
-#      which means a device may not be claimed by its driver when
-#      add_device() returns. In this case, We need to wait for a short
-#      period so device manager gets chance to run and complete driver
-#      binding.
-#   2) We have seen some intermittent i2c transaction errors which leads
-#      to device probe failure. The root cause is not clear yet, but for
-#      now, let's try to bind the device again.
-#
-fixup_i2c_driver_binding() {
-    local total_devs=0
-    local errors=0
-    local errors_fixed=0
-    local sum_info error_info
-    local driver_name dev_name dev_path
-
-    echo "checking i2c driver binding status"
-
-    # sleep 50 milliseconds just in case driver binding is "delayed".
-    usleep 50000
-
-    for filename in `ls ${SYSFS_I2C_DEVICES}`; do
-        if [[ ${filename} == *-00* ]]; then
-           total_devs=$((total_devs + 1))
-
-           dev_path="${SYSFS_I2C_DEVICES}/${filename}"
-           if [ -L "${dev_path}/driver" ]; then
-              continue # driver binded successfully
-           fi
-
-           errors=$((errors + 1))
-
-           dev_name=`cat ${dev_path}/name`
-           driver_name=$(i2c_lookup_driver ${dev_name})
-           if [ -z ${driver_name} ]; then
-               echo "unable to find driver for ${filename}"
-               continue
-           fi
-
-           echo "manually binding ${filename} to driver ${driver_name}"
-           manual_driver_binding ${driver_name} ${filename}
-           if [ "$?" -eq "0" ]; then
-               errors_fixed=$((errors_fixed + 1))
-           fi
-        fi
-    done
-
-    sum_info="total ${total_devs} i2c devices checked"
-    if [ ${errors} -eq 0 ]; then
-        err_info="no errors found"
-    else
-        err_info="${errors} errors found, ${errors_fixed} errors fixed"
-    fi
-    echo "${sum_info}: ${err_info}"
 }
 
 KERNEL_VERSION=`uname -r`
@@ -251,22 +84,22 @@ if [[ ${KERNEL_VERSION} != 4.1.* ]]; then
 fi
 
 # EEPROM
-add_device 24c64 0x51 6
+i2c_device_add 6 0x51 24c64
 
 #PCB EEPROM
-add_device 24c64 0x50 28
+i2c_device_add 28 0x50 24c64
 
 # Two temperature sensors on CMM
-add_device tmp75 0x48 3         # inlet
-add_device tmp75 0x49 3         # outlet
+i2c_device_add 3 0x48 tmp75 # inlet
+i2c_device_add 3 0x49 tmp75 # outlet
 
 # CMM CPLD
-add_device cmmcpld 0x3e 13
+i2c_device_add 13 0x3e cmmcpld
 
 # FAN CPLD
 fancpld_busses="171 179 187 195"
 for bus in ${fancpld_busses}; do
-    add_device fancpld 0x33 ${bus}
+    i2c_device_add ${bus} 0x33 fancpld
 done
 
 # Temperature sensors
@@ -303,15 +136,15 @@ temp_busses="42 45 58 61 74 77 90 93 106 122 138 154"
 temp_addrs="0x48 0x49 0x4a 0x4b"
 for bus in ${temp_busses}; do
     for addr in ${temp_addrs}; do
-        add_device tmp75 ${addr} ${bus}
+        i2c_device_add ${bus} ${addr} tmp75
     done
 done
 
 # Temperature sensors on SCM
 temp_busses="113 129 49 65 81 97 145 161"
 for bus in ${temp_busses}; do
-    add_device tmp75 0x48 ${bus}
-    add_device tmp75 0x49 $((bus + 1))
+    i2c_device_add ${bus} 0x48 tmp75
+    i2c_device_add $((bus + 1)) 0x49 tmp75
 done
 
 # FCB PCA9534
@@ -320,26 +153,29 @@ done
 # so that GPIO numbers are increasing from FCB1-FCB4
 fcb_busses="196 188 180 172"
 for bus in ${fcb_busses}; do
-    add_device pca9534 0x22 ${bus}
+    i2c_device_add ${bus} 0x22 pca9534
 done
 
 # PSU PRESENCE
 # Spec says pca9506 but this kernel version supports only 9505
 # which works for us
-add_device pca9505 0x20 29
+i2c_device_add 29 0x20 pca9505
 
 # PFE modules
-add_device pfe3000 0x10 27 # psu1
-add_device pfe3000 0x10 26 # psu2
-add_device pfe3000 0x10 25 # psu3
-add_device pfe3000 0x10 24 # psu4
+i2c_device_add 27 0x10 pfe3000 # psu1
+i2c_device_add 26 0x10 pfe3000 # psu2
+i2c_device_add 25 0x10 pfe3000 # psu3
+i2c_device_add 24 0x10 pfe3000 # psu4
 
 # LEDs
-add_device pca9535 0x21 30
+i2c_device_add 30 0x21 pca9535
 
 #
 # Go through all i2c devices and trigger manual driver binding if needed.
 #
 if [[ ${KERNEL_VERSION} != 4.1.* ]]; then
-    fixup_i2c_driver_binding
+    # sleep 50 milliseconds just in case driver binding is "delayed".
+    usleep 50000
+
+    i2c_check_driver_binding "force_binding"
 fi

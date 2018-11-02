86a0dbaaccfc8ef793040ed2289f07980d669fa3
Amithash Prasad <amithash@fb.com>
2018-11-02 11:59:07 -0700

    fbtp: Avoid unnecessary env variable sets
    Summary:
    If the env variable is set to the value we need,
    avoid calling fw_setenv.
    
    Test Plan:
    # Get current state of key (and u-boot env key)
    root@bmc-oob:~# cfg-util server_por_cfg
    on
    root@bmc-oob:~# fw_printenv por_policy
    por_policy=on
    
    # Change value to off. Check change is persisted.
    root@bmc-oob:~# cfg-util server_por_cfg off
    root@bmc-oob:~# cfg-util server_por_cfg
    off
    root@bmc-oob:~# fw_printenv por_policy
    por_policy=off
    
    # rename fw_setenv to ensure future calls fail.
    root@bmc-oob:~# mv /sbin/fw_setenv /sbin/fw_setenv.back
    
    # Set key to current value (Ensure fw_setenv is not called).
    root@bmc-oob:~# cfg-util server_por_cfg off
    root@bmc-oob:~# fw_printenv por_policy
    por_policy=off
    
    # Set key to a different value from current (Ensure it throws an error in trying to execute fw_setenv)
    root@bmc-oob:~# cfg-util server_por_cfg on
    sh: /sbin/fw_setenv: No such file or directory
    
    Reviewed By: covracer
    
    fbshipit-source-id: ff1b95eb9

--- meta-facebook/meta-fbtp/recipes-fbtp/plat-libs/files/pal/pal.c
+++ meta-facebook/meta-fbtp/recipes-fbtp/plat-libs/files/pal/pal.c
@@ -3066,10 +3066,48 @@ pal_set_key_value(char *key, char *value) {
   return kv_set(key, value, 0, KV_FPERSIST);
 }
 
+static int fw_getenv(char *key, char *value)
+{
+  char cmd[MAX_KEY_LEN + 32] = {0};
+  char *p;
+  FILE *fp;
+
+  sprintf(cmd, "/sbin/fw_printenv -n %s", key);
+  fp = popen(cmd, "r");
+  if (!fp) {
+    return -1;
+  }
+  if (fgets(value, MAX_VALUE_LEN, fp) == NULL) {
+    pclose(fp);
+    return -1;
+  }
+  for (p = value; *p != '\0'; p++) {
+    if (*p == '\n' || *p == '\r') {
+      *p = '\0';
+      break;
+    }
+  }
+  pclose(fp);
+  return 0;
+}
+
+static void fw_setenv(char *key, char *value)
+{
+  char old_value[MAX_VALUE_LEN] = {0};
+  if (fw_getenv(key, old_value) != 0 ||
+      strcmp(old_value, value) != 0) {
+    /* Set the env key:value if either the key
+     * does not exist or the value is different from
+     * what we want set */
+    char cmd[MAX_VALUE_LEN] = {0};
+    snprintf(cmd, MAX_VALUE_LEN, "/sbin/fw_setenv %s %s", key, value);
+    system(cmd);
+  }
+}
+
 static int
 key_func_por_policy (int event, void *arg)
 {
-  char cmd[MAX_VALUE_LEN] = {0};
   char value[MAX_VALUE_LEN] = {0};
 
   switch (event) {
@@ -3078,8 +3116,7 @@ key_func_por_policy (int event, void *arg)
         return -1;
       // sync to env
       if ( !strcmp(arg,"lps") || !strcmp(arg,"on") || !strcmp(arg,"off")) {
-        snprintf(cmd, MAX_VALUE_LEN, "/sbin/fw_setenv por_policy %s", (char *)arg);
-        system(cmd);
+        fw_setenv("por_policy", (char *)arg);
       }
       else
         return -1;
@@ -3087,8 +3124,7 @@ key_func_por_policy (int event, void *arg)
     case KEY_AFTER_INI:
       // sync to env
       kv_get("server_por_cfg", value, NULL, KV_FPERSIST);
-      snprintf(cmd, MAX_VALUE_LEN, "/sbin/fw_setenv por_policy %s", value);
-      system(cmd);
+      fw_setenv("por_policy", value);
       break;
   }
 
@@ -3098,20 +3134,17 @@ key_func_por_policy (int event, void *arg)
 static int
 key_func_lps (int event, void *arg)
 {
-  char cmd[MAX_VALUE_LEN] = {0};
   char value[MAX_VALUE_LEN] = {0};
 
   switch (event) {
     case KEY_BEFORE_SET:
       if (pal_is_fw_update_ongoing(FRU_MB))
         return -1;
-      snprintf(cmd, MAX_VALUE_LEN, "/sbin/fw_setenv por_ls %s", (char *)arg);
-      system(cmd);
+      fw_setenv("por_ls", (char *)arg);
       break;
     case KEY_AFTER_INI:
       kv_get("pwr_server_last_state", value, NULL, KV_FPERSIST);
-      snprintf(cmd, MAX_VALUE_LEN, "/sbin/fw_setenv por_ls %s", value);
-      system(cmd);
+      fw_setenv("por_ls", value);
       break;
   }
 

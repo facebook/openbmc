638a181d7945b4d1e7658bc3b3792c39a63dcc7c
Mike Choi <mikechoi@fb.com>
2019-01-14 18:34:13 -0800

    minipack: Detect error before display cpld veriosn (#121)
    Summary:
    1. Detect error before display cpld veriosn
    Pull Request resolved: https://github.com/facebookexternal/openbmc.accton/pull/121
    
    Test Plan:
    Noraml case:
    root@bmc-oob:~# cpld_ver.sh
    SMBCPLD: 2.9
    SCMCPLD: 1.31
    Top FCMCPLD: 1.11
    Bottom FCMCPLD: 1.11
    Left PDBCPLD: 1.0
    Right PDBCPLD: 1.0
    
    Error case:
    root@bmc-oob:~# cpld_ver.sh
    SMBCPLD: 2.9
    SCMCPLD is not detected
    Top FCMCPLD: 1.11
    Bottom FCMCPLD: 1.11
    Left PDBCPLD: 1.0
    Right PDBCPLD is not detected
    
    Test on Minipack:pass
    
    Reviewed By: benwei13
    
    fbshipit-source-id: c5856d6c6

--- meta-facebook/meta-minipack/recipes-utils/openbmc-utils/files/cpld_ver.sh
+++ meta-facebook/meta-minipack/recipes-utils/openbmc-utils/files/cpld_ver.sh
@@ -24,26 +24,57 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
 board_rev=$(wedge_board_rev)
 
-smb_ver=$(cat /sys/class/i2c-adapter/i2c-12/12-003e/cpld_ver | head -n 1)
-smb_sub_ver=$(cat /sys/class/i2c-adapter/i2c-12/12-003e/cpld_sub_ver | head -n 1)
-scm_ver=$(cat /sys/class/i2c-adapter/i2c-2/2-0035/cpld_ver | head -n 1)
-scm_sub_ver=$(cat /sys/class/i2c-adapter/i2c-2/2-0035/cpld_sub_ver | head -n 1)
-fcm_top_ver=$(cat /sys/class/i2c-adapter/i2c-64/64-0033/cpld_ver | head -n 1)
-fcm_top_sub_ver=$(cat /sys/class/i2c-adapter/i2c-64/64-0033/cpld_sub_ver | head -n 1)
-fcm_bottom_ver=$(cat /sys/class/i2c-adapter/i2c-72/72-0033/cpld_ver | head -n 1)
-fcm_bottom_sub_ver=$(cat /sys/class/i2c-adapter/i2c-72/72-0033/cpld_sub_ver | head -n 1)
+smb_ver=`head -n 1 /sys/class/i2c-adapter/i2c-12/12-003e/cpld_ver 2> /dev/null`
+smb_sub_ver=`head -n 1 /sys/class/i2c-adapter/i2c-12/12-003e/cpld_sub_ver 2> /dev/null`
+if [ $? -eq 0 ]; then
+  echo "SMBCPLD: $(($smb_ver)).$(($smb_sub_ver))"
+else
+  echo "SMBCPLD is not detected"
+fi
+usleep 50000
 
-echo "SMBCPLD: $(($smb_ver)).$(($smb_sub_ver))"
-echo "SCMCPLD: $(($scm_ver)).$(($scm_sub_ver))"
-echo "Top FCMCPLD: $(($fcm_top_ver)).$(($fcm_top_sub_ver))"
-echo "Bottom FCMCPLD: $(($fcm_bottom_ver)).$(($fcm_bottom_sub_ver))"
+scm_ver=`head -n 1 /sys/class/i2c-adapter/i2c-2/2-0035/cpld_ver 2> /dev/null`
+scm_sub_ver=`head -n 1 /sys/class/i2c-adapter/i2c-2/2-0035/cpld_sub_ver 2> /dev/null`
+if [ $? -eq 0 ]; then
+  echo "SCMCPLD: $(($scm_ver)).$(($scm_sub_ver))"
+else
+  echo "SCMCPLD is not detected"
+fi
+usleep 50000
+
+fcm_top_ver=`head -n 1 /sys/class/i2c-adapter/i2c-64/64-0033/cpld_ver 2> /dev/null`
+fcm_top_sub_ver=`head -n 1 /sys/class/i2c-adapter/i2c-64/64-0033/cpld_sub_ver 2> /dev/null`
+if [ $? -eq 0 ]; then
+  echo "Top FCMCPLD: $(($fcm_top_ver)).$(($fcm_top_sub_ver))"
+else
+  echo "Top FCMCPLD is not detected"
+fi
+usleep 50000
+
+fcm_bottom_ver=`head -n 1 /sys/class/i2c-adapter/i2c-72/72-0033/cpld_ver 2> /dev/null`
+fcm_bottom_sub_ver=`head -n 1 /sys/class/i2c-adapter/i2c-72/72-0033/cpld_sub_ver 2> /dev/null`
+if [ $? -eq 0 ]; then
+  echo "Bottom FCMCPLD: $(($fcm_bottom_ver)).$(($fcm_bottom_sub_ver))"
+else
+  echo "Bottom FCMCPLD is not detected"
+fi
+usleep 50000
 
 if [ $board_rev -ne 4 ]; then
-  pdbl_ver=$(cat /sys/class/i2c-adapter/i2c-55/55-0060/cpld_ver | head -n 1)
-  pdbl_sub_ver=$(cat /sys/class/i2c-adapter/i2c-55/55-0060/cpld_sub_ver | head -n 1)
-  pdbr_ver=$(cat /sys/class/i2c-adapter/i2c-63/63-0060/cpld_ver | head -n 1)
-  pdbr_sub_ver=$(cat /sys/class/i2c-adapter/i2c-63/63-0060/cpld_sub_ver | head -n 1)
+  pdbl_ver=`head -n 1 /sys/class/i2c-adapter/i2c-55/55-0060/cpld_ver 2> /dev/null`
+  pdbl_sub_ver=`head -n 1 /sys/class/i2c-adapter/i2c-55/55-0060/cpld_sub_ver 2> /dev/null`
+  if [ $? -eq 0 ]; then
+    echo "Left PDBCPLD: $(($pdbl_ver)).$(($pdbl_sub_ver))"
+  else
+    echo "Left PDBCPLD is not detected"
+  fi
+  usleep 50000
 
-  echo "Left PDBCPLD: $(($pdbl_ver)).$(($pdbl_sub_ver))"
-  echo "Right PDBCPLD: $(($pdbr_ver)).$(($pdbr_sub_ver))"
+  pdbr_ver=`head -n 1 /sys/class/i2c-adapter/i2c-63/63-0060/cpld_ver 2> /dev/null`
+  pdbr_sub_ver=`head -n 1 /sys/class/i2c-adapter/i2c-63/63-0060/cpld_sub_ver 2> /dev/null`
+  if [ $? -eq 0 ]; then
+    echo "Right PDBCPLD: $(($pdbr_ver)).$(($pdbr_sub_ver))"
+  else
+    echo "Right PDBCPLD is not detected"
+  fi
 fi

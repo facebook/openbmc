fa0ae123b0e23dbb5cf9bc7f16444dff5689e743
Vineela Kukkadapu <vineelak@fb.com>
2019-03-08 16:56:41 -0800

    Move host mac test to citv2
    Summary:
    Migrate host mac test to citv2 and add support for
    testing if hostmac is all 0s.
    
    Test Plan:
    ost_mac.HostMacTest.test_host_macsts2# python cit_runner.py --run-test tests.wedge100.test_ho
    test_host_mac (tests.wedge100.test_host_mac.HostMacTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.531s
    
    OK
    ost_mac.HostMacTest.test_host_mac_zero_teston cit_runner.py --run-test tests.wedge100.test_ho
    test_host_mac_zero_test (tests.wedge100.test_host_mac.HostMacTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.480s
    
    OK
    
    Reviewed By: tomrepo
    
    fbshipit-source-id: 312b3759f4

---
+++ tests2/common/base_host_mac_test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+import unittest
+from abc import abstractmethod
+from utils.shell_util import run_shell_cmd
+from utils.cit_logger import Logger
+from utils.test_utils import mac_verify
+
+
+class BaseHostMacTest(unittest.TestCase):
+
+    def setUp(self):
+        Logger.start(name=__name__)
+        self.host_mac_cmd = None
+        pass
+
+    def tearDown(self):
+        pass
+
+    @abstractmethod
+    def set_host_mac(self):
+        pass
+
+    def test_host_mac(self):
+        self.set_host_mac()
+        self.assertNotEqual(self.host_mac_cmd, None, "Host MAC command not set")
+        Logger.info("Executing cmd={}".format(self.host_mac_cmd))
+        info = run_shell_cmd(cmd=self.host_mac_cmd)
+        self.assertTrue(mac_verify(info), "Host MAC is incorrectly formatted to \
+                        {}".format(info))
+
+    def test_host_mac_zero_test(self):
+        self.set_host_mac()
+        self.assertNotEqual(self.host_mac_cmd, None, "Host MAC command not set")
+        Logger.info("Executing cmd={}".format(self.host_mac_cmd))
+        info = run_shell_cmd(cmd=self.host_mac_cmd)
+        zero_mac = '00:00:00:00:00:00'
+        self.assertNotEqual(info, zero_mac, "Host MAC is zeros {}".format(info))

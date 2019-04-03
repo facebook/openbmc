98b58f5fb699d01209e558ca943150703a9677b6
Tao Ren <taoren@fb.com>
2019-04-03 10:47:45 -0700

    yamp: fixup eth0 after bringing up the interface
    Summary:
    Execute "eth0_mac_fixup.sh" after "/etc/init.d/networking start" so ipv6
    link-local address can be re-generated based on the permanent MAC address
    obtained from NSCI NIC.
    
    This is a workaround, and we are still investigating proper fixes (which
    most likely goes to kernel).
    
    Test Plan:
    1) "bitbake yamp-image" succeeded.
    2) rebooted yamp-4.18 for 3 times and verified ipv6 link-local address
       is always derived from the permanent MAC.
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 024a983004

--- meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/eth0_mac_fixup.sh
+++ meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/eth0_mac_fixup.sh
@@ -29,27 +29,43 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
-# get the MAC from EEPROM
-mac=$(weutil 2>/dev/null | grep '^Local MAC' 2>/dev/null | cut -d' ' -f3 2>/dev/null)
+#
+# FIXME restart eth0 interface to refresh ipv6 link-local address. The
+# workaround is needed for yamp mainly because ipv6 link-local address
+# might be derived from the "random" mac address which leads to DHCP
+# request failures.
+#
+# Below explains why the problem happens. We are still investigating the
+# official fix, which most likely goes to kernel space.
+#   1) when "ftgmac100" driver is binded to BMC's MAC Controller, the
+#      controller is assigned with a "random" MAC address.
+#   2) later when "/etc/init.d/networking start" is executed for the first
+#      time, "ftgmac100" driver starts to communicate with (Broadcom) NCSI
+#      NIC, including issuing "GET_MAC_ADDR" command to NCSI NIC.
+#   3) given ftgmac100->ndo_open() and NCSI command-handling are running
+#      asynchronously, BMC's ipv6 link-local address is "undefined": it may
+#      be derived from the "random" mac address, or the permanent one (from
+#      NCSI NIC), depending on which task (ipv6 link-local address generation
+#      or handling "GET_DMA_ADDR" response from NCSI NIC) happens first.
+#   4) If unfortunately random MAC is used (which happens most times), DHCP
+#      server will ignore BMC's DHCP request due to UUID mismatch.
+#
+# Note:
+#   1) the workaround is not needed by kernel 4.1 because ftgmac100->ndo_open
+#      won't return until "GET_MAC_ADDR" NSCI command is handled.
+#
+KERNEL_VERSION=`uname -r`
+if [[ ${KERNEL_VERSION} != 4.1.* ]]; then
+  echo "fixup eth0 interface.."
+  sleep 1
+  ifdown eth0
+  ifup eth0
+fi
 
-# get the MAC from u-boot environment
+# set the MAC from ifconfig back to u-boot environment so that u-boot
+# can use it
+mac=$(ifconfig eth0 2>/dev/null |grep HWaddr 2>/dev/null |awk '{ print $5 }')
 ethaddr=$(fw_printenv ethaddr 2>/dev/null | cut -d'=' -f2 2>/dev/null)
-
-if [ -z "$mac" ] && [ -n "$ethaddr" ]; then
-    # no MAC from EEPROM, use the one from u-boot environment
-    mac="$ethaddr"
-fi
-
-if [ -n "$mac" ]; then
-    ifconfig eth0 hw ether $mac
-else
-    # no MAC from either EEPROM or u-boot environment
-    mac=$(ifconfig eth0 2>/dev/null |grep HWaddr 2>/dev/null |awk '{ print $5 }')
-
-fi
-
 if [ "$ethaddr" != "$mac" ]; then
-    # set the MAC from EEPROM or ifconfig back to u-boot environment so that u-boot
-    # can use it
     fw_setenv "ethaddr" "$mac"
 fi
--- meta-facebook/meta-yamp/recipes-utils/openbmc-utils/openbmc-utils_%.bbappend
+++ meta-facebook/meta-yamp/recipes-utils/openbmc-utils/openbmc-utils_%.bbappend
@@ -75,10 +75,12 @@ do_install_board() {
     install -m 755 setup_i2c.sh ${D}${sysconfdir}/init.d/setup_i2c.sh
     update-rc.d -r ${D} setup_i2c.sh start 60 S .
 
-    # networking is done after rcS, any start level within rcS
-    # for mac fixup should work
+    # "eth0_mac_fixup.sh" needs to be executed after "networking start"
+    # (runlevel 5, order #1), but before "setup-dhc6.sh" (runlevel 5,
+    # order #3): this is to make sure ipv6 link-local address can be
+    # derivied from the correct MAC address.
     install -m 755 eth0_mac_fixup.sh ${D}${sysconfdir}/init.d/eth0_mac_fixup.sh
-    update-rc.d -r ${D} eth0_mac_fixup.sh start 70 S .
+    update-rc.d -r ${D} eth0_mac_fixup.sh start 2 2 3 4 5 .
 
     install -m 755 setup_board.sh ${D}${sysconfdir}/init.d/setup_board.sh
     update-rc.d -r ${D} setup_board.sh start 80 S .

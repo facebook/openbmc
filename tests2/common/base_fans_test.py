3d90fed88663122d3dfba87804ab3fd3cd3933ff
Amithash Prasad <amithash@fb.com>
2019-04-18 15:30:55 -0700

    fbtp: Base tests for CITv2
    Summary:
    Add two basic tests for Tiogapass
    1) Running process test
    2) Fan-util test which should be common to all compute platforms.
    
    Test Plan:
    root@bmc-oob:/usr/local/bin/tests2# python ./cit_runner.py --platform fbtp
    test_all_fans_read (tests.fbtp.test_fans.FansTest) ... ok
    test_fan_pwm_set (tests.fbtp.test_fans.FansTest) ... ok
    test_fan_read (tests.fbtp.test_fans.FansTest) ... ok
    test_set_all_fans_and_read (tests.fbtp.test_fans.FansTest) ... ok
    test_installed_processes (tests.fbtp.test_process_running.ProcessRunningTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 5 tests in 77.333s
    
    OK
    
    Reviewed By: sammyblueeyes
    
    fbshipit-source-id: 9ee4dac406

--- tests2/common/base_fans_test.py
+++ tests2/common/base_fans_test.py
@@ -32,6 +32,8 @@ class BaseFansTest(unittest.TestCase):
         self.write_fans_cmd = None
         self.kill_fan_ctrl_cmd = None
         self.start_fan_ctrl_cmd = None
+        self.fans = None
+        self.pwms = None
 
     def tearDown(self):
         pass
@@ -174,3 +176,162 @@ class CommonShellBasedFansTest(BaseFansTest):
         speed = self.get_speed(data)
         self.assertAlmostEqual(speed, pwm, delta=5,
             msg="Speed was not set to {} pwm [FAILED]".format(pwm))
+
+
+class CommonFanUtilBasedFansTest(BaseFansTest):
+
+    def get_fan_speed(self, fan_id=None):
+        '''
+        Helper function to get fan speed
+        '''
+        cmd = '/usr/local/bin/fan-util --get'
+        if fan_id is not None:
+            cmd = cmd + " " + str(fan_id)
+        Logger.debug("Executing: {}".format(cmd))
+        ret = run_shell_cmd(cmd)
+        return ret
+
+    def set_fan_speed(self, fan_id, pwm):
+        '''
+        Helper function to set fan speed
+        '''
+        cmd = '/usr/local/bin/fan-util --set ' + str(pwm)  + ' ' + str(fan_id)
+        Logger.debug("Executing: {}".format(cmd))
+        run_shell_cmd(cmd)
+        time.sleep(10)  # allow time for fans to be properly set
+
+    def set_all_fans_speed(self, pwm):
+        '''
+        Helper function to set fan speed
+        '''
+        cmd = '/usr/local/bin/fan-util --set ' + str(pwm)
+        Logger.debug("Executing: {}".format(cmd))
+        run_shell_cmd(cmd)
+        time.sleep(10)  # allow time for fans to be properly set
+
+    def verify_get_fan_speed_output(self, info, fan=None):
+        '''
+        Parses fan-util output
+        and test it is in the expected format
+        Arguments: info = dump of get_fan_speed.sh
+        '''
+        self.assertNotEqual(self.fans, None, "Number of fans undefined")
+        self.assertNotEqual(self.pwms, None, "Number of PWM undefined")
+        Logger.info("Parsing Fans dump: {}".format(info))
+        info = info.split('\n')
+        num = 0
+        for line in info:
+            m = re.match(r'Fan (\d+) Speed: (\d+) RPM \((\d+)%\)', line)
+            if not m:
+                if re.match(r'^\s*$', line):
+                    continue
+                attrs = line.split(": ")
+                self.assertIn(attrs[0], ["Fan Mode", "Sensor Fail", "Fan Fail"], "Accepted printed attributes")
+                if attrs[0] == "Sensor Fail" and attrs[1] != "None":
+                    self.assertEqual(attrs[1], "None", "Sensor failure")
+                continue
+            read_fan = int(m.group(1))
+            if fan is not None:
+                self.assertEqual(read_fan, fan, "Incorrect fan read")
+            num += 1
+        self.assertLessEqual(num, len(self.fans), "Number of fans")
+        self.assertGreater(num, 0, "At least one fan")
+        if fan is None:
+            self.assertEqual(num, len(self.fans), "Exact number of fans")
+        return True
+
+    def get_speed(self, info):
+        '''
+        Given a dump of 1 fan rpm read extract speed
+        '''
+        out = info.splitlines()
+        ret = []
+        for line in out:
+            m = re.match(r'Fan (\d+) Speed: (\d+) RPM \((\d+)%\)', line)
+            if m is not None:
+                ret.append(int(m.group(3)))
+        return ret
+
+    def test_all_fans_read(self):
+        '''
+        Test if all fan dump is returning sane data
+        '''
+        Logger.log_testname(name=__name__)
+        data = self.get_fan_speed()
+        Logger.info("Fans dump:\n" + data)
+        self.assertTrue(self.verify_get_fan_speed_output(data),
+            "Get fan speeds failed")
+
+    def fan_read(self, fan=None):
+        '''
+        Test if fan read for a specific fan is returning sane data
+        '''
+        data = self.get_fan_speed(fan_id=fan)
+        Logger.info("Fan dump:\n" + data)
+        self.assertTrue(self.verify_get_fan_speed_output(data, fan),
+            "Get fan speed failed")
+
+    def fan_set_and_read(self, fan_id=None, pwm=None):
+        '''
+        Test if fan set is setting pwm as expected to 0
+        '''
+        self.assertNotEqual(pwm, None, "PWM needs to be set for testing")
+        self.assertNotEqual(fan_id, None, "fan_id needs to be set for testing")
+
+        # stop fan controller
+        self.assertNotEqual(self.kill_fan_ctrl_cmd, None,
+            "Kill Fan Controller cmd not set")
+        self.kill_fan_controller()
+
+        # set fan speed
+        self.set_fan_speed(fan_id=fan_id, pwm=pwm)
+
+        # read fan speed
+        data = self.get_fan_speed(fan_id=fan_id)
+        Logger.info("Fans dump:\n" + data)
+
+        # parse fan data and get pwm only
+        speed = self.get_speed(data)
+        self.assertEqual(len(speed), 1, msg="More than one fan speed was returned")
+        self.assertAlmostEqual(speed[0], pwm, delta=5,
+            msg="Speed was not set to {} pwm [FAILED]".format(pwm))
+
+    def test_set_all_fans_and_read(self):
+        '''
+        Test setting all fans speed and read to ensure it is the same
+        '''
+        # stop fan controller
+        self.assertNotEqual(self.kill_fan_ctrl_cmd, None,
+            "Kill Fan Controller cmd not set")
+        self.kill_fan_controller()
+        # set fan speed
+        pwm=75
+        self.set_all_fans_speed(pwm=pwm)
+
+        # read fan speed
+        data = self.get_fan_speed()
+        Logger.info("Fans dump:\n" + data)
+        # parse fan data and get pwm only
+        speed = self.get_speed(data)
+        self.assertEqual(len(speed), len(self.pwms), msg="Expected number of PWMs")
+        for s in speed:
+            self.assertAlmostEqual(s, pwm, delta=5,
+                msg="Speed was not set to {} pwm [FAILED]".format(pwm))
+
+    def test_fan_read(self):
+        '''
+        For each fan read and test speed
+        '''
+        Logger.log_testname(name=__name__)
+        self.assertNotEqual(self.fans, None, "Fans must be defined")
+        for fan in self.fans:
+            self.fan_read(fan=fan)
+
+    def test_fan_pwm_set(self):
+        '''
+        For each fan read and test speed
+        '''
+        Logger.log_testname(name=__name__)
+        self.assertNotEqual(self.fans, None, "Fans must be defined")
+        for fan in self.pwms:
+            self.fan_set_and_read(fan_id=fan, pwm=40)

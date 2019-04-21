c50322e4d51d20f9e6e97e5e94180ad0a1ad098b
Mike Choi <mikechoi@fb.com>
2019-04-20 23:20:31 -0700

    Fix wedge_us_mac.sh command of YAMP
    Summary:
    [YAMP] Fix wedge_us_mac.sh command of YAMP
    We fix this command by making it do the right thing : read SUP's MAC address from EEPROM's Extended MAC Base field
    
    Test Plan: P62689181
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: aa6d0d9454

--- meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/wedge_us_mac.sh
+++ meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/wedge_us_mac.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-bmc_mac=`cat /sys/class/net/eth0/address`
-# YAMP Uses NC-SI interface. So US MAC is always BMC - 1
-offset=-1
-for i in 6 5 4 3 2 1 
-do
-  # First, dissolve mac address into hexadecimals
-  mac[$i]=`echo $bmc_mac| cut -d ':' -f $i`
-  int_val=$(( 16#${mac[$i]} ))
-  # Do the new address calculation based on offset value
-  int_val=$(( $int_val + $offset ))
-  # Take care of overflow or underflow
-  offset=0
-  if [ $int_val -eq -1 ]; then
-     int_val=255
-     offset=-1
-  fi
-  if [ $int_val -eq 256 ]; then
-     int_val=0
-     offset=1
-  fi
-  mac[$i]=`printf '%02x' $int_val`
-done
-echo ${mac[1]}:${mac[2]}:${mac[3]}:${mac[4]}:${mac[5]}:${mac[6]}
-
+# YAMP's SUP MAC address is the first address of the block
+# allocated from "Extended MAC Base" field in SUP's EEPROM.
+# That is, SUP's MAC address is the same as "Extended MAC Base" 
+# field in SUP's EEPROM
+# Any error in reading SUP's EEPROM will result in empty output,
+# which will be caught in the provisioning script.
+weutil SUP 2>&1 | grep "MAC Base" | cut -d ' ' -f 4

824b1e2ae6927cea432c779d344b0dff4f12cd9b
Vineela Kukkadapu <vineelak@fb.com>
2019-05-17 15:43:45 -0700

    Kernel module installed
    Summary: in title
    
    Test Plan:
    root@0000085847315f33413035000000000000:/usr/local/bin/tests2# python cit_runner.py --run-test tests.minipack.test_kernel_module_presence.KernelModulePresenceTest
    test_installed_kmods (tests.minipack.test_kernel_module_presence.KernelModulePresenceTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.396s
    
    OK
    
    Reviewed By: joancaneus
    
    fbshipit-source-id: d580402235

---
+++ tests2/tests/minipack/test_kernel_module_presence.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+from common.base_kernel_module_presence_test import BaseKernelModulePresenceTest
+
+
+class KernelModulePresenceTest(BaseKernelModulePresenceTest):
+    def set_kmods(self):
+        self.expected_kmod = [
+            "usb_f_acm",
+            "u_serial",
+            "usb_f_ecm",
+            "g_cdc",
+            "u_ether",
+            "pmbus",
+            "at24",
+            "adm1275",
+            "pmbus_core",
+            "tmp421",
+            "lm75",
+            "powr1220",
+            "tpm",
+            "tpm_i2c_infineon",
+            "smbcpld",
+            "scmcpld",
+            "psu_driver",
+            "pdbcpld",
+            "max34461",
+            "isl68127",
+            "ir3595",
+            "iobfpga",
+            "fcmcpld",
+            "domfpga",
+            "i2c_dev_sysfs",
+            "at25",
+            "spidev",
+            "i2c_mux_pca954x",
+            "i2c_mux",
+        ]

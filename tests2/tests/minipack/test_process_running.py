3f22f4d20e2d7b7534416df64682cbc8f40dd0d0
Vineela Kukkadapu <vineelak@fb.com>
2019-05-17 15:39:25 -0700

    Running processes test
    Summary: in title
    
    Test Plan:
    root@0000085847315f33413035000000000000:/usr/local/bin/tests2# python cit_runner.py --run-test tests.minipack.test_process_running.ProcessRunningTest
    test_installed_processes (tests.minipack.test_process_running.ProcessRunningTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.381s
    
    OK
    
    Reviewed By: joancaneus
    
    fbshipit-source-id: 3cdfdd6d58

---
+++ tests2/tests/minipack/test_process_running.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+from common.base_process_running_test import BaseProcessRunningTest
+
+
+class ProcessRunningTest(BaseProcessRunningTest):
+    def set_processes(self):
+        self.expected_process = [
+            "dhclient -6 -d -D LL -pf /var/run/dhclient6.eth0.pid eth0",
+            "dhclient -pf /var/run/dhclient.eth0.pid eth0",
+            "front-paneld",
+            "fscd",
+            "gpiod",
+            "ipmbd",
+            "ipmid",
+            "mTerm_server",
+            "rest.py",
+            "restapi",
+            "sensord",
+            "rsyslogd",
+            "spatula_wrapper.py",
+            "usbmon.sh",
+        ]

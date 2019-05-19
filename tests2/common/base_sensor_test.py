28e7b9df396552902544d1a4c68ffbb0946bbb1a
Vineela Kukkadapu <vineelak@fb.com>
2019-05-19 01:00:36 -0700

    Sensors test for minipack
    Summary: in title
    
    Test Plan:
    test_pim1_sensor_keys (tests.minipack.test_sensor.Pim1SensorTest) ... ok
    test_pim1_temp_sensor_range (tests.minipack.test_sensor.Pim1SensorTest) ... ok
    test_pim2_sensor_keys (tests.minipack.test_sensor.Pim2SensorTest) ... ok
    test_pim2_temp_sensor_range (tests.minipack.test_sensor.Pim2SensorTest) ... ok
    test_pim3_sensor_keys (tests.minipack.test_sensor.Pim3SensorTest) ... ok
    test_pim3_temp_sensor_range (tests.minipack.test_sensor.Pim3SensorTest) ... ok
    test_pim4_sensor_keys (tests.minipack.test_sensor.Pim4SensorTest) ... ok
    test_pim4_temp_sensor_range (tests.minipack.test_sensor.Pim4SensorTest) ... ok
    test_pim5_sensor_keys (tests.minipack.test_sensor.Pim5SensorTest) ... ok
    test_pim5_temp_sensor_range (tests.minipack.test_sensor.Pim5SensorTest) ... ok
    test_pim6_sensor_keys (tests.minipack.test_sensor.Pim6SensorTest) ... ok
    test_pim6_temp_sensor_range (tests.minipack.test_sensor.Pim6SensorTest) ... ok
    test_pim7_sensor_keys (tests.minipack.test_sensor.Pim7SensorTest) ... ok
    test_pim7_temp_sensor_range (tests.minipack.test_sensor.Pim7SensorTest) ... ok
    test_pim8_sensor_keys (tests.minipack.test_sensor.Pim8SensorTest) ... ok
    test_pim8_temp_sensor_range (tests.minipack.test_sensor.Pim8SensorTest) ... ok
    test_psu1_sensor_keys (tests.minipack.test_sensor.Psu1SensorTest) ... ok
    test_psu2_sensor_keys (tests.minipack.test_sensor.Psu2SensorTest) ... ok
    test_psu3_sensor_keys (tests.minipack.test_sensor.Psu3SensorTest) ... ok
    test_psu4_sensor_keys (tests.minipack.test_sensor.Psu4SensorTest) ... ok
    test_scm_sensor_keys (tests.minipack.test_sensor.ScmSensorTest) ... ok
    test_scm_temp_sensor_range (tests.minipack.test_sensor.ScmSensorTest) ... ok
    test_smb_sensor_keys (tests.minipack.test_sensor.SmbSensorTest) ... ok
    
    Reviewed By: joancaneus
    
    fbshipit-source-id: 660bb9e0d7

--- tests2/common/base_sensor_test.py
+++ tests2/common/base_sensor_test.py
@@ -17,18 +17,19 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
-import unittest
 import re
+import unittest
 from abc import abstractmethod
+
 from utils.cit_logger import Logger
 from utils.shell_util import run_shell_cmd
 
-class BaseSensorsTest(unittest.TestCase):
 
+class BaseSensorsTest(unittest.TestCase):
     def setUp(self):
-        '''
+        """
         For now the scope if for sensors exposed from lm sensors
-        '''
+        """
         self.start_logging(__name__)
         self.sensors_cmd = None
         self.parsed_result = None
@@ -49,7 +50,6 @@ class BaseSensorsTest(unittest.TestCase):
 
 
 class LmSensorsTest(BaseSensorsTest):
-
     def set_sensors_cmd(self):
         self.sensors_cmd = ["sensors"]
 
@@ -63,16 +63,48 @@ class LmSensorsTest(BaseSensorsTest):
         data = run_shell_cmd(self.sensors_cmd)
         result = {}
 
-        data = re.sub('\(.+?\)', '', data)
-        for edata in data.split('\n\n'):
-            adata = edata.split('\n', 1)
-            if (len(adata) < 2):
-                break;
-            for sdata in adata[1].split('\n'):
-                tdata = sdata.split(':')
-                if (len(tdata) < 2):
+        data = re.sub(r"\(.+?\)", "", data)
+        for edata in data.split("\n\n"):
+            adata = edata.split("\n", 1)
+            if len(adata) < 2:
+                break
+            for sdata in adata[1].split("\n"):
+                tdata = sdata.split(":")
+                if len(tdata) < 2:
                     continue
-                if 'Adapter' in tdata:
+                if "Adapter" in tdata:
                     continue
                 result[tdata[0].strip()] = tdata[1].strip()
+        return result
+
+
+class SensorUtilTest(BaseSensorsTest):
+    def set_sensors_cmd(self):
+        self.sensors_cmd = ["sensor-util all"]
+
+    def get_parsed_result(self):
+        if not self.parsed_result:
+            self.parsed_result = self.parse_sensors()
+        return self.parsed_result
+
+    def parse_sensors(self):
+        self.set_sensors_cmd()
+        data = run_shell_cmd(self.sensors_cmd)
+        result = {}
+
+        for edata in data.split("\n"):
+            adata = edata.split()
+            # For each key value pair
+            if len(adata) < 4:
+                continue
+            key = adata[0].strip()
+            value = adata[3].strip()
+            try:
+                if value == "NA":
+                    value = 0
+                value = float(value)
+                if value >= 0.0:
+                    result[key] = str(value)
+            except Exception:
+                pass
         return result

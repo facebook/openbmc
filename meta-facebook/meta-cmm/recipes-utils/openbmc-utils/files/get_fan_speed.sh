5ab64b663d0cb207d99b3d4eb82a813183d149b8
Joan Olguy Canéus <joancaneus@fb.com>
2019-05-21 21:57:22 -0700

    Re-implement cmm get_fan_speed.sh
    Summary:
    The current version of cmm get_fan_speed.sh was only this line
    of code "sensors fancpld-*|grep ^Fan". It would never be possible to
    make this work with the current citv2 base class. Therefore, I had to
    re-implement the whole get_fan_speed.sh so that I can make it work.
    
    updating cmm get_fan_speed.sh output format so it's consistent with other network bmc platforms. Test code can also be simplified when output is consistent cross different platforms
    
    Test Plan:
    Before reimplementation, the output  was like this paste P64837619 and you couldn't
    grab fan by index. It was impossible to make it work with citv2
    framework.
    
    After implementation, the output (test cases) becomes like this
    P64835783 which is what I need to get it to work with citv2 framework
    
    Reviewed By: tomrepo
    
    fbshipit-source-id: 4ac1fc609f

--- meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/get_fan_speed.sh
+++ meta-facebook/meta-cmm/recipes-utils/openbmc-utils/files/get_fan_speed.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright 2015-present Facebook. All Rights Reserved.
 #
@@ -17,4 +17,41 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
-sensors fancpld-*|grep ^Fan
+usage() {
+  echo "Usage: $0 [Fan Unit (1...12)]" >&2
+}
+
+# map the output of the command into an array
+mapfile -t FAN_ARRAY < <(sensors fancpld-*|grep ^Fan)
+
+show_rpm(){
+  front_fan_idx=$(( ($1 - 1) * 2 ))
+  rear_fan_idx=$((( ( $1 - 1) * 2) + 1))
+  IFS=' ' read -r -a fan_front <<< "${FAN_ARRAY[$front_fan_idx]}"
+  IFS=' ' read -r -a fan_rear <<< "${FAN_ARRAY[$rear_fan_idx]}"
+
+  if [ "${#fan_front[@]}" -ne 5 ] || [ "${#fan_rear[@]}" -ne 5 ]; then
+      echo "wrong sensors fan reading input. Exiting ..."
+      exit 1
+  fi
+
+  echo "${fan_front[3]}, ${fan_rear[3]}"
+}
+
+# Return appropriate value based on the argument passed to it.
+if [ "$#" -eq 0 ]; then
+  FANS="1 2 3 4 5 6 7 8 9 10 11 12"
+elif [ "${#}" -eq 1 ]; then
+    if [ "${1}" -lt 1 ] || [ "${1}" -gt 12 ]; then
+        usage
+        exit 1
+    fi
+    FANS="$1"
+else
+    usage
+    exit 1
+fi
+
+for fan in $FANS; do
+  echo "Fan $fan RPMs: $(show_rpm "${fan}")"
+done

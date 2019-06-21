22efa7f4fdb5dd8ec5d3aa5d3758de5dbb6d383a
Alison Gim <alisongim@fb.com>
2019-06-21 10:28:22 -0700

    adding test for process_restarts
    Summary:
    Check for process restarts by polling the PIDs of important processes, checking for changes.
    Poll at 10 second intervals 10 times.
    Only for wedge100
    
    Test Plan:
    How to run:
    In OpenBMC in  /usr/local/bin/tests2
    python cit_runner.py --platform wedge100 --stress --run-test tests.wedge100.stress_process_restart
    
    Expected Output Example:
    [This will be updated with a pastebin link in the future]
    Starting cit logging for test_process_restarts
    Finished logging for test_process_restarts
    Starting cit logging for test_process_restarts
    PID changed in rest.py: original pid=32663; new pid = 7802
    Finished logging for test_process_restarts
    
    Test Procedure:
    0 .  Open two terminals (Terminal 1 and Terminal 2). Go into the OpenBMC /usr/local/bin/tests2 directory
    1 .  In Terminal 1, run the stress test
    2 .  In Terminal 2, run the command, ps. Copy the PID of an expected process (e.g. rest.py)
    
    {F162368377}
    3 .  In Terminal 2, run the command, kill -9 <expected process's PID>
    
    {F162368471}
    
    4 .  This is what the terminal should look like after the stress test has finished running and there were no restarts:
    
    {F162631352}
    
    This is what the terminal should look like after the stress test has finished running and there were restarts in multiple processes:
    
    {F162955880}
    
    After the stress test has finished running, open the file /tmp/cit.log in a text editor. Go to the bottom of the file to see the most recent log updates. If an expected process has restarted, the log should look similar to the Expected Output Example.
    
    Note:
    The expected processes list can be located in /tests/wedge100/test_data/process/process.py
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: ea1cc44e44

---
+++ tests2/tests/wedge100/test_data/process/process.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+
+EXPECTED_PROCS = [
+    "spatula_wrapper.py",
+    "fscd",
+    "psumuxmon",
+    "rackmond",
+    "/var/run/dhclient.eth0.pid",
+    "/var/run/dhclient6.eth0.pid",
+    "rest.py",
+    "mTerm_server",
+]

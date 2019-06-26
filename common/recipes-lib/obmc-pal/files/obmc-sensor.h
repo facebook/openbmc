1d96c2e6c1851b26886937ed28d6aab5129c08df
Ben Wei <benwei@fb.com>
2019-06-25 18:26:17 -0700

    obmc-sensor: add generic PLDM sensor #defines
    Summary:
    as title, add generic #defines for all PLDM numeric and state
    sensors for all platforms.
    
    Test Plan:
    compiles.  This diff is used by ncsid and sensor-util to
    sync on PLDM sensors. Will udpate in next diff
    
    Reviewed By: tomrepo
    
    fbshipit-source-id: 2f81ce41fb

--- common/recipes-lib/obmc-pal/files/obmc-sensor.h
+++ common/recipes-lib/obmc-pal/files/obmc-sensor.h
@@ -33,6 +33,33 @@ extern "C" {
 #define ERR_SENSOR_NA   -2
 #define ERR_FAILURE     -3
 
+/* shm object for pldm sensors */
+#define PLDM_SNR_INFO "pldm_snr_info"
+#define NUM_PLDM_SENSORS 5
+#define PLDM_SENSOR_TYPE_NUMERIC 0
+#define PLDM_SENSOR_TYPE_STATE   1
+
+/* PLDM sensor range. Should be within  physical sensors */
+#define MAX_PLDM_SENSORS          0x20
+#define PLDM_NUMERIC_SENSOR_START 0xb0
+#define PLDM_STATE_SENSOR_START   0xc0
+#define PLDM_SENSOR_START         PLDM_NUMERIC_SENSOR_START
+#define PLDM_SENSOR_END           (PLDM_SENSOR_START + MAX_PLDM_SENSORS)
+
+
+typedef struct {
+  uint8_t pltf_sensor_id;
+  uint16_t pldm_sensor_id;
+  uint8_t sensor_type;
+  float unr;
+  float ucr;
+  float unc;
+  float lnc;
+  float lcr;
+  float lnr;
+} pldm_sensor_t;
+
+
 /* Range for sensor num for various sensors */
 #define PHYSICAL_SENSOR_START   0
 #define PHYSICAL_SENSOR_END     0xff
@@ -64,7 +91,7 @@ int sensor_read_history(uint8_t fru, uint8_t sensor_num, float *min,
 int sensor_clear_history(uint8_t fru, uint8_t sensor_num);
 
 /* Read sensor directly from the hardware. Note, this function does not
- * protect the caller from other readers. The caller should ensure 
+ * protect the caller from other readers. The caller should ensure
  * exclusivity. The simplest method being limiting all calls to this
  * function to a single daemon. */
 int sensor_raw_read(uint8_t fru, uint8_t sensor_num, float *value);

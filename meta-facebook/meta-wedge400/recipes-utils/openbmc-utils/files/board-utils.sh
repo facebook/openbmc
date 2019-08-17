8173a173448db7b811ed2675e82e2ac742d2245f
Joan Olguy Canéus <joancaneus@fb.com>
2019-08-16 22:41:10 -0700

    wedge400: board-util.sh code revised (#97)
    Summary:
    1. Copyright change
    2. wedge_power_on judgement follow CPLD spec
    3. support wedge400-c/wedge400-2
    4. remove code not used
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/97
    
    Reviewed By: joancaneus
    
    fbshipit-source-id: 2cac97e09e

--- meta-facebook/meta-wedge400/recipes-utils/openbmc-utils/files/board-utils.sh
+++ meta-facebook/meta-wedge400/recipes-utils/openbmc-utils/files/board-utils.sh
@@ -18,7 +18,8 @@
 # Boston, MA 02110-1301 USA
 #
 
-. /usr/local/bin/openbmc-utils.sh
+# Do not change this line to openbmc-utils.sh, or it will generate a source loop.
+. /usr/local/bin/i2c-utils.sh
 
 SCMCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 2-003e)
 SMBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 12-003e)
@@ -32,16 +33,6 @@ fi
 PWR_USRV_SYSFS="${SCMCPLD_SYSFS_DIR}/com_exp_pwr_enable"
 PWR_USRV_FORCE_OFF="${SCMCPLD_SYSFS_DIR}/com_exp_pwr_force_off"
 PWR_TH_RST_SYSFS="${SMBCPLD_SYSFS_DIR}/mac_reset_n"
-
-wedge_iso_buf_enable() {
-    # TODO, no isolation buffer
-    return 0
-}
-
-wedge_iso_buf_disable() {
-    # TODO, no isolation buffer
-    return 0
-}
 
 wedge_is_us_on() {
     local val0 val1
@@ -59,7 +50,12 @@ wedge_is_us_on() {
 }
 
 wedge_board_type() {
-    echo 'WEDGE400'
+    rev=$(wedge_board_rev)
+    if [ $((rev&0x04)) -eq 4 ]; then
+        echo 'WEDGE400'
+    else
+        echo 'WEDGE400-2'
+    fi
 }
 
 wedge_slot_id() {
@@ -72,12 +68,6 @@ wedge_board_rev() {
     val1=$(gpio_get BMC_CPLD_BOARD_REV_ID1)
     val2=$(gpio_get BMC_CPLD_BOARD_REV_ID2)
     echo $((val0 | (val1 << 1) | (val2 << 2)))
-}
-
-# Should we enable OOB interface or not
-wedge_should_enable_oob() {
-    # wedge400 uses BMC MAC since beginning
-    return -1
 }
 
 wedge_power_on_board() {

92261f1840a0c04818c10d3b2f802ceac0040066
Joan Olguy Canéus <joancaneus@fb.com>
2019-08-30 16:30:45 -0700

    wedge400-cit: Add test_kernel_module_presence (#120)
    Summary:
    As Title
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/120
    
    Test Plan:
    root@bmc-oob:~/tests2# python cit_runner.py --run-test "test_kernel_module_presence"
    test_installed_kmods (test_kernel_module_presence.KernelModulePresenceTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.992s
    
    OK
    
    root@bmc-oob:~# lsmod
        Tainted: G
    usb_f_acm 6661 1 - Live 0x7f1c9000
    u_serial 10170 3 usb_f_acm, Live 0x7f1c2000
    usb_f_ecm 8918 1 - Live 0x7f1bb000
    g_cdc 2798 0 - Live 0x7f1b7000
    u_ether 10644 2 usb_f_ecm,g_cdc, Live 0x7f1b0000
    adm1275 5760 0 - Live 0x7f1ab000
    tmp421 4330 0 - Live 0x7f1a6000
    powr1220 3838 0 - Live 0x7f1a2000
    gpio_pca953x 12503 0 - Live 0x7f19a000
    crc32c_generic 2105 1 - Live 0x7f196000
    xdpe132g5c 1284 0 - Live 0x7f192000 (O)
    tpm_i2c_infineon 8874 0 - Live 0x7f18c000
    smb_syscpld 18877 0 - Live 0x7f184000 (O)
    smb_pwrcpld 2621 0 - Live 0x7f180000 (O)
    scmcpld 5837 0 - Live 0x7f17b000 (O)
    psu 5742 0 - Live 0x7f176000 (O)
    isl68137 1344 0 - Live 0x7f172000 (O)
    ir35215 1630 0 - Live 0x7f16e000 (O)
    fcbcpld 8184 0 - Live 0x7f169000 (O)
    domfpga 7086 0 - Live 0x7f164000 (O)
    i2c_dev_sysfs 3604 6 smb_syscpld,smb_pwrcpld,scmcpld,psu,fcbcpld,domfpga, Live 0x7f160000 (O)
    ftdi_sio 37282 0 - Live 0x7f14a000
    i2c_mux_pca954x 7054 0 - Live 0x7f138000
    aspeed_vhub 24057 0 - Live 0x7f12e000
    libcomposite 45222 4 usb_f_acm,usb_f_ecm,g_cdc,aspeed_vhub, Live 0x7f11b000
    udc_core 28533 6 usb_f_acm,u_serial,usb_f_ecm,u_ether,aspeed_vhub,libcomposite, Live 0x7f10c000
    uhci_hcd 43874 0 - Live 0x7f0fc000
    configfs 28991 4 usb_f_acm,usb_f_ecm,libcomposite, Live 0x7f0ef000
    ext4 592532 1 - Live 0x7f032000
    mbcache 4758 1 ext4, Live 0x7f02d000
    jbd2 98575 1 ext4, Live 0x7f009000
    crypto_hash 14687 3 crc32c_generic,ext4,jbd2, Live 0x7f000000
    root@bmc-oob:~#
    
    Pass
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 68b7b33aa7

---
+++ tests2/tests/wedge400/test_kernel_module_presence.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright 2019-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_kernel_module_presence_test import BaseKernelModulePresenceTest
+
+
+class KernelModulePresenceTest(BaseKernelModulePresenceTest, unittest.TestCase):
+    def set_kmods(self):
+        self.expected_kmod = [
+            "usb_f_acm",
+            "u_serial",
+            "usb_f_ecm",
+            "g_cdc",
+            "u_ether",
+            "adm1275",
+            "gpio_pca953x",
+            "tmp421",
+            "powr1220",
+            "tpm",
+            "tpm_i2c_infineon",
+            "smb_syscpld",
+            "smb_pwrcpld",
+            "scmcpld",
+            "fcbcpld",
+            "psu",
+            "isl68137",
+            "ir35215",
+            "domfpga",
+            "i2c_dev_sysfs",
+            "i2c_mux_pca954x",
+            "i2c_mux",
+            "aspeed_vhub",
+            "libcomposite",
+            "uhci_hcd",
+            "udc_core",
+            "configfs",
+            "ext4"
+        ]

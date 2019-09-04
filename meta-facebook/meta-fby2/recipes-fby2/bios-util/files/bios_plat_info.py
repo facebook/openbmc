6d182acc153c8d2de233a14dd5a85c42da7d14dc
Ben Wei <benwei@fb.com>
2019-09-04 09:52:58 -0700

    fbgpv2: bios-util display right PCIe config for Yv2 GPv2 (#1005)
    Summary:
    bios-util display GPv2 PCIe config:
    0x10 as Yv2 GPv2 PCIe config
    0x11 as Yv2.5 GPv2 PCIe config
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1005
    
    Test Plan:
    bios-util display right PCIe config for Yv2 GPv2
    
    root@bmc-oob:~# bios-util slot2 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V2
    Slot Index: 2
    PCIe Configuration:2x GPv2 + 2x Twin Lakes
    root@bmc-oob:~# bios-util slot4 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V2
    Slot Index: 4
    PCIe Configuration:2x GPv2 + 2x Twin Lakes
    root@bmc-oob:~# bios-util slot1 --plat_info get
    slot1 is not a server
    root@bmc-oob:~# bios-util slot3 --plat_info get
    slot3 is not a server
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 687b71cd21

--- meta-facebook/meta-fby2/recipes-fby2/bios-util/files/bios_plat_info.py
+++ meta-facebook/meta-fby2/recipes-fby2/bios-util/files/bios_plat_info.py
@@ -137,7 +137,7 @@ def pcie_config(fru):
         config = "2x GP + 2x " + server_name
     elif result[0] == "0F":
         config = "2x CF + 2x " + server_name
-    elif result[0] == "11":
+    elif result[0] == "10" or result[0] == "11":
         config = "2x GPv2 + 2x " + server_name
     else:
         config = "Unknown"

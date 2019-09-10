07a88c385a9b7c0f43248571b9851149ac3de59d
Joan Olguy Canéus <joancaneus@fb.com>
2019-09-09 17:50:34 -0700

    wedge400: Add ipmid (#77)
    Summary:
    Add ipmid service of wedge400
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/77
    
    Test Plan:
    [root@localhost ~]# ipmitool mc info
    Device ID                 : 32
    Device Revision           : 1
    Firmware Revision         : 1.03
    IPMI Version              : 2.0
    Manufacturer ID           : 40981
    Manufacturer Name         : Facebook, Inc.
    Product ID                : 12614 (0x3146)
    Product Name              : Unknown (0x3146)
    Device Available          : yes
    Provides Device SDRs      : yes
    Additional Device Support :
        Sensor Device
        SDR Repository Device
        SEL Device
        FRU Inventory Device
        IPMB Event Receiver
        IPMB Event Generator
        Chassis Device
    Aux Firmware Rev Info     :
        0x00
        0x00
        0x00
        0x00
    [root@localhost ~]#
    
    Pass
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: b1cb6de885

---
+++ meta-facebook/meta-wedge400/recipes-wedge400/ipmid/files/fruid.c
@@ -0,0 +1,305 @@
+/*
+ *
+ * Copyright 2019-present Facebook. All Rights Reserved.
+ *
+ * This file provides platform specific implementation of FRUID information
+ *
+ * FRUID specification can be found at
+ * www.intel.com/content/dam/www/public/us/en/documents/product-briefs/
+ * platform-management-fru-document-rev-1-2-feb-2013.pdf
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Wedge400 specific FRUID related routine. Porting old Wedge100 style
+ * fruid code to fit into new IPMID v0.2 code.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <openbmc/log.h>
+#include <openbmc/pal.h>
+#include <openbmc/obmc-i2c.h>
+#include <openbmc/fruid.h>
+#include <facebook/wedge_eeprom.h>
+#include "fruid.h"
+
+#define WEDGE400_FRUID_SIZE 0x100
+#define WEDGE400_FRU_NUM 11
+
+// Global structures
+static unsigned char g_fruid[WEDGE400_FRU_NUM * WEDGE400_FRUID_SIZE] = {0};
+
+static struct fruid_dev fruid_list[] = {
+    {FRU_SCM, 17, 0x52, &g_fruid[0 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_SMB, 6, 0x51, &g_fruid[1 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_PEM1, 22, 0x50, &g_fruid[2 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_PEM2, 23, 0x50, &g_fruid[3 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_FCM, 31, 0x51, &g_fruid[4 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_PSU1, 22, 0x50, &g_fruid[5 * WEDGE400_FRUID_SIZE], FRU_PSU_FORMAT},
+    {FRU_PSU2, 23, 0x50, &g_fruid[6 * WEDGE400_FRUID_SIZE], FRU_PSU_FORMAT},
+    {FRU_FAN1, 34, 0x52, &g_fruid[7 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_FAN2, 35, 0x52, &g_fruid[8 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_FAN3, 36, 0x52, &g_fruid[9 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT},
+    {FRU_FAN4, 37, 0x52, &g_fruid[10 * WEDGE400_FRUID_SIZE], FRU_FB_FORMAT}};
+
+void *get_frudev(unsigned char id)
+{
+  for (int i = 0; i < WEDGE400_FRU_NUM; i++)
+  {
+    if (fruid_list[i].id == id)
+    {
+      return &fruid_list[i];
+    }
+  }
+  return NULL;
+}
+
+#define COMMON_HDR_VER 1
+#define PROD_INFO_VER 1
+#define PROD_INFO_AREA_OFFSET 0x10
+#define PROD_INFO_CKSUM_OFFSET (PROD_INFO_AREA_OFFSET + 1)
+#define LANG_CODE_ENGLISH 25
+#define TYPE_STR 0xC0
+#define TYPE_LAST 0xC1
+#define ZERO_CKSUM_CONST 0x100
+#define LEN_BYTE_SIZE 8
+
+typedef struct _fruid_common_hdr_t
+{
+  unsigned char ver;
+  unsigned char internal_use_area_offset;
+  unsigned char chassis_info_area_offset;
+  unsigned char board_info_area_offset;
+  unsigned char prod_info_area_offset;
+  unsigned char multi_record_area_offset;
+  unsigned char padding;
+  unsigned char cksum;
+} fruid_common_hdr_t;
+
+// A macro to add an entry.
+#define _APPEND_STR_VALUE(name)                                          \
+  do                                                                     \
+  {                                                                      \
+    if (strlen(name) < 1 || i + 1 + strlen(name) >= WEDGE400_FRUID_SIZE) \
+    {                                                                    \
+      break;                                                             \
+    }                                                                    \
+    frumem[i++] = TYPE_STR + strlen(name);                               \
+    memcpy(&frumem[i], name, strlen(name));                              \
+    i += strlen(name);                                                   \
+  } while (0)
+
+// Main function to populate the fruid data structure, using the data
+// from eeprom
+static void populate_fruid(unsigned char id)
+{
+  int rc = 0;
+  int len = 0;
+  unsigned char zero_val = 0x0;
+  char filename[64];
+  struct fruid_dev *fruid = get_frudev(id);
+  if (fruid == NULL)
+  {
+    OBMC_WARN("populate_fruid: can't find fruid in list %d\n", id);
+    return;
+  }
+  unsigned char *frumem = fruid->mem;
+  i2c_sysfs_slave_abspath(filename, sizeof(filename), fruid->bus, fruid->addr);
+  strncat(filename, "/eeprom", sizeof(filename) - strlen(filename) - 1);
+  if (fruid->format == FRU_STD_FORMAT)
+  {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+      OBMC_WARN("populate_fruid: can't open file fruid %d : %s\n",
+             id, filename);
+      return;
+    }
+    fread(frumem, 1, WEDGE400_FRUID_SIZE, fp);
+    fclose(fp);
+    return;
+  }
+  else if (fruid->format == FRU_PSU_FORMAT)
+  {
+    char eeprom[16];
+    int i = PROD_INFO_AREA_OFFSET;
+    fruid_common_hdr_t *chdr = (fruid_common_hdr_t *)frumem;
+    fruid_info_t frupsu;
+    memset(&frupsu, 0, sizeof(fruid_info_t));
+
+    sprintf(eeprom,"24c02");
+    pal_add_i2c_device(fruid->bus,fruid->addr,eeprom);
+    rc = fruid_parse(filename, &frupsu);
+    pal_del_i2c_device(fruid->bus,fruid->addr);
+
+    if (rc)
+    {
+      sprintf(eeprom,"24c64");
+      pal_add_i2c_device(fruid->bus,fruid->addr,eeprom);
+      rc = fruid_parse(filename, &frupsu);
+      pal_del_i2c_device(fruid->bus,fruid->addr);
+      if (rc) {
+        OBMC_WARN(
+              "populate_fruid: wedge_eeprom_parse fruid_parse fail %d returns %d\n",
+              id, rc);
+        return;
+      }
+    }
+
+    chdr->ver = COMMON_HDR_VER;
+    chdr->cksum = chdr->ver + chdr->prod_info_area_offset;
+    chdr->cksum = ZERO_CKSUM_CONST - chdr->cksum;
+
+    chdr->prod_info_area_offset = PROD_INFO_AREA_OFFSET / LEN_BYTE_SIZE;
+    frumem[i++] = PROD_INFO_VER;
+    frumem[i++] = 0x00;
+    frumem[i++] = LANG_CODE_ENGLISH;
+    _APPEND_STR_VALUE(frupsu.product.mfg);
+    _APPEND_STR_VALUE(frupsu.product.name);
+    _APPEND_STR_VALUE(frupsu.product.part);
+    _APPEND_STR_VALUE(frupsu.product.version);
+    _APPEND_STR_VALUE(frupsu.product.serial);
+    _APPEND_STR_VALUE(frupsu.product.asset_tag);
+    _APPEND_STR_VALUE(frupsu.product.fruid);
+    frumem[i++] = TYPE_LAST;
+
+    len = i - PROD_INFO_AREA_OFFSET + 1;
+    if (len % LEN_BYTE_SIZE)
+    {
+      frumem[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE + 1;
+      i += LEN_BYTE_SIZE - (len % LEN_BYTE_SIZE);
+    }
+    else
+    {
+      frumem[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE;
+    }
+
+    for (int j = PROD_INFO_AREA_OFFSET; j < i; j++)
+    {
+      frumem[i] += frumem[j];
+    }
+
+    frumem[i] = ZERO_CKSUM_CONST - frumem[i];
+
+    free_fruid_info(&frupsu);
+    return;
+  }
+  else if (fruid->format == FRU_FB_FORMAT)
+  {
+    int i = 0x10;
+    char vbuf[5] = {0};
+    char fruid_file_name[] = "fruid_1.0";
+    struct wedge_eeprom_st eeprom;
+    // Will keep data to buffer in IPMI FRU format
+    fruid_common_hdr_t *chdr = (fruid_common_hdr_t *)frumem;
+    memset(&fruid, WEDGE400_FRUID_SIZE, zero_val);
+
+    chdr->ver = COMMON_HDR_VER;
+    chdr->prod_info_area_offset = PROD_INFO_AREA_OFFSET / LEN_BYTE_SIZE;
+    chdr->cksum = chdr->ver + chdr->prod_info_area_offset;
+    chdr->cksum = ZERO_CKSUM_CONST - chdr->cksum;
+
+    rc = wedge_eeprom_parse(filename, &eeprom);
+    if (rc)
+    {
+      OBMC_WARN(
+             "populate_fruid: wedge_eeprom_parse on fruid %d returns %d\n",
+             id, rc);
+      return;
+    }
+
+    frumem[i++] = PROD_INFO_VER;
+    frumem[i++] = 0x00;
+    frumem[i++] = LANG_CODE_ENGLISH;
+    _APPEND_STR_VALUE(eeprom.fbw_system_manufacturer);
+    _APPEND_STR_VALUE(eeprom.fbw_product_name);
+    _APPEND_STR_VALUE(eeprom.fbw_product_number);
+    snprintf(vbuf, sizeof(vbuf), "%0X", eeprom.fbw_product_version);
+    _APPEND_STR_VALUE(vbuf);
+    _APPEND_STR_VALUE(eeprom.fbw_product_serial);
+    _APPEND_STR_VALUE(eeprom.fbw_product_asset);
+    _APPEND_STR_VALUE(fruid_file_name);
+    frumem[i++] = TYPE_LAST;
+
+    len = i - PROD_INFO_AREA_OFFSET + 1;
+    if (len % LEN_BYTE_SIZE)
+    {
+      frumem[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE + 1;
+      i += LEN_BYTE_SIZE - (len % LEN_BYTE_SIZE);
+    }
+    else
+    {
+      frumem[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE;
+    }
+
+    for (int j = PROD_INFO_AREA_OFFSET; j < i; j++)
+    {
+      frumem[i] += frumem[j];
+    }
+
+    frumem[i] = ZERO_CKSUM_CONST - frumem[i];
+
+    return;
+  }
+  else
+  {
+    OBMC_WARN("populate_fruid: fruid %d not define format %d\n", id,
+           fruid->format);
+    return;
+  }
+}
+
+int plat_fruid_size(unsigned char fru_id)
+{
+  struct fruid_dev *frudev = get_frudev(fru_id);
+  if (!frudev)
+    return 0;
+
+  populate_fruid(fru_id);
+  return WEDGE400_FRUID_SIZE;
+}
+
+int plat_fruid_data(unsigned char fru_id, int offset, int count,
+                    unsigned char *data)
+{
+
+  struct fruid_dev *frudev = get_frudev(fru_id);
+  if (!frudev)
+    return -1;
+  unsigned char *frumem = frudev->mem;
+
+  if ((offset + count) > WEDGE400_FRUID_SIZE)
+  {
+    return -1;
+  }
+
+  memcpy(data, &(frumem[offset]), count);
+  return 0;
+}
+
+int plat_fruid_init(void)
+{
+  for (unsigned char id = 0; id < WEDGE400_FRU_NUM; id++)
+  {
+    populate_fruid(fruid_list[id].id);
+  }
+  return 0;
+}
---
+++ meta-facebook/meta-wedge400/recipes-wedge400/ipmid/files/sel.c
@@ -0,0 +1,670 @@
+/*
+ *
+ * Copyright 2019-present Facebook. All Rights Reserved.
+ *
+ * This file represents platform specific implementation for storing
+ * SEL logs and acts as back-end for IPMI stack
+ *
+ * TODO: Optimize the file handling to keep file open always instead of
+ * current open/seek/close
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#define _XOPEN_SOURCE
+#include "sel.h"
+#include "timestamp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <time.h>
+#include <openbmc/log.h>
+#include <openbmc/pal.h>
+
+// SEL File.
+#define SEL_LOG_FILE  "/mnt/data/sel%d.bin"
+#define SIZE_PATH_MAX 32
+
+// SEL Header magic number
+#define SEL_HDR_MAGIC 0xFBFBFBFB
+
+// SEL Header version number
+#define SEL_HDR_VERSION 0x01
+
+// SEL Data offset from file beginning
+#define SEL_DATA_OFFSET 0x100
+
+// SEL reservation IDs can not be 0x00 or 0xFFFF
+#define SEL_RSVID_MIN  0x01
+#define SEL_RSVID_MAX  0xFFFE
+
+// Number of SEL records before wrap
+#define SEL_RECORDS_MAX 128 // TODO: Based on need we can make it bigger
+#define SEL_ELEMS_MAX (SEL_RECORDS_MAX+1)
+
+// Index for circular array
+#define SEL_INDEX_MIN 0x00
+#define SEL_INDEX_MAX SEL_RECORDS_MAX
+
+// Record ID can not be 0x0 (IPMI/Section 31)
+#define SEL_RECID_MIN (SEL_INDEX_MIN+1)
+#define SEL_RECID_MAX (SEL_INDEX_MAX+1)
+
+// Special RecID value for first and last (IPMI/Section 31)
+#define SEL_RECID_FIRST 0x0000
+#define SEL_RECID_LAST 0xFFFF
+
+#define RAS_SEL_LENGTH 1024
+
+// SEL header struct to keep track of SEL Log entries
+typedef struct {
+  int magic; // Magic number to check validity
+  int version; // version number of this header
+  int begin; // index to the beginning of the log
+  int end; // index to end of the log
+  time_stamp_t ts_add; // last addition time stamp
+  time_stamp_t ts_erase; // last erase time stamp
+} sel_hdr_t;
+
+// Keep track of last Reservation ID
+static int g_rsv_id[MAX_NODES+1];
+
+// Cached version of SEL Header and data
+static sel_hdr_t g_sel_hdr[MAX_NODES+1];
+static sel_msg_t g_sel_data[MAX_NODES+1][SEL_ELEMS_MAX];
+
+// Local helper functions to interact with file system
+static int
+file_get_sel_hdr(int node) {
+  FILE *fp;
+  char fpath[SIZE_PATH_MAX] = {0};
+
+  sprintf(fpath, SEL_LOG_FILE, node);
+
+  fp = fopen(fpath, "r");
+  if (fp == NULL) {
+    return -1;
+  }
+
+  if (fread(&g_sel_hdr[node], sizeof(sel_hdr_t), 1, fp) <= 0) {
+    OBMC_WARN("file_get_sel_hdr: fread\n");
+    fclose (fp);
+    return -1;
+  }
+
+  fclose(fp);
+  return 0;
+}
+
+static int
+file_get_sel_data(int node) {
+  FILE *fp;
+  int i, j;
+  char fpath[SIZE_PATH_MAX] = {0};
+
+  sprintf(fpath, SEL_LOG_FILE, node);
+
+  fp = fopen(fpath, "r");
+  if (fp == NULL) {
+    OBMC_WARN("file_get_sel_data: fopen\n");
+    return -1;
+  }
+
+  if (fseek(fp, SEL_DATA_OFFSET, SEEK_SET)) {
+    OBMC_WARN("file_get_sel_data: fseek\n");
+    fclose(fp);
+    return -1;
+  }
+
+  unsigned char buf[SEL_ELEMS_MAX * 16];
+  if (fread(buf, 1, SEL_ELEMS_MAX * sizeof(sel_msg_t), fp) <= 0) {
+    OBMC_WARN("file_get_sel_data: fread\n");
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  for (i = 0; i < SEL_ELEMS_MAX; i++) {
+    for (j = 0; j < sizeof(sel_msg_t);j++) {
+      g_sel_data[node][i].msg[j] = buf[i*16 + j];
+    }
+  }
+
+  return 0;
+}
+
+static int
+file_store_sel_hdr(int node) {
+  FILE *fp;
+  char fpath[SIZE_PATH_MAX] = {0};
+
+  sprintf(fpath, SEL_LOG_FILE, node);
+
+  fp = fopen(fpath, "r+");
+  if (fp == NULL) {
+    OBMC_WARN("file_store_sel_hdr: fopen\n");
+    return -1;
+  }
+
+  if (fwrite(&g_sel_hdr[node], sizeof(sel_hdr_t), 1, fp) <= 0) {
+    OBMC_WARN("file_store_sel_hdr: fwrite\n");
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  return 0;
+}
+
+static int
+file_store_sel_data(int node, int recId, sel_msg_t *data) {
+  FILE *fp;
+  int index;
+  char fpath[SIZE_PATH_MAX] = {0};
+
+  sprintf(fpath, SEL_LOG_FILE, node);
+
+  fp = fopen(fpath, "r+");
+  if (fp == NULL) {
+    OBMC_WARN("file_store_sel_data: fopen\n");
+    return -1;
+  }
+
+  // Records are stored using zero-based index
+  index = (recId-1) * sizeof(sel_msg_t);
+
+  if (fseek(fp, SEL_DATA_OFFSET+index, SEEK_SET)) {
+    OBMC_WARN("file_store_sel_data: fseek\n");
+    fclose(fp);
+    return -1;
+  }
+
+  if (fwrite(data->msg, sizeof(sel_msg_t), 1, fp) <= 0) {
+    OBMC_WARN("file_store_sel_data: fwrite\n");
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  return 0;
+}
+
+static void
+dump_sel_syslog(int fru, sel_msg_t *data) {
+  int i = 0;
+  char temp_str[8]  = {0};
+  char str[128] = {0};
+
+  for (i = 0; i < 15; i++) {
+    sprintf(temp_str, "%02X:", data->msg[i]);
+    strcat(str, temp_str);
+  }
+  sprintf(temp_str, "%02X", data->msg[15]);
+  strcat(str, temp_str);
+
+  OBMC_WARN("SEL Entry, FRU: %d, Content: %s\n", fru, str);
+}
+
+static void
+dump_ras_sel_syslog(uint8_t fru, ras_sel_msg_t *data) {
+  int i = 0;
+  char temp_str[8]  = {0};
+  char str[256] = {0};
+
+  for (i = 0; i < SIZE_RAS_SEL - 1; i++) {
+    sprintf(temp_str, "%02X:", data->msg[i]);
+    strcat(str, temp_str);
+  }
+  sprintf(temp_str, "%02X", data->msg[SIZE_RAS_SEL - 1]);
+  strcat(str, temp_str);
+
+  OBMC_WARN("SEL Entry, FRU: %d, Content: %s\n", fru, str);
+}
+
+static void
+parse_ras_sel(uint8_t fru, ras_sel_msg_t *data) {
+  uint8_t *sel = data->msg;
+  char error_log[RAS_SEL_LENGTH];
+  char mfg_id[16];
+
+  /* Manufacturer ID (byte 2:0) */
+  sprintf(mfg_id, "%02x%02x%02x", sel[2], sel[1], sel[0]);
+
+  /* Command-specific (byte 3:34) */ 
+  pal_parse_ras_sel(fru, &sel[3], error_log);
+
+  OBMC_CRIT("SEL Entry: FRU: %d, MFG ID: %s, "
+         "%s",
+         fru, mfg_id, error_log);
+
+  pal_update_ts_sled();
+}
+
+/* Parse SEL Log based on IPMI v2.0 Section 32.1 & 32.2*/
+static void
+parse_sel(uint8_t fru, sel_msg_t *data) {
+  uint32_t timestamp;
+  uint8_t *sel = data->msg;
+  uint8_t sensor_num;
+  uint8_t record_type;
+  char sensor_name[32];
+  char error_log[256];
+  char error_type[64];
+  char oem_data[32];
+  int ret;
+  struct tm ts;
+  char time[64];
+  char mfg_id[16];
+  char event_data[8];
+
+  /* Record Type (Byte 2) */
+  record_type = (uint8_t) sel[2];
+  if (record_type == 0x02) {
+    sprintf(error_type, "Standard");
+  } else if (record_type >= 0xC0 && record_type <= 0xDF) {
+    sprintf(error_type, "OEM timestamped");
+  } else if (record_type == 0xFB) {
+    sprintf(error_type, "Facebook Unified SEL");
+  } else if (record_type >= 0xE0 && record_type <= 0xFF) {
+    sprintf(error_type, "OEM non-timestamped");
+  } else {
+    sprintf(error_type, "Unknown");
+  }
+
+  if (record_type < 0xE0) {
+    /* Timestamped SEL. including Standard and OEM. */
+
+    /* Convert Timestamp from Unix time (Byte 6:3) to Human readable format */
+    timestamp = 0;
+    timestamp |= sel[3];
+    timestamp |= sel[4] << 8;
+    timestamp |= sel[5] << 16;
+    timestamp |= sel[6] << 24;
+    sprintf(time, "%u", timestamp);
+    memset(&ts, 0, sizeof(struct tm));
+    strptime(time, "%s", &ts);
+    memset(&time, 0, sizeof(time));
+    strftime(time, sizeof(time), "%Y-%m-%d %H:%M:%S", &ts);
+  }
+
+  if (record_type < 0xC0) {
+    /* standard SEL records*/
+
+    /* Sensor num (Byte 11) */
+    sensor_num = (uint8_t) sel[11];
+    ret = pal_get_event_sensor_name(fru, sel, sensor_name);
+
+    /* Event Data (Byte 13:15) */
+    ret = pal_parse_sel(fru, sel, error_log);
+    sprintf(event_data, "%02X%02X%02X", sel[13], sel[14], sel[15]);
+
+    /* Check if action needs to be taken based on the SEL message */
+    if (!ret)
+      ret = pal_sel_handler(fru, sensor_num, &sel[10]);
+
+    OBMC_CRIT("SEL Entry: FRU: %d, Record: %s (0x%02X), Time: %s, "
+        "Sensor: %s (0x%02X), Event Data: (%s) %s ",
+        fru,
+        error_type, record_type,
+        time,
+        sensor_name, sensor_num,
+        event_data, error_log);
+  } else if (record_type < 0xE0) {
+    /* timestamped OEM SEL records */
+
+    ret = pal_parse_oem_sel(fru, sel, error_log);
+
+    /* Manufacturer ID (byte 9:7) */
+    sprintf(mfg_id, "%02x%02x%02x", sel[9], sel[8], sel[7]);
+
+    /* OEM Data (Byte 10:15) */
+    sprintf(oem_data, "%02X%02X%02X%02X%02X%02X", sel[10], sel[11], sel[12],
+          sel[13], sel[14], sel[15]);
+
+    OBMC_CRIT("SEL Entry: FRU: %d, Record: %s (0x%02X), Time: %s, "
+        "MFG ID: %s, OEM Data: (%s) %s ",
+        fru,
+        error_type, record_type,
+        time,
+        mfg_id,
+        oem_data, error_log);
+  } else {
+    /* non-timestamped OEM SEL records */
+    //special case.
+    if ( record_type == 0xFB )
+    {
+      time_stamp_fill(&sel[4]);
+      ret = pal_parse_oem_unified_sel(fru, sel, error_log);
+      OBMC_CRIT("SEL Entry: FRU: %d, Record: %s (0x%02X), %s", fru, error_type, record_type, error_log); 
+    }
+    else
+    {
+
+      ret = pal_parse_oem_sel(fru, sel, error_log);
+      /* OEM Data (Byte 3:15) */
+      sprintf(oem_data, "%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X", sel[3], sel[4], sel[5],
+          sel[6], sel[7], sel[8], sel[9], sel[10], sel[11], sel[12], sel[13], sel[14], sel[15]);
+
+      OBMC_CRIT("SEL Entry: FRU: %d, Record: %s (0x%02X), "
+          "OEM Data: (%s) %s ",
+          fru,
+          error_type, record_type,
+          oem_data, error_log);
+    }
+  }
+
+  pal_update_ts_sled();
+}
+
+// Platform specific SEL API entry points
+// Retrieve time stamp for recent add operation
+void
+sel_ts_recent_add(int node, time_stamp_t *ts) {
+  memcpy(ts->ts, g_sel_hdr[node].ts_add.ts, 0x04);
+}
+
+// Retrieve time stamp for recent erase operation
+void
+sel_ts_recent_erase(int node, time_stamp_t *ts) {
+  memcpy(ts->ts, g_sel_hdr[node].ts_erase.ts, 0x04);
+}
+
+// Retrieve total number of entries in SEL log
+int
+sel_num_entries(int node) {
+  if (g_sel_hdr[node].begin <= g_sel_hdr[node].end) {
+      return (g_sel_hdr[node].end - g_sel_hdr[node].begin);
+  } else {
+    return (g_sel_hdr[node].end + (SEL_INDEX_MAX - g_sel_hdr[node].begin + 1));
+  }
+}
+
+// Retrieve total free space available in SEL log
+int
+sel_free_space(int node) {
+  int total_space;
+  int used_space;
+
+  total_space = SEL_RECORDS_MAX * sizeof(sel_msg_t);
+  used_space = sel_num_entries(node) * sizeof(sel_msg_t);
+
+  return (total_space - used_space);
+}
+
+// Reserve an ID that will be used in later operations
+// IPMI/Section 31.4
+int
+sel_rsv_id(int node) {
+  // Increment the current reservation ID and return
+  if (g_rsv_id[node]++ == SEL_RSVID_MAX) {
+    g_rsv_id[node] = SEL_RSVID_MIN;
+  }
+
+  return g_rsv_id[node];
+}
+
+// Get the SEL entry for a given record ID
+// IPMI/Section 31.5
+int
+sel_get_entry(int node, int read_rec_id, sel_msg_t *msg, int *next_rec_id) {
+
+  int index;
+
+  // Find the index in to array based on given index
+  if (read_rec_id == SEL_RECID_FIRST) {
+    index = g_sel_hdr[node].begin;
+  } else if (read_rec_id == SEL_RECID_LAST) {
+    if (g_sel_hdr[node].end) {
+      index = g_sel_hdr[node].end - 1;
+    } else {
+      index = SEL_INDEX_MAX;
+    }
+  } else {
+    index = read_rec_id;
+  }
+
+  // If the log is empty return error
+  if (sel_num_entries(node) == 0) {
+    OBMC_WARN("sel_get_entry: No entries\n");
+    return -1;
+  }
+
+  // Check for boundary conditions
+  if ((index < SEL_INDEX_MIN) || (index > SEL_INDEX_MAX)) {
+    OBMC_WARN("sel_get_entry: Invalid Record ID %d\n", read_rec_id);
+    return -1;
+  }
+
+  // If begin < end, check to make sure the given id falls between
+  if (g_sel_hdr[node].begin < g_sel_hdr[node].end) {
+    if (index < g_sel_hdr[node].begin || index >= g_sel_hdr[node].end) {
+      OBMC_WARN("sel_get_entry: Wrong Record ID %d\n", read_rec_id);
+      return -1;
+    }
+  }
+
+  // If end < begin, check to make sure the given id is valid
+  if (g_sel_hdr[node].begin > g_sel_hdr[node].end) {
+    if (index >= g_sel_hdr[node].end && index < g_sel_hdr[node].begin) {
+      OBMC_WARN("sel_get_entry: Wrong Record ID2 %d\n", read_rec_id);
+      return -1;
+    }
+  }
+
+  memcpy(msg->msg, g_sel_data[node][index].msg, sizeof(sel_msg_t));
+
+  // Return the next record ID in the log
+  *next_rec_id = ++read_rec_id;
+  if (*next_rec_id > SEL_INDEX_MAX) {
+    *next_rec_id = SEL_INDEX_MIN;
+  }
+
+  // If this is the last entry in the log, return 0xFFFF
+  if (*next_rec_id == g_sel_hdr[node].end) {
+    *next_rec_id = SEL_RECID_LAST;
+  }
+
+  return 0;
+}
+
+// Add a new entry in to SEL log for RAS SEL
+int
+ras_sel_add_entry(int node, ras_sel_msg_t *msg) {
+
+  // Print the data in syslog
+  dump_ras_sel_syslog(node, msg);
+
+  // Parse the RAS SEL message
+  parse_ras_sel(node, msg);
+
+  return 0;
+}
+
+// Add a new entry in to SEL log
+// IPMI/Section 31.6
+int
+sel_add_entry(int node, sel_msg_t *msg, int *rec_id) {
+  // If the SEL if full, roll over. To keep track of empty condition, use
+  // one empty location less than the max records.
+  if (sel_num_entries(node) == SEL_RECORDS_MAX) {
+      OBMC_WARN("sel_add_entry: SEL rollover\n");
+    if (++g_sel_hdr[node].begin > SEL_INDEX_MAX) {
+      g_sel_hdr[node].begin = SEL_INDEX_MIN;
+    }
+  }
+
+  // Update message's time stamp starting at byte 4
+  if (msg->msg[2] < 0xE0)
+    time_stamp_fill(&msg->msg[3]);
+
+  // Return the newly added record ID
+  *rec_id = g_sel_hdr[node].end+1;
+  msg->msg[0] = (*rec_id ) & 0xFF;
+  msg->msg[1] = (*rec_id >> 8 ) & 0xFF;
+
+  // Add the enry at end
+  memcpy(g_sel_data[node][g_sel_hdr[node].end].msg, msg->msg, sizeof(sel_msg_t));
+
+  // Print the data in syslog
+  dump_sel_syslog(node, msg);
+
+  // Parse the SEL message
+  parse_sel((uint8_t) node, msg);
+
+  if (file_store_sel_data(node, *rec_id, msg)) {
+    OBMC_WARN("sel_add_entry: file_store_sel_data\n");
+    return -1;
+  }
+
+  // Increment the end pointer
+  if (++g_sel_hdr[node].end > SEL_INDEX_MAX) {
+    g_sel_hdr[node].end = SEL_INDEX_MIN;
+  }
+
+  // Update timestamp for add in header
+  time_stamp_fill(g_sel_hdr[node].ts_add.ts);
+
+  // Store the structure persistently
+  if (file_store_sel_hdr(node)) {
+    OBMC_WARN("sel_add_entry: file_store_sel_hdr\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+// Erase the SEL completely
+// IPMI/Section 31.9
+// Note: To reduce wear/tear, instead of erasing, manipulating the metadata
+int
+sel_erase(int node, int rsv_id) {
+  if (rsv_id != g_rsv_id[node]) {
+    return -1;
+  }
+
+  // Erase SEL Logs
+  g_sel_hdr[node].begin = SEL_INDEX_MIN;
+  g_sel_hdr[node].end = SEL_INDEX_MIN;
+
+  // Update timestamp for erase in header
+  time_stamp_fill(g_sel_hdr[node].ts_erase.ts);
+
+  // Store the structure persistently
+  if (file_store_sel_hdr(node)) {
+    OBMC_WARN("sel_erase: file_store_sel_hdr\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+// To get the erase status while erase happens
+// IPMI/Section 31.2
+// Note: Since we are not doing offline erasing, need not return in-progress state
+int
+sel_erase_status(int node, int rsv_id, sel_erase_stat_t *status) {
+  if (rsv_id != g_rsv_id[node]) {
+    return -1;
+  }
+
+  // Since we do not do any offline erasing, always return erase done
+  *status = SEL_ERASE_DONE;
+
+  return 0;
+}
+
+// Initialize SEL log file
+static int
+sel_node_init(int node) {
+  FILE *fp;
+  int i;
+  char fpath[SIZE_PATH_MAX] = {0};
+
+  sprintf(fpath, SEL_LOG_FILE, node);
+
+  // Check if the file exists or not
+  if (access(fpath, F_OK) == 0) {
+    // Since file is present, fetch all the contents to cache
+    if (file_get_sel_hdr(node)) {
+      OBMC_WARN("init_sel: file_get_sel_hdr\n");
+      return -1;
+    }
+
+    if (file_get_sel_data(node)) {
+      OBMC_WARN("init_sel: file_get_sel_data\n");
+      return -1;
+    }
+
+    return 0;
+  }
+
+  // File not present, so create the file
+  fp = fopen(fpath, "w+");
+  if (fp == NULL) {
+    OBMC_WARN("init_sel: fopen\n");
+    return -1;
+  }
+
+  fclose (fp);
+
+  // Populate SEL Header in to the file
+  g_sel_hdr[node].magic = SEL_HDR_MAGIC;
+  g_sel_hdr[node].version = SEL_HDR_VERSION;
+  g_sel_hdr[node].begin = SEL_INDEX_MIN;
+  g_sel_hdr[node].end = SEL_INDEX_MIN;
+  memset(g_sel_hdr[node].ts_add.ts, 0x0, 4);
+  memset(g_sel_hdr[node].ts_erase.ts, 0x0, 4);
+
+  if (file_store_sel_hdr(node)) {
+    OBMC_WARN("init_sel: file_store_sel_hdr\n");
+    return -1;
+  }
+
+  // Populate SEL Data in to the file
+  for (i = 1;  i <= SEL_RECORDS_MAX; i++) {
+    sel_msg_t msg = {0};
+    if (file_store_sel_data(node, i, &msg)) {
+      OBMC_WARN("init_sel: file_store_sel_data\n");
+      return -1;
+    }
+  }
+
+  g_rsv_id[node] = 0x01;
+
+  return 0;
+}
+
+int
+sel_init(void) {
+  int ret;
+  int i;
+
+  for (i = 1; i < MAX_NODES+1; i++) {
+    ret = sel_node_init(i);
+    if (ret) {
+      return ret;
+    }
+  }
+
+  return ret;
+}
+

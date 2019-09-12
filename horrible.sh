1e20b8b17e34f1f8d8c662d45709b294aac42292
Marco Kawajiri <kawajiri@fb.com>
2019-09-12 05:36:28 -0700

    [DO NOT MERGE] travisci lint test

---
+++ horrible.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+GREATER_THAN=3
+
+if [ $a -gt GRATER_THAN ]; then
+ echo 'Its grat'
+fi
---
+++ terrible.py
@@ -0,0 +1,1 @@
+if not a in 123: print("a is not a number that I particularly like >:(")

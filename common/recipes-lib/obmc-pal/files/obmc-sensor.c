573cff42d3657e80ce6b93114638c26bacfff81e
Amithash Prasad <amithash@fb.com>
2019-09-30 14:04:12 -0700

    obmc-sensor: Refactor duplicated code between obmc-pal and obmc-sensor
    Summary: Remove all sensor related duplicated functions and ensure they are in obmc-sensor.c
    
    Test Plan: Build on all platforms.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 8a5b3704f2

--- common/recipes-lib/obmc-pal/files/obmc-sensor.c
+++ common/recipes-lib/obmc-pal/files/obmc-sensor.c
@@ -861,8 +861,8 @@ pal_sensor_thresh_modify(uint8_t fru,  uint8_t sensor_num, uint8_t thresh_type, 
   int ret = -1;
   thresh_sensor_t snr;
   char fru_name[8];
-  char fpath[64];
-  char initpath[64];
+  char fpath[64] = {0};
+  char initpath[64] = {0};
   char cmd[128] = {0};
 
   ret = pal_get_fru_name(fru, fru_name);
@@ -881,7 +881,6 @@ pal_sensor_thresh_modify(uint8_t fru,  uint8_t sensor_num, uint8_t thresh_type, 
   }
 
   ret = pal_get_thresh_from_file(fru, sensor_num, &snr);
-
   if (ret < 0) {
     syslog(LOG_ERR, "%s: Fail to get %s sensor threshold file\n",__func__,fru_name);
     return ret;
@@ -889,26 +888,68 @@ pal_sensor_thresh_modify(uint8_t fru,  uint8_t sensor_num, uint8_t thresh_type, 
 
   switch (thresh_type) {
     case UCR_THRESH:
+      if (((snr.flag & GETMASK(UNR_THRESH)) && (value > snr.unr_thresh)) ||
+          ((snr.flag & GETMASK(UNC_THRESH)) && (value < snr.unc_thresh)) ||
+          ((snr.flag & GETMASK(LNC_THRESH)) && (value < snr.lnc_thresh)) ||
+          ((snr.flag & GETMASK(LCR_THRESH)) && (value < snr.lcr_thresh)) ||
+          ((snr.flag & GETMASK(LNR_THRESH)) && (value < snr.lnr_thresh))) {
+        return -1;
+      }
       snr.ucr_thresh = value;
       snr.flag |= SETMASK(UCR_THRESH);
       break;
     case UNC_THRESH:
+      if (((snr.flag & GETMASK(UNR_THRESH)) && (value > snr.unr_thresh)) ||
+          ((snr.flag & GETMASK(UCR_THRESH)) && (value > snr.ucr_thresh)) ||
+          ((snr.flag & GETMASK(LNC_THRESH)) && (value < snr.lnc_thresh)) ||
+          ((snr.flag & GETMASK(LCR_THRESH)) && (value < snr.lcr_thresh)) ||
+          ((snr.flag & GETMASK(LNR_THRESH)) && (value < snr.lnr_thresh))) {
+        return -1;
+      }
       snr.unc_thresh = value;
       snr.flag |= SETMASK(UNC_THRESH);
       break;
     case UNR_THRESH:
+      if (((snr.flag & GETMASK(UCR_THRESH)) && (value < snr.ucr_thresh)) ||
+          ((snr.flag & GETMASK(UNC_THRESH)) && (value < snr.unc_thresh)) ||
+          ((snr.flag & GETMASK(LNC_THRESH)) && (value < snr.lnc_thresh)) ||
+          ((snr.flag & GETMASK(LCR_THRESH)) && (value < snr.lcr_thresh)) ||
+          ((snr.flag & GETMASK(LNR_THRESH)) && (value < snr.lnr_thresh))) {
+        return -1;
+      }
       snr.unr_thresh = value;
       snr.flag |= SETMASK(UNR_THRESH);
       break;
     case LCR_THRESH:
+      if (((snr.flag & GETMASK(LNR_THRESH)) && (value < snr.lnr_thresh)) ||
+          ((snr.flag & GETMASK(LNC_THRESH)) && (value > snr.lnc_thresh)) ||
+          ((snr.flag & GETMASK(UNC_THRESH)) && (value > snr.unc_thresh)) ||
+          ((snr.flag & GETMASK(UCR_THRESH)) && (value > snr.ucr_thresh)) ||
+          ((snr.flag & GETMASK(UNR_THRESH)) && (value > snr.unr_thresh))) {
+        return -1;
+      }
       snr.lcr_thresh = value;
       snr.flag |= SETMASK(LCR_THRESH);
       break;
     case LNC_THRESH:
+      if (((snr.flag & GETMASK(LNR_THRESH)) && (value < snr.lnr_thresh)) ||
+          ((snr.flag & GETMASK(LCR_THRESH)) && (value < snr.lcr_thresh)) ||
+          ((snr.flag & GETMASK(UNC_THRESH)) && (value > snr.unc_thresh)) ||
+          ((snr.flag & GETMASK(UCR_THRESH)) && (value > snr.ucr_thresh)) ||
+          ((snr.flag & GETMASK(UNR_THRESH)) && (value > snr.unr_thresh))) {
+        return -1;
+      }
       snr.lnc_thresh = value;
       snr.flag |= SETMASK(LNC_THRESH);
       break;
     case LNR_THRESH:
+      if (((snr.flag & GETMASK(LCR_THRESH)) && (value > snr.lcr_thresh)) ||
+          ((snr.flag & GETMASK(LNC_THRESH)) && (value > snr.lnc_thresh)) ||
+          ((snr.flag & GETMASK(UNC_THRESH)) && (value > snr.unc_thresh)) ||
+          ((snr.flag & GETMASK(UCR_THRESH)) && (value > snr.ucr_thresh)) ||
+          ((snr.flag & GETMASK(UNR_THRESH)) && (value > snr.unr_thresh))) {
+        return -1;
+      }
       snr.lnr_thresh = value;
       snr.flag |= SETMASK(LNR_THRESH);
       break;
@@ -955,4 +996,8 @@ pal_sensor_deassert_handle(uint8_t fru, uint8_t snr_num, float val, uint8_t thre
   return;
 }
 
+int __attribute__((weak))
+pal_get_sensor_util_timeout(uint8_t fru) {
+  return 4;
+}
 

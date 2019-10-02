75ce43a4de4de1d12b352e9abb2ac5bba414a2bb
Amithash Prasad <amithash@fb.com>
2019-10-02 16:00:38 -0700

    fbal: fw-util support to display ME version
    Summary: Add ME component to fw-util.
    
    Test Plan: #doitlive
    
    Reviewed By: benwei13
    
    fbshipit-source-id: be44871cf2

---
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/me.cpp
@@ -0,0 +1,23 @@
+#include "fw-util.h"
+#include <openbmc/pal.h>
+#include <openbmc/pal_sensors.h>
+
+using namespace std;
+
+class MeComponent : public Component {
+  public:
+    MeComponent(string fru, string comp)
+      : Component(fru, comp) {}
+    int print_version() {
+      char ver[128] = {0};
+      // Print ME Version
+      if (pal_get_me_fw_ver(NM_IPMB_BUS_ID, NM_SLAVE_ADDR, (uint8_t *)ver)){
+        printf("ME Version: NA\n");
+      } else {
+        printf("ME Version: %x.%x.%x.%x%x\n", ver[0], ver[1], ver[2], ver[3], ver[4]);
+      }
+      return 0;
+    }
+};
+
+MeComponent me("mb", "me");

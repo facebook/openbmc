8374b1c02b9e7c1bb16d59e2dc916877e276e65f
Tao Ren <taoren@fb.com>
2019-10-02 12:28:53 -0700

    tests2: cmm: add i2c test
    Summary:
    Add cmm i2c test to make sure all 106 i2c devices are created and
    according drivers are binded properly.
    
    Test Plan: Launched the new test on cmm-v2019.39.0: passed.
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 91730e10bd

---
+++ tests2/tests/cmm/test_data/i2c/i2c.py
@@ -0,0 +1,426 @@
+plat_i2c_tree = {
+    '77-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '74-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '17-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '195-0033': {
+        'name': 'fancpld',
+        'driver': 'fancpld',
+    },
+    '17-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '45-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '45-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '50-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '138-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '106-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '74-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '22-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '77-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '129-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '61-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '138-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '49-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '42-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '90-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '90-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '1-0077': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '77-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '154-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '106-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '77-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '154-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '179-0033': {
+        'name': 'fancpld',
+        'driver': 'fancpld',
+    },
+    '8-0077': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '82-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '161-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '106-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '122-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '23-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '34-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '145-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '154-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '58-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '81-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '74-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '180-0022': {
+        'name': 'pca9534',
+        'driver': 'pca953x',
+    },
+    '33-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '16-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '162-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '35-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '61-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '61-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '28-0050': {
+        'name': '24c64',
+        'driver': 'at24',
+    },
+    '45-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '13-003e': {
+        'name': 'cmmcpld',
+        'driver': 'cmmcpld',
+    },
+    '146-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '42-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '19-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '21-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '23-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '26-0010': {
+        'name': 'pfe3000',
+        'driver': 'bel-pfe',
+    },
+    '93-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '187-0033': {
+        'name': 'fancpld',
+        'driver': 'fancpld',
+    },
+    '90-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '58-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '58-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '122-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '2-0071': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '122-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '130-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '138-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '65-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '24-0010': {
+        'name': 'pfe3000',
+        'driver': 'bel-pfe',
+    },
+    '66-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '97-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '30-0021': {
+        'name': 'pca9535',
+        'driver': 'pca953x',
+    },
+    '106-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '113-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '172-0022': {
+        'name': 'pca9534',
+        'driver': 'pca953x',
+    },
+    '122-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '29-0020': {
+        'name': 'pca9505',
+        'driver': 'pca953x',
+    },
+    '42-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '42-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '138-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '3-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '3-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '6-0051': {
+        'name': '24c64',
+        'driver': 'at24',
+    },
+    '74-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '196-0022': {
+        'name': 'pca9534',
+        'driver': 'pca953x',
+    },
+    '45-004a': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '188-0022': {
+        'name': 'pca9534',
+        'driver': 'pca953x',
+    },
+    '114-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '98-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '93-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '16-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '18-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '58-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '18-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '171-0033': {
+        'name': 'fancpld',
+        'driver': 'fancpld',
+    },
+    '90-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '93-0048': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '22-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '20-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '93-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '32-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '154-0049': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+    '21-0073': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '27-0010': {
+        'name': 'pfe3000',
+        'driver': 'bel-pfe',
+    },
+    '20-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '19-0070': {
+        'name': 'pca9548',
+        'driver': 'pca954x',
+    },
+    '25-0010': {
+        'name': 'pfe3000',
+        'driver': 'bel-pfe',
+    },
+    '61-004b': {
+        'name': 'tmp75',
+        'driver': 'lm75',
+    },
+}

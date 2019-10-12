eeb7caa340e35d56ebbfb634591b0051a33af510
Joan Olguy Canéus <joancaneus@fb.com>
2019-10-12 16:46:03 -0700

    wedge400 restful: remove spare ':' of the result of restful feutil endpoint (#178)
    Summary:
    As title
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/178
    
    Test Plan:
    **Old output:**
    root@bmc-oob:~# curl -g http://127.0.0.1:8080/api/sys/feutil/fan1 | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   794  100   794    0     0    302      0  0:00:02  0:00:02 --:--:--   305
    {
        "Actions": [],
        "Resources": [],
        "Information": {
            **"Fan 1 eeprom:": {**
                "CRC8": "0x14",
                "Product Asset Tag": "NA",
                "System Manufacturer": "CLS",
                "Local MAC": "00:00:00:00:00:00",
                "ODM PCBA Serial Number": "G091219330487",
                "Product Sub-Version": "0",
                "PCB Manufacturer": "WUS",
                "System Manufacturing Date": "09-02-19",
                "Location on Fabric": "FAN",
                "Facebook PCBA Part Number": "132-000102-01",
                "ODM PCBA Part Number": "R1149G100901",
                "Facebook PCB Part Number": "131-000070-01",
                "Assembled At": "CLS",
                "Product Production State": "2",
                "System Assembly Part Number": "050-00421",
                "Product Version": "0",
                "Product Name": "WEDGE400-FAN",
                "Product Serial Number": "F023219350012",
                "Extended MAC Base": "00:00:00:00:00:00",
                "Product Part Number": "NA-",
                "Extended MAC Address Size": "0",
                "Version": "3"
            }
        }
    }
    **New output:**
    root@bmc-oob:~# curl -g http://127.0.0.1:8080/api/sys/feutil/fan1 | python -m json.tool
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   793  100   793    0     0    352      0  0:00:02  0:00:02 --:--:--   356
    {
        "Actions": [],
        "Resources": [],
        "Information": {
            **"Fan 1 eeprom": {**
                "Product Production State": "1",
                "Product Version": "2",
                "Extended MAC Base": "7A:00:00:00:00:00",
                "Product Part Number": "NA-",
                "Local MAC": "7A:00:00:00:00:00",
                "ODM PCBA Serial Number": "G092119140001",
                "Assembled At": "CLS",
                "CRC8": "0x14",
                "Facebook PCBA Part Number": "132-000092-01",
                "Product Serial Number": "R06421916xxxx",
                "Version": "3",
                "Location on Fabric": "FAN",
                "Product Sub-Version": "0",
                "Facebook PCB Part Number": "131-000070-01",
                "Product Asset Tag": "NA",
                "PCB Manufacturer": "WUS",
                "System Manufacturing Date": "05-22-19",
                "System Assembly Part Number": "050-00388",
                "Product Name": "WEDGE400-FAN",
                "Extended MAC Address Size": "0",
                "System Manufacturer": "CLS",
                "ODM PCBA Part Number": "R1018G000902"
            }
        }
    }
    
    Pass
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: f5be6c9008

--- meta-facebook/meta-wedge400/recipes-utils/rest-api/files/rest-api-1/rest_feutil.py
+++ meta-facebook/meta-wedge400/recipes-utils/rest-api/files/rest-api-1/rest_feutil.py
@@ -20,67 +20,70 @@
 
 import subprocess
 
-
 from rest_utils import DEFAULT_TIMEOUT_SEC
 
 
 # Handler for feutil resource endpoint
 def get_feutil():
     return {
-            "Information": [],
-            "Actions": [],
-            "Resources": ["all", "fcm", "fan1", "fan2", "fan3", "fan4"]
-           }
+        "Information": [],
+        "Actions": [],
+        "Resources": ["all", "fcm", "fan1", "fan2", "fan3", "fan4"],
+    }
+
 
 def _parse_feutil_data(data):
     result = {}
     for eeprom in data:
         sresult = {}
         # the first info line from feutil is eeprom device
-        adata = eeprom.split('\n', 1)
+        adata = eeprom.split("\n", 1)
         if len(adata) < 2:
             continue
-        for sdata in adata[1].split('\n'):
-            tdata = sdata.split(':', 1)
-            if (len(tdata) < 2):
+        for sdata in adata[1].split("\n"):
+            tdata = sdata.split(":", 1)
+            if len(tdata) < 2:
                 if len(tdata[0].strip()) > 0:
                     sresult = tdata[0].strip()
                 continue
             if len(tdata[1].strip()) > 0:
                 sresult[tdata[0].strip()] = tdata[1].strip()
-        result[adata[0].strip()] = sresult
+        result[adata[0].strip().strip(":")] = sresult
     return result
+
 
 def _packet_feutil_date(cmd):
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     data, _ = proc.communicate(timeout=DEFAULT_TIMEOUT_SEC)
-    data = data.decode(errors='ignore')
-    data = data.split('\r\n')
-    return {
-            "Information": _parse_feutil_data(data),
-            "Actions": [],
-            "Resources": []
-           }
+    data = data.decode(errors="ignore")
+    data = data.split("\r\n")
+    return {"Information": _parse_feutil_data(data), "Actions": [], "Resources": []}
+
 
 def get_feutil_all_data():
     cmd = ["/usr/local/bin/feutil", "all"]
     return _packet_feutil_date(cmd)
 
+
 def get_feutil_fcm_data():
     cmd = ["/usr/local/bin/feutil", "fcm"]
     return _packet_feutil_date(cmd)
+
 
 def get_feutil_fan1_data():
     cmd = ["/usr/local/bin/feutil", "1"]
     return _packet_feutil_date(cmd)
 
+
 def get_feutil_fan2_data():
     cmd = ["/usr/local/bin/feutil", "2"]
     return _packet_feutil_date(cmd)
 
+
 def get_feutil_fan3_data():
     cmd = ["/usr/local/bin/feutil", "3"]
     return _packet_feutil_date(cmd)
+
 
 def get_feutil_fan4_data():
     cmd = ["/usr/local/bin/feutil", "4"]

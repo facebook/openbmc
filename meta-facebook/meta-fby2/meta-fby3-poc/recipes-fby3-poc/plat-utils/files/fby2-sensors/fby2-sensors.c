c71cc774590b75f6cbd916d30d152db881e89156
Ben Wei <benwei@fb.com>
2019-10-22 10:55:03 -0700

    fby3-poc: support reading temperature sensor of 2U Expansion (#802)
    Summary:
    Summary
    - cmd only reads one temperature sensor on 1OU, 2OU expansion board, and baseboard.
    - User needs to run bic-cached tool to dump a sdr, then run fby2-sensor
    
    TestPlan
    -Build code(Pass)
    -Read one temperature on 2OU platform(Pass)
    -Read one temperature on baseboard(Pass)
    -Read one temperature on 1OU platform(Pass)
    Ps. All test are done with the version of server board BIC v1.8
    
    //Run the tool on class 2
    root@bmc-oob:~# bic-cached -r 0x10 1
    root@bmc-oob:~# bic-cached -r 0x15 1
    root@bmc-oob:~# ls /tmp/sdr_slot1*
    /tmp/sdr_slot1.16.bin  /tmp/sdr_slot1.21.bin
    root@bmc-oob:~# ./fby2-sensors
    MB Inlet Temp                (0x1) :   27.00 C | UCR: 93.00. (baseboard)
    ---1ou expansion card is not existed---
    MB Outlet Temp_T            (0x7) :   24.00 C | UCR: 93.00. (2ou expansion card)
    root@bmc-oob:~#
    
    root@bmc-oob:~# ./fby2-sensors 2
    Specify the slot_id to 2
    ---baseboard is not existed---
    ---1ou expansion card is not existed---
    ---2ou expansion card is not existed---
    
    root@bmc-oob:/tmp# ./fby2-sensors 3
    Specify the slot_id to 3
    ---baseboard is not existed---
    ---1ou expansion card is not existed---
    ---2ou expansion card is not existed---
    
    //Run the tool on class 1
    root@bmc-oob:/tmp# bic-cached -r 0x05 1
    root@bmc-oob:/tmp# ./fby2-sensors
    ---baseboard is not existed---
    MB Outlet Temp_T            (0x7) :   23.00 C | UCR: 93.00. (1ou expansion card)
    ---2ou expansion card is not existed---
    root@bmc-oob:/tmp# ls /tmp/sdr_slot1.*
    /tmp/sdr_slot1.5.bin  /tmp/sdr_slot1.bin
    root@bmc-oob:/tmp#
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/802
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 2eb2592844

--- meta-facebook/meta-fby2/meta-fby3-poc/recipes-fby3-poc/plat-utils/files/fby2-sensors/fby2-sensors.c
+++ meta-facebook/meta-fby2/meta-fby3-poc/recipes-fby3-poc/plat-utils/files/fby2-sensors/fby2-sensors.c
@@ -90,8 +90,9 @@ struct remote_dev {
   uint8_t sensor_cnt;
   char *desc;
 } remote_snr_list[] = {
-  {"/tmp/sdr_slot1.16.bin", 0x1, 0x10/*baseboard*/, {0x01}, 0x1, "baseboard"},
-  {"/tmp/sdr_slot1.5.bin", 0x1, 0x05/*m2 exp*/, {0x7}, 0x1, "m2 expansion"},
+  {NULL, 0x1, 0x10/*baseboard*/, {0x01}, 0x1, "baseboard"},
+  {NULL, 0x1, 0x05/*2ou exp*/  , {0x07}, 0x1, "1ou expansion card"},
+  {NULL, 0x1, 0x15/*1ou exp*/  , {0x07}, 0x1, "2ou expansion card"}
 };
 
 static int remote_snr_size = sizeof(remote_snr_list) / sizeof(struct remote_dev);
@@ -140,8 +141,10 @@ main(int argc, char **argv) {
 #define FIELD_LEN(x)        (x & 0x1F)
   sensor_info_t sinfo[MAX_SENSOR_NUM];
   sdr_full_t *sdr;
-  uint8_t slot_id;
+  char *path[32]={0};
+  uint8_t slot_id = 1;
   uint8_t intf;
+  uint8_t specify_id = 0;
   uint8_t sensor_num;
   ipmi_sensor_reading_t sensor;
   int field_len;
@@ -149,13 +152,27 @@ main(int argc, char **argv) {
   int ret;
   float ucr = 0;
 
+  if ( argc == 2 ) {
+    slot_id = (uint8_t)strtol(argv[1], NULL, 16);
+    specify_id = 1;
+    printf("specify the slot id: %d\n", slot_id);
+  }
+
   for ( i = 0; i < remote_snr_size; i++ ) {
     memset(sinfo, 0, sizeof(sinfo));
+
+    //set the path
+    snprintf(path, sizeof(path), "/tmp/sdr_slot%d.%d.bin", slot_id, remote_snr_list[i].intf);
+    remote_snr_list[i].path = path;
     //init sdr
     if ( sdr_init(remote_snr_list[i].path, &sinfo) < 0 ) {
+      printf("---%s is not existed---\n", remote_snr_list[i].desc);
       continue;
     }
-    slot_id = remote_snr_list[i].slot_id;
+
+    if ( specify_id == 0 )
+      slot_id = remote_snr_list[i].slot_id;
+
     intf = remote_snr_list[i].intf;
     for ( j = 0; j < remote_snr_list[i].sensor_cnt; j++ ) {
       sensor_num = remote_snr_list[i].sensor_list[j];
@@ -165,7 +182,7 @@ main(int argc, char **argv) {
       sinfo[sensor_num].sdr.str[field_len] = '\0';
       ret = bic_read_sensor_param(slot_id, sensor_num, &sensor, intf);
       if ( sensor.value != 0 ) {
-        printf("%-28s (0x%X) : %7.2f C | UCR: %.2f. (%s)\n", sinfo[sensor_num].sdr.str, sensor_num, (float)sensor.value, ucr, remote_snr_list[i].desc);
+        printf("%-28s (0x%X) : %7.2f C | UCR: %.2f (%s)\n", sinfo[sensor_num].sdr.str, sensor_num, (float)sensor.value, ucr, remote_snr_list[i].desc);
       } else {
         printf("%-28s (0x%X) : NA | UCR: %.2f (%s)\n", sinfo[sensor_num].sdr.str, sensor_num, ucr, remote_snr_list[i].desc);
       }

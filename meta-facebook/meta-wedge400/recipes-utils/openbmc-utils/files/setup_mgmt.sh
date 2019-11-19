b39a5c6dd71ae0a823f981bedb9d65b753f7f3e4
Joan Olguy Canéus <joancaneus@fb.com>
2019-11-18 23:40:35 -0800

    Modified setup script to use MAC1 MDIO for initializing MGMT LED port. (#217)
    Summary:
    Ethernet led status definition:
    ![image](https://user-images.githubusercontent.com/42166389/68929672-cac4c180-07c7-11ea-91d3-8e928726bd35.png)
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/217
    
    Test Plan:
    reboot the BMC and see if LED follows the spec.
    
    Expected result:
        Green LED represents Link LED
        Amber LED represents Activity LED
    
    Pass
    
    Reviewed By: joancaneus
    
    fbshipit-source-id: 97fdfe4bca

--- meta-facebook/meta-wedge400/recipes-utils/openbmc-utils/files/setup_mgmt.sh
+++ meta-facebook/meta-wedge400/recipes-utils/openbmc-utils/files/setup_mgmt.sh
@@ -18,106 +18,55 @@
 # Boston, MA 02110-1301 USA
 #
 
-source /usr/local/bin/openbmc-utils.sh
+. /usr/local/bin/openbmc-utils.sh
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
-SFP_AUTO_DETECT="${SCMCPLD_SYSFS_DIR}/sfp_auto_detect"
-COM_E_PWRGD=`head -n 1 ${SCMCPLD_SYSFS_DIR}/pwrgd_pch_pwrok`
+COM_E_PWRGD=$(head -n 1 "${SCMCPLD_SYSFS_DIR}/pwrgd_pch_pwrok")
 
 usage(){
-    program=`basename "$0"`
+    program=$(basename "$0")
     echo "Usage:"
-    echo "     $program led/combo/all"
+    echo "     $program led"
 }
 
 if [ $# -ne 1 ]; then
     usage
-    exit -1
+    exit 1
 fi
 
 # When COM-e power is not ready, it doesn't work to set PHY register.
 # Therefore, waiting for COM-e power ready, then start to set PHY register.
-while [ "$COM_E_PWRGD" == "0x0" ];
+while [ "$COM_E_PWRGD" = "0x0" ];
 do
-    COM_E_PWRGD=`head -n 1 ${SCMCPLD_SYSFS_DIR}/pwrgd_pch_pwrok`
+    COM_E_PWRGD=$(head -n 1 "${SCMCPLD_SYSFS_DIR}/pwrgd_pch_pwrok")
     sleep 5
 done
 
-devmem_set_bit $(scu_addr 88) 30
-devmem_set_bit $(scu_addr 88) 31
+# Enable MAC#1 Clock, refer to ast2500-v16.pdf Page 355
+# SCU 0x0C register: Bit20 MAC#1 Clock:
+#     0 Enable clock running, 1 disable clock running
+devmem_clear_bit "$(scu_addr 0C)" 20
 
-if [ "$1" == "led" ]; then
+# Enable MAC#1 MDIO function, refer to ast2500-v16.pdf Page 376
+# SCU 0x88 register: Bit31 Enable MAC#1 MDIO1 function pin
+# SCU 0x88 register: Bit30 Enable MAC#1 MDC1 function pin
+devmem_set_bit "$(scu_addr 88)" 30
+devmem_set_bit "$(scu_addr 88)" 31
+
+
+if [ "$1" = "led" ]; then
     echo "Wait a few seconds to setup management port LED..."
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x01d
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x3453
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x012
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0xa03
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x019
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x2418
+    # refer to BCM54616S Section 5, register 1C access
+    # Register 1C (Shadow 00010): Spare Control 1
+    # Bit0: 1: Enable link LED mode.
+    ast-mdio.py --mac 1 --phy 0xe write 0x1c 0x8801
+    # Register 1C (Shadow 01110): LED Selector 2
+    # Bit7~4: LED4 Selector, b0011: Activity LED 
+    # Bit3~0: LED4 Selector, b0101: Slave
+    ast-mdio.py --mac 1 --phy 0xe write 0x1c 0xb435
     echo "Done!"
-elif [ "$1" == "combo" ]; then
-    echo "Wait a few minutes to setup management combo port..."
-    ast-mdio.py --mac 1 --phy 0x9 write 0x17 0x0f7e
-    ast-mdio.py --mac 1 --phy 0x9 write 0x15 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x083c
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x2f
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7167
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x21
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7c05
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x1140
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x21
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7c04
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x09
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x0200
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x04
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x01e1
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x1340
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x234
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0xd18f
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0xb00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x1140
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x23e
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7ae2
-    echo 1 > $SFP_AUTO_DETECT
-    echo "Done!"
-elif [ "$1" == "all" ]; then
-    echo "Wait a few minutes to setup management port..."
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x01d
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x3453
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x012
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0xa03
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x019
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x2418
-    ast-mdio.py --mac 1 --phy 0x9 write 0x17 0x0f7e
-    ast-mdio.py --mac 1 --phy 0x9 write 0x15 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x083c
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x2f
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7167
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x21
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7c05
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x1140
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x21
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7c04
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x09
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x0200
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x04
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x01e1
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x1340
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x234
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0xd18f
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0xb00
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x1140
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1e 0x23e
-    ast-mdio.py --mac 1 --phy 0x9 write 0x1f 0x7ae2
-    echo 1 > $SFP_AUTO_DETECT
 else
     usage
-    exit -1
+    exit 1
 fi

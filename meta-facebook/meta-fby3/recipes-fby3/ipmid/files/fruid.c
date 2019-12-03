881706b68f687e44b2c5d32f2f8384583ad5feaa
Ben Wei <benwei@fb.com>
2019-12-03 14:32:12 -0800

    fby3: add the custom fruid in ipmid (#835)
    Summary:
    -Instantiate the eeprom based on the board id
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/835
    
    Test Plan: -Build code(pass)
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 9bdb3833d1

---
+++ meta-facebook/meta-fby3/recipes-fby3/ipmid/files/fruid.c
@@ -0,0 +1,162 @@
+/*
+ *
+ * Copyright 2015-present Facebook. All Rights Reserved.
+ *
+ * This file provides platform specific implementation of FRUID information
+ *
+ * FRUID specification can be found at
+ * www.intel.com/content/dam/www/public/us/en/documents/product-briefs/platform-management-fru-document-rev-1-2-feb-2013.pdf
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <openbmc/pal.h>
+#include <facebook/fby3_common.h>
+#include <facebook/fby3_fruid.h>
+#include "fruid.h"
+
+#define FRUID_SIZE        512
+/*
+ * copy_eeprom_to_bin - copy the eeprom to binary file im /tmp directory
+ *
+ * @eeprom_file   : path for the eeprom of the device
+ * @bin_file      : path for the binary file
+ *
+ * returns 0 on successful copy
+ * returns non-zero on file operation errors
+ */
+int copy_eeprom_to_bin(const char *eeprom_file, const char *bin_file) {
+
+  int eeprom;
+  int bin;
+  uint64_t tmp[FRUID_SIZE];
+  ssize_t bytes_rd, bytes_wr;
+
+  errno = 0;
+
+  eeprom = open(eeprom_file, O_RDONLY);
+  if (eeprom == -1) {
+    syslog(LOG_ERR, "%s: unable to open the %s file: %s",
+	__func__, eeprom_file, strerror(errno));
+    return errno;
+  }
+
+  bin = open(bin_file, O_WRONLY | O_CREAT, 0644);
+  if (bin == -1) {
+    syslog(LOG_ERR, "%s: unable to create %s file: %s",
+	__func__, bin_file, strerror(errno));
+    goto err;
+  }
+
+  bytes_rd = read(eeprom, tmp, FRUID_SIZE);
+  if (bytes_rd < 0) {
+    syslog(LOG_ERR, "%s: read %s file failed: %s",
+	__func__, eeprom_file, strerror(errno));
+    goto exit;
+  } else if (bytes_rd < FRUID_SIZE) {
+    syslog(LOG_ERR, "%s: less than %d bytes", __func__, FRUID_SIZE);
+    goto exit;
+  }
+
+  bytes_wr = write(bin, tmp, bytes_rd);
+  if (bytes_wr != bytes_rd) {
+    syslog(LOG_ERR, "%s: write to %s file failed: %s",
+	__func__, bin_file, strerror(errno));
+    goto exit;
+  }
+
+exit:
+  close(bin);
+err:
+  close(eeprom);
+
+  return errno;
+}
+
+static int
+fruid_init_local_fru() {
+  int ret = 0;
+  char path[128] = {0};
+  int path_len = sizeof(path);
+  int fd = 0;
+  char *dev[2] = {"24c128 0x54\n", "24c128 0x51\n"};
+  uint8_t bmc_location = 0;
+
+  ret = get_bmc_location(&bmc_location);
+  if ( ret < 0 ) {
+    syslog(LOG_WARNING, "%s() Cannot get the location of BMC", __func__);
+    return ret;;
+  }
+
+  //create the path
+  snprintf(path, path_len, I2C_PATH, (bmc_location == BB_BMC)?CLASS1_FRU_BUS:CLASS2_FRU_BUS);
+  
+  fd = open(path, O_WRONLY);
+  if ( fd < 0 ) {
+    syslog(LOG_WARNING, "%s() Failed to open %s, %s", __func__, path, strerror(errno));
+    goto error_exit;
+  }
+    
+  write(fd, dev[0], strlen(dev[0]));
+  write(fd, dev[1], strlen(dev[1]));
+
+  //create the binary in /tmp/
+  snprintf(path, path_len, EEPROM_PATH, (bmc_location == BB_BMC)?CLASS1_FRU_BUS:CLASS2_FRU_BUS, BMC_FRU_ADDR);
+  if ( copy_eeprom_to_bin(path, FRU_BMC_BIN) < 0 ) {
+    syslog(LOG_WARNING, "%s() Failed to copy %s to %s", __func__, path, FRU_BMC_BIN);
+    goto error_exit;
+  }
+
+  snprintf(path, path_len, EEPROM_PATH, (bmc_location == BB_BMC)?CLASS1_FRU_BUS:CLASS2_FRU_BUS, BB_FRU_ADDR);
+  if ( copy_eeprom_to_bin(path, FRU_BB_BIN) < 0 ) {
+    syslog(LOG_WARNING, "%s() Failed to copy %s to %s", __func__, path, FRU_BB_BIN);
+    goto error_exit;
+  }
+
+error_exit:
+  if ( fd > 0 ) close(fd);
+  return ret;
+}
+
+/* Populate the platform specific eeprom for fruid info */
+int plat_fruid_init(void) {
+  int ret;
+
+  //export FRU that is connected to BMC directly.
+  ret = fruid_init_local_fru();  
+  if ( ret < 0 ) {
+    syslog(LOG_WARNING, "%s() somethings went wrong in fruid_init_local_fru()", __func__); 
+  } 
+ 
+  return ret;
+}
+
+int plat_fruid_data(unsigned char payload_id, int fru_id, int offset, int count, unsigned char *data) {
+  return 0;
+}
+
+int plat_fruid_size(unsigned char payload_id) {
+  return 0;
+}

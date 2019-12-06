ca3f8b9aebd39d67acc241a5ffb4f22fc6d2c0ba
Ben Wei <benwei@fb.com>
2019-12-06 09:40:52 -0800

    fby3: initialize sensord (#845)
    Summary:
    -add init script to run sensord
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/845
    
    Test Plan: -Build code(pass)
    
    Reviewed By: benwei13
    
    fbshipit-source-id: fa7ece7e69

---
+++ meta-facebook/meta-fby3/recipes-fby3/sensor-mon/files/run-sensord.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+. /usr/local/fbpackages/utils/ast-functions
+echo -n "Setup sensor monitoring for FBY3... "
+SLOTS=
+
+function init_class2_sensord() {
+  SLOTS="slot0 bmc nic"
+}
+
+function init_class1_sensord() {
+  # Check for the slots present and run sensord for those slots only.
+  if [ $(is_server_prsnt 0) == "1" ]; then
+    if [ $(is_slot_12v_on 0) == "1" ]; then
+      SLOTS="$SLOTS slot0"
+    fi
+  fi
+
+  if [ $(is_server_prsnt 1) == "1" ]; then
+    if [ $(is_slot_12v_on 1) == "1" ]; then
+      SLOTS="$SLOTS slot1"
+    fi
+  fi
+
+  if [ $(is_server_prsnt 2) == "1" ]; then
+    if [ $(is_slot_12v_on 2) == "1" ]; then
+      SLOTS="$SLOTS slot2"
+    fi
+  fi
+
+  if [ $(is_server_prsnt 3) == "1" ]; then
+    if [ $(is_slot_12v_on 3) == "1" ]; then
+      SLOTS="$SLOTS slot3"
+    fi
+  fi
+
+  SLOTS="$SLOTS bmc nic"
+}
+
+bmc_location=$(get_bmc_board_id)
+if [ $bmc_location -eq 9 ]; then
+  #The BMC of class2
+  init_class2_sensord
+elif [ $bmc_location -eq 14 ]; then
+  #The BMC of class1
+  init_class1_sensord
+else
+  echo -n "Is board id correct(id=$bmc_location)?..."
+fi
+
+exec /usr/local/bin/sensord $SLOTS
+
+if [ ! -n "$SLOTS" ]; then
+  echo "sensord is stopped"
+  sv stop sensord
+fi

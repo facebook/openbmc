d3c526c6cf577abe10aca3d70fafc990015007de
Ben Wei <benwei@fb.com>
2019-12-17 13:26:41 -0800

    fby3: add YV3 plat_tree for REST API (#860)
    Summary:
    -Provide plat_tree function to rest-api service to prevent rest-api service from looping crashing/restart and causing high CPU usage
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/860
    
    Test Plan:
    -Build code(pass)
    -Use `top` to check the CPU usage
    ```
    Mem: 144432K used, 287100K free, 584K shrd, 16K buff, 98084K cached
    CPU:   4% usr   8% sys   0% nic  87% idle   0% io   0% irq   0% sirq
    Load average: 0.34 0.18 0.15 1/92 21162
      PID  PPID USER     STAT   VSZ %VSZ %CPU COMMAND
     2400  2397 root     S    49068  11%   7% /usr/local/bin/sensord slot2 bmc nic
     1939  1935 root     S    40492   9%   3% /usr/local/bin/ipmbd 1 2
    20770  2480 root     R     3100   1%   1% top
        7     2 root     SW       0   0%   0% [ksoftirqd/0]
     1425     1 root     S    12548   3%   0% /usr/sbin/rngd -r /dev/hwrng
       10     2 root     IW       0   0%   0% [kworker/0:1-eve]
     2396  2394 root     S    48788  11%   0% /usr/local/bin/healthd
     2402  2399 root     S    44732  10%   0% python3 /usr/local/bin/rest.py
     1491     1 root     S    35200   8%   0% /usr/sbin/rsyslogd
     1429  1427 root     S    34472   8%   0% dhclient -6 -d -D LL --address-prefix-len 64 -pf /var/run/dhclient6.eth0.pid eth0
     1403     1 root     S    34472   8%   0% dhclient -pf /var/run/dhclient.eth0.pid eth0
     2415     1 root     S    18156   4%   0% python3 /usr/local/bin/spatula_wrapper.py
     1912  1910 root     S    14924   3%   0% /usr/local/bin/ipmid
     1487     1 ntp      S     7864   2%   0% /usr/sbin/ntpd -u ntp:ntp -p /var/run/ntpd.pid -g
     3054  1479 root     S     5452   1%   0% sshd: root@pts/0
     6499  1479 root     S     5452   1%   0% sshd: root@pts/1
     1479     1 root     S     5332   1%   0% /usr/sbin/sshd
     6557  6499 root     S     3196   1%   0% -bash
     3112  3054 root     S     3172   1%   0% -bash
     2440     1 root     S     3100   1%   0% /usr/sbin/crond -c /etc/cron/crontabs
     2480  2461 root     S     3072   1%   0% -bash
     1190     1 root     S     3052   1%   0% /sbin/udevd -d
     2461     1 root     S     2980   1%   0% {start_getty} /bin/sh /bin/start_getty 57600 ttyS0 vt102
     1427     1 root     S     2876   1%   0% runsv /etc/sv/dhc6
     1901     1 root     S     2876   1%   0% runsv /etc/sv/mTerm1
     1903     1 root     S     2876   1%   0% runsv /etc/sv/mTerm3
     1904     1 root     S     2876   1%   0% runsv /etc/sv/mTerm4
     1910     1 root     S     2876   1%   0% runsv /etc/sv/ipmid
     1935     1 root     S     2876   1%   0% runsv /etc/sv/ipmbd_1
     1947     1 root     S     2876   1%   0% runsv /etc/sv/ipmbd_2
     1957     1 root     S     2876   1%   0% runsv /etc/sv/ipmbd_3
     2394     1 root     S     2876   1%   0% runsv /etc/sv/healthd
    ^C399     1 root     S     2876   1%   0% runsv /etc/sv/restapi
    ```
    
    Reviewed By: benwei13
    
    fbshipit-source-id: e25be83767

---
+++ meta-facebook/meta-fby3/recipes-fby3/rest-api/files/plat_tree.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+#
+# Copyright 2014-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+import json
+import os
+import socket
+import ssl
+from ctypes import *
+from time import sleep
+
+from node_api import get_node_api
+from node_bios import *
+from node_bmc import get_node_bmc
+from node_config import get_node_config
+from node_fans import get_node_fans
+from node_fruid import get_node_fruid
+from node_logs import get_node_logs
+from node_mezz import get_node_mezz
+from node_sensors import get_node_sensors
+from node_server import get_node_device, get_node_server
+from node_spb import get_node_spb
+from pal import *
+from tree import tree
+
+def populate_server_node(r_api, num):
+    prsnt = pal_is_fru_prsnt(num)
+    if prsnt == None or prsnt == 0:
+        return None
+
+    r_server = tree("server" + repr(num), data=get_node_server(num))
+
+    r_api.addChild(r_server)
+    r_fruid = tree("fruid", data=get_node_fruid("slot" + repr(num)))
+    r_sensors = tree("sensors", data=get_node_sensors("slot" + repr(num)))
+    r_logs = tree("logs", data=get_node_logs("slot" + repr(num)))
+    r_config = tree("config", data=get_node_config("slot" + repr(num)))
+    if slot_type == 0:
+        r_bios = tree("bios", data=get_node_bios("server" + repr(num)))
+        r_server.addChildren([r_fruid, r_sensors, r_logs, r_config, r_bios])
+    else:
+        r_server.addChildren([r_fruid, r_sensors, r_logs, r_config])
+
+    r_boot_order_trunk = tree(
+        "boot-order", data=get_node_bios_boot_order_trunk("slot" + repr(num))
+    )
+    r_postcode_trunk = tree(
+        "postcode", data=get_node_bios_postcode_trunk("slot" + repr(num))
+    )
+    r_plat_info_trunk = tree(
+        "plat-info", data=get_node_bios_plat_info_trunk("slot" + repr(num))
+    )
+    if slot_type == 0:
+        r_bios.addChildren([r_boot_order_trunk, r_postcode_trunk, r_plat_info_trunk])
+
+    r_boot_mode = tree("boot_mode", data=get_node_bios_boot_mode("slot" + repr(num)))
+    r_clear_cmos = tree("clear_cmos", data=get_node_bios_clear_cmos("slot" + repr(num)))
+    r_force_boot_bios_setup = tree(
+        "force_boot_bios_setup", data=get_node_bios_force_boot_setup("slot" + repr(num))
+    )
+    r_boot_order = tree("boot_order", data=get_node_bios_boot_order("slot" + repr(num)))
+    r_boot_order_trunk.addChildren(
+        [r_boot_mode, r_clear_cmos, r_force_boot_bios_setup, r_boot_order]
+    )
+
+    return r_server
+
+
+# Initialize Platform specific Resource Tree
+def init_plat_tree():
+
+    # Create /api end point as root node
+    r_api = tree("api", data=get_node_api())
+
+    # Add /api/spb to represent side plane board
+    r_spb = tree("spb", data=get_node_spb())
+    r_api.addChild(r_spb)
+
+    # Add /api/mezz to represent Network Mezzaine card
+    r_mezz = tree("mezz", data=get_node_mezz())
+    r_api.addChild(r_mezz)
+
+    # Add servers /api/server[1-max]
+    num = pal_get_num_slots()
+    for i in range(1, num + 1):
+        r_server = populate_server_node(r_api, i)
+
+    # Add /api/spb/fruid end point
+    r_temp = tree("fruid", data=get_node_fruid("spb"))
+    r_spb.addChild(r_temp)
+    # /api/spb/bmc end point
+    r_temp = tree("bmc", data=get_node_bmc())
+    r_spb.addChild(r_temp)
+    # /api/spb/sensors end point
+    r_temp = tree("sensors", data=get_node_sensors("spb"))
+    r_spb.addChild(r_temp)
+    # Add /api/spb/fans end point
+    r_temp = tree("fans", data=get_node_fans())
+    r_spb.addChild(r_temp)
+    # /api/spb/logs end point
+    r_temp = tree("logs", data=get_node_logs("spb"))
+    r_spb.addChild(r_temp)
+
+    # Add /api/mezz/fruid end point
+    r_temp = tree("fruid", data=get_node_fruid("nic"))
+    r_mezz.addChild(r_temp)
+    # /api/mezz/sensors end point
+    r_temp = tree("sensors", data=get_node_sensors("nic"))
+    r_mezz.addChild(r_temp)
+    # /api/mezz/logs end point
+    r_temp = tree("logs", data=get_node_logs("nic"))
+    r_mezz.addChild(r_temp)
+
+    return r_api

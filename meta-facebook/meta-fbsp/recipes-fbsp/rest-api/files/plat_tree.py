f53542c5704d79ca1aefb670596ba948ba4a9513
Patrick Williams <patrickw3@fb.com>
2019-12-18 07:04:43 -0800

    fbsp: rest-api: fix lint errors
    Test Plan: `arc lint`
    
    Reviewed By: amithash
    
    fbshipit-source-id: 988a0d0fb6

--- meta-facebook/meta-fbsp/recipes-fbsp/rest-api/files/plat_tree.py
+++ meta-facebook/meta-fbsp/recipes-fbsp/rest-api/files/plat_tree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright 2014-present Facebook. All Rights Reserved.
 #
@@ -18,84 +18,81 @@
 # Boston, MA 02110-1301 USA
 #
 
-from ctypes import *
-import json
-import ssl
-import socket
 import os
+
+# from node_config import get_node_config
 from node_api import get_node_api
-from node_sled import get_node_sled
-from node_mezz import get_node_mezz
 from node_bmc import get_node_bmc
-from node_server import get_node_server_2s
 from node_fruid import get_node_fruid
-from node_sensors import get_node_sensors
 from node_logs import get_node_logs
-from node_config import get_node_config
-from tree import tree
+from node_mezz import get_node_mezz
+from node_sensors import get_node_sensors
+from node_server import get_node_server_2s
+from node_sled import get_node_sled
 from pal import *
+from tree import tree
+
 
 # Initialize Platform specific Resource Tree
 def init_plat_tree():
 
     # Create /api end point as root node
-    r_api = tree("api", data = get_node_api())
+    r_api = tree("api", data=get_node_api())
 
     # Add /api/sled to represent entire SLED
-    r_sled = tree("sled", data = get_node_sled())
+    r_sled = tree("sled", data=get_node_sled())
     r_api.addChild(r_sled)
 
-
     # Add mb /api/sled/mb
-    r_mb = tree("mb", data = get_node_server_2s(1, "mb"))
+    r_mb = tree("mb", data=get_node_server_2s(1, "mb"))
     r_sled.addChild(r_mb)
 
     # Add /api/sled/mb/fruid end point
-    r_temp = tree("fruid", data = get_node_fruid("mb"))
+    r_temp = tree("fruid", data=get_node_fruid("mb"))
     r_mb.addChild(r_temp)
 
     # /api/sled/mb/bmc end point
-    r_temp = tree("bmc", data = get_node_bmc())
+    r_temp = tree("bmc", data=get_node_bmc())
     r_mb.addChild(r_temp)
 
     # /api/sled/mb/sensors end point
-    r_temp = tree("sensors", data = get_node_sensors("mb"))
+    r_temp = tree("sensors", data=get_node_sensors("mb"))
     r_mb.addChild(r_temp)
 
     # /api/sled/mb/logs end point
-    r_temp = tree("logs", data = get_node_logs("mb"))
+    r_temp = tree("logs", data=get_node_logs("mb"))
     r_mb.addChild(r_temp)
 
     # Add /api/sled/mezz0 to represent Network Mezzaine card
-    r_mezz = tree("mezz0", data = get_node_mezz())
+    r_mezz = tree("mezz0", data=get_node_mezz())
     r_sled.addChild(r_mezz)
 
     # Add /api/sled/mezz0/fruid end point
-    r_temp = tree("fruid", data = get_node_fruid("nic0"))
+    r_temp = tree("fruid", data=get_node_fruid("nic0"))
     r_mezz.addChild(r_temp)
 
     # /api/sled/mezz0/sensors end point
-    r_temp = tree("sensors", data = get_node_sensors("nic0"))
+    r_temp = tree("sensors", data=get_node_sensors("nic0"))
     r_mezz.addChild(r_temp)
 
     # /api/sled/mezz0/logs end point
-    r_temp = tree("logs", data = get_node_logs("nic0"))
+    r_temp = tree("logs", data=get_node_logs("nic0"))
     r_mezz.addChild(r_temp)
 
     # Add /api/sled/mezz1/fruid end point
-    r_mezz = tree("mezz1", data = get_node_mezz())
+    r_mezz = tree("mezz1", data=get_node_mezz())
     r_sled.addChild(r_mezz)
 
     # Add /api/sled/mezz1/fruid end point
-    r_temp = tree("fruid", data = get_node_fruid("nic1"))
+    r_temp = tree("fruid", data=get_node_fruid("nic1"))
     r_mezz.addChild(r_temp)
 
     # /api/sled/mezz1/sensors end point
-    r_temp = tree("sensors", data = get_node_sensors("nic1"))
+    r_temp = tree("sensors", data=get_node_sensors("nic1"))
     r_mezz.addChild(r_temp)
 
     # /api/sled/mezz1/logs end point
-    r_temp = tree("logs", data = get_node_logs("nic1"))
+    r_temp = tree("logs", data=get_node_logs("nic1"))
     r_mezz.addChild(r_temp)
 
     return r_api

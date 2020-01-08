028b34dd11390471cacd323d330cd982dd9e3017
Amithash Prasad <amithash@fb.com>
2020-01-08 15:30:26 -0800

    FBAL:Support CFM0 and CFM1 region for global, module, pfr CPLD
    Summary: Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1276
    
    Test Plan:
    Platform:FBAL
     Kernel:5.0.3
    
    root@bmc-oob:~# fw-util
    USAGE: fw-util all|FRU --version [all|COMPONENT]
           fw-util FRU --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --force --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --dump [--]COMPONENT IMAGE_PATH
    FRU        : Components
    ---------- : ----------
    bmc        : bmc rom tpm
    cpld       : glb_cfm0 glb_cfm1 mod_cfm0 mod_cfm1 pfr_cfm0 pfr_cfm1
    mb         : bios bmc(bmc:bmc) me rom(bmc:rom) vr
    nic        : nic
    ocpdbg     : mcu mcubl
    pdb        : cm cmbl
    
    Reviewed By: benwei13
    
    fbshipit-source-id: e878458b24

--- meta-facebook/meta-fbal/recipes-fbal/fw-util/files/cpld.cpp
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/cpld.cpp
@@ -15,10 +15,17 @@ using namespace std;
 #define ON_CHIP_FLASH_IP_DATA_REG        (0x00000000)
 // Dual-boot IP
 #define DUAL_BOOT_IP_BASE                (0x00100000)
-#define CFM0_START_ADDR                  (0x0004A000)
-#define CFM0_END_ADDR                    (0x0008BFFF)
-#define CFM1_START_ADDR                  (0x00008000)
-#define CFM1_END_ADDR                    (0x00049FFF)
+
+#define CFM0_10M25_START_ADDR            (0x00064000)
+#define CFM0_10M25_END_ADDR              (0x000BFFFF)
+#define CFM1_10M25_START_ADDR            (0x00008000)
+#define CFM1_10M25_END_ADDR              (0x00063FFF)
+
+#define CFM0_10M16_START_ADDR            (0x0004A000)
+#define CFM0_10M16_END_ADDR              (0x0008BFFF)
+#define CFM1_10M16_START_ADDR            (0x00008000)
+#define CFM1_10M16_END_ADDR              (0x00049FFF)
+
 
 enum {
   CFM_IMAGE_NONE = 0,
@@ -26,19 +33,59 @@ enum {
   CFM_IMAGE_2,
 };
 
-class CpldComponent : public Component {
+
+//CPLD 10M25
+class CPLD10m25cfm0 : public Component {
   string pld_name;
   uint8_t pld_type;
 
   altera_max10_attr_t attr;
   public:
-    CpldComponent(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
-      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_1, CFM0_START_ADDR, CFM0_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
+    CPLD10m25cfm0(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
+      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_1, CFM0_10M25_START_ADDR, CFM0_10M25_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
     int print_version();
     int update(string image);
 };
 
-int CpldComponent::print_version() {
+class CPLD10m25cfm1 : public Component {
+  string pld_name;
+  uint8_t pld_type;
+
+  altera_max10_attr_t attr;
+  public:
+    CPLD10m25cfm1(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
+      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_2, CFM1_10M25_START_ADDR, CFM1_10M25_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
+    int print_version();
+    int update(string image);
+};
+
+//CPLD 10M16
+class CPLD10m16cfm0 : public Component {
+  string pld_name;
+  uint8_t pld_type;
+
+  altera_max10_attr_t attr;
+  public:
+    CPLD10m16cfm0(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
+      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_1, CFM0_10M16_START_ADDR, CFM0_10M16_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
+    int print_version();
+    int update(string image);
+};
+
+class CPLD10m16cfm1 : public Component {
+  string pld_name;
+  uint8_t pld_type;
+
+  altera_max10_attr_t attr;
+  public:
+    CPLD10m16cfm1(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
+      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_2, CFM1_10M16_START_ADDR, CFM1_10M16_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
+    int print_version();
+    int update(string image);
+};
+
+//10M25 Version
+int CPLD10m25cfm0::print_version() {
   int ret;
   uint8_t ver[4];
 
@@ -58,14 +105,73 @@ int CpldComponent::print_version() {
   return 0;
 }
 
-int CpldComponent::update(string image) {
+int CPLD10m25cfm1::print_version() {
   int ret;
+  uint8_t ver[4];
 
   if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
     printf("Cannot open i2c!\n");
     return -1;
   }
 
+  ret = cpld_get_ver((uint32_t *)ver);
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("%s CPLD Version: NA\n", pld_name.c_str());
+  } else {
+    printf("%s CPLD Version: v%02x.%02x.%02x.%02x\n", pld_name.c_str(), ver[3], ver[2], ver[1], ver[0]);
+  }
+
+  return 0;
+}
+
+//10M16 Version
+int CPLD10m16cfm0::print_version() {
+  int ret;
+  uint8_t ver[4];
+
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
+    printf("Cannot open i2c!\n");
+    return -1;
+  }
+
+  ret = cpld_get_ver((uint32_t *)ver);
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("%s CPLD Version: NA\n", pld_name.c_str());
+  } else {
+    printf("%s CPLD Version: v%02x.%02x.%02x.%02x\n", pld_name.c_str(), ver[3], ver[2], ver[1], ver[0]);
+  }
+
+  return 0;
+}
+
+int CPLD10m16cfm1::print_version() {
+  int ret;
+  uint8_t ver[4];
+
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
+    printf("Cannot open i2c!\n");
+    return -1;
+  }
+
+  ret = cpld_get_ver((uint32_t *)ver);
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("%s CPLD Version: NA\n", pld_name.c_str());
+  } else {
+    printf("%s CPLD Version: v%02x.%02x.%02x.%02x\n", pld_name.c_str(), ver[3], ver[2], ver[1], ver[0]);
+  }
+
+  return 0;
+}
+
+//10M25 update
+int CPLD10m25cfm0::update(string image) {
+  int ret;
+
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) 
+    printf("Cannot open i2c!\n");
   ret = cpld_program((char *)image.c_str());
   cpld_intf_close(INTF_I2C);
   if (ret) {
@@ -75,6 +181,52 @@ int CpldComponent::update(string image) {
   return ret;
 }
 
-CpldComponent pfr("cpld", "pfr", "PFR", MAX10_10M25, 4, 0x5a);
-CpldComponent mod("cpld", "mod", "Modular", MAX10_10M16, 4, 0x55);
-CpldComponent glb("cpld", "glb", "Global", MAX10_10M16, 23, 0x55);
+int CPLD10m25cfm1::update(string image) {
+  int ret;
+
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) 
+    printf("Cannot open i2c!\n");
+  ret = cpld_program((char *)image.c_str());
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("Error Occur at updating CPLD FW!\n");
+  }
+
+  return ret;
+}
+
+//10M16 update
+int CPLD10m16cfm0::update(string image) {
+  int ret;
+
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr))
+    printf("Cannot open i2c!\n");
+  ret = cpld_program((char *)image.c_str());
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("Error Occur at updating CPLD FW!\n");
+  }
+
+  return ret;
+}
+
+int CPLD10m16cfm1::update(string image) {
+  int ret;
+
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr))
+    printf("Cannot open i2c!\n");
+  ret = cpld_program((char *)image.c_str());
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("Error Occur at updating CPLD FW!\n");
+  }
+
+  return ret;
+}
+
+CPLD10m25cfm0 pfr_cfm0("cpld", "pfr_cfm0", "PFR_CFM0", MAX10_10M25, 4, 0x5a);
+CPLD10m25cfm1 pfr_cfm1("cpld", "pfr_cfm1", "PFR_CFM1", MAX10_10M25, 4, 0x5a);
+CPLD10m16cfm0 mod_cfm0("cpld", "mod_cfm0", "MOD_CFM0", MAX10_10M16, 4, 0x55);
+CPLD10m16cfm1 mod_cfm1("cpld", "mod_cfm1", "MOD_CFM1", MAX10_10M16, 4, 0x55);
+CPLD10m16cfm0 glb_cfm0("cpld", "glb_cfm0", "GLB_CFM0", MAX10_10M16, 23, 0x55);
+CPLD10m16cfm1 glb_cfm1("cpld", "glb_cfm1", "GLB_CFM1", MAX10_10M16, 23, 0x55);

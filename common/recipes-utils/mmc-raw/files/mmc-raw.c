703592f4899967983150df6e6c827bfcc429a676
Tao Ren <taoren@fb.com>
2020-01-10 15:49:16 -0800

    mmc-raw: code refactoring
    Summary:
    restructuring "struct mmc_ioc_cmd" handling so it's easier to add more
    mmc commands.
    
    Test Plan:
    root@fboss200932661-oob:~# mmcraw -h
    Usage: mmcraw [options] <mmc-cmd> <mmc-device>
    
    Available options:
        -h|--help    - print this help message
        -v|--verbose - enable verbose logging
        -s|--secure  - secure trim/erase mmc device
        -o|--offset  - offset of the operation
        -l|--length  - length of the operation
    
    Supported mmc commands:
        trim       - send trim command to the mmc device
        erase      - send erase command to the mmc device
        read-extcsd - read Extended CSD register of the mmc device
    
    root@fboss200932661-oob:~# mmcraw -s -v trim /dev/mmcblk0
    starting 'trim' command (mmc=/dev/mmcblk0)
    total 15269888 write blocks, write block size 512
    starting secure-trim step #1: range [0, 15269887]
    starting ioctl cmd (opcode=35, arg=0, flags=0x195)
    starting ioctl cmd (opcode=36, arg=0xe8ffff, flags=0x195)
    starting ioctl cmd (opcode=38, arg=0x80000001, flags=0x49d)
    starting secure-trim step #2: range [0, 15269887]
    starting ioctl cmd (opcode=35, arg=0, flags=0x195)
    starting ioctl cmd (opcode=36, arg=0xe8ffff, flags=0x195)
    starting ioctl cmd (opcode=38, arg=0x80008000, flags=0x49d)
    'trim' command completed successfully!
    
    root@fboss200932661-oob:~# mmcraw erase /dev/mmcblk0
    total 15269888 write blocks, write block size 512
    'erase' command completed successfully!
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: b31fc4603d

--- common/recipes-utils/mmc-raw/files/mmc-raw.c
+++ common/recipes-utils/mmc-raw/files/mmc-raw.c
@@ -61,6 +61,25 @@
 #define MMC_INFO(fmt, args...)	printf(fmt, ##args)
 #define MMC_ERR(fmt, args...)	fprintf(stderr, fmt, ##args)
 
+/*
+ * Utilities to initialize struct mmc_ioc_cmd.
+ */
+#define MMC_IOC_INIT(_idata, _opcode, _arg, _flags)	\
+	do {						\
+		memset(&(_idata), 0, sizeof(_idata));	\
+		(_idata).opcode = (_opcode);		\
+		(_idata).arg = (_arg);			\
+		(_idata).flags = (_flags);		\
+	} while (0)
+
+#define MMC_IOC_SET_DATA(_idata, _data, _blocks, _blksz, _wr_flag)	\
+	do {								\
+		mmc_ioc_cmd_set_data(_idata, _data);			\
+		(_idata).blocks = (_blocks);				\
+		(_idata).blksz = (_blksz);				\
+		(_idata).write_flag = (_wr_flag);			\
+	} while (0)
+
 struct m_cmd_args {
 	const char *cmd;
 	const char *dev_path;
@@ -88,6 +107,17 @@ static struct {
 	unsigned int extcsd_loaded:1;
 	__u8 extcsd[MMC_EXTCSD_SIZE];
 } mmc_reg_cache;
+
+
+static int mmc_ioc_issue_cmd(int fd, struct mmc_ioc_cmd *idata)
+{
+	MMC_DEBUG("starting ioctl cmd " MMC_CMD_FMT "\n",
+		  idata->opcode, idata->arg, idata->flags);
+	if (ioctl(fd, MMC_IOC_CMD, idata) == -1)
+		return -1;
+
+	return 0;
+}
 
 /*
  * Check whether the device supports secure feature (defined in
@@ -198,115 +228,71 @@ static int mmc_cal_erase_range(struct m_cmd_args *args,
 	return 0;
 }
 
-/*
- * Issue CMD35/CMD36 to set ERASE_GROUP_START|END.
- * NOTE:
- *   - For devices <=2GB, <arg> defines byte address.
- *   - For devices >2GB, <arg> defines sector (512-byte) address.
- *   - Erase operation applies to Erase Groups, while Trim/Discard applies
- *     to Write Blocks.
- */
-static int mmc_set_erase_range(int fd, __u32 opcode, __u32 arg)
+static int mmc_erase_range(int fd, __u32 erase_arg, __u32 start, __u32 end)
 {
 	int ret;
 	struct mmc_ioc_cmd idata;
 
-	memset(&idata, 0, sizeof(idata));
-	idata.write_flag = 0;
-	idata.opcode = opcode;
-	idata.arg = arg;
-	idata.flags = MMC_ERASE_GROUP_FLAGS;
-	ret = ioctl(fd, MMC_IOC_CMD, &idata);
-	if (ret == -1) {
-		MMC_ERR("set erase range " MMC_CMD_FMT " failed: %s\n",
-			opcode, arg, idata.flags, strerror(errno));
+	/*
+	 * Issue CMD35/CMD36 to set ERASE_GROUP_START|END.
+	 * NOTE:
+	 *   - For devices <=2GB, <arg> defines byte address.
+	 *   - For devices >2GB, <arg> defines sector (512-byte) address.
+	 *   - Erase applies to Erase Groups, while Trim/Discard applies
+	 *     to Write Blocks.
+	 */
+	MMC_IOC_INIT(idata, MMC_ERASE_GROUP_START, start,
+		     MMC_ERASE_GROUP_FLAGS);
+	ret = mmc_ioc_issue_cmd(fd, &idata);
+	if (ret != 0) {
+		MMC_ERR("failed to set erase start " MMC_CMD_FMT ": %s\n",
+			MMC_ERASE_GROUP_START, start, MMC_ERASE_GROUP_FLAGS,
+			strerror(errno));
+		return ret;
+	}
+
+	MMC_IOC_INIT(idata, MMC_ERASE_GROUP_END, end,
+		     MMC_ERASE_GROUP_FLAGS);
+	ret = mmc_ioc_issue_cmd(fd, &idata);
+	if (ret != 0) {
+		MMC_ERR("failed to set erase end " MMC_CMD_FMT ": %s\n",
+			MMC_ERASE_GROUP_END, end, MMC_ERASE_GROUP_FLAGS,
+			strerror(errno));
+		return ret;
+	}
+
+	/*
+	 * Issue CMD38 to erase/trim/discard blocks. Erase type (erase,
+	 * trim or discard) is determined by <erase_arg>.
+	 */
+	MMC_IOC_INIT(idata, MMC_ERASE, erase_arg, MMC_ERASE_FLAGS);
+	ret = mmc_ioc_issue_cmd(fd, &idata);
+	if (ret != 0) {
+		MMC_ERR("erase command " MMC_CMD_FMT " failed: %s\n",
+			MMC_ERASE, erase_arg, 0, strerror(errno));
 		return ret;
 	}
 
 	return 0;
 }
 
-/*
- * Issue CMD38 to erase/trim/discard blocks.
- * NOTE:
- *   - Erase type (erase, trim or discard) is determined by <arg> value.
- */
-static int mmc_issue_erase(int fd, __u32 arg)
+static int mmc_do_sec_trim(int fd, __u32 start, __u32 end)
 {
 	int ret;
-	struct mmc_ioc_cmd idata;
 
-	memset(&idata, 0, sizeof(idata));
-	idata.write_flag = 0;
-	idata.opcode = MMC_ERASE;
-	idata.arg = arg;
-	idata.flags = MMC_ERASE_FLAGS;
-	ret = ioctl(fd, MMC_IOC_CMD, &idata);
-	if (ret == -1) {
-		MMC_ERR("mmc erase " MMC_CMD_FMT " failed: %s\n",
-			MMC_ERASE, arg, idata.flags, strerror(errno));
+	MMC_DEBUG("starting secure-trim step #1: range [%u, %u]\n",
+		  start, end);
+	ret = mmc_erase_range(fd, MMC_SECURE_TRIM1_ARG, start, end);
+	if (ret != 0) {
 		return ret;
 	}
 
-	return 0;
-}
-
-/*
- * TODO: fill in the function.
- */
-static int mmc_do_trim(struct m_cmd_args *args, __u32 start, __u32 end)
-{
-	int ret;
-
-	MMC_DEBUG("starting trim, range [%u, %u]\n", start, end);
-
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_START, start);
-	if (ret != 0)
+	MMC_DEBUG("starting secure-trim step #2: range [%u, %u]\n",
+		  start, end);
+	ret = mmc_erase_range(fd, MMC_SECURE_TRIM2_ARG, start, end);
+	if (ret != 0) {
 		return ret;
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_END, end);
-	if (ret != 0)
-		return ret;
-	ret = mmc_issue_erase(args->dev_fd, MMC_TRIM_ARG);
-	if (ret != 0)
-		return -1;
-
-	MMC_DEBUG("trim completed\n");
-	return 0;
-}
-
-static int mmc_do_sec_trim(struct m_cmd_args *args, __u32 start, __u32 end)
-{
-	int ret;
-
-	MMC_DEBUG("starting secure trim, range [%u, %u]\n", start, end);
-
-	/*
-	 * Secure trim step 1.
-	 */
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_START, start);
-	if (ret != 0)
-		return ret;
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_END, end);
-	if (ret != 0)
-		return ret;
-	ret = mmc_issue_erase(args->dev_fd, MMC_SECURE_TRIM1_ARG);
-	if (ret != 0)
-		return -1;
-	MMC_DEBUG("secure trim step #1 completed\n");
-
-	/*
-	 * Secure trim step 2.
-	 */
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_START, start);
-	if (ret != 0)
-		return -1;
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_END, end);
-	if (ret != 0)
-		return -1;
-	ret = mmc_issue_erase(args->dev_fd, MMC_SECURE_TRIM2_ARG);
-	if (ret != 0)
-		return ret;
-	MMC_DEBUG("secure trim step #2 completed\n");
+	}
 
 	return 0;
 }
@@ -316,25 +302,25 @@ static int mmc_trim_cmd(struct m_cmd_args *args)
 	int ret;
 	__u32 start, end;
 
-	if (mmc_flags.secure && !mmc_is_secure_supported(args->dev_fd)) {
-		MMC_ERR("secure trim failed: feature not supported\n");
-		return -1;
-	}
-
 	/*
 	 * Determine the trim range.
 	 */
-	if (mmc_cal_erase_range(args, &start, &end) != 0) {
+	if (mmc_cal_erase_range(args, &start, &end) != 0)
 		return -1;
-	}
-	if (start >= end) {
+	if (start >= end)
 		return 0;	/* Nothing needs to be done. */
-	}
 
-	if (mmc_flags.secure)
-		ret = mmc_do_sec_trim(args, start, end);
-	else
-		ret = mmc_do_trim(args, start, end);
+	if (mmc_flags.secure) {
+		if (!mmc_is_secure_supported(args->dev_fd)) {
+			MMC_ERR("Error: secure feature not supported\n");
+			return -1;
+		}
+
+		ret = mmc_do_sec_trim(args->dev_fd, start, end);
+	} else {
+		MMC_DEBUG("starting trim: range [%u, %u]\n", start, end);
+		ret = mmc_erase_range(args->dev_fd, MMC_TRIM_ARG, start, end);
+	}
 
 	return ret;
 }
@@ -342,66 +328,39 @@ static int mmc_trim_cmd(struct m_cmd_args *args)
 static int mmc_erase_cmd(struct m_cmd_args *args)
 {
 	int ret;
-	const char *erase_type;
-	__u32 start, end, erase_arg;
+	__u32 start, end;
 
-	if (mmc_flags.secure && !mmc_is_secure_supported(args->dev_fd)) {
-		MMC_ERR("secure erase failed: feature not supported\n");
+	if (mmc_cal_erase_range(args, &start, &end) != 0)
 		return -1;
-	}
+	if (start >= end)
+		return 0;       /* Nothing needs to be done. */
 
-	/*
-	 * Set up erase range.
-	 */
-	if (mmc_cal_erase_range(args, &start, &end) != 0) {
-		return -1;
-	}
-	if (start >= end) {
-		return 0;	/* Nothing needs to be done. */
-	}
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_START, start);
-	if (ret != 0)
-		return ret;
-	ret = mmc_set_erase_range(args->dev_fd, MMC_ERASE_GROUP_END, end);
-	if (ret != 0)
-		return ret;
-
-	/*
-	 * Send erase command to the device.
-	 */
 	if (mmc_flags.secure) {
-		erase_arg = MMC_SECURE_ERASE_ARG;
-		erase_type = "secure erase";
-	} else {
-		erase_arg = MMC_ERASE_ARG;
-		erase_type = "erase";
-	}
-	MMC_DEBUG("start %s: range [%u, %u]\n", erase_type, start, end);
-	ret = mmc_issue_erase(args->dev_fd, erase_arg);
-	if (ret != 0)
-		return -1;
+		if (!mmc_is_secure_supported(args->dev_fd)) {
+			MMC_ERR("Error: secure feature not supported\n");
+			return -1;
+		}
 
-	MMC_DEBUG("%s completed\n", erase_type);
-	return 0;
+		MMC_DEBUG("starting secure-erase: range [%u, %u]\n",
+			  start, end);
+		ret = mmc_erase_range(args->dev_fd, MMC_SECURE_ERASE_ARG,
+				      start, end);
+	} else {
+		MMC_DEBUG("starting erase: range [%u, %u]\n", start, end);
+		ret = mmc_erase_range(args->dev_fd, MMC_ERASE_ARG, start, end);
+	}
+
+	return ret;
 }
 
 static int mmc_read_extcsd(int fd, __u8 *extcsd)
 {
-	int ret;
 	struct mmc_ioc_cmd idata;
 
-	memset(&idata, 0, sizeof(idata));
 	memset(extcsd, 0, sizeof(__u8) * MMC_EXTCSD_SIZE);
-	idata.opcode = MMC_SEND_EXT_CSD;
-	idata.blksz = MMC_EXTCSD_SIZE;
-	idata.blocks = 1;
-	mmc_ioc_cmd_set_data(idata, extcsd);
-
-	ret = ioctl(fd, MMC_IOC_CMD, &idata);
-	if (ret < 0)
-		return -1;
-
-	return 0;
+	MMC_IOC_INIT(idata, MMC_SEND_EXT_CSD, 0, 0);
+	MMC_IOC_SET_DATA(idata, extcsd, 1, MMC_EXTCSD_SIZE, 0);
+	return mmc_ioc_issue_cmd(fd, &idata);
 }
 
 static int mmc_load_extcsd(struct m_cmd_args *cmd_args)
@@ -545,8 +504,8 @@ int main(int argc, char **argv)
 		{"help",	no_argument,		NULL,	'h'},
 		{"verbose",	no_argument,		NULL,	'v'},
 		{"secure",	no_argument,		NULL,	's'},
-		{"offset",	required_argument,	NULL,	's'},
-		{"length",	required_argument,	NULL,	's'},
+		{"offset",	required_argument,	NULL,	'o'},
+		{"length",	required_argument,	NULL,	'l'},
 		{NULL,		0,			NULL,	0},
 	};
 
@@ -609,14 +568,18 @@ int main(int argc, char **argv)
 	/*
 	 * Launch command handler.
 	 */
-	MMC_DEBUG("launching <%s> command (mmc=%s)\n",
+	MMC_DEBUG("starting '%s' command (mmc=%s)\n",
 		  cmd_args.cmd, cmd_args.dev_path);
 	ret = cmd_info->func(&cmd_args);
+
 	close(cmd_args.dev_fd);
 
 	if (ret == 0) {
-		MMC_INFO("mmc <%s> command completed successfully!\n",
+		MMC_INFO("'%s' command completed successfully!\n",
 			 cmd_args.cmd);
+	} else {
+		MMC_ERR("'%s' command failed with errors!\n", cmd_args.cmd);
 	}
+
 	return ret;
 }

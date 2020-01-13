c9b2ea0b8bb6bd5bb9c733fbe44108e16703cb54
Amithash Prasad <amithash@fb.com>
2020-01-13 12:26:39 -0800

    fbal: fw-util change to use common libvr (#1285)
    Summary:
    1. as title
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1285
    
    Test Plan:
    Build and test pass on fbal system
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TPS53688\ \@\ PMBus\ Address\ 112d\ Project_1D93_F0C_CPU1_VCCIN_VCCSA_20190904.csv
    Update VR: VR_CPU1_VCCIN/VCCSA
    File CRC : 1D93
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TPS53688\ \@\ PMBus\ Address\ 114d\ Project_9C07_F0C_CPU1_VCCIO_20190904.csv
    Update VR: VR_CPU1_VCCIO
    File CRC : 9C07
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TPS53688\ \@\ PMBus\ Address\ 96d\ Project_C8B2_F0C_CPU0_VCCIN_VCCSA_20190904.csv
    Update VR: VR_CPU0_VCCIN/VCCSA
    File CRC : C8B2
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TPS53688\ \@\ PMBus\ Address\ 98d\ Project_4926_F0C_CPU0_VCCIO_20190904.csv
    Update VR: VR_CPU0_VCCIO
    File CRC : 4926
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TTPS53688\ \@\ PMBus\ Address\ 102d\ Project_4E60_F0C_CPU0_VDDQ_ABC_20190828.csv
    Update VR: VR_CPU0_VDDQ_ABC
    File CRC : 4E60
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TTPS53688\ \@\ PMBus\ Address\ 104d\ Project_D70E_F0C_CPU0_VDDQ_DEF_20190828.csv
    Update VR: VR_CPU0_VDDQ_DEF
    File CRC : D70E
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TTPS53688\ \@\ PMBus\ Address\ 108d\ Project_8247_F0C_CPU1_VDDQ_DEF_20190828.csv
    Update VR: VR_CPU1_VDDQ_DEF
    File CRC : 8247
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/TTPS53688\ \@\ PMBus\ Address\ 118d\ Project_9B41_F0C_CPU1_VDDQ_ABC_20190828.csv
    Update VR: VR_CPU1_VDDQ_ABC
    File CRC : 9B41
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --update --vr /tmp/F0C_PVNN_1V05pch_initial_rev00_BCBA1E93_20191012.psf
    Update VR: VR_PCH_PVNN/P1V05
    File CRC : BCBA1E93
    Remaining writes: 6
    updated: 100 %
    Upgrade of mb : vr succeeded
    
    root@bmc-oob:~# fw-util mb --version vr
    VR_PCH_PVNN/P1V05 Version: BCBA1E93
    VR_CPU0_VCCIN/VCCSA Version: C8B2
    VR_CPU0_VCCIO Version: 4926
    VR_CPU0_VDDQ_ABC Version: 4E60
    VR_CPU0_VDDQ_DEF Version: D70E
    VR_CPU1_VCCIN/VCCSA Version: 1D93
    VR_CPU1_VCCIO Version: 9C07
    VR_CPU1_VDDQ_ABC Version: 9B41
    VR_CPU1_VDDQ_DEF Version: 8247
    
    Reviewed By: amithash
    
    fbshipit-source-id: d2cd080eb3

--- meta-facebook/meta-fbal/recipes-fbal/fw-util/files/vr.cpp
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/vr.cpp
@@ -1,6 +1,9 @@
 #include "fw-util.h"
 #include <openbmc/pal.h>
-#include <facebook/vr.h>
+#include <openbmc/vr.h>
+
+extern struct vr_info *dev_list;
+extern int dev_list_count;
 
 using namespace std;
 
@@ -14,72 +17,39 @@ class VrComponent : public Component {
 
 int VrComponent::print_version() {
   char ver[MAX_VER_STR_LEN] = {0};
+  int i;
 
-  if (vr_fw_version(VR_PCH_PVNN, ver)) {
-    cout << "VR_PCH_[PVNN, P1V05] Version: NA" << endl;
-  } else {
-    cout << "VR_PCH_[PVNN, P1V05] " << string(ver) << endl;
+  if (vr_probe() < 0) {
+    cout << "VR probe failed!" << endl;
+    return -1;
   }
 
-  if (vr_fw_version(VR_CPU0_VCCIN, ver)) {
-    cout << "VR_CPU0_[VCCIN, VCCSA] Version: NA" << endl;
-  } else {
-    cout << "VR_CPU0_[VCCIN, VCCSA] " << string(ver) << endl;
+  for (i = 0; i < dev_list_count; i++) {
+    if (vr_fw_version(i, NULL, ver)) {
+      cout << string(dev_list[i].dev_name) << " Version: NA" << endl;
+    } else {
+      cout << string(dev_list[i].dev_name) << " Version: " << string(ver) << endl;
+    }
   }
 
-  if (vr_fw_version(VR_CPU0_VCCIO, ver)) {
-    cout << "VR_CPU0_VCCIO Version: NA" << endl;
-  } else {
-    cout << "VR_CPU0_VCCIO " << string(ver) << endl;
-  }
-
-  if (vr_fw_version(VR_CPU0_VDDQ_ABC, ver)) {
-    cout << "VR_CPU0_VDDQ_ABC Version: NA" << endl;
-  } else {
-    cout << "VR_CPU0_VDDQ_ABC " << string(ver) << endl;
-  }
-
-  if (vr_fw_version(VR_CPU0_VDDQ_DEF, ver)) {
-    cout << "VR_CPU0_VDDQ_DEF Version: NA" << endl;
-  } else {
-    cout << "VR_CPU0_VDDQ_DEF " << string(ver) << endl;
-  }
-
-  if (vr_fw_version(VR_CPU1_VCCIN, ver)) {
-    cout << "VR_CPU1_[VCCIN, VCCSA] Version: NA" << endl;
-  } else {
-    cout << "VR_CPU1_[VCCIN, VCCSA] " << string(ver) << endl;
-  }
-
-  if (vr_fw_version(VR_CPU1_VCCIO, ver)) {
-    cout << "VR_CPU1_VCCIO Version: NA" << endl;
-  } else {
-    cout << "VR_CPU1_VCCIO " << string(ver) << endl;
-  }
-
-  if (vr_fw_version(VR_CPU1_VDDQ_ABC, ver)) {
-    cout << "VR_CPU1_VDDQ_ABC Version: NA" << endl;
-  } else {
-    cout << "VR_CPU1_VDDQ_ABC " << string(ver) << endl;
-  }
-
-  if (vr_fw_version(VR_CPU1_VDDQ_DEF, ver)) {
-    cout << "VR_CPU1_VDDQ_DEF Version: NA" << endl;
-  } else {
-    cout << "VR_CPU1_VDDQ_DEF " << string(ver) << endl;
-  }
-
+  vr_remove();
   return 0;
 }
 
 int VrComponent::update(string image) {
-  int ret = 0;
+  int ret;
 
-  ret = vr_fw_update((char *)image.c_str());
-  if (ret < 0) {
-    printf("ERROR: VR Firmware update failed!\n");
+  if (vr_probe() < 0) {
+    cout << "VR probe failed!" << endl;
+    return -1;
   }
 
+  ret = vr_fw_update(NULL, (char *)image.c_str());
+  if (ret < 0) {
+    cout << "ERROR: VR Firmware update failed!" << endl;
+  }
+
+  vr_remove();
   return ret;
 }
 

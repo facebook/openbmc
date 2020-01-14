85c2b9c991edc95837d84f75a226a76f825a2da1
Patrick Williams <patrickw3@fb.com>
2020-01-14 08:59:04 -0800

    fbsp: fix the issue that cannot sync time from ME (#892)
    Summary:
    1. ME cannot respond the SEL time via IPMI in that time while BMC runs the sync_date script.
       Thus, BMC does not get the correct time from ME then keep the system time to BMC build time.
    2. Reference from fbttn to add a retry mechanism to get SEL time from ME.
       The max retry count we set it to 5. Expect to wait for ME ready and get the correct system time from ME within 5 seconds.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/892
    
    Test Plan:
    Build and test PASS on sonorapass EVT MB.
    1. Set the system time in Host.
        [root@localhost ~]# date -s "20200114 16:36:00"
        Tue Jan 14 16:36:00 CST 2020
        [root@localhost ~]# hwclock -w
        [root@localhost ~]#
    2. AC cycle system.
        root@bmc-oob:~# power-util sled-cycle
    3. Check the system time of BMC and Host.
        [BMC logs]
            root@bmc-oob:~# log-util all --print
            FRU# FRU_NAME TIME_STAMP             APP_NAME         MESSAGE
            2020 Jan 14 02:05:18 log-util: User cleared all logs
    
            0    all      2020-01-14 02:05:24    power-util       SLED_CYCLE starting...
            0    all      2020-01-14 00:39:34    healthd          SLED Powered OFF at Tue Jan 14 02:05:24 2020
            0    all      2020-01-14 00:41:13    healthd          SLED Powered ON at Tue Jan 14 00:37:58 2020
            root@bmc-oob:~#
        [Host time]
            [root@localhost ~]# date
            Tue Jan 14 16:42:57 CST 2020
            [root@localhost ~]#
        [BMC time]
            root@bmc-oob:~# ipmitool -b 5 -t 0x2c sel time get; hwclock; date
            01/14/2020 08:43:05
            2020-01-14 00:43:04.330338-08:00
            Tue Jan 14 00:43:05 PST 2020
            root@bmc-oob:~#
        [BMC sync_date logs]
            root@bmc-oob:~# cat /var/log/messages | grep sync_date
             2020 Jan 14 00:23:03 bmc-oob. user.info sonorapass-e768fecd0c-dirty: sync_date: Get SEL time from ME failed, syncing up BMC time with server failed for 1 times.
             2020 Jan 14 00:39:31 bmc-oob. user.info sonorapass-e768fecd0c-dirty: sync_date: Syncing up BMC time with server...
             2020 Jan 14 00:39:31 bmc-oob. user.info sonorapass-e768fecd0c-dirty: sync_date: Date: Tue Jan 14 00:39:31 PST 2020
            root@bmc-oob:~#
    4. Reboot BMC.
        root@bmc-oob:~# reboot
    5. Check the system time of BMC and Host.
        [BMC logs]
            root@bmc-oob:~# log-util all --print
            FRU# FRU_NAME TIME_STAMP             APP_NAME         MESSAGE
            2020 Jan 14 00:43:52 log-util: User cleared all logs
    
            0    all      2020-01-14 00:47:08    healthd          BMC Reboot detected
            root@bmc-oob:~#
        [Host time]
            [root@localhost ~]# date
            Tue Jan 14 16:47:37 CST 2020
            [root@localhost ~]#
        [BMC time]
            root@bmc-oob:~# ipmitool -b 5 -t 0x2c sel time get; hwclock; date
            01/14/2020 08:47:44
            2020-01-14 00:47:43.630257-08:00
            Tue Jan 14 00:47:44 PST 2020
            root@bmc-oob:~#
        [BMC sync_date logs]
            root@bmc-oob:~# cat /var/log/messages | grep sync_date
             2020 Jan 14 00:47:03 bmc-oob. user.info sonorapass-e768fecd0c-dirty: sync_date: Get SEL time from ME failed, syncing up BMC time with server failed for 1 times.
             2020 Jan 14 00:47:05 bmc-oob. user.info sonorapass-e768fecd0c-dirty: sync_date: Syncing up BMC time with server...
             2020 Jan 14 00:47:05 bmc-oob. user.info sonorapass-e768fecd0c-dirty: sync_date: Date: Tue Jan 14 00:47:05 PST 2020
            root@bmc-oob:~#
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: cf8e5baf17

--- meta-facebook/meta-fbsp/recipes-fbsp/plat-utils/files/sync_date.sh
+++ meta-facebook/meta-fbsp/recipes-fbsp/plat-utils/files/sync_date.sh
@@ -19,18 +19,31 @@
 #
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
+MAX_RETRY_CNT=5
 
 # Sync BMC's date with the server
 sync_date()
 {
   # if no ntp peer has been declared the system peer
   if ! /usr/sbin/ntpq -p | grep '^\*' > /dev/null ; then
-    echo Syncing up BMC time with server...
-    # Use standard IPMI command 'get-sel-time' to read RTC time
-    output=$(/usr/local/bin/me-util 0x28 0x48)
-    [ ${#output} != 12 ] && exit
-    date -s @$((16#$(echo "$output" | awk '{print $4$3$2$1}')))
-    test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop
+    retry=0
+    while [ $retry -lt $MAX_RETRY_CNT ]
+    do
+      # Use standard IPMI command 'get-sel-time' to read RTC time
+      output=$(/usr/local/bin/me-util 0x28 0x48)
+      if [ ${#output} == 12 ]; then
+        ts=$(date -s @$((16#$(echo "$output" | awk '{print $4$3$2$1}'))))
+        test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop
+
+        logger -s -p user.info -t sync_date "Syncing up BMC time with server..."
+        logger -s -p user.info -t sync_date "Date: $ts"
+        return
+      else
+        retry=$((retry+1))
+        logger -s -p user.info -t sync_date "Get SEL time from ME failed, syncing up BMC time with server failed for $retry times."
+        sleep 1
+      fi
+    done
   fi
 }
 

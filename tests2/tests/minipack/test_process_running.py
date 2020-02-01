bdc4549c9df0dd876f0a1451b9d0a8d11a69dab2
Tao Ren <taoren@fb.com>
2020-01-31 17:09:20 -0800

    tests2: minipack: fix running_process test
    Summary:
    "--address-prefix-len 64" option is added to "dhclient" command for
    yocto-warrior platforms: see commit fa02ab1242c1 ("dhcpv6: warrior:
    change default IPv6 prefix to 64") for details. So let's update the
    command string in test script accordingly.
    
    Test Plan:
    root@dhcp6-2620-10d-c082-9002-0-0-0-1b8f:~#
    /usr/local/bin/tests2/cit_runner.py --run-test
    tests.minipack.test_process_running.ProcessRunningTest
    test_installed_processes
    (tests.minipack.test_process_running.ProcessRunningTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.464s
    
    OK
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: de135d482a

--- tests2/tests/minipack/test_process_running.py
+++ tests2/tests/minipack/test_process_running.py
@@ -25,7 +25,10 @@ from common.base_process_running_test import BaseProcessRunningTest
 class ProcessRunningTest(BaseProcessRunningTest, unittest.TestCase):
     def set_processes(self):
         self.expected_process = [
-            "dhclient -6 -d -D LL -pf /var/run/dhclient6.eth0.pid eth0",
+            (
+                "dhclient -6 -d -D LL --address-prefix-len 64 "
+                + "-pf /var/run/dhclient6.eth0.pid eth0"
+            ),
             "dhclient -pf /var/run/dhclient.eth0.pid eth0",
             "front-paneld",
             "fscd",

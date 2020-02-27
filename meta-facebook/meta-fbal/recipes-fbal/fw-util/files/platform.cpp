fd5a9a241caf38d70485d3d2ec7b60fd812506d8
Amithash Prasad <amithash@fb.com>
2020-02-26 16:59:30 -0800

    fbal: fw-tuil: get nic1 firmware version through package=1/channel=0 (#1421)
    Summary:
    1. According to OCP NIC 3.0 spec, nic1 should use package=1 since OCP_V3_2_SLOT_ID0=1
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1421
    
    Test Plan:
    Build and test pass on fbal
    ps: with BRCM NIC test firmware
    
    root@bmc-oob:\~# fw-util nic --version
    Mellanox NIC firmware version: 16.26.1040
    Broadcom NIC firmware version: 216.0.254.1
    
    root@bmc-oob:\~# fw-util nic --version nic0
    Mellanox NIC firmware version: 16.26.1040
    root@bmc-oob:\~# fw-util nic --version nic1
    Broadcom NIC firmware version: 216.0.254.1
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 92fbb58528

--- meta-facebook/meta-fbal/recipes-fbal/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/platform.cpp
@@ -7,8 +7,8 @@ AliasComponent bmc("mb", "bmc", "bmc", "bmc");
 AliasComponent rom("mb", "rom", "bmc", "rom");
 BiosComponent bios("mb", "bios", "\"pnor\"", "F0C_");
 
-NicExtComponent nic0("nic", "nic0", "nic0_fw_ver", FRU_NIC0, 0);  // fru_name, component, kv, fru_id, eth_index
-NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1);
+NicExtComponent nic0("nic", "nic0", "nic0_fw_ver", FRU_NIC0, 0, 0x00);  // fru_name, component, kv, fru_id, eth_index, ch_id
+NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1, 0x20);
 
 UsbDbgComponent usbdbg("ocpdbg", "mcu", "F0C", 0, 0x60, false);
 UsbDbgBlComponent usbdbgbl("ocpdbg", "mcubl", 0, 0x60, 0x02);  // target ID of bootloader = 0x02

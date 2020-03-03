91b59e1795059307a6f03002f788dd3cd1830699
Patrick Williams <patrickw3@fb.com>
2020-03-03 10:30:57 -0800

    fbsp: Modify regular expression used in bios image checking. (#949)
    Summary:
    1. Modify regular expression used in bios image checking when bios update.
    2. Now the tags 'F0G_.*', 'SP[EDPM][0-9]{2}.*' and 'SP_PPO.*' are allowed to update BIOS image without '--force' option.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/949
    
    Test Plan:
    Build and test PASS in Sonorapass EVT system.
    1. BIOS update with image tag 'SP[EDPM][0-9]{2}.*' and 'F0G_.*'.
    (The image with 'SP[EDPM][0-9]{2}.*' tag is test image. Because there is no 'SP' tag in current official firmware)
    
    root@bmc-oob:~# fw-util mb --version bios
    BIOS Version: SPE03
    root@bmc-oob:~# fw-util mb --update bios /mnt/emmc/FW/F0G_1A06.BIN
    Erasing block: 1024/1024 (100%)
    Writing kb: 65536/65536 (100%)
    Verifying kb: 65536/65536 (100%)
    Upgrade of mb : bios succeeded
    
    === After cycle ===
    root@bmc-oob:~# fw-util mb --version bios
    BIOS Version: F0G_1A06
    
    root@bmc-oob:~# fw-util mb --version bios
    BIOS Version: F0G_1A06
    root@bmc-oob:~# fw-util mb --update bios /mnt/emmc/FW/SPR03.01.bin
    Erasing block: 1024/1024 (100%)
    Writing kb: 65536/65536 (100%)
    Verifying kb: 65536/65536 (100%)
    Upgrade of mb : bios succeeded
    
    === After cycle ===
    root@bmc-oob:~# fw-util mb --version bios
    BIOS Version: SPE03.01
    
    2. BIOS update with invalid image.
    root@bmc-oob:~# fw-util mb --update bios /mnt/emmc/FW/flash-sonorapass-v2020.06.0
    Invalid image. Stopping the update!
    Upgrade of mb : bios failed
    
    Reviewed By: benwei13
    
    fbshipit-source-id: b130136c10

--- meta-facebook/meta-fbsp/recipes-fbsp/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbsp/recipes-fbsp/fw-util/files/platform.cpp
@@ -3,7 +3,7 @@
 #include "usbdbg.h"
 #include "nic_ext.h"
 
-BiosComponent bios("mb", "bios", "\"pnor\"", "F0G_|SP");
+BiosComponent bios("mb", "bios", "\"pnor\"", "^F0G_.*$|^SP[EDPM][0-9]{2}.*$|^SP_PPO.*$");
 
 NicExtComponent nic0("nic", "nic0", "nic0_fw_ver", FRU_NIC0, 0);  // fru_name, component, kv, fru_id, eth_index
 NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1);

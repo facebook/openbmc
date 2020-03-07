2f70314cb30ae328db1efb290b90c9cfff622496
Amithash Prasad <amithash@fb.com>
2020-03-06 17:55:05 -0800

    angelslanding: Adopt common framework for BIOS upgrade
    Summary:
    Provide the correct configuration to BiosComponent and remove
    duplicated code in pal_fw_update_*.
    
    Test Plan: TBD
    
    Reviewed By: williamspatrick, benwei13
    
    fbshipit-source-id: 7783c64526

--- meta-facebook/meta-fbal/recipes-fbal/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/platform.cpp
@@ -5,7 +5,7 @@
 
 AliasComponent bmc("mb", "bmc", "bmc", "bmc");
 AliasComponent rom("mb", "rom", "bmc", "rom");
-BiosComponent bios("mb", "bios", "\"pnor\"", "F0C_");
+BiosComponent bios("mb", "bios", "pnor", "spi0.0", "FM_BIOS_SPI_BMC_CTRL", true, "F0C_");
 
 NicExtComponent nic0("nic", "nic0", "nic0_fw_ver", FRU_NIC0, 0, 0x00);  // fru_name, component, kv, fru_id, eth_index, ch_id
 NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1, 0x20);

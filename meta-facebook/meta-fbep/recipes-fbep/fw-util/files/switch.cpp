02667a52b39e21ea05e7db683d4104acc239996a
Amithash Prasad <amithash@fb.com>
2020-03-09 11:01:53 -0700

    emeraldpools: Extend GPIOSwitchedSPIMTDComponent to force-update switch
    Summary:
    A lot of code in pal_fw_update_prepare/finalize is taken care of by
    GPIOSwitchedSPIMTDComponent. Hence inherit from it to use it for
    the forced update of PAX Switch firmware.
    
    Test Plan: #doitlive
    
    Reviewed By: williamspatrick, benwei13
    
    fbshipit-source-id: faa4af1197

--- meta-facebook/meta-fbep/recipes-fbep/fw-util/files/switch.cpp
+++ meta-facebook/meta-fbep/recipes-fbep/fw-util/files/switch.cpp
@@ -1,17 +1,17 @@
 #include "fw-util.h"
+#include "spiflash.h"
 #include <openbmc/pal.h>
 
 using namespace std;
 
-class PAXComponent : public Component {
+class PAXComponent : public GPIOSwitchedSPIMTDComponent {
   uint8_t _paxid;
-  std::string _mtd_name;
   public:
-    PAXComponent(string fru, string comp, uint8_t paxid, std::string mtd)
-      : Component(fru, comp), _paxid(paxid), _mtd_name(mtd) {}
-    int print_version();
-    int update(string image);
-    int fupdate(string image);
+    PAXComponent(string fru, string comp, uint8_t paxid, std::string shadow)
+      : GPIOSwitchedSPIMTDComponent(fru, comp, "switch0", "spi2.0", shadow, true), _paxid(paxid) {}
+    int print_version() override;
+    int update(string image) override;
+    int fupdate(string image) override;
 };
 
 int PAXComponent::print_version()
@@ -36,50 +36,10 @@ int PAXComponent::update(string image)
 
 int PAXComponent::fupdate(string image)
 {
-  int ret;
-  int mtdno, found = 0;
-  char line[128], name[32];
-  FILE *fp;
-
-  // TODO:
-  // 	Check fwimg before force update
-  //
-  ret = pal_fw_update_prepare(FRU_MB, _component.c_str());
-  if (ret) {
-    return -1;
-  }
-
-  fp = fopen("/proc/mtd", "r");
-  if (fp) {
-    while (fgets(line, sizeof(line), fp)) {
-      if (sscanf(line, "mtd%d: %*x %*x %s", &mtdno, name) == 2) {
-        if (!strcmp(_mtd_name.c_str(), name)) {
-          found = 1;
-          break;
-        }
-      }
-    }
-    fclose(fp);
-  }
-
-  if (found) {
-    snprintf(line, sizeof(line), "flashcp -v %s /dev/mtd%d", image.c_str(), mtdno);
-    ret = system(line);
-    if (WIFEXITED(ret) && (WEXITSTATUS(ret) == 0))
-      ret = FW_STATUS_SUCCESS;
-    else
-      ret = FW_STATUS_FAILURE;
-
-  } else {
-    ret = FW_STATUS_FAILURE;
-  }
-
-  ret = pal_fw_update_finished(FRU_MB, _component.c_str(), ret);
-
-  return ret;
+  return GPIOSwitchedSPIMTDComponent::update(image);
 }
 
-PAXComponent pax0("mb", "pax0", 0, "\"switch0\"");
-PAXComponent pax1("mb", "pax1", 1, "\"switch0\"");
-PAXComponent pax2("mb", "pax2", 2, "\"switch0\"");
-PAXComponent pax3("mb", "pax3", 3, "\"switch0\"");
+PAXComponent pax0("mb", "pax0", 0, "SEL_FLASH_PAX0");
+PAXComponent pax1("mb", "pax1", 1, "SEL_FLASH_PAX1");
+PAXComponent pax2("mb", "pax2", 2, "SEL_FLASH_PAX2");
+PAXComponent pax3("mb", "pax3", 3, "SEL_FLASH_PAX3");

6cf2ea97946f33bb0f6782cf3d42a7b47b2c2ce2
Amithash Prasad <amithash@fb.com>
2020-03-11 10:54:00 -0700

    fbal: fw-util: simplify cpld component definition (#1466)
    Summary:
    simplify cpld component class definition
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1466
    
    Test Plan:
    Build and test pass on fbal
    
    root@bmc-oob:~# fw-util cpld --update --pfr_cfm1 /tmp/f0c_main_cpld_v1_05_non_pfr_al+ep_cfm1_auto.rpd_en
    Singed Key Match
    rpd file size = 376832 bytes.
    OnChip Flash Status = 0xFFFFFC18.
    image type = 0x2.
    Max10 Unprotect Sector 3.
    Max10 Unprotect Sector 4.
    Max10 Erase Sector 3
    Erase sector SUCCESS.
    Max10 Erase Sector 4
    Erase sector SUCCESS.
    Max10 Erase Sector 0
    Erase sector SUCCESS.
    Progress: Addr: 0x63FFC  [100 %]
     Done!
    Upgrade of cpld : pfr_cfm1 succeeded
    
    root@bmc-oob:~# fw-util cpld --update --pfr_cfm0 /tmp/f0c_main_cpld_v1_05_non_pfr_al+ep_cfm0_auto.rpd_en
    Singed Key Match
    rpd file size = 376832 bytes.
    OnChip Flash Status = 0xFFFFFC00.
    image type = 0x1.
    Max10 Unprotect Sector 5.
    Max10 Erase Sector 5
    Erase sector SUCCESS.
    Max10 Erase Sector 0
    Erase sector SUCCESS.
    Progress: Addr: 0xBFFFC  [100 %]
     Done!
    Upgrade of cpld : pfr_cfm0 succeeded
    
    root@bmc-oob:~# fw-util cpld --update --mod_cfm1 /tmp/f0c_mod_cpld_v1_03_cfm1_auto.rpd_en
    Singed Key Match
    rpd file size = 270336 bytes.
    OnChip Flash Status = 0xFFFFFC18.
    image type = 0x2.
    Max10 Unprotect Sector 3.
    Max10 Unprotect Sector 4.
    Max10 Erase Sector 3
    Erase sector SUCCESS.
    Max10 Erase Sector 4
    Erase sector SUCCESS.
    Max10 Erase Sector 0
    Erase sector SUCCESS.
    Progress: Addr: 0x49FFC  [100 %]
     Done!
    Upgrade of cpld : mod_cfm1 succeeded
    
    root@bmc-oob:~# fw-util cpld --update --mod_cfm0 /tmp/f0c_mod_cpld_v1_03_cfm0_auto.rpd_en
    Singed Key Match
    rpd file size = 270336 bytes.
    OnChip Flash Status = 0xFFFFFC18.
    image type = 0x1.
    Max10 Unprotect Sector 5.
    Max10 Erase Sector 5
    Erase sector SUCCESS.
    Max10 Erase Sector 0
    Erase sector SUCCESS.
    Progress: Addr: 0x8BFFC  [100 %]
     Done!
    Upgrade of cpld : mod_cfm0 succeeded
    
    root@bmc-oob:~# fw-util cpld --update --glb_cfm1 /tmp/f0c_global_cpld_v1_03_cfm1_auto.rpd_en
    Singed Key Match
    rpd file size = 270336 bytes.
    OnChip Flash Status = 0xFFFFFC18.
    image type = 0x2.
    Max10 Unprotect Sector 3.
    Max10 Unprotect Sector 4.
    Max10 Erase Sector 3
    Erase sector SUCCESS.
    Max10 Erase Sector 4
    Erase sector SUCCESS.
    Max10 Erase Sector 0
    Erase sector SUCCESS.
    Progress: Addr: 0x49FFC  [100 %]
     Done!
    Upgrade of cpld : glb_cfm1 succeeded
    
    root@bmc-oob:~# fw-util cpld --update --glb_cfm0 /tmp/f0c_global_cpld_v1_03_cfm0_auto.rpd_en
    Singed Key Match
    rpd file size = 270336 bytes.
    OnChip Flash Status = 0xFFFFFC00.
    image type = 0x1.
    Max10 Unprotect Sector 5.
    Max10 Erase Sector 5
    Erase sector SUCCESS.
    Max10 Erase Sector 0
    Erase sector SUCCESS.
    Progress: Addr: 0x8BFFC  [100 %]
     Done!
    Upgrade of cpld : glb_cfm0 succeeded
    
    root@bmc-oob:~# fw-util cpld --version
    GLB_CFM0 CPLD Version: v00.00.01.03
    GLB_CFM1 CPLD Version: v00.00.01.03
    MOD_CFM0 CPLD Version: v00.00.01.03
    MOD_CFM1 CPLD Version: v00.00.01.03
    PFR_CFM0 CPLD Version: v00.00.01.05
    PFR_CFM1 CPLD Version: v00.00.01.05
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 85cdd55160

--- meta-facebook/meta-fbal/recipes-fbal/fw-util/files/cpld.cpp
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/cpld.cpp
@@ -26,67 +26,84 @@ using namespace std;
 #define CFM1_10M16_START_ADDR            (0x00008000)
 #define CFM1_10M16_END_ADDR              (0x00049FFF)
 
-
 enum {
   CFM_IMAGE_NONE = 0,
   CFM_IMAGE_1,
   CFM_IMAGE_2,
 };
 
-
-//CPLD 10M25
-class CPLD10m25cfm0 : public Component {
-  string pld_name;
-  uint8_t pld_type;
-
-  altera_max10_attr_t attr;
-  public:
-    CPLD10m25cfm0(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
-      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_1, CFM0_10M25_START_ADDR, CFM0_10M25_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
-    int print_version();
-    int update(string image);
+enum {
+  CFM0_10M25 = 0,
+  CFM1_10M25,
+  CFM0_10M16,
+  CFM1_10M16
 };
 
-class CPLD10m25cfm1 : public Component {
-  string pld_name;
-  uint8_t pld_type;
-
-  altera_max10_attr_t attr;
-  public:
-    CPLD10m25cfm1(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
-      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_2, CFM1_10M25_START_ADDR, CFM1_10M25_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
-    int print_version();
-    int update(string image);
+static altera_max10_attr_t s_attrs[] = {
+  [CFM0_10M25] = {
+    .img_type = CFM_IMAGE_1,
+    .start_addr = CFM0_10M25_START_ADDR,
+    .end_addr = CFM0_10M25_END_ADDR,
+  },
+  [CFM1_10M25] = {
+    .img_type = CFM_IMAGE_2,
+    .start_addr = CFM1_10M25_START_ADDR,
+    .end_addr = CFM1_10M25_END_ADDR,
+  },
+  [CFM0_10M16] = {
+    .img_type = CFM_IMAGE_1,
+    .start_addr = CFM0_10M16_START_ADDR,
+    .end_addr = CFM0_10M16_END_ADDR,
+  },
+  [CFM1_10M16] = {
+    .img_type = CFM_IMAGE_2,
+    .start_addr = CFM1_10M16_START_ADDR,
+    .end_addr = CFM1_10M16_END_ADDR,
+  }
 };
 
-//CPLD 10M16
-class CPLD10m16cfm0 : public Component {
+class CpldComponent : public Component {
   string pld_name;
   uint8_t pld_type;
-
   altera_max10_attr_t attr;
+  int _update(const char *path, uint8_t is_signed);
   public:
-    CPLD10m16cfm0(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
-      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_1, CFM0_10M16_START_ADDR, CFM0_10M16_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
-    int print_version();
+    CpldComponent(string fru, string comp, string name, uint8_t type, uint8_t ctype, uint8_t bus, uint8_t addr)
+      : Component(fru, comp), pld_name(name), pld_type(type),
+        attr{bus, addr, s_attrs[ctype].img_type, s_attrs[ctype].start_addr, s_attrs[ctype].end_addr,
+             ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
     int update(string image);
+    int fupdate(string image);
+    int print_version();
 };
 
-class CPLD10m16cfm1 : public Component {
-  string pld_name;
-  uint8_t pld_type;
+int CpldComponent::_update(const char *path, uint8_t is_signed) {
+  int ret = -1;
 
-  altera_max10_attr_t attr;
-  public:
-    CPLD10m16cfm1(string fru, string comp, string name, uint8_t type, uint8_t bus, uint8_t addr)
-      : Component(fru, comp), pld_name(name), pld_type(type), attr{bus, addr, CFM_IMAGE_2, CFM1_10M16_START_ADDR, CFM1_10M16_END_ADDR, ON_CHIP_FLASH_IP_CSR_BASE, ON_CHIP_FLASH_IP_DATA_REG, DUAL_BOOT_IP_BASE} {}
-    int print_version();
-    int update(string image);
-};
+  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
+    printf("Cannot open i2c!\n");
+    return -1;
+  }
 
-//10M25 Version
-int CPLD10m25cfm0::print_version() {
-  int ret;
+  ret = cpld_program((char *)path, (char *)pld_name.c_str(), is_signed);
+  cpld_intf_close(INTF_I2C);
+  if (ret) {
+    printf("Error Occur at updating CPLD FW!\n");
+  }
+
+  return ret;
+}
+
+int CpldComponent::update(string image) {
+  return _update(image.c_str(), 1);
+}
+
+int CpldComponent::fupdate(string image) {
+  return _update(image.c_str(), 0);
+}
+
+int CpldComponent::print_version() {
+  int ret = -1;
   uint8_t ver[4];
 
   if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
@@ -96,6 +113,7 @@ int CPLD10m25cfm0::print_version() {
 
   ret = cpld_get_ver((uint32_t *)ver);
   cpld_intf_close(INTF_I2C);
+
   if (ret) {
     printf("%s CPLD Version: NA\n", pld_name.c_str());
   } else {
@@ -105,128 +123,9 @@ int CPLD10m25cfm0::print_version() {
   return 0;
 }
 
-int CPLD10m25cfm1::print_version() {
-  int ret;
-  uint8_t ver[4];
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
-    printf("Cannot open i2c!\n");
-    return -1;
-  }
-
-  ret = cpld_get_ver((uint32_t *)ver);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("%s CPLD Version: NA\n", pld_name.c_str());
-  } else {
-    printf("%s CPLD Version: v%02x.%02x.%02x.%02x\n", pld_name.c_str(), ver[3], ver[2], ver[1], ver[0]);
-  }
-
-  return 0;
-}
-
-//10M16 Version
-int CPLD10m16cfm0::print_version() {
-  int ret;
-  uint8_t ver[4];
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
-    printf("Cannot open i2c!\n");
-    return -1;
-  }
-
-  ret = cpld_get_ver((uint32_t *)ver);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("%s CPLD Version: NA\n", pld_name.c_str());
-  } else {
-    printf("%s CPLD Version: v%02x.%02x.%02x.%02x\n", pld_name.c_str(), ver[3], ver[2], ver[1], ver[0]);
-  }
-
-  return 0;
-}
-
-int CPLD10m16cfm1::print_version() {
-  int ret;
-  uint8_t ver[4];
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) {
-    printf("Cannot open i2c!\n");
-    return -1;
-  }
-
-  ret = cpld_get_ver((uint32_t *)ver);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("%s CPLD Version: NA\n", pld_name.c_str());
-  } else {
-    printf("%s CPLD Version: v%02x.%02x.%02x.%02x\n", pld_name.c_str(), ver[3], ver[2], ver[1], ver[0]);
-  }
-
-  return 0;
-}
-
-//10M25 update
-int CPLD10m25cfm0::update(string image) {
-  int ret;
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) 
-    printf("Cannot open i2c!\n");
-  ret = cpld_program((char *)image.c_str(), (char *)pld_name.c_str(), true);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("Error Occur at updating CPLD FW!\n");
-  }
-
-  return ret;
-}
-
-int CPLD10m25cfm1::update(string image) {
-  int ret;
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr)) 
-    printf("Cannot open i2c!\n");
-  ret = cpld_program((char *)image.c_str(), (char *)pld_name.c_str(), true);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("Error Occur at updating CPLD FW!\n");
-  }
-
-  return ret;
-}
-
-//10M16 update
-int CPLD10m16cfm0::update(string image) {
-  int ret;
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr))
-    printf("Cannot open i2c!\n");
-  ret = cpld_program((char *)image.c_str(), (char *)pld_name.c_str(), true);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("Error Occur at updating CPLD FW!\n");
-  }
-
-  return ret;
-}
-
-int CPLD10m16cfm1::update(string image) {
-  int ret;
-
-  if (cpld_intf_open(pld_type, INTF_I2C, &attr))
-    printf("Cannot open i2c!\n");
-  ret = cpld_program((char *)image.c_str(), (char *)pld_name.c_str(), true);
-  cpld_intf_close(INTF_I2C);
-  if (ret) {
-    printf("Error Occur at updating CPLD FW!\n");
-  }
-
-  return ret;
-}
-
-CPLD10m25cfm0 pfr_cfm0("cpld", "pfr_cfm0", "PFR_CFM0", MAX10_10M25, 4, 0x5a);
-CPLD10m25cfm1 pfr_cfm1("cpld", "pfr_cfm1", "PFR_CFM1", MAX10_10M25, 4, 0x5a);
-CPLD10m16cfm0 mod_cfm0("cpld", "mod_cfm0", "MOD_CFM0", MAX10_10M16, 4, 0x55);
-CPLD10m16cfm1 mod_cfm1("cpld", "mod_cfm1", "MOD_CFM1", MAX10_10M16, 4, 0x55);
-CPLD10m16cfm0 glb_cfm0("cpld", "glb_cfm0", "GLB_CFM0", MAX10_10M16, 23, 0x55);
-CPLD10m16cfm1 glb_cfm1("cpld", "glb_cfm1", "GLB_CFM1", MAX10_10M16, 23, 0x55);
+CpldComponent pfr_cfm0("cpld", "pfr_cfm0", "PFR_CFM0", MAX10_10M25, CFM0_10M25, 4, 0x5a);
+CpldComponent pfr_cfm1("cpld", "pfr_cfm1", "PFR_CFM1", MAX10_10M25, CFM1_10M25, 4, 0x5a);
+CpldComponent mod_cfm0("cpld", "mod_cfm0", "MOD_CFM0", MAX10_10M16, CFM0_10M16, 4, 0x55);
+CpldComponent mod_cfm1("cpld", "mod_cfm1", "MOD_CFM1", MAX10_10M16, CFM1_10M16, 4, 0x55);
+CpldComponent glb_cfm0("cpld", "glb_cfm0", "GLB_CFM0", MAX10_10M16, CFM0_10M16, 23, 0x55);
+CpldComponent glb_cfm1("cpld", "glb_cfm1", "GLB_CFM1", MAX10_10M16, CFM1_10M16, 23, 0x55);

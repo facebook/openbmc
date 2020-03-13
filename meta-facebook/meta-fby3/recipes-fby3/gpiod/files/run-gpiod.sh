62d71133c39ee53dd9bb9f98deac5f1061df5216
Ben Wei <benwei@fb.com>
2020-03-13 08:00:09 -0700

    fby3: fix gpiod message output on Config C (#966)
    Summary:
    - as title, ensure gpiod runs correctly on config  C
    run-gpiod.sh need to identify the class 1 or class 2. Otherwise, it would run slot1~slot4 in Config C.
    There is only slot1 in Config C.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/966
    
    Test Plan:
    - [Build code]:apss
    - [Run gpiod]: pass
    ```
    root@bmc-oob:/etc/sv/gpiod# sv stop gpiod
    ok: down: gpiod: 0s, normally up
    root@bmc-oob:/etc/sv/gpiod# bash run
    ```
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: ec7423f886

--- meta-facebook/meta-fby3/recipes-fby3/gpiod/files/run-gpiod.sh
+++ meta-facebook/meta-fby3/recipes-fby3/gpiod/files/run-gpiod.sh
@@ -3,6 +3,11 @@
 
 SLOTS=
 
+function init_class2_gpiod() {
+  SLOTS="$SLOTS slot1"
+}
+
+function init_class1_gpiod() {
   if [[ $(is_server_prsnt 1) == "1" ]]; then
     SLOTS="$SLOTS slot1"
   fi
@@ -18,5 +23,17 @@ SLOTS=
   if [[ $(is_server_prsnt 4) == "1" ]]; then
     SLOTS="$SLOTS slot4"
   fi
+}
+
+bmc_location=$(get_bmc_board_id)
+if [ $bmc_location -eq 9 ]; then
+  #The BMC of class2
+  init_class2_gpiod
+elif [ $bmc_location -eq 14 ]; then
+  #The BMC of class1
+  init_class1_gpiod
+else
+  echo -n "Is board id correct(id=$bmc_location)?..."
+fi
 
 exec /usr/local/bin/gpiod $SLOTS 

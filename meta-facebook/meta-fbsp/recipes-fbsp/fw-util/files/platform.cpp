43f3b37b4e78e32154ed5725dfa1556910be7562
Patrick Williams <patrickw3@fb.com>
2020-03-17 12:32:34 -0700

    fbsp: fw-util support display TPM version (#1468)
    Summary:
    as title
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1468
    
    Test Plan:
    build and test pass on FBSP system
    
    root@bmc-oob:~# fw-util all --version | grep -i tpm
    TPM Version: 7.62
    
    root@bmc-oob:~# tpm2_getcap -c properties-fixed
    WARN: More data to be queried: capability: 0x6, property: 0x100
    
    TPM_PT_FAMILY_INDICATOR:
      as UINT32:                0x08322e3000
      as string:                "2.0"
    TPM_PT_LEVEL:               0
    TPM_PT_REVISION:            1.16
    TPM_PT_DAY_OF_YEAR:         0x0000000f
    TPM_PT_YEAR:                0x000007e0
    TPM_PT_MANUFACTURER:        0x49465800
    TPM_PT_VENDOR_STRING_1:
      as UINT32:                0x534c4239
      as string:                "SLB9"
    TPM_PT_VENDOR_STRING_2:
      as UINT32:                0x36373000
      as string:                "670"
    TPM_PT_VENDOR_STRING_3:
      as UINT32:                0x00000000
      as string:                ""
    TPM_PT_VENDOR_STRING_4:
      as UINT32:                0x00000000
      as string:                ""
    TPM_PT_VENDOR_TPM_TYPE:     0x00000000
    TPM_PT_FIRMWARE_VERSION_1:  0x0007003e
    TPM_PT_FIRMWARE_VERSION_2:  0x000c3600
    TPM_PT_INPUT_BUFFER:        0x00000400
    TPM_PT_TPM2_HR_TRANSIENT_MIN:    0x00000003
    TPM_PT_TPM2_HR_PERSISTENT_MIN:   0x00000007
    TPM_PT_HR_LOADED_MIN:       0x00000003
    TPM_PT_ACTIVE_SESSIONS_MAX: 0x00000040
    TPM_PT_PCR_COUNT:           0x00000018
    TPM_PT_PCR_SELECT_MIN:      0x00000003
    TPM_PT_CONTEXT_GAP_MAX:     0x0000ffff
    TPM_PT_NV_COUNTERS_MAX:     0x00000008
    TPM_PT_NV_INDEX_MAX:        0x00000680
    TPM_PT_MEMORY:              0x00000006
    TPM_PT_CLOCK_UPDATE:        0x00080000
    TPM_PT_CONTEXT_HASH:        0x0000000b
    TPM_PT_CONTEXT_SYM:         0x00000006
    TPM_PT_CONTEXT_SYM_SIZE:    0x00000080
    TPM_PT_ORDERLY_COUNT:       0x000000ff
    TPM_PT_MAX_COMMAND_SIZE:    0x00000500
    TPM_PT_MAX_RESPONSE_SIZE:   0x00000500
    TPM_PT_MAX_DIGEST:          0x00000020
    TPM_PT_MAX_OBJECT_CONTEXT:  0x000003b8
    TPM_PT_MAX_SESSION_CONTEXT: 0x000000eb
    TPM_PT_PS_FAMILY_INDICATOR: 0x00000001
    TPM_PT_PS_LEVEL:            0x00000000
    TPM_PT_PS_REVISION:         0x00000100
    TPM_PT_PS_DAY_OF_YEAR:      0x00000000
    TPM_PT_PS_YEAR:             0x00000000
    TPM_PT_SPLIT_MAX:           0x00000080
    TPM_PT_TOTAL_COMMANDS:      0x0000005a
    TPM_PT_LIBRARY_COMMANDS:    0x00000059
    TPM_PT_VENDOR_COMMANDS:     0x00000001
    TPM_PT_NV_BUFFER_MAX:       0x00000300
    
    Reviewed By: benwei13
    
    fbshipit-source-id: e9db886310

--- meta-facebook/meta-fbsp/recipes-fbsp/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbsp/recipes-fbsp/fw-util/files/platform.cpp
@@ -2,6 +2,7 @@
 #include "bios.h"
 #include "usbdbg.h"
 #include "nic_ext.h"
+#include "tpm2.h"
 
 BiosComponent bios("mb", "bios", "pnor", "spi0.0", "FM_BIOS_SPI_BMC_CTRL", true, "^F0G_.*$|^SP[EDPM][0-9]{2}.*$|^SP_PPO.*$");
 
@@ -10,3 +11,5 @@ NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1);
 
 UsbDbgComponent usbdbg("ocpdbg", "mcu", "F0G", 0, 0x60, false);
 UsbDbgBlComponent usbdbgbl("ocpdbg", "mcubl", 0, 0x60, 0x02);  // target ID of bootloader = 0x02
+
+Tpm2Component tpm("bmc", "tpm");
--- meta-facebook/meta-fbsp/recipes-fbsp/fw-util/fw-util_0.2.bbappend
+++ meta-facebook/meta-fbsp/recipes-fbsp/fw-util/fw-util_0.2.bbappend
@@ -31,6 +31,8 @@ SRC_URI += "\
         file://usbdbg.cpp \
         file://usbdbg.h \
         file://vr.cpp \
+        file://tpm2.cpp \
+        file://tpm2.h \
         "
 
 DEPENDS += "libmcu libnm libpal libfpga libast-jtag libvr libncsi libnl-wrapper libkv"

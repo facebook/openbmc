c8be70071b39f7cc6131da483e02152858a1e3eb
Amithash Prasad <amithash@fb.com>
2020-03-25 14:08:09 -0700

    Fbep: Let common enable the TPM2 component
    Summary: Remove adding TPM component since it is now done in common
    
    Test Plan: Build emeraldpools
    
    Reviewed By: tao-ren
    
    fbshipit-source-id: 1890821f3c

--- meta-facebook/meta-fbep/recipes-fbep/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbep/recipes-fbep/fw-util/files/platform.cpp
@@ -1,7 +1,4 @@
 #include "usbdbg.h"
-#include "tpm2.h"
 
 UsbDbgComponent usbdbg("ocpdbg", "mcu", "JG7", 13, 0x60, false);
 UsbDbgBlComponent usbdbgbl("ocpdbg", "mcubl", 13, 0x60, 0x02);  // target ID of bootloader = 0x02
-
-Tpm2Component tpm2("bmc", "tpm");

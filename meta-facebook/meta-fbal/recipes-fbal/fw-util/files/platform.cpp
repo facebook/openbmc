9337962df839548f3afe520b97b2a13b4f83bc35
Amithash Prasad <amithash@fb.com>
2020-03-25 14:08:09 -0700

    Fbal: Let common enable the TPM2 component
    Summary: Remove adding TPM component since it is now done in common
    
    Test Plan: build angelslanding
    
    Reviewed By: tao-ren
    
    fbshipit-source-id: 54cecdbd33

--- meta-facebook/meta-fbal/recipes-fbal/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbal/recipes-fbal/fw-util/files/platform.cpp
@@ -2,7 +2,6 @@
 #include "bios.h"
 #include "usbdbg.h"
 #include "nic_ext.h"
-#include "tpm2.h"
 
 BiosComponent bios("mb", "bios", "pnor", "spi0.0", "FM_BIOS_SPI_BMC_CTRL", true, "(F0C_)(.*)");
 
@@ -11,5 +10,3 @@ NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1, 0x20);
 
 UsbDbgComponent usbdbg("ocpdbg", "mcu", "F0C", 0, 0x60, false);
 UsbDbgBlComponent usbdbgbl("ocpdbg", "mcubl", 0, 0x60, 0x02);  // target ID of bootloader = 0x02
-
-Tpm2Component tpm("bmc", "tpm");

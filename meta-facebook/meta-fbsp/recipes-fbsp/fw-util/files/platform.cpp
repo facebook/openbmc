419dc50f5dc9f90ee03060cdd0a96ef9e6408f28
Amithash Prasad <amithash@fb.com>
2020-03-25 14:01:44 -0700

    Fbsp: Let common enable the TPM2 component
    Summary: Remove adding TPM component since it is now done in common
    
    Test Plan: Build sonorapass
    
    Reviewed By: williamspatrick, tao-ren
    
    fbshipit-source-id: 320c95e98c

--- meta-facebook/meta-fbsp/recipes-fbsp/fw-util/files/platform.cpp
+++ meta-facebook/meta-fbsp/recipes-fbsp/fw-util/files/platform.cpp
@@ -2,7 +2,6 @@
 #include "bios.h"
 #include "usbdbg.h"
 #include "nic_ext.h"
-#include "tpm2.h"
 
 BiosComponent bios("mb", "bios", "pnor", "spi0.0", "FM_BIOS_SPI_BMC_CTRL", true, "^F0G_.*$|^SP[EDPM][0-9]{2}.*$|^SP_PPO.*$");
 
@@ -11,5 +10,3 @@ NicExtComponent nic1("nic", "nic1", "nic1_fw_ver", FRU_NIC1, 1, 0x20);
 
 UsbDbgComponent usbdbg("ocpdbg", "mcu", "F0G", 0, 0x60, false);
 UsbDbgBlComponent usbdbgbl("ocpdbg", "mcubl", 0, 0x60, 0x02);  // target ID of bootloader = 0x02
-
-Tpm2Component tpm("bmc", "tpm");

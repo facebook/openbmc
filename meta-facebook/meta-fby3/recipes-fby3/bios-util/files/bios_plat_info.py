7318e2c98e45a6aa2cc3ea7df5f6bedb70d33719
Ben Wei <benwei@fb.com>
2020-03-31 13:26:02 -0700

    fby3: bios-util: add plat_info function (#990)
    Summary:
    -A user can use bios-util to get the platform information.
    The information is generated by show_sys_config tool.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/990
    
    Test Plan:
    -build code(pass)
    -modify show_sys_config tool and run bios-util with plat_info option(pass)
    Class 2:
    root@bmc-oob:~# bios-util slot1 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 1
    PCIe Configuration: 2x Delta Lakes
    
    Class 1:
    Unknown Config:
    root@bmc-oob:~# bios-util slot3 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 3
    PCIe Configuration: unknown
    
    root@bmc-oob:~# bios-util slot1 --plat_info get
    Presense: Not Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 1
    PCIe Configuration: unknown
    
    Config A:
    root@bmc-oob:~# bios-util slot3 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 3
    PCIe Configuration: 4x Delta Lakes
    root@bmc-oob:~# show_sys_config
    Type : Class 1, Config: A
    
    root@bmc-oob:~# bios-util slot1 --plat_info get
    Presense: Not Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 1
    PCIe Configuration: 4x Delta Lakes
    
    root@bmc-oob:~# find /usr/ -name "*FBY3*"
    /usr/local/fbpackages/bios-util/BIOS_UTIL_FBY3.json
    
    Config B:
    root@bmc-oob:~# bios-util slot3 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 3
    PCIe Configuration: 4x Delta Lakes
    root@bmc-oob:~# show_sys_config
    Type : Class 1, Config: B
    
    root@bmc-oob:~# bios-util slot1 --plat_info get
    Presense: Not Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 1
    PCIe Configuration: 4x Delta Lakes
    
    Config C:
    root@bmc-oob:~# bios-util slot3 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 3
    PCIe Configuration: 2x Delta Lakes
    root@bmc-oob:~# show_sys_config
    Type : Class 1, Config: C
    
    root@bmc-oob:~# bios-util slot1 --plat_info get
    Presense: Not Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 1
    PCIe Configuration: 2x Delta Lakes
    
    Config D:
    root@bmc-oob:~# bios-util slot3 --plat_info get
    Presense: Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 3
    PCIe Configuration: 2x Delta Lakes
    root@bmc-oob:~# show_sys_config
    Type : Class 1, Config: D
    
    root@bmc-oob:~# bios-util slot1 --plat_info get
    Presense: Not Present
    Non Test Board
    SKU: Yosemite V3
    Slot Index: 1
    PCIe Configuration: 2x Delta Lakes
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: d70323e9d5

---
+++ meta-facebook/meta-fby3/recipes-fby3/bios-util/files/bios_plat_info.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module provides the platform information to users.
+Users can run `bios-util slotX --plat_info get` to call it.
+"""
+
+from subprocess import PIPE, Popen
+import json
+import re
+
+SHOW_SYS_CONFIG = "/usr/local/bin/show_sys_config"
+
+""" Get Platform Info """
+def plat_info(fru):
+    """ Prvoide the system configuration """
+
+    sku = "Yosemite V3"
+    slot_index = str(fru)
+    present = ""
+    sys_type = ""
+    pcie_configuration = ""
+
+    try:
+        cmd = [SHOW_SYS_CONFIG, "-json"]
+        proc = Popen(' '.join(cmd), shell=True, stdout=PIPE, stderr=PIPE)
+        data = proc.stdout.read().decode().strip()
+        json_data = json.loads(data)
+
+        present = json_data["server_config"]["slot"+str(fru)]["Type"]
+        if not re.search('not present', present, re.I):
+            present = "Present"
+
+        sys_type = json_data["Type"]
+        if "Class 2" in sys_type:
+            pcie_configuration = "2x Delta Lakes"
+        else:
+            pcie_configuration = json_data["Config"].lower()
+            if re.search(r'a|b', pcie_configuration):
+                pcie_configuration = "4x Delta Lakes"
+            elif re.search(r'c|d', pcie_configuration):
+                pcie_configuration = "2x Delta Lakes"
+
+        print ("Presense: " + present)
+        print ("Non Test Board")
+        print ("SKU: " + sku)
+        print ("Slot Index: " + slot_index)
+        print ("PCIe Configuration: " + pcie_configuration)
+    except OSError:
+        print ("Error: Couldn't get the system configuration")

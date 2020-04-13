883bd4bb29b28aa623d3ea7075295b412426c9a9
Joan Olguy Canéus <joancaneus@fb.com>
2020-04-13 14:20:16 -0700

    Fix process running bug
    Summary:
    This test was failing due to the fact that running this test on
    some platform has a --address in the running process line and others
    don't have it. For example, one some platform we have: dhclient args are
    not the expected values as in "dhclient -6 -d -D LL --address-prefix-len
    64 -pf /var/run/dhclient6.eth0.pid eth0"
    However, on other platform, we simply have: "dhclient -6 -d -D LL -pf
    /var/run/dhclient6.eth0.pid eth0"
    Therefore, we remove some string to fix this test
    
    Test Plan:
    see below.
    ```
    root@dhcp6-2620-10d-c082-9002-0-0-0-1801:/usr/local/bin/tests2# python
    cit_runner.py --platform wedge400 --run-test
    tests.wedge400.test_process_running.ProcessRunningTest.test_installed_processes
    test_installed_processes
    (tests.wedge400.test_process_running.ProcessRunningTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.885s
    
    OK
    ```
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 46b997a454

--- tests2/tests/wedge400/test_process_running.py
+++ tests2/tests/wedge400/test_process_running.py
@@ -25,7 +25,7 @@ from common.base_process_running_test import BaseProcessRunningTest
 class ProcessRunningTest(BaseProcessRunningTest, unittest.TestCase):
     def set_processes(self):
         self.expected_process = [
-            "dhclient -6 -d -D LL -pf /var/run/dhclient6.eth0.pid eth0",
+            "dhclient -6 -d -D LL",
             "dhclient -pf /var/run/dhclient.eth0.pid eth0",
             "front-paneld",
             "gpiod",

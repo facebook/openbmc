5c5d760d6f33a8eb0be2a2739cfea0d345d7d033
Vineela Kukkadapu <vineelak@fb.com>
2020-04-24 12:34:38 -0700

    Add ROMCS1 gpio config test
    Summary: in title
    
    Test Plan:
    root@dhcp-100-97-167-36:/usr/local/bin/tests2# python cit_runner.py --platform wedge100 --run-test tests.wedge100.test_gpio.GpioTest.test_romcs1
    test_romcs1 (tests.wedge100.test_gpio.GpioTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 11.720s
    
    OK
    
    Reviewed By: jhshi
    
    fbshipit-source-id: 3ec51065d8

--- tests2/tests/wedge100/test_gpio.py
+++ tests2/tests/wedge100/test_gpio.py
@@ -21,8 +21,18 @@ import unittest
 
 from common.base_gpio_test import BaseGpioTest
 from tests.wedge100.test_data.gpio.gpio import GPIOS
+from utils.cit_logger import Logger
+from utils.shell_util import run_shell_cmd
 
 
 class GpioTest(BaseGpioTest, unittest.TestCase):
     def set_gpios(self):
         self.gpios = GPIOS
+
+    def test_romcs1(self):
+        rom_cmd = "/usr/local/bin/openbmc_gpio_util.py config ROMCS1#"
+        Logger.info("Executing cmd={}".format(rom_cmd))
+        info = run_shell_cmd(cmd=rom_cmd)
+        self.assertIn(
+            'Function "ROMCS1#" is set', info, "ROMCS1 gpio not setup properly"
+        )

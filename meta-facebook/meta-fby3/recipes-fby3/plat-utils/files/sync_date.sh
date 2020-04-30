04c557c7565aa828fc3f6c18499baa7d6f8d8ebe
Ben Wei <benwei@fb.com>
2020-04-30 15:48:28 -0700

    fby3: fix datetime sync error on class2 (#1630)
    Summary:
    Summary :
    as title.
    
    Test Plan :
    build and test pass on fby3 configC
    
    Mon Jan  1 00:00:00 PST 2018
    Syncing up BMC time with server1...
    Thu Apr 30 00:49:45 PDT 2020
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1630
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: a32ed90b5c

--- meta-facebook/meta-fby3/recipes-fby3/plat-utils/files/sync_date.sh
+++ meta-facebook/meta-fby3/recipes-fby3/plat-utils/files/sync_date.sh
@@ -21,35 +21,58 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
+function get_sel_time_from_server {
+  local BOARD_ID=$(get_bmc_board_id)
+  local output=""
+  local snum=0
+
+  if [ $BOARD_ID -eq 9 ]; then
+    output="$(/usr/local/bin/me-util slot1 0x28 0x48)"
+    [ ${#output} == 12 ] && snum=1
+  else
+    for i in $(seq 1 4)
+    do
+      if [[ $(is_server_prsnt $i) == "1" ]] ; then
+        # Use standard IPMI command 'get-sel-time' to read RTC time
+        output=$(/usr/local/bin/me-util slot$i 0x28 0x48)
+        if [ ${#output} == 12 ]; then
+          snum=$i
+          break
+        fi
+      fi
+    done
+  fi
+
+  # return IPMI "Get sel time" command output, or return empty string if command failed
+  [ ${#output} == 12 ] && echo $output || echo ""
+
+  return $snum
+}
+
+function do_sync {
+  local sel_time=$1
+  date -s @$((16#$(echo "$sel_time" | awk '{print $4$3$2$1}')))
+  test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop
+  echo 1 > /tmp/sync_date
+}
+
 # Sync BMC's date with one of the four servers
-sync_date()
-{
+function sync_date {
   if ! /usr/sbin/ntpq -p | grep '^\*' > /dev/null ; then
     if [ -f /tmp/cache_store/time_sync ] ; then
       # ND Server Time Sync by IPMI command
       output=$(cat /tmp/cache_store/time_sync)
       if [ ${#output} == 11 ] ; then
         echo Syncing up BMC time with server...
-        date -s @$((16#$(echo "$output" | awk '{print $4$3$2$1}')))
-        test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop
-        echo 1 > /tmp/sync_date
+        do_sync "$output"
       fi
     else
-      for i in $(seq 0 3);
-      do
-        if [[ $(is_server_prsnt $i) == "1" ]] ; then
-          # Use standard IPMI command 'get-sel-time' to read RTC time
-          output=$(/usr/local/bin/me-util slot$i 0x28 0x48)
-
-          # if the command fails, continue to next slot
-          [ ${#output} != 12 ] && continue
-          echo Syncing up BMC time with server$i...
-          date -s @$((16#$(echo "$output" | awk '{print $4$3$2$1}')))
-          test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop
-          echo 1 > /tmp/sync_date
-          break
-        fi
-      done
+      output=$(get_sel_time_from_server)
+      num=$?
+      if [ -n "$output" ]; then
+        echo Syncing up BMC time with server$num...
+        do_sync "$output"
+      fi
     fi
   else
     test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop

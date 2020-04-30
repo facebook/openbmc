2c385718014778442891b1a5abb33075c86a0368
Ben Wei <benwei@fb.com>
2020-04-30 10:58:52 -0700

    common: vboot-util check flash0's and flash1's has signed/locked or not (#1628)
    Summary:
    vboot-util check flash0's and flash1's has signed/locked or not
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1628
    
    Test Plan:
    1.Update flash0 with locked signed image
    root@bmc-oob:/etc# fw-util bmc --update bmc /tmp/flash-fbgp2-v2020.18.1.locked.signed
    Flashing to device: /dev/mtd5
    Erasing block: 288/288 (100%)
    Writing kb: 18378/18378 (100%)
    Verifying kb: 18378/18378 (100%)
    Upgrade of bmc : bmc succeeded
    
    2.update flash0 with signed image
    root@bmc-oob:/etc# fw-util bmc --update rom /tmp/flash-fbgp2-v2020.18.1.signed
    Flashing to device: /dev/mtd12
    Erasing block: 288/288 (100%)
    Writing kb: 18378/18378 (100%)
    Verifying kb: 18378/18378 (100%)
    Upgrade of bmc : rom succeeded
    
    3.check vboot-util result:
    root@bmc-oob:~# vboot-util
    ROM executed from:       0x00000058
    ROM KEK certificates:    0x00010c80
    ROM handoff marker:      0x00000000
    U-Boot executed from:    0x28084350
    U-Boot certificates:     0x2808054c
    Certificates fallback:   Sat Jun 17 13:17:06 2017
    Certificates time:       Sat Jun 17 13:17:06 2017
    U-Boot fallback:         not set
    U-Boot time:             Mon Apr 27 15:29:12 2020
    Kernel fallback:         not set
    Kernel time:             not set
    Flags force_recovery:    0x00
    Flags hardware_enforce:  0x00
    Flags software_enforce:  0x01
    Flags recovery_boot:     0x00
    Flags recovery_retried:  0x00
    
    ROM version:             fbgp2-v2020.18.1
    ROM U-Boot version:      2016.07
    FLASH0 Signed:           0x01
    FLASH0 Locked:           0x00
    FLASH1 Signed:           0x01
    FLASH1 Locked:           0x01
    
    Status CRC: 0x5264
    TPM status  (0)
    Status type (0) code (0)
    OpenBMC was verified correctly
    
    Reviewed By: amithash
    
    fbshipit-source-id: dca0270491

--- common/recipes-utils/vboot-utils/files/vboot-util.c
+++ common/recipes-utils/vboot-utils/files/vboot-util.c
@@ -7,8 +7,93 @@
 #include <time.h>
 #include <syslog.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <openbmc/vbs.h>
 #include <openbmc/kv.h>
+
+#define DEVICE_TREE_HEADER_SIZE (4*10)
+#define MTD_REGION_SIZE 0x60000
+#define MTD_PKT_SIZE 0x10000
+#define ROM_MTD "/dev/mtd7" // BMC flash0
+#define ROMX_MTD "/dev/mtd0" // BMC flash1
+
+int check_lock_signed(const char *path, uint8_t * img_sig, uint8_t *img_lock)
+{
+  int fd = 0, ret = -1;
+  int offs, rcnt, end, size_pos, size, dt_offs;
+  uint8_t *buf;
+  uint8_t magic_number[] = { 0xd0, 0x0d, 0xfe, 0xed}; // magic number
+  uint8_t signed_string[] = { 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65}; // signature
+  uint8_t locked_string[] = { 0x68, 0x77, 0x6c, 0x6f, 0x63, 0x6b}; // hwlock
+
+  *img_lock = 0x00;
+  *img_sig = 0x00;
+
+  buf = (uint8_t *)malloc(MTD_REGION_SIZE);
+  if (!buf) {
+    syslog(LOG_WARNING,"%s: malloc failed", __func__);
+    return -1;
+  }
+
+  do {
+    fd = open(path, O_RDONLY);
+    if (fd < 0) {
+      syslog(LOG_WARNING,"%s: %s open failed", __func__, path);
+      break;
+    }
+
+    offs = 0;
+    if (lseek(fd, offs, SEEK_SET) != (off_t)offs) {
+      syslog(LOG_WARNING,"%s: lseek to %d failed", __func__, offs);
+      break;
+    }
+
+    offs = 0;
+    while (offs < MTD_REGION_SIZE) {
+      rcnt = ((offs + MTD_PKT_SIZE) <= MTD_REGION_SIZE) ? MTD_PKT_SIZE : (MTD_REGION_SIZE - offs);
+      rcnt = read(fd, buf, MTD_REGION_SIZE);
+      if (rcnt <= 0) {
+        if (errno == EINTR) {
+          continue;
+        }
+        syslog(LOG_WARNING,"%s: unexpected rcnt: %d", __func__, rcnt);
+        break;
+      }
+      offs += rcnt;
+    }
+    if (offs < MTD_REGION_SIZE) {
+      break;
+    }
+
+    end = MTD_REGION_SIZE - sizeof(magic_number);
+    // Scan through the region looking for the version signature.
+    for (offs = 0; offs < end; offs++) {
+      if (!memcmp(buf+offs, magic_number, sizeof(magic_number))) {
+        size_pos = offs + sizeof(magic_number);
+        size = buf[size_pos]* 0x1000000 + buf[size_pos+1] * 0x10000 + buf[size_pos+2]* 0x100 + buf[size_pos+3];
+        ret = 0;
+        for (dt_offs = offs + DEVICE_TREE_HEADER_SIZE ; dt_offs < offs + size; dt_offs++) { // DEVICE_TREE_HEADER_SIZE
+          if (!memcmp(buf+dt_offs, locked_string, sizeof(locked_string))) {
+            *img_lock = 0x01;
+          }
+          if (!memcmp(buf+dt_offs, signed_string, sizeof(signed_string))) {
+            *img_sig = 0x01;
+          }
+          if (*img_lock && *img_sig) //find lock snd sig
+            break;
+        }
+        break;
+      }
+    }
+  } while (0);
+
+  free(buf);
+  if (fd > 0) {
+    close(fd);
+  }
+
+  return ret;
+}
 
 bool get_mtd_name(char* mtd_name)
 {
@@ -89,6 +174,7 @@ int main(int argc, char *argv[])
   char ver_rom[MAX_VALUE_LEN] = {0};
   char ver_uboot[MAX_VALUE_LEN] = {0};
   struct vbs *v;
+  uint8_t img_sig = 0, img_lock = 0;
 
   if (!vboot_supported()) {
     printf("Verified boot is not supported on this platform!\n");
@@ -125,13 +211,20 @@ int main(int argc, char *argv[])
   printf("Flags recovery_retried:  0x%02x\n", v->recovery_retries);
   printf("\n");
   if (get_rom_ver(ver_rom, ver_uboot)) {
+    check_lock_signed(ROM_MTD,&img_sig,&img_lock);
     printf("ROM version:             %s\n", ver_rom);
     printf("ROM U-Boot version:      %s\n", ver_uboot);
+    printf("FLASH0 Signed:           0x%02x\n", img_sig);
+    printf("FLASH0 Locked:           0x%02x\n", img_lock);
+    check_lock_signed(ROMX_MTD,&img_sig,&img_lock);
+    printf("FLASH1 Signed:           0x%02x\n", img_sig);
+    printf("FLASH1 Locked:           0x%02x\n", img_lock);
     printf("\n");
   }
   printf("Status CRC: 0x%04x\n", v->crc);
   printf("TPM status  (%d)\n", v->error_tpm);
   printf("Status type (%d) code (%d)\n", v->error_type, v->error_code);
   printf("%s\n", vboot_error(v->error_code));
+
   return 0;
 }

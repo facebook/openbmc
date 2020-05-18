a36a6d890ed0a8c424877291f911eed02cc9948c
Joan Olguy Canéus <joancaneus@fb.com>
2020-05-18 12:42:30 -0700

    fuji: cit for test fan (#412)
    Summary:
    CIT for fan test.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/412
    
    Test Plan:
    root@athena:/usr/local/bin/tests2# python cit_runner.py --platform fuji --run-test tests.fuji.test_fans
    test_all_fans_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan1_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan1_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan2_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan2_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan3_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan3_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan4_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan4_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan5_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan5_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan6_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan6_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan7_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan7_read (tests.fuji.test_fans.FansTest) ... ok
    test_fan8_pwm_set (tests.fuji.test_fans.FansTest) ... ok
    test_fan8_read (tests.fuji.test_fans.FansTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 17 tests in 288.443s
    
    OK
    
    Pass.
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: dac51b1951

---
+++ tests2/tests/fuji/test_fans.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import time
+import unittest
+
+from common.base_fans_test import CommonShellBasedFansTest
+from utils.cit_logger import Logger
+
+
+class FansTest(CommonShellBasedFansTest, unittest.TestCase):
+    def setUp(self):
+        Logger.start(name=self._testMethodName)
+        self.read_fans_cmd = "/usr/local/bin/get_fan_speed.sh"
+        self.write_fans_cmd = "/usr/local/bin/set_fan_speed.sh"
+        self.kill_fan_ctrl_cmd = ["/usr/bin/sv stop fscd", "/usr/local/bin/wdtcli stop"]
+        self.start_fan_ctrl_cmd = ["/usr/bin/sv start fscd"]
+
+    def tearDown(self):
+        Logger.info("Finished logging for {}".format(self._testMethodName))
+        self.kill_fan_controller()
+        self.start_fan_controller()
+
+    def test_fan1_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=1)
+
+    def test_fan2_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=2)
+
+    def test_fan3_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=3)
+
+    def test_fan4_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=4)
+
+    def test_fan5_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=5)
+
+    def test_fan6_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=6)
+
+    def test_fan7_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=7)
+
+    def test_fan8_read(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.read_fans_cmd, None, "Get Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_read(fan=8)
+
+    def test_fan1_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=1, pwm=0)
+
+    def test_fan2_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=2, pwm=30)
+
+    def test_fan3_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=3, pwm=50)
+
+    def test_fan4_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=4, pwm=70)
+
+    def test_fan5_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=4, pwm=80)
+
+    def test_fan6_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=4, pwm=50)
+
+    def test_fan7_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=4, pwm=40)
+
+    def test_fan8_pwm_set(self):
+        """
+        For each fan read and test speed
+        """
+        Logger.log_testname(self._testMethodName)
+        self.assertNotEqual(self.write_fans_cmd, None, "Set Fan cmd not set")
+        # Sleep needed to test and make sure that we won't run
+        # into accessing sysfs path issue causing test to fail
+        time.sleep(2)
+        super().fan_set_and_read(fan_id=4, pwm=30)

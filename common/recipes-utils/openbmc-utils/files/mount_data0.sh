32560dbb6295d340d145b291be3fb73e3e46657e
Tao Ren <taoren@fb.com>
2020-06-03 16:35:28 -0700

    mount_data0.sh: add jffs2 health check support
    Summary:
    We've seen several instances of jffs2 corruption on minipack and cmm
    BMCs, which might be caused by kernel upgrade (jffs2 is not compatible
    across different kernel versions), or by a certain file operation
    pattern which triggers jffs2 bugs.
    
    The patch aims at improving jffs2 reliability by running a few jffs2
    health check at mount time and re-formatting/recovering the partition
    if critical failures are detected.
    
    Test Plan:
    1) manually corrupted /mnt/data/etc on CMM:
    
    data0 partition found on /dev/mtdblock3; mounting to /mnt/data.
    Checking JFFS2 filesystem health on /dev/mtdblock3..
    JFFS2 health check error: /mnt/data/etc corrupted (not a directory)!
    Unmounting /dev/mtdblock3 and starting JFFS2 recovery..
    Erasing and creating JFFS2 filesystem on /dev/mtd3..
    Re-mounting /dev/mtdblock3 to /mnt/data..
    
    2) rebooted CMM (after jffs2 was recovered in step #1):
    
    data0 partition found on /dev/mtdblock3; mounting to /mnt/data.
    Checking JFFS2 filesystem health on /dev/mtdblock3..
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: 22b4c9a67b

--- common/recipes-utils/openbmc-utils/files/mount_data0.sh
+++ common/recipes-utils/openbmc-utils/files/mount_data0.sh
@@ -30,40 +30,99 @@
 
 . /etc/default/rcS
 
+MOUNT_POINT="/mnt/data"
+
+jffs2_mount_and_check () {
+  error=0
+  block_dev=$1
+  mnt_point=$2
+  persist_etc="${mnt_point}/etc"
+
+  if mount -t jffs2 "$block_dev" "$mnt_point"; then
+    #
+    # Wait a second to make sure filesystem is fully ready.
+    #
+    sleep 1
+
+    echo "Checking JFFS2 filesystem health on $block_dev.."
+
+    #
+    # If JFFS2 prints below messages at mount time, then probably the
+    # filesystem is corrupted. Although mount may still return "success",
+    # the file structure/content can be corrupted in this case.
+    # For details, please refer to:
+    # http://www.linux-mtd.infradead.org/faq/jffs2.html
+    #
+    if dmesg | grep "jffs2_scan_eraseblock(): Magic bitmask.*not found" \
+       > /dev/null 2>&1; then
+      echo "JFFS2 health check error: <Magic bitmask not found> reported!"
+      error=$((error+1))
+    fi
+
+    #
+    # Failing to list JFFS2 root directory is definitely not acceptable.
+    #
+    if ! ls "$mnt_point" > /dev/null 2>&1; then
+      echo "JFFS2 health check error: unable to list files in $mnt_point!"
+      error=$((error+1))
+    fi
+
+    #
+    # We've seen several instances of corrupted /mnt/data/etc on Minipack
+    # and CMM BMC: somehow /mnt/data/etc is interpreted as a regular file
+    # instead of a directory, and people lost connection to BMC in this
+    # condition because sshd cannot be started.
+    #
+    if [ -e "$persist_etc" ] && [ ! -d "$persist_etc" ] ; then
+      echo "JFFS2 health check error: $persist_etc corrupted (not a directory)!"
+      error=$((error+1))
+    fi
+
+    if [ "$error" -gt 0 ]; then
+      echo "Unmounting $block_dev and starting JFFS2 recovery.."
+      umount "$block_dev"
+    fi
+  else
+    echo "Unable to mount $block_dev. Starting JFFS2 recovery.."
+    error=$((error+1))
+  fi
+
+  return "$error"
+}
+
 # Find out which device maps to 'data0' on mtd
 # Note: /proc/mtd lists partitions using mtdX, where X is a number,
 #       but we mount using /dev/mtdblockX. We'll do some magic here
 #       to get the mtdX (char device) and mtdblockX (block device)
 #       names.
-MOUNT_POINT="/mnt/data"
 DATA_CHAR_DEV=$(cat /proc/mtd | awk '{ if ($4 == "\"data0\"") print $1 }' |
   cut -d ':' -f 1 | awk '{ print "/dev/" $1 }')
 DATARO_CHAR_DEV=$(cat /proc/mtd | awk '{ if ($4 == "\"dataro\"") print $1 }' |
   cut -d ':' -f 1 | awk '{ print "/dev/" $1 }')
 
-if [ -z "$DATA_CHAR_DEV" ]; then
-  if [ ! -z "$DATARO_CHAR_DEV" ]; then
-    DATA_CHAR_DEV=$DATARO_CHAR_DEV
-  fi
+if [ -z "$DATA_CHAR_DEV" ] && [ -n "$DATARO_CHAR_DEV" ]; then
+  DATA_CHAR_DEV=$DATARO_CHAR_DEV
 fi
 
-if [ -z "$DATA_CHAR_DEV" ]
-then
+if [ -z "$DATA_CHAR_DEV" ]; then
   echo "No data0/dataro partition found. Not mounting anything to $MOUNT_POINT."
 else
-  DEVICE_ID=$(echo $DATA_CHAR_DEV | tail -c 2)
   DATA_BLOCK_DEV=${DATA_CHAR_DEV/mtd/mtdblock}
-
   echo "data0 partition found on $DATA_BLOCK_DEV; mounting to $MOUNT_POINT."
   [ -d $MOUNT_POINT ] || mkdir -p $MOUNT_POINT
-  mount -t jffs2 $DATA_BLOCK_DEV $MOUNT_POINT
 
-  # if the mount failed, format the partition and remount
-  if [ $? -ne 0 ]
-  then
-    echo "Mount failed; formatting $DATA_BLOCK_DEV and remounting."
-    flash_eraseall $DATA_CHAR_DEV
-    mount -t jffs2 $DATA_BLOCK_DEV $MOUNT_POINT
+  if ! jffs2_mount_and_check "$DATA_BLOCK_DEV" "$MOUNT_POINT"; then
+    echo "Erasing and creating JFFS2 filesystem on $DATA_CHAR_DEV.."
+    if ! flash_eraseall -j "$DATA_CHAR_DEV"; then
+      echo "Error: failed to erase $DATA_CHAR_DEV!"
+      exit 1
+    fi
+
+    echo "Re-mounting $DATA_BLOCK_DEV to $MOUNT_POINT.."
+    if ! mount -t jffs2 "$DATA_BLOCK_DEV" "$MOUNT_POINT"; then
+      echo "Error: failed to mount $DATA_BLOCK_DEV! Exiting!"
+      exit 1
+    fi
   fi
 fi
 

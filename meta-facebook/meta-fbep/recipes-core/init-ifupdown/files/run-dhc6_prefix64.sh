5bb7197e1a353db205ec021a742398719301afa3
Amithash Prasad <amithash@fb.com>
2020-06-04 18:09:44 -0700

    fbep: fix dhcp client init for IPv4/IPv6 (#1705)
    Summary:
    Since the interface usb0 is emulated by AST2500, busybox will fail to initialize the usb0
    Launch IPv4 dhcp client after probing g_ether and modify the interface name in run-dhc6_prefix64 to usb0
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1705
    
    Test Plan:
    Build pass and test on fbep (with fbal which setup IP-bridge)
    
    // Check if dhclient alive
    root@bmc-oob:~# ps | grep usb0
     2109 root     34328 S    dhclient -d -pf /var/run/dhclient.usb0.pid usb0
     2149 root     34328 S    dhclient -6 -d -D LL --address-prefix-len 64 -pf /var/run/dhclient6.usb0.pid usb0
     8456 root      3008 S    grep usb0
    
    // Check usb0 status (MAC address is set to 78:03:9B:96:FC:99)
    root@bmc-oob:~# ifconfig usb0
    usb0      Link encap:Ethernet  HWaddr 78:03:9B:96:FC:99
              inet addr:10.10.12.255  Bcast:10.10.255.255  Mask:255.255.0.0
              inet6 addr: fe80::7a03:9bff:fe96:fc99/64 Scope:Link
              inet6 addr: 2001:dd8:1:0:7a03:9bff:fe96:fc99/64 Scope:Global
              inet6 addr: 2001:db8:0:1:15cc:8b6a:5723:a90a/64 Scope:Global
              UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
              RX packets:80082 errors:0 dropped:0 overruns:0 frame:0
              TX packets:59 errors:0 dropped:13 overruns:0 carrier:0
              collisions:0 txqueuelen:1000
              RX bytes:8495078 (8.1 MiB)  TX bytes:5820 (5.6 KiB)
    
    // Ping to external PC (IPv6)
    root@bmc-oob:~# ping6 2001:dd8:1:0:f42e:9083:7b4b:2216
    PING 2001:dd8:1:0:f42e:9083:7b4b:2216 (2001:dd8:1:0:f42e:9083:7b4b:2216): 56 data bytes
    64 bytes from 2001:dd8:1:0:f42e:9083:7b4b:2216: seq=0 ttl=64 time=1.825 ms
    64 bytes from 2001:dd8:1:0:f42e:9083:7b4b:2216: seq=1 ttl=64 time=1.330 ms
    64 bytes from 2001:dd8:1:0:f42e:9083:7b4b:2216: seq=2 ttl=64 time=1.170 ms
    64 bytes from 2001:dd8:1:0:f42e:9083:7b4b:2216: seq=3 ttl=64 time=1.095 ms
    
    // Ping to external PC (IPv4)
    root@bmc-oob:~# ping 10.10.14.69
    PING 10.10.14.69 (10.10.14.69): 56 data bytes
    64 bytes from 10.10.14.69: seq=0 ttl=64 time=5.634 ms
    64 bytes from 10.10.14.69: seq=1 ttl=64 time=1.000 ms
    64 bytes from 10.10.14.69: seq=2 ttl=64 time=1.078 ms
    64 bytes from 10.10.14.69: seq=3 ttl=64 time=1.023 ms
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 258df6b951

--- meta-facebook/meta-fbep/recipes-core/init-ifupdown/files/run-dhc6_prefix64.sh
+++ meta-facebook/meta-fbep/recipes-core/init-ifupdown/files/run-dhc6_prefix64.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Copyright 2014-present Facebook. All Rights Reserved.
+# Copyright 2015-present Facebook. All Rights Reserved.
 #
 # This program file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -17,21 +17,6 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
-
-mac=$(ipmitool i2c bus=6 0xa8 0x6 0x4 0x0)
-if [[ "$mac" == " ff ff ff ff ff ff" ]]
-then
-  addr="02:00:00:00:00:01"
-else
-  addr="$(echo $mac | cut -d " " -f 1)"
-  addr+=":$(echo $mac | cut -d " " -f 2)"
-  addr+=":$(echo $mac | cut -d " " -f 3)"
-  addr+=":$(echo $mac | cut -d " " -f 4)"
-  addr+=":$(echo $mac | cut -d " " -f 5)"
-  addr+=":$(echo $mac | cut -d " " -f 6)"
-fi
-modprobe g_ether host_addr=02:00:00:00:00:02 dev_addr=$addr
-
-ifconfig usb0 up
-
-dhclient -d -pf /var/run/dhclient.usb0.pid usb0 > /dev/null 2>&1 &
+logger "run-dhc6.sh: dhclient -6 Started.."
+pid="/var/run/dhclient6.usb0.pid"
+exec dhclient -6 -d -D LL --address-prefix-len 64 -pf ${pid} usb0 "$@"

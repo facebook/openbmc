9ff5f85fd33ba2bf40dffb67390efbf8d8878f0e
Amithash Prasad <amithash@fb.com>
2020-06-17 09:17:03 -0700

    fbal: add pdb and nic sensor health (#1735)
    Summary:
    1. add FRU_PDB to server_sensor_health.
    2. add NIC0 and NIC1 sensor health.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1735
    
    Test Plan:
    1.Build and test pass on fbal system.
    
    root@bmc-oob:~# threshold-util nic0 --set 0x10 LCR 80
    root@bmc-oob:~# threshold-util nic1 --set 0x11 LCR 80
    
    root@bmc-oob:~# sensor-util nic0
    NIC_MEZZ0_TEMP               (0x10) :   77.00 C     | (lcr)
    root@bmc-oob:~# sensor-util nic1
    NIC_MEZZ1_TEMP               (0x11) :   63.00 C     | (lcr)
    
    root@bmc-oob:~# cfg-util dump-all
    pwr_server_last_state:on
    sysfw_ver_server:0000084630435f32413034000000000000
    identify_sled:off
    timestamp_sled:1592301725
    server_por_cfg:lps
    server_sensor_health:1
    nic0_sensor_health:0
    nic1_sensor_health:0
    server_sel_error:1
    server_boot_order:0100090203ff
    cpu0_ppin:
    cpu1_ppin:
    ntp_server:
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 457bfbf5fd

--- meta-facebook/meta-fbal/recipes-fbal/plat-libs/files/pal/pal_health.c
+++ meta-facebook/meta-fbal/recipes-fbal/plat-libs/files/pal/pal_health.c
@@ -15,7 +15,14 @@ static int
 pal_get_sensor_health_key(uint8_t fru, char *key) {
   switch (fru) {
     case FRU_MB:
+    case FRU_PDB:
       sprintf(key, "server_sensor_health");
+      break;
+    case FRU_NIC0:
+      sprintf(key, "nic0_sensor_health");
+      break;
+    case FRU_NIC1:
+      sprintf(key, "nic1_sensor_health");
       break;
     default:
       return -1;

8d6c59923a966a497f69e77d814f2392ab4832f0
Tao Ren <taoren@fb.com>
2020-06-23 16:07:50 -0700

    wedge: set_fan_speed.sh: move fan-pwm-ops to functions
    Summary:
    Move fan control logic to functions in "set_fan_speed.sh" so it's easier
    to support new kernel (with different pwm sysfs interface).
    
    Test Plan:
    1) command output in wedge kernel 4.1:
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh
    Usage: ./set_fan_speed.sh <PERCENT (0..100)> <Fan Unit (0..3)>
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh 50
    Successfully set fan 0 (PWM: 7) speed to 50%
    Successfully set fan 1 (PWM: 6) speed to 50%
    Successfully set fan 2 (PWM: 0) speed to 50%
    Successfully set fan 3 (PWM: 1) speed to 50%
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh 50 0
    Successfully set fan 0 (PWM: 7) speed to 50%
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh 50 1
    Successfully set fan 1 (PWM: 6) speed to 50%
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh 50 2
    Successfully set fan 2 (PWM: 0) speed to 50%
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh 50 3
    Successfully set fan 3 (PWM: 1) speed to 50%
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./set_fan_speed.sh 50 4
    Usage: ./set_fan_speed.sh <PERCENT (0..100)> <Fan Unit (0..3)>
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: bee3874b7c

--- meta-facebook/meta-wedge/recipes-wedge/fan-ctrl/fan-ctrl/set_fan_speed.sh
+++ meta-facebook/meta-wedge/recipes-wedge/fan-ctrl/fan-ctrl/set_fan_speed.sh
@@ -17,40 +17,78 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
+set -e
+
+if uname -r | grep "4\.1\.*" > /dev/null 2>&1; then
+    PWM_SYSFS_DIR="/sys/devices/platform/ast_pwm_tacho.0"
+
+    #
+    # Refer to "init_pwm.sh" for more details about pwm configuration as
+    # well as fan-pwm mapping.
+    #
+    PWM_UNIT_MAX=96
+    FAN_PWM_MAP="0:7 1:6 2:0 3:1"
+fi
+
 usage() {
     echo "Usage: $0 <PERCENT (0..100)> <Fan Unit (0..3)> " >&2
 }
 
-PWM_DIR=/sys/devices/platform/ast_pwm_tacho.0
+set_fan_pwm_41() {
+    pwm_id="$1"
+    unit="$2"
 
-# The maximum unit setting.
-# This should be the value in pwm_type_m_unit plus 1
-PWM_UNIT_MAX=96
+    if [ "$unit" -eq 0 ]; then
+        # For 0%, turn off the PWM entirely
+        echo 0 > "$PWM_SYSFS_DIR/pwm${pwm_id}_en"
+    else
+        if [ "$unit" -eq $PWM_UNIT_MAX ]; then
+            # For 100%, set falling and rising to the same value
+            unit=0
+        fi
 
-set -e
+        # always use type M. refer to the comments in init_pwm.sh
+        echo 0 > "$PWM_SYSFS_DIR/pwm${pwm_id}_type"
+        echo 0 > "$PWM_SYSFS_DIR/pwm${pwm_id}_rising"
+        echo "$unit" > "$PWM_SYSFS_DIR/pwm${pwm_id}_falling"
+        echo 1 > "$PWM_SYSFS_DIR/pwm${pwm_id}_en"
+    fi
+}
+
+set_fan_speed() {
+    pwm_id="$1"
+    percent="$2"
+    unit=$((percent * PWM_UNIT_MAX / 100))
+
+    if uname -r | grep "4\.1\.*" > /dev/null 2>&1; then
+        set_fan_pwm_41 "$pwm_id" "$unit"
+    fi
+}
 
 if [ "$#" -ne 2 ] && [ "$#" -ne 1 ]; then
     usage
     exit 1
 fi
+PERCENT="$1"
+FAN_ID="$2"
 
 # refer to the comments in init_pwn.sh regarding
 # the fan unit and PWM mapping
 if [ "$#" -eq 1 ]; then
-    PWMS="0:7 1:6 2:0 3:1"
+    FAN_PWMS="${FAN_PWM_MAP}"
 else
-    case "$2" in
+    case "$FAN_ID" in
     "0")
-        PWMS="0:7"
+        FAN_PWMS=$(echo "$FAN_PWM_MAP" | cut -d' ' -f1)
         ;;
     "1")
-        PWMS="1:6"
+        FAN_PWMS=$(echo "$FAN_PWM_MAP" | cut -d' ' -f2)
         ;;
     "2")
-        PWMS="2:0"
+        FAN_PWMS=$(echo "$FAN_PWM_MAP" | cut -d' ' -f3)
         ;;
     "3")
-        PWMS="3:1"
+        FAN_PWMS=$(echo "$FAN_PWM_MAP" | cut -d' ' -f4)
         ;;
     *)
         usage
@@ -59,27 +97,10 @@ else
     esac
 fi
 
-# Convert the percentage to our 1/96th unit.
-unit=$(( ( $1 * $PWM_UNIT_MAX ) / 100 ))
+for fan_pwm in $FAN_PWMS; do
+    fan_id=${fan_pwm%%:*}
+    pwm_id=${fan_pwm##*:}
 
-for FAN_PWM in $PWMS; do
-    FAN_N=${FAN_PWM%%:*}
-    PWM_N=${FAN_PWM##*:}
-    if [ "$unit" -eq 0 ]; then
-        # For 0%, turn off the PWM entirely
-        echo 0 > $PWM_DIR/pwm${PWM_N}_en
-    else
-        if [ "$unit" -eq $PWM_UNIT_MAX ]; then
-            # For 100%, set falling and rising to the same value
-            unit=0
-        fi
-
-        # always use type M. refer to the comments in init_pwm.sh
-        echo 0 > $PWM_DIR/pwm${PWM_N}_type
-        echo 0 > $PWM_DIR/pwm${PWM_N}_rising
-        echo "$unit" > $PWM_DIR/pwm${PWM_N}_falling
-        echo 1 > $PWM_DIR/pwm${PWM_N}_en
-    fi
-
-    echo "Successfully set fan ${FAN_N} (PWM: $PWM_N) speed to $1%"
+    set_fan_speed "$pwm_id" "$PERCENT"
+    echo "Successfully set fan ${fan_id} (PWM: $pwm_id) speed to $percent%"
 done

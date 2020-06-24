ae3f77c1f04d0788d2ce3c42e53535bda7cf4aef
Tao Ren <taoren@fb.com>
2020-06-24 12:08:35 -0700

    wedge: get_fan_speed.sh: move fan-tacho-ops to functions
    Summary:
    Move fan tacho operations to functions so it's easier to support new
    kernel with different sysfs interfaces.
    
    Test Plan:
    command output in wedge kernel 4.1:
    
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./get_fan_speed.sh
    Fan 0 RPMs: 9852,  7187
    Fan 1 RPMs: 10002,  7002
    Fan 2 RPMs: 9791,  7311
    Fan 3 RPMs: 9933,  7223
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./get_fan_speed.sh 0
    Fan 0 RPMs: 9852,  7190
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./get_fan_speed.sh 1
    Fan 1 RPMs: 10003,  7001
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./get_fan_speed.sh 2
    Fan 2 RPMs: 9800,  7312
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:/tmp# ./get_fan_speed.sh 3
    Fan 3 RPMs: 9937,  7238
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: 5f9abd22ad

--- meta-facebook/meta-wedge/recipes-wedge/fan-ctrl/fan-ctrl/get_fan_speed.sh
+++ meta-facebook/meta-wedge/recipes-wedge/fan-ctrl/fan-ctrl/get_fan_speed.sh
@@ -17,30 +17,59 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
+set -e
+
+if uname -r | grep "4\.1\.*" > /dev/null 2>&1; then
+    PWM_SYSFS_DIR="/sys/devices/platform/ast_pwm_tacho.0"
+
+    #
+    # Refer to "init_pwm.sh" for more details about fan-tacho mapping.
+    #
+    FAN_TACHO_MAP="0:3,7 1:2,6 2:0,4 3:1,5"
+fi
+
 usage() {
     echo "Usage: $0 [Fan Unit (0..3)]" >&2
 }
 
-PWM_DIR=/sys/devices/platform/ast_pwm_tacho.0
-set -e
+get_fan_speed_41() {
+    fan_id="$1"
+    front_tacho="$2"
+    rear_tacho="$3"
+
+    front_rpm=$(cat "${PWM_SYSFS_DIR}/tacho${front_tacho}_rpm")
+    rear_rpm=$(cat "${PWM_SYSFS_DIR}/tacho${rear_tacho}_rpm")
+    echo "Fan $fan_id RPMs: $front_rpm, $rear_rpm"
+}
+
+get_fan_speed() {
+    fan_id="$1"
+    tacho_list="$2"
+
+    front_tacho=${tacho_list%%,*}
+    rear_tacho=${tacho_list##*,}
+    if uname -r | grep "4\.1\.*" > /dev/null 2>&1; then
+        get_fan_speed_41 "$fan_id" "$front_tacho" "$rear_tacho"
+    fi
+}
 
 # refer to the comments in init_pwn.sh regarding
 # the fan unit and tacho mapping
 if [ "$#" -eq 0 ]; then
-    TACHOS="0:3 1:2 2:0 3:1"
+    FAN_TACHOS="$FAN_TACHO_MAP"
 elif [ "$#" -eq 1 ]; then
     case "$1" in
     "0")
-        TACHOS="0:3"
+        FAN_TACHOS=$(echo "$FAN_TACHO_MAP" | cut -d' ' -f1)
         ;;
     "1")
-        TACHOS="1:2"
+        FAN_TACHOS=$(echo "$FAN_TACHO_MAP" | cut -d' ' -f2)
         ;;
     "2")
-        TACHOS="2:0"
+        FAN_TACHOS=$(echo "$FAN_TACHO_MAP" | cut -d' ' -f3)
         ;;
     "3")
-        TACHOS="3:1"
+        FAN_TACHOS=$(echo "$FAN_TACHO_MAP" | cut -d' ' -f4)
         ;;
     *)
         usage
@@ -52,8 +81,9 @@ else
     exit 1
 fi
 
-for fan_tacho in $TACHOS; do
-    fan=${fan_tacho%%:*}
-    tacho=${fan_tacho##*:}
-    echo "Fan $fan RPMs:    $(cat $PWM_DIR/tacho${tacho}_rpm), $(cat $PWM_DIR/tacho$((tacho+4))_rpm)"
+for fan_tacho in $FAN_TACHOS; do
+    fan_id=${fan_tacho%%:*}
+    tacho_list=${fan_tacho##*:}
+
+    get_fan_speed "$fan_id" "$tacho_list"
 done

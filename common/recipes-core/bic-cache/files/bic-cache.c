04baaf75557dffb36472938ab0e5484de5d24b7a
Tao Ren <taoren@fb.com>
2020-07-06 13:11:36 -0700

    bic-cache: remove redundant code
    Summary: Remove redundant code and improve logging for easier maintenance.
    
    Test Plan: 1) "bic-cache" command completed successfully on minipack.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: e75d234d0c

--- common/recipes-core/bic-cache/files/bic-cache.c
+++ common/recipes-core/bic-cache/files/bic-cache.c
@@ -29,57 +29,48 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/file.h>
+#include <linux/limits.h>
 #include <openbmc/log.h>
 #include <openbmc/ipmi.h>
 #include <openbmc/ipmb.h>
 #include <openbmc/obmc-pal.h>
 #include <facebook/bic.h>
 
-
 #define LAST_RECORD_ID 0xFFFF
-#define MAX_SENSOR_NUM 0xFF
 #define BYTES_ENTIRE_RECORD 0xFF
 
 int
 fruid_cache_init(uint8_t slot_id) {
 
   int ret = 0;
-  int fru_size=0;
-  char fruid_temp_path[64];
-  char fruid_path[64];
-  char fru_name[32];
+  int fru_size = 0;
+  char fruid_path[PATH_MAX];
+  char fru_name[NAME_MAX];
 
   pal_get_fru_name(slot_id + 1, fru_name);
-  sprintf(fruid_temp_path, "/tmp/tfruid_%s.bin", fru_name);
   sprintf(fruid_path, "/tmp/fruid_%s.bin", fru_name);
 
-  ret = bic_read_fruid(slot_id, 0, fruid_temp_path, &fru_size);
+  ret = bic_read_fruid(slot_id, 0, fruid_path, &fru_size);
   if (ret) {
-    syslog(LOG_WARNING, "fruid_cache_init: bic_read_fruid returns %d, fru_size: %d\n", ret, fru_size);
+    syslog(LOG_WARNING, "failed to read fruid: ret=%d, fru_size: %d\n",
+           ret, fru_size);
   }
-
-  rename(fruid_temp_path, fruid_path);
 
   return ret;
 }
 
 void
 sdr_cache_init(uint8_t slot_id) {
-  int ret;
-  int fd;
+  int fd, ret, retry;
   size_t rlen;
   uint8_t rbuf[MAX_IPMB_RES_LEN];
-  char *path = NULL;
-  char sdr_temp_path[64];
-  char sdr_path[64];
-  char fru_name[32];
-
-  pal_get_fru_name(slot_id + 1, fru_name);
-  sprintf(sdr_temp_path, "/tmp/tsdr_%s.bin", fru_name);
-  sprintf(sdr_path, "/tmp/sdr_%s.bin", fru_name);
-
+  char sdr_path[PATH_MAX];
+  char fru_name[NAME_MAX];
   ipmi_sel_sdr_req_t req;
   ipmi_sel_sdr_res_t *res = (ipmi_sel_sdr_res_t *) rbuf;
+
+  pal_get_fru_name(slot_id + 1, fru_name);
+  snprintf(sdr_path, sizeof(sdr_path), "/tmp/sdr_%s.bin", fru_name);
 
   req.rsv_id = 0;
   req.rec_id = 0;
@@ -87,40 +78,44 @@ sdr_cache_init(uint8_t slot_id) {
   req.nbytes = BYTES_ENTIRE_RECORD;
 
   /* Read SCM's SDR records and store */
-  path = sdr_temp_path;
-  unlink(path);
-  fd = open(path, O_WRONLY | O_CREAT | O_EXCL, 0666);
+  unlink(sdr_path);
+  fd = open(sdr_path, O_WRONLY | O_CREAT | O_EXCL, 0666);
   if (fd < 0) {
-    syslog(LOG_WARNING, "%s: open fails for path: %s\n", __func__, path);
+    syslog(LOG_WARNING, "failed to open %s: %s\n", sdr_path, strerror(errno));
     return;
   }
 
   ret = pal_flock_retry(fd);
   if (ret == -1) {
-   syslog(LOG_WARNING, "%s: failed to flock on %s", __func__, path);
+   syslog(LOG_WARNING, "failed to flock %s: %s", sdr_path, strerror(errno));
    close(fd);
    return;
   }
 
+  retry = 3;
   while (1) {
+    sdr_full_t *sdr;
+
     ret = bic_get_sdr(slot_id, &req, res, &rlen);
     if (ret) {
       syslog(LOG_WARNING, "%s: bic_get_sdr returns %d\n", __func__, ret);
-      continue;
+      if (retry-- > 0) {
+        msleep(100);
+        continue;
+      }
+
+      break;
     }
 
-    sdr_full_t *sdr = (sdr_full_t *)res->data;
-
+    sdr = (sdr_full_t *)res->data;
     ret = write(fd, sdr, sizeof(sdr_full_t));
     if (ret < 0) {
-      OBMC_ERROR(errno, "write %s failed", path);
+      OBMC_ERROR(errno, "write %s failed", sdr_path);
       break;
     } else if (ret != sizeof(sdr_full_t)) {
       OBMC_WARN("data truncated (write %s): expect %i, actual %d\n",
-                path, sizeof(sdr_full_t), ret);
-      /*
-       * XXX shall we exit or continue??
-       */
+                sdr_path, sizeof(sdr_full_t), ret);
+      break;
     }
 
     req.rec_id = res->next_rec_id;
@@ -132,13 +127,10 @@ sdr_cache_init(uint8_t slot_id) {
 
   ret = pal_unflock_retry(fd);
   if (ret == -1) {
-   syslog(LOG_WARNING, "%s: failed to unflock on %s", __func__, path);
-   close(fd);
-   return;
+   syslog(LOG_WARNING, "failed to unflock %s: %s\n", sdr_path, strerror(errno));
   }
 
   close(fd);
-  rename(sdr_temp_path, sdr_path);
 }
 
 int
@@ -151,6 +143,8 @@ main (int argc, char * const argv[])
   int max_retry = 3;
 
   if (argc != 2) {
+    syslog(LOG_WARNING,
+           "invalid command line argument: <slot-id> is missing\n");
     return -1;
   }
 
@@ -160,23 +154,30 @@ main (int argc, char * const argv[])
   do {
     ret = bic_get_self_test_result(slot_id, (uint8_t *)&self_test_result);
     if (ret == 0) {
-      syslog(LOG_INFO, "bic_get_self_test_result: %X %X\n", self_test_result[0], self_test_result[1]);
+      syslog(LOG_INFO, "bic self test result: %X %X\n",
+             self_test_result[0], self_test_result[1]);
       break;
     }
     sleep(5);
-  } while (ret != 0);
+  } while (retry++ < max_retry);
+  if (ret != 0) {
+    syslog(LOG_ERR, "failed to get bic self test result. Exiting!\n");
+    return -1;
+  }
 
   /* Get uServer FRU */
+  retry = 0;
   do {
-    if (fruid_cache_init(slot_id) == 0)
+    ret = fruid_cache_init(slot_id);
+    if (ret == 0) {
       break;
+    }
 
-    retry++;
     sleep(1);
-  } while (retry < max_retry);
-
-  if (retry == max_retry)
+  } while (retry++ < max_retry);
+  if (ret != 0) {
     syslog(LOG_CRIT, "Fail on getting uServer FRU.");
+  }
 
   sdr_cache_init(slot_id);
 

d9ed43469bcb1bdff6fbbe01d611b485b1be7540
Amithash Prasad <amithash@fb.com>
2020-07-24 15:32:53 -0700

    fbep: fw-util: enable update CPLD recovery (#1814)
    Summary:
    revise cpld.cpp to add component, "pfr_cpld_rc", for updating CPLD recovery.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1814
    
    Test Plan:
    Build and test pass on fbep dvt system:
    
    fw-util mb --update pfr_cpld /tmp/pfr_capsule_PFR_max10_JG7_DVT.signed
    fw-util mb --update pfr_cpld_rc /tmp/pfr_capsule_PFR_max10_JG7_DVT.signed
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 5a175a55f2

--- meta-facebook/meta-fbep/recipes-fbep/fw-util/files/cpld.cpp
+++ meta-facebook/meta-fbep/recipes-fbep/fw-util/files/cpld.cpp
@@ -9,91 +9,33 @@
 using namespace std;
 
 class CpldComponent : public Component {
-  uint8_t pld_type;
   public:
-    CpldComponent(string fru, string comp, uint8_t type)
-      : Component(fru, comp), pld_type(type) {}
+    CpldComponent(string fru, string comp)
+      : Component(fru, comp) {}
     int print_version() {
-      int ret = -1;
-      int ifd = 0;
-      uint8_t i, tbuf[8], rbuf[8], cmds[4] = {0x7f, 0x01, 0x7e, 0x7d};
       uint8_t var[4];
-      char dev_i2c[16];
 
-      if (pld_type == MAX10_10M16) {
-        if (pal_is_pfr_active()) {
-          do {
-            sprintf(dev_i2c, "/dev/i2c-%d", PFR_MAILBOX_BUS);
-            ifd = open(dev_i2c, O_RDWR);
-            if (ifd < 0) {
-              break;
-            }
-
-            for (i = 0; i < sizeof(cmds); i++) {
-              tbuf[0] = cmds[i];
-              if ((ret = i2c_rdwr_msg_transfer(ifd, PFR_MAILBOX_ADDR, tbuf, 1, rbuf, 1))) {
-                break;
-              }
-              var[i] = rbuf[0];
-            }
-          } while (0);
-
-          if (ifd > 0) {
-            close(ifd);
-          }
-        }
-
-        if (ret) {
-          printf("PFR CPLD Version: NA\n");
+      if (!cpld_intf_open(LCMXO2_7000HC, INTF_JTAG, NULL)) {
+        // Print CPLD Version
+        if (cpld_get_ver((unsigned int *)&var)) {
+          printf("CPLD Version: NA, ");
         } else {
-          printf("PFR CPLD Version: %02X%02X%02X%02X\n", var[3], var[2], var[1], var[0]);
+          printf("CPLD Version: %02X%02X%02X%02X, ", var[3], var[2], var[1], var[0]);
         }
-      } else {
-        if (!cpld_intf_open(LCMXO2_7000HC, INTF_JTAG, NULL)) {
-          // Print CPLD Version
-          if (cpld_get_ver((unsigned int *)&var)) {
-            printf("CPLD Version: NA, ");
-          } else {
-            printf("CPLD Version: %02X%02X%02X%02X, ", var[3], var[2], var[1], var[0]);
-          }
 
-          // Print CPLD Device ID
-          if (cpld_get_device_id((unsigned int *)&var)) {
-            printf("CPLD DeviceID: NA\n");
-          } else {
-            printf("CPLD DeviceID: %02X%02X%02X%02X\n", var[3], var[2], var[1], var[0]);
-          }
-          cpld_intf_close(INTF_JTAG);
+        // Print CPLD Device ID
+        if (cpld_get_device_id((unsigned int *)&var)) {
+          printf("CPLD DeviceID: NA\n");
+        } else {
+          printf("CPLD DeviceID: %02X%02X%02X%02X\n", var[3], var[2], var[1], var[0]);
         }
+        cpld_intf_close(INTF_JTAG);
       }
 
       return 0;
     }
     int update(string image) {
-      int ret = -1, pfr_active;
-
-      if (pld_type == MAX10_10M16) {
-        pfr_active = pal_is_pfr_active();
-        if (pfr_active == PFR_UNPROVISIONED) {
-          return FW_STATUS_NOT_SUPPORTED;
-        }
-
-        if (pfr_active == PFR_ACTIVE) {
-          string dev;
-          string cmd;
-
-          if (!sys.get_mtd_name(string("stg-cpld"), dev)) {
-            return FW_STATUS_FAILURE;
-          }
-
-          sys.output << "Flashing to device: " << dev << endl;
-          cmd = "flashcp -v " + image + " " + dev;
-          ret = sys.runcmd(cmd);
-          return pal_fw_update_finished(0, _component.c_str(), ret);
-        }
-
-        return FW_STATUS_NOT_SUPPORTED;
-      }
+      int ret = -1;
 
       if (!cpld_intf_open(LCMXO2_7000HC, INTF_JTAG, NULL)) {
         ret = cpld_program((char *)image.c_str(), NULL, false);
@@ -108,5 +50,88 @@ class CpldComponent : public Component {
     }
 };
 
-CpldComponent cpld("mb", "cpld", LCMXO2_7000HC);
-CpldComponent pfr_cpld("mb", "pfr_cpld", MAX10_10M16);
+class PfrCpldComponent : public Component {
+  public:
+    PfrCpldComponent(string fru, string comp)
+      : Component(fru, comp) {}
+    int update(string image);
+    int print_version();
+};
+
+int PfrCpldComponent::update(string image) {
+  int ret;
+  string dev, cmd;
+
+  if (pal_is_pfr_active() != PFR_ACTIVE) {
+    return FW_STATUS_NOT_SUPPORTED;
+  }
+
+  if (!sys.get_mtd_name(string("stg-cpld"), dev)) {
+    return FW_STATUS_FAILURE;
+  }
+
+  sys.output << "Flashing to device: " << dev << endl;
+  cmd = "flashcp -v " + image + " " + dev;
+  ret = sys.runcmd(cmd);
+  ret = pal_fw_update_finished(0, _component.c_str(), ret);
+
+  return ret;
+}
+
+int PfrCpldComponent::print_version() {
+  int ret = -1;
+  int ifd = 0;
+  uint8_t i, tbuf[8], rbuf[8], cmds[4] = {0x7f, 0x01, 0x7e, 0x7d};
+  uint8_t ver[4];
+  char strbuf[16];
+  string comp;
+
+  if (_component == "pfr_cpld_rc") {
+    return 0;
+  }
+
+  do {
+    sprintf(strbuf, "/dev/i2c-%d", PFR_MAILBOX_BUS);
+    ifd = open(strbuf, O_RDWR);
+    if (ifd < 0) {
+      break;
+    }
+
+    for (i = 0; i < sizeof(cmds); i++) {
+      tbuf[0] = cmds[i];
+      if ((ret = i2c_rdwr_msg_transfer(ifd, PFR_MAILBOX_ADDR, tbuf, 1, rbuf, 1))) {
+        break;
+      }
+      ver[i] = rbuf[0];
+    }
+  } while (0);
+  if (ifd > 0) {
+    close(ifd);
+  }
+
+  if (ret) {
+    sprintf(strbuf, "NA");
+  } else {
+    sprintf(strbuf, "%02X%02X%02X%02X", ver[3], ver[2], ver[1], ver[0]);
+  }
+
+  comp = _component;
+  transform(comp.begin(), comp.end(),comp.begin(), ::toupper);
+  sys.output << comp << " Version: " << string(strbuf) << endl;
+
+  return 0;
+}
+
+class CpldConfig {
+  public:
+  CpldConfig() {
+    static CpldComponent cpld("mb", "cpld");
+
+    if (pal_is_pfr_active()) {
+      static PfrCpldComponent pfr_cpld("mb", "pfr_cpld");
+      static PfrCpldComponent pfr_cpld_rc("mb", "pfr_cpld_rc");
+    }
+  }
+};
+
+CpldConfig cpld_conf;

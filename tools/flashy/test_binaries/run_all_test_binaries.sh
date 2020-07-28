ce1ac68e03d5ac05e3d7e21b3a5402bbecdefc6e
Lin Hao Lee <linhaolee@fb.com>
2020-07-28 11:52:34 -0700

    flashy: Vendorise dependencies
    Summary:
    Vendorise dependencies
    - github.com/Jeffail/gabs
    - github.com/pkg/errors
    
    Test Plan:
    Unit testing and building as before works.
    
    To make sure dependencies used are ones in the vendor directory,
    rename the folders in the vendor directory to some gibberish,
    then import it accordingly in flashy. It should work.
    
    Reviewed By: kawmarco
    
    fbshipit-source-id: 2a72bec443

---
+++ tools/flashy/test_binaries/run_all_test_binaries.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2020-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+# This script runs all `.test` binaries within the folder it is located in
+set -e
+
+test_binaries=($(find . -type f -name \*.test))
+
+for t in "${test_binaries[@]}"; do
+    "$t"
+done

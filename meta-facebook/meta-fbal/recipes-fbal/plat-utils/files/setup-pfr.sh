04c4d21b1edc299f53579c8737f3f0e8fb9f809e
Amithash Prasad <amithash@fb.com>
2020-07-27 19:34:31 -0700

    zion: PFR: delayed send boot complete to CPLD (#1824)
    Summary:
    For the scenario of updating BMC recovery, CPLD will update BMC active first, and
    reset BMC right away once receiving BMC boot complete (to start updating BMC recovery).
    
    To avoid BMC being reset before logging the PFR events, delay sending the boot complete
    after the 1st round PFR state monitoring.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1824
    
    Test Plan:
    Build and test pass on zion
    
    example: update bmc_rc (fw-util bmc --update bmc_rc /tmp/fbal-bmc_signed_cap.bin)
    
    root@bmc-oob:~# log-util all --print |grep healthd
    0    all      2020-07-24 13:13:02    healthd          BMC Reboot detected - caused by reboot command
    1    mb       2020-07-24 13:13:02    healthd          PFR: BMC_RESET_DETECTED (0x07, 0x03), FRU: 1
    1    mb       2020-07-24 13:13:02    healthd          PFR: ENTER_T-1 (0x03, 0x03), FRU: 1
    1    mb       2020-07-24 13:13:02    healthd          PFR: BMC_FW_UPDATE (0x03, 0x11), FRU: 1
    1    mb       2020-07-24 13:13:02    healthd          PFR: BMC_FLASH_AUTHENTICATION (0x03, 0x06), FRU: 1
    1    mb       2020-07-24 13:13:02    healthd          PFR: ENTER_T0 (0x03, 0x09), FRU: 1
    0    all      2020-07-24 13:19:19    healthd          BMC Reboot detected
    1    mb       2020-07-24 13:19:19    healthd          PFR: T0_BOOT_COMPLETE (0x03, 0x0E), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: BMC_UPDATE_INTENT (0x07, 0x02), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: ENTER_T-1 (0x03, 0x03), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: BMC_FLASH_AUTHENTICATION (0x03, 0x06), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: PCH_FLASH_AUTHENTICATION (0x03, 0x07), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: ENTER_T0 (0x03, 0x09), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: T0_ME_BOOTED (0x03, 0x0B), FRU: 1
    1    mb       2020-07-24 13:19:19    healthd          PFR: T0_ACM_BOOTED (0x03, 0x0C), FRU: 1
    1    mb       2020-07-24 13:19:29    healthd          PFR: T0_BMC_BOOTED (0x03, 0x0A), FRU: 1
    1    mb       2020-07-24 13:19:39    healthd          PFR: T0_BOOT_COMPLETE (0x03, 0x0E), FRU: 1
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: b66f7a6efe

--- meta-facebook/meta-fbal/recipes-fbal/plat-utils/files/setup-pfr.sh
+++ meta-facebook/meta-fbal/recipes-fbal/plat-utils/files/setup-pfr.sh
@@ -19,6 +19,37 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
+# Provision Status
+for i in {1..3}; do
+  pfr=$(/usr/sbin/i2cget -y 4 0x58 0x0a b 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    pfr=$((pfr & 0x20))
+    break
+  fi
+  pfr=255
+  sleep 0.5
+done
+
+if [ $pfr -eq 255 ]; then
+  exit
+fi
+
+for i in {1..3}; do
+  c0=$(/usr/sbin/i2cget -y 4 0x58 0xc0 b 2>/dev/null)
+  [  $? -eq 0 ] && c0=$((c0 & 0xff))
+  c1=$(/usr/sbin/i2cget -y 4 0x58 0xc1 b 2>/dev/null)
+  [  $? -eq 0 ] && c1=$((c1 & 0xff))
+
+  offs=$(devmem 0x1e72120c 2>/dev/null)
+  start=$((offs & 0xff))
+  end=$(((offs >> 8) & 0xff))
+
+  sleep 1
+  if [[ "$c0" == "$start" && "$c1" == "$end" ]]; then
+    break
+  fi
+done
+
 # CHKPT_COMPLETE
 for i in {1..3}; do
   /usr/sbin/i2cset -y 4 0x58 0x0f 0x09 b > /dev/null 2>&1

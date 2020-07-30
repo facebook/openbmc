0eae3022490571882d0385dc10d8d49df7cedc2b
Lin Hao Lee <linhaolee@fb.com>
2020-07-30 06:19:38 -0700

    flashy: Add script to calculate test coverage
    Summary:
    Add script (`./scripts/calculate_test_coverage.sh`) to
    output test coverage percentage
    
    Test Plan:
    Run `./scripts/calculate_test_coverage.sh`. Output should
    be `84.1`.
    
    Reviewed By: kawmarco
    
    fbshipit-source-id: 093d8d44de

---
+++ tools/flashy/scripts/calculate_test_coverage.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Copyright 2020-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+# Run this script to calculate test coverage in percentage
+set -e
+
+# Ensure we're in the project root
+cd "$(dirname "$0")" && cd ..
+
+
+# patterns of package paths to exclude
+# NOTE: Please add a comment on why a package is excluded
+excluded_packages=(
+    # main package, no tests
+    "github.com/facebook/openbmc/tools/flashy"
+    # framework code (install)
+    "github.com/facebook/openbmc/tools/flashy/install"
+    # file-system side-effect code
+    "github.com/facebook/openbmc/tools/flashy/lib/fileutils"
+    # framework code (step)
+    "github.com/facebook/openbmc/tools/flashy/lib/step"
+    # test utilities
+    "github.com/facebook/openbmc/tools/flashy/tests"
+    # facebook-specific tests
+    "github.com/facebook/openbmc/tools/flashy/tests/facebook"
+)
+
+invert_filters=()
+for i in "${excluded_packages[@]}"; do
+    invert_filters+=( "-e \"^${i}$\"" )
+done
+
+mapfile -t included_packages < \
+    <(eval "go list ./... | grep -v ${invert_filters[*]}")
+
+go test ${included_packages[*]} -coverprofile ./cover.out > /dev/null
+go tool cover -func ./cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}'
+rm cover.out

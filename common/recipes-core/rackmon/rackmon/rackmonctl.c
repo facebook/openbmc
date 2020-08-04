6ba325259256e12a5011b6271345691948e43970
Tao Ren <taoren@fb.com>
2020-08-04 00:28:17 -0700

    rackmon: rackmonctrl code refactoring
    Summary:
    Refactoring "rackmonctrl.c" without changing its external interface, and
    the major purpose is to improve code readability and maintainability.
    
    Details include:
      - refining code logic to be more readable.
      - improving logging
    
    Test Plan:
    1) rackmond is running properly.
    2) rackmonctrl prints expected outputs:
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:~# rackmonstatus
    Monitored PSUs:
    Active on last scan:
    Next scan in 104 seconds.
    root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:~# rackmondata
    []root@dhcp6-2620-10d-c0b9-c0c-0-0-0-1d9:~# rackmonscan
    Triggering PSU scan...
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: e486e81a19

--- common/recipes-core/rackmon/rackmon/rackmonctl.c
+++ common/recipes-core/rackmon/rackmon/rackmonctl.c
@@ -21,78 +21,119 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <libgen.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/un.h>
 #include "modbus.h"
 #include "rackmond.h"
 
+static struct {
+    const char *name;
+    uint16_t type;
+} cmd_map[] = {
+    {
+        .name = "data",
+        .type = COMMAND_TYPE_DUMP_DATA_JSON,
+    },
+    {
+        .name = "status",
+        .type = COMMAND_TYPE_DUMP_STATUS,
+    },
+    {
+        .name = "force_scan",
+        .type = COMMAND_TYPE_FORCE_SCAN,
+    },
+    {
+        .name = "pause",
+        .type = COMMAND_TYPE_PAUSE_MONITORING,
+    },
+    {
+        .name = "resume",
+        .type = COMMAND_TYPE_START_MONITORING,
+    },
+
+    /*
+     * Make sure this is the last entry.
+     */
+    {
+        .name = NULL,
+        .type = COMMAND_TYPE_NONE,
+    },
+};
+
+static void usage(const char *prog_name)
+{
+    int i;
+    fprintf(stderr, "Usage: %s <command>\n", prog_name);
+    fprintf(stderr, "Available commands are:\n");
+    for (i = 0; cmd_map[i].name != NULL; i++) {
+        fprintf(stderr, " - %s\n", cmd_map[i].name);
+    }
+}
+
 int main(int argc, char **argv) {
     int error = 0;
+    int i, clisock, addr_len;
     rackmond_command cmd;
-    cmd.type = 0;
-    int clisock;
     uint16_t wire_cmd_len = sizeof(cmd);
     struct sockaddr_un rackmond_addr;
-    char *callname = argv[0];
-    char *s;
+    char *callname;
+    const char *cmd_name;
+    char readbuf[1024];
+    ssize_t n_read;
 
-    for (s = callname; *s != '\0';) {
-      if (*s++ == '/')
-        callname = s;
-    }
+    callname = basename(argv[0]);
     if (strcmp("rackmondata", callname) == 0) {
-      cmd.type = COMMAND_TYPE_DUMP_DATA_JSON;
+        cmd_name = "data";
+    } else if (strcmp("rackmonstatus", callname) == 0) {
+        cmd_name = "status";
+    } else if (strcmp("rackmonscan", callname) == 0) {
+        cmd_name = "force_scan";
+    } else {
+        if (argc < 2) {
+            usage(argv[0]);
+            return -1;
+        }
+        cmd_name = argv[1];
     }
-    if (strcmp("rackmonstatus", callname) == 0) {
-      cmd.type = COMMAND_TYPE_DUMP_STATUS;
+
+    /* Match command name with type. */
+    cmd.type = 0;
+    for (i = 0; cmd_map[i].name != NULL; i++) {
+        if (strcmp(cmd_name, cmd_map[i].name) == 0) {
+            cmd.type = cmd_map[i].type;
+            break;
+        }
     }
-    if (strcmp("rackmonscan", callname) == 0) {
-      cmd.type = COMMAND_TYPE_FORCE_SCAN;
+    if (cmd.type == 0) {
+        usage(argv[0]);
+        return -1;
     }
-    if (argc > 1 && (strcmp("data", argv[1]) == 0)) {
-      cmd.type = COMMAND_TYPE_DUMP_DATA_JSON;
-    }
-    if (argc > 1 && (strcmp("status", argv[1]) == 0)) {
-      cmd.type = COMMAND_TYPE_DUMP_STATUS;
-    }
-    if (argc > 1 && (strcmp("force_scan", argv[1]) == 0)) {
-      cmd.type = COMMAND_TYPE_FORCE_SCAN;
-    }
-    if (argc > 1 && (strcmp("pause", argv[1]) == 0)) {
-      cmd.type = COMMAND_TYPE_PAUSE_MONITORING;
-    }
-    if (argc > 1 && (strcmp("resume", argv[1]) == 0)) {
-      cmd.type = COMMAND_TYPE_START_MONITORING;
-    }
-    if(cmd.type == 0) {
-      fprintf(stderr, "Usage: %s { status | data | force_scan | pause | resume }\n", callname);
-      exit(1);
-    }
+
     clisock = socket(AF_UNIX, SOCK_STREAM, 0);
     ERR_LOG_EXIT(clisock, "failed to create socket");
 
     rackmond_addr.sun_family = AF_UNIX;
-    strcpy(rackmond_addr.sun_path, "/var/run/rackmond.sock");
-    int addr_len = strlen(rackmond_addr.sun_path) + sizeof(rackmond_addr.sun_family);
-    ERR_LOG_EXIT(connect(clisock, (struct sockaddr*) &rackmond_addr, addr_len),
+    strcpy(rackmond_addr.sun_path, RACKMON_IPC_SOCKET);
+    addr_len = strlen(rackmond_addr.sun_path) +
+               sizeof(rackmond_addr.sun_family);
+    ERR_LOG_EXIT(connect(clisock, (struct sockaddr*)&rackmond_addr, addr_len),
                  "failed to connect to socket");
 
     ERR_LOG_EXIT(send(clisock, &wire_cmd_len, sizeof(wire_cmd_len), 0),
-                 "failed to send to socket");
+                 "failed to send command-length to socket");
     ERR_LOG_EXIT(send(clisock, &cmd, wire_cmd_len, 0),
-                 "failed to send to socket");
-    char readbuf[1024];
-    ssize_t n_read;
+                 "failed to send command body to socket");
+
+    /* Read and dump response from server. */
     while((n_read = read(clisock, readbuf, sizeof(readbuf))) > 0) {
-      write(1, readbuf, n_read);
+      write(STDOUT_FILENO, readbuf, n_read);
     }
+
 cleanup:
-    if(error != 0) {
-      if(errno != 0) {
-        fprintf(stderr, "%s\n", strerror(errno));
-      }
-      error = 1;
+    if (clisock >= 0) {
+        close(clisock);
     }
     return error;
 }

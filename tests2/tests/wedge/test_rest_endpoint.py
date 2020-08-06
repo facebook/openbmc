9c80394591ca31f97305a89d13c871d9301f52ce
Tao Ren <taoren@fb.com>
2020-08-06 11:50:28 -0700

    tests2: wedge: fix rest endpoint sensors test
    Summary:
    Replace hardcoded adc and pwm rest endpoint names with auto-detected
    ones to avoid test failures during kernel upgrade.
    
    Test Plan: openbmc CIT passed: https://www.internalfb.com/intern/paste/P137990070/
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: 56eaa15270

--- tests2/tests/wedge/test_rest_endpoint.py
+++ tests2/tests/wedge/test_rest_endpoint.py
@@ -34,6 +34,12 @@ class RestEndpointTest(FbossRestEndpointTest, unittest.TestCase):
     PSU2_I2C_DEVICE = "pfe1100-i2c-7-5a"
     PSU_I2C_CMD = ["sensors | grep pfe"]
 
+    LIST_ADC_CMD = ["sensors | grep _adc"]
+    ASPEED_ADC_DEVICES = ["ast_adc-isa-0000", "ast_adc_hwmon-isa-0000"]
+
+    LIST_PWM_CMD = ["sensors | grep _pwm"]
+    ASPEED_PWM_DEVICES = ["ast_pwm-isa-0000", "aspeed_pwm_tacho-isa-0000"]
+
     # /api/sys
     def set_endpoint_sys_attributes(self):
         self.endpoint_sys_attrb = [
@@ -52,7 +58,6 @@ class RestEndpointTest(FbossRestEndpointTest, unittest.TestCase):
     # /api/sys/sensors
     def set_endpoint_sensors_attributes(self):
         self.endpoint_sensors_attrb = [
-            "ast_pwm-isa-0000",
             "ncp4200-i2c-1-60",
             "ncp4200-i2c-2-60",
             "tmp75-i2c-3-48",
@@ -61,7 +66,6 @@ class RestEndpointTest(FbossRestEndpointTest, unittest.TestCase):
             "fb_panther_plus-i2c-4-40",
             "max127-i2c-6-28",
             "ncp4200-i2c-8-60",
-            "ast_adc-isa-0000",
             "adm1278-i2c-12-10",
         ]
 
@@ -70,6 +74,16 @@ class RestEndpointTest(FbossRestEndpointTest, unittest.TestCase):
             self.endpoint_sensors_attrb.append(RestEndpointTest.PSU1_I2C_DEVICE)
         if RestEndpointTest.PSU2_I2C_DEVICE in psu_i2c_devices:
             self.endpoint_sensors_attrb.append(RestEndpointTest.PSU2_I2C_DEVICE)
+
+        adc_detected = run_shell_cmd(cmd=RestEndpointTest.LIST_ADC_CMD)
+        for name in RestEndpointTest.ASPEED_ADC_DEVICES:
+            if name in adc_detected:
+                self.endpoint_sensors_attrb.append(name)
+
+        pwm_detected = run_shell_cmd(cmd=RestEndpointTest.LIST_PWM_CMD)
+        for name in RestEndpointTest.ASPEED_PWM_DEVICES:
+            if name in pwm_detected:
+                self.endpoint_sensors_attrb.append(name)
 
     # "/api/sys/mb"
     def set_endpoint_mb_attributes(self):

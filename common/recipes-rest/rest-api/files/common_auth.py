cba2ade15edd211846bd816f08a0ef0be95337ad
Balint Csergo <death0wl@fb.com>
2020-08-07 03:32:59 -0700

    fix issue caused by previous diff
    Summary: Fix code failing with http.
    
    Test Plan:
    before applying the change:
    ```
    ➜  rest-api git:(helium) ✗ curl http://fboss8382003-oob.snc1:8080/api
    
    {"Resources": [], "Actions": [], "Information": {"reason": "AttributeError(\"'_SelectorSocketTransport' object has no attribute 'secure'\",)"}}%
    ```
    after applying the change:
    ```
    ➜  rest-api git:(helium) ✗ curl http://fboss8382003-oob.snc1:8080/api
    
    {"Actions": [], "Resources": ["bmc", "sensors", "logs", "fans"], "Information": {"version": "v0.2", "Description": "wedge100 RESTful API Entry"}}%
    ```
    
    Reviewed By: sammyblueeyes
    
    fbshipit-source-id: 918eaaf26c

--- common/recipes-rest/rest-api/files/common_auth.py
+++ common/recipes-rest/rest-api/files/common_auth.py
@@ -29,7 +29,7 @@ async def auth_required(request) -> str:
     # since authenticity of client cert is validated on the TLS level
     # If request is not secure return early with empty identity. ACL provider will catch
     # request as unauthorized due to empty client identity.
-    if not request.transport.secure:
+    if not request.secure:
         return ""
     if await _validate_cert_date(request):
         identity = await _extract_identity(request)

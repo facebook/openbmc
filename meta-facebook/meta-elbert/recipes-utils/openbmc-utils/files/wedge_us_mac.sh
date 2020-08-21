474e0360c0f3a7a7fa6063456157f32a4af99aa4
Joan Olguy Canéus <joancaneus@fb.com>
2020-08-20 17:41:36 -0700

    ELBERT: Update SUP to SCM (#58)
    Summary:
    ELBERT: Update SUP to SCM
    
    Update more SUP to SCM references
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.arista/pull/58
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: 8a31e8ea6d

--- meta-facebook/meta-elbert/recipes-utils/openbmc-utils/files/wedge_us_mac.sh
+++ meta-facebook/meta-elbert/recipes-utils/openbmc-utils/files/wedge_us_mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# ELBERT's SUP MAC address is the first address of the block
-# allocated from "Extended MAC Base" field in SUP's EEPROM.
-weutil SUP 2>&1 | grep "MAC Base" | cut -d ' ' -f 4
+# ELBERT's SCM MAC address is the first address of the block
+# allocated from "Extended MAC Base" field in SCM's EEPROM.
+weutil SCM 2>&1 | grep "MAC Base" | cut -d ' ' -f 4

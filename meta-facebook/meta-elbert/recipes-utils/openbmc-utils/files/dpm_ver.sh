63d10bb22136ade533206302ae56b3f684b40b13
Joan Olguy Canéus <joancaneus@fb.com>
2020-08-21 10:56:20 -0700

    Upstream elbert dpm ver (#54)
    Summary:
    ELBERT: Update dpm_ver
    
    Update dpm_ver to support all hardware types
    
    root@bmc-oob:~# dpm_ver.sh
    !!!
    !!! WARNING: Displaying Digital Power Manager (DPM) version may disrupt sensor reading !!!
    !!!
    SCM.: SFT013030201
    SMB.: SFT013180103
    PIM2 not present... skipping.
    PIM3 not present... skipping.
    PIM4 not present... skipping.
    PIM5 not present... skipping.
    PIM6 not present... skipping.
    PIM7 not present... skipping.
    PIM8.: SFT012990102
    PIM9 not present... skipping.
    !!! Done.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.arista/pull/54
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: 7273ad7f53

--- meta-facebook/meta-elbert/recipes-utils/openbmc-utils/files/dpm_ver.sh
+++ meta-facebook/meta-elbert/recipes-utils/openbmc-utils/files/dpm_ver.sh
@@ -12,9 +12,35 @@ show_dpm_ver() {
   echo "$name" "$dpm_ver"
 }
 echo "!!!"
-echo "!!! WARNING: Displaying DPM version may disrupt sensor reading !!!"
+echo "!!! WARNING: Displaying Digital Power Manager (DPM) version may disrupt sensor reading !!!"
 echo "!!!"
-show_dpm_ver "SUP.:" 9 0x11
-# ELBERTTODO SCD SUPPORT
-# ELBERTTODO LC SUPPORT
+
+scm_dpm_bus='9'
+scm_dpm_addr='0x11'
+smb_dpm_bus='3'
+smb_dpm_addr='0x4e'
+
+show_dpm_ver "SCM.:" "$scm_dpm_bus" "$scm_dpm_addr"
+show_dpm_ver "SMB.:" "$smb_dpm_bus" "$smb_dpm_addr"
+
+# PIM DPM
+pim_index=(0 1 2 3 4 5 6 7)
+# This is a map between PIM2-9 to associated i2c BUS
+# Currently, this doesn't map in order between PIM2-9 to bus 16-23
+pim_bus=(16 17 18 23 20 21 22 19)
+pim_dpm_addr='0x4e'
+
+for i in "${pim_index[@]}"
+do
+    # PIM numbered 2-9
+    pim=$((i+2))
+    pim_prsnt="$(head -n 1 "$SMBCPLD_SYSFS_DIR"/pim"$pim"_present)"
+    if [ "$((pim_prsnt))" -eq 1 ]; then
+        # PIM 2-9, SMBUS 16-23
+        bus_id="${pim_bus[$i]}"
+        show_dpm_ver "PIM${pim}.:" "$bus_id" "$pim_dpm_addr"
+    else
+        echo "PIM${pim} not present... skipping."
+    fi
+done
 echo "!!! Done."

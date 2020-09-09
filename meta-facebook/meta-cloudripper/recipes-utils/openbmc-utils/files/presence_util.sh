21ba1016963e7a6b1318effddbf24555b5c45e13
Mike Choi <mikechoi@fb.com>
2020-09-09 16:43:59 -0700

    =?UTF-8?q?[openbmc.celestica][PR]=20cloudripper:=20openbm?= =?UTF-8?q?c-utils:=20add=20presence=5Futil=20to=20show=20scm/fan/psu/deb?= =?UTF-8?q?=E2=80=A6?= (#646)
    Summary:
    …ug_card present status
    As title
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/646
    
    Test Plan:
    root@bmc-oob:~# presence_util.sh ?
    Usage:
         presence_util.sh
         presence_util.sh <TYPE>
         <TYPE>: scm/fan/psu/debug_card
    root@bmc-oob:~# presence_util.sh
    CARDS
    scm : 1
    FANS
    fan1 : 1
    fan2 : 1
    fan3 : 1
    fan4 : 1
    POWER SUPPLIES
    psu1 : 1
    psu2 : 1
    DEBUG CARD
    debug_card : 0
    root@bmc-oob:~# presence_util.sh scm
    scm : 1
    root@bmc-oob:~# presence_util.sh fan
    fan1 : 1
    fan2 : 1
    fan3 : 1
    fan4 : 1
    root@bmc-oob:~# presence_util.sh psu
    psu1 : 1
    psu2 : 1
    root@bmc-oob:~# presence_util.sh debug_card
    debug_card : 0
    
    Reviewed By: tao-ren
    
    fbshipit-source-id: 0e749b5dca

---
+++ meta-facebook/meta-cloudripper/recipes-utils/openbmc-utils/files/presence_util.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#
+# Copyright 2020-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+#shellcheck disable=SC1091
+. /usr/local/bin/openbmc-utils.sh
+
+SMB_DIR=$SMBCPLD_SYSFS_DIR
+SCM_CMD=$SMB_DIR"/scm_present_int_status"
+FAN_CMD=$FCMCPLD_SYSFS_DIR
+DEBUG_CARD_PRSNT_GPIO="/tmp/gpionames/DEBUG_PRESENT_N/value"
+
+function usage
+{
+  program=$(basename "$0")
+  echo "Usage:"
+  echo "     $program "
+  echo "     $program <TYPE>"
+  echo "     <TYPE>: scm/fan/psu/debug_card"
+}
+
+# Will return <key> : <val>
+# eg - scm : 1
+function get_presence
+{
+  local file=$1 key=$2
+  val=$(head -n1 "$file" | awk '{print substr($0,0,3) }' | sed "s/.x//")
+  if [ "$val" = "0" ]; then
+      val=1
+  else
+      val=0
+  fi
+  echo "$key : $val"
+}
+
+# SCM
+function get_scm_presence
+{
+  key="scm"
+  get_presence "$SCM_CMD" "$key"
+}
+
+# FANs
+function get_fan_presence
+{
+  local num=1
+  while [ $num -le 4 ]; do
+      key="fan${num}"
+      file="$FAN_CMD/fan${num}_present"
+      get_presence "$file" "$key"
+      (( num++ ))
+  done
+}
+
+# Power supplies
+function get_psu_presence
+{
+  for i in {1..2}; do
+      key="psu${i}"
+      file="$SMB_DIR/psu_present_${i}_N_int_status"
+      get_presence "$file" "$key"
+  done
+}
+
+# Debug card
+function get_debug_card_presence
+{
+  key="debug_card"
+  val=$(head -n1 $DEBUG_CARD_PRSNT_GPIO | awk '{print substr($0,0,3) }' | sed "s/.x//")
+  if [ "$val" = "0" ]; then
+      val=0
+  else
+      val=1
+  fi
+  echo $key ":" $val
+}
+
+# get all info
+if [[ $1 = "" ]]; then
+  echo "CARDS"
+  get_scm_presence
+
+  echo "FANS"
+  get_fan_presence
+
+  echo "POWER SUPPLIES"
+  get_psu_presence
+
+  echo "DEBUG CARD"
+  get_debug_card_presence
+
+  exit 0
+else
+  if [[ $1 = "scm" ]]; then
+    get_scm_presence
+  elif [[ $1 = "fan" ]]; then
+    get_fan_presence
+  elif [[ $1 = "psu" ]]; then
+    get_psu_presence
+  elif [[ $1 = "debug_card" ]]; then
+    get_debug_card_presence
+  else
+    usage
+  fi
+  exit 0
+fi

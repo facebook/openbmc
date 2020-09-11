81bbd2b292343d5720b35337bf1a73c26d8885f1
Balint Csergo <death0wl@fb.com>
2020-09-11 04:03:30 -0700

    make gpios restapi work again on wedge40s
    Summary: D23628224 will break gpios api for wedge40s. This commit fixes that
    
    Test Plan:
    Before the change:
    root@rsw1qh-oob:/usr/local/fbpackages/rest-api# curl http://localhost:8080/api/sys/gpios
    {"back_ports": {"port_1": {"pin_3": 1, "pin_2": 1, "pin_1": 1, "pin_4": 1}, "port_2": {"pin_3": 1, "pin_2": 1, "pin_1": 1, "pin_4": 1}}}
    After the change:
    root@rsw1qh-oob:/usr/local/fbpackages/rest-api# curl http://localhost:8080/api/sys/gpios
    {"back_ports": {"port_2": {"pin_2": 1, "pin_1": 1}, "port_1": {"pin_3": 1, "pin_2": 1, "pin_1": 1, "pin_4": 1}}}r
    
    Reviewed By: lsiudut, kawmarco
    
    fbshipit-source-id: 28bcba79f3

--- common/recipes-rest/rest-api/files/rest_gpios.py
+++ common/recipes-rest/rest-api/files/rest_gpios.py
@@ -19,23 +19,41 @@
 #
 
 import re
+
 from rest_fruid import get_fruid
 from rest_helper import read_gpio_by_shadow
 
 
-def read_wedge_back_ports():
-    bhinfo = {
-        "port_1": {
-            "pin_1": read_gpio_by_shadow("RMON1_PF"),
-            "pin_2": read_gpio_by_shadow("RMON1_RF"),
-            "pin_3": read_gpio_by_shadow("RMON2_PF"),
-            "pin_4": read_gpio_by_shadow("RMON2_RF"),
-        },
-        "port_2": {
-            "pin_1": read_gpio_by_shadow("RMON3_PF"),
-            "pin_2": read_gpio_by_shadow("RMON3_RF"),
-        },
-    }
+WEDGE40 = ["Wedge-DC-F", "Wedge-AC-F"]
+
+
+def read_wedge_back_ports(legacy=False):
+    if legacy:
+        bhinfo = {
+            "port_1": {
+                "pin_1": read_gpio_by_shadow("BLOODHOUND_GPIOP0"),
+                "pin_2": read_gpio_by_shadow("BLOODHOUND_GPIOP1"),
+                "pin_3": read_gpio_by_shadow("BLOODHOUND_GPIOP2"),
+                "pin_4": read_gpio_by_shadow("BLOODHOUND_GPIOP3"),
+            },
+            "port_2": {
+                "pin_1": read_gpio_by_shadow("BLOODHOUND_GPIOP4"),
+                "pin_2": read_gpio_by_shadow("BLOODHOUND_GPIOP5"),
+            },
+        }
+    else:
+        bhinfo = {
+            "port_1": {
+                "pin_1": read_gpio_by_shadow("RMON1_PF"),
+                "pin_2": read_gpio_by_shadow("RMON1_RF"),
+                "pin_3": read_gpio_by_shadow("RMON2_PF"),
+                "pin_4": read_gpio_by_shadow("RMON2_RF"),
+            },
+            "port_2": {
+                "pin_1": read_gpio_by_shadow("RMON3_PF"),
+                "pin_2": read_gpio_by_shadow("RMON3_RF"),
+            },
+        }
     return bhinfo
 
 
@@ -43,5 +61,6 @@ def get_gpios():
     fruinfo = get_fruid()
     gpioinfo = {}
     if re.match("WEDGE.*", fruinfo["Information"]["Product Name"], re.IGNORECASE):
-        gpioinfo["back_ports"] = read_wedge_back_ports()
+        is_wedge40 = fruinfo["Information"]["Product Name"] in WEDGE40
+        gpioinfo["back_ports"] = read_wedge_back_ports(is_wedge40)
     return gpioinfo

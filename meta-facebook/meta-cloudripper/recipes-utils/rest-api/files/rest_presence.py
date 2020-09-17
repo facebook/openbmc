5761392ea1886007a9243160d05aa8a1a1a02372
Mike Choi <mikechoi@fb.com>
2020-09-17 11:42:49 -0700

    =?UTF-8?q?[openbmc.celestica][PR]=20cloudripper:=20restap?= =?UTF-8?q?i:=20add=20rest=5Fpresence.py=20for=20/api/sys/presence=20endp?= =?UTF-8?q?=E2=80=A6?= (#669)
    Summary:
    …oint
    Add rest_presence.py for /api/sys/presence endpoint to get present status of SCM/PSU/FAN
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/669
    
    Test Plan:
    root@bmc-oob:~# curl -g http://127.0.0.1:8080/api/sys/presence | python /usr/lib/python3.7/json/tool.py
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100   171  100   171    0     0    335      0 --:--:-- --:--:-- --:--:--   335
    {
        "Information": [
            {
                "scm ": "1"
            },
            {
                "fan1 ": "1",
                "fan2 ": "1",
                "fan3 ": "1",
                "fan4 ": "1"
            },
            {
                "psu1 ": "1",
                "psu2 ": "1"
            }
        ],
        "Actions": [],
        "Resources": [
            "scm",
            "fan",
            "psu"
        ]
    }
    
    Reviewed By: joancaneus
    
    fbshipit-source-id: 1c40842d2b

---
+++ meta-facebook/meta-cloudripper/recipes-utils/rest-api/files/rest_presence.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+import subprocess
+from typing import Dict
+
+from rest_utils import DEFAULT_TIMEOUT_SEC
+
+
+# Handler for sys/presence resource endpoint
+def get_presence_info_scm() -> Dict:
+    return {
+        "Information": get_presence_info_data("scm"),
+        "Actions": [],
+        "Resources": [],
+    }
+
+
+def get_presence_info_fan() -> Dict:
+    return {
+        "Information": get_presence_info_data("fan"),
+        "Actions": [],
+        "Resources": [],
+    }
+
+
+def get_presence_info_psu() -> Dict:
+    return {
+        "Information": get_presence_info_data("psu"),
+        "Actions": [],
+        "Resources": [],
+    }
+
+
+def get_presence_info() -> Dict:
+    result = []
+    devices = ["scm", "fan", "psu"]
+    for dev in devices:
+        result.append(get_presence_info_data(dev))
+    return {"Information": result, "Actions": [], "Resources": devices}
+
+
+def _parse_presence_info_data(data) -> Dict:
+    result = {}
+    for sdata in data.splitlines():
+        dev = sdata.split(": ")[0]
+        presence_status = sdata.split(": ")[1]
+        result[dev] = presence_status
+    return result
+
+
+def get_presence_info_data(dev) -> Dict:
+    cmd = ["/usr/local/bin/presence_util.sh", dev]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data, _ = proc.communicate(timeout=DEFAULT_TIMEOUT_SEC)
+    data = data.decode(errors="ignore")
+    return _parse_presence_info_data(data)

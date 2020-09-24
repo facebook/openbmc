ee7de01c746a3cee08dec3d8e75b20d5f40da364
Tao Ren <taoren@fb.com>
2020-09-24 10:51:33 -0700

    wedge: use new GPIO API in setup_rov.sh
    Summary:
    Use the new gpio APIs in "setup_rov.sh" to make sure the correct gpio
    pins are accessed regardless of kernel versions.
    
    Test Plan:
    "wedge-power.sh reset -s": setup_rov.sh is executed successfully:
    
    Checking microserver power status ... off
    mTerm_log: mTerm: daemon started
    Reset USB Switch ... Done
    T2 ROV value set based on 2.
    Power on microserver ... Done
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 5f04eed41a

--- meta-facebook/meta-wedge/recipes-utils/openbmc-utils/files/setup_rov.sh
+++ meta-facebook/meta-wedge/recipes-utils/openbmc-utils/files/setup_rov.sh
@@ -26,13 +26,15 @@
 
 . /usr/local/bin/openbmc-utils.sh
 
+PMBUS_DRIVER_DIR="${SYSFS_I2C_DRIVERS}/pmbus"
+
 # read the T2 ROV after the GPIOs are enabled
 t2_rov() {
     local val0 val1 val2
     # Note that the values are *not* read in order.
-    val0=$(cat /sys/class/gpio/gpio58/value 2>/dev/null)
-    val1=$(cat /sys/class/gpio/gpio56/value 2>/dev/null)
-    val2=$(cat /sys/class/gpio/gpio57/value 2>/dev/null)
+    val0=$(gpio_get_value T2_ROV3)
+    val1=$(gpio_get_value T2_ROV1)
+    val2=$(gpio_get_value T2_ROV2)
     echo $((val0 | (val1 << 1) | (val2 << 2)))
 }
 
@@ -49,12 +51,13 @@ else
     echo "Unrecognized T2 ROV value $rov, setting failed."
     exit 1
 fi
-target_volts=$(( $target_volts * 1 ))  # normalize to decimal
+target_volts=$((target_volts * 1))  # normalize to decimal
 
-# We shouldn't have to rmmod pmbus, because it hasn't been loaded yet,
-# but if the script is rerun after the system is up, it may be necessary.
-rmmod pmbus
-reload=$?
+#
+# Unbind pmbus driver from 1-0060, otherwise, following i2cget could
+# fail with EBUSY.
+#
+echo 1-0060 > "${PMBUS_DRIVER_DIR}/unbind"
 
 # Get current voltage value.
 # The device here is NCP4200. It turns out the first i2c transaction to this
@@ -65,17 +68,17 @@ reload=$?
 # To workaround this issue, we are doing the first read twice.
 cur_volts=$(i2cget -y 1 0x60 0x8b w 2> /dev/null)
 cur_volts=$(i2cget -y 1 0x60 0x8b w)
-cur_volts=$(( $cur_volts * 1 ))  # normalize to decimal
+cur_volts=$((cur_volts * 1))  # normalize to decimal
 
 # Only bounce the T2 if we actually need to modify the voltage
 if [ $cur_volts -ne $target_volts ]; then
     # Set values before turning out output;  we're using "PCIE, then MCS"
-    echo 1 > /sys/class/gpio/gpio42/value
-    echo 1 > /sys/class/gpio/gpio43/value
-    echo out > /sys/class/gpio/gpio42/direction
-    echo out > /sys/class/gpio/gpio43/direction
-    echo 0 > /sys/class/gpio/gpio16/value
-    echo out > /sys/class/gpio/gpio16/direction
+    gpio_set_value RESET_SEQ1 1
+    gpio_set_value RESET_SEQ0 1
+    gpio_set_direction RESET_SEQ1 out
+    gpio_set_direction RESET_SEQ0 out
+    gpio_set_value T2_RESET_N 0
+    gpio_set_direction T2_RESET_N out
     # T2 is in reset;  note that this may cause NMI messages on the uServer,
     # which shouldn't be up anyway when this is first run.
 
@@ -92,15 +95,16 @@ if [ $cur_volts -ne $target_volts ]; then
     sleep 1
 
     # Let T2 come out of reset
-    echo 1 > /sys/class/gpio/gpio16/value
+    gpio_set_value T2_RESET_N 1
     echo "T2 ROV value set based on $rov."
     sleep 2
-    echo 0 > /sys/class/gpio/gpio42/value
-    echo 0 > /sys/class/gpio/gpio43/value
+    gpio_set_value RESET_SEQ1 0
+    gpio_set_value RESET_SEQ0 0
 else
     echo "T2 ROV already correctly set."
 fi
-# Bring back pmbus if necessary
-if [ $reload -eq 0 ]; then
-    modprobe pmbus
-fi
+
+#
+# Bind pmbus driver to 1-0060.
+#
+echo 1-0060 > "${PMBUS_DRIVER_DIR}/bind"

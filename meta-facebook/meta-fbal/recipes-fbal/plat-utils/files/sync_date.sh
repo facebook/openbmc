f196c62ff25e1f21162bcccb53e648959ac3e205
Patrick Williams <patrickw3@fb.com>
2020-10-07 13:51:28 -0700

    FBAL:Support 4S mode time sync (#1999)
    Summary:
    slave bmc use ipmi bypass cmd to get time from master me.
    if sync fail, slave bmc will get time from master bmc
    
    TestPlan:
      platform:FBALw
      Kenrel:5.0.3
    
    Syncing up BMC time with server...
    Tue Jun  9 18:45:03 PDT 2020
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/1999
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 69b248b979

--- meta-facebook/meta-fbal/recipes-fbal/plat-utils/files/sync_date.sh
+++ meta-facebook/meta-fbal/recipes-fbal/plat-utils/files/sync_date.sh
@@ -19,7 +19,7 @@
 #
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
-
+. /usr/local/fbpackages/utils/ast-functions
 # Sync BMC's date with the server
 sync_date()
 {
@@ -27,21 +27,44 @@ sync_date()
   # if no ntp peer has been declared the system peer
   if ! /usr/sbin/ntpq -p | grep '^\*' > /dev/null ; then
     echo Syncing up BMC time with server...
+
+    position=$(($(gpio_get FM_BLADE_ID_0)))
+    mode=$(($(gpio_get FM_BMC_SKT_ID_0)))
+    if [ $position -eq 0 ]; then #position 0
+      let addr=$((16#22))
+    else
+      let addr=$((16#20))
+    fi
+
     # Use standard IPMI command 'get-sel-time' to read RTC time
     for i in {1..10};
-    do
-      output=$(/usr/local/bin/ipmb-util 5 0x2c 0x28 0x48)
-     # echo Peter DBG $output
-      if [ ${#output} == 12 ] 
-      then
-        break;
+    do 
+      if [ $mode -eq 0 ]; then     
+        output=$(/usr/local/bin/ipmb-util 5 0x2c 0x28 0x48)
+        if [ ${#output} == 12 ] 
+        then
+          break;
+        fi
+
+        if [ ${i} == 10 ]
+        then
+          exit
+        fi 
+
+      else
+        output=$(/usr/local/bin/ipmb-util 2 $addr 0xC0 0x34 0x01 0x0a 0x48)
+        if [ ${#output} == 12 ] 
+        then
+          break;
+        fi
+       
+        if [ ${i} == 10 ]
+        then
+          echo Sync time with Master BMC
+          output=$(/usr/local/bin/ipmb-util 2 $addr 0x28 0x48)
+        fi 
       fi
       usleep 300
-
-      if [ ${i} == 10 ]
-      then
-        exit
-      fi
     done
     date -s @$((16#$(echo "$output" | awk '{print $4$3$2$1}')))
     test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop

70d3308c973fc4d534ee9d1e45ebdb08bfcefafa
Luis Fernando Muñoz Mejías <lfm@fb.com>
2020-10-28 07:14:44 -0700

    Test that syslog stores messages
    Summary: One of the issues we've found with
    
    Test Plan:
    Copied and run it on a wedge100
    TODO: Enable this on all platforms
    
    Reviewed By: vineelasmile
    
    fbshipit-source-id: 0d8ec522ed

---
+++ tests2/tests/wedge100/test_syslog.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of
+# the GNU General Public License version 2.
+import unittest
+
+from common.base_test_syslog import BaseTestSyslog
+
+
+class TestSyslog(BaseTestSyslog, unittest.TestCase):
+    pass

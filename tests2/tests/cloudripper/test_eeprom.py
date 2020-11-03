206afa997477a8cadd729f4a0b0f9561d7d05896
Tao Ren <taoren@fb.com>
2020-11-02 22:41:50 -0800

    test2: cloudripper: add test_eeprom.py (#793)
    Summary:
    Add eeprom test case for cloudripper.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/793
    
    Test Plan:
    ```
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py --run-test tests.cloudripper.test_eeprom
    test_asset_tag (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_extended_mac_base (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_local_mac (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_location_on_fabric (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_odm_pcb (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_product_name (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_product_part_number (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_product_serial_number (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_system_manufacturer (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_version (tests.cloudripper.test_eeprom.EepromTest) ... ok
    test_asset_tag (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_extended_mac_base (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_local_mac (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_location_on_fabric (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_odm_pcb (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_product_name (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_product_part_number (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_product_serial_number (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_system_manufacturer (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    test_version (tests.cloudripper.test_eeprom.FAN1EepromTest) ... ok
    ...
    test_asset_tag (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_extended_mac_base (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_local_mac (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_location_on_fabric (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_odm_pcb (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_product_name (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_product_part_number (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_product_serial_number (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_system_manufacturer (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_version (tests.cloudripper.test_eeprom.FCMEepromTest) ... ok
    test_asset_tag (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_extended_mac_base (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_local_mac (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_location_on_fabric (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_odm_pcb (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_product_name (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_product_part_number (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_product_serial_number (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_system_manufacturer (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    test_version (tests.cloudripper.test_eeprom.SCMEepromTest) ... ok
    
    ----------------------------------------------------------------------
    Ran 80 tests in 73.574s
    
    OK
    ```
    
    Reviewed By: benwei13
    
    fbshipit-source-id: ce1c12bf55

---
+++ tests2/tests/cloudripper/test_eeprom.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_eeprom_test import CommonEepromTest
+
+
+class EepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for weutil
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/bin/weutil"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-SMB"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["SMB"]
+
+
+class BMCEepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for beutil
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/beutil"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-BMC"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["BMC"]
+
+
+class SCMEepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for seutil
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/seutil"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-SCM"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["SCM"]
+
+
+class FCMEepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for feutil fcm
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/feutil fcm"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-FCM"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["FCM"]
+
+
+class FAN1EepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for feutil fan 1
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/feutil 1"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-FAN"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["FAN"]
+
+
+class FAN2EepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for feutil fan 2
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/feutil 2"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-FAN"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["FAN"]
+
+
+class FAN3EepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for feutil fan 3
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/feutil 3"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-FAN"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["FAN"]
+
+
+class FAN4EepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for feutil fan 4
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["/usr/local/bin/feutil 4"]
+
+    def set_product_name(self):
+        self.product_name = ["CLOUDRIPPER-FAN"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["FAN"]

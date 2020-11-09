51047ccc52d4263c9c796538a4bbb1d341f95283
Luis Fernando Muñoz Mejías <lfm@fb.com>
2020-11-09 06:25:38 -0800

    Make the ntp endpoint work with systemd
    Summary:
    Present some fake statistics, but at least show that time is
    synchronized.
    
    Introduces an `offset` field. The swagger definition has been updated
    accordingly.
    
    Test Plan:
    Tested for regressions on a wedge100. On a systemd image we
    get:
    
    ```
    $ curl -s http://localhost:8080/api/sys/ntp|python -mjson.tool
    {
        "Actions": [],
        "Resources": [],
        "Information": {
            "offset": 50,
            "stratum": 2,
            "jitter": 50,
            "delay": 50
        }
    }
    ```
    
    Reviewed By: deathowl
    
    fbshipit-source-id: add229601b

--- common/recipes-rest/rest-api/files/rest_ntpstatus.py
+++ common/recipes-rest/rest-api/files/rest_ntpstatus.py
@@ -20,8 +20,10 @@
 
 from subprocess import check_output
 
+from rest_utils import running_systemd
 
-def get_ntp_stats():
+
+def get_ntpq_stats():
     try:
         cmd = "/usr/sbin/ntpq -6 -p"
         data = check_output(cmd, shell=True)
@@ -38,11 +40,62 @@ def get_ntp_stats():
             ntpstats["poll"] = float(parts[5])
             ntpstats["reach"] = float(parts[6])
             ntpstats["delay"] = float(parts[7])
+            ntpstats["offset"] = float(parts[8])
             ntpstats["jitter"] = float(parts[9])
             return {
                 "data": {"Information": ntpstats, "Actions": [], "Resources": []},
                 "status": 200,
             }
+        return {
+            "data": {"Information": {"reason": data}, "Actions": [], "Resources": []},
+            "status": 404,
+        }
+    except Exception as ex:
+        return {
+            "data": {
+                "Information": {"reason": repr(ex)},
+                "Actions": [],
+                "Resources": [],
+            },
+            "status": 500,
+        }
+
+
+def get_ntp_stats():
+    if running_systemd():
+        return fudge_timedatectl_stats()
+    else:
+        return get_ntpq_stats()
+
+
+def fudge_timedatectl_stats():
+    # timedatectl on rocko displays far less information than ntpq. We
+    # don't have show-timesync either. We print some fake data that
+    # looks alarming if synchronization is bad and looks sane if time
+    # is correctly synchronized.
+    cmd = "/usr/bin/timedatectl"
+    try:
+        data = check_output(cmd.split())
+
+        data = data.decode()
+
+        # Default bad statistics, pointing that there is no real
+        # synchronization...
+        ntpstats = {"stratum": 16, "delay": 5000, "jitter": 5000, "offset": 5000}
+
+        for line in data.splitlines():
+            # Handle differences in whitespaces...
+            if "NTP synchronized: yes" in line:
+                ntpstats = {
+                    "delay": 50,
+                    "jitter": 50,
+                    "stratum": 2,
+                    "offset": 50,
+                }
+                return {
+                    "data": {"Information": ntpstats, "Actions": [], "Resources": []},
+                    "status": 200,
+                }
         return {
             "data": {"Information": {"reason": data}, "Actions": [], "Resources": []},
             "status": 404,

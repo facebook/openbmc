2680cafca5eb5e4323941b0cc48e6e167ba7d823
Tao Ren <taoren@fb.com>
2020-11-12 15:20:48 -0800

    galaxy100: openbmc-utils: fix gpio handling in at93cx6_util.sh
    Summary:
    Improve gpio handling logic so the script works in both legacy and the
    latest kernel.
    
    Test Plan: "at93cx6_util.sh chip read --file /tmp/eeprom.bin" succeeded.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: f2b51fcb82

--- meta-facebook/meta-galaxy100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
+++ meta-facebook/meta-galaxy100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
-source /usr/local/fbpackages/utils/ast-functions
+# shellcheck disable=SC1091
+. /usr/local/bin/openbmc-utils.sh
 
 #At this way, it uses to GPIO mode
 devmem_clear_bit 0x1e6e2070 12
 devmem_clear_bit 0x1e6e2070 13
 
-gpio_export I4
-gpio_export I5
-gpio_export I6
-gpio_export I7
+if [ ! -L "${SHADOW_GPIO}/BMC_EEPROM1_SPI_SS" ]; then
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI4 BMC_EEPROM1_SPI_SS
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI5 BMC_EEPROM1_SPI_SCK
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI6 BMC_EEPROM1_SPI_MOSI
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI7 BMC_EEPROM1_SPI_MISO
+fi
 
-gpio_set E2 1
+gpio_set_value SWITCH_EEPROM1_WRT 1
 
-at93cx6_util.py $@
+/usr/local/bin/at93cx6_util_py3.py --cs BMC_EEPROM1_SPI_SS \
+                                   --clk BMC_EEPROM1_SPI_SCK \
+                                   --mosi BMC_EEPROM1_SPI_MOSI \
+                                   --miso BMC_EEPROM1_SPI_MISO $@
 
 #resolve to SPI mode for another flash using(backup flash)
 devmem_set_bit 0x1e6e2070 12

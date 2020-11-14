e2c4f448101c411fe44532860e60f7ed714dbc3d
Luis Fernando Muñoz Mejías <lfm@fb.com>
2020-11-13 16:56:06 -0800

    Re-use base class on all platforms
    Summary:
    These tests are identical everywhere, let's use the logic in
    the base class...
    
    Test Plan: Tested on a wedge100.
    
    Reviewed By: doranand
    
    fbshipit-source-id: 0f58e810a0

--- tests2/tests/elbert/test_watchdog.py
+++ tests2/tests/elbert/test_watchdog.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python3
-#
-# Copyright 2020-present Facebook. All Rights Reserved.
-#
-# This program file is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program in a file named COPYING; if not, write to the
-# Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301 USA
-#
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
 import unittest
 
 from common.base_watchdog_test import WatchdogTest
 
 
-@unittest.skip("ELBERTODO Test not supported on platform yet")
 class WatchdogTest(WatchdogTest, unittest.TestCase):
-    def set_kill_watchdog_daemon_cmd(self):
-        self.kill_watchdog_daemon_cmd = ["/usr/bin/sv stop fscd"]
-
-    def set_start_watchdog_daemon_cmd(self):
-        self.start_watchdog_daemon_cmd = ["/usr/bin/sv start fscd"]
+    pass

5c5fd7406929ec469c88d2f56d0be6808f18e4ee
Luis Fernando Muñoz Mejías <lfm@fb.com>
2020-11-13 16:54:43 -0800

    Adapt copyright header
    Summary: This is the new copyright header legal wants us to have...
    
    Test Plan: Just a comment
    
    Reviewed By: doranand
    
    fbshipit-source-id: 08d613b3ea

--- tests2/tests/wedge100/test_watchdog.py
+++ tests2/tests/wedge100/test_watchdog.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python3
-#
-# Copyright 2018-present Facebook. All Rights Reserved.
-#
-# This program file is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program in a file named COPYING; if not, write to the
-# Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301 USA
-#
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2.
+
 import unittest
 
 from common.base_watchdog_test import WatchdogTest

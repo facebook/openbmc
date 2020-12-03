6dc62cedb62ec347c616673b21de225d7d8ff6e3
Patrick Williams <patrickw3@fb.com>
2020-12-03 06:55:39 -0800

    fbep: Adjust setting for overcurrent warning of P12V HSC (#2123)
    Summary:
    Since current setting for overcurrent warning is too high and it is disabled too
    Quanta power team asked BMC to overwrite the setting to ~150 Amps and enable it
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2123
    
    Test Plan:
    Build fbep pass and tested by Quanta power team
    
    root@bmc-oob:~# i2cget -f -y 16 0x53 0xd
    0x60
    root@bmc-oob:~# i2cget -f -y 17 0x40 0xd
    0x60
    root@bmc-oob:~# i2cget -f -y 16 0x53 0x3
    0x20
    root@bmc-oob:~# i2cget -f -y 17 0x40 0x3
    0x20
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: ec0582d750

--- meta-facebook/meta-fbep/recipes-fbep/plat-utils/files/workaround.sh
+++ meta-facebook/meta-fbep/recipes-fbep/plat-utils/files/workaround.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 # TODO:
-# 	Remove this workwround at DVT stage
+# 	Remove this workwround at PVT entry
 
 echo "Overwrite the register 0x1 of AUX HSC with 0x2"
-i2cset -f -y 5 0x70 0x4
-i2cset -f -y 5 0x43 0x1 0x2
+i2cset -f -y 18 0x43 0x1 0x2
+
+# Set the OCW of P12V_1/P12V_2 HSC to ~150 Amps then enable it
+# TODO:
+#	If necessary, shall we add the feature in driver probe for DTS?
+
+i2cset -f -y 17 0x40 0xD 0x60
+i2cset -f -y 16 0x53 0xD 0x60
+i2cset -f -y 17 0x40 0x3 0x20
+i2cset -f -y 16 0x53 0x3 0x20

1541e16f5a47bf439e77b9c72a1d36891c23f803
Patrick Williams <patrickw3@fb.com>
2020-12-11 11:10:17 -0800

    fbcc: fix PAX FW update by fw-util (#2171)
    Summary:
    Since we has been changed SPI2 to bitbang. So fix the spipath for fw-util update.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2171
    
    Test Plan:
    Update PAX flash from pax0 to pax3 and pass
    
    root@bmc-oob:/# fw-util mb --update pax1-flash /tmp/F0CE_DVT_20201016_Rev2.bin
    [ 2128.754623] spi-nor spi0.0: n25q128a11 (16384 Kbytes)
    Flashing to device: /dev/mtd13
    Erasing block: 4096/4096 (100%)
    Writing kb: 16384/16384 (100%)
    Verifying kb: 16384/16384 (100%)
    Upgrade of mb : pax0-flash succeeded
    
    Reviewed By: benwei13
    
    fbshipit-source-id: e2d796fa75

--- meta-facebook/meta-clearcreek/recipes-fbcc/fw-util/files/switch.cpp
+++ meta-facebook/meta-clearcreek/recipes-fbcc/fw-util/files/switch.cpp
@@ -9,7 +9,9 @@ class PAXComponent : public GPIOSwitchedSPIMTDComponent {
   uint8_t _paxid;
   public:
     PAXComponent(string fru, string comp, uint8_t paxid, std::string shadow)
-      : GPIOSwitchedSPIMTDComponent(fru, comp, "switch0", "spi1.0", shadow, true), _paxid(paxid) {}
+      : GPIOSwitchedSPIMTDComponent(fru, comp, "switch0", "spi0.0", shadow, true), _paxid(paxid) {
+          spipath = "/sys/bus/spi/drivers/spi-nor";
+        }
     int print_version() override;
     int update(string image) override;
 };

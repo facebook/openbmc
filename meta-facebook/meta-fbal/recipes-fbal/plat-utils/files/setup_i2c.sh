ab88d08d9acc00261b641da1515677acbfeb5ab9
Patrick Williams <patrickw3@fb.com>
2020-12-14 13:33:36 -0800

    fbal: refine the retry of probe pxe1110c (#2178)
    Summary:
    Recently the "i2c_bind_driver()" was refined for easier maintenance.
    So refine the retry script to eliminate unnecessary error message.
    
    for example:
    /usr/local/bin/i2c-utils.sh: line 152: echo: write error: No such device
    /usr/local/bin/i2c-utils.sh: line 152: echo: write error: No such device
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2178
    
    Test Plan:
    Build and test pass on fbal
    
    1. boot normally, without seeing the error message.
    
    2. inject error when probe pxe1110c:
    [   33.840368] pxe1211c: probe of 1-004a failed with error -1
    [   33.925313] pxe1211c: probe of 1-004a failed with error -1
    [   34.028119] pxe1211c: probe of 1-004a failed with error -1
    rebind 1-004a to driver pxe1211c successfully
    
    root@bmc-oob:~# sensor-util mb |grep VR_PCH
    AL_MB_VR_PCH_P1V05_VOLT      (0xC4) :    1.05 Volts | (ok)
    AL_MB_VR_PCH_P1V05_TEMP      (0xC5) :   31.00 C     | (ok)
    AL_MB_VR_PCH_P1V05_IOUT      (0xC6) :    1.69 Amps  | (ok)
    AL_MB_VR_PCH_P1V05_POUT      (0xC7) :    1.06 Watts | (ok)
    AL_MB_VR_PCH_PVNN_VOLT       (0xC8) :    1.00 Volts | (ok)
    AL_MB_VR_PCH_PVNN_TEMP       (0xC9) :   31.00 C     | (ok)
    AL_MB_VR_PCH_PVNN_IOUT       (0xCA) :    1.12 Amps  | (ok)
    AL_MB_VR_PCH_PVNN_POUT       (0xCB) :    1.00 Watts | (ok)
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 4803db87ff

--- meta-facebook/meta-fbal/recipes-fbal/plat-utils/files/setup_i2c.sh
+++ meta-facebook/meta-fbal/recipes-fbal/plat-utils/files/setup_i2c.sh
@@ -27,10 +27,10 @@ rev=$(cat /tmp/cache_store/mb_rev)
 # retry to probe pxe1110c (pxe1211c).
 # note: still keep pxe1110c in dts, so it's the
 # 1st device to be probed on i2c-1
-for i in {1..2}; do
-  i2c_bind_driver pxe1211c 1-004a
-  [ $? -eq 0 ] && break
-done
+if [ ! -L "${SYSFS_I2C_DEVICES}/1-004a/driver" ]; then
+  i2c_bind_driver pxe1211c 1-004a 4 2>/dev/null
+  [ $? -eq 0 ] && echo "rebind 1-004a to driver pxe1211c successfully"
+fi
 
 #DVT SKU_ID[2:1] = 00(TI), 01(INFINEON), TODO: 10(3rd Source)
 sku=$((sku & 0x2))

bac6ac769b06f5c0da34f81c2bf0e0ebcd6e9a57
Joan Olguy Canéus <joancaneus@fb.com>
2020-12-21 12:39:40 -0800

    Upstream elbert ipmid fru (#127)
    Summary:
    The ipmid fruid information for Elbert is currently hardcoded; this removes the hardcoded value and reads FRU information from the FRU EEPROMs using the elbert-eeprom library.
    
    This PR depends on [https://github.com/facebookexternal/openbmc.arista/issues/126](https://github.com/facebookexternal/openbmc.arista/pull/126), so please merge that PR first.
    
    ## Testing
    
    ipmitool can be used to read information for all FRUs (CHASSIS, BMC, SCM, SMB, SMB_EXTRA, PIM2-PIM9)
    ```
    root@bmc-oob:~# ipmitool fru print 1
     Product Name          : ELBERT
     Product Part Number   : 7388
     Product Version       : 10.1
     Product Serial        : SJC20140028
    
    root@bmc-oob:~# ipmitool fru print 2
     Product Name          : ELBERT
     Product Part Number   : 7388-BMC
     Product Version       : 0.0
     Product Serial        : JAS20330262
    
    root@bmc-oob:~# ipmitool fru print 3
     Product Name          : ELBERT
     Product Part Number   : 7388-SUP
     Product Version       : 10.60
     Product Serial        : JAS20390005
    
    root@bmc-oob:~# ipmitool fru print 4
     Product Name          : ELBERT
     Product Part Number   : 7388X5-SC
     Product Version       : 10.1
     Product Serial        : JAS20250004
    
    root@bmc-oob:~# ipmitool fru print 5
     Product Name          : ELBERT
     Product Version       : 10.1
     Product Serial        : JAS20190007
    
    root@bmc-oob:~# ipmitool fru print 6
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20370082
    
    root@bmc-oob:~# ipmitool fru print 7
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20370321
    
    root@bmc-oob:~# ipmitool fru print 8
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20360300
    
    root@bmc-oob:~# ipmitool fru print 9
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20370281
    
    root@bmc-oob:~# ipmitool fru print 10
     Product Name          : ELBERT
     Product Part Number   : 7388-8D4
     Product Version       : 10.1
     Product Serial        : JAS20370017
    
    root@bmc-oob:~# ipmitool fru print 11
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20370155
    
    root@bmc-oob:~# ipmitool fru print 12
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20370192
    
    root@bmc-oob:~# ipmitool fru print 13
     Product Name          : ELBERT
     Product Part Number   : 7388-16CD
     Product Version       : 10.21
     Product Serial        : JAS20370116
    ```
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.arista/pull/127
    
    fbshipit-source-id: 3a6fd0ada7

--- meta-facebook/meta-elbert/recipes-elbert/ipmid/files/fruid.c
+++ meta-facebook/meta-elbert/recipes-elbert/ipmid/files/fruid.c
@@ -21,131 +21,177 @@
  */
 
 #include "fruid.h"
+#include <errno.h>
+#include <facebook/elbert_eeprom.h>
+#include <facebook/wedge_eeprom.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <syslog.h>
 #include <string.h>
-#include <facebook/wedge_eeprom.h>
+#include <syslog.h>
+#include <unistd.h>
 
-// ELBERT start //
-
-#define WEDGE_FRUID_SIZE 0x100
-
-#define COMMON_HDR_VER  1
-#define PROD_INFO_VER  1
+#define COMMON_HDR_VER 1
+#define PROD_INFO_VER 1
 #define PROD_INFO_AREA_OFFSET 0x10
-#define PROD_INFO_CKSUM_OFFSET ( PROD_INFO_AREA_OFFSET + 1 )
+#define PROD_INFO_CKSUM_OFFSET (PROD_INFO_AREA_OFFSET + 1)
 #define LANG_CODE_ENGLISH 25
 #define TYPE_STR 0xC0
 #define TYPE_LAST 0xC1
 #define ZERO_CKSUM_CONST 0x100
 #define LEN_BYTE_SIZE 8
 
+#define ELBERT_FRUID_SIZE 0x100
+
+// FRU IDs for ELBERT
+#define FRUID_ALL 0
+#define FRUID_CHASSIS 1
+#define FRUID_BMC 2
+#define FRUID_SCM 3
+// Switch card has two EEPROMs:
+// 1. SMB is on the switch card with TH4
+// 2. SMB_EXTRA is on the base card
+#define FRUID_SMB 4
+#define FRUID_SMB_EXTRA 5
+#define FRUID_PIM_MIN 6
+#define FRUID_PIM_MAX 13
+
+// Target for elbert_eeprom API
+#define ELBERT_CHASSIS "CHASSIS"
+#define ELBERT_SCM "SCM"
+#define ELBERT_PIM "PIM%d"
+#define ELBERT_SMB "SMB"
+#define ELBERT_SMB_EXTRA "SMB_EXTRA"
+#define ELBERT_BMC "BMC"
+
 typedef struct _fruid_common_hdr_t {
-   unsigned char ver;
-   unsigned char internal_use_area_offset;
-   unsigned char chassis_info_area_offset;
-   unsigned char board_info_area_offset;
-   unsigned char prod_info_area_offset;
-   unsigned char multi_record_area_offset;
-   unsigned char padding;
-   unsigned char cksum;
+  unsigned char ver;
+  unsigned char internal_use_area_offset;
+  unsigned char chassis_info_area_offset;
+  unsigned char board_info_area_offset;
+  unsigned char prod_info_area_offset;
+  unsigned char multi_record_area_offset;
+  unsigned char padding;
+  unsigned char cksum;
 } fruid_common_hdr_t;
 
-// Global structures
-static unsigned char g_fruid[WEDGE_FRUID_SIZE] = {0};
-
 // A macro to add an entry.
-#define _APPEND_STR_VALUE( name ) do { \
-       if ( sizeof( name ) < 1 || i + 1 + sizeof( name ) >= sizeof( g_fruid ) ) { \
-                 break; \
-             } \
-       g_fruid[i++] = TYPE_STR + sizeof( name ); \
-       memcpy( &g_fruid[i], name, sizeof( name ) ); \
-       i += sizeof( name ); \
-} while( 0 )
+#define _APPEND_STR_VALUE(name)                                     \
+  do {                                                              \
+    if (sizeof(name) < 1 || i + 1 + sizeof(name) >= g_fruid_size) { \
+      break;                                                        \
+    }                                                               \
+    g_fruid[i++] = TYPE_STR + sizeof(name);                         \
+    memcpy(&g_fruid[i], name, sizeof(name));                        \
+    i += sizeof(name);                                              \
+  } while (0)
 
 // Main function to populate the fruid data structure, using the data
-// from eeprom
-static void populate_fruid( void ) {
+// from the given target's eeprom
+static void populate_fruid(
+    const char* target,
+    unsigned char* g_fruid,
+    const int g_fruid_size) {
   struct wedge_eeprom_st eeprom;
-   // int rc = 0;
-  int len = 0;
-  int i = PROD_INFO_AREA_OFFSET;
   unsigned char zero_val = 0x0;
+  int i = PROD_INFO_AREA_OFFSET;
+  int rc = 0;
+  int len = 0;
 
-  fruid_common_hdr_t *chdr = ( fruid_common_hdr_t * )g_fruid;
-  memset( &g_fruid, sizeof( g_fruid ), zero_val );
+  fruid_common_hdr_t* chdr = (fruid_common_hdr_t*)g_fruid;
+  memset(&g_fruid, g_fruid_size, zero_val);
 
   chdr->ver = COMMON_HDR_VER;
   chdr->prod_info_area_offset = PROD_INFO_AREA_OFFSET / LEN_BYTE_SIZE;
   chdr->cksum = chdr->ver + chdr->prod_info_area_offset;
   chdr->cksum = ZERO_CKSUM_CONST - chdr->cksum;
-
-#if 0
-  rc = wedge_eeprom_parse( NULL, &eeprom );
-  if ( rc ) {
-   syslog( LOG_ALERT, "populate_fruid: wedge_eeprom_parse returns %d\n", rc );
-   return;
+  rc = elbert_eeprom_parse(target, &eeprom);
+  if (rc) {
+    syslog(
+        LOG_ALERT,
+        "populate_fruid %s: elbert_eeprom_parse returns %d\n",
+        target,
+        rc);
+    return;
   }
-#endif
-  // ELBERTTODO: read contents from eeprom
-  strcpy( eeprom.fbw_system_manufacturer, "Arista" );
-  strcpy( eeprom.fbw_product_name, "Elbert" );
-  strcpy( eeprom.fbw_product_number, "7388" );
-  eeprom.fbw_product_version = 1;
-  strcpy( eeprom.fbw_product_serial, "JAS001" );
-  strcpy( eeprom.fbw_product_asset, "ASS001" );
 
   g_fruid[i++] = PROD_INFO_VER;
   g_fruid[i++] = 0x00;
   g_fruid[i++] = LANG_CODE_ENGLISH;
-  _APPEND_STR_VALUE( eeprom.fbw_system_manufacturer );
-  _APPEND_STR_VALUE( eeprom.fbw_product_name );
-  _APPEND_STR_VALUE( eeprom.fbw_product_number );
-  char vbuf[5] = {0};
-  snprintf( vbuf, sizeof( vbuf ), "%0X", eeprom.fbw_product_version );
-  _APPEND_STR_VALUE( vbuf );
-  _APPEND_STR_VALUE( eeprom.fbw_product_serial );
-  _APPEND_STR_VALUE( eeprom.fbw_product_asset );
+  _APPEND_STR_VALUE(eeprom.fbw_system_manufacturer);
+  _APPEND_STR_VALUE(eeprom.fbw_product_name);
+  _APPEND_STR_VALUE(eeprom.fbw_product_number);
+  char vbuf[8] = {0};
+  snprintf(
+      vbuf,
+      sizeof(vbuf),
+      "%d.%d",
+      eeprom.fbw_product_version,
+      eeprom.fbw_product_subversion);
+  _APPEND_STR_VALUE(vbuf);
+  _APPEND_STR_VALUE(eeprom.fbw_product_serial);
+  _APPEND_STR_VALUE(eeprom.fbw_product_asset);
   char fruid_file_name[] = "fruid_1.0";
-  _APPEND_STR_VALUE( fruid_file_name );
+  _APPEND_STR_VALUE(fruid_file_name);
   g_fruid[i++] = TYPE_LAST;
 
   len = i - PROD_INFO_AREA_OFFSET + 1;
-  if ( len % LEN_BYTE_SIZE ) {
-   g_fruid[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE + 1;
-   i += LEN_BYTE_SIZE - ( len % LEN_BYTE_SIZE );
+  if (len % LEN_BYTE_SIZE) {
+    g_fruid[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE + 1;
+    i += LEN_BYTE_SIZE - (len % LEN_BYTE_SIZE);
   } else {
-   g_fruid[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE;
+    g_fruid[PROD_INFO_CKSUM_OFFSET] = len / LEN_BYTE_SIZE;
   }
 
-  for ( int j = PROD_INFO_AREA_OFFSET; j < i; j++ ) {
-   g_fruid[i] += g_fruid[j];
+  for (int j = PROD_INFO_AREA_OFFSET; j < i; j++) {
+    g_fruid[i] += g_fruid[j];
   }
   g_fruid[i] = ZERO_CKSUM_CONST - g_fruid[i];
 
   return;
 }
 
-int plat_fruid_size( unsigned char payload_id ) {
-  return WEDGE_FRUID_SIZE;
+int plat_fruid_size(unsigned char payload_id) {
+  return ELBERT_FRUID_SIZE;
 }
 
-int plat_fruid_data( unsigned char payload_id, int fru_id, int offset, int count, unsigned char *data ) {
-  if ( ( offset + count ) > WEDGE_FRUID_SIZE ) {
-   return -1;
+int plat_fruid_data(
+    unsigned char payload_id,
+    int fru_id,
+    int offset,
+    int count,
+    unsigned char* data) {
+  unsigned char g_fruid[ELBERT_FRUID_SIZE] = {0};
+  char target[10] = {0};
+
+  if ((offset + count) > ELBERT_FRUID_SIZE) {
+    syslog(
+        LOG_ALERT, "plat_fruid_data: requested offset exceeds eeprom size\n");
+    return -1;
   }
-  memcpy( data, &( g_fruid[offset] ), count );
+
+  if (fru_id == FRUID_ALL || fru_id == FRUID_CHASSIS) {
+    sprintf(target, ELBERT_CHASSIS);
+  } else if (fru_id == FRUID_SCM) {
+    sprintf(target, ELBERT_SCM);
+  } else if (FRUID_PIM_MIN <= fru_id && fru_id <= FRUID_PIM_MAX) {
+    sprintf(target, ELBERT_PIM, fru_id - 4);
+  } else if (fru_id == FRUID_SMB) {
+    sprintf(target, ELBERT_SMB);
+  } else if (fru_id == FRUID_SMB_EXTRA) {
+    sprintf(target, ELBERT_SMB_EXTRA);
+  } else if (fru_id == FRUID_BMC) {
+    sprintf(target, ELBERT_BMC);
+  } else {
+    syslog(LOG_ALERT, "plat_fruid_data: unsupported fru_id %d\n", fru_id);
+    return -1;
+  }
+
+  populate_fruid(target, g_fruid, sizeof(g_fruid));
+  memcpy(data, &(g_fruid[offset]), count);
   return 0;
 }
 
-// ELBERT end //
-
 // Initialize FRUID
-int plat_fruid_init( void ) {
-  populate_fruid();
+int plat_fruid_init(void) {
   return 0;
 }
--- meta-facebook/meta-elbert/recipes-elbert/ipmid/ipmid_0.2.bbappend
+++ meta-facebook/meta-elbert/recipes-elbert/ipmid/ipmid_0.2.bbappend
@@ -1,10 +1,8 @@
-LDFLAGS += " -lwedge_eeprom"
-DEPENDS += "libwedge-eeprom libipmi obmc-pal update-rc.d-native"
-RDEPENDS_${PN} += "libipmi libkv libwedge-eeprom"
+LDFLAGS += " -lwedge_eeprom -lelbert_eeprom"
+DEPENDS += "libwedge-eeprom libelbert-eeprom libipmi obmc-pal update-rc.d-native"
+RDEPENDS_${PN} += "libipmi libkv libwedge-eeprom libelbert-eeprom"
 
 IPMI_FEATURE_FLAGS = "-DSENSOR_DISCRETE_SEL_STATUS -DSENSOR_DISCRETE_WDT"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
-SRC_URI += "file://fruid.c \
-           file://lan.c \
-          "
+SRC_URI += "file://fruid.c"

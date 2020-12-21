67bb736738f382758fc73768fc3775b5a785b148
Mike Choi <mikechoi@fb.com>
2020-12-21 14:42:05 -0800

    Fix Provisioning issue (and potential I2C contention issue) by optimizing PDB detection logic
    Summary:
    [FUJI] Fix Provisioning issue (and potential I2C contention issue) by optimizing PDB detection logic
    Existing logic in board-utils.sh try I2C access to Left / Rigth PDB CPLD everytime it is sourced.
    But this script is sourced by almost all BMC commands and processes, thus potentially creates an issue due to bus contention.
    Moreover, our provisioning logic runs presence_util with bash -x option, and it triggers the provisioning logic to fail because we will hit i2c access error when we source board-utils.sh
    
    This diff fixes the provisioning issue, by remembering the PDB address (by touching a file in /tmp), and hopefully this will fix futuer I2C related bus contention error.
    
    Test Plan:
    BEFORE : Any command sourcing board-utils.sh will do i2c access (then fail) everytime it's executed : P156554332
    AFTER : The issue above is fixed, and i2c test will be done only on the very first time of sourceing board-utils.sh (which is boot up time) : P156554550
    
    Reviewed By: tao-ren
    
    fbshipit-source-id: df06e9e3af

--- meta-facebook/meta-fuji/recipes-utils/openbmc-utils/files/board-utils.sh
+++ meta-facebook/meta-fuji/recipes-utils/openbmc-utils/files/board-utils.sh
@@ -10,16 +10,51 @@ SMBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 12-003e)
 TOP_FCMCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 64-0033)
 BOTTOM_FCMCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 72-0033)
 
-if i2cget -y -f 55 0x60 > /dev/null;then
-    LEFT_PDBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 55-0060)
+#
+# FUJI PDB has different I2C address depending on its version, but
+# trying I2C address everytime we need to access, is not efficient and
+# it also breaks provisioning logic that runs with bash -x option.
+# So we will detect it once one the fly, and save the info in /tmp dir.
+#
+_PDB_L_OLD_CACHE=/tmp/pdb_l_old
+_PDB_L_NEW_CACHE=/tmp/pdb_l_new
+_PDB_R_OLD_CACHE=/tmp/pdb_r_old
+_PDB_R_NEW_CACHE=/tmp/pdb_r_new
+_PDB_L_OLD_ADDR=$(i2c_device_sysfs_abspath 53-0060)
+_PDB_L_NEW_ADDR=$(i2c_device_sysfs_abspath 55-0060)
+_PDB_R_OLD_ADDR=$(i2c_device_sysfs_abspath 61-0060)
+_PDB_R_NEW_ADDR=$(i2c_device_sysfs_abspath 63-0060)
+
+#
+# If previously Left PDB was detected, used that cached info,
+# otherwise try I2C to detect the Left PDB I2C address
+#
+if [ -f "$_PDB_L_NEW_CACHE" ]; then
+  LEFT_PDBCPLD_SYSFS_DIR=$_PDB_L_NEW_ADDR
+elif [ -f "$_PDB_L_OLD_CACHE" ]; then
+  LEFT_PDBCPLD_SYSFS_DIR=$_PDB_L_OLD_ADDR
+elif i2cget -y -f 55 0x60 > /dev/null;then
+  LEFT_PDBCPLD_SYSFS_DIR=$_PDB_L_NEW_ADDR
+  touch $_PDB_L_NEW_CACHE
 else
-    LEFT_PDBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 53-0060)
+  LEFT_PDBCPLD_SYSFS_DIR=$_PDB_L_OLD_ADDR
+  touch $_PDB_L_OLD_CACHE
 fi
 
-if i2cget -y -f 63 0x60 > /dev/null;then
-    RIGHT_PDBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 63-0060)
+#
+# If previously Right PDB was detected, used that cached info,
+# otherwise try I2C to detect the Right PDB I2C address
+#
+if [ -f "$_PDB_R_NEW_CACHE" ]; then
+  RIGHT_PDBCPLD_SYSFS_DIR=$_PDB_R_NEW_ADDR
+elif [ -f "$_PDB_R_OLD_CACHE" ]; then
+  RIGHT_PDBCPLD_SYSFS_DIR=$_PDB_R_OLD_ADDR
+elif i2cget -y -f 63 0x60 > /dev/null;then
+  RIGHT_PDBCPLD_SYSFS_DIR=$_PDB_R_NEW_ADDR
+  touch $_PDB_R_NEW_CACHE
 else
-    RIGHT_PDBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 61-0060)
+  RIGHT_PDBCPLD_SYSFS_DIR=$_PDB_R_OLD_ADDR
+  touch $_PDB_R_OLD_CACHE
 fi
 
 #

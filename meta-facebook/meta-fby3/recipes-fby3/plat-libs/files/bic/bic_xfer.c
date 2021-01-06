18c609822f8047acaad065198659cb2f17fcfd47
Ben Wei <benwei@fb.com>
2021-01-06 15:10:29 -0800

    fby3 : fix incorrect "FRU System powered OFF" event log during 12V-cycle (#2223)
    Summary:
    Summary :
    Remove incorrect SEL"FRU: x, System powered OFF" event randomly (low probability) during slot 12V cycle
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2223
    
    Test Plan:
    build and test on fby3 config D pass.
    
    original behavior
    
    Reviewed By: benwei13
    
    fbshipit-source-id: b89c49c030

--- meta-facebook/meta-fby3/recipes-fby3/plat-libs/files/bic/bic_xfer.c
+++ meta-facebook/meta-fby3/recipes-fby3/plat-libs/files/bic/bic_xfer.c
@@ -181,12 +181,6 @@ int bic_ipmb_wrapper(uint8_t slot_id, uint8_t netfn, uint8_t cmd,
   }
 #endif
 
-  // avoid waiting 5 seconds to do the retry
-  ret = fby3_common_server_stby_pwr_sts(slot_id, &status_12v);
-  if ( ret < 0 || status_12v == 0) {
-    return -1;
-  }
-
   ret = fby3_common_get_bus_id(slot_id);
   if (ret < 0) {
     syslog(LOG_ERR, "%s: Wrong Slot ID %d\n", __func__, slot_id);
@@ -215,6 +209,13 @@ int bic_ipmb_wrapper(uint8_t slot_id, uint8_t netfn, uint8_t cmd,
   tlen = IPMB_HDR_SIZE + IPMI_REQ_HDR_SIZE + txlen;
 
   while(retry < RETRY_TIME) {
+
+    // avoid meaningless retry
+    ret = fby3_common_server_stby_pwr_sts(slot_id, &status_12v);
+    if ( ret < 0 || status_12v == 0) {
+      return -BIC_STATUS_FAILURE;
+    }
+
     // Invoke IPMB library handler
     lib_ipmb_handle(bus_id, tbuf, tlen, rbuf, &rlen);
 

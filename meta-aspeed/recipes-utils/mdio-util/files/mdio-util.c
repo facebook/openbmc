3685db83639277efe6a769e3e06b810d14b58ea3
Tao Ren <taoren@fb.com>
2021-01-07 08:58:45 -0800

    meta-aspeed: mdio-util: optimize check chip logic to support more AST26XX chips (#910)
    Summary:
    Optimize check chip logic to support more AST26XX chip. Because AST2620 A2's chip id 1 and chip id 2 are different.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/910
    
    Test Plan:
    ```
    <1>Fuji AST2620 A1 BMC Card
    
    root@bmc-oob:~# cat /etc/issue
    OpenBMC Release fuji-7f7e57b544-dirty
    
    root@bmc-oob:~# devmem 0x1e6e2004
    0x05010203
    root@bmc-oob:~# devmem 0x1e6e2014
    0x05010203
    root@bmc-oob:~# mdio-util
    Usage: [OPTIONS...]
    OPTIONS:
      -m        Specific mdio bus
      -p        Specific phy address
      -r [hex]  Phy register to read
      -w [hex]  Phy register to write
      -d [hex]  Data to write
      -h|?      Usage
      -D        Use debug mode
    Example:
      mdio-util -m 2 -p 0x0d -r 0x0
      mdio-util -m 2 -p 0x0d -w 0x0 -d 0x0
    root@bmc-oob:~# mdio-util -m 2 -p 0x0d -r 0x0
    Read from PHY 0xd.0x0, value is 0x1140
    
    <2>Fuji AST2620 A2 BMC Card
    root@bmc-oob:~# cat /etc/issue
    OpenBMC Release fuji-7f7e57b544-dirty
    
    root@bmc-oob:~# devmem 0x1e6e2004
    0x05010203
    root@bmc-oob:~# devmem 0x1e6e2014
    0x05020203
    root@bmc-oob:~# mdio-util
    Usage: [OPTIONS...]
    OPTIONS:
      -m        Specific mdio bus
      -p        Specific phy address
      -r [hex]  Phy register to read
      -w [hex]  Phy register to write
      -d [hex]  Data to write
      -h|?      Usage
      -D        Use debug mode
    Example:
      mdio-util -m 2 -p 0x0d -r 0x0
      mdio-util -m 2 -p 0x0d -w 0x0 -d 0x0
    root@bmc-oob:~# mdio-util -m 2 -p 0x0d -r 0x0
    Read from PHY 0xd.0x0, value is 0x1140
    root@bmc-oob:~#
    
    <3>. Wedge400 AST2520 BMC
    root@bmc-oob:~# cat /etc/issue
    OpenBMC Release wedge400-7f7e57b544-dirty
    
    root@bmc-oob:~# mdio-util
    Usage: [OPTIONS...]
    OPTIONS:
      -m        Specific mdio bus
      -p        Specific phy address
      -r [hex]  Phy register to read
      -w [hex]  Phy register to write
      -d [hex]  Data to write
      -h|?      Usage
      -D        Use debug mode
    Example:
      mdio-util -m 2 -p 0x0d -r 0x0
      mdio-util -m 2 -p 0x0d -w 0x0 -d 0x0
    root@bmc-oob:~# mdio-util -m 2 -p 0x0d -r 0x0
    Read from PHY 0xd.0x0, value is 0x0
    root@bmc-oob:~#
    
    ```
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 8b07e5c50b

--- meta-aspeed/recipes-utils/mdio-util/files/mdio-util.c
+++ meta-aspeed/recipes-utils/mdio-util/files/mdio-util.c
@@ -215,16 +215,13 @@ void print_usage()
 static int check_chip()
 {
     uint32_t cpu_silicon_rev_id_0;
-    uint32_t cpu_silicon_rev_id_1;
     uint32_t chip = INVALID_CHIP;
     if(debug)
         printf("Debug: detecting chip ...\n");
     run_devmem(SCU_BASE_ADDR + SILICON_REVISION_ID_0_OFFSET_AST_G6,
                &cpu_silicon_rev_id_0, READ_OP);
-    run_devmem(SCU_BASE_ADDR + SILICON_REVISION_ID_1_OFFSET_AST_G6,
-               &cpu_silicon_rev_id_1, READ_OP);
-    if(cpu_silicon_rev_id_0 == cpu_silicon_rev_id_1 && 
-      (cpu_silicon_rev_id_0 & AST_G6_REV_ID) == AST_G6_REV_ID) {
+
+    if((cpu_silicon_rev_id_0 & AST_G6_REV_ID) == AST_G6_REV_ID) {
         chip = AST_G6; //AST_G6
         goto exit;
     }

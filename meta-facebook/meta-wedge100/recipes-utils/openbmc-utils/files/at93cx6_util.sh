c67d11749418d1a1c1c1ad1c3a85e7479f22067a
Tao Ren <taoren@fb.com>
2021-02-06 16:02:07 -0800

    meta-wedge100: at93cx6_util.sh: prepare env for eeprom access
    Summary:
    Set up correct SCU registers and GPIO pins so BCM5387 eeprom can be
    accessed properly.
    
    Test Plan:
    1) recovered BCM5387 eeprom on rsw005.p061.f01.pnb4 successfully. Refer
       to S221767 for more context.
    2) Read BCM5387 eeprom on basset machine. output looks good (output is
       zero without the fix):
    
    root@bmc-oob:/tmp# ./at93cx6_util.sh chip read --file /tmp/out.bin
    root@bmc-oob:/tmp# hexdump /tmp/out.bin
    0000000 aa82 2f01 0000 0000 0000 0000 0000 0000
    0000010 0000 0000 0000 0000 0000 0000 0000 0000
    *
    0000080
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 6e4de96d55

--- meta-facebook/meta-wedge100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
+++ meta-facebook/meta-wedge100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
@@ -21,6 +21,19 @@
 # shellcheck disable=SC1091
 . /usr/local/bin/openbmc-utils.sh
 
+devmem_clear_bit "$(scu_addr 80)" 18
+devmem_clear_bit "$(scu_addr 8C)" 13
+devmem_clear_bit "$(scu_addr 70)" 22
+
+if [ ! -L "${SHADOW_GPIO}/BMC_EEPROM1_SPI_SS" ]; then
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI4 BMC_EEPROM1_SPI_SS
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI5 BMC_EEPROM1_SPI_SCK
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI6 BMC_EEPROM1_SPI_MOSI
+    gpio_export_by_name "$ASPEED_GPIO" GPIOI7 BMC_EEPROM1_SPI_MISO
+fi
+
+gpio_set_value SWITCH_EEPROM1_WRT 1
+
 /usr/local/bin/at93cx6_util_py3.py --cs BMC_EEPROM1_SPI_SS \
                                    --clk BMC_EEPROM1_SPI_SCK \
                                    --mosi BMC_EEPROM1_SPI_MOSI \

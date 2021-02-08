e3922767a5db256896ec8a02f79b5fb74baf1fbc
Tao Ren <taoren@fb.com>
2021-02-08 13:24:27 -0800

    wedge100: ensure single instance of "at93cx6_util.sh"
    Summary:
    Force a single instance of "at93cx6_util.sh" to avoid potential BCM5387
    eeprom corruption caused by concurrent eeprom access.
    
    Bonus: call cleanup function at script exit.
    
    Test Plan:
    1) BCM5387 eeprom can be read successfully.
    2) The 2nd instance of at93cx6_util.sh exit immediately:
    
    root@bmc-oob:/tmp# ./at93cx6_util.sh chip read --file 1.bin &
    [1] 2874
    root@bmc-oob:/tmp# ./at93cx6_util.sh chip read --file 2.bin
    Another instance is running. Exiting!!
    root@bmc-oob:/tmp#
    [1]+  Done                    ./at93cx6_util.sh chip read --file 1.bin
    root@bmc-oob:/tmp# hexdump 1.bin
    0000000 aa82 2f01 0000 0000 0000 0000 0000 0000
    0000010 0000 0000 0000 0000 0000 0000 0000 0000
    *
    0000080
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 94a794aa6c

--- meta-facebook/meta-wedge100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
+++ meta-facebook/meta-wedge100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
@@ -18,12 +18,34 @@
 # Boston, MA 02110-1301 USA
 #
 
+#
+# This is the wrapper of "at93cx6_util_py3.py": people should call this
+# wrapper instead of running "at93cx6_util_py3.py" directly, because:
+#   - the wrapper ensures correct SS/SCK/MISO/MOSI pins are used.
+#   - the wrapper ensures single instance of eeprom access, which avoids
+#     potential eeprom corruption caused by concurrent eeprom access.
+#
+
 # shellcheck disable=SC1091
 . /usr/local/bin/openbmc-utils.sh
 
-devmem_clear_bit "$(scu_addr 80)" 18
-devmem_clear_bit "$(scu_addr 8C)" 13
-devmem_clear_bit "$(scu_addr 70)" 22
+LOCK_FILE="/var/run/at93cx6_access.lock"
+
+at93cx6_cleanup() {
+    gpio_set_value SWITCH_EEPROM1_WRT 0
+}
+
+#
+# Ensure a single instance of at93cx6_util; otherwise, the GPIO pins
+# could be toggled by another instance unexpectedly.
+#
+exec 99>"$LOCK_FILE"
+if ! flock -n 99; then
+    echo "Another instance is running. Exiting!!"
+    exit 1
+fi
+
+trap at93cx6_cleanup INT TERM QUIT EXIT
 
 if [ ! -L "${SHADOW_GPIO}/BMC_EEPROM1_SPI_SS" ]; then
     gpio_export_by_name "$ASPEED_GPIO" GPIOI4 BMC_EEPROM1_SPI_SS
@@ -32,6 +54,14 @@ if [ ! -L "${SHADOW_GPIO}/BMC_EEPROM1_SPI_SS" ]; then
     gpio_export_by_name "$ASPEED_GPIO" GPIOI7 BMC_EEPROM1_SPI_MISO
 fi
 
+#
+# Below "devmem_clear_bit" operations ensure GPIOE2 (SWITCH_EEPROM1_WRT)
+# is configured as GPIO correctly. The pinctrl should be handled when the
+# GPIO pin was exported, but let's just keep it as is for now.
+#
+devmem_clear_bit "$(scu_addr 80)" 18
+devmem_clear_bit "$(scu_addr 8C)" 13
+devmem_clear_bit "$(scu_addr 70)" 22
 gpio_set_value SWITCH_EEPROM1_WRT 1
 
 /usr/local/bin/at93cx6_util_py3.py --cs BMC_EEPROM1_SPI_SS \

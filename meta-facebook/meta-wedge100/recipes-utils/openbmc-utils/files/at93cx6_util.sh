00deed272d8fb8dbf3f13dab5310bf79e93a169f
Tao Ren <taoren@fb.com>
2021-02-08 14:34:07 -0800

    wedge100: at93cx6_util.sh: support "chip program" option
    Summary:
    Add "chip program" option to combine chip write/read/verify, which
    ensures safer eeprom upgrade.
    
    Test Plan:
    root@bmc-oob:/tmp# ./at93cx6_util.sh chip program ./bcm5387_eeprom.bin
    Reading EEPROM content..
    Writing ./bcm5387_eeprom.bin to the EEPROM..
    Reading back EEPROM after upgrade..
    Validating EEPROM content..
    EEPROM is up to date. Upgrade succeeded!
    root@bmc-oob:/tmp# ./at93cx6_util.sh chip program ./bcm5387_eeprom.bin
    Reading EEPROM content..
    The EEPROM is up to date. Upgrade skipped.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 105eb2b3c6

--- meta-facebook/meta-wedge100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
+++ meta-facebook/meta-wedge100/recipes-utils/openbmc-utils/files/at93cx6_util.sh
@@ -30,9 +30,11 @@
 . /usr/local/bin/openbmc-utils.sh
 
 LOCK_FILE="/var/run/at93cx6_access.lock"
+TMP_EEPROM_FILE="/tmp/__at93cx6.bin"
 
 at93cx6_cleanup() {
     gpio_set_value SWITCH_EEPROM1_WRT 0
+    rm -f "$TMP_EEPROM_FILE"
 }
 
 #
@@ -64,7 +66,64 @@ devmem_clear_bit "$(scu_addr 8C)" 13
 devmem_clear_bit "$(scu_addr 70)" 22
 gpio_set_value SWITCH_EEPROM1_WRT 1
 
-/usr/local/bin/at93cx6_util_py3.py --cs BMC_EEPROM1_SPI_SS \
-                                   --clk BMC_EEPROM1_SPI_SCK \
-                                   --mosi BMC_EEPROM1_SPI_MOSI \
-                                   --miso BMC_EEPROM1_SPI_MISO $@
+at93cx6_bb_helper() {
+    /usr/local/bin/at93cx6_util_py3.py --cs BMC_EEPROM1_SPI_SS \
+                                       --clk BMC_EEPROM1_SPI_SCK \
+                                       --mosi BMC_EEPROM1_SPI_MOSI \
+                                       --miso BMC_EEPROM1_SPI_MISO $@
+}
+
+at93cx6_program() {
+    image_file="$1"
+
+    #
+    # First of all, let's check if upgrade is really needed.
+    #
+    echo "Reading EEPROM content.."
+    if ! at93cx6_bb_helper chip read --file "$TMP_EEPROM_FILE"; then
+        echo "failed to read eeprom. Exiting!"
+        exit 1
+    fi
+    if cmp "$image_file" "$TMP_EEPROM_FILE" > /dev/null 2>&1; then
+        echo "The EEPROM is up to date. Upgrade skipped."
+        exit 0
+    fi
+
+    #
+    # Start upgrading the EEPROM with the given image.
+    #
+    echo "Writing $image_file to the EEPROM.."
+    if ! at93cx6_bb_helper chip write --file "$image_file"; then
+        echo "failed to upgrade eeprom. Exiting!"
+        exit 1
+    fi
+
+    #
+    # Reading EEPROM back and verify if the EEPROM is programmed successfully.
+    #
+    rm -f "$TMP_EEPROM_FILE"
+    echo "Reading back EEPROM after upgrade.."
+    if ! at93cx6_bb_helper chip read --file "$TMP_EEPROM_FILE"; then
+        echo "failed to read eeprom. Exiting!"
+        exit 1
+    fi
+
+    echo "Validating EEPROM content.."
+    if cmp "$image_file" "$TMP_EEPROM_FILE" > /dev/null 2>&1; then
+        echo "EEPROM is up to date. Upgrade succeeded!"
+    else
+        echo "ERROR: got unexpected EEPROM content. Exiting!"
+        exit 1
+    fi
+}
+
+if [ "$1" = "chip" ] && [ "$2" = "program" ]; then
+    if [ "$#" != 3 ]; then
+        echo "Error: invalid command arguments: image file is missing!"
+        echo "Usage: $0 chip program <image-file>"
+        exit 1
+    fi
+    at93cx6_program "$3"
+else
+    at93cx6_bb_helper $@
+fi

8ea5f53264a07b1c48bd96913c1d0300af362ffd
Andrew Doran <doranand@fb.com>
2021-02-25 11:01:33 -0800

    kconfig-5.6: add needed TPM options
    Summary: tcsd doesn't start with kernel 5.6 / systemd because needed TPM driver modules are not available.
    
    Test Plan:
    Build image & boot on BMC: P204163058
    
    ```
    root@rsw1at-oob:~# lsmod | grep tpm
    tpm_i2c_infineon       16384  0
    tpm                    57344  3 tpm_i2c_infineon
    rng_core               20480  2 timeriomem_rng,tpm
    root@rsw1at-oob:~# tpm_version
      TPM 1.2 Version Info:
      Chip Version:        1.2.133.33
      Spec Level:          2
      Errata Revision:     3
      TPM Vendor ID:       IFX
      Vendor Specific data: 852100e3 0074706d 313139ff ff
      TPM Version:         01010000
      Manufacturer Info:   49465800
    root@rsw1at-oob:~# ps | grep tcsd
      680 tss      13492 S    /usr/sbin/tcsd
     4387 root      3060 S    grep tcsd
    root@rsw1at-oob:~#
    ```
    
    Add 2 basic CIT tests for tcsd and tpm_version.
    
    Reviewed By: tao-ren, lsiudut
    
    fbshipit-source-id: 0884e64c88

---
+++ tests2/tests/wedge100/test_tpm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This software may be used and distributed according to the terms of
+# the GNU General Public License version 2.
+import unittest
+
+from common.base_tpm_test import BaseTpmTest
+
+
+class TestTpm(BaseTpmTest, unittest.TestCase):
+    pass

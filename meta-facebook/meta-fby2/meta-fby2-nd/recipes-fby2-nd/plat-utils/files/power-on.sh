fa70d64013fee5892e0f13f286ccd18377639a29
Amithash Prasad <amithash@fb.com>
2021-03-05 09:32:26 -0800

    northdome: add ntp timesync delay for northdome (#2384)
    Summary:
    1. add ntp timesync checking to derfer starting slots on POR till BMC time synced
    2. change order of setup-fan.sh to avoid fan failed event caused by ntp timesync checking
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2384
    
    Test Plan:
    1. Build and test passed on northdome
    2. check the running order of setup-fan.sh (71)
    
    root@bmc-oob:~# ls -l /etc/rc5.d/*setup-fan.sh
    lrwxrwxrwx    1 root     root            22 Mar  9  2018 /etc/rc5.d/S71setup-fan.sh -> ../init.d/setup-fan.sh
    
    3. check ntp timesync checking is working if ntp server set
    
    [Wed Mar  3 22:02:51 PST 2021] ntp is enabled, ntp_server: 162.159.200.1
    [Wed Mar  3 22:02:55 PST 2021] system time synced after 3 sec
    Powering fru 1 to ON state...
    Powering fru 2 to ON state...
    Powering fru 3 to ON state...
    Powering fru 4 to ON state...
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 11d4b7c774

---
+++ meta-facebook/meta-fby2/meta-fby2-nd/recipes-fby2-nd/plat-utils/files/power-on.sh
@@ -0,0 +1,164 @@
+#!/bin/sh
+#
+# Copyright 2014-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+### BEGIN INIT INFO
+# Provides:          power-on
+# Required-Start:
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Power on Server
+### END INIT INFO
+. /usr/local/fbpackages/utils/ast-functions
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+DEF_PWR_ON=1
+TO_PWR_ON=
+POWER_ON_SLOT=
+RETRY=0
+
+DEBUG_ENABLE=false
+
+is_ntp_enabled() {
+  local ntp_server
+  ntp_server=$(/usr/bin/kv get ntp_server persistent)
+  if ! /usr/sbin/ntpdate -q $ntp_server > /dev/null ; then
+    return 1 # diaabled
+  fi
+  return 0 # enabled
+}
+
+is_ntp_time_synced() {
+  local str
+  local ret
+
+  str=$(/usr/sbin/ntpq -np | grep '^\*')
+  ret=$?
+
+  if [ "$DEBUG_ENABLE" = true ] ; then
+    echo "=================================="
+    echo "ret: $ret"
+    echo "----------------------------------"
+    echo "$str"
+    echo "=================================="
+  fi
+
+  # ret == 0, synced
+  # ret == 1, not synced
+  return $ret
+}
+
+check_por_config()
+{
+  TO_PWR_ON=-1
+  local PWR_ON_RST
+  local LAST_STATE
+
+  PWR_ON_RST=$(/usr/bin/kv get slot${1}_por_cfg persistent)
+
+  if [ -z "${PWR_ON_RST}" ]; then       # File not exist
+    TO_PWR_ON=$DEF_PWR_ON
+  elif [ "$PWR_ON_RST" == "on" ]; then    # Case ON
+    TO_PWR_ON=1;
+  elif [ "$PWR_ON_RST" == "off" ]; then   # Case OFF
+    TO_PWR_ON=0;
+  elif [ "$PWR_ON_RST" == "lps" ]; then   # Case LPS
+    LAST_STATE=$(/usr/bin/kv get pwr_server${1}_last_state persistent)
+    if [ -z "${LAST_STATE}" ]; then
+      TO_PWR_ON=$DEF_PWR_ON
+    elif [ "$LAST_STATE" == "on" ]; then
+      TO_PWR_ON=1;
+    elif [ "$LAST_STATE" == "off" ]; then
+      TO_PWR_ON=0;
+    else
+      echo "unexpected value in pwr_server${1}_last_state"
+    fi
+  else
+    echo "unexpected value in slot${1}_por_cfg"
+  fi
+}
+
+remove_gp_m2_info() {
+  for i in {2..13};
+  do
+    /usr/bin/kv del sys_config/fru2_m2_${i}_info persistent > /dev/null 2>&1
+    /usr/bin/kv del sys_config/fru4_m2_${i}_info persistent > /dev/null 2>&1
+  done
+}
+
+ntp_sync_delay() {
+    local NTP_DELAY_COUNT
+
+    NTP_DELAY_COUNT=0
+    echo "[$(/bin/date)] ntp is enabled, ntp_server: $(/usr/bin/kv get ntp_server persistent)"
+    while [ $NTP_DELAY_COUNT -lt 60 ];
+    do
+      if is_ntp_time_synced ; then
+        break
+      fi
+      ((NTP_DELAY_COUNT++))
+      [ "$DEBUG_ENABLE" = true ] && echo "[$(/bin/date)] system time not synced, count: ${NTP_DELAY_COUNT}"
+      sleep 1
+    done
+    if is_ntp_time_synced ; then
+      echo "[$(/bin/date)] system time synced after $NTP_DELAY_COUNT sec"
+    else
+      echo "[$(/bin/date)] system time not synced after $NTP_DELAY_COUNT sec"
+    fi
+}
+
+# Check whether it is fresh power on reset
+if [ $(is_bmc_por) -eq 1 ]; then
+  # Remove all GP/GPv2 info
+  remove_gp_m2_info
+
+  # check time is synced if ntp is enabled
+  if is_ntp_enabled ; then
+    ntp_sync_delay
+  fi
+
+  # check POR config for each slot
+  for i in $(seq 1 4)
+  do
+    check_por_config ${i}
+    if [ $TO_PWR_ON -eq 1 ] && [ $(is_server_prsnt ${i}) == "1" ] && [ $(get_slot_type ${i}) == "0" ] ; then
+      POWER_ON_SLOT+=(${i})
+      power-util slot${i} on &
+    fi
+  done
+
+  # Wait for all slots finish power-up
+  while [ $RETRY -lt 10 ]
+  do
+    FINISH=1
+    for i in ${POWER_ON_SLOT[@]}
+    do
+      status=$(power-util slot$i status)
+      if [ ${status:(-3)} == "OFF" ]; then
+        FINISH=0
+      fi
+    done
+    if [ $FINISH -eq 1 ]; then
+      break
+    fi
+    RETRY=$(($RETRY+1))
+    sleep 1
+  done
+fi

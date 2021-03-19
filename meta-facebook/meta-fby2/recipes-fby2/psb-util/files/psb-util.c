5996830497a01297911bdc6b54b1e3e4f930aee6
Amithash Prasad <amithash@fb.com>
2021-03-19 16:10:06 -0700

    northdome: extend PSB config info to 8 bytes (#2420)
    Summary:
    extend PSB config info to 8 bytes to provide more information
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2420
    
    Test Plan:
    1. Build and test passed on northdome
    2. manually generate PSB info by ipmi-util, then check results with psb-util
    
    root@bmc-oob:/# ipmi-util 1 0xc0 0x75 0x8d 0x00 0x00 0x11 0x00 0x00 0x00 0x50
    C4 75 00
    root@bmc-oob:/# ipmi-util 2 0xc0 0x75 0x8d 0x00 0x00 0x11 0x00 0x00 0x00 0x50
    C4 75 00
    root@bmc-oob:/# ipmi-util 3 0xc0 0x75 0x8d 0x00 0x00 0x11 0x00 0x00 0x00 0x50
    C4 75 00
    root@bmc-oob:/# ipmi-util 4 0xc0 0x75 0x8d 0x00 0x00 0x11 0x00 0x00 0x00 0x50
    C4 75 00
    root@bmc-oob:/# hexdump -C /tmp/cache_store/slot1_psb_config_raw
    00000000  8d 00 00 11 00 00 00 50                           |.......P|
    00000008
    root@bmc-oob:/# hexdump -C /tmp/cache_store/slot2_psb_config_raw
    00000000  8d 00 00 11 00 00 00 50                           |.......P|
    00000008
    root@bmc-oob:/# hexdump -C /tmp/cache_store/slot3_psb_config_raw
    00000000  8d 00 00 11 00 00 00 50                           |.......P|
    00000008
    root@bmc-oob:/# hexdump -C /tmp/cache_store/slot4_psb_config_raw
    00000000  8d 00 00 11 00 00 00 50                           |.......P|
    00000008
    root@bmc-oob:/# psb-util all
    ==================================
    Slot1
    ==================================
    PlatformVendorID:             0x8D
    PlatformModelID:              0x00
    BiosKeyRevisionID:            0x00
    RootKeySelect:                0x00
    PlatformSecureBootEn:         0x01
    DisableBiosKeyAntiRollback:   0x00
    DissableAmdKeyUsage:          0x00
    DissableSecureDebugUnlock:    0x00
    CustomerKeyLock:              0x01
    PSBStatus:                    0x00
    PSBFusingReadiness:           0x00
    HSTIStatePSPSecureEn:         0x01
    HSTIStatePSPPlatformSecureEn: 0x00
    HSTIStatePSPDebugLockOn:      0x01
    
    ==================================
    Slot2
    ==================================
    PlatformVendorID:             0x8D
    PlatformModelID:              0x00
    BiosKeyRevisionID:            0x00
    RootKeySelect:                0x00
    PlatformSecureBootEn:         0x01
    DisableBiosKeyAntiRollback:   0x00
    DissableAmdKeyUsage:          0x00
    DissableSecureDebugUnlock:    0x00
    CustomerKeyLock:              0x01
    PSBStatus:                    0x00
    PSBFusingReadiness:           0x00
    HSTIStatePSPSecureEn:         0x01
    HSTIStatePSPPlatformSecureEn: 0x00
    HSTIStatePSPDebugLockOn:      0x01
    
    ==================================
    Slot3
    ==================================
    PlatformVendorID:             0x8D
    PlatformModelID:              0x00
    BiosKeyRevisionID:            0x00
    RootKeySelect:                0x00
    PlatformSecureBootEn:         0x01
    DisableBiosKeyAntiRollback:   0x00
    DissableAmdKeyUsage:          0x00
    DissableSecureDebugUnlock:    0x00
    CustomerKeyLock:              0x01
    PSBStatus:                    0x00
    PSBFusingReadiness:           0x00
    HSTIStatePSPSecureEn:         0x01
    HSTIStatePSPPlatformSecureEn: 0x00
    HSTIStatePSPDebugLockOn:      0x01
    
    ==================================
    Slot4
    ==================================
    PlatformVendorID:             0x8D
    PlatformModelID:              0x00
    BiosKeyRevisionID:            0x00
    RootKeySelect:                0x00
    PlatformSecureBootEn:         0x01
    DisableBiosKeyAntiRollback:   0x00
    DissableAmdKeyUsage:          0x00
    DissableSecureDebugUnlock:    0x00
    CustomerKeyLock:              0x01
    PSBStatus:                    0x00
    PSBFusingReadiness:           0x00
    HSTIStatePSPSecureEn:         0x01
    HSTIStatePSPPlatformSecureEn: 0x00
    HSTIStatePSPDebugLockOn:      0x01
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 739297819c

--- meta-facebook/meta-fby2/recipes-fby2/psb-util/files/psb-util.c
+++ meta-facebook/meta-fby2/recipes-fby2/psb-util/files/psb-util.c
@@ -30,107 +30,201 @@
 #include <openbmc/kv.h>
 #include <openbmc/pal.h>
 
-static void
-print_usage_help(void) {
-  printf("Usage: psb-util <slot1|slot2|slot3|slot4> \n");
-}
+#define CTRL_FLAG_DEBUG  0x01
+
+struct psb_util_args_t {
+  int fruid;
+  uint8_t ctrl_flags;
+  uint8_t print_help;
+};
+
+static struct psb_util_args_t psb_util_args;
+
+struct psb_config_info {
+  uint8_t platform_vendor_id;                 // Byte[0] : 0-7
+
+  uint8_t platform_model_id : 4;              // Byte[1] : 0-3
+  uint8_t bios_key_revision_id : 4;           // Byte[1] : 4-7
+
+  uint8_t root_key_select : 4;                // Byte[2] : 0-3
+  uint8_t reserved_byte2_4_7 : 4;             // Byte[2] : 4-7
+
+  uint8_t platform_secure_boot_en : 1;        // Byte[3] : 0
+  uint8_t disable_bios_key_antirollback : 1;  // Byte[3] : 1
+  uint8_t disable_amd_key_usage : 1;          // Byte[3] : 2
+  uint8_t disable_secure_debug_unlock : 1;    // Byte[3] : 3
+  uint8_t customer_key_lock : 1;              // Byte[3] : 4
+  uint8_t reserved_byte3_5_7 : 3;             // Byte[3] : 5-7
+
+  uint8_t psb_status;                         // Byte[4] : 0-7
+
+  uint8_t psb_fusing_readiness : 1;           // Byte[5] : 0
+  uint8_t reserved_byte5_1_7 : 7;             // Byte[5] : 1-7
+
+  uint8_t reserved_byte6_0_7;                 // Byte[6] : 0-7
+
+  uint8_t reserved_byte7_0_3 : 4;               // Byte[7] : 0-3
+  uint8_t hstistate_psp_secure_en : 1;          // Byte[7] : 4
+  uint8_t hstistate_psp_platform_secure_en : 1; // Byte[7] : 5
+  uint8_t hstistate_psp_debug_lock_on : 1;      // Byte[7] : 6
+  uint8_t reserved_byte7_7_7 : 1;               // Byte[7] : 7
+} __attribute__((packed));
 
 static void
+print_usage_help(void) {
+  printf("Usage: psb-util <all|slot1|slot2|slot3|slot4>\n");
+}
+
+static int
 print_psb_config(uint8_t slot) {
+  struct psb_config_info *pc_info;
   char key[MAX_KEY_LEN] = {0};
   char value[MAX_VALUE_LEN] = {0};
   int  ret;
+  size_t len;
+  size_t i;
 
   memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_platform_vendor_id", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("PlatformVendorID: %s\n", value);
+  snprintf(key,MAX_KEY_LEN, "slot%d_psb_config_raw", slot);
+  ret = kv_get(key, value, &len, 0);
+  if (ret != 0) {
+    printf("Slot%d psb config cache file not exist: %s\n", slot, key);
+    return -1;
   }
 
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_platform_model_id", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("PlatformModelID: %s\n", value);
+  if (psb_util_args.ctrl_flags & CTRL_FLAG_DEBUG) {
+    printf("RAW: ");
+    for (i=0; i<len; i++) {
+      printf("[%02X]", value[i]);
+    }
+    printf("\n");
   }
 
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_bios_key_revision_id", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("BiosKeyRevisionID: %s\n", value);
-  }
+  pc_info = (struct psb_config_info*)value;
+  printf("PlatformVendorID:             0x%02X\n", pc_info->platform_vendor_id);
+  printf("PlatformModelID:              0x%02X\n", pc_info->platform_model_id);
+  printf("BiosKeyRevisionID:            0x%02X\n", pc_info->bios_key_revision_id);
+  printf("RootKeySelect:                0x%02X\n", pc_info->root_key_select);
+  printf("PlatformSecureBootEn:         0x%02X\n", pc_info->platform_secure_boot_en);
+  printf("DisableBiosKeyAntiRollback:   0x%02X\n", pc_info->disable_bios_key_antirollback);
+  printf("DissableAmdKeyUsage:          0x%02X\n", pc_info->disable_amd_key_usage);
+  printf("DissableSecureDebugUnlock:    0x%02X\n", pc_info->disable_secure_debug_unlock);
+  printf("CustomerKeyLock:              0x%02X\n", pc_info->customer_key_lock);
+  printf("PSBStatus:                    0x%02X\n", pc_info->psb_status);
+  printf("PSBFusingReadiness:           0x%02X\n", pc_info->psb_fusing_readiness);
+  printf("HSTIStatePSPSecureEn:         0x%02X\n", pc_info->hstistate_psp_secure_en);
+  printf("HSTIStatePSPPlatformSecureEn: 0x%02X\n", pc_info->hstistate_psp_platform_secure_en);
+  printf("HSTIStatePSPDebugLockOn:      0x%02X\n", pc_info->hstistate_psp_debug_lock_on);
 
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_root_key_select", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("RootKeySelect: %s\n", value);
-  }
-
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_platform_secure_boot_en", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("PlatformSecureBootEn: %s\n", value);
-  }
-
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_disable_bios_key_antirollback", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("DisableBiosKeyAntiRollback: %s\n", value);
-  }
-
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_disable_amd_key_usage", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("DissableAmdKeyUsage: %s\n", value);
-  }
-
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_disable_secure_debug_unlock", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("DissableSecureDebugUnlock: %s\n", value);
-  }
-
-  memset(value, 0, MAX_VALUE_LEN);
-  snprintf(key,MAX_KEY_LEN, "slot%d_customer_key_lock", slot);
-  ret = kv_get(key, value, NULL, 0);
-  if (!ret) {
-    printf("CustomerKeyLock: %s\n", value);
-  }
+  return 0;
 }
+
+
+int
+parse_args(int argc, char **argv) {
+  int ret;
+  uint8_t fru;
+  char fruname[32];
+  static struct option long_opts[] = {
+    {"help", no_argument, 0, 'h'},
+    {"debug", no_argument, 0, 'd'},
+    {0,0,0,0},
+  };
+
+  psb_util_args.fruid = -1;
+  psb_util_args.ctrl_flags = 0;
+  psb_util_args.print_help = 0;
+
+  if (argc < 2) {
+    printf("Not enough argument\n");
+    goto err_exit;
+  } else {
+    strcpy(fruname, argv[1]);
+  }
+
+  while(1) {
+    ret = getopt_long(argc, argv, "hd", long_opts, NULL);
+    if (ret == -1) {
+      break;
+    }
+
+    switch (ret) {
+      case 'h':
+        psb_util_args.print_help = 1;
+        return 0;
+      case 'd':
+        psb_util_args.ctrl_flags |= CTRL_FLAG_DEBUG;
+        break;
+      default:
+        goto err_exit;
+    }
+  }
+
+  if(pal_get_fru_id(fruname, &fru) != 0 ) {
+    psb_util_args.print_help = 1;
+    printf("Wrong fru: %s\n", fruname);
+    goto err_exit;
+  } else if (fru > MAX_NODES) {   // fru > 4
+    printf("Wrong fru: %s\n", fruname);
+    psb_util_args.print_help = 1;
+    goto err_exit;
+  } else {
+    psb_util_args.fruid = fru;
+  }
+
+  return 0;
+
+err_exit:
+  return -1;
+}
+
+
+int
+do_action(int slot_id) {
+  uint8_t status = 0;
+
+  printf("==================================\n");
+  printf("Slot%d\n", slot_id);
+  printf("==================================\n");
+
+  if (pal_get_server_power(slot_id, &status) != 0) {
+    printf("Can not get Slot%d power status\n", slot_id);
+    goto err_exit;
+  } else if(status != SERVER_POWER_ON) {
+    printf("Slot%d is not POWER ON\n", slot_id);
+    goto err_exit;
+  } 
+    
+  return print_psb_config(slot_id);
+
+err_exit:
+  return -1;
+}
+
 
 int
 main(int argc, char **argv) {
   int ret = 0;
   uint8_t fru;
 
-  if (argc == 2) {
-    ret = pal_get_fru_id(argv[1], &fru);
-    if (ret < 0) {
-      printf("Wrong fru: %s\n", argv[1]);
-      print_usage_help();
-      exit(-1);
-    }
-  } else {
+  if (parse_args(argc, argv) != 0) {
     print_usage_help();
-    fru = -1;
+    return -1;
   }
 
-  if ((fru > 0) && (fru <= MAX_NODES)) {
-      uint8_t status = 0;
-      pal_get_server_power(fru, &status);
-      if(status == SERVER_POWER_ON) {
-        print_psb_config(fru);
-      } else {
-        printf("%s is not POWER ON \n", argv[1]);
-        print_usage_help();
-      }
+  if (psb_util_args.print_help) {
+    print_usage_help();
+    return 0;
+  }
+
+  if (psb_util_args.fruid == FRU_ALL) {
+    size_t i;
+    for (i=1; i<=MAX_NODES; i++) {
+      ret |= do_action(i);
+      printf("\n");
+    }
+  } else {
+    ret = do_action(psb_util_args.fruid);
   }
 
   return ret;

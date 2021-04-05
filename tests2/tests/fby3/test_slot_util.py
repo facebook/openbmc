280548e47ffa2f9039cc239df2f84bf24fa60922
David Cheng <davcheng@fb.com>
2021-04-05 09:36:51 -0700

    add slot-util cit to fby3
    Summary:
    adding slot-util test to fby3:
    1. test slot-util slot<num>
    2. test slot-util <num>
    3. test slot-util --show-mac and verify the output format
    
    Test Plan:
    cit run on fby3:
    ```
    root@sled2402-oob:/usr/local/bin/tests2# python cit_runner.py --platform fby3 --run-test tests.fby3.test_slot_util.SlotUtilTest
    test_show_slot_by_name (tests.fby3.test_slot_util.SlotUtilTest)
    check if [slot_name] exist in the ouput ... ok
    test_show_slot_by_number (tests.fby3.test_slot_util.SlotUtilTest)
    check if [slot_name] exist in the ouput ... ok
    test_show_slot_mac (tests.fby3.test_slot_util.SlotUtilTest)
    test slot-util --show-mac ... ok
    
    ----------------------------------------------------------------------
    Ran 3 tests in 38.029s
    
    OK
    ```
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 425a435436

---
+++ tests2/tests/fby3/test_slot_util.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+from common.base_slot_util_test import BaseSlotUtilTest
+
+slot_nums = [1, 2, 3, 4]
+
+
+class SlotUtilTest(BaseSlotUtilTest):
+    def set_slot_nums(self):
+        self.slot_nums = slot_nums

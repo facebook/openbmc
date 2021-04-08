4b4dc423625637eab8531ce2bd1f7582beebec09
Amithash Prasad <amithash@fb.com>
2021-04-08 13:41:49 -0700

    kv: Add different exceptions which kv can throw
    Summary:
    Interpret errno returned by kv_get/kv_set/kv_del and use that information to throw appropriate exceptions.
    This would allow users to handle errors conditionally.
    
    Test Plan:
    Build and flash on tiogapass and in python interactive commandline:
    ```
    >>> import kv
    >>> kv.kv_get("unknown")
    Traceback (most recent call last):
    <snip>
    kv.KeyNotFoundFailure
    
    ```
    
    Reviewed By: kawmarco
    
    fbshipit-source-id: 03a0aa0451

--- common/recipes-lib/kv/files/kv.py
+++ common/recipes-lib/kv/files/kv.py
@@ -18,9 +18,10 @@
 # Boston, MA 02110-1301 USA
 #
 import ctypes
+import os
 
 
-lkv_hndl = ctypes.CDLL("libkv.so.0")
+lkv_hndl = ctypes.CDLL("libkv.so.0", use_errno=True)
 
 FPERSIST = 1
 FCREATE = 2
@@ -30,12 +31,42 @@ class KeyOperationFailure(Exception):
     pass
 
 
+class InvalidParameter(KeyOperationFailure):
+    pass
+
+
+class KeyNotFoundFailure(KeyOperationFailure):
+    pass
+
+
+class KeyAlreadyExists(KeyOperationFailure):
+    pass
+
+
+class KeyValueTooBig(KeyOperationFailure):
+    pass
+
+
+def _handle_error():
+    e = ctypes.get_errno()
+    if e == os.errno.ENOENT:
+        raise KeyNotFoundFailure()
+    elif e == os.errno.EINVAL:
+        raise InvalidParameter()
+    elif e == os.errno.E2BIG:
+        raise KeyValueTooBig()
+    elif e == os.errno.EEXIST:
+        raise KeyAlreadyExists()
+    else:
+        raise KeyOperationFailure("KV operation failed with errno = " + str(e))
+
+
 def kv_get(key, flags=0, binary=False):
     key_c = ctypes.create_string_buffer(key.encode())
     value = ctypes.create_string_buffer(256)
     ret = lkv_hndl.kv_get(key_c, value, 0, ctypes.c_uint(flags))
     if ret != 0:
-        raise KeyOperationFailure
+        _handle_error()
     if binary:
         return value.value
     return value.value.decode()
@@ -49,11 +80,11 @@ def kv_set(key, value, flags=0):
         value_c = ctypes.create_string_buffer(value.encode())
     ret = lkv_hndl.kv_set(key_c, value_c, 0, ctypes.c_uint(flags))
     if ret != 0:
-        raise KeyOperationFailure
+        _handle_error()
 
 
 def kv_del(key, flags=0):
     key_c = ctypes.create_string_buffer(key.encode())
     ret = lkv_hndl.kv_del(key_c, ctypes.c_uint(flags))
     if ret != 0:
-        raise KeyOperationFailure
+        _handle_error()

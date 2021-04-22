f32e1e68ec603fa4ab9842a1861a7bcc81b107ab
David Cheng <davcheng@fb.com>
2021-04-22 13:27:07 -0700

    adding CIT tests to yosemite platform
    Summary:
    As title. Following tests will be added for yosemite platforms:
    test_fans.py
    test_cfg_util.py
    test_busybox_syntax.py
    test_syslog_restart.py
    test_process_running.py
    test_power_util.py
    test_libpal.py
    test_kernel_abi.py
    test_bic.py
    test_sensor.py
    test_fru.py
    test_fw_util.py
    test_log_util.py
    test_rest_endpoint.py
    
    Test Plan: Sandcastle RUN: https://www.internalfb.com/intern/sandcastle/job/13510799294782004/
    
    Reviewed By: amithash
    
    fbshipit-source-id: 2a21833d7e

---
+++ tests2/tests/yosemite/test_sensor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_sensor_test import SensorUtilTest
+from tests.yosemite.test_data.sensors.sensors import SENSORS
+
+
+class Slot1SensorTest(SensorUtilTest, unittest.TestCase):
+    FRU_NAME = "slot1"
+
+    def set_sensors_cmd(self):
+        self.sensors_cmd = ["/usr/local/bin/sensor-util {}".format(self.FRU_NAME)]
+
+    def test_sensor_keys(self):
+        result = self.get_parsed_result()
+        for key in SENSORS[self.FRU_NAME]:
+            with self.subTest(sensor=key):
+                self.assertIn(key, result.keys(), "Missing sensor {}".format(key))
+
+
+class Slot2SensorTest(Slot1SensorTest):
+    FRU_NAME = "slot2"
+
+
+class Slot3SensorTest(Slot1SensorTest):
+    FRU_NAME = "slot3"
+
+
+class Slot4SensorTest(Slot1SensorTest):
+    FRU_NAME = "slot4"
+
+
+class NicSensorTest(Slot1SensorTest):
+    FRU_NAME = "nic"
+
+
+class SPBSensorTest(Slot1SensorTest):
+    FRU_NAME = "spb"

ce88d3e4c9833eb2a8865ab587f2e4d49094f3aa
Vineela Kukkadapu <vineelak@fb.com>
2021-04-23 11:32:14 -0700

    Add erase data logic for minipack
    Summary: Adding copy-paste logic for minipack to erase /mnt/data partition similar to g100 and yamp. Verified and tested as in testplan. Except for flash erase size no other difference. scuba oobgrader: https://fburl.com/scuba/openbmc_upgrades/ke0si2dj
    
    Test Plan:
    ```
    [vineelak@devbig609 flashy{minipack_jffs}]$ ./build.sh && ./build_dev.sh && go test ./...
    ok      github.com/facebook/openbmc/tools/flashy        (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/common (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/galaxy100      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/minipack       (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/wedge100       (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/yamp   (cached)
    ?       github.com/facebook/openbmc/tools/flashy/flash_procedure        [no test files]
    ok      github.com/facebook/openbmc/tools/flashy/install        (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/fileutils  (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash/flashcp      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash/flashutils   (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash/flashutils/devices   (cached)
    ?       github.com/facebook/openbmc/tools/flashy/lib/logger     [no test files]
    ok      github.com/facebook/openbmc/tools/flashy/lib/step       (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/utils      0.430s
    ok      github.com/facebook/openbmc/tools/flashy/lib/validate   (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/validate/partition (cached)
    ?       github.com/facebook/openbmc/tools/flashy/tests  [no test files]
    ok      github.com/facebook/openbmc/tools/flashy/tests/facebook (cached)
    ?       github.com/facebook/openbmc/tools/flashy/utilities      [no test files]
    ```
    build ephemeral flashy
    
    ```
    [vineelak@devbig609 ~]$ oobgrader  --fbpkg-name openbmc.image.minipack100:provisioning_stable --host fboss124220106-oob.snc1 --wait --flashy-tag 86d9f9c --force
    Will trigger an upgrade on 1 systems, press [enter] to continue or ctrl+c to abort
    WARNING: Logging before InitGoogleLogging() is written to STDERR
    I0423 09:03:52.832588 1506965 Logger.cpp:2791] Logging to non-prod scribe category for hive table named 'cws_api_client_sandbox'
    I0423 09:03:52.832685 1506965 Logger.cpp:2130] Logging to non-prod scuba table named 'cws_api_client/sandbox'
    f4d9c0d3-62ed-4f66-9996-4e1b27beffd3
    Host                     Workflow ID                           Progress    Status                   Result
    -----------------------  ------------------------------------  ----------  -----------------------  ----------------------
    fboss124220106-oob.snc1  f4d9c0d3-62ed-4f66-9996-4e1b27beffd3  finished    WorkflowStatus.FINISHED  FinishStatus.SUCCEEDED
    [vineelak@devbig609 ~]$
    
    ```
    
    Reviewed By: doranand
    
    fbshipit-source-id: ccec6becf5

---
+++ tools/flashy/checks_and_remediations/minipack/00_erase_data_partition_test.go
@@ -0,0 +1,159 @@
+/**
+ * Copyright 2020-present Facebook. All Rights Reserved.
+ *
+ * This program file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in a file named COPYING; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ */
+
+package remediations_minipack
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/facebook/openbmc/tools/flashy/lib/flash/flashutils"
+	"github.com/facebook/openbmc/tools/flashy/lib/flash/flashutils/devices"
+	"github.com/facebook/openbmc/tools/flashy/lib/step"
+	"github.com/facebook/openbmc/tools/flashy/lib/utils"
+	"github.com/pkg/errors"
+)
+
+type mockFlashDevice struct {
+}
+
+func (m *mockFlashDevice) GetType() string         { return "mocktype" }
+func (m *mockFlashDevice) GetSpecifier() string    { return "mockspec" }
+func (m *mockFlashDevice) GetFilePath() string     { return "/dev/mock" }
+func (m *mockFlashDevice) GetFileSize() uint64     { return uint64(1234) }
+func (m *mockFlashDevice) MmapRO() ([]byte, error) { return nil, nil }
+func (m *mockFlashDevice) Munmap([]byte) error     { return nil }
+func (m *mockFlashDevice) Validate() error         { return nil }
+
+func TestEraseDataPartition(t *testing.T) {
+	getFlashDeviceOrig := flashutils.GetFlashDevice
+	isDataPartitionMountedOrig := utils.IsDataPartitionMounted
+	runCommandOrig := utils.RunCommand
+	getMTDMapFromSpecifierOrig := utils.GetMTDMapFromSpecifier
+	defer func() {
+		flashutils.GetFlashDevice = getFlashDeviceOrig
+		utils.IsDataPartitionMounted = isDataPartitionMountedOrig
+		utils.RunCommand = runCommandOrig
+		utils.GetMTDMapFromSpecifier = getMTDMapFromSpecifierOrig
+	}()
+
+	flashDevice := &mockFlashDevice{}
+
+	cases := []struct {
+		name              string
+		getFlashDeviceErr error
+		eraseSize         string
+		dataPartMounted   bool
+		dataPartErr       error
+		wantCmds          []string
+		cmdErr            error
+		want              step.StepExitError
+	}{
+		{
+			name:              "found and erased",
+			getFlashDeviceErr: nil,
+			eraseSize: 	 "00001000",
+			dataPartMounted: false,
+			dataPartErr:     nil,
+			wantCmds:        []string{"flash_eraseall -j /dev/mock"},
+			cmdErr:          nil,
+			want:            nil,
+		},
+		{
+			name:              "No 'data0' partition found",
+			getFlashDeviceErr: errors.Errorf("not found"),
+			eraseSize:         "00001000",
+			dataPartMounted:   false,
+			dataPartErr:       nil,
+			wantCmds:          []string{},
+			cmdErr:            nil,
+			want:              nil,
+		},
+		{
+			name:              "flash_eraseall failed",
+			getFlashDeviceErr: nil,
+			eraseSize:         "00001000",
+			dataPartMounted: false,
+			dataPartErr:     nil,
+			wantCmds:        []string{"flash_eraseall -j /dev/mock"},
+			cmdErr:          errors.Errorf("flash_eraseall failed"),
+			want: step.ExitSafeToReboot{
+				errors.Errorf("Failed to erase data0 partition: flash_eraseall failed"),
+			},
+		},
+		{
+			name:              "error checking /mnt/data mount status",
+			getFlashDeviceErr: nil,
+			eraseSize:         "00001000",
+			dataPartMounted: false,
+			dataPartErr:     errors.Errorf("check failed"),
+			wantCmds:        []string{},
+			cmdErr:          nil,
+			want: step.ExitSafeToReboot{
+				errors.Errorf("Failed to check if /mnt/data is mounted: check failed"),
+			},
+		},
+		{
+			name:              "/mnt/data still mounted (RO, possibly)",
+			getFlashDeviceErr: nil,
+			eraseSize:         "00001000",
+			dataPartMounted: true,
+			dataPartErr:     nil,
+			wantCmds:        []string{},
+			cmdErr:          nil,
+			want: step.ExitSafeToReboot{
+				errors.Errorf("/mnt/data is still mounted, this may mean that the " +
+					"unmount data partition step fell back to remounting RO. This current step " +
+					"needs /mnt/data to be completely unmounted!"),
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			gotCmds := []string{}
+			flashutils.GetFlashDevice = func(deviceID string) (devices.FlashDevice, error) {
+				if deviceID != "mtd:data0" {
+					t.Errorf("deviceID: want '%v' got '%v'", "mtd:data0", deviceID)
+				}
+				return flashDevice, tc.getFlashDeviceErr
+			}
+			utils.GetMTDMapFromSpecifier = func(deviceID string) (map[string]string, error) {
+				m := make(map[string]string)
+				m["erasesize"] = tc.eraseSize
+				return m, nil
+			}
+			utils.RunCommand = func(cmdArr []string, timeout time.Duration) (int, error, string, string) {
+				gotCmds = append(gotCmds, strings.Join(cmdArr, " "))
+				return 0, tc.cmdErr, "", ""
+			}
+			utils.IsDataPartitionMounted = func() (bool, error) {
+				return tc.dataPartMounted, tc.dataPartErr
+			}
+
+			got := eraseDataPartition(step.StepParams{})
+			step.CompareTestExitErrors(tc.want, got, t)
+			if !reflect.DeepEqual(tc.wantCmds, gotCmds) {
+				t.Errorf("cmds: want '%v' got '%v'", tc.wantCmds, gotCmds)
+			}
+		})
+	}
+}

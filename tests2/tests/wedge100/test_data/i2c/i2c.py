39e9753fa303bc13e20aad8e3bdb5882d7967c40
Tao Ren <taoren@fb.com>
2021-05-10 11:19:18 -0700

    tests2: wedge100: add test_i2c.py
    Summary:
    Add test_i2c.py for sanity testing i2c device creation and driver
    binding.
    
    Note: PSU/PEM i2c devices are not covered by this test because device
    topology varies across different wedge100 switches.
    
    Test Plan:
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py --run-test
    tests.wedge100.test_i2c.Wedge100I2cTest.test_i2c_tree
    test_i2c_tree (tests.wedge100.test_i2c.Wedge100I2cTest)
    check if i2c devices are created and drivers are binded properly. ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.342s
    
    OK
    
    Reviewed By: GoldenBug
    
    fbshipit-source-id: cf7804d14e

---
+++ tests2/tests/wedge100/test_data/i2c/i2c.py
@@ -0,0 +1,62 @@
+plat_i2c_tree = {
+    "2-003a": {
+        "name": "pwr1014a",
+        "driver": "pwr1014a",
+    },
+    "3-0048": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "3-0049": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "3-004a": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "3-004b": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "3-004c": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "4-0033": {
+        "name": "com_e_driver",
+        "driver": "com_e_driver",
+    },
+    "6-002f": {
+        "name": "pcf8574",
+        "driver": "",
+    },
+    "6-0051": {
+        "name": "24c64",
+        "driver": "at24",
+    },
+    "7-0070": {
+        "name": "pca9548",
+        "driver": "pca954x",
+    },
+    "8-0033": {
+        "name": "fancpld",
+        "driver": "fancpld",
+    },
+    "8-0048": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "8-0049": {
+        "name": "tmp75",
+        "driver": "lm75",
+    },
+    "9-0020": {
+        "name": "tpm_i2c_infineon",
+        "driver": "tpm_i2c_infineon",
+    },
+    "12-0031": {
+        "name": "syscpld",
+        "driver": "syscpld",
+    },
+}

f7ebfd90fb95d5746044cbb2ba6135d8bca4d3c4
Tao Ren <taoren@fb.com>
2021-05-10 16:53:27 -0700

    tests2: wedge400: add test_usb_host.py
    Summary: Enable usb_host_test for wedge400.
    
    Test Plan:
    1) usb_host_test passed on wedge400.
    2) ondiff netcastle_openbmc_cit_diff_wedge400 passed.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 4289336265

---
+++ tests2/tests/wedge400/test_usb_host.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_usb_host_test import BaseUSBHostTest
+
+
+class USBHostDeviceTest(BaseUSBHostTest, unittest.TestCase):
+    def set_usb_devices(self):
+        self.usb_devices = [
+            "1d6b:0001",  # USB 1.1 Root Hub
+            "1d6b:0002",  # USB 2.0 Root Hub
+            "0403:6001",  # FT232 Serial (UART) IC
+        ]

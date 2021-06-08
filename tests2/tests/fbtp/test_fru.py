ec2f6455c6a9467b993fba67fe85415b9e31365a
David Cheng <davcheng@fb.com>
2021-06-07 21:01:02 -0700

    update fru tests suite
    Summary: as title
    
    Test Plan: sandcastle run: https://www.internalfb.com/intern/sandcastle/job/36028797450510326/
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: 573db3fced

--- tests2/tests/fbtp/test_fru.py
+++ tests2/tests/fbtp/test_fru.py
@@ -25,4 +25,81 @@ from common.base_fru_test import CommonFruTest
 class FruMbTest(CommonFruTest, unittest.TestCase):
     def setUp(self):
         self.fru_cmd = ["/usr/local/bin/fruid-util", "mb"]
-        self.fru_fields = {"product": 2, "board": 2, "chassis": 2}
+        self.fru_fields = {"product": 2, "board": 3, "chassis": 2}
+
+    def getBoardFields(self, num_custom=0):
+        board_fields = {
+            "Board Mfg Date",
+            "Board Mfg",
+            "Board Product",
+            "Board Serial",
+            "Board Part Number",
+        }
+        for i in range(1, num_custom + 1):
+            board_fields.add("Board Custom Data {}".format(i))
+        return board_fields
+
+    def getProductFields(self, num_custom=0):
+        product_fields = {
+            "Product Manufacturer",
+            "Product Name",
+            "Product Part Number",
+            "Product Version",
+            "Product Serial",
+            "Product Asset Tag",
+        }
+        for i in range(1, num_custom + 1):
+            product_fields.add("Product Custom Data {}".format(i))
+        return product_fields
+
+
+class FruNicTest(CommonFruTest, unittest.TestCase):
+    def setUp(self):
+        self.fru_cmd = ["/usr/local/bin/fruid-util", "nic"]
+        self.fru_fields = {"product": 1, "board": 4, "chassis": 0}
+
+
+class FruRiserSlot2Test(CommonFruTest, unittest.TestCase):
+    def setUp(self):
+        self.fru_cmd = ["/usr/local/bin/fruid-util", "riser_slot2"]
+        self.fru_fields = {"product": 1, "board": 4}
+
+    def getBoardFields(self, num_custom=0):
+        board_fields = {
+            "Board Mfg Date",
+            "Board Mfg",
+            "Board Product",
+            "Board Serial",
+            "Board Part Number",
+        }
+        for i in range(1, num_custom + 1):
+            board_fields.add("Board Custom Data {}".format(i))
+        return board_fields
+
+    def getProductFields(self, num_custom=0):
+        product_fields = {
+            "Product Part Number",
+            "Product Serial",
+        }
+        for i in range(1, num_custom + 1):
+            product_fields.add("Product Custom Data {}".format(i))
+        return product_fields
+
+    def getFields(self, field_type, num_custom):
+        field_funcs = {
+            "product": self.getProductFields,
+            "board": self.getBoardFields,
+        }
+        self.assertIn(
+            field_type, field_funcs, "Unknown field type: {}".format(field_type)
+        )
+        return field_funcs[field_type](num_custom)
+
+
+class FruRiserSlot3Test(FruRiserSlot2Test):
+    pass
+
+
+@unittest.skip("not available")
+class FruRiserSlot4Test(FruRiserSlot2Test):
+    pass

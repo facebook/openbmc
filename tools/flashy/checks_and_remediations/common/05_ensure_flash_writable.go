20762f4bbb573178d77f8dcc1e2c9e7795580d23
Andrew Doran <doranand@fb.com>
2021-06-17 08:54:11 -0700

    vboot: ensure flash is writable
    Summary:
    If the two chips (one read-only & one read-write) are swapped, or the second
    chip is read-only for some other reason, it can cause big headaches.
    
    Test Plan:
    Build ephemeral and run in netcastle:
    
    ```
    $ buck run //neteng/netcastle:netcastle -- --team openbmc --debug --platform fbttn --dev --basset-query openbmc.cit:fbttn:fbttn.312681474.fre101 --flashy-tag a6413bb --fbpkg-name openbmc.image.brycecanyon1:weeklybuild --regex syslog
    ...
    [   ERROR] 0
    [  FAILED] 0
    [ TIMEOUT] 0
    [  PASSED] 5
    [ SKIPPED] 0
    [ OMITTED] 0
    [ RETRIED] 0
    [06/17/2021-04:23:19.152] [2469666] [140381038589888] [    INFO] All tests passed!
    ...
    [06/17/2021-04:23:29.808] [2469666] [140381038589888] [    INFO] Console log has been everpasted: https://www.internalfb.com/intern/everpaste/?color=1&handle=GEPpgwRSiuh5jNQDAMlFMuHJSc5dbr0LAAAz
    ```
    
    Build & run unit tests:
    
    ```
    0 ~/local/openbmc/tools/flashy $ ./build.sh && ./build_dev.sh && go test ./...
    ok      github.com/facebook/openbmc/tools/flashy        (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/common (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/galaxy100      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/minipack       (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/wedge100       (cached)
    ok      github.com/facebook/openbmc/tools/flashy/checks_and_remediations/yamp   (cached)
    ?       github.com/facebook/openbmc/tools/flashy/flash_procedure        [no test files]
    ok      github.com/facebook/openbmc/tools/flashy/install        (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/fileutils  (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash/flashcp      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash/flashutils   (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/flash/flashutils/devices   (cached)
    ?       github.com/facebook/openbmc/tools/flashy/lib/logger     [no test files]
    ok      github.com/facebook/openbmc/tools/flashy/lib/step       (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/utils      (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/validate   (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/validate/image     (cached)
    ok      github.com/facebook/openbmc/tools/flashy/lib/validate/partition (cached)
    ?       github.com/facebook/openbmc/tools/flashy/tests  [no test files]
    ok      github.com/facebook/openbmc/tools/flashy/tests/facebook (cached)
    ?       github.com/facebook/openbmc/tools/flashy/utilities      [no test files]
    ```
    
    Use it to do manual brycecanyon upgrades in prod.  Bad BMC detected, see workflow: 040dd0c8-7442-4733-802c-e842c3bbb6cc
    
    Reviewed By: lsiudut, kawmarco
    
    fbshipit-source-id: 637c394f04

---
+++ tools/flashy/checks_and_remediations/common/05_ensure_flash_writable.go
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2020-present Facebook. All Rights Reserved.
+ *
+ * This program file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in a file named COPYING; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ */
+
+package common
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"strings"
+	"time"
+
+	"github.com/facebook/openbmc/tools/flashy/lib/fileutils"
+	"github.com/facebook/openbmc/tools/flashy/lib/step"
+	"github.com/facebook/openbmc/tools/flashy/lib/utils"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	step.RegisterStep(ensureFlashWritable)
+}
+
+// A check for verified boot systems where the second flash is unwritable.
+//
+// On vboot BMCs the first chip is intended to be read-only in hardware and
+// contains the Golden Image.  The second chip is intended to be read-write. 
+// Some possible & obvious reasons this could go wrong:
+//
+// - the chips were installed in the wrong slots during manufacture.
+// - the chips were erroneously swapped during repair (this is a valid
+//   repair flow for non-vboot BMCs, though).
+// - two read-only chips were installed.
+//
+// If the second chip is read-only, it's not possible to flash the BMC here. 
+// Rebooting the BMC could have unpredictable consequences.  Leave these
+// BMCs in place for later detection and repair.
+//
+// NB: it's conceivable that a non-vboot BMCs could boot up without an mtd
+// partition for the U-Boot environment, so skip the check on those.
+func ensureFlashWritable(stepParams step.StepParams) step.StepExitError {
+	if !fileutils.FileExists("/usr/local/bin/vboot-util") {
+		log.Printf("Non-vboot platform, skipping flash writability check")
+		return nil
+	}
+
+	// First up check that fw_printenv works okay and produces output.
+	cmd := []string{"fw_printenv", "bootargs"}
+	_, err, stdout, stderr := utils.RunCommand(cmd, 30*time.Second)
+	if err != nil || !strings.Contains(stdout, "bootargs") {
+		log.Printf("fw_printenv doesn't work: %v, stderr: %v", err, stderr)
+		return nil
+	}
+
+	// A dummy u-Boot environment variable & value.
+	key := "_flashy_test"
+	value := fmt.Sprintf("%08x", rand.Int31())
+
+	// Set the variable and on failure just ignore the error.
+	cmd = []string{"fw_setenv", key, value}
+	_, err, stdout, stderr = utils.RunCommand(cmd, 30*time.Second)
+	if err != nil {
+		log.Printf("fw_setenv doesn't work: %v, stderr: %v", err, stderr)
+		return nil
+	}
+
+	// Did the set take hold?  If not the environment is read-only.
+	var errRet step.StepExitError = nil
+	cmd = []string{"fw_printenv", key}
+	_, err, stdout, stderr = utils.RunCommand(cmd, 30*time.Second)
+	if err != nil || !strings.Contains(stdout, value) {
+		errMsg := errors.Errorf(
+			"U-Boot environment is read only: flash chips swapped?" +
+			" Error code: %v, stderr: %v", err, stderr)
+		errRet = step.ExitUnsafeToReboot{Err: errMsg}
+		log.Printf("U-Boot environment is READ ONLY")
+	} else {
+		log.Printf("U-Boot environment confirmed writable")
+	}
+
+	// Immediately clear even on error, in case something wacky happened.
+	// The environment will typically be overwritten when the image is
+	// flashed but cleaning up anyway doesn't hurt.
+	cmd = []string{"fw_setenv", key}
+	_, err, stdout, stderr = utils.RunCommand(cmd, 30*time.Second)
+	if err != nil {
+		log.Printf("Couldn't fw_setenv: %v, stderr: %v", err, stderr)
+	}
+
+	return errRet
+}
---
+++ tools/flashy/checks_and_remediations/common/05_ensure_flash_writable_test.go
@@ -0,0 +1,160 @@
+/**
+ * Copyright 2020-present Facebook. All Rights Reserved.
+ *
+ * This program file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program in a file named COPYING; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ */
+
+package common
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/facebook/openbmc/tools/flashy/lib/fileutils"
+	"github.com/facebook/openbmc/tools/flashy/lib/step"
+	"github.com/facebook/openbmc/tools/flashy/lib/utils"
+	"github.com/pkg/errors"
+)
+
+func TestEnsureFlashWritable(t *testing.T) {
+	// save log output into buf for testing
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	fileExistsOrig := fileutils.FileExists
+	runCommandOrig := utils.RunCommand
+
+	defer func() {
+		log.SetOutput(os.Stderr)
+		fileutils.FileExists = fileExistsOrig
+		utils.RunCommand = runCommandOrig
+	}()
+
+	cases := []struct {
+		name              string
+		vbootUtilExists   bool
+		failPrint         bool
+		failSet           bool
+		failCheck         bool
+		failClear         bool
+		want              step.StepExitError
+	}{
+		{
+			name:              "non-vboot case",
+			vbootUtilExists:   false,
+			failPrint:         false,
+			failSet:           false,
+			failCheck:         false,
+			failClear:         false,
+			want:              nil,
+		},
+		{
+			name:              "working case",
+			vbootUtilExists:   true,
+			failPrint:         false,
+			failSet:           false,
+			failCheck:         false,
+			failClear:         false,
+			want:              nil,
+		},
+		{
+			name:              "fw_printenv broken",
+			vbootUtilExists:   true,
+			failPrint:         true,
+			failSet:           false,
+			failCheck:         false,
+			failClear:         false,
+			want:              nil,
+		},
+		{
+			name:              "set _flashy_test fails",
+			vbootUtilExists:   true,
+			failPrint:         false,
+			failSet:           true,
+			failCheck:         false,
+			failClear:         false,
+			want:              nil,
+		},
+		{
+			name:              "check _flashy_test fails",
+			vbootUtilExists:   true,
+			failPrint:         false,
+			failSet:           false,
+			failCheck:         true,
+			failClear:         false,
+			want:              step.ExitUnsafeToReboot{
+                                Err: errors.Errorf("U-Boot environment is read only: flash chips swapped? Error code: <nil>, stderr: ")},
+		},
+		{
+			name:              "clear _flashy_test fails",
+			vbootUtilExists:   true,
+			failPrint:         false,
+			failSet:           false,
+			failCheck:         false,
+			failClear:         true,
+			want:              nil,
+		},
+	}
+
+	key := "_fake_key"
+	value := "_fake_value"
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			fileutils.FileExists = func(filename string) bool {
+				return tc.vbootUtilExists
+			}
+			utils.RunCommand = func(cmdArr []string, timeout time.Duration) (int, error, string, string) {
+				if (cmdArr[0] == "fw_printenv") {
+					if (cmdArr[1] == "bootargs") {
+						if (tc.failPrint) {
+							return 0, nil, "", ""
+						} else {
+							return 0, nil, "bootargs=foo", ""
+						}
+					} else if (cmdArr[1] == key) {
+						if (tc.failCheck) {
+							return 0, nil, "", ""
+						} else {
+							return 0, nil, key + "=" + value, ""
+						}
+					}
+				} else if (cmdArr[0] == "fw_setenv") {
+					if (len(cmdArr) <= 2) {
+						if (tc.failClear) {
+							return 0, errors.Errorf("err1"), "", "err1"
+						} else {
+							return 0, nil, "bootargs=foo", ""
+						}
+					} else {
+						if (tc.failSet) {
+							return 0, errors.Errorf("err2"), "", "err2"
+						} else {
+							key = cmdArr[1]
+							value = cmdArr[2]
+							return 0, nil, "", ""
+						}
+					}
+				}
+				return 0, errors.Errorf("err3"), "", "err3"
+			}
+			got := ensureFlashWritable(step.StepParams{})
+			step.CompareTestExitErrors(tc.want, got, t)
+		})
+	}
+}

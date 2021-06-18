819acfd7ebefacc733183adf4334b2327fd02574
Mike Choi <mikechoi@fb.com>
2021-06-17 19:57:10 -0700

    Increase the number of retries in init.d/hostname.sh
    Summary:
    [Common] Increase the number of retries in init.d/hostname.sh
    In some DC, the existing 10 retries doesn't seem to be enough. So increased it to 20.
    In order to make BMC keep retry for a longer time without adding too much load to our DNS,
    we use exponental back-off. That is,
    1) Up to retry iteration #10, wait 15 seconds in between retries (No Change)
    2) After iteration #11 until #20 (end), doubles the wait time in every iteration
      (30,60,120,240, ...  with the max 3600 seconds)
    
    Test Plan: Tested on fsw001.p047.f01.atn6 and fuji.01.07.mpk17
    
    Reviewed By: amithash
    
    fbshipit-source-id: 116428440e

--- common/recipes-core/initscripts/files/hostname.sh
+++ common/recipes-core/initscripts/files/hostname.sh
@@ -22,11 +22,25 @@ set_hostname() {
 }
 
 update_hostname() {
-  RETRIES=10
-  while [ $RETRIES -gt 0 ]; do
-    RETRIES=$((RETRIES-1))
+  #
+  # Increasing the number of retries to 20, as 10 is not enough in some DC.
+  # In order to reduce the load to DNS server, we use exponential back-off. 
+  # That is, the delay between retry will double each time, after loop #10.
+  #
+  RETRY_MAX=20
+  RETRY_DELAY=15
+  DELAY_MAX=3600
+  LOOP_NUMBER=0
+  while [ $LOOP_NUMBER -lt $RETRY_MAX ]; do
+    LOOP_NUMBER=$((LOOP_NUMBER+1))
+    if [ $LOOP_NUMBER -gt 10 ]; then
+      RETRY_DELAY=$((RETRY_DELAY*2))
+      if [ $RETRY_DELAY -gt $DELAY_MAX ]; then
+         RETRY_DELAY=$DELAY_MAX
+      fi
+    fi
     # Wait till DHCP gets its IP addresses.
-    sleep 15
+    sleep $RETRY_DELAY
     # Do a DNS lookup for each IP address on eth0 to find a hostname.
     # If multiple hostnames are returned, the sled one, if present, is preferred.
     FOUND_NAME=""

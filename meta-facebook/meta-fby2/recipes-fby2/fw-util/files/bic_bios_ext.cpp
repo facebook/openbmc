363203800483eae8e9f433774c536433096b5d0c
Peter Fetros <goldenbug@fb.com>
2021-06-22 12:25:09 -0700

    fby2: fw-util, remove EP and RC related code (#2564)
    Summary:
    Remove EP (meta-fby2-ep) & RC (meta-fby2-rc) related code because this two platforms are no longer supported.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2564
    
    Test Plan:
    1. Build and test on fby2, fbgp2 & northdome
    2. fw-util update works normally as before for both BIOS & VR updating
    
    Reviewed By: tao-ren
    
    fbshipit-source-id: bf7084c57d

--- meta-facebook/meta-fby2/recipes-fby2/fw-util/files/bic_bios_ext.cpp
+++ meta-facebook/meta-fby2/recipes-fby2/fw-util/files/bic_bios_ext.cpp
@@ -36,72 +36,9 @@ int BiosExtComponent::update_internal(const std::string &image, int fd, bool for
   uint8_t status;
   int retry_count = 60;
 
-#if defined(CONFIG_FBY2_EP) || defined(CONFIG_FBY2_RC)
-  uint8_t server_type = 0xFF;
-  uint8_t gs_action = 1;
-#endif
-
   try {
     server.ready();
-#if defined(CONFIG_FBY2_EP) || defined(CONFIG_FBY2_RC)
-    ret = fby2_get_server_type(slot_id, &server_type);
-    if (ret) {
-      syslog(LOG_ERR, "%s, Get server type failed\n", __func__);
-      return -1;
-    }
 
-    uint8_t power_cmd;
-    switch (server_type) {
-      case SERVER_TYPE_EP:
-        power_cmd = SERVER_POWER_OFF;
-        break;
-      case SERVER_TYPE_RC:
-      case SERVER_TYPE_TL:
-      default:
-        power_cmd = SERVER_GRACEFUL_SHUTDOWN;
-        break;
-    }
-    if (force) power_cmd = SERVER_POWER_OFF;
-    syslog(LOG_INFO, "%s: Slot%d power cmd %u", __func__, slot_id, power_cmd);
-    pal_set_server_power(slot_id, power_cmd);
-
-power_check:
-    //Checking Server Power Status to make sure Server is really Off
-    while (retry_count > 0) {
-      ret = pal_get_server_power(slot_id, &status);
-      if ((ret == 0) && (status == SERVER_POWER_OFF)) {
-        break;
-      }
-      if ((--retry_count) > 0) {
-        sleep(1);
-      }
-    }
-    if (retry_count <= 0) {
-      switch (server_type) {
-        case SERVER_TYPE_EP:
-          cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the update!" << endl;
-          break;
-        case SERVER_TYPE_RC:
-          if (gs_action == 1) {
-            syslog(LOG_WARNING, "%s: Graceful Shutdown failed before BIOS update on RC, using power off instead for slot%u", __func__, slot_id);
-            pal_set_server_power(slot_id, SERVER_POWER_OFF);     // Change to power off server due to graceful shutdown failed
-            retry_count = 60;
-            gs_action = 0;
-            goto power_check;
-          } else {
-            syslog(LOG_WARNING, "%s: Power-off still failed on RC before BIOS update for slot%u", __func__, slot_id);
-            cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the update!" << endl;
-          }
-          break;
-        case SERVER_TYPE_TL:
-        default:
-          syslog(LOG_WARNING, "%s: Graceful Shutdown failed before BIOS update on TL for slot%u", __func__, slot_id);
-          cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the update!" << endl;
-          break;
-      }
-      return -1;
-    }
-#else
     uint8_t power_cmd = (force ? SERVER_POWER_OFF : SERVER_GRACEFUL_SHUTDOWN);
 
     pal_set_server_power(slot_id, power_cmd);
@@ -120,21 +57,13 @@ power_check:
       cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the update!" << endl;
       return -1;
     }
-#endif
 
-#if defined(CONFIG_FBY2_EP) || defined(CONFIG_FBY2_RC)
-    if (server_type != SERVER_TYPE_EP && server_type != SERVER_TYPE_RC) {
-      cerr << "Putting ME into recovery mode..." << endl;
-      me_recovery(slot_id, RECOVERY_MODE);
-      sleep(1);
-    }
-#elif defined(CONFIG_FBY2_ND)
-    sleep(1);
-#else
+#if !defined(CONFIG_FBY2_ND)
     cerr << "Putting ME into recovery mode..." << endl;
     me_recovery(slot_id, RECOVERY_MODE);
-    sleep(1);
 #endif
+    sleep(1);
+
     if (!image.empty()) {
       ret = bic_update_firmware(slot_id, UPDATE_BIOS, (char *)image.c_str(), force);
     } else {
@@ -167,70 +96,9 @@ int BiosExtComponent::dump(string image) {
   int ret;
   uint8_t status;
   int retry_count = 60;
-#if defined(CONFIG_FBY2_EP) || defined(CONFIG_FBY2_RC)
-  uint8_t server_type = 0xFF;
-  uint8_t gs_action = 1;
-#endif
 
   try {
     server.ready();
-#if defined(CONFIG_FBY2_EP) || defined(CONFIG_FBY2_RC)
-    ret = fby2_get_server_type(slot_id, &server_type);
-    if (ret) {
-      syslog(LOG_ERR, "%s, Get server type failed\n", __func__);
-      return -1;
-    }
-    switch (server_type) {
-      case SERVER_TYPE_EP:
-        pal_set_server_power(slot_id, SERVER_POWER_OFF);
-        break;
-      case SERVER_TYPE_RC:
-        syslog(LOG_INFO, "%s: Start Graceful Shutdown before BIOS dump on RC for slot%u", __func__, slot_id);
-        pal_set_server_power(slot_id, SERVER_GRACEFUL_SHUTDOWN);
-        break;
-      case SERVER_TYPE_TL:
-      default:
-        syslog(LOG_INFO, "%s: Start Graceful Shutdown before BIOS dump on TL for slot%u", __func__, slot_id);
-        pal_set_server_power(slot_id, SERVER_GRACEFUL_SHUTDOWN);
-        break;
-    }
-
-power_check:
-    //Checking Server Power Status to make sure Server is really Off
-    while (retry_count > 0) {
-      ret = pal_get_server_power(slot_id, &status);
-      if ((ret == 0) && (status == SERVER_POWER_OFF)) {
-        break;
-      }
-      if ((--retry_count) > 0)
-        sleep(1);
-    }
-    if (retry_count <= 0) {
-      switch (server_type) {
-        case SERVER_TYPE_EP:
-          cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the dump!" << endl;
-          break;
-        case SERVER_TYPE_RC:
-          if (gs_action == 1) {
-            syslog(LOG_WARNING, "%s: Graceful Shutdown failed before BIOS dump on RC, using power off instead for slot%u", __func__, slot_id);
-            pal_set_server_power(slot_id, SERVER_POWER_OFF);     // Change to power off server due to graceful shutdown failed
-            retry_count = 60;
-            gs_action = 0;
-            goto power_check;
-          } else {
-            syslog(LOG_WARNING, "%s: Power-off still failed on RC before BIOS dump for slot%u", __func__, slot_id);
-            cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the dump!" << endl;
-          }
-          break;
-        case SERVER_TYPE_TL:
-        default:
-          syslog(LOG_WARNING, "%s: Graceful Shutdown failed before BIOS dump on TL for slot%u", __func__, slot_id);
-          cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the dump!" << endl;
-          break;
-      }
-      return -1;
-    }
-#else
     pal_set_server_power(slot_id, SERVER_GRACEFUL_SHUTDOWN);
 
     //Checking Server Power Status to make sure Server is really Off
@@ -246,17 +114,10 @@ power_check:
       cerr << "Failed to Power Off Server " << (int)slot_id << ". Stopping the dump!" << endl;
       return -1;
     }
-#endif
 
-#if defined(CONFIG_FBY2_EP) || defined(CONFIG_FBY2_RC)
-    if (server_type != SERVER_TYPE_EP && server_type != SERVER_TYPE_RC) {
-      me_recovery(slot_id, RECOVERY_MODE);
-      sleep(1);
-    }
-#else
     me_recovery(slot_id, RECOVERY_MODE);
     sleep(1);
-#endif
+
     ret = bic_dump_fw(slot_id, DUMP_BIOS, (char *)image.c_str());
     sleep(1);
     pal_set_server_power(slot_id, SERVER_12V_CYCLE);
--- meta-facebook/meta-fby2/recipes-fby2/fw-util/files/vr.cpp
+++ meta-facebook/meta-fby2/recipes-fby2/fw-util/files/vr.cpp
@@ -48,8 +48,7 @@ class VrComponent : public Component {
       }
       return 0;
     }
-#if defined(CONFIG_FBY2_RC) || defined(CONFIG_FBY2_EP) || \
-    defined(CONFIG_FBY2_ND)
+
     int ret;
     uint8_t server_type = 0xFF;
     ret = fby2_get_server_type(slot_id, &server_type);
@@ -58,128 +57,6 @@ class VrComponent : public Component {
     }
 
     switch (server_type) {
-      case SERVER_TYPE_RC:
-        try {
-          server.ready();
-          // Print VDD_APC_CBF VR Version
-          if (bic_get_fw_ver(slot_id, FW_VDD_APC_CBF_VR, ver)) {
-            printf("VDD_APC_CBF VR Version: NA\n");
-          } else {
-            printf(
-                "VDD_APC_CBF VR Version: 0x%02x%02x%02x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3],
-                ver[4]);
-          }
-
-          // Print DDR510 VR Version Version
-          if (bic_get_fw_ver(slot_id, FW_DDR510_VR, ver)) {
-            printf("DDR510 VR Version: NA\n");
-          } else {
-            printf(
-                "DDR510 VR Version: 0x%02x%02x%02x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3],
-                ver[4]);
-          }
-
-          // Print DDR423 VR Version Version
-          if (bic_get_fw_ver(slot_id, FW_DDR423_VR, ver)) {
-            printf("DDR423 VR Version: NA\n");
-          } else {
-            printf(
-                "DDR423 VR Version: 0x%02x%02x%02x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3],
-                ver[4]);
-          }
-        } catch (string err) {
-          printf("VDD_APC_CBF VR Version: NA (%s)\n", err.c_str());
-          printf("DDR510 VR Version: NA (%s)\n", err.c_str());
-          printf("DDR423 VR Version: NA (%s)\n", err.c_str());
-        }
-        break;
-
-      case SERVER_TYPE_EP:
-        try {
-          server.ready();
-          // Print VDD_CORE VR Version
-          if (bic_get_fw_ver(slot_id, FW_VDD_CORE_VR, ver)) {
-            printf("VDD_CORE VR Version: NA\n");
-          } else {
-            printf(
-                "VDD_CORE VR Version: 0x%02x%02x, 0x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3]);
-          }
-
-          // Print VDD_SRAM VR Version
-          if (bic_get_fw_ver(slot_id, FW_VDD_SRAM_VR, ver)) {
-            printf("VDD_SRAM VR Version: NA\n");
-          } else {
-            printf(
-                "VDD_SRAM VR Version: 0x%02x%02x, 0x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3]);
-          }
-
-          // Print VDD_MEM VR Version
-          if (bic_get_fw_ver(slot_id, FW_VDD_MEM_VR, ver)) {
-            printf("VDD_MEM VR Version: NA\n");
-          } else {
-            printf(
-                "VDD_MEM VR Version: 0x%02x%02x, 0x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3]);
-          }
-
-          // Print VDD_SOC VR Version
-          if (bic_get_fw_ver(slot_id, FW_VDD_SOC_VR, ver)) {
-            printf("VDD_SOC VR Version: NA\n");
-          } else {
-            printf(
-                "VDD_SOC VR Version: 0x%02x%02x, 0x%02x%02x\n",
-                ver[0],
-                ver[1],
-                ver[2],
-                ver[3]);
-          }
-
-          // Print DDR_AG VR Version
-          if (bic_get_fw_ver(slot_id, FW_DDR_AG_VR, ver)) {
-            printf("DDR_AG VR Version: NA\n");
-          } else {
-            printf("DDR_AG VR Version: 0x%02x%02x\n", ver[0], ver[1]);
-          }
-
-          // Print DDR_BH VR Version
-          if (bic_get_fw_ver(slot_id, FW_DDR_BH_VR, ver)) {
-            printf("DDR_BH VR Version: NA\n");
-          } else {
-            printf("DDR_BH VR Version: 0x%02x%02x\n", ver[0], ver[1]);
-          }
-        } catch (string err) {
-          printf("VDD_CORE VR Version: NA (%s)\n", err.c_str());
-          printf("VDD_SRAM VR Version: NA (%s)\n", err.c_str());
-          printf("VDD_MEM VR Version: NA (%s)\n", err.c_str());
-          printf("VDD_SOC VR Version: NA (%s)\n", err.c_str());
-          printf("DDR_AG VR Version: NA (%s)\n", err.c_str());
-          printf("DDR_BH VR Version: NA (%s)\n", err.c_str());
-        }
-        break;
-
       case SERVER_TYPE_ND:
         try {
           server.ready();
@@ -320,133 +197,16 @@ class VrComponent : public Component {
         }
         break;
     }
-#else // Non FBY2 platforms
-    try {
-      server.ready();
-      // Print PVCCIO VR Version
-      if (bic_get_fw_ver(slot_id, FW_PVCCIO_VR, ver)) {
-        printf("PVCCIO VR Version: NA\n");
-      } else {
-        printf(
-            "PVCCIO VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
 
-      // Print PVCCIN VR Version
-      if (bic_get_fw_ver(slot_id, FW_PVCCIN_VR, ver)) {
-        printf("PVCCIN VR Version: NA\n");
-      } else {
-        printf(
-            "PVCCIN VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
-
-      // Print PVCCSA VR Version
-      if (bic_get_fw_ver(slot_id, FW_PVCCSA_VR, ver)) {
-        printf("PVCCSA VR Version: NA\n");
-      } else {
-        printf(
-            "PVCCSA VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
-
-      // Print DDRAB VR Version
-      if (bic_get_fw_ver(slot_id, FW_DDRAB_VR, ver)) {
-        printf("DDRAB VR Version: NA\n");
-      } else {
-        printf(
-            "DDRAB VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
-
-      // Print DDRDE VR Version
-      if (bic_get_fw_ver(slot_id, FW_DDRDE_VR, ver)) {
-        printf("DDRDE VR Version: NA\n");
-      } else {
-        printf(
-            "DDRDE VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
-
-      // Print PVNNPCH VR Version
-      if (bic_get_fw_ver(slot_id, FW_PVNNPCH_VR, ver)) {
-        printf("PVNNPCH VR Version: NA\n");
-      } else {
-        printf(
-            "PVNNPCH VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
-
-      // Print P1V05 VR Version
-      if (bic_get_fw_ver(slot_id, FW_P1V05_VR, ver)) {
-        printf("P1V05 VR Version: NA\n");
-      } else {
-        printf(
-            "P1V05 VR Version: 0x%02x%02x, 0x%02x%02x\n",
-            ver[0],
-            ver[1],
-            ver[2],
-            ver[3]);
-      }
-    } catch (string err) {
-      printf("PVCCIO VR Version: NA (%s)\n", err.c_str());
-      printf("PVCCIN VR Version: NA (%s)\n", err.c_str());
-      printf("PVCCSA VR Version: NA (%s)\n", err.c_str());
-      printf("DDRAB VR Version: NA (%s)\n", err.c_str());
-      printf("DDRDE VR Version: NA (%s)\n", err.c_str());
-      printf("PVNNPCH VR Version: NA (%s)\n", err.c_str());
-      printf("P1V05 VR Version: NA (%s)\n", err.c_str());
-    }
-#endif
     return 0;
   }
 
   int update(string image) {
     int ret;
-#if defined(CONFIG_FBY2_EP)
-    uint8_t server_type = 0xFF;
-    char pwr_state[MAX_VALUE_LEN] = {0};
-#endif
 
     try {
       server.ready();
       ret = bic_update_fw(slot_id, UPDATE_VR, (char*)image.c_str());
-
-#if defined(CONFIG_FBY2_EP)
-      if (ret)
-        return ret;
-
-      ret = fby2_get_server_type(slot_id, &server_type);
-      if (ret) {
-        syslog(LOG_ERR, "%s, Get server type failed\n", __func__);
-        return -1;
-      }
-      switch (server_type) {
-        case SERVER_TYPE_EP:
-          pal_get_last_pwr_state(slot_id, pwr_state);
-          pal_set_server_power(slot_id, SERVER_12V_CYCLE);
-          pal_power_policy_control(slot_id, pwr_state);
-          break;
-      }
-#endif
     } catch (string err) {
       ret = FW_STATUS_NOT_SUPPORTED;
     }

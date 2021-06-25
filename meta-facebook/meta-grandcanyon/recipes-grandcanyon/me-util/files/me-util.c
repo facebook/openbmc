4ea17cc0f30e1cd4f042168a1586cd1950407f67
Dan Zhang <zhdaniel@fb.com>
2021-06-24 22:35:34 -0700

    fbgc: support power capping string command (#1658)
    Summary:
    Allow using me-util to set the cap power consumption by string command.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/1658
    
    Test Plan:
    1. Build and test pass on Grand Canyon.
    
    2. Check there are no unexpected error logs in BMC /var/log/messages and BMC SELs
    
    3. Check usage.
    root@bmc-oob:~# me-util
    age: me-util server <[0..n]data_bytes_to_send>
    Usage: me-util server <cmd>
      cmd list:
        --get_dev_id                         Get device ID
        --file <file>                        Input commands from file
        --get_nm_power_statistics            Get node manager power statistics
        --set_nm_power_policy <limit power>  Set node manager power policy
    
    4. Check can get power by string command.
    (1) Run stress
    [host console]
    [root@BartonSprings home]# ./ptugen -p 100 -b 1
    cmdline specified: ./ptugen -p 100 -b 1
    Power Thermal Utility (ptugen) v1.9 - Skylake/Cascade Lake Server
    Release Date: 5/10/2018
    ...
    [06/22/18 19:13:33 CST] ptugen started.
    CPU_0: CFreq=1919MHz, UFreq=2000MHz, Usage=0.0%, Power=16.76W, Temp=30C, Volt=0.701V
    CPU_0: Running... Test=TDP, CoreMask=0xFFFFFFFFFFFFFFFF, ThreadMask=0xFFFFFFFF, PwrLevel=TDP, Turbo=1
    CPU_0: CFreq=1640MHz, UFreq=2000MHz, Usage=95.1%, Power=83.44W, Temp=38C, Volt=0.697V
    
    Press <Ctrl-C> to stop.
    
    (2) Get current power
    [BMC console]
    - string command
    root@bmc-oob:~# me-util server --get_nm_power_statistics
    Manufacturer ID:                  57 01 00
    Statistics Power data:
            Current Value:            105 Watts
            Minimum Value:              3 Watts
            Maximum Value:            106 Watts
            Average Value:             35 Watts
    Timestamp:                        1969-12-31 15:59:59
    Statistics Reporting Period:      47 06 00 00
    Domain ID | Policy State:         50
    
    - raw command
    root@bmc-oob:~# me-util server 0xB8 0xC8 0x57 0x01 0x00 0x01 0x00 0x00
    57 01 00 69 00 03 00 6A 00 23 00 FF FF FF FF 47 06 00 00 50
    
    - current power
    root@bmc-oob:~# sensor-util server --force | grep BS_HSC_IN_PWR
    BS_HSC_IN_PWR                (0x9) :  106.00 Watts | (ok)
    
    5. Check can set limit power by string command.
    (1) Set limit power 80W
    [BMC console]
    root@bmc-oob:~# me-util server --set_nm_power_policy 80
    root@bmc-oob:~#
    
    (2) Run stress
    [host console]
    [root@BartonSprings home]# ./ptugen -p 100 -b 1cmdline specified: ./ptugen -p 100 -b 1
    Power Thermal Utility (ptugen) v1.9 - Skylake/Cascade Lake Server
    Release Date: 5/10/2018
    ...
    [06/22/18 19:37:28 CST] ptugen started.
    CPU_0: CFreq=2253MHz, UFreq=1600MHz, Usage=0.0%, Power=15.82W, Temp=33C, Volt=0.699V
    CPU_0: Running... Test=TDP, CoreMask=0xFFFFFFFFFFFFFFFF, ThreadMask=0xFFFFFFFF, PwrLevel=TDP, Turbo=1
    CPU_0: CFreq=1066MHz, UFreq=800MHz, Usage=96.0%, Power=57.47W, Temp=38C, Volt=0.687V
    
    Press <Ctrl-C> to stop.
    
    (3) Get current power
    [BMC console]
    - string command
    root@bmc-oob:~# me-util server --get_nm_power_statistics
    Manufacturer ID:                  57 01 00
    Statistics Power data:
            Current Value:             80 Watts
            Minimum Value:              3 Watts
            Maximum Value:            107 Watts
            Average Value:             38 Watts
    Timestamp:                        1969-12-31 15:59:59
    Statistics Reporting Period:      EF 06 00 00
    Domain ID | Policy State:         50
    
    -raw command
    root@bmc-oob:~# me-util server 0xB8 0xC8 0x57 0x01 0x00 0x01 0x00 0x00
    57 01 00 50 00 03 00 6B 00 26 00 FF FF FF FF EF 06 00 00 50
    
    - current power
    root@bmc-oob:~# sensor-util server --force | grep BS_HSC_IN_PWR
    BS_HSC_IN_PWR                (0x9) :   80.00 Watts | (ok)
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 1f90b4abb1

--- meta-facebook/meta-grandcanyon/recipes-grandcanyon/me-util/files/me-util.c
+++ meta-facebook/meta-grandcanyon/recipes-grandcanyon/me-util/files/me-util.c
@@ -38,13 +38,72 @@
 #define MAX_ME_READ_BUFFER         1024
 #define COUNT_ARGC_UTIL_FRU        2
 
+const uint32_t NM_INTEL_MANUFANCTURER_ID = 0x000157;
+const uint32_t NM_POLICY_CORRECT_TIME_LIMIT = 0x000003e8;
+const uint16_t NM_POLICY_TRIGGER_LIMIT = 0x0000;
+const uint16_t NM_POLICY_STATISTICS_PERIOD = 0x0001;
+#define NM_STATISTICS_MODE_GLOBAL_POWER       0x01
+#define NM_STATISTICS_DOMAIN_ID               0x00
+#define NM_STATISTICS_POLICY_ID               0x00
+#define NM_POLICY_DOMAIN_ID                   0x00
+#define NM_POLICY_POLICY_ID                   0x02
+#define NM_POLICY_POLICY_TYPE                 0xb0
+#define NM_POLICY_POLICY_EXCEPTION_ACTION     0x00
+#define NM_POLICY_ENABLE_FLAG                 0x05
+
+enum {
+  ME_ENABLE_DISABLE_NODE_MANAGER_POLICY = 0xC0,
+  ME_SET_NODE_MANAGER_POLICY = 0xC1,
+  ME_GET_NODE_MANAGER_STATISTICS= 0xC8,
+};
+
+typedef struct {
+  ipmi_req_t_common_header header;
+  uint8_t intel_manufacturer_id[3];
+  uint8_t mode;
+  uint8_t domain_id;
+  uint8_t plicy_id;
+} me_get_nm_statistics_req;
+
+typedef struct {
+  uint8_t cc;
+  uint8_t intel_manufacturer_id[3];
+  uint8_t statistics_data[8];
+  uint8_t timestamp[4];
+  uint8_t statistics_period[4];
+  uint8_t domain_id;
+} me_get_nm_statistics_res;
+
+typedef struct {
+  ipmi_req_t_common_header header;
+  uint8_t intel_manufacturer_id[3];
+  uint8_t domain_id;
+  uint8_t policy_id;
+  uint8_t policy_type;
+  uint8_t policy_exception_actions;
+  uint8_t policy_target_limit[2];
+  uint8_t correction_time_limit[4];
+  uint8_t policy_trigger_limit[2];
+  uint8_t statistics_period[2];
+} me_set_nm_policy_req;
+
+typedef struct {
+  ipmi_req_t_common_header header;
+  uint8_t intel_manufacturer_id[3];
+  uint8_t policy_enable_disable_flag;
+  uint8_t domain_id;
+  uint8_t policy_id;
+} me_enable_disable_nm_policy_req;
+
 static void
 print_usage_help(void) {
   printf("Usage: me-util server <[0..n]data_bytes_to_send>\n");
   printf("Usage: me-util server <cmd>\n");
   printf("  cmd list:\n");
-  printf("    --get_dev_id      Get device ID\n");
-  printf("    --file <file>     Input commands from file\n");
+  printf("    --get_dev_id                         Get device ID\n");
+  printf("    --file <file>                        Input commands from file\n");
+  printf("    --get_nm_power_statistics            Get node manager power statistics\n");
+  printf("    --set_nm_power_policy <limit power>  Set node manager power policy\n");
 }
 
 static int
@@ -84,6 +143,142 @@ me_get_dev_id() {
   printf("Product ID:                %02x %02x\n", res->ipmi_dev_id.prod_id[0], res->ipmi_dev_id.prod_id[1]);
   printf("Aux Firmware Revision:     %02x %02x %02x %02x\n", res->ipmi_dev_id.aux_fw_rev[0], res->ipmi_dev_id.aux_fw_rev[1],
                                                              res->ipmi_dev_id.aux_fw_rev[2], res->ipmi_dev_id.aux_fw_rev[3]);
+  return ret;
+}
+
+static int
+me_get_nm_power_statistic() {
+  int ret = 0;
+  me_get_nm_statistics_req req;
+  me_get_nm_statistics_res res;
+  uint8_t rbuf[MAX_IPMB_RES_LEN] = {0};
+  uint8_t tlen = 0, rlen = 0;
+  uint32_t timestamp = 0;
+  char time[MAX_VALUE_LEN] = {0};
+  struct tm ts = {0};
+
+  memset(&req, 0, sizeof(me_get_nm_statistics_req));
+  req.header.netfn_lun = IPMI_NETFN_SHIFT(NETFN_NM_REQ);
+  req.header.cmd = ME_GET_NODE_MANAGER_STATISTICS;
+  memcpy(&req.intel_manufacturer_id, (uint8_t *) &NM_INTEL_MANUFANCTURER_ID, sizeof(req.intel_manufacturer_id));
+  req.mode = NM_STATISTICS_MODE_GLOBAL_POWER;
+  req.domain_id = NM_STATISTICS_DOMAIN_ID;
+  req.plicy_id = NM_STATISTICS_POLICY_ID;
+  tlen = sizeof(me_get_nm_statistics_req);
+
+  ret = bic_me_xmit((uint8_t *)(&req), tlen, (uint8_t *)rbuf, &rlen);
+
+  if (ret < 0) {
+    printf("%s(): ME transmission failed\n", __func__);
+    return ret;
+  }
+
+  if (rlen == sizeof(me_get_nm_statistics_res)) {
+    memset(&res, 0, rlen);
+    memcpy(&res, rbuf, rlen);
+  } else {
+    printf("Invalid response length of get node manager statistic: %d, expected: %zu\n", rlen, sizeof(me_get_nm_statistics_res));
+    return -1;
+  }
+
+  if (res.cc != CC_SUCCESS) {
+    printf("Fail to get node manager statistic: Completion Code: %02X\n", res.cc);
+    return -1;
+  }
+
+  timestamp |= res.timestamp[0];
+  timestamp |= res.timestamp[1] << 8;
+  timestamp |= res.timestamp[2] << 16;
+  timestamp |= res.timestamp[3] << 24;
+  snprintf(time, sizeof(time), "%u", timestamp);
+  memset(&ts, 0, sizeof(struct tm));
+  strptime(time, "%s", &ts);
+  memset(&time, 0, sizeof(time));
+  strftime(time, sizeof(time), "%Y-%m-%d %H:%M:%S", &ts);
+
+  printf("Manufacturer ID:                  %02X %02X %02X\n", res.intel_manufacturer_id[0], res.intel_manufacturer_id[1],
+                                                               res.intel_manufacturer_id[2]);
+  printf("Statistics Power data:\n");
+  printf("        Current Value:            %3d Watts\n", (res.statistics_data[1] << 8) + res.statistics_data[0]);
+  printf("        Minimum Value:            %3d Watts\n", (res.statistics_data[3] << 8) + res.statistics_data[2]);
+  printf("        Maximum Value:            %3d Watts\n", (res.statistics_data[5] << 8) + res.statistics_data[4]);
+  printf("        Average Value:            %3d Watts\n", (res.statistics_data[7] << 8) + res.statistics_data[6]);
+  printf("Timestamp:                        %s\n", time);
+  printf("Statistics Reporting Period:      %02X %02X %02X %02X\n", res.statistics_period[0], res.statistics_period[1],
+                                                                    res.statistics_period[2], res.statistics_period[3]);
+  printf("Domain ID | Policy State:         %02X\n", res.domain_id);
+
+  return ret;
+}
+
+static int
+me_set_nm_power_policy(char *limit_power) {
+  me_set_nm_policy_req set_req;
+  me_enable_disable_nm_policy_req enable_req;
+  uint16_t power = 0;
+  uint8_t rbuf[MAX_IPMB_RES_LEN] = {0};
+  uint8_t tlen = 0, rlen = 0;
+  int ret = 0;
+
+  if (limit_power == NULL) {
+    printf("Fail to set node manager policy because parameter *limit_power is NULL\n");
+    return -1;
+  }
+
+  if ((atoi(limit_power) >= 0) && (atoi(limit_power) < 0xFFFF)) {
+    power = atoi(limit_power);
+  } else {
+    printf("Fail to set node manager policy because limit: %s out of range 0~0xFFFF\n", limit_power);
+    return -1;
+  }
+
+  // set node manager policy config
+  memset(&set_req, 0, sizeof(me_set_nm_policy_req));
+  set_req.header.netfn_lun = IPMI_NETFN_SHIFT(NETFN_NM_REQ);
+  set_req.header.cmd = ME_SET_NODE_MANAGER_POLICY;
+  memcpy(&set_req.intel_manufacturer_id, (uint8_t *) &NM_INTEL_MANUFANCTURER_ID, sizeof(set_req.intel_manufacturer_id));
+  set_req.domain_id = NM_POLICY_DOMAIN_ID;
+  set_req.policy_id = NM_POLICY_POLICY_ID;
+  set_req.policy_type = NM_POLICY_POLICY_TYPE;
+  set_req.policy_exception_actions = NM_POLICY_POLICY_EXCEPTION_ACTION;
+  memcpy(&set_req.policy_target_limit, &power, sizeof(set_req.policy_target_limit));
+  memcpy(&set_req.correction_time_limit, (uint8_t *) &NM_POLICY_CORRECT_TIME_LIMIT, sizeof(set_req.correction_time_limit));
+  memcpy(&set_req.policy_trigger_limit, (uint8_t *) &NM_POLICY_TRIGGER_LIMIT, sizeof(set_req.policy_trigger_limit));
+  memcpy(&set_req.statistics_period, (uint8_t *) &NM_POLICY_STATISTICS_PERIOD, sizeof(set_req.statistics_period));
+  tlen = sizeof(me_set_nm_policy_req);
+
+  ret = bic_me_xmit((uint8_t *)(&set_req), tlen, (uint8_t *)rbuf, &rlen);
+  if (ret < 0) {
+    printf("%s(): ME transmission failed cmd:%d\n", __func__, ME_SET_NODE_MANAGER_POLICY);
+    return ret;
+  }
+
+  if (rbuf[0] != CC_SUCCESS) {
+    printf("Fail to set node manager policy config: Completion Code: %02X\n", rbuf[0]);
+    return -1;
+  }
+
+  // enable node manager policy
+  memset(&enable_req, 0, sizeof(me_enable_disable_nm_policy_req));
+  enable_req.header.netfn_lun = IPMI_NETFN_SHIFT(NETFN_NM_REQ);
+  enable_req.header.cmd = ME_ENABLE_DISABLE_NODE_MANAGER_POLICY;
+  memcpy(&enable_req.intel_manufacturer_id, (uint8_t *) &NM_INTEL_MANUFANCTURER_ID, sizeof(enable_req.intel_manufacturer_id));
+  enable_req.policy_enable_disable_flag = NM_POLICY_ENABLE_FLAG;
+  enable_req.domain_id = NM_POLICY_DOMAIN_ID;
+  enable_req.policy_id = NM_POLICY_POLICY_ID;
+  tlen = sizeof(me_enable_disable_nm_policy_req);
+
+  ret = bic_me_xmit((uint8_t *)(&enable_req), tlen, (uint8_t *)rbuf, &rlen);
+  if (ret < 0) {
+    printf("%s(): ME transmission failed cmd:%d\n", __func__, ME_SET_NODE_MANAGER_POLICY);
+    return ret;
+  }
+
+  if (rbuf[0] != CC_SUCCESS) {
+    printf("Fail to enable node manager policy config: Completion Code: %02X\n", rbuf[0]);
+    return -1;
+  }
+
   return ret;
 }
 
@@ -193,9 +388,11 @@ do_cmds(char *cmd) {
 
   if ((strcmp(cmd, "--get_dev_id") == 0)) {
     return me_get_dev_id();
+  } else if (strcmp(cmd, "--get_nm_power_statistics") == 0) {
+    return me_get_nm_power_statistic();
   } else {
     print_usage_help();
-  } 
+  }
 
   return ret;
 }
@@ -221,12 +418,17 @@ main(int argc, char **argv) {
     printf("%s is not present!\n", argv[1]);
     return -1;
   }
-  
+
   if ((strcmp(argv[2], "--file") == 0)) {
     if (argc < 4) {
       goto err_exit;
     }
     return process_file(argv[3]);
+  } else if (strcmp(argv[2], "--set_nm_power_policy") == 0) {
+    if (argc < 4) {
+      goto err_exit;
+    }
+    return me_set_nm_power_policy(argv[3]);
   } else if ((start_with(argv[2], "--") == true)) {
     return do_cmds(argv[2]);
   } else {

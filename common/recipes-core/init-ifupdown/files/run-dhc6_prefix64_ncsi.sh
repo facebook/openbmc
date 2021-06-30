e84d42f5f1d3c15fb53f7cefbdb2750cb98c40ad
Amithash Prasad <amithash@fb.com>
2021-06-30 09:40:08 -0700

    dhc6: fix dhc6 may start slower on some systems (#2563)
    Summary:
    1. revise the "mac_to_ll" to let the format matched with "ip/ifconfig" output.
    For example, MAC address: 0C:42:A1:4C:F5:B5
    i. link-local address output by ip/ifconfig: fe80::e42:a1ff:fe4c:f5b5
    ii. before the fix, output by "mac_to_ll": fe80::0e42:a1ff:fe4c:f5b5
    
    So that the comparison is always false, and which causes dhc6 start slower about 10 secs.
    
    2. eliminate shellcheck warning
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2563
    
    Test Plan:
    Build and test passed.
    (1) check if link-local address is expected.
    sv stop dhc6; sv start dhc6; and check if it started quickly.
    
    Reviewed By: benwei13
    
    fbshipit-source-id: eb0d1bbba9

--- common/recipes-core/init-ifupdown/files/run-dhc6_prefix64_ncsi.sh
+++ common/recipes-core/init-ifupdown/files/run-dhc6_prefix64_ncsi.sh
@@ -20,12 +20,17 @@
 logger "run-dhc6.sh: dhclient -6 Started.."
 pid="/var/run/dhclient6.eth0.pid"
 
+# shellcheck disable=SC2086
 mac_to_ll() {
   IFS=':'; set $1; unset IFS
-  echo "fe80::$(printf %02x $((0x$1 ^ 2)))$2:${3}ff:fe$4:$5$6"
+  echo "fe80::$(printf %x $((0x$1 ^ 2)))$2:${3}ff:fe$4:$5$6" | \
+    sed -E 's/:0+/:/g; s/:{3,}/::/; s/:$/:0/'
 }
 
-for i in {1..30}; do
+exp_ll=
+ll=
+
+for _ in {1..30}; do
   # wait getting address from NC-SI
   if [ "$(cat /sys/class/net/eth0/addr_assign_type)" = "3" ]; then
     exp_ll=$(mac_to_ll "$(cat /sys/class/net/eth0/address)")
@@ -37,17 +42,19 @@ for i in {1..30}; do
     ip -6 addr flush dev eth0 scope link
     echo 1 > /proc/sys/net/ipv6/conf/eth0/addr_gen_mode
     echo 0 > /proc/sys/net/ipv6/conf/eth0/addr_gen_mode
-
-    for j in {1..100}; do
-      usleep 10000
-      ll=$(ip -6 addr show dev eth0 scope link | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
-      if [ "$ll" = "$exp_ll" ]; then
-        break;
-      fi
-    done
     break;
   fi
   sleep 1
 done
+
+if [ "$ll" != "$exp_ll" ]; then
+  for _ in {1..100}; do
+    usleep 10000
+    ll=$(ip -6 addr show dev eth0 scope link | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
+    if [ "$ll" = "$exp_ll" ]; then
+      break;
+    fi
+  done
+fi
 
 exec dhclient -6 -d -D LL --address-prefix-len 64 -pf ${pid} eth0 "$@"

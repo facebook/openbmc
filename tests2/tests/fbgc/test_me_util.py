95b1b832d800004091e7cec9f44f46e3da8341bc
Dan Zhang <zhdaniel@fb.com>
2021-07-14 15:48:19 -0700

    test2: fbgc: support me-util CIT test (#1691)
    Summary:
    Support CIT tests for me-util get device id and get node manager power statistics.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/1691
    
    Test Plan:
    1. Built and test pass on Grand Canyon.
    
    2. Check there are no unexpected error logs on BMC /var/log/messages and SELs.
    
    3. Check me-util.
    (1) Get dev id
    root@bmc-oob:/usr/local/bin/tests2# me-util server --get_dev_id
    Device ID:                 50
    Device Revision:           01
    Firmware Revision:         04 44
    IPMI Version:              02
    Additional Device Support: 21
    Manufacturer ID:           57 01 00
    Product ID:                0f 0b
    Aux Firmware Revision:     04 03 30 01
    
    (2) Get node manager power statistics
    root@bmc-oob:/usr/local/bin/tests2# me-util server --get_nm_power_statistics
    Manufacturer ID:                  57 01 00
    Statistics Power data:
            Current Value:            108 Watts
            Minimum Value:             10 Watts
            Maximum Value:            411 Watts
            Average Value:            110 Watts
    Timestamp:                        2021-07-07 18:13:18
    Statistics Reporting Period:      44 FD 00 00
    Domain ID | Policy State:         50
    
    4. Check CIT test pass.
    (1) Copy tests2 folder to BMC
    root@guaigaui-Freedom:~/Sara/project/CIT_meutil# scp -r tests2 192.168.88.190:/usr/local/bin
    
    (2) Execute me-util test
    root@bmc-oob:~# cd /usr/local/bin/tests2
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py --run-test tests.fbgc.test_me_util.METest
    test_me_dev_id (tests.fbgc.test_me_util.METest)
    Method to verify ME device data. ... ok
    test_me_nm_power_statistic (tests.fbgc.test_me_util.METest)
    Method to verify ME power data. ... ok
    
    ----------------------------------------------------------------------
    Ran 2 tests in 0.153s
    
    OK
    
    Reviewed By: benwei13
    
    fbshipit-source-id: 9bd87b813d

---
+++ tests2/tests/fbgc/test_me_util.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_me_util_test import CommonMETest
+
+
+class METest(CommonMETest, unittest.TestCase):
+    def set_me_cmd(self):
+        self.me_cmd = "/usr/bin/me-util server"

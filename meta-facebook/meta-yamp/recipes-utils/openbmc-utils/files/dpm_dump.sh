2e33550875a30854af6d255ac72446f31dfabe26
Joan Olguy Canéus <joancaneus@fb.com>
2021-07-18 23:16:52 -0700

    YAMP: Rotate dpm_log on boot (#197)
    Summary:
    On boot, rotate the dpm_log file if it is more than 1000 lines.
    This will prevent unbounded log growth.
    
    Testing performed:
    
    1. Filled the dpm_log with over 1000 lines of data. Then power cycled
       and verified the old data was moved to dpm_log.1 and a new
       dpm_log file was started.
    
    2. After a power cycle event, verified the dpm_log reflected that:
    ```
    {'Date':'2021-07-14 15:57:38','Log':'0x0101020000000000000000020002','detail_0':{'m':'0x038df444480049860000','e':'0x12'},'detail_1':{'m':'0x038df4778c804986a227','e':'0x12'},'detail_2':{'m':'0x038df47888004986381a','e':'0x12'}}
    ```
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.arista/pull/197
    
    Reviewed By: binhuang00
    
    fbshipit-source-id: 1d690d9f0b

--- meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/dpm_dump.sh
+++ meta-facebook/meta-yamp/recipes-utils/openbmc-utils/files/dpm_dump.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+#
+# Copyright 2021-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
 
 dpm_path="/sys/bus/i2c/drivers/ucd9000/3-004e"
 logfile="/mnt/data1/log/dpm_log"
+logfile_old="${logfile}.1"
+max_loglines=1000
 
 trap cleanup INT TERM QUIT EXIT
 
@@ -11,6 +30,17 @@ if [ -d "$dpm_path" ]; then
     REBIND=true
     echo "# Unbinding SWITCHCARD DPM from ucd9000 driver"
     echo "3-004e" > /sys/bus/i2c/drivers/ucd9000/unbind
+fi
+
+# Check to see if the log file needs to be rotated
+touch $logfile $logfile_old
+lines="$(wc -l $logfile|cut -d' ' -f1)"
+if [ "$lines" -gt $max_loglines ]
+then
+    echo "Rotating DPM log file"
+    rm -f "$logfile_old"
+    cp "$logfile" "$logfile_old"
+    rm -f "$logfile"
 fi
 
 echo "Reading DPM logs..."

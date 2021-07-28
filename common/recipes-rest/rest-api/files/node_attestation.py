fb7c969c6e9b300fdea71b3b2d82173436699b91
Chenxi Zhang <chenxiz@fb.com>
2021-07-28 15:28:31 -0700

    Added device attestation RESTful API endpoint
    Summary:
    We have added the logic in previous diff D29617088, and then reverted it by D29922516. It is because i thought we need a totally separate file file device attestation.
    
    After thinking over it again, i would be better to keep attestation REST api in a single file and a single parent REST root /api/attestation.
    
    Test Plan:
    Step 1
    ```
    cd ~/local/openbmc
    source openbmc-init-build-env meta-facebook/meta-fby3 build-fby3 && bitbake rest-api
    ```
    
    Step 2.
    ```
    export bitbake_target=device-attestation
    export target_bmc=sled3103.r0003.p0103.f0001.05.prn6
    ```
    
    Step 4:
    ```
    cd "$BUILDDIR" && bitbake "$bitbake_target" && cd tmp/work/arm*-fb-linux-gnueabi/$bitbake_target/*/image && tar -cz $(find . -type f) | sush "$target_bmc" 'tar -xzvf - -C /' && scp "$BUILDDIR"/../../common/recipes-rest/rest-api/files/node_attestation.py  root@"$target_bmc":/usr/local/fbpackages/rest-api
    ```
    
    Step5:
    ```
    root@sled3103-oob:/usr/local/fbpackages/rest-api# sv stop restapi
    
    [chenxiz@devvm1588.prn# /data/users/chenxiz/openbmc/tools/rest_api/run_rest_on_oob sled3103.r0003.p0103.f0001.05.prn6
    ```
    
    Step 6:
    on Dev server:
    ```
    [chenxiz@devvm1588.prn0 ~] curl -k -X POST -d '{"action":"measurement", "device":"nic", "spdm_req":"EeAB/xJYBXFFAjF2OZhUVJlDWFJxUGkVkmWGgxlQUzclRggHAA=="}' https://sled3103.r0003.p0103.f0001.05.prn6:8443/api/attestation/device  --cert $THRIFT_TLS_CL_CERT_PATH --key $THRIFT_TLS_CL_KEY_PATH
    ```
    Output:
    {P428158201}
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: 21bfc9990b

--- common/recipes-rest/rest-api/files/node_attestation.py
+++ common/recipes-rest/rest-api/files/node_attestation.py
@@ -18,15 +18,25 @@
 # Boston, MA 02110-1301 USA
 #
 try:
-    # Add all attestation related imports here
+    # Add all bmc attestation related imports here
     import obmc_attestation.helpers
     import obmc_attestation.measure
     import obmc_attestation.tpm2
 
-    ATTESTATION_AVAILABLE = True
+    BMC_ATTESTATION_AVAILABLE = True
 except ImportError:
-    ATTESTATION_AVAILABLE = False
+    BMC_ATTESTATION_AVAILABLE = False
     # Doing this so that we don't break upstream
+
+try:
+    # Add all device attestation related imports here
+    import device_attestation.measure
+
+    DEVICE_ATTESTATION_AVAILABLE = True
+except ImportError:
+    DEVICE_ATTESTATION_AVAILABLE = False
+    # Doing this so that we don't break obmc attestation
+
 from aiohttp.web import Application
 from compute_rest_shim import RestShim
 from node import node
@@ -37,7 +47,7 @@ ACCEPTABLE_ALGORITHMS = ["sha1", "sha256"]
 def setup_attestation_endpoints(app: Application) -> None:
     attestation_shim = RestShim(node(), "/api/attestation")
     app.router.add_get(attestation_shim.path, attestation_shim.get_handler)
-    if not ATTESTATION_AVAILABLE:
+    if not BMC_ATTESTATION_AVAILABLE:
         # Return witout actually adding any endpoints/attaching
         # them to any logic
         return
@@ -50,6 +60,15 @@ def setup_attestation_endpoints(app: Application) -> None:
     tpm_shim = RestShim(get_tpm_nodes(tpm_actions), "/api/attestation/tpm")
     app.router.add_get(tpm_shim.path, tpm_shim.get_handler)
     app.router.add_post(tpm_shim.path, tpm_shim.post_handler)
+
+    if DEVICE_ATTESTATION_AVAILABLE:
+        device_attestation_actions = ["measurement"]
+        device_attestation_shim = RestShim(
+            NodeDeviceAttestation(device_attestation_actions), "/api/attestation/device"
+        )
+        app.router.add_post(
+            device_attestation_shim.path, device_attestation_shim.post_handler
+        )
 
 
 class NodeSystemInfo(node):
@@ -113,3 +132,23 @@ class NodeTPM(node):
 
 def get_tpm_nodes(actions):
     return NodeTPM(actions)
+
+
+class NodeDeviceAttestation(node):
+    def __init__(self, actions=None):
+        self.actions = actions
+
+    async def doAction(self, data, param):
+        # We will delete the action key from the data
+        # and directly pass it onto the underlying function
+        # as kwargs hoping that the function callee
+        # obeys arguments structure
+        action = data["action"]
+        del data["action"]
+        if action == "measurement":
+            return device_attestation.measure.return_measure(**data)
+        else:
+            return {
+                "status": "1",
+                "message": "not supported",
+            }

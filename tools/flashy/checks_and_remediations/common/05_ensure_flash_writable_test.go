1f8b61e9e32ddb833ed6fb393c233573cb69e2d9
Łukasz Siudut <lsiudut@fb.com>
2021-09-06 05:46:16 -0700

    Rename Broken System -> Bad Flash Chip
    Summary: This is more explicit way of naming the failures we detect.
    
    Test Plan: let's wait for the testing pipeline
    
    Reviewed By: lhl2617, doranand
    
    fbshipit-source-id: 1d52f89cba

--- tools/flashy/checks_and_remediations/common/05_ensure_flash_writable_test.go
+++ tools/flashy/checks_and_remediations/common/05_ensure_flash_writable_test.go
@@ -97,7 +97,7 @@ func TestEnsureFlashWritable(t *testing.T) {
 			failSet:           false,
 			failCheck:         true,
 			failClear:         false,
-			want:              step.ExitBrokenSystem{
+			want:              step.ExitBadFlashChip{
                                 Err: errors.Errorf("U-Boot environment is read only: flash chips swapped? Error code: <nil>, stderr: ")},
 		},
 		{

693feb2045e722494d8d6a4b98ba378d1fc752e1
Tao Ren <taoren@fb.com>
2021-09-13 23:31:07 -0700

    tests2: wedge100: fix PEM i2c path in test_psumuxmon.py
    Summary:
    Update PEM bus number from 7 to 14 or 15, because PEM is connected
    behind i2c mux 7-0070.
    
    Besides, duplicated get_power_type() function is deleted: the one in
    power_supply.py is used.
    
    Test Plan:
    Output on Wedge100 with PSU:
    
    root@bmc-oob:/tmp# python /usr/local/bin/tests2/cit_runner.py --run-test tests.wedge100.test_psumuxmon
    test_psumuxmon_ltc4151_sensor_path_exists (tests.wedge100.test_psumuxmon.PsumuxmonTest) ... skipped 'ltc4151 eeprom i2c addr not detected. skipping this test...'
    test_psumuxmon_ltc4281_sensor_sysfs_nodes_exists (tests.wedge100.test_psumuxmon.PsumuxmonTest) ... skipped 'ltc4281 eeprom i2c addr not detected. skipping this test...'
    test_psumuxmon_runit_sv_status (tests.wedge100.test_psumuxmon.PsumuxmonTest) ... skipped 'pem not detected. skipping this test...'
    
    ----------------------------------------------------------------------
    Ran 3 tests in 0.205s
    
    OK (skipped=3)
    
    Reviewed By: binhuang00
    
    fbshipit-source-id: 987725c7ae

--- tests2/tests/wedge100/test_psumuxmon.py
+++ tests2/tests/wedge100/test_psumuxmon.py
@@ -21,13 +21,11 @@ import os
 import re
 import unittest
 
+from tests.wedge100.power_supply import get_power_type
 from tests.wedge100.test_data.sysfs_nodes.sysfs_nodes import LTC4281_SYSFS_NODES
 from utils.cit_logger import Logger
 from utils.shell_util import run_shell_cmd
 from utils.test_utils import running_systemd
-
-
-POWER_TYPE_CACHE = "/var/cache/detect_power_module_type.txt"
 
 
 class PsumuxmonTest(unittest.TestCase):
@@ -37,52 +35,6 @@ class PsumuxmonTest(unittest.TestCase):
     def tearDown(self):
         Logger.info("Finished logging for {}".format(self._testMethodName))
         pass
-
-    def get_kmods_list(self, dev, presence_value):
-        """
-        check to see which power device is being used and
-        if it is being detected. Then we return the list
-        of the power device kernel driver modules that must
-        present in the image.
-        pem1 = 1 --> means pem with ltc4151 inserted
-        pem2 = 1 --> means pem with ltc4281 inserted
-        psu1 and psu2 = 1 -> means system with PSU inserted
-        """
-        power_module_type = None
-        if dev == "pem1" and presence_value == 1:
-            power_module_type = "pem1"
-        elif dev == "pem2" and presence_value == 1:
-            power_module_type = "pem2"
-        elif dev == "psu1" or dev == "psu2":
-            if presence_value == 1:
-                power_module_type = "psu"
-        else:
-            raise Exception("file contains unknown module")
-
-        return power_module_type
-
-    def get_power_module(self):
-        """
-        Read appropriate path that contains the presence
-        status of various power module option.
-        """
-        power_module_type = None
-        if not os.path.exists(POWER_TYPE_CACHE):
-            raise Exception("Path for power type doesn't exist")
-        with open(POWER_TYPE_CACHE, "r") as fp:
-            lines = fp.readlines()
-            if lines:
-                for line in lines:
-                    dev = line.split(": ")[0]
-                    presence_status = line.split(": ")[1]
-                    if int(presence_status) == 1:
-                        power_module_type = self.get_kmods_list(
-                            dev, int(presence_status)
-                        )
-                        break
-            else:
-                raise Exception("Power module file is empty")
-        return power_module_type
 
     def get_ltc_hwmon_path(self, path):
         result = re.split("hwmon", path)
@@ -135,20 +87,24 @@ class PsumuxmonTest(unittest.TestCase):
         return [True, None, None, None]
 
     def is_ltc4151_i2c_dev_addr_present(self):
-        return self.get_power_module() == "pem1"
+        return get_power_type() == "pem1"
 
     def is_ltc4281_i2c_dev_addr_present(self):
-        return self.get_power_module() == "pem2"
+        return get_power_type() == "pem2"
 
     def test_psumuxmon_runit_sv_status(self):
-        if running_systemd():
-            cmd = ["/bin/systemctl status psumuxmon"]
-            data = run_shell_cmd(cmd, ignore_err=True)
-            self.assertIn("running", data, "psumuxmon unit not running")
+        power_type = get_power_type()
+        if power_type.startswith("pem"):
+            if running_systemd():
+                cmd = ["/bin/systemctl status psumuxmon"]
+                data = run_shell_cmd(cmd, ignore_err=True)
+                self.assertIn("running", data, "psumuxmon unit not running")
+            else:
+                cmd = ["/usr/bin/sv status psumuxmon"]
+                data = run_shell_cmd(cmd)
+                self.assertIn("run", data, "psumuxmon process not running")
         else:
-            cmd = ["/usr/bin/sv status psumuxmon"]
-            data = run_shell_cmd(cmd)
-            self.assertIn("run", data, "psumuxmon process not running")
+            self.skipTest("pem not detected. skipping this test...")
 
     def test_psumuxmon_ltc4151_sensor_path_exists(self):
         # Based on lab device deployment, sensor data might not be accessible.
@@ -157,9 +113,12 @@ class PsumuxmonTest(unittest.TestCase):
             self.is_ltc4151_i2c_dev_addr_present()
             or self.is_ltc4281_i2c_dev_addr_present()
         ):
-            cmd = "/sys/bus/i2c/devices/7-006f/hwmon/hwmon*/in1_input"
+            if os.path.exists("/sys/bus/i2c/devices/14-006f/"):
+                path = "/sys/bus/i2c/devices/14-006f/hwmon/hwmon*/in1_input"
+            else:
+                path = "/sys/bus/i2c/devices/15-006f/hwmon/hwmon*/in1_input"
             self.assertTrue(
-                os.path.exists(self.get_ltc_hwmon_path(cmd)),
+                os.path.exists(self.get_ltc_hwmon_path(path)),
                 "psumuxmon LTC sensor path accessible",
             )
         else:
@@ -169,8 +128,11 @@ class PsumuxmonTest(unittest.TestCase):
         # check to see if the sysfs path for ltc4281 has
         # been created
         if self.is_ltc4281_i2c_dev_addr_present():
-            cmd = "/sys/bus/i2c/devices/7-004a"
-            sysfs_nodes_results = self.check_ltc4281_sysfs_nodes(cmd)
+            if os.path.exists("/sys/bus/i2c/devices/14-004a"):
+                path = "/sys/bus/i2c/devices/14-004a"
+            else:
+                path = "/sys/bus/i2c/devices/15-004a"
+            sysfs_nodes_results = self.check_ltc4281_sysfs_nodes(path)
             self.assertTrue(
                 sysfs_nodes_results[0],
                 "{} path failed {} with value {}".format(

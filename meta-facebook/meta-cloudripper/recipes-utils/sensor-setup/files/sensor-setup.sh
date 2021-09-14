19a70b585aab91cd6ced73097cf045ae26a0354d
Tao Ren <taoren@fb.com>
2021-09-13 21:06:58 -0700

    cloudripper: sensor-setup: fixup pxe1211 and xdpe132g5c (#1194)
    Summary:
    add pxe1211 and xdpe132g5c fixup.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/1194
    
    Test Plan:
    ```
    root@bmc:~# cat /etc/issue
    OpenBMC Release cloudripper-b368b198bce-dirty
    
    * sensor-setup.service - Setup hwmon sensors
         Loaded: loaded (/lib/systemd/system/sensor-setup.service; enabled; vendor preset: enabled)
         Active: active (exited) since Wed 1969-12-31 16:01:05 PST; 10min ago
        Process: 419 ExecStart=/usr/local/bin/sensor-setup.sh (code=exited, status=0/SUCCESS)
       Main PID: 419 (code=exited, status=0/SUCCESS)
    
    Dec 31 16:00:56 bmc systemd[1]: Starting Setup hwmon sensors...
    Dec 31 16:00:56 bmc sensor-setup.sh[419]: Check special sensors and rebind driver...checking i2c driver binding status
    Dec 31 16:00:56 bmc sensor-setup.sh[419]: total 50 i2c devices checked: all bound to drivers
    Dec 31 16:00:57 bmc sensor-setup.sh[440]: Setup xdpe12284 on EVT3 unit ...done.
    Dec 31 16:01:03 bmc sensor-setup.sh[419]: Check ir35215 done
    Dec 31 16:01:04 bmc sensor-setup.sh[419]: Check pxe1211 done
    Dec 31 16:01:05 bmc sensor-setup.sh[419]: Check xdpe132g5c done
    Dec 31 16:01:05 bmc systemd[1]: Started Setup hwmon sensors.
    
    ```
    
    Reviewed By: GoldenBug
    
    fbshipit-source-id: bff2d8268d

--- meta-facebook/meta-cloudripper/recipes-utils/sensor-setup/files/sensor-setup.sh
+++ meta-facebook/meta-cloudripper/recipes-utils/sensor-setup/files/sensor-setup.sh
@@ -29,19 +29,70 @@ retry=0
 # ir35215 chip enabled by COMe power on, 
 # so need sleep some time to wait COMe Power on
 sleep 5
-
-fixup_i2c_devices() {
+#
+# We have seen intermittent ir35215 voltage reading value unreasonable and root cause is
+# not clear as of now. Testing shows below retry will help to mitigate
+# such error.
+#
+fixup_i2c_ir35215() {
     while [ $retry -lt 5 ]; do
-        sensor_ok=$(/usr/local/bin/sensor-util smb 0x12 --force | grep ok &> /dev/null; echo $?)
-        if [ $((sensor_ok)) -eq 0 ]; then
-            echo "Done"
-            exit 0
+        sensor1_ok=$(/usr/local/bin/sensor-util smb 0x12 --force | grep ok &> /dev/null; echo $?)
+        sensor2_ok=$(/usr/local/bin/sensor-util smb 0x20 --force | grep ok &> /dev/null; echo $?)
+        if [ $((sensor1_ok)) -eq 0 ] && [ $((sensor2_ok)) -eq 0 ]; then
+            echo "Check ir35215 done"
+            return
         fi
         retry=$((retry + 1))
         echo "trying fixup driver ir35215 for i2c device 16-4d, time $retry"
         i2c_device_delete 16 0x4d
         i2c_device_add 16 0x4d ir35215
     done
+    logger -p user.err "Unable to fix ir35215 sensor reading after 5 retries"
+}
+#
+# We have seen intermittent pxe1211 voltage reading value unreasonable and root cause is
+# not clear as of now. Testing shows below retry will help to mitigate
+# such error.
+#
+retry=0
+fixup_i2c_pxe1211() {
+    while [ $retry -lt 5 ]; do
+        sensor1_ok=$(/usr/local/bin/sensor-util smb 0x6e --force | grep ok &> /dev/null; echo $?)
+        sensor2_ok=$(/usr/local/bin/sensor-util smb 0x75 --force | grep ok &> /dev/null; echo $?)
+        sensor3_ok=$(/usr/local/bin/sensor-util smb 0x7c --force | grep ok &> /dev/null; echo $?)
+        if [ $((sensor1_ok)) -eq 0 ] && [ $((sensor2_ok)) -eq 0 ]  && [ $((sensor3_ok)) -eq 0 ]; then
+            echo "Check pxe1211 done"
+            return
+        fi
+        retry=$((retry + 1))
+        echo "trying fixup driver pxe1211 for i2c device 20-0e, time $retry"
+        i2c_device_delete 20 0x0e
+        i2c_device_add 20 0x0e pxe1211
+    done
+    logger -p user.err "Unable to fix pxe1211 sensor reading after 5 retries"
+}
+#
+# We have seen intermittent xdpe132g5c voltage reading value unreasonable and root cause is
+# not clear as of now. Testing shows below retry will help to mitigate
+# such error.
+#
+retry=0
+fixup_i2c_xdpe132g5c() {
+    while [ $retry -lt 5 ]; do
+        sensor1_ok=$(/usr/local/bin/sensor-util smb 0x35 --force | grep ok &> /dev/null; echo $?)
+        sensor2_ok=$(/usr/local/bin/sensor-util smb 0x2e --force | grep ok &> /dev/null; echo $?)
+        if [ $((sensor1_ok)) -eq 0 ] && [ $((sensor2_ok)) -eq 0 ]; then
+            echo "Check xdpe132g5c done"
+            return
+        fi
+        retry=$((retry + 1))
+        echo "trying fixup driver xdpe132g5c for i2c device 17-40, time $retry"
+        i2c_device_delete 17 0x40
+        i2c_device_add 17 0x40 xdpe132g5c
+    done
+    logger -p user.err "Unable to fix xdpe132g5c sensor reading after 5 retries"
 }
 
-fixup_i2c_devices
+fixup_i2c_ir35215
+fixup_i2c_pxe1211
+fixup_i2c_xdpe132g5c

3cc02e7758397b4cf82b85e90e74a2bb2b6708fa
Peter Fetros <goldenbug@fb.com>
2021-09-20 15:43:53 -0700

    fby35:setup-gpio: correct typo of setting wdt1c (#2664)
    Summary:
    as title
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2664
    
    Test Plan:
    Build and test pass on fby35
    
    root@bmc-oob:~# devmem 0x1e6e2060 32
    0x070F1FF1
    root@bmc-oob:~# devmem 0x1e6e2070 32
    0x03FFDFF1
    root@bmc-oob:~# devmem 0x1e78501c 32
    0x030F1FF1
    root@bmc-oob:~# devmem 0x1e785020 32
    0x03FFFFF1
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: cfae4aefed

--- meta-facebook/meta-fby35/recipes-fby35/setup-gpio/files/setup-gpio.sh
+++ meta-facebook/meta-fby35/recipes-fby35/setup-gpio/files/setup-gpio.sh
@@ -8,7 +8,7 @@ devmem 0x1e6e2060 32 $((val | 0x1000000))
 val=$(devmem 0x1e6e2070 2>/dev/null)
 devmem 0x1e6e2070 32 $((val | 0x200))
 # WDT1C
-al=$(devmem 0x1e78501C 2>/dev/null)
+val=$(devmem 0x1e78501C 2>/dev/null)
 devmem 0x1e78501C 32 $((val | 0x1000000))
 # WDT20
 val=$(devmem 0x1e785020 2>/dev/null)

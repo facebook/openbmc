bc37ef863a9b1e6cd42cba1556c3fc4655790c44
Patrick Williams <patrickw3@fb.com>
2021-09-22 08:49:27 -0700

    zeus,rocko: add override syntax backports
    Summary:
    The latest Yocto branch has changed the 'override' syntax for variables.
    What use to be 'FOO_append' or 'FOO_remove' is now 'FOO:append'.  This
    makes all recipes incompatible between "honister" and earlier releases.
    
    To mitigate for this, the Yocto team has backported some support to
    automatically change ':' into '_' for old Yocto releases.  They
    backported this support back to dunfell and we currently have this in the
    sync we are utilizing.  Unfortunately, they did not backport to EOL
    branches that we still use: zeus and rocko.
    
    I was able to, with minimal merge conflicts, further backport these
    commits from dunfell to zeus and rocko.  Create patch files for public
    facing github Yocto sync script and point our internal submodules at the
    revision containing my manual backport of these patches.
    
    update-submodule: yocto/rocko/poky
    update-submodule: yocto/zeus/poky
    
    Test Plan:
    Build a bunch of rocko and zeus systems with both the current
    layers _and_ a commit that changes all our recipes to the new override
    syntax.
    
    Reviewed By: GoldenBug
    
    fbshipit-source-id: 1d3d0056af

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,11 +12,13 @@ rocko_repos=(
     meta-security:74860b2
     poky:5f660914cd # bitbake: bitbake-user-manual: Fixed section head typo
 )
+rocko_poky_patch="0001-rocko-backport-support-for-override.patch"
 zeus_repos=(
     meta-openembedded:2b5dd1eb8
     meta-security:52e83e6
     poky:d88d62c20d # selftest/signing: Ensure build path relocation is safe
 )
+zeus_poky_patch="0001-zeus-backport-support-for-override.patch"
 lf_master_repos=(
     lf-openbmc:e6866cac1
 )

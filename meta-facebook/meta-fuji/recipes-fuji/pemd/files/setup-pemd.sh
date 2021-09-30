4ef928bece8b25e82aecc6d85f34813ef01f9158
Tao Ren <taoren@fb.com>
2021-09-29 21:55:08 -0700

    fuji: pemd: add bbappend and platform files (#1236)
    Summary:
    Because use the meta-facebook common pemd code, specific settings for fuji 12V PEM.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/1236
    
    Test Plan:
    ```
    root@bmc:~# cat /etc/issue
    OpenBMC Release fuji-f96de021310-dirty
    
    root@bmc:~# ps w |grep pemd
     1072 root      2128 S    runsv /etc/sv/pemd
     1074 root     42072 S    /usr/local/bin/pemd pem1 pem2 pem3 pem4
     8674 root      2260 S    grep pemd
    root@bmc:~#
    ```
    
    Reviewed By: binhuang00
    
    fbshipit-source-id: 6b21837ccc

---
+++ meta-facebook/meta-fuji/recipes-fuji/pemd/files/setup-pemd.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# Copyright 2021-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+### BEGIN INIT INFO
+# Provides:          setup-sensord
+# Required-Start:
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Setup sensor monitoring
+### END INIT INFO
+
+# shellcheck disable=SC1091
+. /usr/local/bin/openbmc-utils.sh
+
+# If either one pem is present, pemd will start
+if [ "$(wedge_power_supply_type)" != "PEM" ]; then
+    echo "Skip pemd as PEM is not present"
+    exit 0
+fi
+
+# shellcheck disable=SC2039
+echo -n "Setup pem monitoring for Fuji... "
+
+runsv /etc/sv/pemd > /dev/null 2>&1 &
+
+echo "done."

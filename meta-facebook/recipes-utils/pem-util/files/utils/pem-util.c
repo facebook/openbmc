95d1b547975ea46d39bd48ba220c2458aae623be
Tao Ren <taoren@fb.com>
2021-09-29 21:53:53 -0700

    pem-util: move the pem-util to meta-facebook common (#1230)
    Summary:
    Because both wedge400 and fuji will use the 12V PEM, so move the pem-util to meta-facebook common.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/1230
    
    Test Plan:
    ```
    root@bmc:~# cat /etc/issue
    OpenBMC Release fuji-c6987ddfa32-dirty
    
    root@bmc:~# pem-util pem1
    Usage: pem-util <pem1|pem2|pem3|pem4> --update [--force] <file_path>
    Usage: pem-util <pem1|pem2|pem3|pem4> <command> <options>
           command:
             --get_pem_info
             --get_eeprom_info
               options:
                 --print
                 --clear
             --get_blackbox_info
               options:
                 --print
                 --clear
             --get_archive_log
               options:
                 --print
                 --clear
             --archive_pem_chips
    root@bmc:~# pem-util pem1 --get_pem_info
    
    PEM Information [ 5810.269086] at24 48-0050: 256 byte 24c02 EEPROM, writable, 1 bytes/write
                    [ 5810.368685] i2c i2c-48: new_device: Instantiated device 24c02 at 0x50
    : PEM1 (Bus:48 Addr:0x58)
    [ 5810.529756] i2c i2c-48: delete_device: Deleting device 24c02 at 0x50
    --------------- [ 5810.630275] at24 48-0050: 256 byte 24c02 EEPROM, writable, 1 bytes/write
                    [ 5810.722956] i2c i2c-48: new_device: Instantiated device 24c02 at 0x50
    : -----------------------
    Version              [ 5810.862923] i2c i2c-48: delete_device: Deleting device 24c02 at 0x50
               : 3
    Product Name                    : WEDGE400-PEM
    Product Part Number             : 00-000000
    Product Version                 : 2
    Product Sub-Version             : 0
    Product Serial Number           : S112420471162
    System Manufacturer             : CLSSAN
    System Manufacturing Date       : 12-12-20
    
    PEM Hot Swap status             : PEM1 (Bus:48 Addr:0x58)
    ---------------                 : -----------------------
    PEM1_IN_VOLT                    : 11.81 Volts
    PEM1_OUT_VOLT                   : 11.83 Volts
    PEM1_FET_BAD                    : 0.00 Volts
    PEM1_FET_SHORT                  : 0.00 Volts
    PEM1_CURR                       : 22.08 Amps
    PEM1_POWER                      : 261.00 Watts
    PEM1_FAN1_SPEED                 : 19200.00 RPM
    PEM1_FAN2_SPEED                 : 24000.00 RPM
    PEM1_HOT_SWAP_TEMP              : 29.59 C
    PEM1_AIR_INLET_TEMP             : 27.93 C
    PEM1_AIR_OUTLET_TEMP            : 29.92 C
    
    ON_STATUS                       : 1
    FET_BAD_COOLDOWN_STATUS         : 0
    FET_SHORT_PRESENT               : 0
    ON_PIN_STATUS                   : 1
    POWER_GOOD_STATUS               : 1
    OC_COOLDOWN_STATUS              : 0
    UV_STATUS                       : 0
    OV_STATUS                       : 0
    GPIO3_STATUS                    : 0
    GPIO2_STATUS                    : 1
    GPIO1_STATUS                    : 1
    ALERT_STATUS                    : 1
    EEPROM_BUSY                     : 0
    ADC_IDLE                        : 0
    TICKER_OVERFLOW_PRESENT         : 0
    METER_OVERFLOW_PRESENT          : 0
    
    EEPROM_Done                     : 0
    FET_Bad_Fault                   : 0
    FET_Short_Fault                 : 0
    On_Fault                        : 0
    Power_Bad_Fault                 : 0
    OC_Fault                        : 0
    UV_Fault                        : 0
    OV_Fault                        : 0
    
    Power_Alarm_High                : 0
    Power_Alarm_Low                 : 1
    Vsense_Alarm_High               : 0
    Vsense_Alarm_Low                : 1
    VSourve_Alarm_High              : 0
    VSourve_Alarm_Low               : 0
    VGPIO_Alarm_High                : 1
    VGPIO_Alarm_Low                 : 0
    root@bmc:~#
    
    ```
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: a3de498b7b

---
+++ meta-facebook/recipes-utils/pem-util/files/utils/pem-util.c
@@ -0,0 +1,162 @@
+/*
+ * pem-util
+ *
+ * Copyright 2019-present Facebook. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <facebook/pem.h>
+
+static const char *option_list[] = {
+  "  --get_pem_info",
+  "  --get_eeprom_info",
+  "    options:",
+  "      --print",
+  "      --clear",
+  "  --get_blackbox_info",
+  "    options:",
+  "      --print",
+  "      --clear",
+  "  --get_archive_log",
+  "    options:",
+  "      --print",
+  "      --clear",
+  "  --archive_pem_chips",
+};
+
+
+static void
+print_usage(const char *name, const char pem_num) {
+  int i;
+  printf("Usage: %s <", name);
+  for (i = 0; i < pem_num; i++)
+  {
+    printf("pem%d", i + 1);
+
+    if(i + 1< pem_num)
+    {
+      printf("|");
+    }
+  }
+  printf("> --update [--force] <file_path>\n");
+
+  printf("Usage: %s <", name);
+  for (i = 0; i < pem_num; i++)
+  {
+    printf("pem%d", i + 1);
+
+    if(i + 1< pem_num)
+    {
+      printf("|");
+    }
+  }
+  printf("> <command> <options>\n");
+
+  printf("       command:\n");
+  for (i = 0; i < sizeof(option_list)/sizeof(option_list[0]); i++)
+    printf("       %s\n", option_list[i]);
+}
+
+char get_pem_id(const char *name, const char pem_num)
+{
+  char ret = -1;
+  char i = 0;
+  char buffer[32];
+  for (i = 0; i < pem_num; i++)
+  {
+    snprintf(buffer, sizeof(buffer), "pem%d", i+1);
+    if(!strcmp(name, buffer))
+    {
+      ret = i;
+      break;
+    }
+  }
+  return ret;
+}
+
+int
+main(int argc, const char *argv[]) {
+  uint8_t pem_num = 0, prsnt = 0;
+  int pid_file = 0;
+  int ret = 0;
+
+  if (argc < 3) {
+    print_usage(argv[0], PEM_NUM);
+    return -1;
+  }
+
+  pem_num = get_pem_id(argv[1], PEM_NUM);
+  if (pem_num < 0) {
+    print_usage(argv[0], PEM_NUM);
+    return -1;
+  }
+
+  pid_file = open("/var/run/pem-util.pid", O_CREAT | O_RDWR, 0666);
+  if (flock(pid_file, LOCK_EX | LOCK_NB) && (errno == EWOULDBLOCK)) {
+    printf("Another pem-util instance is running...\n");
+    exit(EXIT_FAILURE);
+  }
+
+  ret = is_pem_prsnt(pem_num, &prsnt);
+  if (ret) {
+    printf("Get PEM%d present error!\n", pem_num + 1);
+    return ret;
+  }
+  if (!prsnt) {
+    printf("PEM%d is not present!\n", pem_num + 1);
+    return 0;
+  }
+
+  if (!strcmp(argv[2], "--get_pem_info")) {
+    ret = get_pem_info(pem_num);
+  }
+  else if (!strcmp(argv[2], "--get_blackbox_info") && argv[3] != NULL) {
+    ret = get_blackbox_info(pem_num, argv[3]);
+  }
+  else if (!strcmp(argv[2], "--get_eeprom_info") && argv[3] != NULL) {
+    ret = get_eeprom_info(pem_num, argv[3]);
+  }
+  else if (!strcmp(argv[2], "--get_archive_log") && argv[3] != NULL) {
+    ret = get_archive_log(pem_num, argv[3]);
+  }
+  else if (!strcmp(argv[2], "--archive_pem_chips")) {
+    ret = archive_pem_chips(pem_num);
+  }
+  else if (!strcmp(argv[2], "--update") && argv[3] != NULL) {
+    if (!strcmp(argv[3], "--force") && argv[4] != NULL) {
+      ret = do_update_pem(pem_num, argv[4], argv[5], 1);
+    } else {
+      ret = do_update_pem(pem_num, argv[3], argv[4], 0);
+    }
+    if (ret < 0) {
+      syslog(LOG_WARNING, "PEM%d update fail!", pem_num + 1);
+      printf("PEM%d update fail!\n", pem_num + 1);
+    } else {
+      if (ret == FW_IDENTICAL) {
+        printf("PEM%d firmware is identical to the target, skip to update.\n" \
+               "Please add \"--force\" option to update forcibly!\n", pem_num + 1);
+      } else {
+        syslog(LOG_WARNING, "PEM%d update success!", pem_num + 1);
+      }
+    }
+  }
+  else {
+    print_usage(argv[0], PEM_NUM);
+    return -1;
+  }
+
+  return ret;
+}

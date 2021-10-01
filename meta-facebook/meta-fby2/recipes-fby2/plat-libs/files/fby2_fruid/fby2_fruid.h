8180f68d423a97a3f32e5a750f44b7aa8b89316c
Michael Garner <garnermic@fb.com>
2021-10-01 13:59:59 -0700

    Replace code which directly accesses the temp data store with calls through libkv
    Summary: Replace code which directly accesses the temp data store with calls through libkv
    
    Test Plan:
    root@sled0503me-oob:/var/log# fruid-util  nic
    
    FRU Information           : Mellanox NIC
    ---------------           : ------------------
    Board Mfg Date            : Thu Jul 11 06:13:00 2019
    Board Mfg                 : Mellanox
    Board Product             : Mellanox-ConnectX-4 Lx
    Board Serial              : MT1920K20540
    Board Part Number         : MCX4431N-GCAN_FB
    Board FRU ID              : FRU Ver 0.01
    Board Custom Data 1       : 19-000974
    Product Manufacturer      : Mellanox
    Product Name              : Adaptor Type 1 / 50G NIC
    Product Part Number       : 1HYQZZZ002I
    Product Serial            : MT1920K20540
    Product Custom Data 1     : 19-000980
    Product Custom Data 2     : MP
    Product Custom Data 3     : ConnectX-4 Lx
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: d8e3c36a1d

--- meta-facebook/meta-fby2/recipes-fby2/plat-libs/files/fby2_fruid/fby2_fruid.h
+++ meta-facebook/meta-fby2/recipes-fby2/plat-libs/files/fby2_fruid/fby2_fruid.h
@@ -25,7 +25,7 @@
 #define FBY2_FRU_PATH "/tmp/fruid_%s.bin"
 #define FBY2_FRU_DEV_PATH "/tmp/fruid_%s_dev%d.bin"
 
-#define NIC_FW_VER_PATH "/tmp/cache_store/nic_fw_ver"
+#define NIC_FW_VER_PATH "nic_fw_ver"
 
 #define MFG_MELLANOX 0x19810000
 #define MFG_BROADCOM 0x3D110000

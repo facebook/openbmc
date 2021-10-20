234464f910961fe198a79d7d1e11a14ec0cc076a
Patrick Williams <patrickw3@fb.com>
2021-10-20 13:47:39 -0700

    northdome: add SEL log for 10K fan installed on northdome (#2707)
    Summary:
    1. add SEL log for 10K fan (only northdome)
    2. increase sleep to 5 seconds (fan takes a while to stabilize)
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2707
    
    Test Plan:
    1. Build passed on northdome
    2. Install 10K & 15 Fan then reboot BMC, check SEL log & fan_config
    
    - 15K Fan installed
    ```
    root@bmc-oob:~# log-util all --print | grep 10K
    root@bmc-oob:~# cat /tmp/fan_config
    1
    ```
    - 10K Fan installed, a "10K Fan detected" SEL log recorded
    ```
    root@bmc-oob:~# log-util all --print | grep 10K
    0    all      2018-03-09 04:35:26    root             10K Fan detected
    root@bmc-oob:~# cat /tmp/fan_config
    0
    ```
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: 4eb1848e7e

--- meta-facebook/meta-fby2/recipes-fby2/fscd/fscd/check_fan_config.sh
+++ meta-facebook/meta-fby2/recipes-fby2/fscd/fscd/check_fan_config.sh
@@ -2,6 +2,7 @@
 
 . /usr/local/fbpackages/utils/ast-functions
 
+fan_check_delay=5
 spb_type=$(get_spb_type)
 kernel_ver=$(get_kernel_ver)
 
@@ -11,7 +12,7 @@ if [ $kernel_ver == 5 ]; then
     set_fan2='echo 180 > /sys/devices/platform/ahb/ahb:apb/1e786000.pwm-tacho-controller/hwmon/hwmon*/pwm2'
     eval $set_fan1
     eval $set_fan2
-    sleep 1
+    sleep $fan_check_delay
     if [ $spb_type == 1 ] ; then
         # for Yv2.50
         fan0_rpm=`cat /sys/bus/platform/drivers/aspeed_pwm_tacho/1e786000.pwm-tacho-controller/hwmon/hwmon*/fan3_input`
@@ -21,14 +22,14 @@ if [ $kernel_ver == 5 ]; then
         fan1_rpm=`cat /sys/bus/platform/drivers/aspeed_pwm_tacho/1e786000.pwm-tacho-controller/hwmon/hwmon*/fan2_input`
     fi
     #restore the initial value 100% (init_pwm.sh)
-    reset_fan1='echo 0 > /sys/devices/platform/ahb/ahb:apb/1e786000.pwm-tacho-controller/hwmon/hwmon*/pwm1'
-    reset_fan2='echo 0 > /sys/devices/platform/ahb/ahb:apb/1e786000.pwm-tacho-controller/hwmon/hwmon*/pwm2'
+    reset_fan1='echo 255 > /sys/devices/platform/ahb/ahb:apb/1e786000.pwm-tacho-controller/hwmon/hwmon*/pwm1'
+    reset_fan2='echo 255 > /sys/devices/platform/ahb/ahb:apb/1e786000.pwm-tacho-controller/hwmon/hwmon*/pwm2'
     eval $reset_fan1
     eval $reset_fan2
 else
     echo 70 > /sys/devices/platform/ast_pwm_tacho.0/pwm0_falling
     echo 70 > /sys/devices/platform/ast_pwm_tacho.0/pwm1_falling
-    sleep 1
+    sleep $fan_check_delay
     if [ $spb_type == 1 ] ; then
         # for Yv2.50
         fan0_rpm=`cat /sys/devices/platform/ast_pwm_tacho.0/tacho2_rpm`
@@ -42,8 +43,17 @@ else
     echo 0 > /sys/devices/platform/ast_pwm_tacho.0/pwm1_falling
 fi
 
+# default 10K_FAN
+fan_config=0
+
 if [ $fan0_rpm -ge "8000" ] || [ $fan1_rpm -ge "8000" ]; then
-    echo 1 > /tmp/fan_config
-else
-    echo 0 > /tmp/fan_config
+    # 15K_FAN
+    fan_config=1
+fi
+
+echo "$fan_config" > /tmp/fan_config
+
+# add SEL log for Northdome if 10K fan detected
+if [ "$spb_type" -eq "3" ] && [ "$fan_config" -eq "0" ]; then
+    logger -p user.crit "10K Fan detected"
 fi

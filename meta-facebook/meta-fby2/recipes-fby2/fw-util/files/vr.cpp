9aa30eb1e0b50096b43ab02a903f0df2bebdd449
Patrick Williams <patrickw3@fb.com>
2021-10-22 14:32:40 -0700

    northdome: fw-util, update vr version format (#2706)
    Summary:
    1. update vr version format
    2. add json format support
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2706
    
    Test Plan:
    t rebase --interactive
    
    1. Build passed on northdome
    2. Check vr version by fw-util
    
    - Normal format
    ```
    root@bmc-oob:~# fw-util slot1 --version vr
    PVDDCR_CPU VR Version: E000CA46, Remaining Writes: 23
    PVDDCR_SOC VR Version: 2911482B, Remaining Writes: 26
    PVDDIO_ABCD VR Version: DC637D5A, Remaining Writes: 26
    PVDDIO_EFGH VR Version: DC637D5A, Remaining Writes: 26
    ```
    
    - JSON format
    ```
    root@bmc-oob:~# fw-util slot1 --version-json vr
    [
        {
            "COMPONENT": "vr",
            "FRU": "slot1",
            "VERSION": {
                "PVDDCR_CPU": {
                    "rmng_w": "23",
                    "version": "e000ca46"
                },
                "PVDDCR_SOC": {
                    "rmng_w": "26",
                    "version": "2911482b"
                },
                "PVDDIO_ABCD": {
                    "rmng_w": "26",
                    "version": "dc637d5a"
                },
                "PVDDIO_EFGH": {
                    "rmng_w": "26",
                    "version": "dc637d5a"
                }
            }
        }
    ]
    ```
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: 3334cc29b0

--- meta-facebook/meta-fby2/recipes-fby2/fw-util/files/vr.cpp
+++ meta-facebook/meta-fby2/recipes-fby2/fw-util/files/vr.cpp
@@ -42,7 +42,7 @@ class VrComponent : public Component {
         } else {
           printf("0V92 VR Version: 0x%02x\n", ver[0]);
         }
-      } catch (string err) {
+      } catch (const string& err) {
         printf("3V3 VR Version: NA (%s)\n", err.c_str());
         printf("0V92 VR Version: NA (%s)\n", err.c_str());
       }
@@ -64,34 +64,34 @@ class VrComponent : public Component {
             printf("PVDDCR_CPU VR Version: NA\n");
           } else {
             printf(
-                "PVDDCR_CPU VR Version: 0x%02x%02x, 0x%02x%02x, Remaining Writes: %u\n",
-                ver[0], ver[1], ver[2], ver[3], ver[4]);
+                "PVDDCR_CPU VR Version: %02X%02X%02X%02X, Remaining Writes: %u\n",
+                ver[3], ver[2], ver[1], ver[0], ver[4]);
           }
 
           if (bic_get_fw_ver(slot_id, FW_PVDDCR_SOC_VR, ver)) {
             printf("PVDDCR_SOC VR Version: NA\n");
           } else {
             printf(
-                "PVDDCR_SOC VR Version: 0x%02x%02x, 0x%02x%02x, Remaining Writes: %u\n",
-                ver[0], ver[1], ver[2], ver[3], ver[4]);
+                "PVDDCR_SOC VR Version: %02X%02X%02X%02X, Remaining Writes: %u\n",
+                ver[3], ver[2], ver[1], ver[0], ver[4]);
           }
 
           if (bic_get_fw_ver(slot_id, FW_PVDDIO_ABCD_VR, ver)) {
             printf("PVDDIO_ABCD VR Version: NA\n");
           } else {
             printf(
-                "PVDDIO_ABCD VR Version: 0x%02x%02x, 0x%02x%02x, Remaining Writes: %u\n",
-                ver[0], ver[1], ver[2], ver[3], ver[4]);
+                "PVDDIO_ABCD VR Version: %02X%02X%02X%02X, Remaining Writes: %u\n",
+                ver[3], ver[2], ver[1], ver[0], ver[4]);
           }
 
           if (bic_get_fw_ver(slot_id, FW_PVDDIO_EFGH_VR, ver)) {
             printf("PVDDIO_EFGH VR Version: NA\n");
           } else {
             printf(
-                "PVDDIO_EFGH VR Version: 0x%02x%02x, 0x%02x%02x, Remaining Writes: %u\n",
-                ver[0], ver[1], ver[2], ver[3], ver[4]);
+                "PVDDIO_EFGH VR Version: %02X%02X%02X%02X, Remaining Writes: %u\n",
+                ver[3], ver[2], ver[1], ver[0], ver[4]);
           }
-        } catch (string err) {
+        } catch (const string& err) {
           printf("PVDDCR_CPU VR Version: NA (%s)\n", err.c_str());
           printf("PVDDCR_SOC VR Version: NA (%s)\n", err.c_str());
           printf("PVDDIO_ABCD VR Version: NA (%s)\n", err.c_str());
@@ -186,7 +186,7 @@ class VrComponent : public Component {
                 ver[2],
                 ver[3]);
           }
-        } catch (string err) {
+        } catch (const string& err) {
           printf("PVCCIO VR Version: NA (%s)\n", err.c_str());
           printf("PVCCIN VR Version: NA (%s)\n", err.c_str());
           printf("PVCCSA VR Version: NA (%s)\n", err.c_str());
@@ -207,12 +207,138 @@ class VrComponent : public Component {
     try {
       server.ready();
       ret = bic_update_fw(slot_id, UPDATE_VR, (char*)image.c_str());
-    } catch (string err) {
+    } catch (const string& err) {
       ret = FW_STATUS_NOT_SUPPORTED;
     }
     return ret;
   }
+
+  void get_version(json& j);
+
+private:
+  void get_version_gpv2(json& j);
+  void get_version_server(json& j);
+  void get_version_server_nd(json& j);
+
 };
+
+void VrComponent::get_version(json& j)
+{
+  if (fby2_get_slot_type(slot_id) == SLOT_TYPE_GPV2) {
+    return get_version_gpv2(j);
+  } else {
+    int ret;
+    uint8_t server_type = 0xFF;
+    ret = fby2_get_server_type(slot_id, &server_type);
+    if (ret) {
+      syslog(LOG_ERR, "%s, Get server type failed\n", __func__);
+    }
+    switch (server_type) {
+    case SERVER_TYPE_ND:
+      get_version_server_nd(j);
+      break;
+    case SERVER_TYPE_TL:
+    default:
+      get_version_server(j);
+      break;
+    }
+  }
+
+}
+
+void VrComponent::get_version_gpv2(json& j)
+{
+  uint8_t buf[32] = {0};
+  char ver_str[16] = {0};
+  const map<uint8_t, string> list = {
+    {FW_3V3_VR, "3V3"},
+    {FW_0V92,   "0V92"}
+  };
+
+  for ( auto& vr:list ) {
+    try {
+      server.ready();
+      if (bic_get_fw_ver(slot_id, vr.first, buf)) {
+        throw "Error in getting the version of " + vr.second;
+      }
+
+      snprintf(ver_str, sizeof(ver_str), "0x%02x", buf[0]);
+      j["VERSION"][vr.second]["version"] = std::string(ver_str);
+    } catch (const string& err) {
+      if ( err.find("empty") != string::npos )
+        j["VERSION"] = "not_present";
+      else
+        j["VERSION"] = "error_returned";
+      return;
+    }
+  }
+}
+
+void VrComponent::get_version_server(json& j)
+{
+  uint8_t buf[32] = {0};
+  char ver_str[16] = {0};
+  const map<uint8_t, string> list = {
+    {FW_PVCCIO_VR,  "PVCCIO"},
+    {FW_PVCCIN_VR,  "PVCCIN"},
+    {FW_PVCCSA_VR,  "PVCCSA"},
+    {FW_DDRAB_VR,   "DDRAB"},
+    {FW_DDRDE_VR,   "DDRDE"},
+    {FW_PVNNPCH_VR, "PVNNPCH"},
+    {FW_P1V05_VR,   "P1V05"}
+  };
+
+  for ( auto& vr:list ) {
+    try {
+      server.ready();
+      if (bic_get_fw_ver(slot_id, vr.first, buf)) {
+        throw "Error in getting the version of " + vr.second;
+      }
+
+      snprintf(ver_str, sizeof(ver_str), "0x%02x%02x, 0x%02x%02x", buf[0], buf[1], buf[2], buf[3]);
+      j["VERSION"][vr.second]["version"] = std::string(ver_str);
+    } catch (const string& err) {
+      if ( err.find("empty") != string::npos )
+        j["VERSION"] = "not_present";
+      else
+        j["VERSION"] = "error_returned";
+      return;
+    }
+  }
+}
+
+void VrComponent::get_version_server_nd(json& j)
+{
+  uint8_t buf[32] = {0};
+  char ver_str[16] = {0};
+  char rmng_w_str[4] = {0};
+  const map<uint8_t, string> list = {
+    {FW_PVDDCR_CPU_VR,  "PVDDCR_CPU"},
+    {FW_PVDDCR_SOC_VR,  "PVDDCR_SOC"},
+    {FW_PVDDIO_ABCD_VR, "PVDDIO_ABCD"},
+    {FW_PVDDIO_EFGH_VR, "PVDDIO_EFGH"}
+  };
+
+  for ( auto& vr:list ) {
+    try {
+      server.ready();
+      if (bic_get_fw_ver(slot_id, vr.first, buf)) {
+        throw "Error in getting the version of " + vr.second;
+      }
+
+      snprintf(ver_str, sizeof(ver_str), "%02x%02x%02x%02x", buf[3], buf[2], buf[1], buf[0]);
+      snprintf(rmng_w_str, sizeof(rmng_w_str), "%u", buf[4]);
+      j["VERSION"][vr.second]["version"] = std::string(ver_str);
+      j["VERSION"][vr.second]["rmng_w"] = std::string(rmng_w_str);
+    } catch (const string& err) {
+      if ( err.find("empty") != string::npos )
+        j["VERSION"] = "not_present";
+      else
+        j["VERSION"] = "error_returned";
+      return;
+    }
+  }
+}
 
 VrComponent vr1("slot1", "vr", 1);
 VrComponent vr2("slot2", "vr", 2);

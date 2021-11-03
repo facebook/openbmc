d1d0bf714070ad8a10d2b62cf745ddbcfb90c877
Marco Kawajiri <kawajiri@fb.com>
2021-11-03 08:47:26 -0700

    Remove DNS as source of truth
    Summary: Logic seems flaky and there's no guarantee of getting a stable name from DNS if there are multiple entries.
    
    Test Plan: Run `hostname.sh` on both sysv and systemd test devices with and without `/mnt/data/hostname`
    
    Reviewed By: doranand
    
    fbshipit-source-id: c026a70cce

--- common/recipes-core/initscripts/files/hostname.sh
+++ common/recipes-core/initscripts/files/hostname.sh
@@ -8,7 +8,6 @@
 # Short-Description: Set hostname based on /etc/hostname
 ### END INIT INFO
 HOSTNAME="bmc-oob."
-INTERFACE=eth0
 
 set_hostname() {
   local name=$1
@@ -21,59 +20,8 @@ set_hostname() {
   /etc/init.d/syslog reload
 }
 
-update_hostname() {
-  #
-  # Increasing the number of retries to 20, as 10 is not enough in some DC.
-  # In order to reduce the load to DNS server, we use exponential back-off. 
-  # That is, the delay between retry will double each time, after loop #10.
-  #
-  RETRY_MAX=20
-  RETRY_DELAY=15
-  DELAY_MAX=3600
-  LOOP_NUMBER=0
-  while [ $LOOP_NUMBER -lt $RETRY_MAX ]; do
-    LOOP_NUMBER=$((LOOP_NUMBER+1))
-    if [ $LOOP_NUMBER -gt 10 ]; then
-      RETRY_DELAY=$((RETRY_DELAY*2))
-      if [ $RETRY_DELAY -gt $DELAY_MAX ]; then
-         RETRY_DELAY=$DELAY_MAX
-      fi
-    fi
-    # Wait till DHCP gets its IP addresses.
-    sleep $RETRY_DELAY
-    # Do a DNS lookup for each IP address on eth0 to find a hostname.
-    # If multiple hostnames are returned, the sled one, if present, is preferred.
-    FOUND_NAME=""
-    for ip in $(ip a s $INTERFACE | sed -nr 's, *inet6? ([0-9a-f:.]+)\/.*,\1,p'); do
-      dns_ptr=$(dig +short -x "$ip" | sed -nr '/sled.*-oob/ { p; q }; $ { p }')
-      dns_ptr=${dns_ptr%?}
-      if [ "${dns_ptr:0:2}" != ";;" ]; then
-          FOUND_NAME=$dns_ptr
-          break
-      fi
-	  done
-
-    if [ -n "$FOUND_NAME" ]; then
-      break
-    fi
-  done
-
-  # dig didn't return anything. Do not update files.
-  [[ -z $FOUND_NAME ]] && return 0
-
-  # Only update if the hostname is different from the one set
-  # at bootup.
-  if [ "$HOSTNAME" != "$FOUND_NAME" ]; then
-    set_hostname $FOUND_NAME
-    logger "Hostname changed from $HOSTNAME to $FOUND_NAME"
-  fi
-}
-
-if [ -e /mnt/data/hostname ]; then
+if [ -s /mnt/data/hostname ]; then
   # Use the cached copy to set the hostname
   HOSTNAME=$(cat /mnt/data/hostname)
 fi
 set_hostname $HOSTNAME
-# Start the update process in the background so
-# we may update the cached copy if it has changed.
-(update_hostname &)&

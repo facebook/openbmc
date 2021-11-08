cced25bc984aed19aace0f04528319b38ea67986
Peter Fetros <goldenbug@fb.com>
2021-11-08 09:27:58 -0800

    fby3: enclosure-util dispaly more GPv3 m.2 device info (#2727)
    Summary:
    enclosure-util dispaly more GPv3 m.2 device info
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2727
    
    Test Plan:
    Could show more GPv3 m.2 device info:
    
    root@bmc-oob:~# enclosure-util slot1 --drive-status 2U-dev0
    * Mux selecting...bus 2, chn: 0, dev_id: 0
    slot1-2U-dev0 :
    Status Flags: 0x3B
        SMBUS block read complete: No
        Drive Ready: Ready
        Drive Functional: Functional
        Reset Required: No
        Port 0 PCIe Link Active: Up
        Port 1 PCIe Link Active: Down
    SMART Critical Warning: 0x00
        Spare Space: Low
        Temperature Warning: Abnormal
        NVM Subsystem Reliability: Degraded
        Media Status: Read Only mode
        Volatile Memory Backup Device: Failed
    Composite Temperature: 55 C
    Percentage Drive Life Used: 0
    Vendor: Broadcom(0x14E4)
    Serial Number: M2AFB21370007IFVX00
    Module Product Part Number: BCM958402M2AFB
    Management End Point Form Factor: M.2 22110 (0x35)
    Form Factor Information 0 Register: Accelerator (0x01)
    Module health: 0x00
    Lower Thermal Threshold: 0 C
    Upper Thermal Threshold: 0 C
    Power State: Unknown (0x00)
    SMBus/I2C Frquency: 400 kHz
    Module Static TDP level setting: Unknown (0x00)
    ASIC version: 0x01
    FW version: v2.0
    ASIC Core1 Voltage: 0.1789 V
    ASIC Core2 Voltage: 0.3314 V
    Module Power Rail1 Voltage: 0.0669 V
    Module Power Rail2 Voltage: 0.0000 V
    ASIC Error Type Report: 0x00
    Module Error Type Report: 0x00
    Warning Flag: 0x00
    Interrupt Flag: 0x00
    Historical Max ASIC Temperature: 0 C
    Total internal memory error count: 0
    Total external memory error count: 0
    SMBus error: 0x00
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: ab6962153c

--- meta-facebook/meta-fby3/recipes-fby3/plat-utils/files/enclosure-util/enclosure-util.c
+++ meta-facebook/meta-fby3/recipes-fby3/plat-utils/files/enclosure-util/enclosure-util.c
@@ -36,7 +36,7 @@
 #define NVME_SMART_WARNING_MASK_BIT 0x1F  // check bit 0~4
 #define MAX_SERIAL_NUM 20
 #define SPE_SSD_NUM 6
-
+#define MAX_PART_NUM 40
 
 char path[64] = {0};
 int fd;
@@ -71,6 +71,30 @@ drive_status(ssd_data *ssd) {
   t_key_value_pair pdlu_decoding;
   t_key_value_pair vendor_decoding;
   t_key_value_pair sn_decoding;
+  t_key_value_pair pn_decoding;
+  t_key_value_pair meff_decoding;
+  t_key_value_pair ffi_0_decoding;
+  t_key_value_pair sinfo_0_decoding;
+  t_key_value_pair module_helath_decoding;
+  t_key_value_pair lower_thermal_temp_decoding;
+  t_key_value_pair upper_thermal_temp_decoding;
+  t_key_value_pair power_state_decoding;
+  t_key_value_pair i2c_freq_decoding;
+  t_key_value_pair tdp_level_decoding;
+  t_key_value_pair asic_version_decoding;
+  t_key_value_pair fw_version_decoding;
+  t_key_value_pair asic_core_vol1_decoding;
+  t_key_value_pair asic_core_vol2_decoding;
+  t_key_value_pair power_rail_vol1_decoding;
+  t_key_value_pair power_rail_vol2_decoding;
+  t_key_value_pair asic_error_type_decoding;
+  t_key_value_pair module_error_type_decoding;
+  t_key_value_pair warning_flag_decoding;
+  t_key_value_pair interrupt_flag_decoding;
+  t_key_value_pair max_asic_temp_decoding;
+  t_key_value_pair total_int_mem_err_count_decoding;
+  t_key_value_pair total_ext_mem_err_count_decoding;
+  t_key_value_pair smbus_err_decoding;
 
   printf("\n");
   nvme_sflgs_decode(ssd->sflgs, &status_flag_decoding);
@@ -101,6 +125,68 @@ drive_status(ssd_data *ssd) {
 
   nvme_serial_num_decode(ssd->serial_num, &sn_decoding);
   printf("%s: %s\n", sn_decoding.key, sn_decoding.value);
+
+  if (ssd->fb_defined == 0x01) {
+    nvme_part_num_decode(ssd->block_len_module_id_area, ssd->part_num, &pn_decoding);
+    printf("%s: %s\n", pn_decoding.key, pn_decoding.value);
+    nvme_meff_decode(ssd->block_len_module_id_area, ssd->meff, &meff_decoding);
+    printf("%s: %s\n", meff_decoding.key, meff_decoding.value);
+    nvme_ffi_0_decode(ssd->block_len_module_id_area, ssd->ffi_0, &ffi_0_decoding);
+    printf("%s: %s\n", ffi_0_decoding.key, ffi_0_decoding.value);
+
+    if (ssd->ffi_0 == FFI_0_ACCELERATOR) {
+      nvme_raw_data_prase("Module health", ssd->block_len_module_stat_area, ssd->module_helath, &module_helath_decoding);
+      printf("%s: %s\n", module_helath_decoding.key, module_helath_decoding.value);
+      nvme_lower_threshold_temp_decode(ssd->block_len_module_stat_area, ssd->lower_theshold, &lower_thermal_temp_decoding);
+      printf("%s: %s\n", lower_thermal_temp_decoding.key, lower_thermal_temp_decoding.value);
+      nvme_upper_threshold_temp_decode(ssd->block_len_module_stat_area, ssd->upper_threshold, &upper_thermal_temp_decoding);
+      printf("%s: %s\n", upper_thermal_temp_decoding.key, upper_thermal_temp_decoding.value);
+
+      nvme_power_state_decode(ssd->block_len_module_stat_area, ssd->power_state, &power_state_decoding);
+      printf("%s: %s\n", power_state_decoding.key, power_state_decoding.value);
+      nvme_i2c_freq_decode (ssd->block_len_module_stat_area, ssd->i2c_freq, &i2c_freq_decoding);
+      printf("%s: %s\n", i2c_freq_decoding.key, i2c_freq_decoding.value);
+      nvme_tdp_level_decode (ssd->block_len_module_stat_area, ssd->tdp_level, &tdp_level_decoding);
+      printf("%s: %s\n", tdp_level_decoding.key, tdp_level_decoding.value);
+
+      nvme_raw_data_prase("ASIC version", ssd->block_len_ver_area, ssd->asic_version, &asic_version_decoding);
+      printf("%s: %s\n", asic_version_decoding.key, asic_version_decoding.value);
+      nvme_fw_version_decode(ssd->block_len_ver_area, ssd->fw_major_ver, ssd->fw_minor_ver, &fw_version_decoding);
+      printf("%s: %s\n", fw_version_decoding.key, fw_version_decoding.value);
+
+      nvme_monitor_area_decode("ASIC Core1 Voltage", ssd->block_len_mon_area, ssd->asic_core_vol1, ASIC_CORE_VOL_UNIT, &asic_core_vol1_decoding);
+      printf("%s: %s\n", asic_core_vol1_decoding.key, asic_core_vol1_decoding.value);
+      nvme_monitor_area_decode("ASIC Core2 Voltage", ssd->block_len_mon_area, ssd->asic_core_vol2, ASIC_CORE_VOL_UNIT, &asic_core_vol2_decoding);
+      printf("%s: %s\n", asic_core_vol2_decoding.key, asic_core_vol2_decoding.value);
+      nvme_monitor_area_decode("Module Power Rail1 Voltage", ssd->block_len_mon_area, ssd->power_rail_vol1, POWER_RAIL_VOL_UNIT, &power_rail_vol1_decoding);
+      printf("%s: %s\n", power_rail_vol1_decoding.key, power_rail_vol1_decoding.value);
+      nvme_monitor_area_decode("Module Power Rail2 Voltage", ssd->block_len_mon_area, ssd->power_rail_vol2, POWER_RAIL_VOL_UNIT, &power_rail_vol2_decoding);
+      printf("%s: %s\n", power_rail_vol2_decoding.key, power_rail_vol2_decoding.value);
+
+      nvme_raw_data_prase("ASIC Error Type Report", ssd->block_len_err_ret_area, ssd->asic_error_type, &asic_error_type_decoding);
+      printf("%s: %s\n", asic_error_type_decoding.key, asic_error_type_decoding.value);
+      nvme_raw_data_prase("Module Error Type Report", ssd->block_len_err_ret_area, ssd->module_error_type, &module_error_type_decoding);
+      printf("%s: %s\n", module_error_type_decoding.key, module_error_type_decoding.value);
+      nvme_raw_data_prase("Warning Flag", ssd->block_len_err_ret_area, ssd->warning_flag, &warning_flag_decoding);
+      printf("%s: %s\n", warning_flag_decoding.key, warning_flag_decoding.value);
+      nvme_raw_data_prase("Interrupt Flag", ssd->block_len_err_ret_area, ssd->interrupt_flag, &interrupt_flag_decoding);
+      printf("%s: %s\n", interrupt_flag_decoding.key, interrupt_flag_decoding.value);
+      nvme_max_asic_temp_decode(ssd->block_len_err_ret_area, ssd->max_asic_temp, &max_asic_temp_decoding);
+      printf("%s: %s\n", max_asic_temp_decoding.key, max_asic_temp_decoding.value);
+      nvme_total_int_mem_err_count_decode(ssd->block_len_err_ret_area, ssd->total_int_mem_err_count, &total_int_mem_err_count_decoding);
+      printf("%s: %s\n", total_int_mem_err_count_decoding.key, total_int_mem_err_count_decoding.value);
+      nvme_total_ext_mem_err_count_decode(ssd->block_len_err_ret_area, ssd->total_ext_mem_err_count, &total_ext_mem_err_count_decoding);
+      printf("%s: %s\n", total_ext_mem_err_count_decoding.key, total_ext_mem_err_count_decoding.value);
+      nvme_smbus_err_decode(ssd->block_len_err_ret_area, ssd->smbus_err, &smbus_err_decoding);
+      printf("%s: %s\n", smbus_err_decoding.key, smbus_err_decoding.value);
+    } else if (ssd->ffi_0 == FFI_0_STORAGE) {
+      printf("%s: 0x%02X\n", "Storage version", ssd->ssd_ver);
+      printf("%s: %d GB\n", "Storage Capacity", ssd->ssd_capacity);
+      printf("%s: %d W\n", "Storage Power", ssd->ssd_pwr);
+      nvme_sinfo_0_decode (ssd->ssd_sinfo_0,&sinfo_0_decoding);
+      printf("%s: %s\n", sinfo_0_decoding.key, sinfo_0_decoding.value);
+    }
+  }
 
   return 0;
 }
@@ -167,6 +253,7 @@ read_nvme_data(uint8_t fru_id, uint8_t slot_id, uint8_t device_id, uint8_t cmd) 
   uint8_t rbuf[64] = {0};
   uint8_t tlen = 0;
   uint8_t rlen = 0;
+  float tdp_val = 0;
   static const uint8_t spe_ssd_bus[SPE_SSD_NUM] = {2, 3, 4, 5, 6, 7};
   ssd_data ssd;
   
@@ -212,7 +299,7 @@ read_nvme_data(uint8_t fru_id, uint8_t slot_id, uint8_t device_id, uint8_t cmd) 
     ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
     if (ret != 0) {
       printf("slot%u-%s : %s\n", slot_id, str, "NA");
-      syslog(LOG_DEBUG, "%s(): bic_master_write_read offset=%d read length=%d failed", __func__,tbuf[0],rlen);
+      syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
       return -1;
     }
   }  
@@ -227,7 +314,7 @@ read_nvme_data(uint8_t fru_id, uint8_t slot_id, uint8_t device_id, uint8_t cmd) 
       tlen = 4;
       ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
       if (ret != 0) {
-        syslog(LOG_DEBUG, "%s(): bic_master_write_read offset=%d read length=%d failed", __func__,tbuf[0],rlen);
+        syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
         printf("NA");
         break;
       }
@@ -241,12 +328,118 @@ read_nvme_data(uint8_t fru_id, uint8_t slot_id, uint8_t device_id, uint8_t cmd) 
       tbuf[3] = 0x08;  // offset 08
       ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
       if (ret != 0) {
-        syslog(LOG_DEBUG, "%s(): bic_master_write_read offset=%d read length=%d failed", __func__,tbuf[0],rlen);
+        syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
         printf("NA");
         break;
       }
       ssd.vendor = (rbuf[offset_base + 1] << 8) | rbuf[offset_base + 2];
       memcpy(ssd.serial_num, &rbuf[offset_base + 3], MAX_SERIAL_NUM);
+
+      if ( type_2ou == GPV3_MCHP_BOARD || type_2ou == GPV3_BRCM_BOARD ) {
+        tbuf[2] = 55; //read back
+        tbuf[3] = 0x20;  // offset 32
+        ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
+        if (ret != 0) {
+          syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
+          break;
+        }
+        ssd.block_len_module_id_area = rbuf[offset_base + 0];
+        ssd.fb_defined = rbuf[offset_base + 1];
+        memcpy(ssd.part_num, &rbuf[offset_base + 2], MAX_PART_NUM);
+        ssd.meff = rbuf[offset_base + 42];
+        ssd.ffi_0 = rbuf[offset_base + 43];
+
+        if (ssd.fb_defined == 1) {
+          if (ssd.ffi_0 == FFI_0_ACCELERATOR) {
+            tbuf[2] = 8; //read back
+            tbuf[3] = 0x60;  // offset 96
+            ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
+            if (ret != 0) {
+              syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
+              break;
+            }
+            ssd.block_len_module_stat_area = rbuf[offset_base + 0];
+            ssd.module_helath = rbuf[offset_base + 1];
+            ssd.lower_theshold = rbuf[offset_base + 2];
+            ssd.upper_threshold = rbuf[offset_base + 3];
+            ssd.power_state = rbuf[offset_base + 4];
+            ssd.i2c_freq = rbuf[offset_base + 5];
+
+            // Formula provided by SPH
+            if (ssd.vendor == VENDOR_ID_INTEL) {
+              tdp_val = rbuf[offset_base + 7] + rbuf[offset_base + 6] / 250;
+              if (tdp_val > 13) {
+                ssd.tdp_level = TDP_LEVEL3;
+              } else if (tdp_val >= 10.5) {
+                ssd.tdp_level = TDP_LEVEL2;
+              } else {
+                ssd.tdp_level = TDP_LEVEL1;
+              }
+            } else {
+              ssd.tdp_level = rbuf[offset_base + 6];
+            }
+
+            tbuf[2] = 8; //read back
+            tbuf[3] = 0x68;  // offset 104
+            ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
+            if (ret != 0) {
+              syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
+              break;
+            }
+            ssd.block_len_ver_area = rbuf[offset_base + 0];
+            ssd.asic_version = rbuf[offset_base + 1];
+            ssd.fw_major_ver = rbuf[offset_base + 2];
+            ssd.fw_minor_ver = rbuf[offset_base + 3];
+
+            tbuf[2] = 10; //read back
+            tbuf[3] = 0x70;  // offset 112
+            ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
+            if (ret != 0) {
+              syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
+              break;
+            }
+            ssd.block_len_mon_area = rbuf[offset_base + 0];
+            ssd.asic_core_vol1 = (rbuf[offset_base + 1] << 8) | rbuf[offset_base + 2];
+            ssd.asic_core_vol2 = (rbuf[offset_base + 3] << 8) | rbuf[offset_base + 4];
+            ssd.power_rail_vol1 = (rbuf[offset_base + 5] << 8) | rbuf[offset_base + 6];
+            ssd.power_rail_vol2 = (rbuf[offset_base + 7] << 8) | rbuf[offset_base + 8];
+
+            tbuf[2] = 10; //read back
+            if (ssd.vendor == VENDOR_ID_BROARDCOM) {
+              tbuf[3] = 0x84;  // offset 132
+            } else {
+              tbuf[3] = 0x7A;  // offset 122
+            }
+            ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
+            if (ret != 0) {
+              syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
+              break;
+            }
+            ssd.block_len_err_ret_area = rbuf[offset_base + 0];
+            ssd.asic_error_type = rbuf[offset_base + 1];
+            ssd.module_error_type = rbuf[offset_base + 2];
+            ssd.warning_flag = rbuf[offset_base + 3];
+            ssd.interrupt_flag = rbuf[offset_base + 4];
+            ssd.max_asic_temp = rbuf[offset_base + 5];
+            ssd.total_int_mem_err_count = rbuf[offset_base + 6];
+            ssd.total_ext_mem_err_count = rbuf[offset_base + 7];
+            ssd.smbus_err = rbuf[offset_base + 8];
+          } else if (ssd.ffi_0 == FFI_0_STORAGE) {
+            tbuf[2] = 9; //read back
+            tbuf[3] = 0x57;  // offset 87
+            ret = bic_ipmb_send(slot_id, NETFN_APP_REQ, CMD_APP_MASTER_WRITE_READ, tbuf, tlen, rbuf, &rlen, intf);
+            if (ret != 0) {
+              syslog(LOG_DEBUG, "%s(): bic_ipmb_send offset=%d read length=%d failed", __func__,tbuf[3],rlen);
+              break;
+            }
+            ssd.ssd_ver = rbuf[1];
+            ssd.ssd_capacity = (rbuf[2] << 8) | rbuf[3];
+            ssd.ssd_pwr = rbuf[4];
+            ssd.ssd_sinfo_0 = rbuf[5];
+          }
+        }
+      }
+
 
       drive_status(&ssd);
     } while (0);

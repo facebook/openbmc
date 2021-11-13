98d60db52d832e7b61f4dbf593d36728c22bb418
Amithash Prasad <amithash@fb.com>
2021-11-12 16:11:38 -0800

    rackmond2: Add containers for register map(s)
    Summary:
    This replaces rackmon-config.py design of v1. Instead
    of user feediong the config via the service layer,
    ME
    the configuration is read from JSON files.
    Example configuration
    ```
    {
      "name": "orv2_psu",
      "address_range": [160, 191],
      "probe_register": 104,
      "default_baudrate": 19200,
      "preferred_baudrate": 19200,
      "registers": [
        {"begin": 0, "length": 8, "name": "MFG_MODEL", "format": "ascii"},
        {"begin": 152, "length": 1, "name": "RPM fan0", "keep": 10, "format": "decimal"},
        {"begin": 127, "length": 1, "name": "BBU Absolute State of Charge",
          "keep": 10, "format": "fixedpoint", "precision": 6},
        {"begin": 104, "length": 1, "name": "PSU Status register", "keep": 10,
          "changes_only": true, "format": "table",
          "table": {
            "Main Converter Fail": 0,
            "Aux 54V Converter Fail": 1,
            "Battery Charger Fail": 2,
            "Current Feed (Boost Converter) Fail": 3,
            "Temp Alarm   Alarm set at shutdown temp": 4,
            "Fan Alarm": 5,
            "Battery Alarm Set for BBU Fail or BBU voltage =< 26 VDC": 6,
            "SoH Requested": 7,
            "SoH Dicharge": 8
          }
        }
      ]
    }
    ```
    
    Test Plan: #doitlive
    
    Reviewed By: garnermic
    
    fbshipit-source-id: 87deb9c5e9

---
+++ common/recipes-core/rackmon2/rackmon/regmap.cpp
@@ -0,0 +1,116 @@
+#include "regmap.hpp"
+#include <fstream>
+#include <iostream>
+
+#if (__GNUC__ < 8)
+#include <experimental/filesystem>
+namespace std {
+namespace filesystem = experimental::filesystem;
+}
+#else
+#include <filesystem>
+#endif
+
+using nlohmann::json;
+
+bool addr_range::operator<(const addr_range& rhs) const {
+  return range.second < rhs.range.first;
+}
+
+bool addr_range::contains(uint8_t addr) const {
+  return addr >= range.first && addr <= range.second;
+}
+
+void from_json(const json& j, addr_range& a) {
+  a.range = j;
+}
+void to_json(json& j, const addr_range& a) {
+  j = a.range;
+}
+
+NLOHMANN_JSON_SERIALIZE_ENUM(
+    RegisterFormatType,
+    {
+        {RegisterFormatType::HEX, "hex"},
+        {RegisterFormatType::ASCII, "ascii"},
+        {RegisterFormatType::DECIMAL, "decimal"},
+        {RegisterFormatType::FIXED_POINT, "fixedpoint"},
+        {RegisterFormatType::TABLE, "table"},
+    })
+
+void from_json(const json& j, RegisterInterval& i) {
+  j.at("length").get_to(i.length);
+  j.at("name").get_to(i.name);
+  i.keep = j.value("keep", 1);
+  i.changes_only = j.value("changes_only", false);
+  j.at("format").get_to(i.format);
+  if (i.format == RegisterFormatType::FIXED_POINT) {
+    j.at("precision").get_to(i.precision);
+  } else if (i.format == RegisterFormatType::TABLE) {
+    j.at("table").get_to(i.table);
+  }
+}
+void to_json(json& j, const RegisterInterval& i) {
+  j["length"] = i.length;
+  j["name"] = i.name;
+  j["keep"] = i.keep;
+  j["changes_only"] = i.changes_only;
+  j["format"] = i.format;
+  if (i.format == RegisterFormatType::FIXED_POINT) {
+    j["precision"] = i.precision;
+  } else if (i.format == RegisterFormatType::TABLE) {
+    j["table"] = i.table;
+  }
+}
+
+void from_json(const json& j, RegisterMap& m) {
+  j.at("address_range").get_to(m.applicable_addresses);
+  j.at("probe_register").get_to(m.probe_register);
+  j.at("name").get_to(m.name);
+  j.at("preferred_baudrate").get_to(m.preferred_baudrate);
+  j.at("default_baudrate").get_to(m.default_baudrate);
+  std::vector<RegisterInterval> tmp;
+  j.at("registers").get_to(tmp);
+  for (auto& i : tmp) {
+    m.register_descriptors[i.begin] = i;
+  }
+}
+void to_json(json& j, const RegisterMap& m) {
+  j["address_range"] = m.applicable_addresses;
+  j["probe_register"] = m.probe_register;
+  j["name"] = m.name;
+  j["preferred_baudrate"] = m.preferred_baudrate;
+  j["default_baudrate"] = m.preferred_baudrate;
+  j["registers"] = m.register_descriptors;
+}
+
+RegisterMap& RegisterMapDatabase::at(uint16_t addr) {
+  RegisterMap& ret = regmaps.at(addr_range(addr, addr));
+  if (!ret.applicable_addresses.contains(addr)) {
+    throw std::out_of_range("not found: " + std::to_string(addr));
+  }
+  return ret;
+}
+
+void RegisterMapDatabase::load(const std::string& dir_s) {
+  std::filesystem::path dir(dir_s);
+  for (auto const& dir_entry : std::filesystem::directory_iterator{dir}) {
+    std::string path = dir_entry.path().string();
+    std::ifstream ifs(path);
+    json j;
+    ifs >> j;
+    addr_range range;
+    j.at("address_range").get_to(range);
+    regmaps[range] = j;
+  }
+}
+
+void RegisterMapDatabase::print(std::ostream& os) {
+  json j = {};
+  std::transform(
+      regmaps.begin(),
+      regmaps.end(),
+      std::back_inserter(j),
+      [](const auto& kv) { return kv.second; });
+  os << j.dump(4);
+}
---
+++ common/recipes-core/rackmon2/rackmon/regmap.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <map>
+#include <utility>
+#include <vector>
+
+// Address formats:
+// |--7--|--6--|--5--|--4--|--3--|--2--|--1--|--0--|
+// | T1  | T0  | R2  | R1  | R0  | D2  | D1  | D0  |
+// |-----|-----|-----|-----|-----|-----|-----|-----|
+// T1:T0  | Desc
+// -------------
+// 1  1   | PSU
+// 0  1   | BBU
+// 0  0   | Special devices
+// 1  0   | PSU+BBU (ORv2)
+//
+// R2:R0 - Rack address.
+// D2:D0 - Device (PSU/BBU/PSU+BBU/Special) address.
+//
+// For legacy ORv2 devices,
+// R2 - always 1
+// R2:R1 - Rack address
+// D2 - Shelf address
+// D1:D0 - PSU Address
+//
+// For more flexibility, this is all provided by the JSON
+// regmap files. All the source files here care about
+// is the address ranges for which particular maps
+// apply. We also use this to limit our scans.
+struct addr_range {
+  std::pair<uint8_t, uint8_t> range{};
+  addr_range() {}
+  explicit addr_range(uint8_t a, uint8_t b) : range(a, b) {}
+  explicit addr_range(uint8_t a) : range(a, a) {}
+
+  bool contains(uint8_t) const;
+
+  // less operator for addr_range to allow for it to be
+  // used as a key for map (Cool range map in DB)
+  bool operator<(const addr_range& rhs) const;
+};
+
+enum RegisterFormatType {
+  HEX,
+  ASCII,
+  DECIMAL,
+  FIXED_POINT,
+  TABLE,
+};
+
+struct RegisterInterval {
+  uint16_t begin = 0;
+  uint16_t length = 0;
+  std::string name{};
+  uint16_t keep = 1;
+  bool changes_only = false;
+  RegisterFormatType format = RegisterFormatType::HEX;
+  uint16_t precision = 0;
+  std::map<std::string, uint8_t> table{};
+};
+
+struct RegisterMap {
+  addr_range applicable_addresses;
+  std::string name;
+  uint8_t probe_register;
+  uint32_t default_baudrate;
+  uint32_t preferred_baudrate;
+  std::map<uint16_t, RegisterInterval> register_descriptors;
+  const RegisterInterval& at(uint16_t reg) const {
+    return register_descriptors.at(reg);
+  }
+};
+
+struct RegisterMapDatabase {
+  std::map<addr_range, RegisterMap> regmaps{};
+  RegisterMap& at(uint16_t addr);
+  void load(const std::string& dir_s);
+  // For debug purpose only.
+  void print(std::ostream& os);
+};
+
+// JSON conversion
+void from_json(const nlohmann::json& j, RegisterMap& m);
+void from_json(const nlohmann::json& j, addr_range& a);
+void from_json(const nlohmann::json& j, RegisterInterval& i);
+
+void from_json(nlohmann::json& j, const RegisterMap& m);
+void from_json(nlohmann::json& j, const addr_range& a);
+void from_json(nlohmann::json& j, const RegisterInterval& i);

644c3f5d0f5d3c7aa28b3deac7f7fe3749c25af0
Amithash Prasad <amithash@fb.com>
2021-11-12 16:11:55 -0800

    rackmond2: Add basic monitoring capability.
    Summary:
    Add rackmon which allows scanning all the connected
    busses and also monitoring the registers in each
    of the discovered devices.
    
    Test Plan: Builds
    
    Reviewed By: garnermic
    
    fbshipit-source-id: c7598a8ac9

---
+++ common/recipes-core/rackmon2/rackmon/rackmon.cpp
@@ -0,0 +1,237 @@
+#include "rackmon.hpp"
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <thread>
+
+using nlohmann::json;
+using namespace std::literals;
+
+void Rackmon::load(
+    const std::string& conf_path,
+    const std::string& regmap_dir) {
+  // TODO: Catch parse exceptions and print a pretty
+  // message on exactly which configuration fail
+  // was bad/missing.
+  std::ifstream ifs(conf_path);
+  json j;
+  ifs >> j;
+  for (const auto& iface_conf : j["interfaces"]) {
+    std::unique_ptr<Modbus> iface = std::make_unique<Modbus>();
+    iface->initialize(iface_conf);
+    interfaces.push_back(std::move(iface));
+  }
+  regmap_db.load(regmap_dir);
+
+  // Precomputing this makes our scan soooo much easier.
+  // its 256 bytes wasted. but worth it.
+  for (uint16_t addr = 0; addr <= 0xff; addr++) {
+    try {
+      regmap_db.at(uint8_t(addr));
+      possible_dev_addrs.push_back(uint8_t(addr));
+    } catch (std::out_of_range& e) {
+      continue;
+    }
+  }
+}
+
+bool Rackmon::probe(Modbus& iface, uint8_t addr) {
+  RegisterMap& rmap = regmap_db.at(addr);
+  std::vector<uint16_t> v(1);
+  try {
+    ReadHoldingRegistersReq req(addr, rmap.probe_register, v.size());
+    ReadHoldingRegistersResp resp(v);
+    iface.command(req, resp, rmap.default_baudrate, probe_timeout);
+    std::unique_lock lock(devices_mutex);
+    active_devices[addr] = std::make_unique<ModbusDevice>(iface, addr, rmap);
+    return true;
+  } catch (...) {
+    return false;
+  }
+}
+
+void Rackmon::probe(uint8_t addr) {
+  // We do not support the same address
+  // on multiple interfaces.
+  for (auto& iface : interfaces)
+    if (probe(*iface, addr))
+      break;
+}
+
+void Rackmon::mark_active(uint8_t addr) {
+  std::unique_lock lock(devices_mutex);
+  std::unique_ptr<ModbusDevice> dev = std::move(dormant_devices.at(addr));
+  dormant_devices.erase(addr);
+  active_devices[addr] = std::move(dev);
+}
+
+void Rackmon::mark_dormant(uint8_t addr) {
+  std::unique_lock lock(devices_mutex);
+  std::unique_ptr<ModbusDevice> dev = std::move(active_devices.at(addr));
+  active_devices.erase(addr);
+  dormant_devices[addr] = std::move(dev);
+}
+
+std::vector<uint8_t> Rackmon::inspect_dormant() {
+  time_t curr = std::time(0);
+  std::vector<uint8_t> ret{};
+  std::shared_lock lock(devices_mutex);
+  for (auto it = dormant_devices.begin(); it != dormant_devices.end();) {
+    // If its more than 300s since last activity, start probing it.
+    // change to something larger if required.
+    if ((it->second->last_active() + dormant_min_inactive_time) < curr) {
+      RegisterMap& rmap = regmap_db.at(it->first);
+      uint16_t probe = rmap.probe_register;
+      std::vector<uint16_t> v(1);
+      try {
+        uint8_t addr = it->first;
+        it->second->ReadHoldingRegisters(probe, v);
+        ret.push_back(addr);
+      } catch (...) {
+        it++;
+      }
+    }
+  }
+  return ret;
+}
+void Rackmon::recover_dormant() {
+  std::vector<uint8_t> candidates = inspect_dormant();
+  for (auto& addr : candidates) {
+    mark_active(addr);
+  }
+}
+
+std::vector<uint8_t> Rackmon::monitor_active() {
+  std::vector<uint8_t> ret{};
+  std::shared_lock lock(devices_mutex);
+  for (auto dev_it = active_devices.begin(); dev_it != active_devices.end();) {
+    try {
+      dev_it->second->monitor();
+    } catch (...) {
+      if (dev_it->second->is_flaky()) {
+        uint8_t addr = dev_it->first;
+        ret.push_back(addr);
+      }
+    }
+  }
+  return ret;
+}
+
+void Rackmon::monitor(void) {
+  while (!req_stop.load()) {
+    if (scan_active.load()) {
+      std::vector<uint8_t> dormant = monitor_active();
+      for (auto& addr : dormant) {
+        mark_dormant(addr);
+      }
+    }
+    last_monitor_time = std::time(0);
+    std::this_thread::sleep_for(2min);
+  }
+}
+
+void Rackmon::scan_all() {
+  for (auto& addr : possible_dev_addrs) {
+    if (active_devices.find(addr) != active_devices.end())
+      continue;
+    if (dormant_devices.find(addr) != dormant_devices.end())
+      continue;
+    try {
+      probe(addr);
+    } catch (...) {
+      continue;
+    }
+  }
+}
+
+void Rackmon::scan() {
+  auto it = possible_dev_addrs.begin();
+  // circular iterator
+  auto next_it = [this](auto& it) {
+    it++;
+    if (it == possible_dev_addrs.end()) {
+      it = possible_dev_addrs.begin();
+    }
+    return it;
+  };
+
+  while (!req_stop.load()) {
+    if (force_scan.load()) {
+      scan_all();
+      force_scan = false;
+      continue;
+    }
+
+    // Try and recover dormant devices
+    recover_dormant();
+
+    // Dont try to waste time scanning known
+    // active/dormant devices.
+    if (active_devices.find(*it) != active_devices.end() ||
+        dormant_devices.find(*it) != dormant_devices.end()) {
+      it = next_it(it);
+      continue;
+    }
+    probe(*it);
+    it = next_it(it);
+    last_scan_time = std::time(0);
+    std::this_thread::sleep_for(2min);
+  }
+}
+
+void Rackmon::start() {
+  if (threads.size() != 0)
+    throw std::runtime_error("Already running");
+  req_stop = false;
+  threads.push_back(std::thread(&Rackmon::scan, this));
+  threads.push_back(std::thread(&Rackmon::monitor, this));
+}
+
+void Rackmon::stop() {
+  req_stop = true;
+  while (threads.size()) {
+    threads.back().join();
+    threads.pop_back();
+  }
+}
+
+void Rackmon::rawCmd(Msg& req, Msg& resp, modbus_time timeout) {
+  uint8_t addr = req.addr;
+  active_devices.at(addr)->command(req, resp, timeout);
+}
+
+void Rackmon::get_monitor_data(std::vector<ModbusDeviceMonitorData>& ret) {
+  ret.clear();
+  std::shared_lock lock(devices_mutex);
+  std::transform(
+      active_devices.begin(),
+      active_devices.end(),
+      std::back_inserter(ret),
+      [](auto& kv) { return kv.second->get_monitor_data(); });
+}
+
+void Rackmon::get_monitor_status(RackmonStatus& ret) {
+  ret.started = threads.size() > 0;
+  ret.last_scan = last_scan_time;
+  ret.last_monitor = last_monitor_time;
+
+  std::shared_lock lock(devices_mutex);
+  ret.active_devices.clear();
+  ret.dormant_devices.clear();
+  std::transform(
+      active_devices.begin(),
+      active_devices.end(),
+      std::back_inserter(ret.active_devices),
+      [](auto& kv) { return kv.second->get_status(); });
+  std::transform(
+      dormant_devices.begin(),
+      dormant_devices.end(),
+      std::back_inserter(ret.dormant_devices),
+      [](auto& kv) { return kv.second->get_status(); });
+}
+
+void to_json(json& j, const RackmonStatus& m) {
+  j["running_status"] = m.started;
+  j["last_scan"] = m.last_scan;
+  j["active_devices"] = m.active_devices;
+  j["dormant_devices"] = m.dormant_devices;
+}
---
+++ common/recipes-core/rackmon2/rackmon/rackmon.hpp
@@ -0,0 +1,115 @@
+#pragma once
+#include <atomic>
+#include <shared_mutex>
+#include <thread>
+#include "modbus.hpp"
+#include "modbus_device.hpp"
+
+struct RackmonStatus {
+  bool started;
+  time_t last_scan;
+  time_t last_monitor;
+  std::vector<ModbusDeviceStatus> active_devices;
+  std::vector<ModbusDeviceStatus> dormant_devices;
+};
+void to_json(nlohmann::json& j, const RackmonStatus& m);
+
+class Rackmon {
+  static constexpr time_t dormant_min_inactive_time = 300;
+  static constexpr modbus_time probe_timeout = std::chrono::milliseconds(50);
+  std::vector<std::thread> threads{};
+  // Has to be before defining active or dormant devices
+  // to ensure users get destroyed before the interface.
+  std::vector<std::unique_ptr<Modbus>> interfaces{};
+  RegisterMapDatabase regmap_db{};
+
+  mutable std::shared_mutex devices_mutex{};
+
+  // These devices are reachable and are functioning
+  // properly.
+  std::map<uint8_t, std::unique_ptr<ModbusDevice>> active_devices{};
+
+  // These devices were active at one point. Now, they
+  // are either bad or removed. These will be scanned
+  // more regularly than the unknown addresses.
+  std::map<uint8_t, std::unique_ptr<ModbusDevice>> dormant_devices{};
+
+  // contains all the possible address allowed by currently
+  // loaded register maps. A majority of these are not expected
+  // to exist, but are candidates for a scan.
+  std::vector<uint8_t> possible_dev_addrs{};
+
+  // Request the scan/monitor threads to stop by setting to true.
+  std::atomic<bool> req_stop = false;
+  // Set to false to stop monitoring. Note, user might need to
+  // wait for ongoing scan/monitor cycle to complete.
+  std::atomic<bool> scan_active = true;
+
+  // As an optimization, devices are normally scanned one by one
+  // This allows someone to initiate a forced full scan.
+  // This mimicks a restart of rackmond.
+  std::atomic<bool> force_scan = true;
+
+  // Timestamps of last scan
+  time_t last_scan_time;
+  time_t last_monitor_time;
+
+  // Probe an interface for the presence of the address.
+  bool probe(Modbus& iface, uint8_t addr);
+  // Probe all interfaces for the presence of the address.
+  void probe(uint8_t addr);
+
+  // --------- Private Methods --------
+
+  // Marks an address as dormant
+  void mark_dormant(uint8_t addr);
+
+  // Marks an address as active
+  void mark_active(uint8_t addr);
+
+  // probe dormant devices and return recovered devices.
+  std::vector<uint8_t> inspect_dormant();
+  // Try and recover dormant devices.
+  void recover_dormant();
+
+  // Does a monitoring scan of all defined registers
+  // returns detected dormant devices.
+  std::vector<uint8_t> monitor_active();
+  // Monitor loop. Blocks forever as long as req_stop is true.
+  void monitor();
+
+  // Scan all possible devices. Skips active/dormant devices.
+  void scan_all();
+
+  // Scan loop. Blocks forever as long as req_stop is true.
+  void scan();
+
+ public:
+  // Load configuration, preferable before starting, but can be
+  // done at any time, but this is a one time only.
+  void load(const std::string& conf_path, const std::string& regmap_dir);
+
+  // Start the monitoring/scanning loops
+  void start();
+  // Stop the monitoring/scanning loops
+  void stop();
+
+  // Force rackmond to do a full scan on the next scan loop.
+  void force_scan_all() {
+    force_scan = true;
+  }
+
+  // Executes the Raw command. Throws an exception on error.
+  void rawCmd(Msg& req, Msg& resp, modbus_time timeout);
+
+  // Get monitored data
+  void get_monitor_data(std::vector<ModbusDeviceMonitorData>& ret);
+
+  // Get status of devices
+  void get_monitor_status(RackmonStatus& ret);
+
+  // TODO, this actually uses the formatting options in the regmap.
+  std::string getMonitorDataParsed() {
+    return "";
+  }
+};

eb5dc8528eb108c58e6d236a3da26f1469df2e79
Amithash Prasad <amithash@fb.com>
2021-11-12 16:11:47 -0800

    rackmond2: Add container to describe a modbus device
    Summary:
    A self contained data structure representing a modbus
    device on one of the connected UART interfaces.
    
    It is assumed that we can only communicate with devices
    with a defined register map. This also allows us to
    narrow the scans significantly.
    
    Test Plan: it all compiles
    
    Reviewed By: garnermic
    
    fbshipit-source-id: 0a9fceadca

---
+++ common/recipes-core/rackmon2/rackmon/modbus_device.cpp
@@ -0,0 +1,110 @@
+#include "modbus_device.hpp"
+#include <iomanip>
+#include <sstream>
+
+using nlohmann::json;
+
+ModbusDevice::ModbusDevice(Modbus& iface, uint8_t a, const RegisterMap& reg)
+    : interface(iface),
+      addr(a),
+      register_map(reg),
+      baudrate(reg.default_baudrate) {
+  mon_data.addr = a;
+  for (auto& it : reg.register_descriptors) {
+    mon_data.history.emplace_back(it.first, it.second.keep, it.second.length);
+  }
+}
+
+void ModbusDevice::command(
+    Msg& req,
+    Msg& resp,
+    modbus_time timeout,
+    modbus_time settle_time) {
+  try {
+    interface.command(req, resp, baudrate, timeout, settle_time);
+    num_consecutive_failures = 0;
+    last_active_time = std::time(0);
+  } catch (timeout_exception& e) {
+    mon_data.timeouts++;
+    num_consecutive_failures++;
+    throw;
+  } catch (crc_exception& e) {
+    mon_data.crc_failures++;
+    num_consecutive_failures++;
+    throw;
+  } catch (std::runtime_error& e) {
+    mon_data.misc_failures++;
+    num_consecutive_failures++;
+    std::cerr << e.what() << std::endl;
+    throw;
+  } catch (...) {
+    std::cerr << "Unknown exception" << std::endl;
+    mon_data.misc_failures++;
+    num_consecutive_failures++;
+    throw;
+  }
+}
+
+void ModbusDevice::ReadHoldingRegisters(
+    uint16_t register_offset,
+    std::vector<uint16_t>& regs) {
+  ReadHoldingRegistersReq req(addr, register_offset, regs.size());
+  ReadHoldingRegistersResp resp(regs);
+  command(req, resp);
+}
+
+void ModbusDevice::monitor() {
+  uint32_t timestamp = std::time(0);
+  std::unique_lock lk(history_mutex);
+  for (auto& h : mon_data.history) {
+    uint16_t reg = h.reg_addr;
+    auto& v = h.history[h.idx];
+    if (register_map.at(reg).changes_only) {
+      std::vector<uint16_t> value(v.value.size());
+      ReadHoldingRegisters(reg, value);
+      int prev_idx = h.idx == 0 ? h.history.size() - 1 : h.idx - 1;
+      if (h.history[prev_idx].timestamp == 0 ||
+          h.history[prev_idx].value != value) {
+        v.value = value;
+        v.timestamp = timestamp;
+        h.idx = (h.idx + 1) % h.history.size();
+      }
+    } else {
+      ReadHoldingRegisters(reg, v.value);
+      v.timestamp = timestamp;
+      h.idx = (h.idx + 1) % h.history.size();
+    }
+  }
+}
+
+void to_json(json& j, const RegisterValue& m) {
+  j["time"] = m.timestamp;
+  std::stringstream ss;
+  for (auto& d : m.value) {
+    uint8_t l = d & 0xff, h = (d >> 8) & 0xff;
+    ss << std::setfill('0') << std::setw(2) << std::right << std::hex << int(l);
+    ss << std::setfill('0') << std::setw(2) << std::right << std::hex << int(h);
+  }
+  j["data"] = ss.str();
+}
+
+void to_json(json& j, const RegisterValueHistory& m) {
+  j["begin"] = m.reg_addr;
+  j["readings"] = m.history;
+}
+
+void to_json(json& j, const ModbusDeviceStatus& m) {
+  j["addr"] = m.addr;
+  j["crc_fails"] = m.crc_failures;
+  j["timeouts"] = m.timeouts;
+  j["misc_fails"] = m.misc_failures;
+  j["last_active"] = m.last_active;
+  j["consecutive_failures"] = m.num_consecutive_failures;
+}
+
+void to_json(json& j, const ModbusDeviceMonitorData& m) {
+  const ModbusDeviceStatus& s = m;
+  to_json(j, s);
+  j["now"] = std::time(0);
+  j["ranges"] = m.history;
+}
---
+++ common/recipes-core/rackmon2/rackmon/modbus_device.hpp
@@ -0,0 +1,85 @@
+#include <nlohmann/json.hpp>
+#include <ctime>
+#include <iostream>
+#include "modbus.hpp"
+#include "modbus_cmds.hpp"
+#include "regmap.hpp"
+
+struct ModbusDeviceStatus {
+  uint8_t addr = 0;
+  uint32_t baudrate = 0;
+  uint32_t crc_failures = 0;
+  uint32_t timeouts = 0;
+  uint32_t misc_failures = 0;
+  time_t last_active = 0;
+  uint32_t num_consecutive_failures = 0;
+};
+void to_json(nlohmann::json& j, const ModbusDeviceStatus& m);
+
+struct RegisterValue {
+  uint32_t timestamp = 0;
+  std::vector<uint16_t> value;
+  explicit RegisterValue(uint16_t length) : value(length) {}
+};
+
+void to_json(nlohmann::json& j, const RegisterValue& m);
+
+struct RegisterValueHistory {
+  uint16_t reg_addr;
+  std::vector<RegisterValue> history;
+  int32_t idx = 0;
+
+ public:
+  RegisterValueHistory(uint16_t reg, uint16_t keep, uint16_t length)
+      : reg_addr(reg), history(keep, RegisterValue(length)) {}
+};
+void to_json(nlohmann::json& j, const RegisterValueHistory& m);
+
+struct ModbusDeviceMonitorData : public ModbusDeviceStatus {
+  std::vector<RegisterValueHistory> history{};
+};
+void to_json(nlohmann::json& j, const ModbusDeviceMonitorData& m);
+
+class ModbusDevice {
+  static constexpr uint32_t max_consecutive_failures = 10;
+  Modbus& interface;
+  uint8_t addr;
+  const RegisterMap& register_map;
+  uint32_t baudrate;
+  uint32_t num_consecutive_failures = 0;
+  time_t last_active_time = 0;
+  std::mutex history_mutex{};
+  ModbusDeviceMonitorData mon_data{};
+
+ public:
+  ModbusDevice(Modbus& iface, uint8_t a, const RegisterMap& reg);
+
+  void command(
+      Msg& req,
+      Msg& resp,
+      modbus_time timeout = modbus_time::zero(),
+      modbus_time settle_time = modbus_time::zero());
+
+  void ReadHoldingRegisters(
+      uint16_t register_offset,
+      std::vector<uint16_t>& regs);
+
+  void monitor();
+  bool is_flaky() const {
+    return num_consecutive_failures > max_consecutive_failures;
+  }
+  time_t last_active() const {
+    return last_active_time;
+  }
+  // Simple func, returns a copy of the monitor
+  // data.
+  ModbusDeviceMonitorData get_monitor_data() {
+    std::unique_lock lk(history_mutex);
+    // Makes a deep copy.
+    return mon_data;
+  }
+  ModbusDeviceStatus get_status() {
+    std::unique_lock lk(history_mutex);
+    return mon_data;
+  }
+};

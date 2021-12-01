6fe6bc4fa2b3b0cc524cdea1b878d985b6b1bd7e
Michael Garner <garnermic@fb.com>
2021-12-01 15:23:11 -0800

    =?UTF-8?q?[openbmc.quanta][PR]=20healthd:=20enable=20rebo?= =?UTF-8?q?ot=20log=20"caused=20by=20reboot=20command"=20for=20ast-g6=20pl?= =?UTF-8?q?at=E2=80=A6?= (#2744)
    Summary:
    …forms
    as title, revise /etc/rc6.d/setup-reboot.sh to support SOC_MODEL_ASPEED_G6
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2744
    
    Test Plan:
    Build and test pass on fby35
    
    root@bmc-oob:~# log-util all --print | grep Reboot
    0    all      2021-12-01 01:39:31    healthd          BMC Reboot detected - caused by reboot command
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: 1a9749e20e

--- common/recipes-utils/openbmc-utils/files/setup-reboot.sh
+++ common/recipes-utils/openbmc-utils/files/setup-reboot.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+soc_model=$(cat /etc/soc_model 2>/dev/null)
+sram_reboot="0x1e721204"
+if [ "$soc_model" = "SOC_MODEL_ASPEED_G6" ]; then
+    sram_reboot="0x10015c04"
+fi
+
 # flag for healthd logging reboot cause
-val=$(devmem 0x1e721204 32 2>/dev/null)
-devmem 0x1e721204 32 $((val | 0x101)) 2>/dev/null
+val=$(devmem "$sram_reboot" 32 2>/dev/null)
+devmem "$sram_reboot" 32 $((val | 0x101)) 2>/dev/null

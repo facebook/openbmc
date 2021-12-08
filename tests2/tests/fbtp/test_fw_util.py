5ec3324c273f0b85fbf263f906bff6555b8a4152
David Cheng <davcheng@fb.com>
2021-12-07 18:23:03 -0800

    exclude(skip) test cases not available in qemu
    Summary: skip tests when CIT running inside QEMU
    
    Test Plan: Sandcastle RUN: https://www.internalfb.com/intern/sandcastle/job/27021598296139334/
    
    Reviewed By: binhuang00
    
    fbshipit-source-id: f4a1890dda

--- tests2/tests/fbtp/test_fw_util.py
+++ tests2/tests/fbtp/test_fw_util.py
@@ -20,10 +20,12 @@
 import unittest
 
 from common.base_fw_util_test import CommonFwUtilTest
+from utils.test_utils import qemu_check
 
 PLATFORM = "fbtp"
 
 
+@unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
 class FwUtilVersionTest(CommonFwUtilTest, unittest.TestCase):
     def set_platform(self):
         self.platform = PLATFORM

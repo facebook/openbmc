236f0462e43102aeb2b33546ff2ebb8810f314eb
Amithash Prasad <amithash@fb.com>
2022-01-06 13:29:57 -0800

    rackmon2: Fix potential compiler warnings
    Summary:
    This fixes unused compiler warnings when including log.hpp
    and not actually logging in the source.
    
    Test Plan: Build on rocko where this was observed.
    
    Reviewed By: garnermic
    
    fbshipit-source-id: ec12fb9666

--- common/recipes-core/rackmon2/rackmon/log.hpp
+++ common/recipes-core/rackmon2/rackmon/log.hpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #ifdef RACKMON_SYSLOG
 #include <openbmc/syslog.hpp>
-static thread_local std::ostream& log_error = openbmc::syslog_error;
-static thread_local std::ostream& log_warn = openbmc::syslog_warn;
-static thread_local std::ostream& log_info = openbmc::syslog_notice;
+[[maybe_unused]] static thread_local std::ostream& log_error = openbmc::syslog_error;
+[[maybe_unused]] static thread_local std::ostream& log_warn = openbmc::syslog_warn;
+[[maybe_unused]] static thread_local std::ostream& log_info = openbmc::syslog_notice;
 #else
-static thread_local std::ostream& log_error = std::cerr;
-static thread_local std::ostream& log_info = std::cout;
-static thread_local std::ostream& log_warn = std::cout;
+[[maybe_unused]] static thread_local std::ostream& log_error = std::cerr;
+[[maybe_unused]] static thread_local std::ostream& log_info = std::cout;
+[[maybe_unused]] static thread_local std::ostream& log_warn = std::cout;
 #endif
 
 #ifdef PROFILING

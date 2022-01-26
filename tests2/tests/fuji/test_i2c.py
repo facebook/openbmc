592cb45b34d2e392249cdbf054bc6e174ec1093e
Mike Choi <mikechoi@fb.com>
2022-01-26 13:35:40 -0800

    Fuji : CIT : aligned with "lm75" driver multi-devices name (#1304)
    Summary:
    Test result shows in some fuji platform machine, lm75 driver sometimes result in tmp75 device name. so need to change test name to align with such devices.
    
    TestLog:
    ```
    root@bmc:/sys/bus/i2c/devices/17-004c# ls -al driver
    lrwxrwxrwx    1 root     root             0 Jan  7 18:42 driver -> ../../../../../../../../../bus/i2c/drivers/lm75
    root@bmc:/sys/bus/i2c/devices/17-004c# cat name
    tmp75
    root@bmc:/sys/bus/i2c/devices/17-004c# python3 /usr/local/bin/tests2/cit_runner.py --platform fuji -r tests.fuji.test_i2c.I2cTest
    test_i2c_tree (tests.fuji.test_i2c.I2cTest)
    check if i2c devices are created and drivers are binded properly. ...
    ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.355s
    
    OK
    root@bmc:/sys/bus/i2c/devices/17-004c# cat /etc/issue
    OpenBMC Release fuji-v4.20
    
    root@bmc:/sys/bus/i2c/devices/17-004c# source /usr/local/bin/openbmc-utils.sh
    root@bmc:/sys/bus/i2c/devices/17-004c# wedge_board_ver
    BOARD_FUJI_DVT1
    root@bmc:/sys/bus/i2c/devices/17-004c#
    
    ```
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/1304
    
    Reviewed By: garnermic
    
    fbshipit-source-id: e01be5c7ce

--- tests2/tests/fuji/test_i2c.py
+++ tests2/tests/fuji/test_i2c.py
@@ -20,11 +20,25 @@
 import unittest
 
 from common.base_i2c_test import BaseI2cTest
-from tests.fuji.test_data.i2c.i2c import plat_i2c_tree
+from tests.fuji.test_data.i2c.i2c import plat_i2c_tree, device_lm75
 from utils.test_utils import qemu_check
+from utils.i2c_utils import I2cSysfsUtils
 
 
 @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
 class I2cTest(BaseI2cTest, unittest.TestCase):
+    def i2c_tree_update_lm75(self,i2c_path):
+        name = I2cSysfsUtils.i2c_device_get_name(i2c_path)
+        if name=="tmp75":
+            self.i2c_tree.update({i2c_path:device_lm75["tmp75"]})
+        elif name=="lm75":
+            self.i2c_tree.update({i2c_path:device_lm75["lm75"]})
+
+    def i2c_tree_update(self):
+        for i2c_path, i2c_info in plat_i2c_tree.items():
+            if i2c_info["driver"]=="lm75":
+                self.i2c_tree_update_lm75(i2c_path)
+
     def load_golden_i2c_tree(self):
         self.i2c_tree = plat_i2c_tree
+        self.i2c_tree_update()

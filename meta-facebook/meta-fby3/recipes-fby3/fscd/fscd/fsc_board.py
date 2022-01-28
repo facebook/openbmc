9f365331e19033d94b1fe144939c782db7e41c9a
Peter Fetros <goldenbug@fb.com>
2022-01-28 11:47:48 -0800

    fby3: fix fscd does not work (#2810)
    Summary:
    fix fscd does not work
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/2810
    
    Test Plan:
    Before (Fan Mode: Unknown):
    root@bmc-oob:~# fan-util --get
    Fan 0 Speed: 7788 RPM (70%)
    Fan 1 Speed: 6797 RPM (70%)
    Fan 2 Speed: 7805 RPM (70%)
    Fan 3 Speed: 6843 RPM (70%)
    Fan 4 Speed: 7723 RPM (70%)
    Fan 5 Speed: 6816 RPM (70%)
    Fan 6 Speed: 7695 RPM (70%)
    Fan 7 Speed: 6861 RPM (70%)
    Fan Mode: Unknown
    FSCD Driver: N/A
    Sensor Fail: None
    Fan Fail: None
    
    After: (Fan Mode: Normal):
    root@bmc-oob:~# fan-util --get
    Fan 0 Speed: 1277 RPM (12%)
    Fan 1 Speed: 1103 RPM (12%)
    Fan 2 Speed: 1314 RPM (12%)
    Fan 3 Speed: 1071 RPM (12%)
    Fan 4 Speed: 1263 RPM (12%)
    Fan 5 Speed: 1075 RPM (12%)
    Fan 6 Speed: 1267 RPM (12%)
    Fan 7 Speed: 1109 RPM (12%)
    Fan Mode: Normal
    FSCD Driver: linear_frontIO_inlet(all:slot1_front_io_temp)
    Sensor Fail: None
    Fan Fail: None
    
    Reviewed By: ningrassia
    
    fbshipit-source-id: e0c623130c

--- meta-facebook/meta-fby3/recipes-fby3/fscd/fscd/fsc_board.py
+++ meta-facebook/meta-fby3/recipes-fby3/fscd/fscd/fsc_board.py
@@ -37,7 +37,7 @@ try:
     Logger.warn("sled_system_conf: %s" % (sled_system_conf))
 
     # Add sensor fail scenario for DP case
-    if (search(r"Type_DP", sled_system_conf) is not None):
+    if search(r"Type_DP", sled_system_conf) is not None:
         Logger.warn("Add sensor fail scenario for DP case")
         get_fan_mode_scenario_list = [
             "one_fan_failure",
@@ -142,7 +142,7 @@ def set_all_pwm(boost):
 
 def is_valid_dp_pcie(pcie_info_key):
     try:
-        pcie_info_raw = kv.kv_get(pcie_info_key, flags=kv.FPERSIST, binary=True)
+        pcie_info_raw = kv_get(pcie_info_key, flags=kv.FPERSIST, binary=True)
         pcie_info_data = struct.unpack_from("<HH", pcie_info_raw, 2)
         if pcie_info_data in valid_dp_pcie_list:
             return 1
@@ -195,9 +195,9 @@ def sensor_valid_check(board, sname, check_name, attribute):
                 return 1
 
             # If sensor fail, dp will boost without checking host ready
-            if (search(r"Type_DP", sled_system_conf) is None):
+            if search(r"Type_DP", sled_system_conf) is None:
                 try:
-                    flag_status = kv.kv_get(host_ready_map[board], FPERSIST)
+                    flag_status = kv_get(host_ready_map[board])
                 except kv.KeyOperationFailure:
                     return 0
 
@@ -238,7 +238,8 @@ def sensor_valid_check(board, sname, check_name, attribute):
                         dev_id,
                         byref(nvme_ready),
                         byref(dev_status),
-                        byref(dev_type))
+                        byref(dev_type),
+                    )
                     if response == 0:  # bic can get device power status
                         if dev_status.value == 1:  # device power on
                             return 1
@@ -257,8 +258,9 @@ def sensor_valid_check(board, sname, check_name, attribute):
                         dev_id,
                         byref(nvme_ready),
                         byref(dev_status),
-                        byref(dev_type))
-                   if response == 0:  # bic can get device power status
+                        byref(dev_type),
+                    )
+                    if response == 0:  # bic can get device power status
                         if dev_status.value == 1:  # device power on
                             return 1
                         else:
@@ -290,7 +292,7 @@ def sensor_valid_check(board, sname, check_name, attribute):
                 if status.value == 1:  # power on
                     if search(r"Type_DP", sled_system_conf) is None:
                         try:
-                            flag_status = kv.kv_get(host_ready_map[board], FPERSIST))
+                            flag_status = kv_get(host_ready_map[board])
                         except kv.KeyOperationFailure:
                             return 0
 

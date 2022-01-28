df1c1708805043f7683614eff4d61601f6a1fae3
Michael Garner <garnermic@fb.com>
2022-01-27 17:50:54 -0800

    fby35: correct the sensor name BMC_SENSOR_PDB_CL_VDELTA (#2022)
    Summary:
    Use Crater Lake abbreviation "CL" for sensor name instead of "DL".
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/2022
    
    Test Plan:
    Build and test pass on fby35 system.
    1. Check sensor-util
    root@bmc-oob:~# sensor-util bmc 0xcc
    BMC_SENSOR_PDB_CL_VDELTA     (0xCC) :    0.05 Volts | (ok)
    
    Reviewed By: zhdaniel12
    
    fbshipit-source-id: 1de41ed08b

--- meta-facebook/meta-fby35/recipes-fby35/plat-libs/files/pal/pal_sensors.c
+++ meta-facebook/meta-fby35/recipes-fby35/plat-libs/files/pal/pal_sensors.c
@@ -57,7 +57,7 @@ static int read_cached_val(uint8_t snr_number, float *value);
 static int read_fan_speed(uint8_t snr_number, float *value);
 static int read_fan_pwm(uint8_t pwm_id, float *value);
 static int read_curr_leakage(uint8_t snr_number, float *value);
-static int read_pdb_dl_vdelta(uint8_t snr_number, float *value);
+static int read_pdb_cl_vdelta(uint8_t snr_number, float *value);
 
 static int pal_sdr_init(uint8_t fru);
 static sensor_info_t g_sinfo[MAX_NUM_FRUS][MAX_SENSOR_NUM + 1] = {0};
@@ -110,7 +110,7 @@ const uint8_t bmc_sensor_list[] = {
   BMC_SENSOR_MEDUSA_CURR,
   BMC_SENSOR_MEDUSA_PWR,
   BMC_SENSOR_MEDUSA_VDELTA,
-  BMC_SENSOR_PDB_DL_VDELTA,
+  BMC_SENSOR_PDB_CL_VDELTA,
   BMC_SENSOR_PDB_BB_VDELTA,
   BMC_SENSOR_CURR_LEAKAGE,
   BMC_SENSOR_FAN_IOUT,
@@ -842,7 +842,7 @@ PAL_SENSOR_MAP sensor_map[] = {
   {"BMC_SENSOR_HSC_PEAK_PIN" , HSC_ID0, read_hsc_peak_pin ,    0, {      0,     0,       0,      0,     0,     0, 0, 0}, POWER}, //0xC9
   {"BMC_SENSOR_FAN_PWR"      ,    0xCA, read_cached_val   , true, {201.465,     0,  544.88,      0,     0,     0, 0, 0}, POWER}, //0xCA
   {"BMC_SENSOR_HSC_EIN"      , HSC_ID0, read_hsc_ein      , true, {  287.5,     0,  398.75,      0,     0,     0, 0, 0}, POWER}, //0xCB
-  {"BMC_SENSOR_PDB_DL_VDELTA",    0xCC, read_pdb_dl_vdelta, true, {    0.9,     0,       0,      0,     0,     0, 0, 0}, VOLT}, //0xCC
+  {"BMC_SENSOR_PDB_CL_VDELTA",    0xCC, read_pdb_cl_vdelta, true, {    0.9,     0,       0,      0,     0,     0, 0, 0}, VOLT}, //0xCC
   {"BMC_SENSOR_CURR_LEAKAGE" ,    0xCD, read_curr_leakage , true, {      0,     0,       0,      0,     0,     0, 0, 0}, PERCENT}, //0xCD
   {"BMC_SENSOR_PDB_BB_VDELTA",    0xCE, read_cached_val   , true, {    0.8,     0,       0,      0,     0,     0, 0, 0}, VOLT}, //0xCE
   {"BMC_SENSOR_MEDUSA_VDELTA",    0xCF, read_medusa_val   , true, {    0.5,     0,       0,      0,     0,     0, 0, 0}, VOLT}, //0xCF
@@ -1449,9 +1449,9 @@ read_snr_from_all_slots(uint8_t target_snr_num, uint8_t action, float *val) {
   return PAL_EOK;
 }
 
-// Calculate Deltalake vdelta
+// Calculate Crater lake vdelta
 static int
-read_pdb_dl_vdelta(uint8_t snr_number, float *value) {
+read_pdb_cl_vdelta(uint8_t snr_number, float *value) {
   float medusa_vout = 0;
   float min_hsc_vin = 0;
 

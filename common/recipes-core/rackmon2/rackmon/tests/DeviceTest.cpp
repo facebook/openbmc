b786fb9808e98eef4191010386080ee3e8ca83ba
Amithash Prasad <amithash@fb.com>
2022-02-01 10:17:22 -0800

    rackmon2: DeviceTest to use temp directory instead of current
    Summary:
    When running in special test environments, CWD might not
    be writable, thus causing test failures. Use temp directory
    instead
    
    Test Plan: All unit tests pass (Both fbcode and openbmc)
    
    Reviewed By: garnermic, GoldenBug
    
    fbshipit-source-id: 0478f17d50

--- common/recipes-core/rackmon2/rackmon/tests/DeviceTest.cpp
+++ common/recipes-core/rackmon2/rackmon/tests/DeviceTest.cpp
@@ -6,32 +6,48 @@
 #include <thread>
 #include "Device.hpp"
 
+#if (__GNUC__ < 8)
+#include <experimental/filesystem>
+namespace std {
+namespace filesystem = experimental::filesystem;
+}
+#else
+#include <filesystem>
+#endif
+
 using namespace std::literals;
 using namespace rackmon;
 
 class DeviceTest : public ::testing::Test {
+ public:
+  std::string test_device_path{};
  protected:
   void SetUp() override {
+    std::filesystem::path test_path = std::filesystem::temp_directory_path();
+    if (!std::filesystem::exists(test_path)) {
+      test_path = std::filesystem::current_path();
+    }
+    test_device_path = test_path / "test.bin";
     std::vector<uint8_t> out = {1, 2, 3, 4};
-    std::ofstream fp("./test.bin", std::ios::binary);
+    std::ofstream fp(test_device_path, std::ios::binary);
     std::copy(out.begin(), out.end(), std::ostreambuf_iterator<char>(fp));
     fp.close();
   }
   void TearDown() override {
-    remove("./test.bin");
+    remove(test_device_path.c_str());
   }
 };
 
 TEST_F(DeviceTest, OpenClose) {
   Device dev_bad("./test2.bin");
   EXPECT_THROW(dev_bad.open(), std::system_error);
-  Device dev("./test.bin");
+  Device dev(test_device_path);
   dev.open();
   dev.close();
 }
 
 TEST_F(DeviceTest, ReadTest) {
-  Device dev("./test.bin");
+  Device dev(test_device_path);
   dev.open();
   std::vector<uint8_t> buf(4);
   dev.read(buf.data(), buf.size(), 10);
@@ -40,7 +56,7 @@ TEST_F(DeviceTest, ReadTest) {
 }
 
 TEST_F(DeviceTest, WriteTest) {
-  Device dev("./test.bin");
+  Device dev(test_device_path);
   dev.open();
   std::vector<uint8_t> wbuf = {5, 6, 7, 8};
   dev.write(wbuf.data(), wbuf.size());

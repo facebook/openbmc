222cb8adbdb0f9ea8fa7d899eb2afab61dab6d96
Michael Garner <garnermic@fb.com>
2022-03-02 16:28:43 -0800

    Remove direct access to kv store for setup-dev.sh
    Summary:
    Direct access to the kv data store should only be done via the kv commands
      or associated APIs.  This change removes a call to directly write the
      bb_hsc_conf with use of the kv set command to set the chip value in
      the persistent kv data store.
    
    Test Plan:
    1. Verify /mnt/data/kv_store/bb_hsc_conf file exists
    2. Remove the file
    3. Run the script (expectation is commands will fail as devices have already been defined)
    4. Verify file was correctly created in the persistent data store
    5. Dump file contents to ensure it match 'ltc4282' as expected.
    
    root@bmc-oob:/etc/init.d# ls /mnt/data/kv_store/
    bb_hsc_conf             ntp_server              sled_system_conf        slot1_sensor_health     slot2_sel_error         slot3_sel_error         slot4_por_cfg           sysfw_ver_slot1         vboot_error
    fru1_restart_cause      pwr_server1_last_state  slot1_boot_order        slot1_system_conf       slot2_sensor_health     slot3_sensor_health     slot4_sel_error         sysfw_ver_slot2
    fru2_restart_cause      pwr_server2_last_state  slot1_cpu_ppin          slot2_boot_order        slot3_boot_order        slot3_system_conf       slot4_sensor_health     sysfw_ver_slot3
    fru3_restart_cause      pwr_server3_last_state  slot1_por_cfg           slot2_cpu_ppin          slot3_cpu_ppin          slot4_boot_order        slot4_system_conf       sysfw_ver_slot4
    fru4_restart_cause      pwr_server4_last_state  slot1_sel_error         slot2_por_cfg           slot3_por_cfg           slot4_cpu_ppin          sys_config              timestamp_sled
    root@bmc-oob:/etc/init.d# rm /mnt/data/kv_store/bb_hsc_conf
    root@bmc-oob:/etc/init.d# ./setup-dev.sh
    ./setup-dev.sh: line 27: echo: write error: Device or resource busy
    ./setup-dev.sh: line 27: echo: write error: Device or resource busy
    Setup devs for fby35..../setup-dev.sh: line 27: echo: write error: Device or resource busy
    ./setup-dev.sh: line 27: echo: write error: Device or resource busy
    ./setup-dev.sh: line 27: echo: write error: Device or resource busy
    ./setup-dev.sh: line 27: echo: write error: Device or resource busy
    ./setup-dev.sh: line 27: echo: write error: Device or resource busy
    Done.
    root@bmc-oob:/etc/init.d# ls /mnt/data/kv_store/
    bb_hsc_conf             ntp_server              sled_system_conf        slot1_sensor_health     slot2_sel_error         slot3_sel_error         slot4_por_cfg           sysfw_ver_slot1         vboot_error
    fru1_restart_cause      pwr_server1_last_state  slot1_boot_order        slot1_system_conf       slot2_sensor_health     slot3_sensor_health     slot4_sel_error         sysfw_ver_slot2
    fru2_restart_cause      pwr_server2_last_state  slot1_cpu_ppin          slot2_boot_order        slot3_boot_order        slot3_system_conf       slot4_sensor_health     sysfw_ver_slot3
    fru3_restart_cause      pwr_server3_last_state  slot1_por_cfg           slot2_cpu_ppin          slot3_cpu_ppin          slot4_boot_order        slot4_system_conf       sysfw_ver_slot4
    fru4_restart_cause      pwr_server4_last_state  slot1_sel_error         slot2_por_cfg           slot3_por_cfg           slot4_cpu_ppin          sys_config              timestamp_sled
    root@bmc-oob:/etc/init.d# cat /mnt/data/kv_store/bb_hsc_conf
    ltc4282root@bmc-oob:/etc/init.d#
    
    Reviewed By: peterdelevoryas
    
    fbshipit-source-id: 6b5aa4704c

--- meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/setup-dev.sh
+++ meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/setup-dev.sh
@@ -55,7 +55,7 @@ function init_class1_dev() {
   if [ ! -d /mnt/data/kv_store ]; then
     /bin/mkdir -p /mnt/data/kv_store
   fi
-  echo -n $chip > /mnt/data/kv_store/bb_hsc_conf
+  kv set bb_hsc_conf $chip persistent
 
   # check nic power to see if it's need to be set to standby mode
   set_nic_power

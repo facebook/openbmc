913ce3eb4d6f314f68630a89659df1b4377b6b90
charlene.yang <CHARLENE.YANG@deltaww.com>
2022-03-04 13:45:11 +0800

    Delta : Revise agc032a-psu.c & agc032a-psu.h psu-util to show correct fru info & command

--- meta-delta/meta-agc032a/recipes-utils/rest-api/files/board_setup_routes.py
+++ meta-delta/meta-agc032a/recipes-utils/rest-api/files/board_setup_routes.py
@@ -23,19 +23,11 @@ import os
 from aiohttp.web import Application
 from compute_rest_shim import RestShim
 from node_fruid import get_node_fruid
-from node_logs import get_node_logs
-from node_sensors import get_node_sensors
 from rest_pal_legacy import pal_is_fru_prsnt, pal_get_num_slots
 
 # Initialize Platform specific Resource Tree
 def setup_board_routes(app: Application, write_enabled: bool):
 
-    ## /api/sensors end point
-    ## sensors_shim = RestShim(get_node_sensors("smb"), "/api/sensors")
-    ## app.router.add_get(sensors_shim.path, sensors_shim.get_handler)
-    ## app.router.add_post(sensors_shim.path, sensors_shim.post_handler)
-
-    ## Add /api/spb/fruid end point
-    ## fruid_shim = RestShim(get_node_fruid("all"), "/api/fruid")
-    ##  app.router.add_get(fruid_shim.path, fruid_shim.get_handler)
-    pass
+    # Add /api/fruid end point
+    fruid_shim = RestShim(get_node_fruid("all"), "/api/fruid")
+    app.router.add_get(fruid_shim.path, fruid_shim.get_handler)

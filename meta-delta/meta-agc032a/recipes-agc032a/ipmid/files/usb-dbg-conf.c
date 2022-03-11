a2e182ccbc98bb7ce94532e8f00ed96d4ece44ca
charlene.yang <CHARLENE.YANG@deltaww.com>
2022-03-11 12:14:50 +0800

    Delta : Rename gpio_desc_t to dbg_gpio_desc_t

--- meta-delta/meta-agc032a/recipes-agc032a/ipmid/files/usb-dbg-conf.c
+++ meta-delta/meta-agc032a/recipes-agc032a/ipmid/files/usb-dbg-conf.c
@@ -357,7 +357,7 @@ static post_desc_t pdesc_phase2[] = {
 
 };
 
-static gpio_desc_t gdesc[] = {
+static dbg_gpio_desc_t gdesc[] = {
   { 0x10, 0, 2, "DBG_RST_BTN_N" },
   { 0x11, 0, 1, "DBG_PWR_BTN_N" },
   { 0x12, 0, 0, "DBG_GPIO_BMC2" },
@@ -401,7 +401,7 @@ int plat_get_post_phase(uint8_t fru, post_phase_desc_t **desc, size_t *desc_coun
   return 0;
 }
 
-int plat_get_gdesc(uint8_t fru, gpio_desc_t **desc, size_t *desc_count)
+int plat_get_gdesc(uint8_t fru, dbg_gpio_desc_t **desc, size_t *desc_count)
 {
   if (!desc || !desc_count) {
     return -1;

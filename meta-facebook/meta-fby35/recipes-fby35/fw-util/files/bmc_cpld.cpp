027b3705d8b9cd92967112cf2d0752e37c6663a0
Michael Garner <garnermic@fb.com>
2022-03-17 11:40:16 -0700

    fby35: support bb bic recovery (#2069)
    Summary:
    Support BB BIC recovery and adjust the command format.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2069
    
    Test Plan:
    Build and test pass on fby35 system.
    1. BB BIC recovery
    root@bmc-oob:~# recovery-bic-util slot1 -f bb uart_Y35BBB_2099.bin Y35BBB_2022.bin
    Setting slot UART to SB BIC
    Could not get the server board index, please enter the slot number manually (1 or 3)
    1
    Setting BIC boot from UART
    Doing the recovery update...
    Executing the fw-util for updating bic...
    slot_id: 1, comp: d, intf: 0, img: Y35BBB_2022.bin, force: 1
    Set fan mode to manual and set PWM to 70%
    file size = 260312 bytes, slot = 1, intf = 0x10
    updating fw on slot 1:
    updated bic: 100 %
    Elapsed time:  28   sec.
    
    Set fan mode to auto and start fscd
    Force upgrade of slot1 : bb_bic succeeded
    Power-cycling the server...
    root@bmc-oob:~# fw-util slot1 --version bb_bic
    BB Bridge-IC Version: oby35-bb-v2022.01.01
    
    Reviewed By: GoldenBug
    
    fbshipit-source-id: 2fc1dd25d8096db73d86b8814a5b45c306152e87

--- meta-facebook/meta-fby35/recipes-fby35/fw-util/files/bmc_cpld.cpp
+++ meta-facebook/meta-fby35/recipes-fby35/fw-util/files/bmc_cpld.cpp
@@ -1,24 +1,17 @@
 #include <cstdio>
 #include <syslog.h>
-#include <sys/stat.h>
-#include <openbmc/pal.h>
-#include <openbmc/obmc-i2c.h>
+#include <unistd.h>
 #include <openbmc/kv.h>
-#include "bmc_cpld.h"
-#include "server.h"
+#include <openbmc/pal.h>
 #include <facebook/bic.h>
+#include "bmc_cpld.h"
 
 using namespace std;
 #define JBC_FILE_NAME ".jbc"
 #define MAX10_RPD_SIZE 0x23000
 
 image_info BmcCpldComponent::check_image(const string& image, bool force) {
-  string fru_name = fru();
-  size_t bmc_found = fru_name.find("bmc");
-  uint8_t slot_id = 0;
-  uint8_t board_type_index = 0;
-  struct stat file_info;
-  uint8_t fw_comp = 0;
+  uint8_t board_rev = 0;
   image_info image_sts = {"", false, false};
   uint8_t bmc_location = 0;
 
@@ -32,139 +25,30 @@ image_info BmcCpldComponent::check_image(const string& image, bool force) {
     return image_sts;
   }
 
-  if (stat(image.c_str(), &file_info) < 0) {
-    cerr << "Cannot check " << image << " file information" << endl;
+  if (force == true) {
+    image_sts.result = true;
+  }
+
+  if (get_board_rev(0, BOARD_ID_BB, &board_rev) < 0) {
+    cerr << "Failed to get board revision ID" << endl;
     return image_sts;
   }
 
-  if (file_info.st_size == MAX10_RPD_SIZE + IMG_POSTFIX_SIZE)
+  if (fby35_common_is_valid_img(image.c_str(), FW_BB_CPLD, board_rev) == true) {
+    image_sts.result = true;
     image_sts.sign = true;
-
-  if (force == false) {
-    if (image_sts.sign != true) {
-      cerr << "Image " << image << " is not a signed image, please use --force option" << endl;
-      return image_sts;
-    }
-
-    // Read Board Revision from CPLD
-    if (bmc_found != string::npos) {
-      if(bmc_location == NIC_BMC){
-        if (get_board_rev(0, BOARD_ID_NIC_EXP, &board_type_index) < 0) {
-          cerr << "Failed to get board revision ID" << endl;
-          return image_sts;
-        }
-      } else{
-        if (get_board_rev(0, BOARD_ID_BB, &board_type_index) < 0) {
-          cerr << "Failed to get board revision ID" << endl;
-          return image_sts;
-        }
-      }
-      fw_comp = FW_BB_CPLD;
-    } else {
-      slot_id = fru_name.at(4) - '0';
-      if (get_board_rev(slot_id, BOARD_ID_SB, &board_type_index) < 0) {
-        cerr << "Failed to get board revision ID" << endl;
-        return image_sts;
-      }
-      fw_comp = FW_CPLD;
-    }
-
-    if (fby35_common_is_valid_img(image.c_str(), fw_comp, board_type_index) == false) {
-      return image_sts;
-    }
-    image_sts.result = true;
-  } else {
-    image_sts.result = true;
   }
 
   return image_sts;
 }
 
-int BmcCpldComponent::get_ver_str(string& s) {
-  int ret = 0;
-  char ver[32] = {0};
-  uint8_t rbuf[4] = {0};
-
-  ret = pal_get_cpld_ver(FRU_BMC, rbuf);
-  if (!ret) {
-    snprintf(ver, sizeof(ver), "%02X%02X%02X%02X", rbuf[3], rbuf[2], rbuf[1], rbuf[0]);
-    s = string(ver);
-  }
-
-  return ret;
-}
-
-int BmcCpldComponent::print_version()
-{
-  string ver("");
-  string fru_name = fru();
-  char ver_key[MAX_KEY_LEN] = {0};
-  char value[MAX_VALUE_LEN] = {0};
-  size_t slot_found = fru_name.find("slot");
-  int ret = 0;
-
-  try {
-    // Print CPLD Version
-    if (slot_found != string::npos) {
-      uint8_t slot_id = fru_name.at(4) - '0';
-      Server server(slot_id, fru_name);
-      fru_name = "SB";
-      server.ready();
-    } else {
-     transform(fru_name.begin(), fru_name.end(), fru_name.begin(), ::toupper);
-    }
-    if ( get_ver_str(ver) < 0 ) {
-      throw "Error in getting the version of " + fru_name;
-    }
-    cout << fru_name << " CPLD Version: " << ver << endl;
-    snprintf(ver_key, sizeof(ver_key), FRU_STR_CPLD_NEW_VER_KEY, fru().c_str());
-    ret = kv_get(ver_key, value, NULL, 0);
-    if ((ret < 0) && (errno == ENOENT)) { // no update before
-      cout << fru_name << " CPLD Version After activation: " << ver << endl;
-    } else if (ret == 0) {
-      cout << fru_name << " CPLD Version After activation: " << value << endl;
-    }
-  } catch(string& err) {
-    printf("%s CPLD Version: NA (%s)\n", fru_name.c_str(), err.c_str());
-  }
-  return 0;
-}
-
-void BmcCpldComponent::get_version(json& j) {
-  string ver("");
-  string fru_name = fru();
-  size_t slot_found = fru_name.find("slot");
-  try {
-    if (slot_found != string::npos) {
-      uint8_t slot_id = fru_name.at(4) - '0';
-      Server server(slot_id, fru_name);
-      fru_name = "SB";
-      server.ready();
-    }
-
-    if ( get_ver_str(ver) < 0 ) {
-      throw "Error in getting the version of " + fru_name;
-    }
-    j["VERSION"] = ver;
-  } catch(string& err) {
-    if ( err.find("empty") != string::npos ) j["VERSION"] = "not_present";
-    else j["VERSION"] = "error_returned";
-  }
-}
-
-int BmcCpldComponent::update_cpld(const string& image, bool force)
-{
+int BmcCpldComponent::update_cpld(const string& image, bool force) {
   int ret = FW_STATUS_FAILURE;
   char ver_key[MAX_KEY_LEN] = {0};
   char ver[16] = {0};
   uint8_t bmc_location = 0;
   string bmc_location_str;
   image_info image_sts = check_image(image, force);
-
-  if (image_sts.result == false) {
-    syslog(LOG_CRIT, "Update %s CPLD Fail. File: %s is not a valid image", fru().c_str(), image.c_str());
-    return FW_STATUS_FAILURE;
-  }
 
   if (fby35_common_get_bmc_location(&bmc_location) < 0) {
     printf("Failed to initialize the fw-util\n");
@@ -175,6 +59,12 @@ int BmcCpldComponent::update_cpld(const string& image, bool force)
     bmc_location_str = "NIC Expansion";
   } else {
     bmc_location_str = "Baseboard";
+  }
+
+  if (image_sts.result == false) {
+    syslog(LOG_CRIT, "Update CPLD on %s Fail. File: %s is not a valid image",
+           bmc_location_str.c_str(), image.c_str());
+    return FW_STATUS_FAILURE;
   }
 
   syslog(LOG_CRIT, "Updating CPLD on %s. File: %s", bmc_location_str.c_str(), image.c_str());
@@ -208,7 +98,10 @@ int BmcCpldComponent::update_cpld(const string& image, bool force)
       int rc = read(fd_r, memblock + r_b, MAX10_RPD_SIZE - r_b);
       if (rc > 0) {
         r_b += rc;
-      } else if (!rc || (rc < 0 && errno != EINTR)) {
+      } else {
+        if (rc < 0 && errno == EINTR) {
+          continue;
+        }
         break;
       }
     }
@@ -216,7 +109,10 @@ int BmcCpldComponent::update_cpld(const string& image, bool force)
       int rc = write(fd_w, memblock + w_b, r_b - w_b);
       if (rc > 0) {
         w_b += rc;
-      } else if (!rc || (rc < 0 && errno != EINTR)) {
+      } else {
+        if (rc < 0 && errno == EINTR) {
+          continue;
+        }
         break;
       }
     }
@@ -245,19 +141,21 @@ int BmcCpldComponent::update_cpld(const string& image, bool force)
   }
 
   snprintf(ver_key, sizeof(ver_key), FRU_STR_CPLD_NEW_VER_KEY, fru().c_str());
-  if (fby35_common_get_img_ver(image.c_str(), ver, FW_BB_CPLD) < 0) {
-     kv_set(ver_key, "Unknown", 0, 0);
-  } else {
-    if (ret) {
-      kv_set(ver_key, "NA", 0, 0);
-    } else if (force && (image_sts.sign == false)) {
-      kv_set(ver_key, "Unknown", 0, 0);
+  if (ret == 0) {
+    if (image_sts.sign == true) {
+      if (fby35_common_get_img_ver(image.c_str(), ver, FW_BB_CPLD) == 0) {
+        kv_set(ver_key, ver, 0, 0);
+      } else {
+        kv_set(ver_key, "Unknown", 0, 0);
+      }
     } else {
-      kv_set(ver_key, ver, 0, 0);
+      kv_set(ver_key, "Unknown", 0, 0);
     }
+  } else {
+    kv_set(ver_key, "NA", 0, 0);
   }
 
-  syslog(LOG_CRIT, "Updated CPLD on %s. File: %s. Result: %s", bmc_location_str.c_str(), image.c_str(), (ret < 0)?"Fail":"Success");
+  syslog(LOG_CRIT, "Updated CPLD on %s. File: %s. Result: %s", bmc_location_str.c_str(), image.c_str(), (ret)?"Fail":"Success");
   return ret;
 }
 
@@ -269,4 +167,64 @@ int BmcCpldComponent::update(const string image)
 int BmcCpldComponent::fupdate(const string image)
 {
   return update_cpld(image, true);
+}
+
+int BmcCpldComponent::get_ver_str(string& s) {
+  int ret = 0;
+  char ver[32] = {0};
+  uint8_t rbuf[4] = {0};
+
+  ret = pal_get_cpld_ver(FRU_BMC, rbuf);
+  if (!ret) {
+    snprintf(ver, sizeof(ver), "%02X%02X%02X%02X", rbuf[3], rbuf[2], rbuf[1], rbuf[0]);
+    s = string(ver);
+  }
+
+  return ret;
+}
+
+int BmcCpldComponent::print_version() {
+  string ver("");
+  string board_name = fru();
+  char ver_key[MAX_KEY_LEN] = {0};
+  char value[MAX_VALUE_LEN] = {0};
+  int ret = 0;
+
+  transform(board_name.begin(), board_name.end(), board_name.begin(), ::toupper);
+  try {
+    if (get_ver_str(ver) < 0) {
+      throw "Error in getting the version of " + board_name;
+    }
+    cout << board_name << " CPLD Version: " << ver << endl;
+    snprintf(ver_key, sizeof(ver_key), FRU_STR_CPLD_NEW_VER_KEY, fru().c_str());
+    ret = kv_get(ver_key, value, NULL, 0);
+    if (ret == 0) {
+      cout << board_name << " CPLD Version After activation: " << value << endl;
+    } else {
+      cout << board_name << " CPLD Version After activation: " << ver << endl;
+    }
+  } catch (string& err) {
+    printf("%s CPLD Version: NA (%s)\n", board_name.c_str(), err.c_str());
+  }
+
+  return FW_STATUS_SUCCESS;
+}
+
+void BmcCpldComponent::get_version(json& j) {
+  string ver("");
+  string board_name = fru();
+
+  transform(board_name.begin(), board_name.end(), board_name.begin(), ::toupper);
+  try {
+    if (get_ver_str(ver) < 0) {
+      throw "Error in getting the version of " + board_name;
+    }
+    j["VERSION"] = ver;
+  } catch (string& err) {
+    if (err.find("empty") != string::npos) {
+      j["VERSION"] = "not_present";
+    } else {
+      j["VERSION"] = "error_returned";
+    }
+  }
 }

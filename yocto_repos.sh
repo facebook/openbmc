a0bf008e82fe06fd21cf7341ca7b2ffac56c3df8
Kofi James <kofijames0509@fb.com>
2022-03-29 12:49:57 -0700

    yocto: update master
    Summary:
    Update master at 76fccf615
    update-submodule: yocto/lf-master
    
    diff made by sandcastleOpenBMCBuilderCommand: lf-obmc-update
    https://www.internalfb.com/intern/sandcastle/job/18014399123307967
    
    Test Plan: CIT build passes.
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: ba6f4f6add7cccf0c33806c6fa87588454d63881

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch"
 lf_master_repos=(
-    lf-openbmc:dbbba8e3a
+    lf-openbmc:76fccf615
 )
 lf_dunfell_repos=(
     lf-openbmc:e093d3473

ccfc6936137fd7038c9e7c011596d906c7469a96
Patrick Williams <patrickw3@fb.com>
2022-04-19 01:20:21 -0700

    yocto: update master
    Summary:
    Update master at 4451470c9
    update-submodule: yocto/lf-master
    
    Test Plan: CIT build passes.
    
    Reviewed By: garnermic
    
    fbshipit-source-id: 1ab8798cae2efb8b3e50c0fb44f370e1444502e0

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch"
 lf_master_repos=(
-    lf-openbmc:12b67e68d
+    lf-openbmc:4451470c9
 )
 lf_dunfell_repos=(
     lf-openbmc:e093d3473

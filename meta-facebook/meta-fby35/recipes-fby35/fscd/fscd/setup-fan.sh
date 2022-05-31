8cda2a768ceba392353af53c7c728f85e5c8fe07
Michael Garner <garnermic@fb.com>
2022-05-31 12:13:44 -0700

    fby35: Support to detect fan type in corresponding system (#2182)
    Summary:
    Support to detect which fan type should present in corresponding system.
    In EVT system:
    Config A - single fan, Config B - dual fan
    In DVT system:
    Config A - single fan, Config B - single fan
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2182
    
    Test Plan:
    1. Build and test pass on fby35 system
    (1) Test Config A with dual fan
    (2) Test Config B with daul fan
    
    (1) Config A with dual fan
    root@bmc-oob:~# log-util all --print grep fan_check
    0    all      2018-03-09 05:04:12    fan_check        Fan slot 0 is dual fan, please install single fan in DVT system
    0    all      2018-03-09 05:04:12    fan_check        Fan slot 1 is dual fan, please install single fan in DVT system
    
    (2) Config B with daul fan
    root@bmc-oob:~# log-util all --print | grep fan
    0    all      2022-05-25 17:58:46    fan_check        Fan slot 0 is dual fan, please install single fan in DVT system
    0    all      2022-05-25 17:58:46    fan_check        Fan slot 1 is dual fan, please install single fan in DVT system
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: f1ae6dbaee5b82b2e2f917ae73fb55f7b91bab23

--- meta-facebook/meta-fby35/recipes-fby35/fscd/fscd/setup-fan.sh
+++ meta-facebook/meta-fby35/recipes-fby35/fscd/fscd/setup-fan.sh
@@ -37,6 +37,31 @@ default_fsc_config_path="/etc/fsc-config.json"
 sys_config=$(/usr/local/bin/show_sys_config | grep -i "config:" | awk -F ": " '{print $3}')
 bmc_location=$(get_bmc_board_id)
 
+#For config A B C, we get the Baseboard revision from BB CPLD through i2cget. 
+CLASS1_GET_BOARD_REVID="/usr/sbin/i2cget -y 12 0x0f 0x08"
+#For config D, we get the Baseboard revision from BB BPLD. command: bic-util slot1 IPMI Send request message to BIC, IANA, BB bic, IPMI Master write read, CPLD i2cbus addr register 
+CLASS2_GET_BOARD_REVID="/usr/bin/bic-util slot1 0xe0 0x2 0x9c 0x9c 0x0 0x10 0x18 0x52 0x1 0x1e 0x1 0x8 "
+
+check_dvt_fan(){
+  local fan_type=$1
+  if [ "$fan_type" = "$TYPE_DUAL_FAN" ]; then
+    logger -t "fan_check" -p daemon.crit "Fan slot $i is dual fan, please install single fan in DVT system"
+  fi
+}
+
+check_evt_fan(){
+  local fan_type=$1
+  if [ "$fan_type" = "$TYPE_DUAL_FAN" ]; then
+    if [ "$sys_config" = "A" ]; then
+      logger -t "fan_check" -p daemon.crit "Fan slot $i is dual fan, please install single fan on 1U system"
+    fi
+  else
+    if [[ "$sys_config" = "B" || "$sys_config" = "D" ]]; then
+      logger -t "fan_check" -p daemon.crit "Fan slot $i is single fan, please install dual fan on 2U system"
+    fi
+  fi
+}
+
 check_fan_type() {
   if [ "$bmc_location" -eq "$BMC_ID_CLASS2" ]; then
     # check CC of from BIC, if there is single fan TACH 1, 3, 5, 7 will not turn CC "00"
@@ -65,14 +90,22 @@ check_fan_type() {
   fan_type=("$fan0_type" "$fan1_type")
   for ((i=0; i < ${#fan_type[@]}; i++))
   do
-    if [ "${fan_type[$i]}" = "$TYPE_DUAL_FAN" ]; then
-      if [ "$sys_config" = "A" ]; then
-        logger -t "fan_check" -p daemon.crit "Fan slot $i is dual fan, please install single fan on 1U system"
+    if [ "$bmc_location" -eq "$BMC_ID_CLASS1" ]; then
+      BOARD_REVID=$($CLASS1_GET_BOARD_REVID)
+      if [ "$((0x${BOARD_REVID#*x}))" -ge "$DVT_REVID" ]; then # for revision id >= 5 means the stage is DVT or after DVT
+        check_dvt_fan "${fan_type[$i]}"
+      else
+        check_evt_fan "${fan_type[$i]}"
+      fi
+    elif [ "$bmc_location" -eq "$BMC_ID_CLASS2" ]; then
+      BOARD_REVID=$($CLASS2_GET_BOARD_REVID | awk '{print $8;}')
+      if [ "$((0x${BOARD_REVID#*x}))" -ge "$DVT_REVID" ]; then
+        check_dvt_fan "${fan_type[$i]}"
+      else
+        check_evt_fan "${fan_type[$i]}"
       fi
     else
-      if [[ "$sys_config" = "B" || "$sys_config" = "D" ]]; then
-        logger -t "fan_check" -p daemon.crit "Fan slot $i is single fan, please install dual fan on 2U system"
-      fi
+      logger -t "fan_check" -p daemon.crit "Cannot get the location of BMC"
     fi
   done
 }

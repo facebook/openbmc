b6005977840d1482bf0321d3db0b418f6f01b8f0
Amithash Prasad <amithash@fb.com>
2022-08-04 12:39:24 -0700

    fbep: fix switch FW update result not correct (#3081)
    Summary:
    1. The test found that no matter whether the update was successful or not, it always showed that the update was successful. The reason is because the result of update is overwired by the result of modprobe.
    2. Separate the result of update from the result of modprobe, to avoid the result of modprobe to overwirte the result of update.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3081
    
    Test Plan:
    1.Build and test pass on fbep system.
    
    root@bmc-oob:~# fw-util mb --update pex0-flash /mnt/data2/EP_BRCM_iSSW_SBR_E0080001_CFG_E0080001_PEX0_SSCi_Signed_Img.bin
    [  286.057084] m25p80 spi4.0: unrecognized JEDEC id bytes: ff, ff, ff
    [  287.109708] m25p80 spi4.0: unrecognized JEDEC id bytes: ff, ff, ff
    [  287.512957] tpm_tis_spi spi4.0: 2.0 TPM (device-id 0x1B, rev-id 22)
    [  287.616153] m25p80 spi3.0: unrecognized JEDEC id bytes: ff, ff, ff
    [  287.628680] m25p80: probe of spi3.0 failed with error -2
    [  288.693914] m25p80 spi3.0: unrecognized JEDEC id bytes: ff, ff, ff
    [  288.706487] m25p80: probe of spi3.0 failed with error -2
    [  289.160737] m25p80 spi4.0: unrecognized JEDEC id bytes: ff, ff, ff
    Upgrade of mb : pex0-flash failed
    
    Reviewed By: ningrassia, garnermic
    
    fbshipit-source-id: 465f32b0a0904a5dd1138990cbb5563f5aec410b

--- meta-facebook/meta-fbep/recipes-fbep/fw-util/files/switch.cpp
+++ meta-facebook/meta-fbep/recipes-fbep/fw-util/files/switch.cpp
@@ -39,7 +39,7 @@ int PAXComponent::print_version()
 
 int PAXComponent::update(string image)
 {
-  int ret;
+  int ret, rc;
   int i, max_retry = 3;
   bool use_gpio = false;
 
@@ -48,20 +48,20 @@ int PAXComponent::update(string image)
   if (comp.find("flash") != string::npos) {
     for (i = 1; i <= max_retry; i++) {
       if (i == max_retry) {
-        ret = system("modprobe -r spi_aspeed > /dev/null");
-        ret |= system("modprobe -r spi_gpio > /dev/null");
-        ret |= system("modprobe spi_gpio > /dev/null");
-	if (ret != 0) {
+        rc = system("modprobe -r spi_aspeed > /dev/null");
+        rc |= system("modprobe -r spi_gpio > /dev/null");
+        rc |= system("modprobe spi_gpio > /dev/null");
+	      if (rc != 0) {
           syslog(LOG_WARNING, "Failed to switch SPI drivers");
-	  goto bail;
-	}
+	        goto bail;
+	      }
 
         spidev = "spi3.0";
         use_gpio = true;
       }
       ret = GPIOSwitchedSPIMTDComponent::update(image);
       if (ret == 0)
-	break;
+	      break;
     }
   } else {
 
@@ -91,10 +91,11 @@ int PAXComponent::update(string image)
 
 bail:
   if (use_gpio) {
-    ret = system("modprobe -r spi_gpio > /dev/null");
-    ret |= system("modprobe spi_aspeed > /dev/null");
-    ret |= system("modprobe spi_gpio > /dev/null");
-    if (ret != 0)
+    // recover to SPI master driver
+    rc = system("modprobe -r spi_gpio > /dev/null");
+    rc |= system("modprobe -r spi_aspeed > /dev/null");
+    rc |= system("modprobe spi_aspeed > /dev/null");
+    if (rc != 0)
       syslog(LOG_WARNING, "Failed to reload SPI drivers");
   }
   return ret;

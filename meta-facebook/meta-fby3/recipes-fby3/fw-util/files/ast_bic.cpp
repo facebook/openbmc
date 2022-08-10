1c3d0f37dab13e4204fd30fba71cef01590284b3
Peter Fetros <goldenbug@fb.com>
2022-08-09 18:14:55 -0700

    fby3, ast1030 vernal falls: add retry for bic-recovery update (#3107)
    Summary:
    The IO-expander will enter an abnormal state when i2c master sent address without W/R data. (ex. BMC use i2cdetect)
    Only the first i2c W/R command after i2cdetect will fail.
    Add retry in update procedure in order to handle this case.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3107
    
    Test Plan:
    build and test on ast1030 VF pass
    
    root@bmc-oob:/tmp# i2cdetect -y 4
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- 0f
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: 40 -- -- -- -- -- -- -- -- 49 -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    root@bmc-oob:/tmp# i2cget -y 4 0x49 0x01
    Error: Read failed
    root@bmc-oob:/tmp# i2cget -y 4 0x49 0x01
    0x04
    
    before
     ---
    root@bmc-oob:/tmp# i2cdetect -y 4
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- 0f
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: 40 -- -- -- -- -- -- -- -- 49 -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    root@bmc-oob:/tmp# fw-util slot1 --update   1ou_bic_recovery  /tmp/oby3-vf-2022.30.01.bin
    Error: Write failed
    1ou_bic_recovery: bic_recovery_init process failed
    Upgrade of slot1 : 1ou_bic_recovery failed
    
    after
     ---
    root@bmc-oob:/tmp# i2cdetect -y 4
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- 0f
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: 40 -- -- -- -- -- -- -- -- 49 -- -- -- -- -- --
    50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- -- --
    root@bmc-oob:/tmp#   ./fw-util slot1  --update  1ou_bic_recovery /tmp/oby3-vf-2022.30.01.bin
    Error: Write failed
    Start transmission over UART
    Transmission finish
    slot_id: 1, comp: 7, intf: 0, img: /tmp/oby3-vf-2022.30.01.bin, force: 1
    file size = 174436 bytes, slot = 1, intf = 0x5
    updating fw on slot 1:
    updated bic: 100 %
    Elapsed time:  14   sec.
    
    Upgrade of slot1 : 1ou_bic_recovery succeeded
    
    Signed-off-by: Ken Chen <j220584470k@gmail.com>
    
    Reviewed By: garnermic
    
    fbshipit-source-id: 551260c7208cc95f14e252beaba92c3bb5376f1a

--- meta-facebook/meta-fby3/recipes-fby3/fw-util/files/ast_bic.cpp
+++ meta-facebook/meta-fby3/recipes-fby3/fw-util/files/ast_bic.cpp
@@ -9,6 +9,19 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+int retry_system(const char *cmd) {
+  const int retry = 5;
+  int ret = -1;
+
+  for (int curr_try = 0; curr_try < retry; ++curr_try) {
+   ret = system(cmd);
+   if (ret == 0) {
+     return ret;
+   }
+  }
+
+  return ret;
+}
 
 int
 bic_recovery_init(uint8_t slot_id, string image, bool force) {
@@ -16,21 +29,21 @@ bic_recovery_init(uint8_t slot_id, string image, bool force) {
   
   // SET IO-expander OUTPUT value
   snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x01 0x04", slot_id + 3);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
   
   // SET BIC_FWSPICK_IO_EXP to high
   snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x03 0xFB", slot_id + 3);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
   
   // SET UART MUX to 1OU
   snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x0F 0x01 0x01", slot_id + 3);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
@@ -38,21 +51,21 @@ bic_recovery_init(uint8_t slot_id, string image, bool force) {
   sleep(1);
   // SET BIC_SRST_N_IO_EXP_R to low
   snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x03 0xFA", slot_id + 3);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
   sleep(1);
   // SET BIC_SRST_N_IO_EXP_R to high
   snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x03 0xFB", slot_id + 3);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
   sleep(2);
   
   snprintf(cmd, sizeof(cmd), "/bin/stty -F /dev/ttyS%d 115200", slot_id);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
@@ -71,21 +84,21 @@ bic_recovery_init(uint8_t slot_id, string image, bool force) {
   
   snprintf(cmd, sizeof(cmd), "echo -n -e '\\x%x\\x%x\\x%x\\x%x' > /dev/ttyS%d",
            head[0], head[1], head[2], head[3], slot_id);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
   
   cout << "Start transmission over UART" << endl;
   snprintf(cmd, sizeof(cmd), "cat %s > /dev/ttyS%d", image.c_str(), slot_id);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
   cout << "Transmission finish" << endl;
   
   snprintf(cmd, sizeof(cmd), "/bin/stty -F /dev/ttyS%d 57600", slot_id);
-  if (system(cmd) != 0) {
+  if (retry_system(cmd) != 0) {
     syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
     return -1;
   }
@@ -115,21 +128,21 @@ int AstBicFwRecoveryComponent::ast_bic_recovery(string image, bool force) {
     sleep (2);
     // SET BIC_FWSPICK_IO_EXP to low
     snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x03 0xFF", slot_id + 3);
-    if (system(cmd) != 0) {
+    if (retry_system(cmd) != 0) {
       syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
       return -1;
     }
     
     // SET BIC_SRST_N_IO_EXP_R to low
     snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x03 0xFE", slot_id + 3);
-    if (system(cmd) != 0) {
+    if (retry_system(cmd) != 0) {
       syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
       return -1;
     }
     sleep(1);
     // SET BIC_SRST_N_IO_EXP_R to high
     snprintf(cmd, sizeof(cmd), "i2cset -y %d 0x49 0x03 0xFF", slot_id + 3);
-    if (system(cmd) != 0) {
+    if (retry_system(cmd) != 0) {
       syslog(LOG_WARNING, "[%s] %s failed", __func__, cmd);
       return -1;
     }

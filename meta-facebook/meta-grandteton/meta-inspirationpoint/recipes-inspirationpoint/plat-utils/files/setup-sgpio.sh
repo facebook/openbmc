65b004722af486ece1f9085455c068726b846dad
Amithash Prasad <amithash@fb.com>
2022-08-12 11:49:38 -0700

    GTI: modify sgpio table (#3117)
    Summary:
    1. Update sgpio table according to EE team sgpio v0.6 table and CPLD FW v0.005.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3117
    
    Test Plan: 1.Build and test pass on GTI system.
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: ef3b6f5e8f59b2a49d251044af407f62f4b9f03c

--- meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/plat-utils/files/setup-sgpio.sh
+++ meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/plat-utils/files/setup-sgpio.sh
@@ -32,15 +32,12 @@
 sgpio_export PWRGD_OCP_SFF_PWR_GOOD 0
 sgpio_export IRQ_SML1_PMBUS_ALERT_N 2
 sgpio_export FM_PWRGD_PVDD18_S5_P0 4
-sgpio_export P12V_HSC_TEMP_ALERT_R_N 6
 sgpio_export GPU_FPGA_READY_ISO_R 8
 sgpio_export APML_CPU0_ALERT_R_N 10
 sgpio_export APML_CPU1_ALERT_R_N 12
 sgpio_export FM_PVDD11_S3_P0_OCP_N 14
 sgpio_export FM_PVDD11_S3_P1_OCP_N 16
-sgpio_export P12V_HSC_T_CRIT_R_N 18
 sgpio_export FM_SYS_THROTTLE_R_N 20
-sgpio_export P12V_ALL_PWROK_R 22
 sgpio_export HPDB_HSC_PWRGD_ISO_R 24
 sgpio_export FM_UV_ADR_TRIGGER_R_N 26
 sgpio_export PRSNT_OCP_SFF_N 28
@@ -87,9 +84,7 @@ sgpio_export FM_UARTSW_MSB_N 108
 sgpio_export FM_THERMAL_ALERT_R_N 110
 sgpio_export FM_PRSNT_CPU0_R_N 112
 sgpio_export FM_PRSNT_CPU1_R_N 114
-sgpio_export FM_SYS_THROTTLE_N_reg 116
-sgpio_export CPU0_THERMTRIP_R_N 118
-sgpio_export FM_THROTTLE_IN_N 120
+sgpio_export CPU1_THERMTRIP_R_N 118
 sgpio_export FM_PVDDCR_CPU0_P0_OCP_N 122
 sgpio_export FM_PVDDCR_CPU0_P1_OCP_N 124
 sgpio_export FM_PVDDCR_CPU1_P0_OCP_N 126
@@ -97,7 +92,7 @@ sgpio_export FM_PVDDCR_CPU1_P1_OCP_N 128
 sgpio_export PWRGD_CHGL_CPU0 130
 sgpio_export PWRGD_CHAF_CPU0 132
 sgpio_export PWRGD_CHAF_CPU1 134
-sgpio_export CPU1_THERMTRIP_R_N 136
+sgpio_export CPU0_THERMTRIP_R_N 136
 sgpio_export FM_CPU0_RSMRST_N 138
 sgpio_export FM_SCM_PRSNT1_N 140
 sgpio_export FM_CPU0_SMERR_N 142
@@ -110,14 +105,10 @@ sgpio_export PVDDCR_CPU0_P0_PMALERT 154
 sgpio_export PVDDCR_CPU0_P1_PMALERT 156
 sgpio_export PVDDCR_CPU1_P0_SMB_ALERT 158
 sgpio_export PVDDCR_CPU1_P1_SMB_ALERT 160
-sgpio_export FM_BMC_DBP_PRESENT_N 162
+sgpio_export PRSNT_HDT_N 162
 sgpio_export FM_CPU0_SLP_S3_N 164
 sgpio_export FM_CPU0_SLP_S5_N 166
-sgpio_export FM_CPU0_PROCHOT_R_N_reg 168
-sgpio_export FM_CPU1_PROCHOT_R_N_reg 170
 sgpio_export E1S_0_PRSNT_R_N 172
-sgpio_export FM_SMB_RST_E1S_0_N 174
-sgpio_export P12V_E1S_0_EN 176
 sgpio_export OC_ALERT_N 178
 sgpio_export PWRGD_CHGL_CPU1 180
 sgpio_export PVDD11_S3_P0_PMALERT 182
@@ -140,9 +131,8 @@ sgpio_export FAB_BMC_REV_ID2 222
 sgpio_export FAB_BMC_REV_ID1 224
 sgpio_export FAB_BMC_REV_ID0 226
 sgpio_export BMC_JTAG_SEL_R 228
-sgpio_export CPLD_JTAG_EN 230
-sgpio_export FM_CPU0_PWR_GOOD 232
-sgpio_export FM_CPU1_PWR_GD_IN 234
+sgpio_export P12V_HSC_TEMP_ALERT_R_N 232
+sgpio_export P12V_HSC_T_CRIT_R_N 234
 sgpio_export FM_CPU1_PWRGD_OUT 236
 sgpio_export FM_PWRGD_CPU0_PWROK 238
 sgpio_export FM_PWRGD_CPU1_PWROK 240
@@ -155,12 +145,6 @@ sgpio_export OCP_V3_1_P3V3_PWRGD 252
 sgpio_export HP_LVC3_OCP_V3_2_P12V_PWRGD_R 254
 
 #output
-sgpio_export FM_ESPI_PLD_R1_EN 1
-gpio_set FM_ESPI_PLD_R1_EN 1
-
-sgpio_export IRQ_BMC_CPU_NMI 3
-gpio_set IRQ_BMC_CPU_NMI 1
-
 sgpio_export RST_SMB_SWITCH_N 5
 gpio_set RST_SMB_SWITCH_N 1
 
@@ -173,29 +157,23 @@ gpio_set FM_DEBUG_PORT_PRSNT_R1_N 1
 sgpio_export IRQ_PCH_SCI_WHEA_R_N 11
 gpio_set IRQ_PCH_SCI_WHEA_R_N 1
 
-sgpio_export FM_PECI_SEL_R_N 13
-gpio_set FM_PECI_SEL_R_N 1
-
 sgpio_export FM_JTAG_BMC_MUX_SEL_R1 15
-gpio_set FM_JTAG_BMC_MUX_SEL_R1 1
+gpio_set FM_JTAG_BMC_MUX_SEL_R1 0
 
 sgpio_export FM_SPD_REMOTE_EN_R 17
-gpio_set FM_SPD_REMOTE_EN_R 1
+gpio_set FM_SPD_REMOTE_EN_R 0
 
 sgpio_export RST_BMC_RSTBTN_OUT_R_N 21
 gpio_set RST_BMC_RSTBTN_OUT_R_N 1
 
-sgpio_export IRQ_BMC_PCH_NMI_N 27
-gpio_set IRQ_BMC_PCH_NMI_N 1
-
-sgpio_export FM_BMC_ONCTL_R_N 35
-gpio_set FM_BMC_ONCTL_R_N 1
-
-sgpio_export FM_BMC_READY_R_N 37
+sgpio_export FM_BMC_READY_R_N 23
 gpio_set FM_BMC_READY_R_N 1
 
-sgpio_export FM_BIOS_DEBUG_EN_R_N 41
-gpio_set FM_BIOS_DEBUG_EN_R_N 1
+sgpio_export FM_BMC_SGPIO_READY_N 37
+gpio_set FM_BMC_SGPIO_READY_N 1
+
+sgpio_export FM_BIOS_DEBUG_EN_N 41
+gpio_set FM_BIOS_DEBUG_EN_N 1
 
 sgpio_export GPU_NVS_PWR_BRAKE_R_N 43
 gpio_set GPU_NVS_PWR_BRAKE_R_N 1
@@ -206,23 +184,11 @@ gpio_set GPU_WP_HW_CTRL_R_N 1
 sgpio_export CLK_GEN2_BMC_RC_OE_R_N 47
 gpio_set CLK_GEN2_BMC_RC_OE_R_N 1
 
-sgpio_export IRQ_SML0_ALERT_R_N 49
-gpio_set IRQ_SML0_ALERT_R_N 1
-
 sgpio_export BMC_MONITER 51
 gpio_set BMC_MONITER 1
 
-sgpio_export FM_PFR_DSW_PWROK_N 53
-gpio_set FM_PFR_DSW_PWROK_N 1
-
-sgpio_export RST_PFR_OVR_RTC_R 55
-gpio_set RST_PFR_OVR_RTC_R 1
-
 sgpio_export FM_BMC_TPM_PRES_N_R 57
 gpio_set FM_BMC_TPM_PRES_N_R 1
-
-sgpio_export FM_TPM_PRSNT_1_N 59
-gpio_set FM_TPM_PRSNT_1_N 1
 
 sgpio_export IRQ_PCH_TPM_SPI_N 61
 gpio_set IRQ_PCH_TPM_SPI_N 1
@@ -233,62 +199,20 @@ gpio_set USB_OC0_REAR_R_N 1
 sgpio_export GPU_FPGA_RST_N 65
 gpio_set GPU_FPGA_RST_N 1
 
-sgpio_export FM_JTAG_TCK_MUX_BMC_SEL 67
-gpio_set FM_JTAG_TCK_MUX_BMC_SEL 1
-
 sgpio_export CLK_GEN2_GPU_FPGA_OE_R_N 69
 gpio_set CLK_GEN2_GPU_FPGA_OE_R_N 1
-
-sgpio_export FM_BMC_CPU_FBRK_OUT_N 71
-gpio_set FM_BMC_CPU_FBRK_OUT_N 1
-
-sgpio_export FM_REMOTE_DEBUG_EN_N 73
-gpio_set FM_REMOTE_DEBUG_EN_N 1
-
-sgpio_export RST_IOEXP_N 75
-gpio_set RST_IOEXP_N 1
-
-sgpio_export PMB_ALERT_EN_N 77
-gpio_set PMB_ALERT_EN_N 1
-
-sgpio_export FM_SPD_SWITCH_CTRL_N 79
-gpio_set FM_SPD_SWITCH_CTRL_N 1
-
-sgpio_export FM_SYS_RESET_N 81
-gpio_set FM_SYS_RESET_N 1
-
-sgpio_export Int_SCM_USB_OC_R_N 83
-gpio_set Int_SCM_USB_OC_R_N 1
-
-sgpio_export FM_SMB_RST_E1S_1_N 85
-gpio_set FM_SMB_RST_E1S_1_N 1
-
-sgpio_export RM_THROTTLE_EN_N 87
-gpio_set RM_THROTTLE_EN_N 1
 
 sgpio_export JTAG_SCM_TRST_R_N 91
 gpio_set JTAG_SCM_TRST_R_N 1
 
-sgpio_export BIOS_DEBUG_MODE_DIS 93
-gpio_set BIOS_DEBUG_MODE_DIS 1
-
 sgpio_export FM_BMC_PWRBRK_N 95
 gpio_set FM_BMC_PWRBRK_N 1
-
-sgpio_export BMC_PWRCAP_N 97
-gpio_set BMC_PWRCAP_N 1
 
 sgpio_export BMC_FORCE_NM_THROTTLE_N 99
 gpio_set BMC_FORCE_NM_THROTTLE_N 1
 
-sgpio_export FM_APML_MUX2_OE_R_N 101
-gpio_set FM_APML_MUX2_OE_R_N 1
-
 sgpio_export FM_APML_MUX2_SEL_R 103
 gpio_set FM_APML_MUX2_SEL_R 1
-
-sgpio_export FM_SEC_MUX_OE_R_N 105
-gpio_set FM_SEC_MUX_OE_R_N 1
 
 sgpio_export FM_SEC_MUX_R_SEL 107
 gpio_set FM_SEC_MUX_R_SEL 1

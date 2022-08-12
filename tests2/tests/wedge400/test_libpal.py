ebb6330df88cbb3f4825591c6a6448f3d1fbe94a
Tao Ren <taoren@fb.com>
2022-08-11 23:35:04 -0700

    tests2: wedge400: skip gb sensors if sdk is not running
    Summary:
    Skip GB sensors tests if SDK is not running on Wedge400C, because the
    net_casic driver always reports error when SDK is not running.
    
    Test Plan: Wedge400 CIT passed: P521882434
    
    Reviewed By: GoldenBug
    
    fbshipit-source-id: 6a353ae7194f955fa557250e3f7c15f8423fbc13

--- tests2/tests/wedge400/test_libpal.py
+++ tests2/tests/wedge400/test_libpal.py
@@ -17,14 +17,60 @@
 # Boston, MA 02110-1301 USA
 #
 
+import os
 import unittest
 
 import common.base_libpal_test
+from tests.wedge400.helper.libpal import pal_get_board_type
 from utils.test_utils import qemu_check
 
 
 class LibPalTest(common.base_libpal_test.LibPalTest):
     PLATFORM_NAME = "wedge400"
+
+    def get_skip_sensors(self):
+        # we need to skip Wedge400C GB sensors if SDK is not running in
+        # host side.
+        board_type = pal_get_board_type()
+        if board_type == "Wedge400":
+            return []
+
+        # GB sensors are located on SMB (FRU ID 2), and the sensor names
+        # are SMB_SENSOR_SW_DIE_TEMP[1-2] and SMB_SENSOR_GB_TEMP[1-10].
+        # NOTE: we will need to update fru_id and sensor_num if the macros
+        # were updated in pal.h. Is there a way to reference the definitions
+        # in pal.h?
+        gb_sensors = [
+            [2, 56],
+            [2, 57],
+            [2, 100],
+            [2, 101],
+            [2, 102],
+            [2, 103],
+            [2, 104],
+            [2, 105],
+            [2, 106],
+            [2, 107],
+            [2, 108],
+            [2, 109],
+        ]
+
+        try:
+            gb_hwmon = os.path.realpath("/sys/bus/i2c/devices/3-002a/hwmon/hwmon*")
+            if not os.path.exists(gb_hwmon):
+                return gb_sensors
+
+            with open(os.path.join(gb_hwmon, "sdk_status"), "r") as f:
+                output = f.readline().rstrip("\n")
+                sdk_status = int(output, 10)
+        except Exception:
+            sdk_status = 0
+
+        # "sdk_status == 0" means SDK is not running.
+        if sdk_status == 0:
+            return gb_sensors
+
+        return []
 
     @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
     def test_pal_is_fru_prsnt(self):
@@ -32,11 +78,13 @@ class LibPalTest(common.base_libpal_test.LibPalTest):
 
     @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
     def test_sensor_raw_read(self):
-        super().test_sensor_raw_read()
+        skipped = self.get_skip_sensors()
+        super().test_sensor_raw_read(skip_sensors=skipped)
 
     @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
     def test_sensor_read(self):
-        super().test_sensor_read()
+        skipped = self.get_skip_sensors()
+        super().test_sensor_read(skip_sensors=skipped)
 
     @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
     def test_sensor_read_fru_not_present(self):

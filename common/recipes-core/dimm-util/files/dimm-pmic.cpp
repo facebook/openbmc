01c806e987222a2325bd39b571e23d572463cdc2
Michael Garner <garnermic@fb.com>
2022-09-09 09:47:35 -0700

    dimm-util: block PMIC error inject in write protect mode (#2340)
    Summary:
    If PMIC is in write protect mode, BMC can not write the PMIC error injection register.
    Block PMIC error inject in write protect mode.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2340
    
    Test Plan:
    Build and test pass on fby35 system.
    1. Inject PMIC error
    (1) Write protect mode disabled
    root@bmc-oob:~# dimm-util slot1 --pmic --dimm A0 --err_inj high_temp
    Error inject successfully on DIMM A0
    root@bmc-oob:~# dimm-util slot1 --pmic --dimm A0 -l
    DIMM A0 PMIC Error: high_temp
    (2) Write protect mode enabled
    root@bmc-oob:~# dimm-util slot3 --pmic --dimm A0 --err_inj high_temp
    Inject failed! Please disable DIMM A0 write protect before error injection
    root@bmc-oob:~# dimm-util slot3 --pmic --dimm A0 -l
    DIMM A0 PMIC Error: No Error
    
    Reviewed By: williamspatrick
    
    fbshipit-source-id: d9f026831d5117f28e89343e23273cc87a676482

--- common/recipes-core/dimm-util/files/dimm-pmic.cpp
+++ common/recipes-core/dimm-util/files/dimm-pmic.cpp
@@ -6,6 +6,7 @@
 
 #define PMIC_ERR_INJ_REG 0x35
 #define ERR_PATTERN_LEN  6
+#define PMIC_WRITE_PROTECT_BIT 2
 
 #define STR_SWA_OV     "SWAout_OV"
 #define STR_SWB_OV     "SWBout_OV"
@@ -185,6 +186,14 @@ int
 pmic_inject_err(uint8_t fru_id, uint8_t cpu, uint8_t dimm, uint8_t option) {
   uint8_t data[8] = {0};
 
+  // Read PMIC R2F register to get write protect mode status
+  if (pmic_rdwr_with_retry(fru_id, cpu, dimm, 0x2F, 0, 1, data) != 0) {
+    return -1;
+  }
+  if ((data[0] & (1 << PMIC_WRITE_PROTECT_BIT)) == 0) {
+    printf("Inject failed! Please disable DIMM %s write protect before error injection\n", get_dimm_label(cpu, dimm));
+    return -1;
+  }
   memcpy(data, &pmic_err[option].einj_reg, 1);
   if (pmic_rdwr_with_retry(fru_id, cpu, dimm, PMIC_ERR_INJ_REG, 1, 0, data) != 0) {
     return -1;

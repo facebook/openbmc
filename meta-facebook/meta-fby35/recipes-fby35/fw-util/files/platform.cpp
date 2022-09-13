ed4de39f35bccf850abeda5391657748d81ad122
Michael Garner <garnermic@fb.com>
2022-09-13 08:50:06 -0700

    fby35: fw-util only displays common components on empty slot (#3177)
    Summary:
    Some components are not common for HD and CL, such as ME and VRs.
    So change to only shows the common components when the slot is not present.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3177
    
    Test Plan:
    Build and test pass on fby35
    
    root@bmc-oob:~# fw-util slot1 --version
    1OU Bridge-IC Version: NA (1ou is empty)
    1OU CPLD Version: NA (1ou is empty)
    SB Bridge-IC Version: oby35-cl-v2022.34.01
    BIOS Version: F0ES2A02
    SB CPLD Version: 00010E10
    SB CPLD Version after activation: 00010E10
    MP5990 HSC Checksum: 158B
    ME Version: 6.0.3.162
    VCCIN/VCCFA_EHV_FIVRA Version: Infineon 496C051B, Remaining Writes: 16
    VCCIN/VCCFA_EHV_FIVRA Version after activation: Infineon 496C051B, Remaining Writes: 16
    VCCD Version: Infineon D981FA2D, Remaining Writes: 13
    VCCD Version after activation: Infineon D981FA2D, Remaining Writes: 13
    VCCINFAON/VCCFA_EHV Version: Infineon 76FA2211, Remaining Writes: 20
    VCCINFAON/VCCFA_EHV Version after activation: Infineon 76FA2211, Remaining Writes: 20
    
    root@bmc-oob:~# fw-util slot2 --version
    1OU Bridge-IC Version: NA (slot2 is empty)
    1OU CPLD Version: NA (slot2 is empty)
    SB Bridge-IC Version: NA (slot2 is empty)
    BIOS Version: NA (slot2 is empty)
    SB CPLD Version: NA (slot2 is empty)
    
    Reviewed By: peterdelevoryas
    
    fbshipit-source-id: 8274ec572a306bdbe07d6de01dba2b250164cf81

--- meta-facebook/meta-fby35/recipes-fby35/fw-util/files/platform.cpp
+++ meta-facebook/meta-fby35/recipes-fby35/fw-util/files/platform.cpp
@@ -1,5 +1,7 @@
-#include <syslog.h>
 #include <facebook/fby35_common.h>
+#include <facebook/bic.h>
+#include <facebook/bic_fwupdate.h>
+#include <facebook/bic_ipmi.h>
 #include "bic_fw.h"
 #include "bic_cpld.h"
 #include "bmc_cpld.h"
@@ -8,41 +10,23 @@
 #include "bic_bios.h"
 #include "bic_vr.h"
 #include "bic_cxl.h"
-#include "bic_pcie_sw.h"
-#include "bic_m2_dev.h"
-#include <facebook/bic.h>
-#include <facebook/bic_fwupdate.h>
-#include <facebook/bic_ipmi.h>
-#include <openbmc/pal.h>
 #include "usbdbg.h"
 #include "mp5990.h"
 
 NicExtComponent nic_fw("nic", "nic", "nic_fw_ver", FRU_NIC, 0x00);
 
-//slot1 sb bic/cpld/bios/me/vr
+//slot1 sb bic/bios
 BicFwComponent  bic_fw1("slot1", "bic", "sb", FW_SB_BIC);
 BicFwComponent  bic_rcvy_fw1("slot1", "bic_rcvy", "sb", FW_BIC_RCVY);
 BiosComponent   bios_fw1("slot1", "bios", FRU_SLOT1, FW_BIOS);
-VrComponent     vr_fw1("slot1", "vr", FW_VR);
 
 class ClassConfig {
   public:
     ClassConfig() {
-      uint8_t bmc_location = 0;
+      uint8_t bmc_location = 0, prsnt;
       uint8_t hsc_type = HSC_UNKNOWN;
       uint8_t board_rev = UNKNOWN_REV;
-      int slot1_config_status, slot3_config_status = 0;
-
-      // Only slot1 and slot3 may be 2OU, so we need to get the config status of slot1 and slot3
-      slot1_config_status = bic_is_exp_prsnt(FRU_SLOT1);
-      if (slot1_config_status < 0) {
-        slot1_config_status = 0;
-      }
-
-      slot3_config_status = bic_is_exp_prsnt(FRU_SLOT3);
-      if (slot3_config_status < 0) {
-        slot3_config_status = 0;
-      }
+      int config_status;
 
       if (fby35_common_get_bmc_location(&bmc_location) < 0) {
         printf("Failed to initialize the fw-util\n");
@@ -57,8 +41,8 @@ class ClassConfig {
         static CpldComponent     cpld_bb_fw1("slot1", "bb_cpld", "bb", FW_BB_CPLD, 0, 0);
 
         //slot1 2ou bic/cpld
-        static BicFwComponent  bic_2ou_fw1("slot1", "2ou_bic", "2ou", FW_2OU_BIC);
-        static CpldComponent   cpld_2ou_fw1("slot1", "2ou_cpld", "2ou", FW_2OU_CPLD, 0, 0);
+        static BicFwComponent    bic_2ou_fw1("slot1", "2ou_bic", "2ou", FW_2OU_BIC);
+        static CpldComponent     cpld_2ou_fw1("slot1", "2ou_cpld", "2ou", FW_2OU_CPLD, 0, 0);
       } else {
         // Register USB Debug Card components
         static UsbDbgComponent   usbdbg("ocpdbg", "mcu", "FBY35", 9, 0x60, false);
@@ -68,31 +52,7 @@ class ClassConfig {
 
         if (fby35_common_get_bb_hsc_type(&hsc_type) == 0) {
           if (hsc_type == HSC_MP5990) {
-            static MP5990Component  hsc_bb("bmc", "hsc", FRU_BMC, 11, 0x40);
-          }
-        }
-        if (get_board_rev(FRU_SLOT1, BOARD_ID_SB, &board_rev) == 0) {
-          if (IS_BOARD_REV_MPS(board_rev)) {
-            static MP5990Component hsc_fw1("slot1", "hsc", FRU_SLOT1, 1, 0x16);
-          }
-        }
-        if ((slot1_config_status & PRESENT_2OU) != PRESENT_2OU) {
-          if (get_board_rev(FRU_SLOT2, BOARD_ID_SB, &board_rev) == 0) {
-            if (IS_BOARD_REV_MPS(board_rev)) {
-              static MP5990Component hsc_fw2("slot2", "hsc", FRU_SLOT2, 1, 0x16);
-            }
-          }
-        }
-        if (get_board_rev(FRU_SLOT3, BOARD_ID_SB, &board_rev) == 0) {
-          if (IS_BOARD_REV_MPS(board_rev)) {
-            static MP5990Component hsc_fw3("slot3", "hsc", FRU_SLOT3, 1, 0x16);
-          }
-        }
-        if ((slot1_config_status & PRESENT_2OU) != PRESENT_2OU) {
-          if (get_board_rev(FRU_SLOT4, BOARD_ID_SB, &board_rev) == 0) {
-            if (IS_BOARD_REV_MPS(board_rev)) {
-              static MP5990Component hsc_fw4("slot4", "hsc", FRU_SLOT4, 1, 0x16);
-            }
+            static MP5990Component hsc_bb("bmc", "hsc", FRU_BMC, 11, 0x40);
           }
         }
 
@@ -100,93 +60,152 @@ class ClassConfig {
         static BicFwComponent    bic_1ou_fw1("slot1", "1ou_bic", "1ou", FW_1OU_BIC);
         static CpldComponent     cpld_1ou_fw1("slot1", "1ou_cpld", "1ou", FW_1OU_CPLD, 0, 0);
 
-        //slot1 vr/cpld/me
-        if (fby35_common_get_slot_type(FRU_SLOT1) == SERVER_TYPE_HD) {
-            static CpldComponent   cpld_fw1("slot1", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x44);
-            static VrComponent     vr_vddcpu0_fw1("slot1", "vr_vddcrcpu0", FW_VR_VDDCRCPU0);
-            static VrComponent     vr_vddcpu1_fw1("slot1", "vr_vddcrcpu1", FW_VR_VDDCRCPU1);
-            static VrComponent     vr_vdd11s3_fw1("slot1", "vr_vdd11s3", FW_VR_VDD11S3);
-        } else {
-            static CpldComponent   cpld_fw1("slot1", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
-            static MeComponent     me_fw1("slot1", "me", FRU_SLOT1);
-            static VrComponent     vr_vccin_fw1("slot1", "vr_vccin", FW_VR_VCCIN);
-            static VrComponent     vr_vccd_fw1("slot1", "vr_vccd", FW_VR_VCCD);
-            static VrComponent     vr_vccinfaon_fw1("slot1", "vr_vccinfaon", FW_VR_VCCINFAON);
+        if (fby35_common_is_fru_prsnt(FRU_SLOT1, &prsnt)) {
+          prsnt = 0;
         }
-
-        //slot1 1ou vr
-        if( (slot1_config_status & PRESENT_1OU) == PRESENT_1OU ) {
-            if( isRainbowFalls(FRU_SLOT1) ){
-                static VrComponent     vr_1ou_va0v8_fw1("slot1", "1ou_vr_v9asica", FW_1OU_VR_V9_ASICA);
-                static VrComponent     vr_1ou_vddqab_fw1("slot1", "1ou_vr_vddqab", FW_1OU_VR_VDDQAB);
-                static VrComponent     vr_1ou_vddqcd_fw1("slot1", "1ou_vr_vddqcd", FW_1OU_VR_VDDQCD);
-                static CxlComponent    cxl_1ou_fw1("slot1", "cxl", "1ou", FW_1OU_CXL);
+        if (prsnt) {
+          if (get_board_rev(FRU_SLOT1, BOARD_ID_SB, &board_rev) == 0) {
+            if (IS_BOARD_REV_MPS(board_rev)) {
+              static MP5990Component hsc_fw1("slot1", "hsc", FRU_SLOT1, 1, 0x16);
             }
+          }
+
+          //slot1 cpld/me/vr
+          if (fby35_common_get_slot_type(FRU_SLOT1) == SERVER_TYPE_HD) {
+            static CpldComponent cpld_fw1("slot1", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x44);
+            static VrComponent   vr_vddcpu0_fw1("slot1", "vr_vddcrcpu0", FW_VR_VDDCRCPU0);
+            static VrComponent   vr_vddcpu1_fw1("slot1", "vr_vddcrcpu1", FW_VR_VDDCRCPU1);
+            static VrComponent   vr_vdd11s3_fw1("slot1", "vr_vdd11s3", FW_VR_VDD11S3);
+          } else {
+            static CpldComponent cpld_fw1("slot1", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
+            static MeComponent   me_fw1("slot1", "me", FRU_SLOT1);
+            static VrComponent   vr_vccin_fw1("slot1", "vr_vccin", FW_VR_VCCIN);
+            static VrComponent   vr_vccd_fw1("slot1", "vr_vccd", FW_VR_VCCD);
+            static VrComponent   vr_vccinfaon_fw1("slot1", "vr_vccinfaon", FW_VR_VCCINFAON);
+          }
+          static VrComponent     vr_fw1("slot1", "vr", FW_VR);
+
+          //slot1 1ou vr
+          if ((config_status = bic_is_exp_prsnt(FRU_SLOT1)) < 0) {
+            config_status = 0;
+          }
+          if ((config_status & PRESENT_1OU) == PRESENT_1OU) {
+            if (isRainbowFalls(FRU_SLOT1)) {
+              static VrComponent  vr_1ou_va0v8_fw1("slot1", "1ou_vr_v9asica", FW_1OU_VR_V9_ASICA);
+              static VrComponent  vr_1ou_vddqab_fw1("slot1", "1ou_vr_vddqab", FW_1OU_VR_VDDQAB);
+              static VrComponent  vr_1ou_vddqcd_fw1("slot1", "1ou_vr_vddqcd", FW_1OU_VR_VDDQCD);
+              static CxlComponent cxl_1ou_fw1("slot1", "cxl", "1ou", FW_1OU_CXL);
+            }
+          }
+        } else {
+          static CpldComponent   cpld_fw1("slot1", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
         }
 
-        //slot2 sb bic/cpld/bios/me/vr
+        //slot2 sb bic/cpld/bios
         static BicFwComponent    bic_fw2("slot2", "bic", "sb", FW_SB_BIC);
         static BicFwComponent    bic_rcvy_fw2("slot2", "bic_rcvy", "sb", FW_BIC_RCVY);
         static CpldComponent     cpld_fw2("slot2", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
         static BiosComponent     bios_fw2("slot2", "bios", FRU_SLOT2, FW_BIOS);
-        static MeComponent       me_fw2("slot2", "me", FRU_SLOT2);
-        static VrComponent       vr_fw2("slot2", "vr", FW_VR);
-        static VrComponent       vr_vccin_fw2("slot2", "vr_vccin", FW_VR_VCCIN);
-        static VrComponent       vr_vccd_fw2("slot2", "vr_vccd", FW_VR_VCCD);
-        static VrComponent       vr_vccinfaon_fw2("slot2", "vr_vccinfaon", FW_VR_VCCINFAON);
 
         //slot2 1ou bic/cpld
         static BicFwComponent    bic_1ou_fw2("slot2", "1ou_bic", "1ou", FW_1OU_BIC);
         static CpldComponent     cpld_1ou_fw2("slot2", "1ou_cpld", "1ou", FW_1OU_CPLD, 0, 0);
 
-        //slot3 sb bic/bios/vr
+        if (fby35_common_is_fru_prsnt(FRU_SLOT2, &prsnt)) {
+          prsnt = 0;
+        }
+        if (prsnt) {
+          if (get_board_rev(FRU_SLOT2, BOARD_ID_SB, &board_rev) == 0) {
+            if (IS_BOARD_REV_MPS(board_rev)) {
+              static MP5990Component hsc_fw2("slot2", "hsc", FRU_SLOT2, 1, 0x16);
+            }
+          }
+
+          //slot2 me/vr
+          static MeComponent     me_fw2("slot2", "me", FRU_SLOT2);
+          static VrComponent     vr_vccin_fw2("slot2", "vr_vccin", FW_VR_VCCIN);
+          static VrComponent     vr_vccd_fw2("slot2", "vr_vccd", FW_VR_VCCD);
+          static VrComponent     vr_vccinfaon_fw2("slot2", "vr_vccinfaon", FW_VR_VCCINFAON);
+          static VrComponent     vr_fw2("slot2", "vr", FW_VR);
+        }
+
+        //slot3 sb bic/bios
         static BicFwComponent    bic_fw3("slot3", "bic", "sb", FW_SB_BIC);
         static BicFwComponent    bic_rcvy_fw3("slot3", "bic_rcvy", "sb", FW_BIC_RCVY);
         static BiosComponent     bios_fw3("slot3", "bios", FRU_SLOT3, FW_BIOS);
-        static VrComponent       vr_fw3("slot3", "vr", FW_VR);
-
-        //slot3 vr/cpld/me
-        if (fby35_common_get_slot_type(FRU_SLOT3) == SERVER_TYPE_HD) {
-            static CpldComponent   cpld_fw3("slot3", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x44);
-            static VrComponent     vr_vddcpu0_fw3("slot3", "vr_vddcrcpu0", FW_VR_VDDCRCPU0);
-            static VrComponent     vr_vddcpu1_fw3("slot3", "vr_vddcrcpu1", FW_VR_VDDCRCPU1);
-            static VrComponent     vr_vdd11s3_fw3("slot3", "vr_vdd11s3", FW_VR_VDD11S3);
-        } else {
-            static CpldComponent   cpld_fw3("slot3", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
-            static MeComponent       me_fw3("slot3", "me", FRU_SLOT3);
-            static VrComponent       vr_vccin_fw3("slot3", "vr_vccin", FW_VR_VCCIN);
-            static VrComponent       vr_vccd_fw3("slot3", "vr_vccd", FW_VR_VCCD);
-            static VrComponent       vr_vccinfaon_fw3("slot3", "vr_vccinfaon", FW_VR_VCCINFAON);
-        }
-
-        //slot3 1ou vr
-        if( (slot3_config_status & PRESENT_1OU) == PRESENT_1OU ) {
-            if(isRainbowFalls(FRU_SLOT3)){
-                static VrComponent     vr_1ou_va0v8_fw3("slot3", "1ou_vr_v9asica", FW_1OU_VR_V9_ASICA);
-                static VrComponent     vr_1ou_vddqab_fw3("slot3", "1ou_vr_vddqab", FW_1OU_VR_VDDQAB);
-                static VrComponent     vr_1ou_vddqcd_fw3("slot3", "1ou_vr_vddqcd", FW_1OU_VR_VDDQCD);
-                static CxlComponent    cxl_1ou_fw3("slot3", "cxl", "1ou", FW_1OU_CXL);
-            }
-        }
 
         //slot3 1ou bic/cpld
         static BicFwComponent    bic_1ou_fw3("slot3", "1ou_bic", "1ou", FW_1OU_BIC);
         static CpldComponent     cpld_1ou_fw3("slot3", "1ou_cpld", "1ou", FW_1OU_CPLD, 0, 0);
 
-        //slot4 sb bic/cpld/bios/me/vr
+        if (fby35_common_is_fru_prsnt(FRU_SLOT3, &prsnt)) {
+          prsnt = 0;
+        }
+        if (prsnt) {
+          if (get_board_rev(FRU_SLOT3, BOARD_ID_SB, &board_rev) == 0) {
+            if (IS_BOARD_REV_MPS(board_rev)) {
+              static MP5990Component hsc_fw3("slot3", "hsc", FRU_SLOT3, 1, 0x16);
+            }
+          }
+
+          //slot3 cpld/me/vr
+          if (fby35_common_get_slot_type(FRU_SLOT3) == SERVER_TYPE_HD) {
+            static CpldComponent cpld_fw3("slot3", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x44);
+            static VrComponent   vr_vddcpu0_fw3("slot3", "vr_vddcrcpu0", FW_VR_VDDCRCPU0);
+            static VrComponent   vr_vddcpu1_fw3("slot3", "vr_vddcrcpu1", FW_VR_VDDCRCPU1);
+            static VrComponent   vr_vdd11s3_fw3("slot3", "vr_vdd11s3", FW_VR_VDD11S3);
+          } else {
+            static CpldComponent cpld_fw3("slot3", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
+            static MeComponent   me_fw3("slot3", "me", FRU_SLOT3);
+            static VrComponent   vr_vccin_fw3("slot3", "vr_vccin", FW_VR_VCCIN);
+            static VrComponent   vr_vccd_fw3("slot3", "vr_vccd", FW_VR_VCCD);
+            static VrComponent   vr_vccinfaon_fw3("slot3", "vr_vccinfaon", FW_VR_VCCINFAON);
+          }
+          static VrComponent     vr_fw3("slot3", "vr", FW_VR);
+
+          //slot3 1ou vr
+          if ((config_status = bic_is_exp_prsnt(FRU_SLOT3)) < 0) {
+            config_status = 0;
+          }
+          if ((config_status & PRESENT_1OU) == PRESENT_1OU) {
+            if (isRainbowFalls(FRU_SLOT3)) {
+              static VrComponent  vr_1ou_va0v8_fw3("slot3", "1ou_vr_v9asica", FW_1OU_VR_V9_ASICA);
+              static VrComponent  vr_1ou_vddqab_fw3("slot3", "1ou_vr_vddqab", FW_1OU_VR_VDDQAB);
+              static VrComponent  vr_1ou_vddqcd_fw3("slot3", "1ou_vr_vddqcd", FW_1OU_VR_VDDQCD);
+              static CxlComponent cxl_1ou_fw3("slot3", "cxl", "1ou", FW_1OU_CXL);
+            }
+          }
+        } else {
+          static CpldComponent   cpld_fw3("slot3", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
+        }
+
+        //slot4 sb bic/cpld/bios
         static BicFwComponent    bic_fw4("slot4", "bic", "sb", FW_SB_BIC);
         static BicFwComponent    bic_rcvy_fw4("slot4", "bic_rcvy", "sb", FW_BIC_RCVY);
         static CpldComponent     cpld_fw4("slot4", "cpld", "sb", FW_CPLD, LCMXO3_4300C, 0x40);
         static BiosComponent     bios_fw4("slot4", "bios", FRU_SLOT4, FW_BIOS);
-        static MeComponent       me_fw4("slot4", "me", FRU_SLOT4);
-        static VrComponent       vr_fw4("slot4", "vr", FW_VR);
-        static VrComponent       vr_vccin_fw4("slot4", "vr_vccin", FW_VR_VCCIN);
-        static VrComponent       vr_vccd_fw4("slot4", "vr_vccd", FW_VR_VCCD);
-        static VrComponent       vr_vccinfaon_fw4("slot4", "vr_vccinfaon", FW_VR_VCCINFAON);
 
         //slot4 1ou bic/cpld
         static BicFwComponent    bic_1ou_fw4("slot4", "1ou_bic", "1ou", FW_1OU_BIC);
         static CpldComponent     cpld_1ou_fw4("slot4", "1ou_cpld", "1ou", FW_1OU_CPLD, 0, 0);
+
+        if (fby35_common_is_fru_prsnt(FRU_SLOT4, &prsnt)) {
+          prsnt = 0;
+        }
+        if (prsnt) {
+          if (get_board_rev(FRU_SLOT4, BOARD_ID_SB, &board_rev) == 0) {
+            if (IS_BOARD_REV_MPS(board_rev)) {
+              static MP5990Component hsc_fw4("slot4", "hsc", FRU_SLOT4, 1, 0x16);
+            }
+          }
+
+          //slot4 me/vr
+          static MeComponent     me_fw4("slot4", "me", FRU_SLOT4);
+          static VrComponent     vr_vccin_fw4("slot4", "vr_vccin", FW_VR_VCCIN);
+          static VrComponent     vr_vccd_fw4("slot4", "vr_vccd", FW_VR_VCCD);
+          static VrComponent     vr_vccinfaon_fw4("slot4", "vr_vccinfaon", FW_VR_VCCINFAON);
+          static VrComponent     vr_fw4("slot4", "vr", FW_VR);
+        }
       }
   }
 

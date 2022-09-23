63337db4b54c1a335c46c1034f01f8f50d9305c5
Mike Choi <mikechoi@fb.com>
2022-09-23 09:59:51 -0700

    netlakemtp: Switch ME back to operational after BIOS update (#2370)
    Summary:
    - ME was switched to recovery mode before BIOS update
    - BMC have to switch ME back to operational after update complete
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2370
    
    Test Plan:
    - Do BIOS update then check ME status - pass root@bmc-oob:~# fw-util server --version
    BIOS Version: NLE03
    ...
    root@bmc-oob:~# fw-util server --update bios NLE02.bin Flashing to device: /dev/mtd13
    Erasing block: 512/512 (100%)
    Writing kb: 32768/32768 (100%)
    Verifying kb: 32768/32768 (100%)
    Upgrade of server : bios succeeded
    root@bmc-oob:~# sol-util server
    ...
                                   Aptio Setup - AMI
                        Platform Configuration
    /----------------------------------------------------+-------------------------\
    |  General ME Configuration                         ^|The altitude of the     ^|
    |  Oper. Firmware Version   11:5.0.4.17             *|platform location above *|
    |  Backup Firmware          N/A                     *|the sea level,          *|
    |  Version                                          *|expressed in meters.    *|
    |  Recovery Firmware        11:5.0.4.17             *|The hex number is       *|
    |  Version                                          *|decoded as 2's          +|
    |  ME Firmware Status https://github.com/facebookexternal/openbmc.wiwynn/issues/1    0x00000255              *|complement signed       +|
    |  ME Firmware Status https://github.com/facebookexternal/openbmc.wiwynn/issues/2    0x8911A006              +|integer.                v|
    |    Current State          Operational             +|                         |
    |    Error Code             No Error                +|-------------------------|
    |    Recovery Cause         N/A                     +|><: Select Screen        |
    |  Altitude                 8000                    +|^v: Select Item          |
    |  MCTP Bus Owner           0                       +|Enter: Select            |
    |  Server ME firmware features list                 +|+/-: Change Opt.         |
    |    SiEn                                           +|F1: General Help         |
    |    NodeManager                                    +|F2: Previous Values      |
    |    PECIProxy                                      +|F3: Optimized Defaults   |
    |    ICC                                            v|F4: Save & Exit          |
    |                                                    |ESC: Exit                |
    \----------------------------------------------------+-------------------------/
                        Version 2.22.1285 Copyright (C) 2022 AMI
                                                                                 AB
    root@bmc-oob:~# fw-util server --version
    BIOS Version: NLE02
    ...
    
    Reviewed By: GoldenBug
    
    fbshipit-source-id: 474e287d6148567eeb5ce113698633ce27a08fbd

--- meta-facebook/meta-netlakemtp/recipes-netlakemtp/fw-util/files/bmc_bios.cpp
+++ meta-facebook/meta-netlakemtp/recipes-netlakemtp/fw-util/files/bmc_bios.cpp
@@ -3,6 +3,7 @@
 #include "bios.h"
 #include <openbmc/pal.h>
 #include <thread>
+#include <facebook/netlakemtp_common.h>
 
 using namespace std;
 
@@ -16,6 +17,7 @@ class BmcBiosComponent : public BiosComponent {
     int fupdate(std::string image) override;
     int unbindDevice();
     int check_image(const char *path) override;
+    void quit_ME_recovery();
 };
 
 int BmcBiosComponent::unbindDevice() {
@@ -30,13 +32,38 @@ int BmcBiosComponent::unbindDevice() {
   return 0;
 }
 
+void BmcBiosComponent::quit_ME_recovery() {
+  int ret = 0;
+  /*
+    Byte 1:3 = Intel Manufacturer ID – 000157h, LS byte first.
+    Byte 4 - Command = 01h Restart using Recovery Firmware.
+             Command = 02h Restore Factory Default Variable values and restart the intel ME FW.
+  */
+  uint8_t tbuf[] = {0x57, 0x1, 0x0, 0x2};
+  /*
+    Byte 1 – Completion Code = 00h – Success
+             Completion Code = 81h – Unsupported Command parameter value
+    Byte 2:4 = Intel Manufacturer ID – 000157h, LS byte first.
+  */
+  uint8_t rlen = 4;
+  uint8_t rbuf_quitMERecovery[rlen];
+
+  ret = netlakemtp_common_me_ipmb_wrapper(NETFN_NM_REQ, CMD_NM_FORCE_ME_RECOVERY, tbuf, sizeof(tbuf), rbuf_quitMERecovery, &rlen);
+  if (ret < 0) {
+    cout << "Failed to switch ME from recovery to operational." << endl;
+  }
+}
+
 int BmcBiosComponent::update(string image) {
   int res = 0;
   res = unbindDevice();
   if (res < 0) {
     return -1;
   }
-  return BiosComponent::update(image, false);
+
+  res = BiosComponent::update(image, false);
+  quit_ME_recovery();
+  return res;
 }
 
 int BmcBiosComponent::fupdate(string image) {
@@ -45,7 +72,10 @@ int BmcBiosComponent::fupdate(string image) {
   if (res < 0) {
     return -1;
   }
-  return BiosComponent::update(image, true);
+
+  res = BiosComponent::update(image, true);
+  quit_ME_recovery();
+  return res;
 }
 
 int BmcBiosComponent::check_image(const char *path){

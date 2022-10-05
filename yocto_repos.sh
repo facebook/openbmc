b9f8fd1d2e1f5b881a0607af76f725492d5231f2
Amithash Prasad <amithash@meta.com>
2022-10-05 09:47:17 -0700

    Add rocko github patch to use python3
    Summary: As title
    
    Test Plan: Build using github checkout
    
    Reviewed By: peterdelevoryas
    
    fbshipit-source-id: ce65a7a901e8b3de95ffdad88f6ac2491bea4763

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -10,7 +10,7 @@ rocko_repos=(
     meta-security:74860b2
     poky:5f660914cd # bitbake: bitbake-user-manual: Fixed section head typo
 )
-rocko_poky_patch="0001-rocko-backport-support-for-override.patch"
+rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch"
 lf_master_repos=(
     lf-openbmc:1361b328c
 )

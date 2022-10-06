1c0e2527d9b6e6d7098d29232f614aa297b2d037
=?UTF-8?q?Joan=20Olguy=20Can=C3=A9us?= <joancaneus@meta.com>
2022-10-06 13:59:07 -0700

    Elbert: bios_util.sh Aboot configuration support (#270)
    Summary:
    This change provides support for configuring Aboot configuration when an Aboot image is written using the bios_util.sh utility. This is needed in order to use Aboot as the bios so that the boot methods feature is configured.
    
    When writing or verifying, a temporary image is first created with the aboot_conf section programmed. The default configuration for elbert is:
    BOOT_METHOD1=IPV6_PXE,BOOT_METHOD2 (https://github.com/facebook/openbmc/commit/b604789fffa50de8b7ab217cc0b08ecbf36f9ef4)=LOCAL,BOOT_METHOD3 (https://github.com/facebook/openbmc/commit/e74ea59d33dacadae466d3373bc6c8bbf726057e)=ARISTA
    
    In order for this change to be backward compatible with non-Aboot images such as coreboot, the Aboot version is parsed from the image to be programmed and the Aboot configuration is only programmed for Aboot images.
    
    1. Ran bios_util.sh write using an Aboot image. Verified the aboot_conf section was populated and boot methods were present when booting to Aboot.
    2. Ran bios_util.sh verify using an Aboot image when boot methods were populated.
    3. Ran bios_util.sh verify using an Aboot image when boot methods were not populated. The verification failed.
    4. Ran bios_util.sh write using a coreboot image. Verified that the aboot_conf section was not changed and the image was bootable.
    5. Ran bios_util.sh verify using a coreboot image.
    6. With the Aboot image, booted a Centos image and validated that FPGAs were accessible via PCI.
    
    X-link: https://github.com/facebookexternal/openbmc.arista/pull/270
    
    Reviewed By: mikechoifb
    
    fbshipit-source-id: 2eb65a1f3850b092739e60ae5ac3ef64832c4f20

--- meta-facebook/meta-elbert/recipes-utils/openbmc-utils/files/bios_util.sh
+++ meta-facebook/meta-elbert/recipes-utils/openbmc-utils/files/bios_util.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
 # shellcheck disable=SC1091
+# shellcheck disable=SC2012
+# shellcheck disable=SC2039
 . /usr/local/bin/openbmc-utils.sh
 
 # Check if another fw upgrade is ongoing
 check_fwupgrade_running
 
-trap disconnect_spi INT TERM QUIT EXIT
+trap cleanup INT TERM QUIT EXIT
 
 SCM_SPI="spi1.0"
 SCM_SPIDEV="spidev1.0"
 SCM_MTD=""
+ABOOT_CONF_START=$((0x9FA000))
+FLASH_SIZE=$((0x1000000))
+ABOOT_CONF_SIZE=$((0x5000))
+SECTION_BLOCK_SIZE=$((0x1000))
+
+# Temp files for storing bios file.
+TEMP_BIOS_IMAGE="/tmp/tmp_bios_image"
+# Temp file for storing aboot_conf data
+TEMP_ABOOT_CONF="/tmp/aboot_conf.bin"
+
+DEFAULT_ABOOT_CONF="BOOT_METHOD1=IPV6_PXE,BOOT_METHOD2=LOCAL,BOOT_METHOD3=ARISTA"
+
+cleanup() {
+    disconnect_spi
+    rm -f $TEMP_ABOOT_CONF $TEMP_BIOS_IMAGE
+}
 
 bind_spi_nor() {
     # Unbind spidev
@@ -80,21 +98,76 @@ read_flash() {
     return 0
 }
 
+create_bios_image() {
+    start_block="$(($1 / SECTION_BLOCK_SIZE))"
+    num_blocks="$(($2 / SECTION_BLOCK_SIZE))"
+    rm -f "$TEMP_BIOS_IMAGE"
+    dd if="$3" of="$TEMP_BIOS_IMAGE" bs="$SECTION_BLOCK_SIZE" count="$start_block" \
+       2> /dev/null
+    dd if="$4" bs="$SECTION_BLOCK_SIZE" count="$num_blocks" >> "$TEMP_BIOS_IMAGE" \
+       2> /dev/null
+    end_blocks=$(( (FLASH_SIZE / SECTION_BLOCK_SIZE) - start_block - num_blocks ))
+    dd if="$3" bs="$SECTION_BLOCK_SIZE" count="$end_blocks" skip=$((start_block + num_blocks)) \
+       >> "$TEMP_BIOS_IMAGE" 2> /dev/null
+}
+
+create_aboot_conf() {
+    echo "Using Aboot conf: $1"
+
+    nvs="$1,"
+
+    rm -f "$TEMP_ABOOT_CONF"
+    touch "$TEMP_ABOOT_CONF"
+    while [ -n "${nvs%%,*}" ]; do
+        nv="${nvs%%,*}"
+        name="${nv%%=*}"
+        if [ "$name" == "$nv" ]; then
+            echo "Invalid name-value argument $nv" >&2
+            return 1
+        fi
+        echo -n "${nv%%=*}=" >> "$TEMP_ABOOT_CONF"
+        echo -n "${nv#*=}" | base64 >> "$TEMP_ABOOT_CONF"
+        nvs="${nvs#*,}"
+    done
+
+    size="$(ls -l "$TEMP_ABOOT_CONF" | awk '{print $5}')"
+    pad_size="$((ABOOT_CONF_SIZE - size))"
+    dd if=/dev/zero bs=1 count="$pad_size" >> "$TEMP_ABOOT_CONF" 2> /dev/null
+}
+
+aboot_version() {
+    grep -a CONFIG_LOCALVERSION "$1" 2> /dev/null | awk -F'"' '{print $2}'
+}
+
 if [ "$1" = "erase" ]; then
     echo "Erasing flash content ..."
     flashrom -f -p linux_mtd:dev="$SCM_MTD" -E || exit 1
 elif [ "$1" = "read" ]; then
     retry_command 5 read_flash "$2" || exit 1
 elif [ "$1" = "verify" ]; then
+    bios_image="$2"
+    if [ -n "$(aboot_version "$bios_image")" ]; then
+        create_aboot_conf "$DEFAULT_ABOOT_CONF" || exit 1
+        create_bios_image "$ABOOT_CONF_START" "$ABOOT_CONF_SIZE" \
+                          "$bios_image" "$TEMP_ABOOT_CONF" || exit 1
+        bios_image="$TEMP_BIOS_IMAGE"
+    fi
     echo "Verifying flash content..."
-    flashrom -f -p linux_mtd:dev="$SCM_MTD" -v "$2" || exit 1
+    flashrom -f -p linux_mtd:dev="$SCM_MTD" -v "$bios_image" || exit 1
 elif [ "$1" = "write" ]; then
+    bios_image="$2"
+    if [ -n "$(aboot_version "$bios_image")" ]; then
+        create_aboot_conf "$DEFAULT_ABOOT_CONF" || exit 1
+        create_bios_image "$ABOOT_CONF_START" "$ABOOT_CONF_SIZE" \
+                          "$bios_image" "$TEMP_ABOOT_CONF" || exit 1
+        bios_image="$TEMP_BIOS_IMAGE"
+    fi
     echo "Writing flash content..."
-    flashrom -n -f -p linux_mtd:dev="$SCM_MTD" -w "$2" || exit 1
+    flashrom -n -f -p linux_mtd:dev="$SCM_MTD" -w "$bios_image" || exit 1
     sleep 1
     echo "Verifying flash content..."
     # Retry verification up to 5 times
-    retry_command 5 flashrom -f -p linux_mtd:dev="$SCM_MTD" -v "$2" || exit 1
+    retry_command 5 flashrom -f -p linux_mtd:dev="$SCM_MTD" -v "$bios_image" || exit 1
 else
     usage
 fi

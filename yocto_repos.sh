29fdb3c2f7362c6be609d3f9bf0fe62675f6edc6
Patrick Williams <patrickw3@meta.com>
2022-10-18 10:58:22 -0700

    yocto: update master
    Summary:
    Update master at e0ff06a15
    update-submodule: yocto/lf-master
    
    Regenerate kernel patches due to upstream 5.15 -> 6.0 upgrade.
    
    Test Plan: CIT build passes.
    
    Reviewed By: peterdelevoryas
    
    fbshipit-source-id: cfac7d71afd4ed6a8d97aaf0a8080fc8a196fae3

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch"
 lf_master_repos=(
-    lf-openbmc:f5ea9eb09
+    lf-openbmc:e0ff06a15
 )
 lf_dunfell_repos=(
     lf-openbmc:cddccf4ad

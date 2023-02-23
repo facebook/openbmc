edaf5fff93f040dc633bd79130c974acffcde7b5
Michael Garner <garnermic@meta.com>
2023-02-23 12:08:10 -0800

    GT: move pal_parse_oem_sel to pal_cover.c (#3625)
    Summary:
    1. because this oem sel log request from intel, GTI not need this log. so move to GTT pal_cover.c.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3625
    
    Test Plan: 1.Build and test pass on GT system.
    
    Reviewed By: zhdaniel12
    
    Differential Revision: D43503991
    
    fbshipit-source-id: a20ba3a9af465c4804527aedb0c5e68172e7f77c

--- meta-facebook/meta-grandteton/recipes-grandteton/plat-libs/files/pal/pal_cover.c
+++ meta-facebook/meta-grandteton/recipes-grandteton/plat-libs/files/pal/pal_cover.c
@@ -22,3 +22,18 @@ pal_parse_sel(uint8_t fru, uint8_t *sel, char *error_log)
   pal_parse_sel_helper(fru, sel, error_log);
   return 0;
 }
+
+int
+pal_parse_oem_sel(uint8_t fru, uint8_t *sel, char *error_log)
+{
+  uint8_t index = sel[11] & 0x01;
+  char* yellow_track[] = {
+    "Corrected Cache Error",
+    "Persistent Cache Fault"
+  };
+
+  // error_log size 256
+  snprintf(error_log, 256, "%s", yellow_track[index]);
+
+  return 0;
+}

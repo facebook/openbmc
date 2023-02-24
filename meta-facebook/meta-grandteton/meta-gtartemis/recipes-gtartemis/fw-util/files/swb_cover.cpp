58fb23f23eb942fc88c4fd7c42cdd4e3f69ccb09
Ian-I-Chien <ian_chien@wiwynn.com>
2023-02-24 08:26:40 -0800

    GTA: remove unused components related to swb (#2523)
    Summary:
    Summry:
    - Remove swb nic components which are not in artemis platform.
    
    Test Plan:
    - fw-util not shows swb nic components - pass
    
    Log:
    ```
    - Before Change
    root@bmc-oob:~# fw-util
    USAGE: fw-util all|FRU --version|--version-json [all|COMPONENT]
           fw-util FRU --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --force --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --dump [--]COMPONENT IMAGE_PATH
           fw-util FRU --update COMPONENT IMAGE_PATH --schedule now
           fw-util all --show-schedule
           fw-util all --delete-schedule TASK_ID
    
    FRU        : Components
    ---------- : ----------
    acb        : bic cpld pesw0 pesw1 pesw_vr
    bmc        : bmc fscd rom tpm
    mb         : bios cpld cpu0_pvdd11 cpu0_vcore0 cpu0_vcore1 cpu1_pvdd11 cpu1_vcore0 cpu1_vcore1
    meb        : bic cpld
    meb_cxl1   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl2   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl3   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl4   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl5   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl6   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl7   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl8   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    nic0       : nic0
    nic1       : nic1
    scm        : cpld
    swb        : swb_nic0 swb_nic1 swb_nic2 swb_nic3 swb_nic4 swb_nic5 swb_nic6 swb_nic7
    
    - After Change
    root@bmc-oob:~# fw-util
    USAGE: fw-util all|FRU --version|--version-json [all|COMPONENT]
           fw-util FRU --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --force --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --dump [--]COMPONENT IMAGE_PATH
           fw-util FRU --update COMPONENT IMAGE_PATH --schedule now
           fw-util all --show-schedule
           fw-util all --delete-schedule TASK_ID
    
    FRU        : Components
    ---------- : ----------
    acb        : bic cpld pesw0 pesw1 pesw_vr
    bmc        : bmc fscd rom tpm
    mb         : bios cpld cpu0_pvdd11 cpu0_vcore0 cpu0_vcore1 cpu1_pvdd11 cpu1_vcore0 cpu1_vcore1
    meb        : bic cpld
    meb_cxl1   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl2   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl3   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl4   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl5   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl6   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl7   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    meb_cxl8   : vr_a0v8_9 vr_vddq_ab vr_vddq_cd
    nic0       : nic0
    nic1       : nic1
    scm        : cpld
    root@bmc-oob:~#
    ```
    
    Reviewed By: amithash
    
    Differential Revision: D43537169
    
    Pulled By: garnermic
    
    fbshipit-source-id: 9aa44c6ac4632c0bdd40cfc1e2e8c20c5adbe791

--- meta-facebook/meta-grandteton/meta-gtartemis/recipes-gtartemis/fw-util/files/swb_cover.cpp
+++ meta-facebook/meta-grandteton/meta-gtartemis/recipes-gtartemis/fw-util/files/swb_cover.cpp
@@ -4,12 +4,3 @@ SwbBicFwComponent acb_bic("acb", "bic", ACB_BIC_BUS, ACB_BIC_EID, BIC_COMP);
 SwbBicFwComponent meb_bic("meb", "bic", MEB_BIC_BUS, MEB_BIC_EID, BIC_COMP);
 SwbPexFwComponent acb_pesw0("acb", "pesw0", ACB_BIC_BUS, ACB_BIC_EID, PEX0_COMP, 0);
 SwbPexFwComponent acb_pesw1("acb", "pesw1", ACB_BIC_BUS, ACB_BIC_EID, PEX1_COMP, 1);
-
-PLDMNicComponent swb_nic0("swb", "swb_nic0", "SWB_NIC0", 0x10, 3);
-PLDMNicComponent swb_nic1("swb", "swb_nic1", "SWB_NIC1", 0x11, 3);
-PLDMNicComponent swb_nic2("swb", "swb_nic2", "SWB_NIC2", 0x12, 3);
-PLDMNicComponent swb_nic3("swb", "swb_nic3", "SWB_NIC3", 0x13, 3);
-PLDMNicComponent swb_nic4("swb", "swb_nic4", "SWB_NIC4", 0x14, 3);
-PLDMNicComponent swb_nic5("swb", "swb_nic5", "SWB_NIC5", 0x15, 3);
-PLDMNicComponent swb_nic6("swb", "swb_nic6", "SWB_NIC6", 0x16, 3);
-PLDMNicComponent swb_nic7("swb", "swb_nic7", "SWB_NIC7", 0x17, 3);

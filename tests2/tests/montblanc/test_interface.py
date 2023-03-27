dea2d68b7f48ac3e7c18a2fd2c266b6221bc0fc3
Sittisak Sinprem <ssinprem@celestica.com>
2023-03-27 16:34:56 -0700

    cit: montblanc: add test_interface (#1483)
    Summary:
    enable test_interface test case in Montblanc cit
    
    ### TestPlan:
    Test both FUJI device and QEMU.
    
    CIT_test eth0_v4_interface in FUJI device:
    ```
    root@bmc:/mnt/data1/tests2# python cit_runner.py -p montblanc -r tests.montblanc.test_interface
    test_eth0_v4_interface (tests.montblanc.test_interface.InterfaceTest.test_eth0_v4_interface)
    Tests eth0 v4 interface ... ok
    test_eth0_v6_interface (tests.montblanc.test_interface.InterfaceTest.test_eth0_v6_interface)
    Tests eth0 v6 interface ... ok
    test_usb0_v6_interface (tests.montblanc.test_interface.InterfaceTest.test_usb0_v6_interface)
    Tests usb0 v6 interface ... ok
    
    ----------------------------------------------------------------------
    Ran 3 tests in 0.078s
    
    OK
    ```
    
    CIT_test eth0_v4_interface in QEMU:
    ```
    root@bmc:/usr/local/bin/tests2# touch dummy_qemu
    root@bmc:/usr/local/bin/tests2# python cit_runner.py -p montblanc -r tests.montblanc.test_interface
    test_eth0_v4_interface (tests.montblanc.test_interface.InterfaceTest.test_eth0_v4_interface)
    Tests eth0 v4 interface ... ok
    test_eth0_v6_interface (tests.montblanc.test_interface.InterfaceTest.test_eth0_v6_interface)
    Tests eth0 v6 interface ... ok
    test_usb0_v6_interface (tests.montblanc.test_interface.InterfaceTest.test_usb0_v6_interface)
    Tests usb0 v6 interface ... skipped 'test env is QEMU, skipped'
    
    ----------------------------------------------------------------------
    Ran 3 tests in 0.356s
    
    OK (skipped=1)
    root@bmc:/usr/local/bin/tests2#
    ```
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1483
    
    Reviewed By: rootkeeper
    
    Differential Revision: D44269987
    
    Pulled By: binhuang00
    
    fbshipit-source-id: 4f4c800a805c2712f8e95cd7fa29c07dc92b4242

---
+++ tests2/tests/montblanc/test_interface.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_interface_test import CommonInterfaceTest
+from utils.cit_logger import Logger
+from utils.test_utils import qemu_check
+
+
+"""
+Tests eth0 & usb0 v6 interface
+"""
+
+
+class InterfaceTest(CommonInterfaceTest, unittest.TestCase):
+    def test_eth0_v4_interface(self):
+        """
+        Tests eth0 v4 interface
+        """
+        self.set_ifname("eth0")
+        Logger.log_testname(name=self._testMethodName)
+        self.assertEqual(self.ping_v4(), 0, "Ping test for eth0 v4 failed")
+
+    @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
+    def test_usb0_v6_interface(self):
+        """
+        Tests usb0 v6 interface
+        """
+        self.set_ifname("usb0")
+        Logger.log_testname(self._testMethodName)
+        self.assertEqual(self.ping_v6(), 0, "Ping test for usb0 v6 failed")

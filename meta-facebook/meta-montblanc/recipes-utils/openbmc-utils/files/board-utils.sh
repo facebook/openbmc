68595b472f9862eb2fb881df79f194e6dbefc1d7
Sittisak Sinprem <ssinprem@celestica.com>
2023-04-05 13:44:28 -0700

    montblanc: implement power functions in board-utils.sh (#1481)
    Summary:
    implement power function in board-utils.sh to support wedge_power.sh
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1481
    
    Test Plan:
    compile the image with cherry-pick or merged https://github.com/facebookexternal/openbmc.celestica/issues/1479
    run Montblanc image by qemu
    test wedge_power.sh command
    ```
    root@bmc-oob:~# . openbmc-utils.sh
    root@bmc-oob:~# wedge_power.sh status
    Microserver power is off
    root@bmc-oob:~# wedge_power.sh on
    Power on x86 (userver) ... Done
    root@bmc-oob:~# wedge_power.sh on
     uServer is already on. Skipped.
    root@bmc-oob:~# sleep 5 && echo 1 > $PWR_COME_CYCLE_N  &
    root@bmc-oob:~# wedge_power.sh reset
    Power reset x86 (userver) ... Done
    [1]+  Done                    sleep 5 && echo 1 > $PWR_COME_CYCLE_N
    root@bmc-oob:~# wedge_power.sh off
    Power off x86 (userver) ... Done
    root@bmc-oob:~# wedge_power.sh reset
    x86 (userver) is off. Power on x86 now..
    Power on x86 (userver) ... Done
    root@bmc-oob:~# wedge_power.sh status
    Microserver power is on
    root@bmc-oob:~# wedge_power.sh on -f
    Power on x86 (userver) ... Done
    root@bmc-oob:~# wedge_power.sh reset -s
    Power reset the whole system ...
    Failed to reset the system!!!
    root@bmc-oob:~#
    ```
    
    Reviewed By: binhuang00
    
    Differential Revision: D44268443
    
    Pulled By: tao-ren
    
    fbshipit-source-id: f94988f8ab0941570163acf59e26028aa2bf59ff

--- meta-facebook/meta-montblanc/recipes-utils/openbmc-utils/files/board-utils.sh
+++ meta-facebook/meta-montblanc/recipes-utils/openbmc-utils/files/board-utils.sh
@@ -17,6 +17,25 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 
+#shellcheck disable=SC1091,SC2034
+# Do not change this line to openbmc-utils.sh, or it will generate a source loop.
+. /usr/local/bin/i2c-utils.sh
+
+SCMCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 1-0035)
+MCBCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 12-0060)
+
+PWR_FORCE_OFF="${SCMCPLD_SYSFS_DIR}/pwr_force_off"
+PWR_COME_EN="${SCMCPLD_SYSFS_DIR}/pwr_come_en"
+PWR_COME_CYCLE_N="${SCMCPLD_SYSFS_DIR}/pwr_come_cycle_n"
+
+TIMER_BASE_10S="${MCBCPLD_SYSFS_DIR}/timer_base_10s"
+TIMER_BASE_1S="${MCBCPLD_SYSFS_DIR}/timer_base_1s"
+TIMER_BASE_100MS="${MCBCPLD_SYSFS_DIR}/timer_base_100ms"
+TIMER_BASE_10MS="${MCBCPLD_SYSFS_DIR}/timer_base_10ms"
+TIMER_COUNTER_SETTING="${MCBCPLD_SYSFS_DIR}/timer_counter_setting"
+TIMER_COUNTER_SETTING_UPDATE="${MCBCPLD_SYSFS_DIR}/timer_counter_setting_update"
+POWER_CYCLE_GO="${MCBCPLD_SYSFS_DIR}/power_cycle_go"
+
 wedge_board_type() {
     echo 'montblanc'
 }
@@ -27,28 +46,73 @@ wedge_board_rev() {
 }
 
 userver_power_is_on() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    # TODO: need to test on real unit on bring-up
+    val1=$(head -n 1 < "$PWR_FORCE_OFF" 2> /dev/null)
+    val2=$(head -n 1 < "$PWR_COME_EN" 2> /dev/null)
+
+    if [ $((val1)) -eq $((0x1)) ] && [ $((val2)) -eq $((0x1)) ] ; then
+        return 0            # powered on
+    else
+        return 1
+    fi
 }
 
 userver_power_on() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    # TODO: need to test on real unit on bring-up
+    echo 1 > "$PWR_FORCE_OFF"
+    echo 1 > "$PWR_COME_EN"
+    return 0
 }
 
 userver_power_off() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    # TODO: need to test on real unit on bring-up
+    echo 0 > "$PWR_FORCE_OFF"
+    return 0
 }
 
 userver_reset() {
+    # TODO: need to test on real unit on bring-up
+    
+    # write 0 to trigger CPLD power cycling COMe
+    # then this bit will auto set to 1 after Power cycle finish
+    echo 0 > "$PWR_COME_CYCLE_N"
+    
+    timeout=10 #timeout 10 second
+    while [ $((timeout)) -ge 0 ]; do
+        val=$(head -n 1 < "$PWR_COME_CYCLE_N" 2> /dev/null)
+        if [ $((val)) -eq 1 ]; then
+            # reset successful
+            return 0   
+        fi
+        sleep 1
+        timeout=$((timeout-1))
+    done
+    # reset fail
+    echo " userver reset failed."
     return 1
-    echo "FIXME: feature not implemented!!"
 }
 
 chassis_power_cycle() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    # TODO: need to test on real unit on bring-up
+
+    # TIME_COUTER will be calculated 
+    # based on register TIMER_BASE (10S,1S,100MS,10MS)
+    #
+    # eg. TIMER_BASE set as 100MS
+    #     TIMER_COUNTER_SETTING set as 50
+    #     timer is 50 x 100ms = 5 sec.
+
+    echo 0 > "$TIMER_BASE_10S"
+    echo 0 > "$TIMER_BASE_1S"
+    echo 1 > "$TIMER_BASE_100MS"
+    echo 0 > "$TIMER_BASE_10MS"
+
+    echo 50 > "$TIMER_COUNTER_SETTING"
+
+    echo 1 > "$TIMER_COUNTER_SETTING_UPDATE"
+    echo 1 > "$POWER_CYCLE_GO"
+
+    return 0
 }
 
 bmc_mac_addr() {

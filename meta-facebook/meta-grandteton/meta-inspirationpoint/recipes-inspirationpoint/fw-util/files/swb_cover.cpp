6bce98962a4cf0557a369e522d6f40e3fe60c29c
roy1224 <roy.tsao@quantatw.com>
2023-04-06 14:27:00 -0700

    GT/GTI: fw-uil swb component move to common (#3744)
    Summary:
    1. as title, fix GTI can not read and update swb_cpld swb_pex_vcc FW issue.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3744
    
    Test Plan:
    Build and test pass on GTI system
    
    root@bmc-oob:~# fw-util
    USAGE: fw-util all|FRU --version|--version-json [all|COMPONENT]
           fw-util FRU --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --force --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --dump [--]COMPONENT IMAGE_PATH
           fw-util FRU --update COMPONENT IMAGE_PATH --schedule now
           fw-util all --show-schedule
           fw-util all --delete-schedule TASK_ID
    
    FRU        : Components
    ---------- : ----------
    bmc        : bmc fscd rom tpm
    hgx        : auto erot-fpga erot-gpu1 erot-gpu2 erot-gpu3 erot-gpu4 erot-gpu5 erot-gpu6 erot-gpu7 erot-gpu8 erot-hmc erot-nvswitch0 erot-nvswitch1 erot-nvswitch2 erot-nvswitch3 erot-pax fpga gpu1-fw gpu1-inforom gpu2-fw gpu2-inforom gpu3-fw gpu3-inforom gpu4-fw gpu4-inforom gpu5-fw gpu5-inforom gpu6-fw gpu6-inforom gpu7-fw gpu7-inforom gpu8-fw gpu8-inforom hmc nvswitch0-fw nvswitch0-inforom nvswitch1-fw nvswitch1-inforom nvswitch2-fw nvswitch2-inforom nvswitch3-fw nvswitch3-inforom patch pcie-switch0 pcieretimer0 pcieretimer1 pcieretimer2 pcieretimer3 pcieretimer4 pcieretimer5 pcieretimer6 pcieretimer7
    mb         : bios cpu0_pvdd11 cpu0_rt_p0v9 cpu0_vcore0 cpu0_vcore1 cpu1_pvdd11 cpu1_rt_p0v9 cpu1_vcore0 cpu1_vcore1 mb_cpld retimer0 retimer1 retimer2 retimer3 retimer4 retimer5 retimer6 retimer7
    nic0       : nic0
    nic1       : nic1
    ocpdbg     : mcu mcubl
    scm        : scm_cpld
    swb        : bic bic_recovery pex0 pex1 pex2 pex3 pex01_vcc pex23_vcc swb_cpld
    
    Reviewed By: zhdaniel12
    
    Differential Revision: D44742918
    
    fbshipit-source-id: cd6e3061e8bccbb2bcdab0d2a74300ce44afc956

--- meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/fw-util/files/swb_cover.cpp
+++ meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/fw-util/files/swb_cover.cpp
@@ -1,18 +1,3 @@
 #include "swb_common.hpp"
 
 SwbBicFwRecoveryComponent bic_recovery("swb", "bic_recovery", 3, 0x0A, BIC_COMP);
-
-namespace pldm_signed_info
-{
-GTSwbBicFwComponent bic("swb", "bic", 3, 0x0A, BIC_COMP,
-                        signed_header_t(gt_swb_comps, BIC_COMP, ASPEED));
-
-GTSwbPexFwComponent swb_pex0("swb", "pex0", 3, 0x0A, PEX0_COMP,
-                            signed_header_t(gt_swb_comps, PEX0_COMP, BROADCOM));
-GTSwbPexFwComponent swb_pex1("swb", "pex1", 3, 0x0A, PEX1_COMP,
-                            signed_header_t(gt_swb_comps, PEX1_COMP, BROADCOM));
-GTSwbPexFwComponent swb_pex2("swb", "pex2", 3, 0x0A, PEX2_COMP,
-                            signed_header_t(gt_swb_comps, PEX2_COMP, BROADCOM));
-GTSwbPexFwComponent swb_pex3("swb", "pex3", 3, 0x0A, PEX3_COMP,
-                            signed_header_t(gt_swb_comps, PEX3_COMP, BROADCOM));
-}
--- meta-facebook/meta-grandteton/recipes-grandteton/fw-util/files/swb_cover.cpp
+++ meta-facebook/meta-grandteton/recipes-grandteton/fw-util/files/swb_cover.cpp
@@ -10,27 +10,4 @@ SwbPLDMNicComponent swb_nic5("swb", "swb_nic5", "SWB_NIC5", 0x15, SWB_BUS_ID);
 SwbPLDMNicComponent swb_nic6("swb", "swb_nic6", "SWB_NIC6", 0x16, SWB_BUS_ID);
 SwbPLDMNicComponent swb_nic7("swb", "swb_nic7", "SWB_NIC7", 0x17, SWB_BUS_ID);
 
-namespace pldm_signed_info
-{
-GTSwbBicFwComponent bic("swb", "bic", SWB_BUS_ID, SWB_BIC_EID, BIC_COMP,
-                        signed_header_t(gt_swb_comps, BIC_COMP, ASPEED));
-
-GTSwbPexFwComponent swb_pex0("swb", "pex0", SWB_BUS_ID, SWB_BIC_EID, PEX0_COMP,
-                            signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-GTSwbPexFwComponent swb_pex1("swb", "pex1", SWB_BUS_ID, SWB_BIC_EID, PEX1_COMP,
-                            signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-GTSwbPexFwComponent swb_pex2("swb", "pex2", SWB_BUS_ID, SWB_BIC_EID, PEX2_COMP,
-                            signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-GTSwbPexFwComponent swb_pex3("swb", "pex3", SWB_BUS_ID, SWB_BIC_EID, PEX3_COMP,
-                            signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-
-GTSwbVrComponent vr_pex0_vcc("swb", "pex01_vcc", "VR_PEX01_VCC", SWB_BUS_ID, SWB_BIC_EID, VR0_COMP,
-                            signed_header_t(gt_swb_comps, VR0_COMP));
-GTSwbVrComponent vr_pex1_vcc("swb", "pex23_vcc", "VR_PEX23_VCC", SWB_BUS_ID, SWB_BIC_EID, VR1_COMP,
-                            signed_header_t(gt_swb_comps, VR1_COMP));
-
-GTSwbCpldComponent swb_cpld("swb", "swb_cpld", LCMXO3_9400C, SWB_BUS_ID, 0x40, &cpld_pldm_wr,
-                            SWB_BIC_EID, CPLD_COMP, signed_header_t(gt_swb_comps, CPLD_COMP, LATTICE));
-}
-
 

a66a9c5ede2a7a31ef3c869599a94ece30dcd270
Sittisak Sinprem <ssinprem@celestica.com>
2023-04-13 10:48:47 -0700

    cit: montblanc: add restapi test case (#1462)
    Summary:
    add restapi test case for Montblanc platform
    
    ### TestPlan:
    run CIT restapi on fuji unit
    
    ```
    root@bmc-oob:~# . /usr/local/bin/openbmc-utils.sh
    root@bmc-oob:~# i2c_device_add 8 0x51 24c64
    [ 1846.402873] at24 8-0051: 8 byte 24c64 EOM, writable,bytes/write
    [ 1846.476310] i2c i2c-8: nevice: Instanted device 24cat 0x51
    
    root@bmc-oob:~# sed -i 's_\["weutil"\]_["weutil","/sys/bus/i2c/devices/8-0051/eeprom"]_g' /usr/local/fbpackages/rest-api/rest_fruid.py
    root@bmc-oob:~# systemctl restart restapi
    root@bmc-oob:~# cd /usr/local/bin/tests2/
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py -r tests.montblanc.test_rest_endpoint
    test_endpoint_api (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api) ... ok
    test_endpoint_api_sys (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys) ... ok
    test_endpoint_api_sys_bmc (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_bmc) ... ok
    test_endpoint_api_sys_bmc_mtd (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_bmc_mtd) ... ok
    test_endpoint_api_sys_bmc_secondary_boot (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_bmc_secondary_boot) ... ok
    test_endpoint_api_sys_fc_present (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_fc_present) ... ok
    test_endpoint_api_sys_firmware_info_all (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_firmware_info_all) ... skipped 'not available'
    test_endpoint_api_sys_mb (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_mb) ... ok
    test_endpoint_api_sys_mb_fruid (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_mb_fruid) ... ok
    test_endpoint_api_sys_sensors (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_sensors) ... ok
    test_endpoint_api_sys_server (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_endpoint_api_sys_server) ... ok
    test_server_httperror_code_404 (tests.montblanc.test_rest_endpoint.RestEndpointTest.test_server_httperror_code_404)
    Test if REST server return 404 code when url is not accessible ... ok
    
    ----------------------------------------------------------------------
    Ran 12 tests in 2.614s
    
    OK (skipped=1)
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py -r tests.montblanc.test_rest_mmc
    test_mmc_info (tests.montblanc.test_rest_mmc.RestMMCTest.test_mmc_info) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.133s
    
    OK
    root@bmc-oob:/usr/local/bin/tests2#
    ```
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1462
    
    Reviewed By: rootkeeper
    
    Differential Revision: D43675200
    
    Pulled By: binhuang00
    
    fbshipit-source-id: 756322329a3534cbb6e781980ceb5f6f1d8b0e1a

---
+++ tests2/tests/montblanc/test_rest_endpoint.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_rest_endpoint_test import FbossRestEndpointTest
+from utils.test_utils import qemu_check
+
+
+class RestEndpointTest(FbossRestEndpointTest, unittest.TestCase):
+    """
+    Input data to the test needs to be a list like below.
+    User can choose to sends these lists from jsons too.
+    """
+
+    FRUID_SMB_ENDPOINT = "/api/sys/mb/fruid"
+
+    # "/api/sys"
+    def set_endpoint_sys_attributes(self):
+        self.endpoint_sys_attrb = [
+            "server",
+            "bmc",
+            "mb",
+            "gpios",
+        ]
+
+    @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
+    # "/api/sys"
+    def test_endpoint_api_sys(self):
+        self.set_endpoint_sys_attributes()
+        self.verify_endpoint_attributes(
+            FbossRestEndpointTest.SYS_ENDPOINT, self.endpoint_sys_attrb
+        )
+
+    # "/api/sys/sensors"
+    def set_endpoint_sensors_attributes(self):
+        self.endpoint_sensors_attrb = []
+
+    # "/api/sys/server"
+    def set_endpoint_server_attributes(self):
+        self.endpoint_server_attrb = ["BIC_ok", "status"]
+
+    # "/api/sys/mb/fruid"
+    def set_endpoint_fruid_attributes(self):
+        self.endpoint_fruid_attrb = self.FRUID_ATTRIBUTES
+
+    @unittest.skip("not available")
+    # "/api/sys/firmware_info/all"
+    def test_endpoint_api_sys_firmware_info_all(self):
+        pass
---
+++ tests2/tests/montblanc/test_rest_mmc.py
@@ -0,0 +1,27 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+import unittest
+
+import common.base_rest_mmc
+from utils.test_utils import qemu_check
+
+
+@unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
+class RestMMCTest(common.base_rest_mmc.RestMMCTest):
+    pass  # just run common tests

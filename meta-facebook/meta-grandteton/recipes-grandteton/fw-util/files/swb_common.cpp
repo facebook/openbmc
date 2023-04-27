66bd089ce0e2686832eb2d5775ffb83a8102c33f
Ian-I-Chien <ian_chien@wiwynn.com>
2023-04-27 15:07:28 -0700

    GTA: FIX BIC version and FW component showing error (#2584)
    Summary:
    - Currently, CB/MC bic version will shows error and there are SWB components showing in fw-util
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2584
    
    Test Plan:
    - Built pass on GTI/GT/GTA and tested on GTA. - pass
    - fw-util mc/cb bic update/version can work and no more swb components. - pass
    
    Log:
    ```
    root@bmc-oob:~# fw-util
    USAGE: fw-util all|FRU --version|--version-json [all|COMPONENT]
           fw-util FRU --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --force --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --dump [--]COMPONENT IMAGE_PATH
           fw-util FRU --update COMPONENT IMAGE_PATH --schedule now
           fw-util all --show-schedule
           fw-util all --delete-schedule TASK_ID
    
    FRU        : Components
    ---------- : ----------
    bmc        : bmc fscd rom tpm
    cb         : bic cpld pesw0 pesw1 pesw_vr
    mb         : bios cpld cpu0_pvdd11 cpu0_vcore0 cpu0_vcore1 cpu1_pvdd11 cpu1_vcore0 cpu1_vcore1 retimer0 retimer4
    mc         : bic cpld cxl1 cxl2 cxl3 cxl4 cxl5 cxl6 cxl7 cxl8
    mc_cxl1    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl2    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl3    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl4    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl5    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl6    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl7    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl8    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    nic0       : nic0
    nic1       : nic1
    scm        : cpld
    root@bmc-oob:~# fw-util all --version bic
    CB BIC Version:  cb2023.12.01
    MC BIC Version:  mc2023.08.e8
    root@bmc-oob:~
    root@bmc-oob:~# fw-util mc --force --update bic ./ATMC_2023_08_E8.bin
    The image is not a valid PLDM image. update with IPMI over PLDM.
    updating fw on bus 9
    updated: 100 %
    Elapsed time:  27   sec.
    Force upgrade of mc : bic succeeded
    root@bmc-oob:~# fw-util cb --force --update bic ./ATCB_2023_08_E1.bin
    The image is not a valid PLDM image. update with IPMI over PLDM.
    updating fw on bus 3
    updated: 100 %
    Elapsed time:  17   sec.
    Force upgrade of cb : bic succeeded
    root@bmc-oob:~#
    ```
    
    Reviewed By: amithash
    
    Differential Revision: D45112082
    
    fbshipit-source-id: ae0977f4b28672fc3cbdee68bce9901ec5dbe437

--- meta-facebook/meta-grandteton/recipes-grandteton/fw-util/files/swb_common.cpp
+++ meta-facebook/meta-grandteton/recipes-grandteton/fw-util/files/swb_common.cpp
@@ -192,17 +192,14 @@ int swb_fw_update (uint8_t fruid, uint8_t bus, uint8_t eid, uint8_t target, uint
     // Send data to Bridge-IC
 
     if (send_update_packet(bus, eid, buf, count, target, offset)) {
-      if (comp.compare(0, 3, "cxl") == 0) {
-        retry--;
-        if (retry >= 0) {
-          sleep(1);
-          continue;
-        } else {
-          cout << endl << "Retry reached MAX, update failed!" << endl;
-          goto exit;
-        }
+      retry --;
+      if (retry >= 0) {
+        sleep(1);
+        continue;
+      } else {
+        cout << endl << "Retry reached MAX, update failed!" << endl;
+        goto exit;
       }
-      goto exit;
     }
 
     retry = MAX_CXL_FW_UPDATE_RETRY;
@@ -450,6 +447,13 @@ int SwbBicFwComponent::get_version(json& j) {
     j["VERSION"] = "NA";
   } else if (ver[1] == 7){
     stringstream ver_str;
+    char ver_char = '0';
+    if (pal_is_artemis()) {
+      ver_char = ver[6];
+      ver_str << ver_char;
+      ver_char = ver[7];
+      ver_str << ver_char;
+    }
     ver_str << std::hex << std::setfill('0')
       << std::setw(2) << +ver[2]
       << std::setw(2) << +ver[3] << '.'
@@ -828,6 +832,11 @@ int GTPldmComponent::gt_get_version(json& j, const string& fru, const string& co
   string active_key = fmt::format("swb_{}_active_ver",  pldm_signed_info::comp_str_t.at(target));
   string active_ver;
 
+  // TODO: will remove ipmi get version while pldm get version is supported
+  if (pal_is_artemis()) {
+    return comp_version(j);
+  }
+
   try {
     active_ver = kv::get(active_key, kv::region::temp);
     j["VERSION"] = active_ver;
@@ -976,23 +985,31 @@ int SwbPLDMNicComponent::update(string image)
 
 namespace pldm_signed_info
 {
-GTSwbBicFwComponent bic("swb", "bic", SWB_BUS_ID, SWB_BIC_EID, BIC_COMP,
+  class swb_fw_common_config {
+    public:
+      swb_fw_common_config() {
+        if (!pal_is_artemis()) {
+          static GTSwbBicFwComponent bic("swb", "bic", SWB_BUS_ID, SWB_BIC_EID, BIC_COMP,
                         signed_header_t(gt_swb_comps, BIC_COMP, ASPEED));
 
-GTSwbPexFwComponent swb_pex0("swb", "pex0", SWB_BUS_ID, SWB_BIC_EID, PEX0_COMP,
+          static GTSwbPexFwComponent swb_pex0("swb", "pex0", SWB_BUS_ID, SWB_BIC_EID, PEX0_COMP,
                             signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-GTSwbPexFwComponent swb_pex1("swb", "pex1", SWB_BUS_ID, SWB_BIC_EID, PEX1_COMP,
+          static GTSwbPexFwComponent swb_pex1("swb", "pex1", SWB_BUS_ID, SWB_BIC_EID, PEX1_COMP,
                             signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-GTSwbPexFwComponent swb_pex2("swb", "pex2", SWB_BUS_ID, SWB_BIC_EID, PEX2_COMP,
+          static GTSwbPexFwComponent swb_pex2("swb", "pex2", SWB_BUS_ID, SWB_BIC_EID, PEX2_COMP,
                             signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
-GTSwbPexFwComponent swb_pex3("swb", "pex3", SWB_BUS_ID, SWB_BIC_EID, PEX3_COMP,
+          static GTSwbPexFwComponent swb_pex3("swb", "pex3", SWB_BUS_ID, SWB_BIC_EID, PEX3_COMP,
                             signed_header_t(gt_swb_comps, COMPONENT_VERIFY_SKIPPED, BROADCOM));
 
-GTSwbVrComponent vr_pex0_vcc("swb", "pex01_vcc", "VR_PEX01_VCC", SWB_BUS_ID, SWB_BIC_EID, VR0_COMP,
+          static GTSwbVrComponent vr_pex0_vcc("swb", "pex01_vcc", "VR_PEX01_VCC", SWB_BUS_ID, SWB_BIC_EID, VR0_COMP,
                             signed_header_t(gt_swb_comps, VR0_COMP));
-GTSwbVrComponent vr_pex1_vcc("swb", "pex23_vcc", "VR_PEX23_VCC", SWB_BUS_ID, SWB_BIC_EID, VR1_COMP,
+          static GTSwbVrComponent vr_pex1_vcc("swb", "pex23_vcc", "VR_PEX23_VCC", SWB_BUS_ID, SWB_BIC_EID, VR1_COMP,
                             signed_header_t(gt_swb_comps, VR1_COMP));
 
-GTSwbCpldComponent swb_cpld("swb", "swb_cpld", LCMXO3_9400C, SWB_BUS_ID, 0x40, &cpld_pldm_wr,
+          static GTSwbCpldComponent swb_cpld("swb", "swb_cpld", LCMXO3_9400C, SWB_BUS_ID, 0x40, &cpld_pldm_wr,
                             SWB_BIC_EID, CPLD_COMP, signed_header_t(gt_swb_comps, CPLD_COMP, LATTICE));
+        }
+      }
+  };
+  swb_fw_common_config _swb_fw_common_config;
 }

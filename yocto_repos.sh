5a5c97dfa4f2a570daccd2349f5d79a6f4ec879f
generatedunixname89002005368312 <generatedunixname89002005368312@fb.com>
2023-04-29 02:35:54 -0700

    yocto: update master
    Summary:
    Update master at cf23cbd2e
    update-submodule: yocto/lf-master
    
    Test Plan: CIT build passes.
    
    Reviewed By: williamspatrick
    
    Differential Revision: D45416423
    
    fbshipit-source-id: c5e6d0d5b8e58e56e727fc3f39892463a5d7ab30

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -13,7 +13,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch"
 lf_master_repos=(
-    lf-openbmc:89b5af680
+    lf-openbmc:cf23cbd2e
 )
 lf_dunfell_repos=(
     lf-openbmc:61a2d43a1

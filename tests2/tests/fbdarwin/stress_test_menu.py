ffff86b6dd1d682c345907f678a1467080dcb3a3
Mike Choi <mikechoi@meta.com>
2023-05-03 15:21:28 -0700

    Adding fbdarwin stress test menu for CIT
    Summary: [CIT/stress] Adding fbdarwin stress test menu for CIT
    
    Test Plan: python3 tests2/cit_runner.py --platform fbdarwin --list-tests --start-dir tests2/tests/ --stress |pastry works as in P714779199
    
    Reviewed By: binhuang00
    
    Differential Revision: D45503427
    
    fbshipit-source-id: a788ee23d3d40975cf2b8071b8f036e53a6353c2

---
+++ tests2/tests/fbdarwin/stress_test_menu.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+# In order to add any test into the stress test list, please simply import
+# the classes here. If you want to import all classes from a file,
+# use "import *"
+# flake8: noqa
+from test_eeprom import *
+from test_emmc import *
+from test_cpu_utilization import *

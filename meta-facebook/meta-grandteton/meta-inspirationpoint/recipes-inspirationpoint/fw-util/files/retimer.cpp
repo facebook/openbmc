4c123119800e1ff37fd2eee6ed4245f894b44b06
Ian-I-Chien <ian_chien@wiwynn.com>
2023-05-08 12:25:48 -0700

    GTA: Fix EVT1 MB shows 8 Retimer components via fw-util
    Summary:
    - Currently, fw-util considers only 2 and 8 retimer components. This commit adds EVT1 situation which there is no Retimer on MB.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2601
    
    Test Plan:
    - Built pass on GTI/GT/GTA and fw-util works successfully. - pass
    
    Log:
    ```
    root@bmc-oob:~# fw-util
    USAGE: fw-util all|FRU --version|--version-json [all|COMPONENT]
           fw-util FRU --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --force --update [--]COMPONENT IMAGE_PATH
           fw-util FRU --dump [--]COMPONENT IMAGE_PATH
           fw-util FRU --update COMPONENT IMAGE_PATH --schedule now
           fw-util all --show-schedule
           fw-util all --delete-schedule TASK_ID
    
    FRU        : Components
    ---------- : ----------
    bmc        : bmc fscd rom tpm
    cb         : bic cpld pesw0 pesw1 pesw_vr
    mb         : bios cpld cpu0_pvdd11 cpu0_vcore0 cpu0_vcore1 cpu1_pvdd11 cpu1_vcore0 cpu1_vcore1 retimer0 retimer4
    mc         : bic cpld cxl1 cxl2 cxl3 cxl4 cxl5 cxl6 cxl7 cxl8
    mc_cxl1    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl2    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl3    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl4    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl5    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl6    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl7    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    mc_cxl8    : vr_p0v89a vr_p0v8d_pvddq_ab vr_pvddq_cd
    nic0       : nic0
    nic1       : nic1
    scm        : cpld
    root@bmc-oob:~#
    root@bmc-oob:~# fw-util all --version
    BMC Version: gtartemis-2c42ac93216-dirty
    BMC Version After Activated: gtartemis-2c42ac93216-dirty
    Fan Speed Controller Version: grandteton-v8
    ROM Version: gtartemis-e0ee78adfe2-dirty
    ROM Version After Activated: gtartemis-e0ee78adfe2-dirty
    TPM Version: 7.85
    CB BIC Version: cb2023.12.01
    CB CPLD Version: 00040E06
    CB PESW0 Version: 52.d7.00.06
    CB PESW1 Version: 52.d7.01.06
    VR_PESW_VCC Version: Infineon 77882c92, Remaining Write: 22
    BIOS Version: ATME06_70_pct
    MB CPLD Version: 00010002
    VR_CPU0_PVDD11 Version: Infineon fbda221c, Remaining Write: 22
    VR_CPU0_VCORE0/SOC Version: Infineon d4b5f0ec, Remaining Write: 22
    VR_CPU0_VCORE1/PVDDIO Version: Infineon 9aa4c275, Remaining Write: 22
    VR_CPU1_PVDD11 Version: Infineon fbda221c, Remaining Write: 22
    VR_CPU1_VCORE0/SOC Version: Infineon d4b5f0ec, Remaining Write: 22
    VR_CPU1_VCORE1/PVDDIO Version: Infineon 9aa4c275, Remaining Write: 22
    retimer0 Version: 1.30.1
    retimer4 Version: 1.31.6
    ...
    ```
    
    # Description
    
    Please include a summary of the change and which issue is fixed.
    
    # Motivation
    
    Please include an explanation of why you these changes are necessary
    
    Please describe the tests that you ran to verify your changes. Provide instructions so we can reproduce.
    
    Reviewed By: williamspatrick, wangx6f
    
    Differential Revision: D45648212
    
    fbshipit-source-id: a68c7465665444a437d9c4b246d33a1391ea0525

--- meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/fw-util/files/retimer.cpp
+++ meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/fw-util/files/retimer.cpp
@@ -5,12 +5,11 @@
 #include <openbmc/platform.h>
 #include <openbmc/libgpio.h>
 #include "vr_fw.h"
+#include <openbmc/kv.h>
 
 #define MAX_RETRY 10
 #define RT_LOCK "/tmp/pal_rt_lock"
 
-const char * rev_id0 = "FAB_BMC_REV_ID0";
-const char * rev_id1 = "FAB_BMC_REV_ID1";
 const char * RST_PESET = "RST_PERST_CPUx_SWB_N";
 
 class RetimerComponent : public Component {
@@ -89,25 +88,47 @@ class RetimerComponent : public Component {
     }
 };
 
+// rev_id[2:0] | ID2 | ID1 | ID0 |
+// 000         |  0  |  0  |  0  | EVT  (no retimer)
+// 001         |  0  |  0  |  1  | DVT1 (8 retimer)
+// 010         |  0  |  1  |  0  | DVT2 (2 retimer)
+// 011         |  0  |  1  |  1  | PVT  (TBD)
+
 class RetimerSysConfig {
+  char value[MAX_VALUE_LEN] = {0};
+  uint8_t mb_rev = 0;
   public:
     RetimerSysConfig() {
-      if (gpio_get_value_by_shadow(rev_id0) == 0 &&
-          gpio_get_value_by_shadow(rev_id1) == 1) {
-        static RetimerComponent rt0_comp("mb", "retimer0", 60);
-        static RetimerComponent rt4_comp("mb", "retimer4", 64);
+
+      if (kv_get("mb_rev", value, NULL, 0) == 0) {
+        mb_rev = atoi(value);
+      } else {
+        std::cout << "Get MB REV ID Failed" << std:: endl;
+        return;
       }
-      else {
-        static RetimerComponent rt0_comp("mb", "retimer0", 60);
-        static RetimerComponent rt1_comp("mb", "retimer1", 61);
-        static RetimerComponent rt2_comp("mb", "retimer2", 62);
-        static RetimerComponent rt3_comp("mb", "retimer3", 63);
-        static RetimerComponent rt4_comp("mb", "retimer4", 64);
-        static RetimerComponent rt5_comp("mb", "retimer5", 65);
-        static RetimerComponent rt6_comp("mb", "retimer6", 66);
-        static RetimerComponent rt7_comp("mb", "retimer7", 67);
-        static VrComponent rt_vr0("mb", "cpu0_rt_p0v9", "VR_CPU0_RETIMER_P0V9");
-        static VrComponent rt_vr1("mb", "cpu1_rt_p0v9", "VR_CPU1_RETIMER_P0V9");
+
+      switch (mb_rev) {
+        case 0: { // No Retimer
+          break;
+        }
+        case 2: { // 2 Retimer
+          static RetimerComponent rt0_comp("mb", "retimer0", I2C_BUS_60);
+          static RetimerComponent rt4_comp("mb", "retimer4", I2C_BUS_64);
+          break;
+        }
+        default: { // 8 Retimer
+          static RetimerComponent rt0_comp("mb", "retimer0", I2C_BUS_60);
+          static RetimerComponent rt1_comp("mb", "retimer1", I2C_BUS_61);
+          static RetimerComponent rt2_comp("mb", "retimer2", I2C_BUS_62);
+          static RetimerComponent rt3_comp("mb", "retimer3", I2C_BUS_63);
+          static RetimerComponent rt4_comp("mb", "retimer4", I2C_BUS_64);
+          static RetimerComponent rt5_comp("mb", "retimer5", I2C_BUS_65);
+          static RetimerComponent rt6_comp("mb", "retimer6", I2C_BUS_66);
+          static RetimerComponent rt7_comp("mb", "retimer7", I2C_BUS_67);
+          static VrComponent rt_vr0("mb", "cpu0_rt_p0v9", "VR_CPU0_RETIMER_P0V9");
+          static VrComponent rt_vr1("mb", "cpu1_rt_p0v9", "VR_CPU1_RETIMER_P0V9");
+          break;
+        }
       }
     }
 };

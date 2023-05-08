d7a5c740590affdda8b4be289461ae677927d078
SeanChuangWiwynn <Sean_Chuang@wiwynn.com>
2023-05-08 13:38:51 -0700

    fby35: Fix system couldn't identify the system type
    Summary:
    To fix message "read_snr_from_all_slots() Couldn't identiy the system type: Type_VF".
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2603
    
    Test Plan:
    1. Build and test pass on YV3.5 system. pass
    
    2. Check log message after BMC is ready. root@bmc-oob:~# cat /var/log/messages | grep "Couldn't identiy the system type" 2023 Apr 26 23:12:50 bmc-oob. authpriv.info fby35-v2023.14.1: -bash[12176]: HISTORY: PID=12176 UID=0 cat /var/log/messages | grep "Couldn't identiy the system type" root@bmc-oob:~#
    
    # Description
    
    Please include a summary of the change and which issue is fixed.
    
    # Motivation
    
    Please include an explanation of why you these changes are necessary
    
    Please describe the tests that you ran to verify your changes. Provide instructions so we can reproduce.
    
    Reviewed By: williamspatrick, wangx6f
    
    Differential Revision: D45649503
    
    fbshipit-source-id: 65fa4a2709fa2ce891d6c5d9b2649d1264959e70

--- meta-facebook/meta-fby35/recipes-fby35/plat-libs/files/pal/pal_sensors.c
+++ meta-facebook/meta-fby35/recipes-fby35/plat-libs/files/pal/pal_sensors.c
@@ -2077,6 +2077,7 @@ read_snr_from_all_slots(uint8_t target_snr_num, uint8_t action, float *val) {
     else if ( strcmp(sys_conf, "Type_DPV2") == 0 ) config = CONFIG_B;
     else if ( strcmp(sys_conf, "Type_HD") == 0 ) config = CONFIG_B;
     else if ( (strcmp(sys_conf, "Type_17") == 0) || (strcmp(sys_conf, "Type_8") == 0)) config = CONFIG_D;
+    else if ( strcmp(sys_conf, "Type_VF") == 0 ) config = CONFIG_C;
     else syslog(LOG_WARNING, "%s() Couldn't identiy the system type: %s", __func__, sys_conf);
 
     syslog(LOG_INFO, "%s() Get the system type: %s", __func__, sys_conf);

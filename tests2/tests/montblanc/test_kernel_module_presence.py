5c1e3fe951008644b322950a1510a64941ee588a
rakkesh <in62016453@celestica.com>
2023-05-18 10:46:26 -0700

    MONTBLANC: CIT test_kernel_module_presence addition
    Summary:
    -
    - To add CIT test_kernel_module_presence test case for Montblanc
    
    Motivation:
    -
    - To verify the kernel module presence
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1533
    
    Test Plan:
    -
    - CIT test_kernel_module_presence.py
    
    Test logs:
    ```c
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py -p montblanc -r tests.montblanc.test_kernel_module_presence
    test_installed_kmods (tests.montblanc.test_kernel_module_presence.KernelModulePresenceTest.test_installed_kmods) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.023s
    
    OK
    
    ```
    
    Reviewed By: binhuang00
    
    Differential Revision: D45815506
    
    fbshipit-source-id: ac0641a94cccb6fcb5db515640f0cdbe3d6adefa

---
+++ tests2/tests/montblanc/test_kernel_module_presence.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_kernel_module_presence_test import BaseKernelModulePresenceTest
+
+
+class KernelModulePresenceTest(BaseKernelModulePresenceTest, unittest.TestCase):
+    def set_kmods(self):
+        self.expected_kmod = [
+            "mcbcpld",
+            "at24",
+        ]

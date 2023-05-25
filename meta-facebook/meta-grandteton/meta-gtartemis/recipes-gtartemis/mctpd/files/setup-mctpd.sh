59871a2db108ecea4304ab083326bcfadca1babb
Ian-I-Chien <ian_chien@wiwynn.com>
2023-05-25 09:37:59 -0700

    GTA: Fix NIC1 get version failed
    Summary:
    # Description
     - Set GTA NIC1 EID to 0x22
    
     # Motivation
     - Since GTI/GT set NIC EID to 0x22 but GTA didn't which causes MCTP transcation failed.
    
     # Test Plan
     - Built pass on GTA/GTI/GT and NIC1 can get version. - pass
    
     Log:
     ```
     root@bmc-oob:~# fw-util nic1 --version
     Mellanox NIC Version: 28.34.1002
     root@bmc-oob:~#
     ```
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2639
    
    Reviewed By: williamspatrick, anitazha
    
    Differential Revision: D46175607
    
    fbshipit-source-id: 64f160579d88f48f8597fca2bfe58c76a759d888

--- meta-facebook/meta-grandteton/meta-gtartemis/recipes-gtartemis/mctpd/files/setup-mctpd.sh
+++ meta-facebook/meta-grandteton/meta-gtartemis/recipes-gtartemis/mctpd/files/setup-mctpd.sh
@@ -37,6 +37,9 @@ echo slave-mqueue 0x1010 > /sys/bus/i2c/devices/i2c-3/new_device  #BIC
 echo slave-mqueue 0x1010 > /sys/bus/i2c/devices/i2c-4/new_device  #NIC
 echo slave-mqueue 0x1010 > /sys/bus/i2c/devices/i2c-9/new_device  #BIC
 
+#Set NIC1 EID to 0x22
+/usr/local/bin/mctp-util -d 0x4 0x64 0x00 0x00 0x80 0x01 0x00 0x22
+
 ulimit -q 1024000
 runsv /etc/sv/mctpd_3 > /dev/null 2>&1 &
 runsv /etc/sv/mctpd_4 > /dev/null 2>&1 &

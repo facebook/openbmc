71c7ecb73d0299a8c250540cadcda2d210e6960b
Amithash Prasad <amithash@meta.com>
2023-06-13 16:13:07 -0700

    rocko: Add support for building on newer distros
    Summary:
    Brings in some experimental support to build rocko
    on CentOS9 without py2 and later GCC.
    
    Test Plan: #doitlive
    
    Reviewed By: williamspatrick
    
    Differential Revision: D46704361
    
    fbshipit-source-id: d33f090632ae6c77ad341631644210943f9b6673

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -11,7 +11,7 @@ rocko_repos=(
     meta-security:74860b2
     poky:5f660914cd # bitbake: bitbake-user-manual: Fixed section head typo
 )
-rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch"
+rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
     lf-openbmc:ce3abda64
 )

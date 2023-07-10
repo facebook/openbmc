77083a40c26482f9b71ce53650fc8c1cc00d736b
Patrick Williams <patrickw3@meta.com>
2023-07-10 15:26:57 -0700

    yocto: update master
    Summary:
    Update master at 9e6ea77e9
    update-submodule: yocto/lf-master
    
    Test Plan: CIT build passes.
    
    Reviewed By: tao-ren
    
    Differential Revision: D47338668
    
    fbshipit-source-id: 880d2fee4832b08195df30a31069aaf025e5b2de

--- meta-facebook/meta-backports/master/recipes-extended/libimobiledevice/libplist_%.bbappend
+++
@@ -1,2 +0,0 @@
-FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
-SRC_URI += "file://0001-git-version-gen-Prevent-multiple-lines-of-output.patch"
--- yocto_repos.sh
+++ yocto_repos.sh
@@ -13,7 +13,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:265bd49e0
+    lf-openbmc:9e6ea77e9
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b

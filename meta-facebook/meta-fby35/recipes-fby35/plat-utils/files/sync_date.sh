2a9f9ffcf8c8088b809f29af907681bd2f775c4a
WayneLiu-wiwynn <Wayne_SC_Liu@wiwynn.com>
2023-07-30 17:42:38 -0700

    fby35: gl: support time sync with RTC
    Summary:
    # Description:
    Support time sync with RTC
    
    # Motivation:
    In GL system, BMC should sync date through RTC
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2728
    
    Test Plan:
    Check sel time - pass
    
    # Test Log:
    - Before root@bmc-oob:~# log-util slot1 --print
    1    slot1    2018-03-09 05:00:15    power-util       SERVER_12V_ON successful for FRU: 1
    1    slot1    2018-03-09 05:00:16    gpiointrd        FRU: 1, CPU presence
    1    slot1    2018-03-09 05:00:17    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2018-03-09 05:00:17, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Deassertion
    1    slot1    2018-03-09 05:00:27    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2018-03-09 05:00:27, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Deassertion
    1    slot1    2018-03-09 05:00:31    gpiod            FRU: 1, System powered OFF
    1    slot1    2018-03-09 05:00:34    gpiod            FRU: 1, System powered ON
    1    slot1    2018-03-09 05:01:55    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2018-03-09 05:01:55, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Assertion
    - After
    1    slot1    2023-07-25 17:30:33    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-07-25 17:30:33, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Deassertion
    1    slot1    2023-07-25 17:30:33    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-07-25 17:30:33, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Deassertion
    1    slot1    2023-07-25 17:30:36    gpiod            FRU: 1, System powered OFF
    1    slot1    2023-07-25 17:30:42    power-util       SERVER_POWER_CYCLE successful for FRU: 1
    1    slot1    2023-07-25 17:30:46    gpiod            FRU: 1, System powered ON
    1    slot1    2023-07-25 17:30:54    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-07-25 17:30:54, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Deassertion
    1    slot1    2023-07-25 17:30:57    gpiod            FRU: 1, System powered OFF
    1    slot1    2023-07-25 17:31:00    gpiod            FRU: 1, System powered ON
    1    slot1    2023-07-25 17:32:21    ipmid            SEL Entry: FRU: 1, Record: Standard (0x02), Time: 2023-07-25 17:32:21, Sensor: END_OF_POST (0x11), Event Data: (01FFFF) OEM System boot event Assertion
    
    Reviewed By: williamspatrick
    
    Differential Revision: D47789524
    
    fbshipit-source-id: df041d7a395d6b40f4315bddc630563a3707775c

--- meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/sync_date.sh
+++ meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/sync_date.sh
@@ -21,6 +21,44 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
+sec_offset=0x0
+min_offest=0x2
+hour_offset=0x4
+day_offset=0x7
+mon_offset=0x8
+year_offset=0x9
+rtc_offset_seq=("$sec_offset" "$min_offest" "$hour_offset" "$day_offset" "$mon_offset" "$year_offset")
+
+function set_sel_time_from_rtc {
+
+  for i in $(seq 1 4)
+  do
+    if [[ $(is_sb_bic_ready "$i") == "1" ]] ; then
+      setting=()
+      for offset in "${rtc_offset_seq[@]}"
+      do
+        /usr/bin/bic-util "slot$i" 0x18 0x52 0x7 0xde 0x1 $offset
+        if [ $? -ne 0 ]; then
+          logger -t "sync_date" -p daemon.crit "Fail to get RTC register value, offset: $offset"
+          break
+        fi
+        result=$(/usr/bin/bic-util "slot$i" 0x18 0x52 0x7 0xde 0x1 $offset | sed 's/ //g')
+
+        setting+=($result)
+      done
+
+      date -s "20${setting[5]}-${setting[4]}-${setting[3]} ${setting[2]}:${setting[1]}:${setting[0]}"
+      if [ $? -eq 0 ]; then
+        test -x /etc/init.d/hwclock.sh && /etc/init.d/hwclock.sh stop
+        kv set date_sync 1
+        break
+      else
+        continue
+      fi
+    fi
+  done
+}
+
 function get_sel_time_from_server {
   local BOARD_ID=$(get_bmc_board_id)
   local output=""
@@ -67,11 +105,17 @@ function sync_date {
         do_sync "$output"
       fi
     else
-      output=$(get_sel_time_from_server)
-      num=$?
-      if [ -n "$output" ]; then
-        echo Syncing up BMC time with server$num...
-        do_sync "$output"
+      sys_config=$(/usr/local/bin/show_sys_config | grep -i "config:" | awk -F ": " '{print $3}')
+      server_type=$(get_server_type 1)
+      if [ "$sys_config" = "B" ] && [[ $server_type -eq 4 ]]; then
+        set_sel_time_from_rtc
+      else
+        output=$(get_sel_time_from_server)
+        num=$?
+        if [ -n "$output" ]; then
+          echo Syncing up BMC time with server$num...
+          do_sync "$output"
+        fi
       fi
     fi
   else

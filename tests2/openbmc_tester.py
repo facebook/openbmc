66998f4c38e33ac3f2ad415be45b78b66efd6b92
Amithash Prasad <amithash@meta.com>
2023-08-01 15:46:09 -0700

    Make openbmc_tester functional and do the actual steps
    Summary:
    As title, tar and copy the tests to target system
    and run the tests.
    
    Test Plan:
    ```
    devvm20213:tests2 :-) (add-fw-env-test) # ./openbmc_tester.py --platform grandteton --hostname localhost --port 2222
    test_spi_0_mtd (tests.grandteton.test_image_layout.ImageLayoutTest.test_spi_0_mtd)
    Test actual image layout on spi0.0 matches image meta data ... ok
    test_spi_1_mtd (tests.grandteton.test_image_layout.ImageLayoutTest.test_spi_1_mtd)
    Test actual image layout on spi0.1 matches image meta data ... skipped 'test env is QEMU, skipped'
    test_uboot_tools_part (tests.grandteton.test_image_layout.ImageLayoutTest.test_uboot_tools_part) ... ok
    test_ncsi_util (tests.grandteton.test_ncsi_util.NcsiUtilTest.test_ncsi_util) ... ok
    
    ----------------------------------------------------------------------
    Ran 4 tests in 0.396s
    
    OK (skipped=1)
    in BaseNcsiUtilTest setUp
    ```
    
    Reviewed By: williamspatrick, jagpalgill
    
    Differential Revision: D47968209
    
    fbshipit-source-id: c2f2dd205d31d1800f83d277eed1f853f3c60461

--- tests2/openbmc_tester.py
+++ tests2/openbmc_tester.py
@@ -19,6 +19,10 @@
 #
 
 import argparse
+import os
+import shutil
+import subprocess
+import tempfile
 
 
 """
@@ -31,16 +35,91 @@ Steps of this tool
 
 
 class OpenBMCTester:
-    def __init__(self, hostname, platform):
+    def __init__(self, hostname, platform, password, port):
+        self.tests_base = os.path.dirname(__file__)
         self.platform = platform
         self.hostname = hostname
         self.bmc_start_dir = "/run/tests2"
+        self.password = password
+        self.port = port
+
+    def scp_to(self, src, dest):
+        cmd = [
+            "sshpass",
+            "-p",
+            self.password,
+            "scp",
+            "-P",
+            self.port,
+            src,
+            f"root@{self.hostname}:{dest}",
+        ]
+        subprocess.check_call(cmd)
+
+    def remcmd(self, rcmd):
+        cmd = [
+            "sshpass",
+            "-p",
+            self.password,
+            "ssh",
+            "-p",
+            self.port,
+            f"root@{self.hostname}",
+        ] + rcmd
+        subprocess.check_call(cmd)
+
+    def upload(self):
+        common = [
+            "cit_runner.py",
+            "common",
+            "__init__.py",
+            "qemu_denylist.txt",
+            "utils",
+        ]
+        with tempfile.TemporaryDirectory() as base:
+            tests2 = os.path.join(base, "tests2")
+            os.mkdir(tests2)
+            for c in common:
+                src = os.path.join(self.tests_base, c)
+                dst = os.path.join(tests2, c)
+                if os.path.isdir(src):
+                    shutil.copytree(src, dst)
+                else:
+                    shutil.copy(src, dst)
+            tests = os.path.join(tests2, "tests")
+            os.mkdir(tests)
+            plat_test_dst = os.path.join(tests, self.platform)
+            plat_test_src = os.path.join(self.tests_base, "tests", self.platform)
+            shutil.copytree(plat_test_src, plat_test_dst)
+
+            tests_tar = "tests2.tar.gz"
+            subprocess.check_call(["tar", "-zcf", tests_tar, "tests2/"], cwd=base)
+            self.scp_to(os.path.join(base, tests_tar), "/run/")
+            self.remcmd(["tar", "-xf", "/run/tests2.tar.gz", "-C", "/run/"])
+            self.remcmd(["rm", "-f", "/run/tests2.tar.gz"])
+
+    def run(self):
+        # Port 2222 is for Qemu.
+        if self.port == "2222":
+            self.remcmd(["touch", "/run/tests2/dummy_qemu"])
+        self.remcmd(
+            ["python3", "/run/tests2/cit_runner.py", "--platform", self.platform]
+        )
+
+
+def find_platforms():
+    tests_dir = os.path.join(os.path.dirname(__file__), "tests")
+    return [
+        f for f in os.listdir(tests_dir) if os.path.isdir(os.path.join(tests_dir, f))
+    ]
 
 
 def arg_parse():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--hostname", help="Hostname of the BMC to scp")
+    parser.add_argument("--port", default="22", help="Port of BMC")
+    parser.add_argument("--password", default="0penBmc", help="Password of BMC")
 
     parser.add_argument(
         "--skip-scp", action="store_true", help="Don't scp tests to OpenBMC"
@@ -49,19 +128,7 @@ def arg_parse():
     parser.add_argument(
         "--platform",
         help="Run all tests in platform by platform name",
-        choices=[
-            "wedge",
-            "wedge100",
-            "galaxy100",
-            "cmm",
-            "minipack",
-            "fbtp",
-            "fby2",
-            "fbttn",
-            "fuji",
-            "yamp",
-            "fbgc",
-        ],
+        choices=find_platforms(),
     )
 
     return parser.parse_args()
@@ -69,4 +136,6 @@ def arg_parse():
 
 if __name__ == "__main__":
     args = arg_parse()
-    tester = OpenBMCTester(args.hostname, args.platform)
+    tester = OpenBMCTester(args.hostname, args.platform, args.password, args.port)
+    tester.upload()
+    tester.run()

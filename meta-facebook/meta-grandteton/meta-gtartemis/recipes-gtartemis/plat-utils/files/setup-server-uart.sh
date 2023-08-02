8c4ef940608cdeb1124f2d55c54585d627ba5f24
Ian-I-Chien <ian_chien@wiwynn.com>
2023-08-02 11:35:27 -0700

    GTA: Support ABL showing in SOL
    Summary:
    # Description
     - As title
    
     # Motivation
     - To show ABL log in sol-util
    
     # Test Plan
     - Built pass on GTI/GT/GTA and can show ABL/UEFI. - pass
    
     Log:
     ```
     root@bmc-oob:~# sol-util mb
    
    ------------------TERMINAL MULTIPLEXER---------------------
      CTRL-l ?   : Display help message.
      CTRL-l x : Terminate the connection.
      /var/log/mTerm_mb.log : Log location
      CTRL-l + b : Send Break
    
    -----------------------------------------------------------
    
    	ABL BP[20000]
    	ABL BP[0040]
    
    	ABL BP[0080]
    	    DDR Phy Initialization
    
    	ABL BP[0200]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0002]
    
    	ABL BP[0008]
     ...
     [00000015][00000033][00000032][0000003B][0000004F][00000060][00000061][0000009A][00000062][00000063][00000068][00000069][0000006A][00000070][00000079][00000090][00000091][00000092][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000094][00000095][00000096][00000092][00000092][00000092][00000092][00000092][00000092][00000092][00000099][00000091][00000092][00000092][00000092][00000092][00000092][00000092][00000092][00000092][00000097 ?
     ...
     Last failed login: Fri Jun 22 19:27:34 CST 2018 from 192.168.88.1 on ssh:notty
    There was 1 failed login attempt since the last successful login. Last login: Fri Jun 22 19:12:27 on ttyS1
    [root@localhost ~]#
    [root@localhost ~]#
     ```
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2737
    
    Reviewed By: amithash
    
    Differential Revision: D47988771
    
    fbshipit-source-id: 3a4a0ec2f2ccb221ef9aa7bf569e777b062db0fa

--- meta-facebook/meta-grandteton/meta-gtartemis/recipes-gtartemis/plat-utils/files/setup-server-uart.sh
+++ meta-facebook/meta-grandteton/meta-gtartemis/recipes-gtartemis/plat-utils/files/setup-server-uart.sh
@@ -19,8 +19,10 @@
 #
 
 connect_uart2_4() {
-  printf "uart2" > /sys/devices/platform/ahb/ahb:apb/1e789000.lpc/1e789098.uart_routing/uart4
-  printf "uart4" > /sys/devices/platform/ahb/ahb:apb/1e789000.lpc/1e789098.uart_routing/uart2
+  echo -ne "uart1" > /sys/devices/platform/ahb/ahb:apb/1e789000.lpc/1e789098.uart_routing/uart4
+  echo -ne "uart4" > /sys/devices/platform/ahb/ahb:apb/1e789000.lpc/1e789098.uart_routing/uart1
+  echo -ne "io1" > /sys/devices/platform/ahb/ahb:apb/1e789000.lpc/1e789098.uart_routing/uart2
+  echo -ne "uart2" > /sys/devices/platform/ahb/ahb:apb/1e789000.lpc/1e789098.uart_routing/io1
 }
 
 setup_artemis_uart() {

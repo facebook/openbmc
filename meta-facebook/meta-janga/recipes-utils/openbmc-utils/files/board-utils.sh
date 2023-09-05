052ee75ffee61e53b0902731867df2be79671b6f
Tao Ren <taoren@meta.com>
2023-09-05 12:26:09 -0700

    janga: openbmc-utils: fill power/mac functions in board-utils.sh
    Summary: Implement power control and cpu/bmc-mac functions in board-utils.sh.
    
    Test Plan:
    1) "bitbake janga-image" succeeded.
    2) booted up janga-image on minipack3 successfully.
    
    Reviewed By: Scott8440
    
    Differential Revision: D49001552
    
    fbshipit-source-id: 6e0017b30750cd038bce8e4d29e66a50e5cb330e

--- meta-facebook/meta-janga/recipes-utils/openbmc-utils/files/board-utils.sh
+++ meta-facebook/meta-janga/recipes-utils/openbmc-utils/files/board-utils.sh
@@ -17,6 +17,13 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 
+PWRCPLD_SYSFS_DIR=$(i2c_device_sysfs_abspath 12-003d)
+
+COME_POWER_EN="${PWRCPLD_SYSFS_DIR}/pwr_come_en"
+COME_POWER_OFF="${PWRCPLD_SYSFS_DIR}/pwr_force_off"
+COME_POWER_CYCLE_N="${PWRCPLD_SYSFS_DIR}/pwr_cyc_n"
+CHASSIS_POWER_CYCLE="${PWRCPLD_SYSFS_DIR}/power_cycle_go"
+
 wedge_board_type() {
     echo 'janga'
 }
@@ -27,36 +34,59 @@ wedge_board_rev() {
 }
 
 userver_power_is_on() {
-    echo "FIXME: feature not implemented!!"
+    en_sts=$(head -n 1 "$COME_POWER_EN" 2> /dev/null)
+    off_sts=$(head -n 1 "$COME_POWER_OFF" 2> /dev/null)
+
+    if [ $((en_sts)) -eq $((0x1)) ] && [ $((off_sts)) -eq $((0x1)) ] ; then
+        return 0
+    fi
+
     return 1
 }
 
 userver_power_on() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    echo 1 > "$COME_POWER_OFF"
+    echo 1 > "$COME_POWER_EN"
 }
 
 userver_power_off() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    echo 0 > "$COME_POWER_OFF"
 }
 
 userver_reset() {
+    echo 0 > "$COME_POWER_CYCLE_N"
+
+    # $COME_POWER_CYCLE_N will be auto-set to 1 when power cycle completes.
+    timeout=10 # 10 seconds
+    while [ $((timeout)) -ge 0 ]; do
+        val=$(head -n 1 "$COME_POWER_CYCLE_N" 2> /dev/null)
+        if [ $((val)) -eq 1 ]; then
+            return 0
+        fi
+
+        sleep 1
+        timeout=$((timeout-1))
+    done
+
+    echo "failed to reset userver!"
     return 1
-    echo "FIXME: feature not implemented!!"
 }
 
 chassis_power_cycle() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    echo 1 > "$CHASSIS_POWER_CYCLE"
 }
 
 bmc_mac_addr() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    bmc_mac=$(weutil -e chassis_eeprom | grep 'Local MAC:' | cut -d ' ' -f 3)
+    echo "bmc_mac"
 }
 
+#
+# SCM EEPROM doesn't exist in Janga, and CPU MAC address is obtained by
+# adding 1 to the BMC MAC address.
+#
 userver_mac_addr() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    bmc_mac=$(bmc_mac_addr)
+    cpu_mac=$(mac_addr_inc "$bmc_mac")
+    echo "$cpu_mac"
 }

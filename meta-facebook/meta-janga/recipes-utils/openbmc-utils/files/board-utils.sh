88b0487d8b9707300493823909b45b568930fbd4
Tao Ren <taoren@meta.com>
2023-09-05 17:43:37 -0700

    meta-facebook: add initial machine layer for janga
    Summary:
    The patch adds the initial machine layer of the BMC-Lite FBOSS OpenBMC
    platform janga.
    
    DO NOT EDIT: THE PATCH IS GENERATED BY AUTOMATION TOOLS.
    
    Test Plan:
    1) "bitbake janga-image" succeeded.
    2) booted up janga-image on minipack3:
       - "u-boot" loads and hands off control to kernel successfully.
       - Linux kernel boots up successfully.
       - BMC "eth0" is functional (with random MAC address) after boot up.
       - Flash "data0" partition is mounted under /mnt/data as UBIFS.
       - eMMC is mounted under /mnt/data1 as EXT4.
    
    Reviewed By: Scott8440
    
    Differential Revision: D49001556
    
    fbshipit-source-id: 46617c9838e5eeb42d9c40b74b204821c92a9886

---
+++ meta-facebook/meta-janga/recipes-utils/openbmc-utils/files/board-utils.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+wedge_board_type() {
+    echo 'janga'
+}
+
+wedge_board_rev() {
+    # FIXME if needed.
+    return 1
+}
+
+userver_power_is_on() {
+    echo "FIXME: feature not implemented!!"
+    return 1
+}
+
+userver_power_on() {
+    echo "FIXME: feature not implemented!!"
+    return 1
+}
+
+userver_power_off() {
+    echo "FIXME: feature not implemented!!"
+    return 1
+}
+
+userver_reset() {
+    return 1
+    echo "FIXME: feature not implemented!!"
+}
+
+chassis_power_cycle() {
+    echo "FIXME: feature not implemented!!"
+    return 1
+}
+
+bmc_mac_addr() {
+    echo "FIXME: feature not implemented!!"
+    return 1
+}
+
+userver_mac_addr() {
+    echo "FIXME: feature not implemented!!"
+    return 1
+}

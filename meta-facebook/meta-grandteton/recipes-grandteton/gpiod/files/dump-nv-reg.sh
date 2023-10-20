50db9d6eb300f838b3289021ce5f24a3bed8b5af
Jimmyhuang777 <huang.jimmy@quantatw.com>
2023-10-20 13:34:01 -0700

    GTI: fix HGX FPGA dump
    Summary:
    # Description
    1. correct the mb cpld address
    2. only allow to dump a single file at the same time
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4108
    
    Test Plan:
    Build and test pass on GTI system
    
    1. Perform the HGX FPGA dump
    root@bmc-oob:/mnt/data# sh /usr/local/bin/dump-nv-reg.sh Start to dump NVDIA FPGA Register
    hgx_dump.log
    End of to dump NVDIA FPGA Register
    
    2. Check the SEL log
    root@bmc-oob:/mnt/data# log-util all --print
    2018 Mar 10 04:04:24 log-util: User cleared all logs
    0    all      2018-03-10 04:09:44    gpiod            HGX FPGA dump is generated at /mnt/data/nvlog/20180310-04:09:44_nvlog.tar.gz
    
    3. unzip the file
    root@bmc-oob:/mnt/data# tar zxvf nvlog/20180310-04\:04\:51_nvlog.tar.gz hgx_dump.log
    
    4. check if the HGX dump is correct
    root@bmc-oob:/mnt/data# cat hgx_dump.log
    Dump HGX FPGA reg:
    No size specified (using byte-data access)
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f    0123456789abcdef
    00: 00 00 00 00 00 00 ff 03 00 00 00 00 00 00 00 00    .......?........
    10: 00 00 00 00 ff 00 00 00 ff 00 ff 00 ff 00 ff 00    ................
    20: 00 04 fc 00 ff 00 f7 00 ff 00 ff 30 3e 38 04 00    .??...?....0>8?.
    30: 01 7d 04 ff a9 d1 2f ff 3c ff ff fd ff ff ff ff    ?}?.??/.<..?....
    .
    .
    Dump MB CPLD reg:
    No size specified (using byte-data access)
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f    0123456789abcdef
    00: 79 ef c5 7d f8 fb 39 fc 32 34 ff 21 18 20 c0 30    y??}??9?24.!? ?0
    10: 00 01 1e 00 00 f0 00 00 b1 00 24 00 c8 00 03 61    .??..?..?.$.?.?a
    
    Reviewed By: wangx6f
    
    Differential Revision: D50493979
    
    fbshipit-source-id: 851d92504116c62e2a33d25f6cf92330249fac45

--- meta-facebook/meta-grandteton/recipes-grandteton/gpiod/files/dump-nv-reg.sh
+++ meta-facebook/meta-grandteton/recipes-grandteton/gpiod/files/dump-nv-reg.sh
@@ -1,33 +1,58 @@
 #!/bin/bash
 
+KV_CMD="/usr/bin/kv"
+I2C_CMD="/usr/sbin/i2cdump"
 
-dump_nv_log() {
+DUMP_FILE="/tmp/hgx_dump.log"
+
+dump_mb_cpld_reg() {
+  echo "Dump MB CPLD reg:" >> $DUMP_FILE
+
+  is_gtt=`i2cdetect -y 7  | grep -i 13`
+  if [ -n "$is_gtt" ]; then
+    { $I2C_CMD -y 7 0x13;
+      $I2C_CMD -y 7 0x14; } >> $DUMP_FILE 2>&1
+  else
+    $I2C_CMD -y 7 0x23 >> $DUMP_FILE 2>&1
+  fi
+}
+
+dump_hgx_fpga_reg() {
+  echo "Dump HGX FPGA reg:" > $DUMP_FILE
+  $I2C_CMD -y 11 0x0b >> $DUMP_FILE 2>&1
+}
+
+dump_log() {
   echo "Start to dump NVDIA FPGA Register"
 
-  TIME=$(date +"%Y%m%d-%S")
-  echo $TIME
+  TIME=$(date +"%Y%m%d-%H:%M:%S")
+  LOG_DIR="/mnt/data/nvlog"
+  LOG_ARCHIVE="$LOG_DIR/${TIME}_nvlog.tar.gz"
 
-  DUMP_DIR='/tmp/nv_reg/output'
-  DUMP_REG0B="$DUMP_DIR/reg_0x0b.log"
-  DUMP_REG13="$DUMP_DIR/reg_0x13.log"
-  DUMP_REG14="$DUMP_DIR/reg_0x14.log"
-  LOG_ARCHIVE="/mnt/data/nvlog/${TIME}_nvlog.tar.gz"
+  dump_hgx_fpga_reg
+  echo >> $DUMP_FILE
+  dump_mb_cpld_reg
 
-  mkdir -p "/mnt/data/nvlog"
-  mkdir -p $DUMP_DIR
+  if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
+  fi
 
-  /usr/sbin/i2cdump -y 11 0x0b > $DUMP_REG0B 2>&1
-  /usr/sbin/i2cdump -y 7  0x13 > $DUMP_REG13 2>&1
-  /usr/sbin/i2cdump -y 7  0x14 > $DUMP_REG14 2>&1
-
-
-  tar -zcvf $LOG_ARCHIVE -C $DUMP_DIR reg_0x0b.log reg_0x13.log reg_0x14.log
+  tar -zcvf "$LOG_ARCHIVE" -C "$(dirname "$DUMP_FILE")" "$(basename "$DUMP_FILE")"
+  logger -t "gpiod" -p daemon.crit "HGX FPGA dump is generated at $LOG_ARCHIVE"
 
   echo high > /tmp/gpionames/NV_DUMP_END/direction
   sleep 1
   echo low > /tmp/gpionames/NV_DUMP_END/direction
 
+
   echo "End of to dump NVDIA FPGA Register"
 }
 
-dump_nv_log &
+state=`$KV_CMD get "hgx_dump_stat"`
+if [ "$state" == "Ongoing" ]; then
+  exit 0
+else
+  $KV_CMD set "hgx_dump_stat" "Ongoing"
+  dump_log
+  $KV_CMD set "hgx_dump_stat" "Done"
+fi

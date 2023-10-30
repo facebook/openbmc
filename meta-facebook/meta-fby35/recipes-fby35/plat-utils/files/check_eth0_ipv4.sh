a73b4328ea0a77ed8a3d4673f4625fe2ea977d78
SeanChuangWiwynn <Sean_Chuang@wiwynn.com>
2023-10-30 11:27:20 -0700

    fby35: cl: Check NIC status after power on
    Summary:
    # Description:
    There is a little chance that NIC doesn't power on after pull the sled back for power on. So we make this workaround to reinit NIC first.
    
    # Motivation:
    To reinit NIC if NIC doesn't power on after we pull sled back for power on.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2846
    
    Test Plan:
    1. Build and test pass on yv35 system. pass
    
    2. Simulate a scenario where the NIC has no power and need to be reinited. (1) Power on NIC by setting CPLD reg
    i2cset -f -y 12 0x0f 0x10 0x1
    i2cset -f -y 12 0x0f 0x10 0x0
    
    (2) Restart the nerworking
    /etc/init.d/networking restart
    
    (3) Check the NIC version which BMC will send NCSI command to NIC /usr/bin/fw-util nic --version
    
    pass
    
    Reviewed By: jagpalgill
    
    Differential Revision: D50725152
    
    fbshipit-source-id: 98ee024aec05ca1c3fd3f6f3a051a7ee302e38b1

--- meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/check_eth0_ipv4.sh
+++ meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/check_eth0_ipv4.sh
@@ -5,13 +5,38 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
+#This is a workaround when NIC lost power for now
+#Check NIC status by verifying the existence of the NIC version
+check_nic_status() {
+  for i in {1..4};
+  do
+    if [ "$i" == 4 ]; then
+      logger -t "nic_check" -p daemon.crit "Fail to power on NIC"
+      break;
+    fi
+    /usr/bin/fw-util nic --version
+    if [ -f "/tmp/cache_store/nic_fw_ver" ]; then
+      break;
+    else
+      i2cset -f -y 12 0x0f 0x10 0x1
+      sleep 1
+      i2cset -f -y 12 0x0f 0x10 0x0
+      echo 1e690000.ftgmac > /sys/bus/platform/drivers/ftgmac100/bind
+      sleep 5 #Wait for NIC power on
+      /etc/init.d/networking restart
+      logger -t "nic_check" -p daemon.crit "Reinit NIC..."
+      sleep 5 #Wait for the NCSI initialization
+    fi
+  done
+}
+
 check_eth0_ipv4() {
   for i in {1..10};
   do
     ip=$(ifconfig eth0 |grep "inet addr")
     if [[ -z "$ip" ]]; then
       pid=$(cat /var/run/dhclient.eth0.pid)
-      kill $pid
+      kill "$pid"
       dhclient -d -pf /var/run/dhclient.eth0.pid eth0 > /dev/null 2>&1 &
       sleep 3
     else
@@ -19,6 +44,9 @@ check_eth0_ipv4() {
     fi
   done
 }
+
+echo "check NIC status..."
+check_nic_status
 
 echo "check eth0 ipv4..."
 check_eth0_ipv4 &

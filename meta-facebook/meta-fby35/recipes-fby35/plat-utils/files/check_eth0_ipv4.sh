c4abd9c28d1a47485d80c06aaee53b628a9074a2
cchoux <cosmo.chou@quantatw.com>
2023-11-04 07:48:09 -0700

    fby35: Rebind NIC temperature device when reinitializing the NIC
    Summary:
    # Description
    There is a slight chance that the NIC may not power on after pulling
    the sled back for power. It is unable to access the NIC temperature
    device until the NIC is reset, so rebind the driver to resolve the NIC
    temperature reading issue.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4119
    
    Test Plan:
    Build and test pass on fby35
    ```
    root@bmc-oob:~# log-util all --print |grep nic_check
    0    all      2023-11-03 19:48:11    nic_check        Reinit NIC...
    
    root@bmc-oob:~# sensor-util nic
    nic:
    NIC_TEMP_C                   (0xEF) :  41.000 C     | (ok)
    
    root@bmc-oob:~# ifconfig eth0
    eth0      Link encap:Ethernet  HWaddr 00:62:0B:83:DA:89
              inet addr:10.10.12.18  Bcast:10.10.255.255  Mask:255.255.0.0
              inet6 addr: 2019:7::262:bff:fe83:da89/64 Scope:Global
              inet6 addr: fe80::262:bff:fe83:da89/64 Scope:Link
              inet6 addr: 2018:5::699/128 Scope:Global
              UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
              RX packets:5320 errors:0 dropped:0 overruns:0 frame:0
              TX packets:187 errors:0 dropped:0 overruns:0 carrier:0
              collisions:0 txqueuelen:1000
              RX bytes:646531 (631.3 KiB)  TX bytes:23416 (22.8 KiB)
              Interrupt:33
    ```
    
    Reviewed By: williamspatrick
    
    Differential Revision: D51000335
    
    fbshipit-source-id: a10677084ae479e84672b52fdc20ffc12f89a293

--- meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/check_eth0_ipv4.sh
+++ meta-facebook/meta-fby35/recipes-fby35/plat-utils/files/check_eth0_ipv4.sh
@@ -5,6 +5,9 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 
+# shellcheck source=common/recipes-utils/openbmc-utils/files/i2c-utils.sh
+. /usr/local/bin/i2c-utils.sh
+
 #This is a workaround when NIC lost power for now
 #Check NIC status by verifying the existence of the NIC version
 check_nic_status() {
@@ -21,8 +24,13 @@ check_nic_status() {
       i2cset -f -y 12 0x0f 0x10 0x1
       sleep 1
       i2cset -f -y 12 0x0f 0x10 0x0
-      echo 1e690000.ftgmac > /sys/bus/platform/drivers/ftgmac100/bind
+      echo 1e690000.ftgmac > /sys/bus/platform/drivers/ftgmac100/bind 2>/dev/null
       sleep 5 #Wait for NIC power on
+      if [ ! -L "${SYSFS_I2C_DEVICES}/8-001f/driver" ]; then
+        if i2c_bind_driver tmp421 8-001f 4 2>/dev/null; then
+          echo "rebind 8-001f to driver tmp421 successfully"
+        fi
+      fi
       /etc/init.d/networking restart
       logger -t "nic_check" -p daemon.crit "Reinit NIC..."
       sleep 5 #Wait for the NCSI initialization

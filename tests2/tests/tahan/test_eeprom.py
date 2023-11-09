0eff87d427e7675e293dccf4446c3d1d91fee530
Sittisak Sinprem <ssinprem@celestica.com>
2023-11-09 09:56:06 -0800

    cit: tahan: Add "test_eeprom" tests for Tahan
    Summary:
    # Description
    Add "test_eeprom" CIT test case for Tahan platform.
    
    # Motivation
    To test the Chassis EEPROM content of the Tahan platform
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1661
    
    Test Plan:
    EEPROM Content:
    ```
    root@bmc-oob:/usr/local/bin/tests2# weutil
    Wedge EEPROM chassis_eeprom
    Version: 4
    Product Name: TAHAN
    Product Part Number: 20100016
    System Assembly Part Number: 19100048
    Meta PCBA Part Number: 132100005
    Meta PCB Part Number: 131100004
    ODM/JDM PCBA Part Number: R4055G000201
    ODM/JDM PCBA Serial Number: B091720450023
    Product Production State: 1
    Product Version: 0
    Product Sub-Version: 0
    Product Serial Number: M113220290026
    System Manufacturer: CLS
    System Manufacturing Date: 20230911
    PCB Manufacturer: WUS
    Assembled at: CTH
    Local MAC: B4:DB:91:E1:61:76
    Extended MAC Base: B4:DB:91:E1:61:78
    Extended MAC Address Size: 75
    EEPROM location on Fabric: SMB
    CRC16: 0x422f
    root@bmc-oob:/usr/local/bin/tests2#
    ```
    
    Run CIT (on Tahan hardware):
    ```
    root@bmc-oob:/usr/local/bin/tests2# python cit_runner.py -p tahan -r tests.tahan.test_eeprom
    test_asset_tag (tests.tahan.test_eeprom.ChassisEepromTest.test_asset_tag) ... ok
    test_extended_mac_base (tests.tahan.test_eeprom.ChassisEepromTest.test_extended_mac_base) ... ok
    test_local_mac (tests.tahan.test_eeprom.ChassisEepromTest.test_local_mac) ... ok
    test_location_on_fabric (tests.tahan.test_eeprom.ChassisEepromTest.test_location_on_fabric) ... ok
    test_odm_pcb (tests.tahan.test_eeprom.ChassisEepromTest.test_odm_pcb) ... ok
    test_product_name (tests.tahan.test_eeprom.ChassisEepromTest.test_product_name)
    Tests if the platform defined product name is present ... ok
    test_product_part_number (tests.tahan.test_eeprom.ChassisEepromTest.test_product_part_number) ... ok
    test_product_serial_number (tests.tahan.test_eeprom.ChassisEepromTest.test_product_serial_number) ... ok
    test_system_manufacturer (tests.tahan.test_eeprom.ChassisEepromTest.test_system_manufacturer) ... ok
    test_version (tests.tahan.test_eeprom.ChassisEepromTest.test_version) ... ok
    
    ----------------------------------------------------------------------
    Ran 10 tests in 10.800s
    
    OK
    root@bmc-oob:/usr/local/bin/tests2#
    ```
    
    Reviewed By: tao-ren
    
    Differential Revision: D51067751
    
    fbshipit-source-id: e5bf97d2f47e6920308aa9f0fec30f943550e631

---
+++ tests2/tests/tahan/test_eeprom.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_eeprom_test import CommonEepromTest
+
+
+class ChassisEepromTest(CommonEepromTest, unittest.TestCase):
+    """
+    Test for CHASSIS EEPROM
+    """
+
+    def set_eeprom_cmd(self):
+        self.eeprom_cmd = ["weutil"]
+
+    def set_product_name(self):
+        self.product_name = ["TAHAN"]
+
+    def set_location_on_fabric(self):
+        self.location_on_fabric = ["SMB"]
+
+    # overide for FBOSS EEPROMv4 format, display different
+    def set_eeprom_location(self):
+        self.test_eeprom_location = "EEPROM location on Fabric"
+
+    # overide for FBOSS EEPROMv4 format, display different
+    def set_odm_pcba_number(self):
+        self.odm_pcba_part_number = "ODM/JDM PCBA Part Number"
+        self.odm_pcba_serial_number = "ODM/JDM PCBA Serial Number"
+
+    # not asset tag field for FBOSS EEPROMv4 format
+    def test_asset_tag(self):
+        pass

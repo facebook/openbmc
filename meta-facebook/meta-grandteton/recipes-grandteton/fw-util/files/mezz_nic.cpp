6c62861c0916658eaee32668b03104eab0b7183f
Daniel Hsu <Daniel-Hsu@quantatw.com>
2023-12-06 15:24:47 -0800

    GT/GTI: SWB Nic update would set NIC1 update ongoing flag
    Summary:
    Fix the issue of hard-coded program parameters
    
    # Testing:
      Terminal 1:
      root@bmc-oob:# fw-util swb --update swb_nic0 /tmp/FB_0000000029.pldm
      RequestUpdate Success.
      PassComponentTable Success.
      UpdateComponent Success.
      Download offset : 0x001b6c00/0x0079e2f0, size : 0x00000400
    
      Terminal 2:
      root@bmc-oob:# ls -al /tmp/cache_store/ | grep fru
      -rw-r--r--    1 root     root             3 Nov 16 14:13 fru2_fwupd
      root@bmc-oob:#
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4155
    
    Reviewed By: williamspatrick
    
    Differential Revision: D51881636
    
    fbshipit-source-id: 986908bdeacb2d61cf13f75aade622905ef86a45

--- meta-facebook/meta-grandteton/recipes-grandteton/fw-util/files/mezz_nic.cpp
+++ meta-facebook/meta-grandteton/recipes-grandteton/fw-util/files/mezz_nic.cpp
@@ -22,7 +22,7 @@ int PLDMNicComponent::update(string image) {
   syslog(LOG_CRIT, "Component %s upgrade initiated", _component.c_str());
 
   //Since NIC PLDM update need to take more than 10 minutes, we extend the timeout.
-  pal_set_fw_update_ongoing(FRU_NIC1, 60 * 20);
+  set_update_ongoing(60 * 20);
   ret = oem_pldm_fw_update(_bus_id, _eid, (char *)image.c_str());
 
   if (ret)

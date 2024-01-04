9d0c32bc1ebdbe42d36a6876e9b7a3d4c50e5879
Ian-I-Chien <ian_chien@wiwynn.com>
2024-01-04 04:27:21 -0800

    GTA: Skip setup fa n script checking GPU PIN which causes GTA FSCD sta…
    Summary:
    …rts slow
    
     # Description:
     - As title.
    
     # Motivation:
     - Since GTA does not have GPU config, it causes GTA stucks in loop
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/2940
    
    Test Plan:
    - Built passed and check if GTA fan version is correct - pass
    
     # Log:
     ```
     root@bmc-oob:~# fw-util bmc --version
    BMC Version: gtartemis-1b64a5802b7-dirty
    BMC Version After Activated: gtartemis-1b64a5802b7-dirty Fan Speed Controller Version: gtartemis-v1.3
    ROM Version: gtartemis-50f1c3d9c21
    ROM Version After Activated: gtartemis-50f1c3d9c21 TPM Version: 7.85
    root@bmc-oob:~#
     ```
    
    Reviewed By: williamspatrick
    
    Differential Revision: D52501351
    
    fbshipit-source-id: 390da3536201b28f89dddecf9eabf11050d2472a

--- meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/plat-utils/files/check-fan-cache.sh
+++ meta-facebook/meta-grandteton/meta-inspirationpoint/recipes-inspirationpoint/plat-utils/files/check-fan-cache.sh
@@ -93,19 +93,22 @@ if [ -n "$is_fscd_running" ] && [ "$auto_fsc" == "hgx" ]; then
 fi
 
 check_mb_rev() {
-  gpu_prsnt=$(cat /tmp/gpionames/GPU_PRSNT_N_ISO_R/value)
-  if [ "$gpu_prsnt" -eq 0 ]; then
-    for retry in {1..60}; do
-      gpu_config=$(kv get gpu_config persistent)
-      if [ "$gpu_config" == "hgx" ]; then
-        break
-      elif [ "$gpu_config" == "ubb" ]; then
-        ln -s /etc/fsc-config-8-retimer-ubb.json ${DEFAULT_FSC_CONFIG}
-        return
-      else
-        sleep 5
-      fi
-    done
+  temp_config=`$KV_CMD get "auto_fsc_config" persistent`
+  if [ $temp_config != "$FAN_TABLE_VER_GTA" ]; then
+    gpu_prsnt=$(cat /tmp/gpionames/GPU_PRSNT_N_ISO_R/value)
+    if [ "$gpu_prsnt" -eq 0 ]; then
+      for retry in {1..60}; do
+        gpu_config=$(kv get gpu_config persistent)
+        if [ "$gpu_config" == "hgx" ]; then
+          break
+        elif [ "$gpu_config" == "ubb" ]; then
+          ln -s /etc/fsc-config-8-retimer-ubb.json ${DEFAULT_FSC_CONFIG}
+          return
+        else
+          sleep 5
+        fi
+      done
+    fi
   fi
 
   rev_id=$(kv get mb_rev)

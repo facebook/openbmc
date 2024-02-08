bc86d0087c654c5f4cdb4b9a2339edbd0fe415dd
pokyuser <pokyuser@celestica.com>
2024-02-08 02:29:24 -0800

    montblanc: remove unused GPIO pins from setup-gpio.sh
    Summary:
    # Description
    
    Remove GPIO pins entries which are not required in BMC lite configuration from "setup-gpio.sh" script.
    
    **List Of GPIOs pins:**
    
    1. FM_BMC_READY_R_L : IPMI
    2. BMC_I2C1_EN : host-recovery
    3. BMC_I2C2_EN : host-recovery
    4. IOB_FLASH_SEL : host-recovery
    5. MUX_JTAG_SEL_0 : host-recovery
    6. MUX_JTAG_SEL_1 : host-recovery
    7. SPI_MUX_SEL : host-recovery
    
    # Motivation
    
    Code cleanup and only export necessary GPIO pins.
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1705
    
    Test Plan:
    1. Build BMC image and run on Montbalnc
    2. Check all the exported GPIO pins:
    ```
    root@bmc-oob:~# ls -l /tmp/gpionames/
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 BMC_I2C1_EN -> /sys/class/gpio/gpio560
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 BMC_I2C2_EN -> /sys/class/gpio/gpio553
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 FM_BMC_READY_R_L -> /sys/class/gpio/gpio632
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 IOB_FLASH_SEL -> /sys/class/gpio/gpio564
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 MUX_JTAG_SEL_0 -> /sys/class/gpio/gpio552
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 MUX_JTAG_SEL_1 -> /sys/class/gpio/gpio557
    lrwxrwxrwx    1 root     root            23 Feb  1 00:51 SPI_MUX_SEL -> /sys/class/gpio/gpio646
    ```
    **Note:** The CIT case for testing exported GPIO pins will be updated in a subsequent PR in accordance with this PR changes.
    
    Reviewed By: somasun
    
    Differential Revision: D53566549
    
    Pulled By: tao-ren
    
    fbshipit-source-id: b4200207661435ea5e40090f0adf7a789ab45c66

--- meta-facebook/meta-montblanc/recipes-utils/openbmc-utils/files/setup-gpio.sh
+++ meta-facebook/meta-montblanc/recipes-utils/openbmc-utils/files/setup-gpio.sh
@@ -30,52 +30,19 @@
 # shellcheck disable=SC1091
 . /usr/local/bin/openbmc-utils.sh
 
-# To debug jumper
-setup_gpio BMC_DEBUG_JUMPER1                GPIOG5 in
-setup_gpio BMC_DEBUG_JUMPER2                GPIOB1 in
-setup_gpio BMC_DEBUG_JUMPER3                GPIOB2 in
-# OCP Debug Card present event, high active
-setup_gpio DEBUG_PRSNT                      GPIOG2 in
-
-# Indicate the PCH power good on the Netlake. From COME D83
-setup_gpio PWRGD_PCH_PWROK                  GPIOF4 in
-# Reserved for power button event input, also pass through to BMC Pin40
-setup_gpio PWR_BTN_L                        GPIOP4 in
-# Reserved for reset button event input, also pass through to BMC Pin76
-setup_gpio RST_BTN_L                        GPIOP2 in
-
-# BMC Power Good, output to CPLD 
-setup_gpio BMC_PWRGD_R                      GPIOV4 out 1
 # BMC ready event to COME (COME A85)
 setup_gpio FM_BMC_READY_R_L                 GPIOP0 out 0
-# Reserved for Power button event output, which pass through from BMC Pin160
-setup_gpio BMC_PTH_PWR_BTN_L                GPIOP5 out 1
-# Reserved for reset button event output, which pass through from BMC Pin121
-setup_gpio BMC_PTH_RST_BTN_L                GPIOP3 out 1
 
+# To enable the path to access COMe FPGA and SCMCPLD
 setup_gpio BMC_I2C1_EN                      GPIOG0 out 0
 setup_gpio BMC_I2C2_EN                      GPIOF1 out 0
+
 # JTAG MUX
 setup_gpio MUX_JTAG_SEL_0                   GPIOF0 out 0
 setup_gpio MUX_JTAG_SEL_1                   GPIOF5 out 0
+
 # Mux select signal for BMC to program IOB flash (SPI)
 setup_gpio IOB_FLASH_SEL                    GPIOG4 out 0
+
 # SPI Mux select to COME (B89) and PROT module
 setup_gpio SPI_MUX_SEL                      GPIOQ6 out 0
-
-# BMC CPU_RST#
-setup_gpio BMC_CPU_RST                      GPION2 in
-# To SCM CPLD, reserved.
-setup_gpio BMC_GPIO126_USB2APWREN           GPION4 in
-# CPU power good event indicates to BMC
-setup_gpio COME_ISO_PWRGD_CPU_LVC3          GPIOO2 in
-# CATERR/MSMI event from SOC
-setup_gpio FM_CPU_MSMI_CATERR_LVT3_BUF_N    GPIOM3 in
-# Non-maskable Interrupt to BMC, from COMe B95
-setup_gpio ISO_FM_BMC_NMI_L                 GPIOO3 in
-# Reserve for BMC to enable Netlake power, to COMe A93
-setup_gpio ISO_FM_BMC_ONCTL_L               GPIOG6 out 0
-# COMe FPGA CRC error event indication, from COMe C69
-setup_gpio ISO_FPGA_CRC_ERROR               GPIOQ0 in
-# Netlake FPGA asset it to inform BMC for PCIe hot plug (From COME A16)
-setup_gpio ISO_SMB_FPGA_VPP_ALERT_L         GPIOG3 in

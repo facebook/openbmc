d80f5cdba10e47dcf20251ea2466d10fcf2a55a4
Patrick Williams <patrickw3@meta.com>
2024-02-14 14:11:24 -0800

    mfgtool: sensor-display: add status
    Summary:
    Add a sensor status property similar to what we should
    eventually get out of Redfish.
    
    Test Plan: P1182718704
    
    Reviewed By: tao-ren
    
    Differential Revision: D53600209
    
    fbshipit-source-id: 6ee786b259591c096432eaa53b6643f25adeb08f

--- common/recipes-lfopenbmc/mfg-tool/files/cmd/sensor-display.cpp
+++ common/recipes-lfopenbmc/mfg-tool/files/cmd/sensor-display.cpp
@@ -7,6 +7,7 @@
 #include <sdbusplus/async.hpp>
 #include <sdbusplus/message.hpp>
 
+#include <cmath>
 #include <ranges>
 
 namespace mfgtool::cmds::sensor_display
@@ -37,7 +38,10 @@ struct command
             auto& entry_json = result[last_element(path.str, '/')];
             auto proxy = sensor::Proxy(ctx).service(service).path(path.str);
 
-            entry_json["value"] = co_await proxy.value();
+            auto value = co_await proxy.value();
+            entry_json["value"] = value;
+            entry_json["status"] = std::isfinite(value) ? "ok" : "unavailable";
+
             if (auto v = co_await proxy.max_value(); std::isfinite(v))
             {
                 entry_json["max"] = v;
@@ -51,23 +55,27 @@ struct command
                 '.');
         });
 
-        info("Finding Warning thresholds");
+        info("Finding HardShutdown thresholds");
         co_await utils::mapper::subtree_for_each(
-            ctx, sensor::ns_path, sensor::warning::interface,
+            ctx, sensor::ns_path, sensor::hard_shutdown::interface,
 
             [&](const auto& path,
                 const auto& service) -> sdbusplus::async::task<> {
-            auto& entry_json = result[last_element(path.str, '/')]["warning"];
+            auto& sensor_json = result[last_element(path.str, '/')];
+            auto& entry_json = sensor_json["hard-shutdown"];
             auto proxy =
-                sensor::warning::Proxy(ctx).service(service).path(path.str);
+                sensor::hard_shutdown::Proxy(ctx).service(service).path(
+                    path.str);
 
-            if (auto v = co_await proxy.warning_high(); std::isfinite(v))
+            if (auto v = co_await proxy.hard_shutdown_high(); std::isfinite(v))
             {
                 entry_json["high"] = v;
+                update_status<std::greater>(sensor_json, v, "critical");
             }
-            if (auto v = co_await proxy.warning_low(); std::isfinite(v))
+            if (auto v = co_await proxy.hard_shutdown_low(); std::isfinite(v))
             {
                 entry_json["low"] = v;
+                update_status<std::less>(sensor_json, v, "critical");
             }
         });
 
@@ -77,39 +85,43 @@ struct command
 
             [&](const auto& path,
                 const auto& service) -> sdbusplus::async::task<> {
-            auto& entry_json = result[last_element(path.str, '/')]["critical"];
+            auto& sensor_json = result[last_element(path.str, '/')];
+            auto& entry_json = sensor_json["critical"];
             auto proxy =
                 sensor::critical::Proxy(ctx).service(service).path(path.str);
 
             if (auto v = co_await proxy.critical_high(); std::isfinite(v))
             {
                 entry_json["high"] = v;
+                update_status<std::greater>(sensor_json, v, "critical");
             }
             if (auto v = co_await proxy.critical_low(); std::isfinite(v))
             {
                 entry_json["low"] = v;
+                update_status<std::less>(sensor_json, v, "critical");
             }
         });
 
-        info("Finding HardShutdown thresholds");
+        info("Finding Warning thresholds");
         co_await utils::mapper::subtree_for_each(
-            ctx, sensor::ns_path, sensor::hard_shutdown::interface,
+            ctx, sensor::ns_path, sensor::warning::interface,
 
             [&](const auto& path,
                 const auto& service) -> sdbusplus::async::task<> {
-            auto& entry_json =
-                result[last_element(path.str, '/')]["hard_shutdown"];
+            auto& sensor_json = result[last_element(path.str, '/')];
+            auto& entry_json = sensor_json["warning"];
             auto proxy =
-                sensor::hard_shutdown::Proxy(ctx).service(service).path(
-                    path.str);
+                sensor::warning::Proxy(ctx).service(service).path(path.str);
 
-            if (auto v = co_await proxy.hard_shutdown_high(); std::isfinite(v))
+            if (auto v = co_await proxy.warning_high(); std::isfinite(v))
             {
                 entry_json["high"] = v;
+                update_status<std::greater>(sensor_json, v, "warning");
             }
-            if (auto v = co_await proxy.hard_shutdown_low(); std::isfinite(v))
+            if (auto v = co_await proxy.warning_low(); std::isfinite(v))
             {
                 entry_json["low"] = v;
+                update_status<std::less>(sensor_json, v, "warning");
             }
         });
 
@@ -121,6 +133,28 @@ struct command
     static auto last_element(const auto& s, char c)
     {
         return s.substr(s.find_last_of(c) + 1);
+    }
+
+    template <template <typename> typename comparison>
+    static auto update_status(auto& entry_json, auto value, const auto& name)
+    {
+        if (!entry_json.contains("status"))
+        {
+            entry_json["status"] = "unavailable";
+            return;
+        }
+
+        auto& status = entry_json["status"];
+
+        if (status != "ok")
+        {
+            return;
+        }
+
+        if (comparison<decltype(value)>()(entry_json["value"], value))
+        {
+            status = name;
+        }
     }
 };
 MFGTOOL_REGISTER(command);

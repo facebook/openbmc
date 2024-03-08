182c93164ec85a39efc78768c09cc2ca4f9cec8e
Tao Ren <taoren@meta.com>
2024-03-08 10:12:11 -0800

    montblanc: openbmc-utils: refactor spi_util.sh
    Summary:
    Get rid of udev rules and restructure spi_util.sh for easier
    maintenance.
    
    Test Plan:
    1) "bitbake montblanc-image" passed.
    2) Ran "spi_util.sh read spi1 IOB_FPGA iob.bin" for twice: the image
       files (in 2 reads) are identical.
    3) Ran "spi_util.sh read spi1 COME_BIOS bios.bin" for twice: the image
       files (in 2 reads) are identical.
    4) launched 2 spi_util.sh from different terminals: the 2nd instance failed.
    
    Reviewed By: joancaneus
    
    Differential Revision: D54720067
    
    fbshipit-source-id: ecc266690bf9bea83c5ee5bfd09c0a708aaadf0a

--- meta-facebook/meta-montblanc/recipes-utils/openbmc-utils/files/spi_util.sh
+++ meta-facebook/meta-montblanc/recipes-utils/openbmc-utils/files/spi_util.sh
@@ -17,227 +17,220 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
+
 #shellcheck disable=SC1091
 #shellcheck disable=SC2086
 . /usr/local/bin/openbmc-utils.sh
 
-ret=0
-trap cleanup_spi INT TERM QUIT EXIT
+SPI1_MASTER="1e630000.spi"
+ASPEED_SMC_DRIVER_DIR="/sys/bus/platform/drivers/spi-aspeed-smc"
 
-PIDFILE="/var/run/spi_util.pid"
-check_duplicate_process()
-{
-    exec 2>$PIDFILE
-    flock -n 2 || (echo "Another process is running" && exit 1)
-    ret=$?
-    if [ $ret -eq 1 ]; then
-      exit 1
-    fi
-    pid=$$
-    echo $pid 1>&200
-}
+#
+# Both come_cpld and scm_cpld are needed to select/deselect chips.
+#
+COMECPLD_I2C_BUS=0
+COMECPLD_I2C_ADDR=0x1f
+SCMCPLD_I2C_BUS=1
+SCMCPLD_I2C_ADDR=0x35
 
-remove_pid_file()
-{
-    if [ -f $PIDFILE ]; then
-        rm $PIDFILE
-    fi
-}
+#
+# The chipselect and mtd labels assigned to IOB and BIOS.
+#
+IOB_MTD_LABEL="host-flash"
+BIOS_MTD_LABEL="host-flash"
 
-SPI1_DRIVER="1e630000.spi"
-ASPEED_SMC_PATH="/sys/bus/platform/drivers/spi-aspeed-smc/"
+PID_FILE="/var/run/spi_util.pid"
 
-mtd_driver_unbind_spi1() {
-    [ -e ${ASPEED_SMC_PATH}${SPI1_DRIVER} ] && echo ${SPI1_DRIVER} > ${ASPEED_SMC_PATH}unbind
-}
-
-mtd_driver_rebind_spi1() {
-    # Sometime kernel may complain "vmap allocation for size 268439552 failed: use vmalloc=<size> to increase size"
-    # until now the root cause is not clear, still need more investigation
-    mtd_driver_unbind_spi1
-    echo ${SPI1_DRIVER} > ${ASPEED_SMC_PATH}bind
-
-    # add more delay to make sure the driver has probe done. 
-    sleep 1;
-
-    SPI1_MTD_PATH=$(realpath /run/devmap/spi-devices/spi1-flash)
-    if [ ! -e "$SPI1_MTD_PATH" ]; then
-        echo "cannot find SPI1 MTD path [$SPI1_MTD_PATH]"
-        exit 1
-    fi
-    SPI1_MTD_INDEX=$(echo "$SPI1_MTD_PATH" | sed 's_/dev/mtd__g')
-}
-
-read_spi1_dev(){
-    dev=$1
-    file=$2
-    echo "Reading ${dev} to $file..."
-    flashrom -p linux_mtd:dev=$SPI1_MTD_INDEX -r "$file"
-}
-
-write_spi1_dev(){
-    dev=$1
-    file=$2
-    echo "Writing $file to ${dev}..."
-
-    tmpfile="/tmp/temp_image"
-    flashsize=$(flashrom -p linux_mtd:dev=$SPI1_MTD_INDEX | grep -i kB | xargs echo | cut -d '(' -f 2 | cut -d ' ' -f 0)
-    targetsize=$((flashsize * 1024))
-
-    case ${dev} in
-        "COME_BIOS"|"PROT"|"IOB_FPGA")
-        # store to the temp-file
-        cp "$file" "$tmpfile"
-        ;;        
-    esac
-
-    #extend the temp-file
-    filesize=$(stat -c%s $tmpfile)
-    echo $filesize
-    add_size=$((targetsize - filesize))
-    echo $add_size
-    dd if=/dev/zero bs="$add_size" count=1 | tr "\000" "\377" >> "$tmpfile"
-
-    echo flashrom -p linux_mtd:dev=$SPI1_MTD_INDEX -w "$tmpfile"
-
-    flashrom -p linux_mtd:dev=$SPI1_MTD_INDEX -w "$tmpfile"
-
-    case ${dev} in
-        "IOB_FPGA")
-            # TODO: add reset signal component after programed
-        ;;
-        "COME_BIOS")
-            # no need action, let user run command to reset COMe [wedge_power.sh reset]
-        ;;
-        "PROT")
-            # TODO: need confirm, this path use only for debuging
-        ;;
-    esac
-}
-
-erase_spi1_dev(){
-    flashrom -p linux_mtd:dev=$SPI1_MTD_INDEX -E
-}
-
-config_spi1_pin_and_path(){
-    dev=$1
-
-    # enable i2c buff BMC able to access 
-    #  i2c-0  for COME CPLD  (0x1f)
-    #  i2c-1  for SCM CPLD   (0x35)
-    gpiocli -s BMC_I2C1_EN set-value 1
-    gpiocli -s BMC_I2C2_EN set-value 1
-    case ${dev} in
-        "IOB_FPGA")
-            # SCM CPLD: BMC_SPI1 
-            #                    to (X) IOB_FPGA   0
-            #                       ( ) COME_BIOS  1
-            #                       ( ) PRoT       2
-            i2cset -f -y 1 0x35 0x34 0x0
-
-            # BMC GPIO: IOB_FLASH 
-            #                    to ( ) IOB_FPGA 0
-            #                       (X) BMC_SPI1 1
-            gpiocli -s IOB_FLASH_SEL set-value 1
-        ;;
-        "COME_BIOS")
-            # SCM CPLD: BMC_SPI1 
-            #                    to ( ) IOB_FPGA   0
-            #                       (X) COME_BIOS  1
-            #                       ( ) PRoT       2
-            i2cset -f -y 1 0x35 0x34 0x1
-            
-            # BMC GPIO: BIOS_FLASH 
-            #                    to ( ) NetLake CPU 0
-            #                       (X) BMC_SPI1    1
-            gpiocli -s SPI_MUX_SEL set-value 1
-            
-            # COME CPLD: BIOS_FLASH_CS0
-            #                    to ( ) BMC_SPI1_CS1 0x7
-            #                       (X) BMC_SPI1_CS0 0x5
-            #                       ( ) SPI_PCH_CS0 0x4
-            #                       ( ) SPI_PCH_CS1 0x6
-            i2cset -f -y 0 0x1f 0xa 0x5
-        ;;
-        "PROT")
-            # TODO: need confirm, this path use only for debuging
-            # SCM CPLD: set BMC_SPI1 
-            #                    to ( ) IOB_FPGA   0
-            #                       (X) COME_BIOS  1
-            #                       ( ) PRoT       2
-            i2cset -f -y 1 0x35 0x34 0x2
-        ;;
-    esac
-    mtd_driver_rebind_spi1
-}
-
-cleanup_spi(){
-    if [ $ret -eq 1 ]; then
-        exit 1
-    fi
-
+cleanup_spi() {
+    echo "Restore environment.."
     # select muxing FLASH to normal mode
-    # BMC GPIO: IOB_FLASH 
-    #                    to (X) IOB_FPGA    0
-    #                       ( ) BMC_SPI1    1
+    # BMC GPIO: IOB_FLASH
+    #              to (X) IOB_FPGA    0
+    #                 ( ) BMC_SPI1    1
     gpiocli -s IOB_FLASH_SEL set-value 0
-    # BMC GPIO: BIOS_FLASH 
-    #                    to (X) NetLake CPU 0
-    #                       ( ) BMC_SPI1    1
+    # BMC GPIO: BIOS_FLASH
+    #               to (X) NetLake CPU 0
+    #                  ( ) BMC_SPI1    1
     gpiocli -s SPI_MUX_SEL set-value 0
 
-    rm -rf /tmp/*_spi*_tmp
-    remove_pid_file
+    rm -rf /tmp/.spi_util*
+    rm -f "$PID_FILE"
 }
 
-operate_spi1_dev(){
-    op=$1
-    dev=$2
+check_duplicate_process() {
+    exec 2>"$PID_FILE"
+    if ! flock -n 2; then
+        echo "Another instance is running. Exiting!!"
+        exit 1
+    fi
+}
+
+enable_cpld_access() {
+    echo "Enable access of COME and SCM CPLDs.."
+
+    gpiocli -s BMC_I2C1_EN set-value 1
+    gpiocli -s BMC_I2C2_EN set-value 1
+}
+
+select_iob_flash() {
+    echo "Select IOB_FPGA flash.."
+
+    # SCM CPLD: BMC_SPI1
+    #              to (X) IOB_FPGA   0
+    #                 ( ) COME_BIOS  1
+    #                 ( ) PRoT       2
+    i2cset -f -y "$SCMCPLD_I2C_BUS" "$SCMCPLD_I2C_ADDR" 0x34 0x0
+
+    # BMC GPIO: IOB_FLASH
+    #              to ( ) IOB_FPGA 0
+    #                 (X) BMC_SPI1 1
+    gpiocli -s IOB_FLASH_SEL set-value 1
+}
+
+select_bios_flash() {
+    echo "Select BIOS flash.."
+
+    # SCM CPLD: BMC_SPI1
+    #              to ( ) IOB_FPGA   0
+    #                 (X) COME_BIOS  1
+    #                 ( ) PRoT       2
+    i2cset -f -y "$SCMCPLD_I2C_BUS" "$SCMCPLD_I2C_ADDR" 0x34 0x1
+
+    # BMC GPIO: BIOS_FLASH
+    #              to ( ) NetLake CPU 0
+    #                 (X) BMC_SPI1    1
+    gpiocli -s SPI_MUX_SEL set-value 1
+
+    # COME CPLD: BIOS_FLASH_CS0
+    #               to ( ) BMC_SPI1_CS1 0x7
+    #                  (X) BMC_SPI1_CS0 0x5
+    #                  ( ) SPI_PCH_CS0 0x4
+    #                  ( ) SPI_PCH_CS1 0x6
+    i2cset -f -y "$COMECPLD_I2C_BUS" "$COMECPLD_I2C_ADDR" 0xa 0x5
+}
+
+probe_flashes() {
+    echo "Probe flashes.."
+
+    echo "$SPI1_MASTER" > "${ASPEED_SMC_DRIVER_DIR}/unbind"
+    sleep 1
+
+    echo "$SPI1_MASTER" > "${ASPEED_SMC_DRIVER_DIR}/bind"
+    sleep 1
+}
+
+flash_mtd_read() {
+    mtd_idx="$1"
+    file="$2"
+
+    echo "Read /dev/mtd${mtd_idx} to $file..."
+    flashrom -p linux_mtd:dev="$mtd_idx" -r "$file"
+}
+
+flash_mtd_erase() {
+    mtd_idx="$1"
+
+    echo "Erase /dev/mtd$mtd_idx.."
+    flashrom -p linux_mtd:dev="$mtd_idx" -E
+}
+
+expand_image_file() {
+    mtd_idx=$1
+    file=$2
+
+    flashsize=$(flashrom -p linux_mtd:dev=$mtd_idx | grep -i kB |
+                xargs echo | cut -d '(' -f 2 | cut -d ' ' -f 0)
+    targetsize=$((flashsize * 1024))
+    filesize=$(stat -c%s $file)
+    add_size=$((targetsize - filesize))
+
+    if [ "$add_size" -gt 0 ]; then
+        echo "Append $add_size bytes to $file.."
+        dd if=/dev/zero bs="$add_size" count=1 | tr "\000" "\377" >> "$file"
+    fi
+}
+
+flash_mtd_write() {
+    mtd_idx=$1
+    file=$2
+    tmpfile="/tmp/.spi_util_image.tmp"
+
+    echo "Copy $file to $tmpfile.."
+    cp "$file" "$tmpfile"
+
+    extend_image_file "$mtd_idx" "$tmpfile"
+
+    echo "Write $tmpfile to /dev/mtd$mtd_idx.."
+    flashrom -p linux_mtd:dev="$mtd_idx" -w "$tmpfile"
+}
+
+do_flash_io() {
+    action=$1
+    device=$2
     file=$3
-    ## Operate devices ##
-    case ${op} in
+    mtd_label=""
+
+    enable_cpld_access
+
+    # Select the right flash chip
+    case "$device" in
+        "IOB_FPGA")
+            select_iob_flash
+            mtd_label="${IOB_MTD_LABEL}"
+        ;;
+
+        "COME_BIOS")
+            select_bios_flash
+            mtd_label="${BIOS_MTD_LABEL}"
+        ;;
+
+        *)
+            echo "Error: unsupported device <$device>!"
+            exit 1
+        ;;
+    esac
+
+    # Force re-probe flashes
+    probe_flashes
+    mtd=$(mtd_lookup_by_name "$mtd_label")
+    mtd_idx=$(echo "$mtd" | sed -e 's/mtd//')
+    if [ -z "$mtd_idx" ]; then
+        echo "Error: unable to find <$mtd_label> mtd partition!"
+        exit 1
+    fi
+
+    # Read/write/erase the flash chip
+    case "$action" in
         "read")
-                read_spi1_dev "$dev" "$file"
+            if [ -z "$file" ]; then
+                echo "Error: <pathname> is mandatory for $action!"
+                exit 1
+            fi
+            flash_mtd_read "$mtd_idx" "$file"
         ;;
+
         "write")
-                if [ ! -f "$file" ]; then
-                    echo "$file not exist!!!"
-                    exit 1
-                fi
-                write_spi1_dev "$dev" "$file"
+            if [ ! -e "$file" ]; then
+                echo "Error: <$file> does not exist!"
+                exit 1
+            fi
+            flash_mtd_write "$mtd_idx" "$file"
         ;;
+
         "erase")
-                erase_spi1_dev "$dev"
+            flash_mtd_erase "$mtd_idx"
         ;;
-        *)
-            echo "Operation $op is not defined."
-        ;;
-    esac
-    mtd_driver_unbind_spi1
-}
 
-ui(){
-    . /usr/local/bin/openbmc-utils.sh
-    op=$1
-    spi=$2
-
-    ## Open the path to device ##
-    case ${spi} in
-        "spi1")
-            dev=$3
-            file=$4
-            config_spi1_pin_and_path "$dev"
-            operate_spi1_dev "$op" "$dev" "$file"
-        ;;
         *)
-            echo "No such SPI bus."
-            return 1
+            echo "Error: operation <$action> is not supported!"
+            exit 1
         ;;
     esac
 }
 
-usage(){
-    program=$(basename "$0")
+usage() {
+    program="$1"
+
     echo "Usage:"
     echo "  $program <op> <spi> <spi device> <file>"
     echo "    <op>          : read, write, erase"
@@ -245,75 +238,36 @@ usage(){
     echo "    <spi1 device> : IOB_FPGA, COME_BIOS"
     echo ""
     echo "Examples:"
-    echo "  $program write spi1 IOB_FPGA iofpga.bit"
+    echo "  $program read spi1 IOB_FPGA iofpga.bit"
     echo ""
 }
 
-check_parameter(){
-    program=$(basename "$0")
-    op=$1
-    spi=$2
-
-    # check operations.
-    case ${op} in
-        "read" | "write" | "erase")
-            ;;
-        *)
-            return 1
-            ;;
-    esac
-
-    case ${spi} in
-        # check spi 1 device and parameters.
-        "spi1")
-            dev=$3
-            file=$4
-            case ${dev} in
-                "IOB_FPGA"|"COME_BIOS")
-                    case ${op} in
-                        "read" | "write")
-                            if [ $# -ne 4 ]; then
-                                return 1
-                            fi
-                            ;;
-                        "erase")
-                            if [ $# -ne 3 ]; then
-                                return 1
-                            fi
-                            ;;
-                    esac
-                    ;;
-                "PROT")
-                    echo "PRoT upgrade not support yet"
-                    return 1
-                    ;;
-                *)
-                    return 1
-                    ;;
-            esac
-            ;;
-
-        # check device and parameters fail.
-        *)
-            return 1
-            ;;
-    esac
-
-    return 0
-}
-
-
-# check_duplicate_process - to check if there is the duplicate spi util process, will return fail and stop execute command.
+#
+# Main entry starts here
+#
 check_duplicate_process
 
-# check_parameter - to check command parameters
-check_parameter "$@"
-is_par_ok=$?
-
-if [ $is_par_ok -eq 0 ]; then
-    ui "$@"
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage "$0"
     exit 0
-else
-    usage
+fi
+
+if [ "$#" -ne 4 ] && [ "$#" -ne 5 ]; then
+    echo "Error: invalid command line arguments!"
+    usage "$0"
     exit 1
 fi
+
+action="$1"
+spi_bus="$2"
+device="$3"
+image_file="$4"
+
+if [ "$spi_bus" != "spi1" ]; then
+    echo "Error: invalid spi bus: $spi_bus!"
+    exit 1
+fi
+
+trap cleanup_spi INT TERM QUIT EXIT
+
+do_flash_io "$action" "$device" "$image_file"

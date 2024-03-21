146bbb93b1a16d824edece9baea472276cbfab02
IngridChen-wiwynn <Ingrid_Chen@wiwynn.com>
2024-03-21 10:59:21 -0700

    yosemite4: Improve error handling when CXL BIC update
    Summary:
    # Description
    - Avoid user who wants to update BICs at the same time.
    
    # Motivation
    - Check pldmd is active
    - Check cxl eid exist
    - Check there is no other bic updating
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/3015
    
    Test Plan:
    - Build code: Pass
    - Shell check: Pass
    
    # Test log
    
    - Not allowed to update when EID not exist. Check MCTP EID
    └─ /xyz
      └─ /xyz/openbmc_project └─ /xyz/openbmc_project/mctp └─ /xyz/openbmc_project/mctp/1 ├─ /xyz/openbmc_project/mctp/1/10 ├─ /xyz/openbmc_project/mctp/1/12 ├─ /xyz/openbmc_project/mctp/1/20 ├─ /xyz/openbmc_project/mctp/1/21 ├─ /xyz/openbmc_project/mctp/1/23 ├─ /xyz/openbmc_project/mctp/1/30 └─ /xyz/openbmc_project/mctp/1/8 Not allowed. The CXL EID 25 does not exist.
    
    - When another bic is updating, prohibit to execute.
    
    root@bmc:~# cxl-fw-update.sh 6 2 /tmp/pldm_oby4-sd-2024.11.01 BIC update is already running
    
    Reviewed By: jagpalgill
    
    Differential Revision: D55181168
    
    fbshipit-source-id: 96c50d9c2f59ec0ee0262f897bfcb3b17dc9a3c2

--- meta-facebook/meta-yosemite4/recipes-facebook/cxl-fw-update/files/cxl-fw-update.sh
+++ meta-facebook/meta-yosemite4/recipes-facebook/cxl-fw-update/files/cxl-fw-update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+lockfile="/tmp/bic-update.lock"
+
+exec 200>"$lockfile"
+flock -n 200 || { echo "BIC update is already running"; exit 1; }
+
+CXL1_EID_suffix=4
+CXL2_EID_suffix=5
+
 show_usage() {
     echo "Usage: cxl-fw-update.sh <slot_id> (<instance_num>) <pldm image>"
     echo "       update CXL1  : cxl-fw-update.sh <slot_id> 1 <pldm image>"
@@ -83,6 +91,46 @@ else
     exit 255
 fi
 
+if ! systemctl is-active --quiet pldmd; then
+    echo "STOP. PLDM service is not running. Please check pldmd status."
+    exit 255
+fi
+
+#Show all mctp eids, and check cxl eid should exist
+#The cxl eid would end with 4 or 5.
+mctp_output=$(busctl tree xyz.openbmc_project.MCTP)
+echo "Check MCTP EID"
+echo "$mctp_output"
+if ! echo "$mctp_output" | grep -qE "/xyz/openbmc_project/mctp/1/$((slot_id * 10 + (instance_num == 1 ? CXL1_EID_suffix : CXL2_EID_suffix)))"; then
+  echo "Not allowed. The CXL EID $((slot_id * 10 + (instance_num == 1 ? CXL1_EID_suffix : CXL2_EID_suffix))) does not exist."
+  exit 255
+fi
+
+
+echo "Check if other BICs are updating"
+pldm_output=$(busctl tree xyz.openbmc_project.PLDM)
+if echo "$pldm_output" | grep -qE "/xyz/openbmc_project/software/[0-9]+"; then
+echo "$pldm_output" | grep -E "/xyz/openbmc_project/software/[0-9]+"
+previous_software_id=$(busctl tree xyz.openbmc_project.PLDM |grep /xyz/openbmc_project/software/ | cut -d "/" -f 5)
+busctl get-property xyz.openbmc_project.PLDM /xyz/openbmc_project/software/"$previous_software_id" xyz.openbmc_project.Software.ActivationProgress Progress > /dev/null
+ret=$?
+    if [ "$ret" -eq 0 ]; then
+        echo "It can only be updated one BIC at a time. Please wait until the software update is completed."
+        exit 255
+    fi
+fi
+
+
 update_cxl "$pldm_image"
 
-echo "Update done. If there was no error appeared, please conduct DC cycle to load the new firmware."
+busctl call xyz.openbmc_project.PLDM /xyz/openbmc_project/software/"$software_id" xyz.openbmc_project.Object.Delete Delete
+ret=$?
+if [ "$ret" -ne 0 ]; then
+echo "Failed to delete software id: Exit code $ret"
+exit $ret
+fi
+
+echo "Done. If there was no error appeared, please conduct DC cycle to load the new firmware."
+
+#unlock
+flock -u 200

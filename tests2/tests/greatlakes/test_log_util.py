1c2e89911bdece6d403f10913c8f1abe03f17fc2
Jagpal Singh Gill <jagpalgill@meta.com>
2024-04-02 15:00:16 -0700

    Greatlakes: Add the test2/tests suite
    Summary: Add the unit test suite for greatlakes.
    
    Test Plan: Conveyor error passes.
    
    Reviewed By: wangx6f
    
    Differential Revision: D55627532
    
    fbshipit-source-id: cd00ad9d6d42aa7857695cf16482020da74d0818

---
+++ tests2/tests/greatlakes/test_log_util.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_log_util_test import BaseLogUtilTest
+
+
+class AllLogUtilTest(BaseLogUtilTest, unittest.TestCase):
+    FRU = "all"
+
+    @unittest.skip("not available")
+    def test_log_clear(self):
+        pass
+
+
+class BmcLogUtilTest(AllLogUtilTest):
+    FRU = "bmc"
+
+
+class NicLogUtilTest(AllLogUtilTest):
+    FRU = "nic"
+
+
+class Slot1LogUtilTest(AllLogUtilTest):
+    FRU = "slot1"
+
+
+class Slot2LogUtilTest(AllLogUtilTest):
+    FRU = "slot2"
+
+
+class Slot3LogUtilTest(AllLogUtilTest):
+    FRU = "slot3"
+
+
+class Slot4LogUtilTest(AllLogUtilTest):
+    FRU = "slot4"
+
+
+class SysLogUtilTest(BaseLogUtilTest, unittest.TestCase):
+    FRU = "sys"

4b197ad48c58f74cf7dd929a2488dc4f0b31a704
Potin Lai <potin.lai@quantatw.com>
2024-04-18 10:54:05 -0700

    common: mfg-tool: fix crush when log entry missing
    Summary:
    # Description
    
    Add exception handling during the log entry parsing to avoid program crashes.
    
    # Motivation
    
    Phosphor-logging deletes the oldest log entries when the maximum number of logs is reached, and then adds new logs. mfg-tool may not be able to parse the log entries because some log entries have been deleted.
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4256
    
    Test Plan: mfg-tool log-display works normall without crash happened again.
    
    Reviewed By: williamspatrick
    
    Differential Revision: D56282340
    
    fbshipit-source-id: c85814ee806d5e3b26d117a114ab7623f0d466bb

--- common/recipes-lfopenbmc/mfg-tool/files/cmd/log-display.cpp
+++ common/recipes-lfopenbmc/mfg-tool/files/cmd/log-display.cpp
@@ -47,38 +47,44 @@ struct command
                 co_return;
             }
 
-            auto entry = log_entry::Proxy(ctx)
-                             .service(log_entry::service)
-                             .path(path.str);
+            try {
+                auto entry = log_entry::Proxy(ctx)
+                                .service(log_entry::service)
+                                .path(path.str);
 
-            if (auto resolved = co_await entry.resolved();
-                !resolved || !arg_unresolved_only)
-            {
-                auto& entry_json = result[std::to_string(co_await entry.id())];
+                if (auto resolved = co_await entry.resolved();
+                    !resolved || !arg_unresolved_only)
+                {
+                    auto& entry_json = result[std::to_string(co_await entry.id())];
 
-                entry_json["message"] = co_await entry.message();
-                entry_json["severity"] = sdbusplus::message::convert_to_string(
-                    co_await entry.severity());
-                entry_json["event_id"] = co_await entry.event_id();
-                entry_json["additional_data"] =
-                    co_await entry.additional_data();
-                entry_json["resolution"] = co_await entry.resolution();
-                entry_json["resolved"] = resolved;
+                    entry_json["message"] = co_await entry.message();
+                    entry_json["severity"] = sdbusplus::message::convert_to_string(
+                        co_await entry.severity());
+                    entry_json["event_id"] = co_await entry.event_id();
+                    entry_json["additional_data"] =
+                        co_await entry.additional_data();
+                    entry_json["resolution"] = co_await entry.resolution();
+                    entry_json["resolved"] = resolved;
 
-                auto epoch_to_iso8601 = [](auto ts) {
-                    using namespace std::chrono;
-                    return std::format("{:%FT%TZ}", time_point<system_clock>(
-                                                        milliseconds(ts)));
-                };
+                    auto epoch_to_iso8601 = [](auto ts) {
+                        using namespace std::chrono;
+                        return std::format("{:%FT%TZ}", time_point<system_clock>(
+                                                            milliseconds(ts)));
+                    };
 
-                entry_json["timestamp"] =
-                    epoch_to_iso8601(co_await entry.timestamp());
-                entry_json["updated_timestamp"] =
-                    epoch_to_iso8601(co_await entry.update_timestamp());
+                    entry_json["timestamp"] =
+                        epoch_to_iso8601(co_await entry.timestamp());
+                    entry_json["updated_timestamp"] =
+                        epoch_to_iso8601(co_await entry.update_timestamp());
+                }
+                else
+                {
+                    info("Resolved and filtered out.");
+                }
             }
-            else
+            catch(...)
             {
-                info("Resolved and filtered out.");
+                warning("Failed to parse log entry: {PATH}", "PATH", path.str);
             }
         });
 

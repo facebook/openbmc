b58011eee04203ffa50e0460d1b02867f237f378
JayZhang-wiwynn <jay_zhang@wiwynn.com>
2024-05-20 17:57:32 -0700

    fby35: Support display TPM version with different vendor
    Summary:
    # Description:
     1.Specific vendor's TPM version not fully display in BMC.
     2.TPM vendor include IFX(Infineon) and Nuvoton.
    
    # Motivation:
     Support display fully TPM version with different vendor.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/3066
    
    Test Plan:
    1.Build code:Pass
     2.Confirm can display fully TPM version with different vendor.
    
    # Log:
     1.IFX(Infineon)
     root@bmc-oob:~# fw-util bmc --version tpm
     TPM Version: 7.85
    
     2.Nuvoton
     root@bmc-oob:~# fw-util bmc --version tpm
     TPM Version: 7.2.3.1
    
    Reviewed By: williamspatrick
    
    Differential Revision: D57364165
    
    fbshipit-source-id: 6389dd4c7d9858d2e0a0f992765941f64acff669

--- common/recipes-core/fw-util/files/tpm2.cpp
+++ common/recipes-core/fw-util/files/tpm2.cpp
@@ -8,6 +8,19 @@
 using namespace std;
 
 #define MAX_LINE_LENGTH 80
+#define MANUFACTURER_NUVOTON 0x4E544300
+#define TPM2_IFX 0
+#define TPM2_NUVOTON 1
+
+bool Tpm2Component::read_Tpm2_cmd_info(FILE* stream, const std::string& pattern, uint32_t& hexValue) {
+  char buffer[MAX_LINE_LENGTH] = {0};
+  while (fgets(buffer, sizeof(buffer), stream) != NULL) {
+    if (sscanf(buffer, pattern.c_str(), &hexValue) == 1) {
+      return true;
+    }
+  }
+  return false;
+}
 
 int Tpm2Component::get_version(json& j) {
 
@@ -23,30 +36,57 @@ int Tpm2Component::get_version(json& j) {
   try {
     j["VERSION"] = kv::get(version_cache);
   } catch (std::exception&) {
-    bool match = false;
-    uint32_t fw_ver = 0;
-    for (size_t i = 0; !match && i < version_command.size(); i++) {
-      FILE *fp = popen(version_command[i].c_str(), "r");
-      if (!fp) {
-        syslog(LOG_WARNING, "Cannot open TPM2 file");
-        return FW_STATUS_SUCCESS;
+
+    int tpm2_vendor = 0;
+    uint32_t manufacturer_id = 0;
+
+    FILE* fp_id = popen(version_vendorID.c_str(), "r");
+    FILE* fp_ver1 = popen(version_command[0].c_str(), "r");
+    FILE* fp_ver2 = popen(version_command[1].c_str(), "r");
+
+    if ((!fp_id) || (!fp_ver1) || (!fp_ver2)) {
+      syslog(LOG_WARNING, "Cannot get TPM2 Information!");
+      return FW_STATUS_FAILURE;
+    }
+
+    // use TPM2_PT_MANUFACTURER to get TPM Vendor
+    if (read_Tpm2_cmd_info(fp_id, "  raw: 0x%x", manufacturer_id)) {
+      if (manufacturer_id == MANUFACTURER_NUVOTON) {
+        tpm2_vendor = TPM2_NUVOTON;
+      } else {
+        tpm2_vendor = TPM2_IFX;
       }
-      char value[MAX_LINE_LENGTH] = {0};
-      while (fgets(value, sizeof(value), fp)) {
-        if (sscanf(value, "%*s %x", &fw_ver) == 1) {
-          string vers = std::to_string(fw_ver >> 16) + "."
-                  + std::to_string(fw_ver & 0xFFFF);
-          j["VERSION"] = vers;
-          try {
-            kv::set(version_cache, vers);
-          } catch (std::exception&) {
-            syslog(LOG_WARNING, "Cannot cache TPM2 version");
-          }
-          match = true;
-          break;
-        }
-      }
-      pclose(fp);
+    }
+    pclose(fp_id);
+
+    // IFX     : TPM Version = TPM2_PT_FIRMWARE_VERSION_1
+    // Nuvoton : TPM Version = TPM2_PT_FIRMWARE_VERSION_1 + TPM2_PT_FIRMWARE_VERSION_2
+    string tpm_version_1;
+    uint32_t fw_ver_1 = 0;
+    if (read_Tpm2_cmd_info(fp_ver1, "%*s %x", fw_ver_1)) {
+      tpm_version_1 = std::to_string(fw_ver_1 >> 16) + "." + std::to_string(fw_ver_1 & 0xFFFF);
+    }
+    pclose(fp_ver1);
+
+    string tpm_version_2;
+    uint32_t fw_ver_2 = 0;
+    if (read_Tpm2_cmd_info(fp_ver2, "%*s %x", fw_ver_2)) {
+      tpm_version_2 = std::to_string(fw_ver_2 >> 16) + "." + std::to_string(fw_ver_2 & 0xFFFF);
+    }
+    pclose(fp_ver2);
+
+    // Set TPM Version
+    string tpm_version;
+    if (tpm2_vendor) { //for Nuvoton
+      tpm_version = tpm_version_1 + "." + tpm_version_2;
+    } else { //for IFX
+      tpm_version = tpm_version_1;
+    }
+    j["VERSION"] = tpm_version;
+    try {
+      kv::set(version_cache, tpm_version);
+    } catch (std::exception&) {
+      syslog(LOG_WARNING, "Cannot cache TPM2 version!");
     }
   }
   return FW_STATUS_SUCCESS;
--- common/recipes-core/fw-util/files/tpm2.h
+++ common/recipes-core/fw-util/files/tpm2.h
@@ -8,14 +8,18 @@ class Tpm2Component : public Component {
     std::string device{};
     std::vector<std::string> version_command{};
     std::string version_cache{};
+    std::string version_vendorID{};
     Tpm2Component(std::string fru, std::string comp,
                   std::string dev = "/sys/class/tpm/tpm0",
                   std::vector<std::string> ver_cmd = {
-                    "/usr/bin/tpm2_getcap -c properties-fixed 2>/dev/null | grep TPM_PT_FIRMWARE_VERSION_1",
-                    "/usr/bin/tpm2_getcap properties-fixed 2>/dev/null | grep -A1 TPM2_PT_FIRMWARE_VERSION_1"},
-                  std::string ver_cache = "tpm2_version")
-      : Component(fru, comp), device(dev), version_command(ver_cmd), version_cache(ver_cache) {}
+                    "/usr/bin/tpm2_getcap properties-fixed 2>/dev/null | grep -A1 TPM2_PT_FIRMWARE_VERSION_1",
+                    "/usr/bin/tpm2_getcap properties-fixed 2>/dev/null | grep -A1 TPM2_PT_FIRMWARE_VERSION_2"},
+                  std::string ver_cache = "tpm2_version",
+                  std::string ver_vendor =
+                    "/usr/bin/tpm2_getcap properties-fixed 2>/dev/null | grep -A1 TPM2_PT_MANUFACTURER")
+      : Component(fru, comp), device(dev), version_command(ver_cmd), version_cache(ver_cache), version_vendorID(ver_vendor) {}
     int get_version(json& j) override;
+    bool read_Tpm2_cmd_info(FILE* stream, const std::string& pattern, uint32_t& hexValue);
 };
 
 #endif

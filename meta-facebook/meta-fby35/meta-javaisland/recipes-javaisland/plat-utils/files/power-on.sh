ebe3e6230dcdec027ec5cd4e699c1c3c3a92c567
Ken Chen <j220584470k@gmail.com>
2024-06-10 10:59:20 -0700

    Javaisland: avoid to execute me-util while boot-up
    Summary:
    bmc will try to sync time form ntp or ME when boot-up, however Javaisland didn't have ME function.
    
    1.Same as Halfdome, add power-on.sh for Javaisland and only use ntp to sync time
    2. add ntp timesync checking to defer starting slots on POR till BMC time synced
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/4302
    
    Test Plan:
    build and test on javaisland pass.
    
    Fri Mar  9 04:35:12 2018: Stopping ntpd: done
    Fri Mar  9 04:35:12 2018: Starting ntpd: done
    Fri Mar  9 04:35:12 2018: [Fri Mar  9 04:35:12 PST 2018] ntp is enabled, ntp_server: 216.239.35.0
    Fri Mar  9 04:35:43 2018: [Fri Mar  9 04:35:43 PST 2018] system time not synced after 30 sec
    Fri Mar  9 04:35:43 2018: Powering fru 1 to ON state...
    Fri Mar  9 04:35:47 2018: check eth0 ipv4...
    Fri Mar  9 04:35:47 2018: Setup fan speed...
    Fri Mar  9 04:35:47 2018: Setting Zone 1 speed to 70
    Fri Mar  9 04:35:47 2018: Setting Zone 0 speed to 70
    Fri Mar  9 04:35:47 2018: Setting Zone 2 speed to 70
    Fri Mar  9 04:35:47 2018: Setting Zone 3 speed to 70
    Fri Mar  9 04:35:48 2018: Setup gpio monitoring for fby35... done.
    Fri Mar  9 04:35:48 2018: Setup gpiointrd for fby35...done.
    Fri Mar  9 04:35:48 2018: Setup healthd for BMC done.
    Fri Mar  9 04:35:48 2018: Setup ncsid for BMC done.
    Fri Mar  9 04:35:48 2018: Get MB FW version...
    Fri Mar  9 04:35:49 2018: Setup sensor monitoring for FBYV35... done.
    Fri Mar  9 04:35:49 2018: Setup crond...starting Busybox Periodic Command Scheduler: crond... done.
    Fri Mar  9 04:35:49 2018: done.
    Fri Mar  9 04:35:49 2018: check bmc ready...
    
    Reviewed By: williamspatrick
    
    Differential Revision: D58134267
    
    fbshipit-source-id: 10120ddd642f2cb28d355b1750c9162fb8ec63b4

---
+++ meta-facebook/meta-fby35/meta-javaisland/recipes-javaisland/plat-utils/files/power-on.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+#
+# Copyright 2014-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+### BEGIN INIT INFO
+# Provides:          power-on
+# Required-Start:
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Power on Server
+### END INIT INFO
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+
+# shellcheck disable=SC1091
+. /usr/local/fbpackages/utils/ast-functions
+
+DEF_PWR_ON=1
+TO_PWR_ON=
+DEBUG_ENABLE=false
+
+
+check_por_config() {
+  TO_PWR_ON=0
+
+  if POR=$(kv get "slot${1}_por_cfg" persistent); then
+    if [ "$POR" = "lps" ]; then
+      if LS=$(kv get "pwr_server${1}_last_state" persistent); then
+        if [ "$LS" = "on" ]; then
+          TO_PWR_ON=1;
+        fi
+      else
+        TO_PWR_ON=$DEF_PWR_ON
+      fi
+    elif [ "$POR" = "on" ]; then
+      TO_PWR_ON=1;
+    fi
+  else
+    TO_PWR_ON=$DEF_PWR_ON
+  fi
+}
+
+update_ntp_config_from_kv() {
+  local ntp_server
+  ntp_server=$(/usr/bin/kv get ntp_server persistent)
+  if [ -z "$ntp_server" ]; then
+    return
+  fi
+
+  sed -i "s/.*# REPLACE WITH KV.*/server $ntp_server iburst # REPLACE WITH KV/" /etc/ntp.conf
+  /etc/init.d/ntpd restart
+}
+
+is_ntp_time_synced() {
+  local str
+  local ret
+
+  str=$(/usr/sbin/ntpq -np | grep '^\*')
+  ret=$?
+
+  if [ "$DEBUG_ENABLE" = true ] ; then
+    echo "=================================="
+    echo "ret: $ret"
+    echo "----------------------------------"
+    echo "$str"
+    echo "=================================="
+  fi
+
+  # ret == 0, synced
+  # ret == 1, not synced
+  return $ret
+}
+
+ntp_sync_delay() {
+    local NTP_DELAY_COUNT
+
+    NTP_DELAY_COUNT=0
+    echo "[$(/bin/date)] ntp is enabled, ntp_server: $(/usr/bin/kv get ntp_server persistent)"
+    while [ $NTP_DELAY_COUNT -lt 30 ];
+    do
+      if is_ntp_time_synced ; then
+        break
+      fi
+      ((NTP_DELAY_COUNT++))
+      [ "$DEBUG_ENABLE" = true ] && echo "[$(/bin/date)] system time not synced, count: ${NTP_DELAY_COUNT}"
+      sleep 1
+    done
+    if is_ntp_time_synced ; then
+      echo "[$(/bin/date)] system time synced after $NTP_DELAY_COUNT sec"
+    else
+      echo "[$(/bin/date)] system time not synced after $NTP_DELAY_COUNT sec"
+    fi
+}
+
+# Check whether it is fresh power on reset
+if [ "$(is_bmc_por)" -eq 1 ]; then
+  # Disable clearing of PWM block on WDT SoC Reset
+  devmem_clear_bit "$(scu_addr 70)" 13
+
+  # Wait for NTP sync
+  update_ntp_config_from_kv
+  ntp_sync_delay
+
+  # check POR config for each slot
+  check_por_config 1
+  if [ "$(is_sb_bic_ready 1)" = "1" ] && [ $TO_PWR_ON -eq 1 ]; then
+    power-util slot1 on
+  fi
+
+  check_por_config 2
+  if [ "$(is_sb_bic_ready 2)" = "1" ] && [ $TO_PWR_ON -eq 1 ]; then
+    power-util slot2 on
+  fi
+
+  check_por_config 3
+  if [ "$(is_sb_bic_ready 3)" = "1" ] && [ $TO_PWR_ON -eq 1 ]; then
+    power-util slot3 on
+  fi
+
+  check_por_config 4
+  if [ "$(is_sb_bic_ready 4)" = "1" ] && [ $TO_PWR_ON -eq 1 ]; then
+    power-util slot4 on
+  fi
+
+fi

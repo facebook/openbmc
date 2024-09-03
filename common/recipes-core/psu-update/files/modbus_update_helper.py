77d68f6553e8879744a08b5daf6217db1621841d
Amithash Prasad <amithash@meta.com>
2024-09-03 13:20:01 -0700

    hpr: Add capability to pause monitoring on PMM
    Summary:
    Adds capability for the update to pause monitoring on both
    local (rackmon) and PMM. This is done by extending
    suppress_monitoring to take in an optional address. If that
    address is of a HPR device, it will request the appropriate
    PMM to pause.
    
    Test Plan:
    (With future updates) ensure that PMM pause does the right thing
    ```
    root@bmc-oob:~# time orv3-device-update-mailbox.py --addr 48 --vendor panasonic ./PANA/BBU/HPR-BBU_vA2.19.15_0x9914_FW_TEST.bin
    Upgrade Parameters:  {'block_size': 96, 'boot_mode': 43605, 'block_wait': False}
    Pausing monitoring...
    Setting PMM16 Monitoring pause: 1 # <---- Pausing
    Parsing Firmware...
    Unlock Engineering Mode
    Entering Boot Mode...
    Transferring image
    [100.00%] Sending block 4271 of 4271...
    Request Verify Firmware
    Waiting for verification to complete
    check firmware status
    Exiting Boot Mode...
    done
    Resetting....
    Upgrade success
    Setting PMM16 Monitoring pause: 0 # <--- Resuming
    Resuming monitoring...
    ```
    
    Reviewed By: doranand
    
    Differential Revision: D62052668
    
    fbshipit-source-id: df8605fb9df0d06532c64e0251ad756fc6d4d542

--- common/recipes-core/psu-update/files/modbus_update_helper.py
+++ common/recipes-core/psu-update/files/modbus_update_helper.py
@@ -61,8 +61,52 @@ def get_parser():
     return parser
 
 
+def get_pmm_addr(addr):
+    pmm_addrs = {
+        0x10: [[0x30, 0x35]],
+        0x11: [[0x3A, 0x3F]],
+        0x12: [[0x5A, 0x5F]],
+        0x13: [[0x6A, 0x6F]],
+        0x14: [[0x70, 0x75]],
+        0x15: [[0x7A, 0x7F]],
+        0x16: [[0x80, 0x85]],
+        0x17: [[0x36, 0x39], [0x46, 0x47]],
+        0x18: [[0x56, 0x58], [0x66, 0x68]],
+        0x19: [[0x76, 0x78], [0x87, 0x88]],
+        0x1A: [[0x8A, 0x8F]],
+        0x20: [[0x90, 0x95]],
+        0x21: [[0x9A, 0x9F]],
+        0x22: [[0xAA, 0xAF]],
+        0x23: [[0xBA, 0xBF]],
+        0x24: [[0xD0, 0xD5]],
+        0x25: [[0xDA, 0xDF]],
+        0x26: [[0xE0, 0xE5]],
+        0x27: [[0x96, 0x98], [0xA7, 0xA9]],
+        0x28: [[0xB0, 0xB3], [0xB7, 0xB9]],
+        0x29: [[0xE6, 0xE7], [0xF6, 0xF9]],
+        0x2A: [[0xFA, 0xFF]],
+    }
+    for pmm_addr, addr_ranges in pmm_addrs.items():
+        for addr_range in addr_ranges:
+            if addr >= addr_range[0] and addr <= addr_range[1]:
+                return pmm_addr
+    return None
+
+
+def control_pmm_monitoring(addr, pause):
+    pmm_pause_reg = 0x7B
+    pmm_addr = get_pmm_addr(addr)
+    if pmm_addr is None:
+        return
+    try:
+        print(f"Setting PMM{pmm_addr} Monitoring pause: {pause}")
+        rmd.write(pmm_addr, pmm_pause_reg, pause)
+    except Exception:
+        print(f"WARNING: Control PMM:{pmm_pause_reg} {pause} failed")
+
+
 @contextmanager
-def suppress_monitoring():
+def suppress_monitoring(addr=None):
     """
     contextmanager to pause rackmond monitoring on entry
     and resume on exit, including exits due to exception
@@ -70,8 +114,12 @@ def suppress_monitoring():
     try:
         print("Pausing monitoring...")
         rmd.pause()
+        if addr is not None:
+            control_pmm_monitoring(addr, 0x1)
         yield
     finally:
+        if addr is not None:
+            control_pmm_monitoring(addr, 0x0)
         print("Resuming monitoring...")
         rmd.resume()
         time.sleep(10.0)

d35243a7ebe11d3f5276c8b6d9261a29e0df2bd7
Amithash Prasad <amithash@meta.com>
2024-09-27 09:35:24 -0700

    rackmon2: Allow register maps with different baudrate to overlap address ranges
    Summary:
    This change allows register maps with different baudrates to
    over lap partially or fully. For example, 0xa4 could be considered
    part of orv2_psu or orv3_hpr_psu since the two register maps use
    a different baudrates. Thus allowing rackmon to distinguish
    between them at probe time.
    
    Test Plan:
    All unit tests including the added unit tests pass.
    Test on HPR and a example ORv2 host (with address changed).
    
    Reviewed By: doranand
    
    Differential Revision: D63417444
    
    fbshipit-source-id: 5a694ad0f1a9fc6de16cc197345f9820fdace969

--- common/recipes-core/rackmon2/rackmon/scripts/address_validator.py
+++ common/recipes-core/rackmon2/rackmon/scripts/address_validator.py
@@ -3,18 +3,23 @@ import json
 import sys
 
 
-def check_addr(addr, ranges):
-    for arange in ranges:
-        if addr >= arange[0] and addr <= arange[1]:
-            return True
+def check_addr(addr, baudrate, ranges):
+    if ranges[addr] is not None and baudrate in ranges[addr]["baudrates"]:
+        return True
     return False
 
 
-def check_range(ranges, arange):
+def check_range(ranges, arange, baudrate, name):
     addrs = list(range(arange[0], arange[1] + 1))
     for addr in addrs:
-        if check_addr(addr, ranges):
+        if check_addr(addr, baudrate, ranges):
+            print(f"Conflict found for {addr} adding {name}:", ranges[addr])
             return True
+        if ranges[addr] is None:
+            ranges[addr] = {"names": [name], "baudrates": [baudrate]}
+        else:
+            ranges[addr]["baudrates"].append(baudrate)
+            ranges[addr]["names"].append(name)
     return False
 
 
@@ -22,16 +27,17 @@ def main(name, files):
     if len(files) < 1:
         print("usage: {} jsons".format(name))
         sys.exit(1)
-    address_ranges = []
+    address_ranges = dict(zip(list(range(0, 256)), [None] * 256))
     for conf in files:
         with open(conf) as f:
             data = json.load(f)
+            baudrate = data["baudrate"]
             aranges = data["address_range"]
+            name = data["name"]
             for arange in aranges:
-                if check_range(address_ranges, arange):
+                if check_range(address_ranges, arange, baudrate, name):
                     print("failure:", conf, file=sys.stderr)
                     sys.exit(1)
-                address_ranges.append(arange)
             print("success: ", conf)
 
 

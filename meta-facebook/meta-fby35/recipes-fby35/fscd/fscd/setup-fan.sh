b12f5e66b45044edcf52747325e128eb712d4e08
Sara Lin <sara_sy_lin@wiwynn.com>
2024-10-09 13:56:30 -0700

    olympic 2.2: gl: Modify fan table
    Summary:
    # Description
    - Using the same fan table as in olympic 2.0.
    
    # Motivation
    - Thermal team recommendation and customer agree using the same fan table in olympic 2.0 and 2.2.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/3195
    
    Test Plan:
    - Build code: pass
    - Check fan table: Pass
    
    # Log
    - Olympic 2.0 root@bmc-oob:~# fruid-util bmc | grep "Product Name"
    Product Name              : Olympic 2.0 PVT T8
    root@bmc-oob:~# fw-util bmc --version fscd
    Fan Speed Controller Version: Olympic_GL_FSC-V2024.09.18
    
    - Olympic 2.2 root@bmc-oob:~# fruid-util bmc | grep "Product Name"
    Product Name              : Olympic 2.2 PVT T8
    
    root@bmc-oob:~# fw-util bmc --version fscd
    Fan Speed Controller Version: Olympic_GL_FSC-V2024.09.18
    
    Reviewed By: wangx6f
    
    Differential Revision: D64091929
    
    fbshipit-source-id: 6adf71d7498996da041b195277739b5238712496

--- meta-facebook/meta-fby35/recipes-fby35/fscd/fscd/setup-fan.sh
+++ meta-facebook/meta-fby35/recipes-fby35/fscd/fscd/setup-fan.sh
@@ -176,12 +176,7 @@ init_class1_fsc() {
       case $board_type_1ou in
       8)
         config_type="8"
-        sys_sku=$(get_system_sku)
-        if [[ "$sys_sku" == "2.2" ]]; then
-          target_fsc_config="/etc/FSC_CLASS1_type8_2.2_GL_config.json"
-        else
-          target_fsc_config="/etc/FSC_CLASS1_type8_GL_config.json"
-        fi
+        target_fsc_config="/etc/FSC_CLASS1_type8_GL_config.json"
         ;;
       *)
         config_type="GL"

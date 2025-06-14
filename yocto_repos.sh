29e38db59508501c0c03ba302e769b4709d400f2
generatedunixname89002005368312 <generatedunixname89002005368312@meta.com>
2025-06-14 13:00:05 -0700

    yocto: update master:e28c63f2e
    Summary:
    Update master at e28c63f2e
    update-submodule: yocto/lf-master
    
    Test Plan:
    CIT build passes.
    
    Rollback Plan:
    
    Reviewed By: williamspatrick
    
    Differential Revision: D76661076
    
    fbshipit-source-id: 1259110653346fade2b99aee0b51e0b8b57cc4e3

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:868d4f0d3
+    lf-openbmc:e28c63f2e
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b

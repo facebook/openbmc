6344a4356b3605d7087dcebdc628a985f4a96a06
Xinyuan Wang <xinyuan.wang@meta.com>
2025-06-16 13:46:34 -0700

    refactor http test server into separate files so that multiple tests can depends on it
    Summary: as title, it would be helpful to refactor the http test server into separate header/src so that multiple tests can depends on it, will be used in the following diff in the stack
    
    Test Plan:
    unit test
    ```
    meson test
    ```
    
    Rollback Plan:
    
    Reviewed By: Prabha-Veerubhotla
    
    Differential Revision:
    D76187176
    
    Privacy Context Container: L1188897
    
    fbshipit-source-id: 8288e87b919571740da5e2d311992f031af7edc0

---
+++ common/lfopenbmc/recipes-downstream/redfish-client/files/test/helper.cpp
@@ -0,0 +1,126 @@
+#include "helper.hpp"
+
+namespace redfish_client_daemon
+{
+
+using ReceivedHttpRequest = SimpleTestHttpServer::ReceivedHttpRequest;
+
+SimpleTestHttpServer::SimpleTestHttpServer(
+    const std::string& responseStr,
+    const std::unordered_map<std::string, std::string>& responseHeaders) :
+    responseStr(responseStr), responseHeaders(responseHeaders),
+    // Start on port 0 to let the OS pick an available port.
+    acceptor(ioContext,
+             boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 0)),
+    port(acceptor.local_endpoint().port())
+{
+    startAccept();
+    serverThread = std::make_unique<std::thread>([this]() {
+        ioContext.run();
+        serverThreadStopped = true;
+    });
+}
+
+SimpleTestHttpServer::~SimpleTestHttpServer()
+{
+    if (!serverThreadStopped)
+    {
+        stopListening();
+    }
+}
+
+void SimpleTestHttpServer::stopListening()
+{
+    acceptor.cancel();
+    serverThread->join();
+}
+
+unsigned short SimpleTestHttpServer::getPort()
+{
+    return port;
+}
+
+std::vector<ReceivedHttpRequest> SimpleTestHttpServer::getReceivedRequests()
+{
+    std::lock_guard<std::mutex> lock(requestsMutex);
+    return requests;
+}
+
+ReceivedHttpRequest SimpleTestHttpServer::parseSimpleRequest(
+    const std::string& requestStr)
+{
+    ReceivedHttpRequest rv;
+
+    // Parse the first line (method, url, version)
+    size_t pos = requestStr.find(' ');
+    rv.method = requestStr.substr(0, pos);
+    size_t pos2 = requestStr.find(' ', pos + 1);
+    rv.path = requestStr.substr(pos + 1, pos2 - pos - 1);
+    rv.version = requestStr.substr(pos2 + 1, requestStr.find('\n') - pos2 - 1);
+
+    // Parse the headers
+    while ((pos = requestStr.find('\n', pos2)) != std::string::npos)
+    {
+        pos2 = requestStr.find('\n', pos + 1);
+        if (pos2 == std::string::npos)
+            break;
+        size_t colonPos = requestStr.find(':', pos + 1);
+        if (colonPos == std::string::npos || colonPos > pos2)
+            continue;
+        std::string key = requestStr.substr(pos + 1, colonPos - pos - 1);
+        std::string value =
+            requestStr.substr(colonPos + 1, pos2 - colonPos - 1);
+        rv.headers[key] = value;
+    }
+    return rv;
+}
+
+void SimpleTestHttpServer::handleAccept(
+    const boost::system::error_code& error,
+    std::shared_ptr<boost::asio::ip::tcp::socket> socket)
+{
+    if (error)
+    {
+        // Server stopping, no need to process this request.
+        return;
+    }
+    boost::asio::streambuf requestBuffer;
+    boost::asio::read_until(*socket, requestBuffer, "\r\n\r\n");
+
+    std::string requestStr((std::istreambuf_iterator<char>(&requestBuffer)),
+                           std::istreambuf_iterator<char>());
+    ReceivedHttpRequest request = parseSimpleRequest(requestStr);
+
+    // Response result.
+    std::string response = "HTTP/1.1 200 OK\r\n";
+
+    // Add some response headers.
+    for (const auto& header : responseHeaders)
+    {
+        response += header.first + ": " + header.second + "\r\n";
+    }
+
+    // Response body.
+    response += "\r\n";
+    response += responseStr;
+    boost::asio::write(*socket, boost::asio::buffer(response));
+    updateReceivedRequest(request);
+    startAccept();
+}
+
+void SimpleTestHttpServer::updateReceivedRequest(
+    const ReceivedHttpRequest& request)
+{
+    std::lock_guard<std::mutex> lock(requestsMutex);
+    requests.push_back(request);
+}
+
+void SimpleTestHttpServer::startAccept()
+{
+    auto socket = std::make_shared<boost::asio::ip::tcp::socket>(ioContext);
+    acceptor.async_accept(
+        *socket, boost::bind(&SimpleTestHttpServer::handleAccept, this,
+                             boost::asio::placeholders::error, socket));
+}
+
+}; // namespace redfish_client_daemon
---
+++ common/lfopenbmc/recipes-downstream/redfish-client/files/test/helper.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/system/error_code.hpp>
+
+namespace redfish_client_daemon
+{
+
+class SimpleTestHttpServer
+{
+  public:
+    struct ReceivedHttpRequest
+    {
+        std::string method;
+        std::string path;
+        std::string version;
+        std::unordered_map<std::string, std::string> headers;
+    };
+
+    SimpleTestHttpServer(
+        const std::string& responseStr,
+        const std::unordered_map<std::string, std::string>& responseHeaders);
+
+    ~SimpleTestHttpServer();
+
+    SimpleTestHttpServer(const SimpleTestHttpServer&) = delete;
+    SimpleTestHttpServer(SimpleTestHttpServer&&) = delete;
+    SimpleTestHttpServer& operator=(const SimpleTestHttpServer&) = delete;
+    SimpleTestHttpServer& operator=(SimpleTestHttpServer&&) = delete;
+
+    void stopListening();
+
+    unsigned short getPort();
+
+    std::vector<ReceivedHttpRequest> getReceivedRequests();
+
+  private:
+    static ReceivedHttpRequest parseSimpleRequest(
+        const std::string& requestStr);
+
+    void handleAccept(const boost::system::error_code& error,
+                      std::shared_ptr<boost::asio::ip::tcp::socket> socket);
+
+    void updateReceivedRequest(const ReceivedHttpRequest& request);
+
+    void startAccept();
+
+    std::string responseStr;
+    std::unordered_map<std::string, std::string> responseHeaders;
+    std::vector<ReceivedHttpRequest> requests;
+    std::mutex requestsMutex;
+    boost::asio::io_context ioContext;
+    boost::asio::ip::tcp::acceptor acceptor;
+    unsigned short port;
+    std::unique_ptr<std::thread> serverThread;
+    std::atomic<bool> serverThreadStopped{false};
+};
+
+}; // namespace redfish_client_daemon

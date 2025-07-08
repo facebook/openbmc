5918e4dc1dae4b4347ec4476fc055fc849a0d4ae
Amithash Prasad <amithash@meta.com>
2025-07-08 10:47:17 -0700

    bletchley15: Add initial CI tests
    Summary: As title.
    
    Test Plan:
    #YOLO
    
    Rollback Plan:
    
    Reviewed By: jamesatha
    
    Differential Revision: D77874787
    
    fbshipit-source-id: 1ca8bb7f57a0250bc91a4c478b1572e85a467068

---
+++ tests2/tests/bletchley15/test_process_running.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# Copyright 2018-present Facebook. All Rights Reserved.
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+import unittest
+
+from common.base_process_running_test import BaseProcessRunningTest
+from utils.test_utils import qemu_check
+
+
+class ProcessRunningTest(BaseProcessRunningTest, unittest.TestCase):
+    def set_processes(self):
+        self.expected_process = [
+            "sshd",
+            "rsyslogd",
+        ]

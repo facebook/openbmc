0effdaba0bc7b22dcb56434f3fbaa36e0ccfa319
Scott Smith <smithscott@meta.com>
2025-07-08 19:59:18 -0700

    fixmybmc: add global address check
    Summary: If a global address is not assigned then the switch will not be accessible for ssh. We can do more automated investigation here, but that will come later after discussions with Tao. For now we can at least quickly identify the problem.
    
    Test Plan:
    On a good switch:
    
    ```
    root@fboss325335965-oob:~# fixmybmc
    Ran 8 checks
    -----
    Result summary:
      Passed: 7
        provisioning_stable_image, eth0_up, global_address_assigned, recent_wedge_power_reset, recent_pemd_shutdown, recent_fscd_shutdown, sshd_running
      Skipped: 1
        fpga_ver_ok
    -----
    No remediations available
    ```
    
    Bad switch:
    ```
    root@ssw015-oob:~# fixmybmc
    Ran 8 checks
    -----
    Result summary:
      Passed: 7
        fpga_ver_ok, provisioning_stable_image, eth0_up, recent_wedge_power_reset, recent_pemd_shutdown, recent_fscd_shutdown, sshd_running
      Problems: 1
        global_address_assigned
    -----
    [  problem  ] global_address_assigned
      No global IPv6 address assigned to eth0 interface
      Remediation:
    
              Possible issues include front-panel cable not connected, bad eeprom
              image, management network down, or bad MAC/PHY settings.
    Remediations:
      Manual remediation recommended:
    
                Possible issues include front-panel cable not connected, bad eeprom
                image, management network down, or bad MAC/PHY settings.
    root@ssw015-oob:~#
    ```
    
    Rollback Plan:
    
    Reviewed By: RomanChoporov-Meta
    
    Differential Revision:
    D77891548
    
    Privacy Context Container: L1188897
    
    fbshipit-source-id: c19444e012a95da68edacb59936afdd56e5101d2

--- common/recipes-utils/fixmybmc/files/fixmybmc/modules/interface.py
+++ common/recipes-utils/fixmybmc/files/fixmybmc/modules/interface.py
@@ -32,3 +32,31 @@ def restart_eth0():
     """
     run_cmd("ip link set eth0 down".split(" "), capture_output=False)
     run_cmd("ip link set eth0 up".split(" "), capture_output=False)
+
+
+@bmcCheck
+def global_address_assigned():
+    """
+    Check if a global IPv6 address is assigned to eth0 interface
+    """
+    check_cmd = "ifconfig eth0"
+
+    status = run_cmd(check_cmd.split(" "))
+    if status.returncode != 0:
+        return Error(
+            description="Unable to get eth0 interface information",
+            cmd_status=status,
+        )
+
+    # Look for IPv6 addresses with Scope:Global
+    lines = status.stdout.split("\n")
+    for line in lines:
+        if "inet6 addr:" in line and "Scope:Global" in line:
+            return None  # Found global address, check passes
+
+    return Problem(
+        description="No global IPv6 address assigned to eth0 interface",
+        manual_remediation="""
+        Possible issues include front-panel cable not connected, bad eeprom
+        image, management network down, or bad MAC/PHY settings.""",
+    )

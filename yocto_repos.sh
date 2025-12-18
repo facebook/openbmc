ea0d58352449ec4b8c2e46c55c547099e0b16e08
generatedunixname89002005368312 <generatedunixname89002005368312@meta.com>
2025-12-18 08:41:31 -0800

    yocto: update master:aaa7b5e54
    Summary:
    Update master at aaa7b5e54
    update-submodule: yocto/lf-master
    
    Test Plan: CIT build passes.
    
    Reviewed By: jamesatha
    
    Differential Revision: D89471524
    
    fbshipit-source-id: 4e906adfcdaf40409607fa5b95443060324256fa

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:ae239aaf9
+    lf-openbmc:aaa7b5e54
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b

b21d5bf516a56987e0aa8ecb373eecf6791bcd42
srv-openbmc-robot <srv-openbmc-robot@arista.com>
2026-01-28 09:46:13 -0800

    Add yocto version cit test
    Summary:
    # Description
    
    Adds a basic CIT test which verifies the yocto version of the OpenBMC image by checking the /etc/os-release file on the BMC. Support is currently for fbdarwin and meru images, enforcing master branch.
    
    # Motivation
    
    Meta's OpenBMC test requirements mention covering this test case, and it seems well-suited to a CIT test.
    
    X-link: https://github.com/facebookexternal/openbmc.arista/pull/444
    
    Test Plan:
    New tests:
    - tests.meru.test_yocto_version.YoctoVersionTest.test_yocto_version
    - tests.fbdarwin.test_yocto_version.YoctoVersionTest.test_yocto_version
    
    Ran the cit tests for meru:
    ```
    ----------------------------------------------------------------------
    Ran 16 tests in 173.031s
    
    OK
    ```
    fbdarwin:
    ```
    ----------------------------------------------------------------------
    Ran 23 tests in 147.245s
    
    OK (skipped=3
    ```
    
    Reviewed By: somasun
    
    Differential Revision: D91696038
    
    Pulled By: joancaneus
    
    fbshipit-source-id: 829e85f59a8a242dfb196c4b1e1dd4524ca25a7f

---
+++ tests2/common/base_yocto_version_test.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
+#
+# This program file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in a file named COPYING; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+#
+
+
+import re
+import unittest
+from abc import abstractmethod
+
+from utils.cit_logger import Logger
+from utils.shell_util import run_shell_cmd
+from utils.test_utils import qemu_check
+
+
+@unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
+class BaseYoctoVersionTest(object):
+    def setUp(self):
+        Logger.start(name=self._testMethodName)
+        self.os_release_cmd = "cat /etc/os-release"
+        self.yocto_version = None
+
+    def tearDown(self):
+        Logger.info(f"Finished logging for {self._testMethodName}")
+
+    @abstractmethod
+    def set_yocto_version(self):
+        pass
+
+    def test_yocto_version(self):
+        self.set_yocto_version()
+        self.assertNotEqual(self.yocto_version, None, "Yocto version not set")
+        Logger.info("Executing cmd={}".format(self.os_release_cmd))
+        info = run_shell_cmd(cmd=self.os_release_cmd)
+        version_pattern = rf"VERSION_CODENAME=\"(.*)\""
+        version_match = re.search(version_pattern, info)
+        self.assertTrue(version_match, "Yocto version not found")
+        actual_version = version_match.group(1)
+        self.assertEqual(
+            self.yocto_version,
+            actual_version,
+            f"Yocto version mismatch. Expected: {self.yocto_version}, "
+            f"Actual: {actual_version}",
+        )

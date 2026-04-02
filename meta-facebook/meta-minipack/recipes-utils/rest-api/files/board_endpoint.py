dde88f80cc8a5b2495f35f84919b5b91c4d6adbb
generatedunixname1256125339367757 <noreply+1256125339367757@fb.com>
2026-04-02 14:58:23 -0700

    fbcode/openbmc/openbmc/meta-facebook [B]
    Reviewed By: fried
    
    Differential Revision: D99315364
    
    fbshipit-source-id: 2fba22fcf0edfdfb14f8909270b508474581b4e4

--- meta-facebook/meta-minipack/recipes-utils/rest-api/files/board_endpoint.py
+++ meta-facebook/meta-minipack/recipes-utils/rest-api/files/board_endpoint.py
@@ -42,63 +42,63 @@ from common_utils import dumps_bytestr
 class boardApp_Handler:
     # Handler to reset usb-to-i2c
     async def rest_usb2i2c_reset_hdl(self, request):
-        usb2i2c = await asyncio.get_event_loop().run_in_executor(
+        usb2i2c = await asyncio.get_running_loop().run_in_executor(
             None, rest_usb2i2c_reset.set_usb2i2c
         )
         return web.json_response(usb2i2c, dumps=dumps_bytestr)
 
     # Handler for sys/mb/fruid/scm resource endpoint
     async def rest_fruid_scm_hdl(self, request):
-        fruid_scm = await asyncio.get_event_loop().run_in_executor(
+        fruid_scm = await asyncio.get_running_loop().run_in_executor(
             None, rest_fruid_scm.get_fruid_scm
         )
         return web.json_response(fruid_scm, dumps=dumps_bytestr)
 
     # Handler for sys/pim_present resource endpoint
     async def rest_pim_present_hdl(self, request):
-        pim_present = await asyncio.get_event_loop().run_in_executor(
+        pim_present = await asyncio.get_running_loop().run_in_executor(
             None, rest_pim_present.get_pim_present
         )
         return web.json_response(pim_present, dumps=dumps_bytestr)
 
     # Handler for sys/piminfo resource endpoint
     async def rest_piminfo_hdl(self, request):
-        piminfo = await asyncio.get_event_loop().run_in_executor(
+        piminfo = await asyncio.get_running_loop().run_in_executor(
             None, rest_piminfo.get_piminfo
         )
         return web.json_response(piminfo)
 
     # Handler for sys/pim_serial resource endpoint
     async def rest_pimserial_hdl(self, request):
-        pimserial = await asyncio.get_event_loop().run_in_executor(
+        pimserial = await asyncio.get_running_loop().run_in_executor(
             None, rest_pimserial.get_pimserial
         )
         return web.json_response(pimserial)
 
     # Handler for sys/pimstatus resource endpoint
     async def rest_pimstatus_hdl(self, request):
-        pimstatus = await asyncio.get_event_loop().run_in_executor(
+        pimstatus = await asyncio.get_running_loop().run_in_executor(
             None, rest_pimstatus.get_pimstatus
         )
         return web.json_response(pimstatus)
 
     # Handler for sys/firmware_info_pim resource endpoint
     async def rest_firmware_info_pim_hdl(self, request):
-        firmware_info = await asyncio.get_event_loop().run_in_executor(
+        firmware_info = await asyncio.get_running_loop().run_in_executor(
             None, rest_firmware_info_pim.get_firmware_info
         )
         return web.json_response(firmware_info, dumps=dumps_bytestr)
 
     # Handler for sys/firmware_info_scm resource endpoint
     async def rest_firmware_info_scm_hdl(self, request):
-        firmware_info = await asyncio.get_event_loop().run_in_executor(
+        firmware_info = await asyncio.get_running_loop().run_in_executor(
             None, rest_firmware_info_scm.get_firmware_info
         )
         return web.json_response(firmware_info, dumps=dumps_bytestr)
 
     # Handler for sys/firmware_info_all resource endpoint
     async def rest_firmware_info_all_hdl(self, request):
-        all_fw_ver = await asyncio.get_event_loop().run_in_executor(
+        all_fw_ver = await asyncio.get_running_loop().run_in_executor(
             None, rest_fw_ver.get_all_fw_ver
         )
         return web.json_response(all_fw_ver, dumps=dumps_bytestr)
@@ -108,7 +108,7 @@ class boardApp_Handler:
     async def rest_peutil_hdl(self, request):
         path = request.rel_url.path
         pim = re.search(r"pim(\d)", path).group(1)
-        peutil = await asyncio.get_event_loop().run_in_executor(
+        peutil = await asyncio.get_running_loop().run_in_executor(
             None, rest_peutil.get_peutil, pim
         )
 
@@ -116,7 +116,7 @@ class boardApp_Handler:
 
     # Handler for sys/mb/seutil resource endpoint
     async def rest_seutil_hdl(self, request):
-        seutil = await asyncio.get_event_loop().run_in_executor(
+        seutil = await asyncio.get_running_loop().run_in_executor(
             None, rest_seutil.get_seutil
         )
         return web.json_response(seutil, dumps=dumps_bytestr)
@@ -132,13 +132,13 @@ class boardApp_Handler:
 
     # Handler for sys/system_led_info resource endpoint
     async def rest_system_led_info_hdl(self, request):
-        system_led_info = await asyncio.get_event_loop().run_in_executor(
+        system_led_info = await asyncio.get_running_loop().run_in_executor(
             None, rest_system_led_info.get_system_led_info
         )
         return web.json_response(system_led_info, dumps=dumps_bytestr)
 
     async def rest_pimdetails_hdl(self, request):
-        pim_details = await asyncio.get_event_loop().run_in_executor(
+        pim_details = await asyncio.get_running_loop().run_in_executor(
             None, rest_pim_details.get_pim_details
         )
         return pim_details

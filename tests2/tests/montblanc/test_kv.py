6e395126c8d0efe4ef0d30c434c536398d93941b
Jiamin Fan <jiaminfan@celestica.com>
2026-06-01 11:09:03 -0700

    montblanc: cit: Fix test_kv.KvTest.test_kv_get_key failing on Netlake2.0
    Summary:
    # Description
    Netlake 2.0 (the new COMe) uses an AMD EPYC Fire Range CPU. AMD CPUs do not have microcode, turbo mode or type. And according to the behavior of the AMD platform's BIOS, the DIMM inventory only has DIMM0 and DIMM1.
    
    # Motivation
    This PR is used to identify whether NL2.0 COMe is being used, then select the corresponding key-value key group for testing.
    
    X-link: https://github.com/facebookexternal/openbmc.celestica/pull/1927
    
    Test Plan:
    SCP the entire tests2 folder to the target BMC run directory, then run kv key test.
    Test on NL2.0 COMe based platform:
    ```
    root@bmc:~# cd /run/tests2/
    root@bmc:/run/tests2# python cit_runner.py --run-test tests.montblanc.test_kv.KvTest.test_kv_get_key
    test_kv_get_key (tests.montblanc.test_kv.KvTest.test_kv_get_key) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.059s
    
    OK
    ```
    Test on NL1.0 COMe based platform:
    ```
    root@bmc:~# cd /run/tests2/
    root@bmc:/run/tests2# vi tests/montblanc/test_kv.py
    root@bmc:/run/tests2# python cit_runner.py --run-test tests.montblanc.test_kv.KvTest.test_kv_get_key
    test_kv_get_key (tests.montblanc.test_kv.KvTest.test_kv_get_key) ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.067s
    
    OK
    ```
    
    Reviewed By: Scott8440
    
    Differential Revision: D106287534
    
    Pulled By: ezeob002
    
    fbshipit-source-id: 4c46973713de4d6c718c9c33681c0cac5afcd5e2

--- tests2/tests/montblanc/test_kv.py
+++ tests2/tests/montblanc/test_kv.py
@@ -26,41 +26,78 @@ from utils.test_utils import qemu_check
 @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
 class KvTest(BaseKvTest, unittest.TestCase):
     def set_kv_keys(self):
+        # We should only expect to have kv in python env on BMC, but we can't put
+        # these imports at the top level as they'd break test discovery (P2206870769)
+        from kv import FPERSIST, kv_get
+
+        # used kv_get in binary mode,
+        # because some kv keep the data as binary
+        try:
+            product_name = kv_get(
+                "sys_config/fru1_cpu0_product_name", flags=FPERSIST, binary=True
+            )
+        except Exception as e:
+            self.fail(
+                f"Failed to read 'sys_config/fru1_cpu0_product_name' from KV, error: {e}"
+            )
+
+        # Common keys for both Intel and AMD
         self.kv_keys = [
             "fru1_restart_cause",
             "server_boot_order",
             "server_cpu_ppin",
             "sysfw_ver_server",
             "sys_config/fru1_cpu0_basic_info",
-            "sys_config/fru1_cpu0_micro_code",
             "sys_config/fru1_cpu0_product_name",
-            "sys_config/fru1_cpu0_turbo_mode",
-            "sys_config/fru1_cpu0_type",
             "sys_config/fru1_dimm0_location",
             "sys_config/fru1_dimm0_manufacturer_id",
             "sys_config/fru1_dimm0_part_name",
             "sys_config/fru1_dimm0_serial_num",
             "sys_config/fru1_dimm0_speed",
             "sys_config/fru1_dimm0_type",
-            "sys_config/fru1_dimm10_location",
-            "sys_config/fru1_dimm10_speed",
-            "sys_config/fru1_dimm10_type",
-            "sys_config/fru1_dimm2_location",
-            "sys_config/fru1_dimm2_manufacturer_id",
-            "sys_config/fru1_dimm2_part_name",
-            "sys_config/fru1_dimm2_serial_num",
-            "sys_config/fru1_dimm2_speed",
-            "sys_config/fru1_dimm2_type",
-            "sys_config/fru1_dimm4_location",
-            "sys_config/fru1_dimm4_speed",
-            "sys_config/fru1_dimm4_type",
-            "sys_config/fru1_dimm6_location",
-            "sys_config/fru1_dimm6_speed",
-            "sys_config/fru1_dimm6_type",
-            "sys_config/fru1_dimm8_location",
-            "sys_config/fru1_dimm8_speed",
-            "sys_config/fru1_dimm8_type",
         ]
+
+        if b"AMD" in product_name:
+            self.kv_keys.extend(
+                [
+                    "sys_config/fru1_dimm1_location",
+                    "sys_config/fru1_dimm1_manufacturer_id",
+                    "sys_config/fru1_dimm1_part_name",
+                    "sys_config/fru1_dimm1_serial_num",
+                    "sys_config/fru1_dimm1_speed",
+                    "sys_config/fru1_dimm1_type",
+                ]
+            )
+        elif b"Intel(R)" in product_name:
+            self.kv_keys.extend(
+                [
+                    "sys_config/fru1_cpu0_micro_code",
+                    "sys_config/fru1_cpu0_turbo_mode",
+                    "sys_config/fru1_cpu0_type",
+                    "sys_config/fru1_dimm2_location",
+                    "sys_config/fru1_dimm2_manufacturer_id",
+                    "sys_config/fru1_dimm2_part_name",
+                    "sys_config/fru1_dimm2_serial_num",
+                    "sys_config/fru1_dimm2_speed",
+                    "sys_config/fru1_dimm2_type",
+                    "sys_config/fru1_dimm4_location",
+                    "sys_config/fru1_dimm4_speed",
+                    "sys_config/fru1_dimm4_type",
+                    "sys_config/fru1_dimm6_location",
+                    "sys_config/fru1_dimm6_speed",
+                    "sys_config/fru1_dimm6_type",
+                    "sys_config/fru1_dimm8_location",
+                    "sys_config/fru1_dimm8_speed",
+                    "sys_config/fru1_dimm8_type",
+                    "sys_config/fru1_dimm10_location",
+                    "sys_config/fru1_dimm10_speed",
+                    "sys_config/fru1_dimm10_type",
+                ]
+            )
+        else:
+            self.fail(
+                f"Unknown CPU type: '{product_name.decode(errors='ignore')}'. Cannot determine which KV keys to test."
+            )
 
     def test_kv_get_key(self):
         # We should only expect to have kv in python env on BMC, but we can't put

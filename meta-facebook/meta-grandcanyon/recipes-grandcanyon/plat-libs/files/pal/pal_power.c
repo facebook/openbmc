3762127fce17a48b12a2e67a3fc6a60a891eb61d
Joseph Shih <joseph_shih@wiwynn.com>
2026-06-04 11:08:01 -0700

    bgc2: Fix error propagation for BIC power status query
    Summary:
    # [Issue Description]
    
    Related to GC20T5T7-231
    When bic_get_server_power_status() fails due to no BIC response, the server DC power status should remain unknown. The existing error handling still contains an outdated comment indicating that the status is reset to SERVER_12V_ON. The function also returns a hard-coded -1, which discards the original error code returned by bic_get_server_power_status().
    
    # [Root Cause]
    
    The previous implementation was updated to stop forcing the server power status to SERVER_12V_ON when BIC does not respond, but the related comment and error return handling were not updated accordingly.
    
    # [Solution]
    
    Remove the outdated comment that states the server power status will be reset to SERVER_12V_ON. Return the original error code from bic_get_server_power_status() instead of returning a hard-coded -1. Preserve the warning log to indicate that the server DC power status is unknown when BIC does not respond.
    
    # [Test Log]
    
    Build grandcanyon2-image successfully.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/3591
    
    Reviewed By: amithash
    
    Differential Revision: D107472049
    
    fbshipit-source-id: 60b8fc2b2aedfa872095047fe2820526c5983a60

--- meta-facebook/meta-grandcanyon/recipes-grandcanyon/plat-libs/files/pal/pal_power.c
+++ meta-facebook/meta-grandcanyon/recipes-grandcanyon/plat-libs/files/pal/pal_power.c
@@ -484,9 +484,8 @@ pal_get_server_power(uint8_t fru, uint8_t *status) {
 #ifdef CONFIG_GRANDCANYON2
   ret = bic_get_server_power_status(status);
   if (ret < 0) {
-    // if bic not responding, we reset status to SERVER_12V_ON
     syslog(LOG_WARNING, "%s(): BIC no response, server DC power status is unknown\n", __func__);
-    return -1;
+    return ret;
   }
 #else
   ret = pal_get_fpga_ver_cache(I2C_BS_FPGA_BUS, GET_FPGA_VER_ADDR, server_fpga_ver);

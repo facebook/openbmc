a027e8e621d1d83d3105dd98cd4b6e72a08ae305
James Athappilly <jamesatha@meta.com>
2026-06-16 11:59:55 -0700

    Remove unused variable
    Summary: title
    
    Test Plan: builds succeed
    
    Reviewed By: jagpalgill
    
    Differential Revision: D108783004
    
    fbshipit-source-id: c39e736058e0fdc31b5578eafa3fea060d7da942

--- common/recipes-core/ipmid/files/lan.c
+++ common/recipes-core/ipmid/files/lan.c
@@ -47,7 +47,7 @@ void plat_lan_init(lan_config_t *lan)
   struct ifaddrs *ifaddr, *ifa;
   struct sockaddr_in *addr;
   struct sockaddr_in6 *addr6;
-  int family, n, i;
+  int family, i;
   unsigned long ip;
   unsigned char *ip6, *netmask6;
   int sd;
@@ -65,7 +65,7 @@ void plat_lan_init(lan_config_t *lan)
   }
 
   pal_get_eth_intf_name(intf_name);
-  for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
+  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
     if (ifa->ifa_addr == NULL) {
       continue;
     }

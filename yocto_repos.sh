8756cdfdacf017e5e6d633cfe9898330e8054070
generatedunixname89002005368312 <generatedunixname89002005368312@meta.com>
2026-06-26 09:35:14 -0700

    yocto: update master:57e422871
    Summary:
    Update master at 57e422871
    update-submodule: yocto/lf-master
    
     ---
    # Automated patch results
    
    *Successfully updated patches for*:
      meta-facebook/meta-yosemite4/recipes-phosphor/state/phosphor-state-manager_%.bbappend
      meta-facebook/meta-sanmiguel/recipes-phosphor/configuration/entity-manager_%.bbappend
      common/lfopenbmc/recipes-phosphor/interfaces/bmcweb_%.bbappend
    
     ---
    
    Test Plan: CIT build passes.
    
    Reviewed By: jamesatha
    
    Differential Revision: D109848988
    
    fbshipit-source-id: dceaa5fd2cd251baf56ca885e3592ac00f5ab45e

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:d5db47058
+    lf-openbmc:57e422871
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b

423dd875a8c486a5dd0f803dcfe02fb1431ba6df
Uchenna Ezeobi <uezeobi@meta.com>
2026-07-21 16:11:49 -0700

    Skip test_usb0_v6_x86_interface (usb0 not BMC<->x86 path)
    Summary:
    janga's test_usb0_v6_x86_interface pings the x86 over usb0 IPv6, but usb0 isn't
    the BMC<->x86 communication path on janga, so the ping fails (1 != 0) and reds
    out the openbmc CIT conveyor.
    
    Test Plan: CIT
    
    Reviewed By: joancaneus
    
    Differential Revision: D113087404
    
    fbshipit-source-id: 6565101db3e1c2ae963b15b79ef60e55fd4583f8

--- tests2/tests/janga/test_interface.py
+++ tests2/tests/janga/test_interface.py
@@ -20,11 +20,10 @@
 import unittest
 
 from common.base_interface_test import BaseInterfaceTest
-from utils.test_utils import qemu_check
 
 
 class InterfaceTest(BaseInterfaceTest, unittest.TestCase):
-    @unittest.skipIf(qemu_check(), "test env is QEMU, skipped")
+    @unittest.skip("usb0 is not used for BMC<->x86 communication on janga")
     def test_usb0_v6_x86_interface(self):
         """
         Tests usb0 v6 interface ping to x86 side

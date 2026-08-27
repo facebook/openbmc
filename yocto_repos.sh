631ff4936bfad3e46ae863f64dc3fc293c49f4cb
James Athappilly <jamesatha@meta.com>
2026-08-27 14:29:58 -0700

    yocto: update master:4d4663531
    Summary:
    Update master at 4d4663531
    update-submodule: yocto/lf-master
    
     ---
    # Automated patch results
    
    **Rebase errors**:
      ./facebook/scripts/fix-patches --bbappend common/lfopenbmc/recipes-phosphor/interfaces/bmcweb_%.bbappend
    
      ./facebook/scripts/fix-patches --bbappend common/lfopenbmc/recipes-phosphor/modbus/phosphor-modbus_%.bbappend
    
    *Successfully updated patches for*:
      meta-facebook/meta-santabarbara/recipes-phosphor/configuration/entity-manager_%.bbappend
      meta-facebook/meta-rainiera7/recipes-phosphor/interfaces/bmcweb_%.bbappend
      meta-facebook/meta-rainiera7/recipes-kernel/linux/linux-aspeed_%.bbappend
      meta-facebook/recipes-devtools/qemu/qemu-master_git.inc
    
     ---
    
    Test Plan: CIT build passes.
    
    Reviewed By: amithash
    
    Differential Revision: D117695110
    
    fbshipit-source-id: 969c63c8dd73154c144f8474d6be703ff55720c0

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:743fc6a67
+    lf-openbmc:4d4663531
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b

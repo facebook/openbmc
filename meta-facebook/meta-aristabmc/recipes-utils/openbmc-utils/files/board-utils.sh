e46a2d38c42c9dd38bd0443ac9a21c453c19b344
Adam Calabrigo <adamc@arista.com>
2026-09-25 10:43:14 -0700

    aristabmc: update board-utils support
    Summary:
    # Description
    
    Adds implementations for core `board-utils.sh` functions to provide:
    - userver power control
    - chassis power cycle
    - MAC address discovery
    - Ongoing firmware upgrade detection
    
    # Motivation
    
    The power control endpoints are needed for wedge_power.sh to work correctly.
    The mac endpoints are needed for the BMC MAC to be set correctly.
    The firmware upgrade detection is needed to prevent concurrent firmware upgrades.
    
    X-link: https://github.com/facebookexternal/openbmc.arista/pull/497
    
    Test Plan:
    aristabmc image builds and boots.
    
    wedge_power.sh works correctly. Verified on a system where the CPU baud rate was set to 115200, so used microcom to check CPU liveliness.
    
    After cold boot, CPU power is ON (and therefore non-standby domain as well):
    ```
    root@bmc:~# wedge_power.sh status
    Microserver power is on
    root@bmc:~# microcom -s 115200 /dev/ttyS1
    and required optimization
      SID BoraBora
      optimization not required
    Mounting SWIM Filesystem
      Optimization Default root squash found
      Optimization Default all squashes found
      Mounting optimization Default
    Switching rootfs
    ```
    
    Power CPU off, make sure it's off:
    ```
    root@bmc:~# wedge_power.sh off
    Power off x86 (userver) ... Done
    root@bmc:~# wedge_power.sh status
    Microserver power is off
    root@bmc:~# microcom -s 115200 /dev/ttyS1
    root@bmc:~#
    ```
    
    Power CPU back on:
    ```
    root@bmc:~# wedge_power.sh on
    Power on x86 (userver) ... Done
    root@bmc:~# microcom -s 115200 /dev/ttyS1
    
    [   10.999905] e2fsck on /mnt/flash took 9s
    [   11.017671] Running e2fsck on: /mnt/crash
    [   11.026827] e2fsck on /mnt/crash took 0s
    
    Aboot 22.0.0-ENG-47971262
    ```
    
    Reset CPU:
    ```
    root@bmc:~# wedge_power.sh reset
    Power reset x86 (userver) ... Done
    root@bmc:~# microcom -s 115200 /dev/ttyS1
    
    Found signature for initrd-i386-kdump
    Discovering SID and required optimization
      SID BoraBora
      optimization not required
    Mounting SWIM Filesystem
      Optimization Default root squash found
      Optimization Default all squashes found
      Mounting optimization Default
    ```
    MAC functions work:
    ```
    root@bmc:~# source /usr/local/bin/board-utils.sh
    root@bmc:~# bmc_mac_addr
    B8:A1:B8:AB:51:8B
    root@bmc:~# userver_mac_addr
    B8:A1:B8:AB:51:88
    ```
    
    Reviewed By: ezeob002
    
    Differential Revision: D120601641
    
    Pulled By: tao-ren
    
    fbshipit-source-id: 949e4c6180c2bd8de47e28d6bf15193648a8205f

--- meta-facebook/meta-aristabmc/recipes-utils/openbmc-utils/files/board-utils.sh
+++ meta-facebook/meta-aristabmc/recipes-utils/openbmc-utils/files/board-utils.sh
@@ -17,6 +17,27 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 
+# shellcheck disable=SC2034
+DS4520_BUS=8
+DS4520_IO0_REG=0xf8
+PWRCPLD_SYSFS_DIR="/sys/bus/i2c/drivers/pwrcpld/12-0043"
+SCM_CPU_READY_SYSFS="${PWRCPLD_SYSFS_DIR}/cpu_ready"
+CPU_CONTROL_SYSFS="${PWRCPLD_SYSFS_DIR}/cpu_control"
+SMB_NONSTDBY_PWR_SYSFS="${PWRCPLD_SYSFS_DIR}/smb_nonstdby_pwr"
+CPU_NONSTDBY_PWR_SYSFS="${PWRCPLD_SYSFS_DIR}/cpu_nonstdby_pwr"
+CPU_PWR_CYCLE_SYSFS="${PWRCPLD_SYSFS_DIR}/power_cycle"
+
+WEUTIL_CMD='weutil -e'
+
+wedge_is_cpu_personality() {
+    io_reg_val=$(i2cget -f -y "$DS4520_BUS" 0x52 "$DS4520_IO0_REG")
+    if [ "$((io_reg_val & 0x1))" = "1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 wedge_board_type() {
     echo 'aristabmc'
 }
@@ -27,36 +48,112 @@ wedge_board_rev() {
 }
 
 userver_power_is_on() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    isCpuReady="$(head -n 1 "$SCM_CPU_READY_SYSFS" 2> /dev/null)"
+    if [ "$isCpuReady" = "0x1" ]; then
+        return 0 # uServer is on
+    else
+        return 1
+    fi
+}
+
+userver_power_is_off() {
+    ! userver_power_is_on
+}
+
+wait_until() {
+    local deadline="$1" msg="$2"
+    shift 2
+    until "$@"; do
+        if [ "$SECONDS" -ge "$deadline" ]; then
+            echo "$msg"
+            return 62  # ETIME
+        fi
+        sleep 1
+    done
+}
+
+smb_power_on() {
+    echo 1 > "$SMB_NONSTDBY_PWR_SYSFS"
+    echo 1 > "$CPU_NONSTDBY_PWR_SYSFS"
 }
 
 userver_power_on() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    sync
+    sleep 0.5
+    smb_power_on
+
+    # Proceed with master core power ON unless user explicitly
+    # requested not to at a bootloader stage.
+    # Note: set bmc_only variable at uboot with:
+    # setenv bootargs "${bootargs} bmc_only=yes"
+    # saveenv
+    if grep -q "bmc_only" /proc/cmdline; then
+        echo "Skipping userver power on as bootargs set to BMC only mode."
+        return 0
+    fi
+
+    # Power on using the cpld
+    echo 1 > "$CPU_CONTROL_SYSFS"
+
+    local deadline=$(( SECONDS + 15 ))
+    wait_until "$deadline" "userver failed to power on" userver_power_is_on
 }
 
 userver_power_off() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    # Power off using the cpld
+    echo 0 > "$CPU_CONTROL_SYSFS"
+
+    local deadline=$(( SECONDS + 15 ))
+    wait_until "$deadline" "userver failed to power off" userver_power_is_off
 }
 
 userver_reset() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    userver_power_off || return $?
+    userver_power_on
 }
 
 chassis_power_cycle() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    sleep 1
+    echo 0xDE > "$CPU_PWR_CYCLE_SYSFS"
+    sleep 8
+
+    # The chassis shall be reset now... if not, we are in trouble
+    echo " Failed"
+    return 254
 }
 
 bmc_mac_addr() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    local mac_offset
+
+    mac_base=$(userver_mac_addr)
+    mac_base_hex=$(echo "$mac_base" |  tr '[:lower:]' '[:upper:]' | tr -d ':')
+    mac_dec=$(printf '%d\n' 0x"$mac_base_hex")
+    mac_offset=3
+    mac_dec=$((mac_dec + mac_offset))
+
+    # Convert base to MAC format
+    mac_hex=$(printf '%X\n' "$mac_dec")
+    mac=$(echo "$mac_hex" | sed 's/../&:/g;s/:$//')
+    echo "$mac"
 }
 
+# shellcheck disable=SC2120
 userver_mac_addr() {
-    echo "FIXME: feature not implemented!!"
-    return 1
+    local eeprom_source
+    eeprom_source="scm"
+    # support v4 or v5/v6 eeprom version
+    $WEUTIL_CMD "$eeprom_source" | grep -E '(Extended|CPU) MAC B' | awk -F': ' '{print $2}'
+}
+
+FWUPGRADE_PIDFILE="/var/run/firmware_upgrade.pid"
+check_fwupgrade_running()
+{
+    exec 200>$FWUPGRADE_PIDFILE
+    flock -n 200 || (echo "Another FW upgrade is running" && exit 1)
+    ret=$?
+    if [ $ret -eq 1 ]; then
+      exit 1
+    fi
+    pid=$$
+    echo $pid 1>&200
 }
